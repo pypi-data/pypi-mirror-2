06f46b50ba28c684e230ee3107a3d838b07b98e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package Products.Doormat/0.1.1/Products.Doormat-0.1.1.tar.gz

---
+++ packages/Products.Doormat/Products.Doormat-0.1.1.tar.gz/Products.Doormat-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.1'
+
+setup(name='Products.Doormat',
+      version=version,
+      description="Adds a doormat viewlet and installs in in the Plone footer.  The links in the doormat are manageable as content.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read() +
+                       open(os.path.join("docs", "TODO.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Kees Hink',
+      author_email='hink@gw20e.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
