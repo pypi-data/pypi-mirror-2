0e36958b0d6da6a9ad7cd39e55f3a880cbb4c0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:45 +0000

    Add package zope.publisher/3.4.10/zope.publisher-3.4.10.tar.gz

---
+++ packages/zope.publisher/zope.publisher-3.4.10.tar.gz/zope.publisher-3.4.10/setup.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (c) 2006--2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.publisher package
+
+$Id: setup.py 110562 2010-04-06 08:47:32Z adamg $
+"""
+
+from setuptools import setup, find_packages
+
+
+long_description = (open('README.txt').read() +
+                    '\n\n' +
+                    open('CHANGES.txt').read())
+
+
+setup(name='zope.publisher',
+      version='3.4.10',
+      url='http://pypi.python.org/pypi/zope.publisher',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description="The Zope publisher publishes Python objects on the web.",
+      long_description=long_description,
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope',],
+      tests_require=['zope.testing'],
+      install_requires=['setuptools',
+                        'zope.component',
+                        'zope.event',
+                        'zope.exceptions',
+                        'zope.i18n',
+                        'zope.interface',
+                        'zope.location',
+                        'zope.proxy',
+                        'zope.security',
+                        'zope.testing',
+                        'zope.app.testing',
+                        'zope.deprecation',
+                        'zope.deferredimport'],
+      include_package_data=True,
+      zip_safe=False,
+      )
---
+++ packages/zope.publisher/zope.publisher-3.4.10.tar.gz/zope.publisher-3.4.10/src/zope/publisher/tests/test_publisher.py
@@ -0,0 +1,141 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Test Publisher
+
+$Id: test_publisher.py 101905 2009-07-14 14:43:14Z ulif $
+"""
+import unittest
+
+from zope import component
+from zope.publisher.publish import publish
+from zope.publisher.base import TestRequest
+from zope.publisher.base import DefaultPublication
+from zope.publisher.interfaces import Unauthorized, NotFound, DebugError
+from zope.publisher.interfaces import IPublication, IReRaiseException
+
+from zope.interface.verify import verifyClass
+from zope.interface import implementedBy
+
+from StringIO import StringIO
+
+
+class PublisherTests(unittest.TestCase):
+    def setUp(self):
+        class AppRoot(object):
+            """Required docstring for the publisher."""
+
+        class Folder(object):
+            """Required docstring for the publisher."""
+
+        class Item(object):
+            """Required docstring for the publisher."""
+            def __call__(self):
+                return "item"
+
+        class NoDocstringItem:
+            def __call__(self):
+                return "Yo! No docstring!"
+
+        self.app = AppRoot()
+        self.app.folder = Folder()
+        self.app.folder.item = Item()
+
+        self.app._item = Item()
+        self.app.noDocString = NoDocstringItem()
+
+    def _createRequest(self, path, **kw):
+        publication = DefaultPublication(self.app)
+        path = path.split('/')
+        path.reverse()
+        request = TestRequest(StringIO(''), **kw)
+        request.setTraversalStack(path)
+        request.setPublication(publication)
+        return request
+
+    def _publisherResults(self, path, **kw):
+        request = self._createRequest(path, **kw)
+        response = request.response
+        publish(request, handle_errors=False)
+        return response._result
+
+    def _registerExcAdapter(self, factory):
+        component.provideAdapter(factory, (Unauthorized,), IReRaiseException)
+
+    def _unregisterExcAdapter(self, factory):
+        gsm = component.getGlobalSiteManager()
+        gsm.unregisterAdapter(
+            factory=factory, required=(Unauthorized,),
+            provided=IReRaiseException)
+
+    def testImplementsIPublication(self):
+        self.failUnless(IPublication.providedBy(
+                            DefaultPublication(self.app)))
+
+    def testInterfacesVerify(self):
+        for interface in implementedBy(DefaultPublication):
+            verifyClass(interface, DefaultPublication)
+
+    def testTraversalToItem(self):
+        res = self._publisherResults('/folder/item')
+        self.failUnlessEqual(res, 'item')
+        res = self._publisherResults('/folder/item/')
+        self.failUnlessEqual(res, 'item')
+        res = self._publisherResults('folder/item')
+        self.failUnlessEqual(res, 'item')
+
+    def testUnderscoreUnauthorizedException(self):
+        self.assertRaises(Unauthorized, self._publisherResults, '/_item')
+
+    def testNotFoundException(self):
+        self.assertRaises(NotFound, self._publisherResults, '/foo')
+
+    def testDebugError(self):
+        self.assertRaises(DebugError, self._publisherResults, '/noDocString')
+
+    def testIReRaiseExceptionAdapters(self):
+        def dontReRaiseAdapter(context):
+            def shouldBeReRaised():
+                return False
+            return shouldBeReRaised
+
+        self._registerExcAdapter(dontReRaiseAdapter)
+        try:
+            self._publisherResults('/_item')
+        except Unauthorized:
+            self._unregisterExcAdapter(dontReRaiseAdapter)
+            self.fail('Unauthorized raised though this should '
+                            'not happen')
+        self._unregisterExcAdapter(dontReRaiseAdapter)
+
+        def doReRaiseAdapter(context):
+            def shouldBeReRaised():
+                return True
+            return shouldBeReRaised
+
+        self._registerExcAdapter(doReRaiseAdapter)
+        raised = True
+        try:
+            self._publisherResults('/_item')
+            raised = False
+        except:
+            pass
+        self._unregisterExcAdapter(doReRaiseAdapter)
+        self.failUnlessEqual(raised, True)
+            
+def test_suite():
+    loader = unittest.TestLoader()
+    return loader.loadTestsFromTestCase(PublisherTests)
+
+if __name__=='__main__':
+    unittest.TextTestRunner().run(test_suite())
