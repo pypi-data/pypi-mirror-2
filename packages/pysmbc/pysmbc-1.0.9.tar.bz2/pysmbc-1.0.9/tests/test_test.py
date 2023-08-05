59cefef41f11cde7f98336660876fb1d777a3f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package pysmbc/1.0.9/pysmbc-1.0.9.tar.bz2

---
+++ packages/pysmbc/pysmbc-1.0.9.tar.bz2/pysmbc-1.0.9/tests/test_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+def setUp():
+    print "setUp"
+    pass
+
+def tearDown():
+    print "tearDown"
+    pass
+
+def test_003():
+    print "003"
+    pass
+
+def test_002():
+    print "002"
+    pass
+
+def test_001():
+    print "001"
+    pass
+
+print "hello"
