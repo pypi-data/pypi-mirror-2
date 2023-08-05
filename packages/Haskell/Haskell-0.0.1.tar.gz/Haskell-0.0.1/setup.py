3c5d68490f5f40939afd501e4f5dd1a60b9fb365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:44 +0000

    Add package Haskell/0.0.1/Haskell-0.0.1.tar.gz

---
+++ packages/Haskell/Haskell-0.0.1.tar.gz/Haskell-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Haskell',
+      version='0.0.1',
+      description='Haskell-style currying in python',
+      author='Rev. Johnny Healey',
+      author_email='rev.null@gmail.com',
+      py_modules=['haskell']
+     )
+
+
