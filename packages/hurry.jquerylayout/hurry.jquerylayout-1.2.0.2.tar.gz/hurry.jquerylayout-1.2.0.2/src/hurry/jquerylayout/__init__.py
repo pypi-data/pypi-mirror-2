1e9fbbcfb94e6c9d77bf411a2c42b2fc1d013dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:58 +0000

    Add package hurry.jquerylayout/1.2.0.2/hurry.jquerylayout-1.2.0.2.tar.gz

---
+++ packages/hurry.jquerylayout/hurry.jquerylayout-1.2.0.2.tar.gz/hurry.jquerylayout-1.2.0.2/src/hurry/jquerylayout/__init__.py
@@ -0,0 +1,13 @@
+from hurry.resource import Library, ResourceInclusion
+from hurry.jquery import jquery
+from hurry.jqueryui import jqueryui
+
+jquerylayout_lib = Library('jquerylayout', 'jquerylayout-build')
+
+jquerylayout = ResourceInclusion(
+    jquerylayout_lib, 'jquery.layout.js',
+    depends=[jquery, jqueryui],
+    minified='jquery.layout.min.js')
+
+jquerylayout_css = ResourceInclusion(
+    jquerylayout_lib, 'layout.css')
