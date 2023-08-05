ffbb83a1cfeac0ce064001824b2c69654729e590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:43 +0000

    Add package logtools/0.4/logtools-0.4.tar.gz

---
+++ packages/logtools/logtools-0.4.tar.gz/logtools-0.4/logtools/_plot.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); 
+#  you may not use this file except in compliance with the License. 
+#  You may obtain a copy of the License at 
+#  
+#      http://www.apache.org/licenses/LICENSE-2.0 
+#     
+#  Unless required by applicable law or agreed to in writing, software 
+#  distributed under the License is distributed on an "AS IS" BASIS, 
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+#  See the License for the specific language governing permissions and 
+#  limitations under the License. 
+"""
+logtools._plot
+Plotting methods for logfiles
+"""
+
+import os
+import re
+import sys
+import logging
+from itertools import imap
+from random import randint
+from operator import itemgetter
+from optparse import OptionParser
+from abc import ABCMeta, abstractmethod
+
+from _config import logtools_config, interpolate_config, AttrDict
+
+__all__ = ['logplot_parse_args', 'logplot', 'logplot_main']
+
+class PlotBackend(object):
+    __metaclass__ = ABCMeta
+    
+    @abstractmethod
+    def plot(self, options, args, fh):
+        """Plot using backend implementation"""
+        
+class GChartBackend(PlotBackend):
+    """Google Chart API plotting backend.
+    uses the pygooglechart python package"""
+    
+    def __init__(self):
+        PlotBackend.__init__(self)
+        
+    def plot(self, options, args, fh):
+        """Plot using google charts api"""
+        try:
+            import pygooglechart
+        except ImportError:
+            logging.error("pygooglechart Python package must be installed to use the 'gchart' backend")
+            sys.exit(-1)
+
+        try:
+            chart = {
+                'pie': self._plot_pie,
+                'line': self._plot_line
+            }[options.type](options, args, fh)
+        except KeyError:
+            raise KeyError("Invalid plot type: '%s'" % options.type)
+        else:
+            if options.get('title', None):
+                chart.set_title(options.title)
+            if options.get('output', None):
+                chart.download(options.output)
+                
+            return chart
+
+    def _plot_line(self, options, args, fh):
+        """Plot a line chart"""
+        from pygooglechart import Chart, SimpleLineChart, Axis
+        
+        delimiter = options.delimiter
+        field = options.field
+        
+        pts = []
+        for l in imap(lambda x: x.strip(), fh):
+            splitted_line = l.split(delimiter)
+            k = int(splitted_line.pop(field-1))
+            pts.append((k, ' '.join(splitted_line)))
+        
+        if options.get('limit', None):
+            # Only wanna use top N samples by key, sort and truncate
+            pts = sorted(pts, key=itemgetter(0), reverse=True)[:options.limit]
+                      
+        if not pts:
+            raise ValueError("No data to plot")
+                        
+        max_y = max((v for v, label in pts))  
+        chart = SimpleLineChart(options.width, options.height,y_range=[0, max_y])
+        
+        # Styling
+        chart.set_colours(['0000FF'])
+        chart.fill_linear_stripes(Chart.CHART, 0, 'CCCCCC', 0.2, 'FFFFFF', 0.2)        
+        chart.set_grid(0, 25, 5, 5)
+        
+        data, labels = zip(*pts)
+        chart.add_data(data)
+        
+        # Axis labels
+        chart.set_axis_labels(Axis.BOTTOM, labels)
+        left_axis = range(0, max_y + 1, 25)
+        left_axis[0] = ''
+        chart.set_axis_labels(Axis.LEFT, left_axis)
+        
+        return chart
+        
+    def _plot_pie(self, options, args, fh):
+        """Plot a pie chart"""
+        from pygooglechart import PieChart3D, PieChart2D
+
+        delimiter = options.delimiter
+        field = options.field
+                
+        chart = PieChart2D(options.width, options.height)
+        pts = []
+        for l in imap(lambda x: x.strip(), fh):
+            splitted_line = l.split(delimiter)
+            k = int(splitted_line.pop(field-1))
+            pts.append((k, ' '.join(splitted_line), k))
+            
+        if options.get('limit', None):
+            # Only wanna use top N samples by key, sort and truncate
+            pts = sorted(pts, key=itemgetter(0), reverse=True)[:options.limit]
+            
+        if not pts:
+            raise ValueError("No data to plot")
+        
+        data, labels, legend = zip(*pts)
+        chart.add_data(data)
+        chart.set_pie_labels(labels)
+        if options.get('legend', None) is True:
+            chart.set_legend(map(str, legend))
+                        
+        return chart
+    
+
+def logplot_parse_args():
+    parser = OptionParser()
+    parser.add_option("-b", "--backend", dest="backend",  
+                      help="Backend to use for plotting. Currently available backends: 'gchart'")
+    parser.add_option("-T", "--type", dest="type",  
+                      help="Chart type. Available types: 'pie', 'histogram', 'line'." \
+                      "Availability might differ due to backend.")    
+    parser.add_option("-f", "--field", dest="field", type=int,
+                      help="Index of field to use as input for generating plot")
+    parser.add_option("-d", "--delimiter", dest="delimiter",
+                      help="Delimiter character for field-separation")
+    parser.add_option("-o", "--output", dest="output", help="Output filename")    
+    parser.add_option("-W", "--width", dest="width", type=int, help="Plot Width")   
+    parser.add_option("-H", "--height", dest="height", type=int, help="Plot Height")       
+    parser.add_option("-L", "--limit", dest="limit", type=int, 
+                      help="Only plot the top N rows, sorted decreasing by key")        
+    parser.add_option("-l", "--legend", dest="legend", action="store_true", 
+                      help="Render Plot Legend")
+    parser.add_option("-t", "--title", dest="title",
+                      help="Plot Title")     
+    
+    parser.add_option("-P", "--profile", dest="profile", default='logplot',
+                      help="Configuration profile (section in configuration file)")
+    
+    options, args = parser.parse_args()
+
+    # Interpolate from configuration
+    options.backend  = interpolate_config(options.backend, options.profile, 'backend')
+    options.type = interpolate_config(options.type, options.profile, 'type')
+    options.field  = interpolate_config(options.field, options.profile, 'field', type=int)
+    options.delimiter = interpolate_config(options.delimiter, options.profile, 'delimiter')
+    options.output = interpolate_config(options.output, options.profile, 'output', default=False)
+    options.width = interpolate_config(options.width, options.profile, 'width', type=int)
+    options.height = interpolate_config(options.height, options.profile, 'height', type=int)    
+    options.limit = interpolate_config(options.limit, options.profile, 'limit', type=int, default=False) 
+    options.legend = interpolate_config(options.legend, options.profile, 'legend', type=bool, default=False) 
+    options.title = interpolate_config(options.title, options.profile, 'title', default=False)
+
+    return AttrDict(options.__dict__), args
+
+def logplot(options, args, fh):
+    """Plot some index defined over the logstream,
+    using user-specified backend"""            
+    return {
+        "gchart":  GChartBackend()
+    }[options.backend].plot(options, args, fh)
+
+def logplot_main():
+    """Console entry-point"""
+    options, args = logplot_parse_args()
+    logplot(options, args, fh=sys.stdin.readlines())
+    return 0
---
+++ packages/logtools/logtools-0.4.tar.gz/logtools-0.4/logtools/test/test_logtools.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+"""Unit-test code for logtools"""
+
+import os
+import sys
+import unittest
+import logging
+from tempfile import mkstemp
+from StringIO import StringIO
+from operator import itemgetter
+
+from logtools import (filterbots, geoip, logsample, logsample_weighted, 
+                      logparse, logmerge, logplot)
+from logtools.parsers import *
+from logtools import logtools_config, interpolate_config, AttrDict
+
+logging.basicConfig(level=logging.INFO)
+
+class ConfigurationTestCase(unittest.TestCase):
+    def testInterpolation(self):
+        self.assertEqual(1, interpolate_config(1, 'bogus_sec', 'bogus_key'))
+        self.assertRaises(KeyError, interpolate_config, None, 'bogus_sec', 'bogus_key')
+
+
+class ParsingTestCase(unittest.TestCase):
+    def setUp(self):
+        self.clf_rows = [
+            '127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326',
+            '127.0.0.2 - jay [10/Oct/2000:13:56:12 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326'
+            ]
+        
+    def testAccessLog(self):
+        """Test Apache access_log format parser"""
+        parser = AccessLog()
+        parser.set_format(format='%h %l %u %t "%r" %>s %b')
+        self.assertRaises(ValueError, parser, 'example for invalid format')
+        for logrow in self.clf_rows:
+            parsed = parser(logrow)
+            self.assertNotEquals(parsed, None, "Could not parse line: %s" % str(logrow))
+            
+    def testCommonLogFormat(self):
+        """Test CLF Parser"""
+        parser = CommonLogFormat()
+        self.assertRaises(ValueError, parser, 'example for invalid format')
+        for logrow in self.clf_rows:
+            parsed = parser(logrow)
+            self.assertNotEquals(parsed, None, "Could not parse line: %s" % str(logrow))        
+        
+        
+    def testLogParse(self):
+        options = AttrDict({'parser': 'CommonLogFormat', 'field': 4})
+        fh = StringIO('\n'.join(self.clf_rows))
+        output = [l for l in logparse(options, None, fh)]
+        self.assertEquals(len(output), len(self.clf_rows), "Output size was not equal to input size!")
+        
+            
+class FilterBotsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.options = AttrDict({
+            "reverse": False,
+            "printlines": False,
+            "ip_ua_re": "^(?P<ip>.*?) - USER_AGENT:'(?P<ua>.*?)'",
+            "bots_ips": StringIO(),
+            "bots_ua": StringIO("\n".join([
+                "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+                "r'.*MSIECrawler'"
+                ]) + "\n")
+        })
+        self.fh = StringIO(
+            "127.0.0.1 - USER_AGENT:'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' - ...\n" \
+            "255.255.255.255 - USER_AGENT:'Mozilla' - ...\n" \
+            "1.1.1.1 - USER_AGENT:'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; MSIECrawler)'\n"
+        )
+
+    def testFiltering(self):
+        i=0
+        for l in filterbots(self.options, None, self.fh): 
+            i+=1
+        self.assertEquals(i, 1, "filterbots output size different than expected: %s" % str(i))
+
+
+class GeoIPTestCase(unittest.TestCase):
+    def setUp(self):
+        self.options = AttrDict({ 'ip_re': '^(.*?) -' })
+        
+        self.fh = StringIO(
+            "127.0.0.1 - USER_AGENT:'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' - ...\n" \
+            "255.255.255.255 - USER_AGENT:'Mozilla' - ...\n"
+        )
+
+    def testGeoIP(self):
+        try:
+            import GeoIP
+        except ImportError:
+            print >> sys.stderr, "GeoIP Python package not available - skipping geoip unittest."
+            return
+
+        output = [(geocode, ip, line) for geocode, ip, line in geoip(self.options, None, self.fh)]
+        self.assertEquals(len(output), 2, "Output size was different than expected: %s" % str(len(output)))
+
+        
+class SamplingTestCase(unittest.TestCase):
+    def setUp(self):
+        self.options = AttrDict({ 'num_samples': 1 })
+        self.weighted_opts = AttrDict({
+            'num_samples': 5,
+            'field': 1,
+            'delimiter': ' '
+        })
+        self.fh = StringIO("\n".join([
+            '5 five', '1 one', '300 threehundred', '500 fivehundred',
+            '0 zero', '-1 minusone', '670 sixhundredseventy', '1000 thousand',
+            '22 twentytwo', '80 eighty', '3 three'
+        ]))
+
+    def testUniformSampling(self):
+        output = [r for r in logsample(self.options, None, self.fh)]
+        self.assertEquals(len(output), self.options.num_samples, 
+                          "logsample output size different than expected: %s" % len(output))
+        
+    def testWeightedSampling(self):
+        output = [(k, r) for k, r in logsample_weighted(self.weighted_opts, None, self.fh)]
+        self.assertEquals(len(output), self.weighted_opts.num_samples, 
+                          "logsample output size different than expected: %s" % len(output))        
+
+class MergeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.tempfiles = [mkstemp(), mkstemp(), mkstemp()]
+        self.args = [fname for fh, fname in self.tempfiles]
+
+    def tearDown(self):
+        """Cleanup temporary files created by test"""
+        for fh, fname in self.tempfiles:
+            os.remove(fname)
+            
+    def testNumericMerge(self):
+        os.write(self.tempfiles[0][0], "\n".join(['1 one', '5 five', '300 threehundred', 
+                                            '500 fivehundred']))
+        os.write(self.tempfiles[1][0], "\n".join(['-1 minusone', '0 zero',
+                                            '670 sixhundredseventy' ,'1000 thousand']))
+        os.write(self.tempfiles[2][0], "\n".join(['3 three', '22 twentytwo', '80 eighty']))
+        
+        options = AttrDict({'delimiter': ' ', 'field': 1, 'numeric': True })
+        output = [(k, l) for k, l in logmerge(options, self.args)]
+        
+        self.assertEquals(len(output), 11, "Output size was not equal to input size!")
+        self.assertEquals(map(itemgetter(0), output), sorted(map(lambda x: int(x[0]), output)), 
+                          "Output was not numerically sorted!")
+        
+    def testLexicalMerge(self):
+        os.write(self.tempfiles[0][0], "\n".join(['1 one', '300 threehundred', '5 five', 
+                                            '500 fivehundred']))
+        os.write(self.tempfiles[1][0], "\n".join(['-1 minusone', '0 zero', '1000 thousand',
+                                            '670 sixhundredseventy']))
+        os.write(self.tempfiles[2][0], "\n".join(['22 twentytwo', '3 three', 
+                                            '80 eighty']))
+        
+        options = AttrDict({ 'delimiter': ' ', 'field': 1, 'numeric': False })
+        output = [(k, l) for k, l in logmerge(options, self.args)]
+        
+        self.assertEquals(len(output), 11, "Output size was not equal to input size!")
+        self.assertEquals(map(itemgetter(0), output), sorted(map(itemgetter(0), output)), 
+                          "Output was not lexically sorted!")
+        
+        
+class PlotTestCase(unittest.TestCase):
+    def setUp(self):
+        self.fh = StringIO("\n".join([
+            '5 five', '1 one', '300 threehundred', '500 fivehundred',
+            '0 zero', '-1 minusone', '670 sixhundredseventy', '1000 thousand',
+            '22 twentytwo', '80 eighty', '3 three'
+        ]))
+
+    def testGChart(self):
+        try:
+            import pygooglechart
+        except ImportError:
+            print >> sys.stderr, "pygooglechart Python package not available - skipping logplot gchart unittest."
+            return        
+        options = AttrDict({
+            'backend': 'gchart',
+            'output': False,
+            'limit': 10,
+            'field': 1,
+            'delimiter': ' ',
+            'legend': True,
+            'width': 600,
+            'height': 300
+        })        
+        chart = None
+        for plot_type in ('pie', 'line'):
+            self.fh.seek(0)
+            options['type'] = plot_type
+            chart = logplot(options, None, self.fh)
+            self.assertNotEquals(chart, None, "logplot returned None. Expected a Plot object")
+            
+        # Should raise ValueError here due to fh being at EOF
+        self.assertRaises(ValueError, logplot, options, None, self.fh)
+        
+        tmp_fh, tmp_fname = mkstemp()
+        chart.download(tmp_fname)
+        os.remove(tmp_fname)
+    
+
+if __name__ == "__main__":
+    unittest.main()
