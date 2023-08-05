2e8e7372f80eac70dab14c6bd1abdb350fc28079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package vs.dashboardmanager/0.2.6/vs.dashboardmanager-0.2.6.tar.gz

---
+++ packages/vs.dashboardmanager/vs.dashboardmanager-0.2.6.tar.gz/vs.dashboardmanager-0.2.6/vs/dashboardmanager/__init__.py
@@ -0,0 +1,15 @@
+################################################################
+# vs.dashboardmanager
+# (C) 2010, Veit Schiele & Andreas Jung
+# Published under the GNU Public Licence V 2 (GPL 2)
+################################################################
+
+
+import patches
+from zope.i18nmessageid import MessageFactory
+
+MF = MessageFactory('vs.dashboardmanager')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
+
