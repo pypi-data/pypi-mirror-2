886fae1e7d6486521b827f2f8a6c09cb3b99e5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package django-feedmap/0.1/django-feedmap-0.1.tar.gz

---
+++ packages/django-feedmap/django-feedmap-0.1.tar.gz/django-feedmap-0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+try:
+    long = open('README.rst').read()
+except:
+    long = ''
+
+setup(
+    name = "django-feedmap",
+    version = '0.1',
+    url = 'http://opensource.washingtontimes.com/projects/django-feedmap/',
+    author = 'Justin Quick',
+    author_email = 'justquick@gmail.com',
+    description = 'Use a register command to generate RSS and Atom feeds as well as a corresponding Sitemaps. Integrates easily with ShareThis',
+    long_description = long,
+    packages = ['feedmap','feedmap.templatetags']
+)
+
