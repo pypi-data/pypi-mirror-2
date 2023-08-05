76ee7f5eac28b4386752c7199b786e1d6a0c782b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:19 +0000

    Add package libel/0.1.6/libel-0.1.6.tar.gz

---
+++ packages/libel/libel-0.1.6.tar.gz/libel-0.1.6/libel/merge_dict.py
@@ -0,0 +1,8 @@
+def merge_dict(Left, Right):
+    if not Right:
+        return
+    for key in Right:
+        if isinstance(Right[key], dict) and Left.has_key(key):
+            merge(Left[key], Right[key])
+        else:
+            Left[key] = Right[key]
