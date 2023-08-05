3576376ef86688ef37371e2a95656eed38343a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package zopeskel.niteoweb/0.1a1/zopeskel.niteoweb-0.1a1.tar.gz

---
+++ packages/zopeskel.niteoweb/zopeskel.niteoweb-0.1a1.tar.gz/zopeskel.niteoweb-0.1a1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1a1'
+
+setup(name='zopeskel.niteoweb',
+      version=version,
+      description="Paster templates for standard NiteoWeb Plone projects",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        ],
+      keywords='plone zopeskel',
+      author='NiteoWeb Ltd.',
+      author_email='info@niteoweb.com',
+      url='http://packages.python.org/zopeskel.niteoweb',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zopeskel'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'PasteScript',
+          'ZopeSkel',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [paste.paster_create_template]
+      niteoweb_project = zopeskel.niteoweb.niteoweb:NiteoWeb
+      """,
+      )
