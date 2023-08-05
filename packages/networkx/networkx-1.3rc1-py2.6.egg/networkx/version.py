3f7ff09bfef687af22b9c8fcef593af9054ca185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:19 +0000

    Add package networkx/1.3rc1/networkx-1.3rc1-py2.6.egg

---
+++ packages/networkx/networkx-1.3rc1-py2.6.egg/networkx/version.py
@@ -0,0 +1,11 @@
+"""
+Version information for NetworkX, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+__version__ = '1.3rc1'
+__revision__ = None
+__date__ = 'Fri Aug 20 19:21:18 2010'
+
