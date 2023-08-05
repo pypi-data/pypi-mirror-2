4e4c4f13f1e5efea83fb70784cdc1cda63de1e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package modulegraph/0.8/modulegraph-0.8.tar.gz

---
+++ packages/modulegraph/modulegraph-0.8.tar.gz/modulegraph-0.8/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+try:
+    import setuptools
+
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+
+from setuptools import setup
+import sys
+
+VERSION = '0.8'
+DESCRIPTION = "Python module dependency analysis tool"
+LONG_DESCRIPTION = """
+modulegraph determines a dependency graph between Python modules primarily
+by bytecode analysis for import statements.
+
+modulegraph uses similar methods to modulefinder from the standard library,
+but uses a more flexible internal representation, has more extensive 
+knowledge of special cases, and is extensible.
+
+NEWS
+====
+
+0.8
+---
+
+This is a minor feature release
+
+Features:
+
+- Initial support for Python 3.x
+
+- It is now possible to run the test suite
+  using ``python setup.py test``.
+
+  (The actual test suite is still fairly minimal though)
+"""
+
+CLASSIFIERS = filter(None, map(str.strip,
+"""                 
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Software Development :: Build Tools
+""".splitlines()))
+
+if sys.version_info[0] == 3:
+    extra_args = dict(use_2to3=True)
+else:
+    extra_args = dict()
+
+
+
+setup(
+    name="modulegraph",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+    author="Bob Ippolito",
+    author_email="bob@redivi.com",
+    maintainer="Ronald Oussoren",
+    maintainer_email="ronaldoussoren@mac.com",
+    url="http://undefined.org/python/#modulegraph",
+    license="MIT License",
+    packages=['modulegraph'],
+    platforms=['any'],
+    install_requires=["altgraph>=0.7"],
+    zip_safe=True,
+    test_suite='test',
+    **extra_args
+)
