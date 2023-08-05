30c5bad5c53b62f5e6d15f5f45a595dd44aab100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:08 +0000

    Add package garlicsim_wx/0.3/garlicsim_wx-0.3-py2.6.egg

---
+++ packages/garlicsim_wx/garlicsim_wx-0.3-py2.6.egg/garlicsim_wx/__init__.py
@@ -0,0 +1,39 @@
+# Copyright 2009-2010 Ram Rachum. No part of this program may be used, copied or
+# distributed without explicit written permission from Ram Rachum.
+
+'''
+garlicsim_wx, a wxPython GUI for garlicsim.
+
+The final goal of this project is to become a fully-fledged application for
+working with simulations, friendly enough that it may be used by
+non-programmers.
+
+This program is intended for Python versions 2.5 and 2.6.
+'''
+
+import bootstrap
+
+import wx
+
+from application_window import ApplicationWindow
+from gui_project import GuiProject
+
+__all__ = ['ApplicationWindow', 'GuiProject', 'start']
+
+__version__ = '0.3'
+
+def start():
+    '''
+    Start the gui.
+    '''
+    app = wx.PySimpleApp()
+    my_app_win = ApplicationWindow(None, -1, "GarlicSim", size=(600, 600))
+
+    '''
+    import cProfile
+    cProfile.run("app.MainLoop()")
+    '''
+    app.MainLoop()
+    
+if __name__ == "__main__":
+    start()
