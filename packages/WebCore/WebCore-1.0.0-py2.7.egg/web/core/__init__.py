79c014b41d8004314f4a6b9fa273f02cbef3a471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:42 +0000

    Add package WebCore/1.0.0/WebCore-1.0.0-py2.7.egg

---
+++ packages/WebCore/WebCore-1.0.0-py2.7.egg/web/core/__init__.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+"""
+
+"""
+
+import sys
+from web import release
+from web.core.dialects import Dialect, Controller, RESTMethod
+from web.core.application import Application
+from web.utils.dictionary import adict
+from webob import exc as http
+from web.core.middleware import middleware
+
+from paste.registry import StackedObjectProxy
+
+
+__all__ = [
+        'Application', 'Dialect', 'Controller', 'RESTMethod',
+        'http', 'i18n', 'middleware'
+        'config', 'request', 'response', 'cache', 'session', 'translator', 'namespace']
+
+
+config = adict()
+
+request = StackedObjectProxy(name="request")
+response = StackedObjectProxy(name="response")
+cache = StackedObjectProxy(name="cache")
+session = StackedObjectProxy(name="session")
+
+translator = StackedObjectProxy(name="translator")
+
+namespace = dict(
+        web = adict(
+                request = request,
+                response = response,
+                cache = cache,
+                session = session,
+                i18n = translator,
+                release = release,
+                config = config
+            )
+    )
+
+# To allow 'from web.core.http import ...' statements, we have to register web.core.http as a module.
+sys.modules['web.core.http'] = http
