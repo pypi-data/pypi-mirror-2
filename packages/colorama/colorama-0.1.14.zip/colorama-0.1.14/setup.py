89aa308e4b42962b84ee8a09cbd2ef99bd09413c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package colorama/0.1.14/colorama-0.1.14.zip

---
+++ packages/colorama/colorama-0.1.14.zip/colorama-0.1.14/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from os.path import dirname, join
+from distutils.core import setup
+
+from colorama import __version__
+
+
+name = 'colorama'
+
+
+def get_long_description(filename):
+    readme = join(dirname(__file__), filename)
+    return open(readme).read()
+
+
+setup(
+    name=name,
+    version=__version__,
+    description="Cross-platform colored terminal text.",
+    long_description=get_long_description('README.txt'),
+    keywords='color colour terminal text ansi windows crossplatform xplatform',
+    author='Jonathan Hartley',
+    author_email='tartley@tartley.com',
+    url='http://code.google.com/p/colorama/',
+    license='BSD',
+    packages=[name],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Terminals',
+    ]
+    # see classifiers http://pypi.python.org/pypi?%3Aaction=list_classifiers
+)
+
