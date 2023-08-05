da52c6b79ad3b2bafda9a1bf99f5316086eb0518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package RecordSilo/0.3/RecordSilo-0.3.tar.gz

---
+++ packages/RecordSilo/RecordSilo-0.3.tar.gz/RecordSilo-0.3/setup.py
@@ -0,0 +1,13 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name="RecordSilo",
+      version="0.3",
+      description="An adaptation of a pairtree store, each object with simple JSON keyvalue manifest and crude versioning.",
+      long_description="An adaptation of a pairtree store, each object with simple JSON keyvalue manifest and crude versioning. Designed to be used as a repository of harvested records from OAI-PMH based services and the like. As of version 0.3, it now includes an RDF-enhanced version of the Silo - RDFSilo.",
+      author="Ben O'Steen",
+      author_email="bosteen@gmail.com",
+      packages=find_packages(exclude='tests'),
+      install_requires=['pairtree>0.4.12', 'rdfobject', 'simplejson', 'datetime'],
+      )
