0684ff9d7cd41a8c44e68ec20bb8ecf4e998c96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package cdent/0.5.5/cdent-0.5.5.tar.gz

---
+++ packages/cdent/cdent-0.5.5.tar.gz/cdent-0.5.5/cdent/compiler.py
@@ -0,0 +1,49 @@
+"""\
+Meta class for the C'Dent compiler
+"""
+
+from cdent import __version__
+
+id_to_language = {
+    'as':   "ActionScript",
+    'cd':   "C'Dent",
+    'cpp':  'C++',
+    'go':   'Go',
+    'java': 'Java',
+    'js':   'JavaScript',
+    'php':  'PHP',
+    'pm':   'Perl',
+    'pm6':  'Perl 6',
+    'py':   'Python',
+    'py3':  'Python 3',
+    'rb':   'Ruby',
+}
+
+id_to_class = {
+    'as':   'as_',
+    'cd.yaml': 'cdent.yaml',
+    'cd.xml':  'cdent.xml',
+    'cd.json': 'cdent.json',
+    'cd.js': 'javascript',
+    'cd.pm6': 'perl6',
+    'cd.py': 'python',
+    'cpp':  'cpp',
+    'go':   'go',
+    'java': 'java',
+    'js':   'javascript',
+    'php':  'php',
+    'pm':   'perl',
+    'pm6':  'perl6',
+    'py':   'python',
+    'py3':  'python3',
+    'rb':   'ruby',
+}
+
+def language(id):
+    return id_to_language[id]
+
+def class_(id):
+    return id_to_class[id]
+
+def version():
+    return __version__
