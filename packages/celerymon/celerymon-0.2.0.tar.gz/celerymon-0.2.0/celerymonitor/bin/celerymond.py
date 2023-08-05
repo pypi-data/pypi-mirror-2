f38fe0d23eb7e3c7a586cff5c6048b33efe08e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:53 +0000

    Add package celerymon/0.2.0/celerymon-0.2.0.tar.gz

---
+++ packages/celerymon/celerymon-0.2.0.tar.gz/celerymon-0.2.0/celerymonitor/bin/celerymond.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+"""celerymon
+
+.. program:: celerymon
+
+.. cmdoption:: -P, --port
+
+    Port the webserver should listen to. Default: ``8989``.
+
+.. cmdoption:: -f, --logfile
+
+    Path to log file. If no logfile is specified, ``stderr`` is used.
+
+.. cmdoption:: -l, --loglevel
+
+    Logging level, choose between ``DEBUG``, ``INFO``, ``WARNING``,
+    ``ERROR``, ``CRITICAL``, or ``FATAL``.
+
+"""
+import os
+import sys
+import traceback
+import optparse
+
+import celery
+from celery import conf
+from celery import platform
+from celery.log import emergency_error
+from celery.utils import info
+
+from celerymonitor.service import MonitorService
+
+STARTUP_INFO_FMT = """
+Configuration ->
+    . broker -> %(conninfo)s
+    . webserver -> http://localhost:%(http_port)s
+""".strip()
+
+OPTION_LIST = (
+    optparse.make_option('-f', '--logfile', default=conf.CELERYMON_LOG_FILE,
+            action="store", dest="logfile",
+            help="Path to log file."),
+    optparse.make_option('-l', '--loglevel',
+            default=conf.CELERYMON_LOG_LEVEL,
+            action="store", dest="loglevel",
+            help="Choose between DEBUG/INFO/WARNING/ERROR/CRITICAL/FATAL."),
+    optparse.make_option('-P', '--port',
+            action="store", type="int", dest="http_port", default=8989,
+            help="Port the webserver should listen to."),
+)
+
+
+def run_monitor(loglevel=conf.CELERYMON_LOG_LEVEL,
+        logfile=conf.CELERYMON_LOG_FILE, http_port=8989, **kwargs):
+    """Starts the celery monitor."""
+
+    print("celerymon %s is starting." % celery.__version__)
+
+    # Setup logging
+    if not isinstance(loglevel, int):
+        loglevel = conf.LOG_LEVELS[loglevel.upper()]
+
+    # Dump configuration to screen so we have some basic information
+    # when users sends e-mails.
+    print(STARTUP_INFO_FMT % {
+            "http_port": http_port,
+            "conninfo": info.format_broker_info(),
+    })
+
+    from celery.log import setup_logger, redirect_stdouts_to_logger
+    print("celerymon has started.")
+    arg_start = "manage" in sys.argv[0] and 2 or 1
+    platform.set_process_title("celerymon",
+                               info=" ".join(sys.argv[arg_start:]))
+
+    def _run_monitor():
+        logger = setup_logger(loglevel, logfile)
+        monitor = MonitorService(logger=logger, http_port=http_port)
+
+        try:
+            monitor.start()
+        except Exception, e:
+            emergency_error(logfile,
+                    "celerymon raised exception %s: %s\n%s" % (
+                            e.__class__, e, traceback.format_exc()))
+
+    _run_monitor()
+
+
+def parse_options(arguments):
+    """Parse the available options to ``celerymon``."""
+    parser = optparse.OptionParser(option_list=OPTION_LIST)
+    options, values = parser.parse_args(arguments)
+    return options
+
+
+def main():
+    options = parse_options(sys.argv[1:])
+    run_monitor(**vars(options))
+
+if __name__ == "__main__":
+    main()
---
+++ packages/celerymon/celerymon-0.2.0.tar.gz/celerymon-0.2.0/celerymonitor/handlers/api.py
@@ -0,0 +1,90 @@
+from functools import wraps
+
+import anyjson
+from tornado.web import RequestHandler, Application
+
+from celery import states
+from celery.task.control import revoke
+
+from celery.events.state import state
+
+
+def JSON(fun):
+
+    @wraps(fun)
+    def _write_json(self, *args, **kwargs):
+        content = fun(self, *args, **kwargs)
+        self.write(anyjson.serialize(content))
+
+    return _write_json
+
+
+class APIHandler(RequestHandler):
+
+    def __init__(self, *args, **kwargs):
+        super(APIHandler, self).__init__(*args, **kwargs)
+        self.set_header("Content-Type", "application/javascript")
+
+
+def api_handler(fun):
+
+    @JSON
+    def get(self, *args, **kwargs):
+        return fun(self, *args, **kwargs)
+
+    return type(fun.__name__, (APIHandler, ), {"get": get})
+
+
+@api_handler
+def task_state(request, task_id):
+    task = state.tasks[task_id]
+    if task.state in states.EXCEPTION_STATES:
+        return task.info(extra=["traceback"])
+    return task.info()
+
+
+@api_handler
+def list_tasks(request):
+    return state.tasks_by_timestamp()
+
+
+@api_handler
+def list_tasks_by_name(request, name):
+    return state.tasks_by_type(name)
+
+
+@api_handler
+def list_task_types(request):
+    return state.task_types()
+
+
+@api_handler
+def list_workers(request):
+    return state.alive_workers()
+
+
+@api_handler
+def list_worker_tasks(request, hostname):
+    return state.list_worker_tasks(hostname)
+
+
+class RevokeTaskHandler(APIHandler):
+
+    SUPPORTED_METHODS = ["POST"]
+
+    @JSON
+    def post(self):
+        task_id = self.get_argument("task_id")
+        revoke(task_id)
+        return {"ok": True}
+
+
+API = [
+       (r"/task/name/$", list_task_types),
+       (r"/task/name/(.+?)", list_tasks_by_name),
+       (r"/task/$", list_tasks),
+       (r"/revoke/task/", RevokeTaskHandler),
+       (r"/task/(.+)", task_state),
+       (r"/worker/", list_workers),
+       (r"/worker/(.+?)/tasks", list_worker_tasks),
+]
