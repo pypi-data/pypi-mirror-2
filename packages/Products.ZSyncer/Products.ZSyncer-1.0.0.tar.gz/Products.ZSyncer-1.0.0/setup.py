e87a5cbc0e37a343a5233bc61f8a61be999cd7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:48 +0000

    Add package Products.ZSyncer/1.0.0/Products.ZSyncer-1.0.0.tar.gz

---
+++ packages/Products.ZSyncer/Products.ZSyncer-1.0.0.tar.gz/Products.ZSyncer-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2004 Paul M. Winkler and contributors.
+# Portions copyright (c) 2010 Simplistix Ltd.
+# See license.txt for license details.
+
+import os
+from setuptools import setup,find_packages
+
+base_dir = os.path.dirname(__file__)
+
+setup(
+    name='Products.ZSyncer',
+    version='1.0.0',
+    author='Paul M. Winkler, Chris Withers',
+    author_email='chris@simplistix.co.uk',
+    description=("ZSyncer is a project that allows live zope objects "
+                 "to be synchronized from one Zope to another without "
+                 "doing the tedious export / transfer / import dance"),
+    long_description=open(os.path.join(base_dir,'docs','description.txt')).read(),
+    url='http://pypi.python.org/pypi/Products.ZSyncer',
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    ],    
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires = (
+        'Zope2',
+        ),
+    extras_require=dict(
+        test=[
+            'testfixtures',
+            ],
+        )
+    )
