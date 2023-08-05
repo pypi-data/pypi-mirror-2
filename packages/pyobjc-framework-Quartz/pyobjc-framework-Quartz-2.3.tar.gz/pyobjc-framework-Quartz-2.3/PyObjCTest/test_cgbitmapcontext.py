bb42da903eb7da12afb6e18f049a79f5534d58b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:06 +0000

    Add package pyobjc-framework-Quartz/2.3/pyobjc-framework-Quartz-2.3.tar.gz

---
+++ packages/pyobjc-framework-Quartz/pyobjc-framework-Quartz-2.3.tar.gz/pyobjc-framework-Quartz-2.3/PyObjCTest/test_cgbitmapcontext.py
@@ -0,0 +1,42 @@
+
+from PyObjCTools.TestSupport import *
+from Quartz.CoreGraphics import *
+import array
+
+class TestCGBitmapContext (TestCase):
+    def testFunctions(self):
+        bytes_val = array.array('B', (0 for i in xrange(100*80*4)))
+        self.assertIsInstance(bytes_val, array.array)
+        self.assertEqual(len(bytes_val), 100*80*4)
+        ctx = CGBitmapContextCreate(bytes_val, 100, 80, 8, 400, CGColorSpaceCreateDeviceRGB(), kCGImageAlphaPremultipliedLast)
+        self.assertIsInstance(ctx, CGContextRef)
+
+        buf = CGBitmapContextGetData(ctx)
+        self.assertIsInstance(buf, objc.varlist)
+        self.assertIsInstance(buf[0], bytes)
+
+        self.assertEqual(CGBitmapContextGetWidth(ctx), 100)
+        self.assertEqual(CGBitmapContextGetHeight(ctx), 80)
+        self.assertEqual(CGBitmapContextGetBitsPerComponent(ctx), 8)
+        self.assertEqual(CGBitmapContextGetBitsPerPixel(ctx), 32)
+        self.assertEqual(CGBitmapContextGetBytesPerRow(ctx), 400)
+
+        v = CGBitmapContextGetColorSpace(ctx)
+        self.assertIsInstance(v, CGColorSpaceRef)
+
+        v = CGBitmapContextGetAlphaInfo(ctx)
+        self.assertIsInstance(v, (int, long))
+
+        v = CGBitmapContextGetBitmapInfo(ctx)
+        self.assertIsInstance(v, (int, long))
+
+        img = CGBitmapContextCreateImage(ctx)
+        self.assertIsInstance(img, CGImageRef)
+
+
+    def testFunctions106_(self):
+        self.fail("CGBitmapContextCreateWithData: manual wrapper")
+
+
+if __name__ == "__main__":
+    main()
