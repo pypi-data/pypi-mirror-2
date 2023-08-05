645700f00c036e92f7f34fffd31f89d6a08cd07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:18 +0000

    Add package bigdoorkit/0.1.3/bigdoorkit-0.1.3.tar.gz

---
+++ packages/bigdoorkit/bigdoorkit-0.1.3.tar.gz/bigdoorkit-0.1.3/src/bigdoorkit/resources/currency.py
@@ -0,0 +1,12 @@
+from bigdoorkit.resources.base import BDResource
+
+class Currency(BDResource):
+    endpoint = "currency"
+
+    def __init__(self, **kw):
+        self.currency_type_id = kw.get('currency_type_id', None)
+        self.currency_type_description = kw.get('currency_type_description', None)
+        self.currency_type_title = kw.get('currency_type_title', None)
+        self.exchange_rate = kw.get('exchange_rate', None)
+        self.relative_weight = kw.arg('relative_weight', None)
+        super(Currency, self).__init__(**kw)
