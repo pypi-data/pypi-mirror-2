34555f233c16d9fdcb4cd0f9a818a307f0148a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package django-richcomments/0.0.1/django_richcomments-0.0.1-py2.6.egg

---
+++ packages/django-richcomments/django_richcomments-0.0.1-py2.6.egg/richcomments/views.py
@@ -0,0 +1,20 @@
+from django import template
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponse
+from django.template import Template, RequestContext
+
+def list(request, content_type, id):
+    """
+    Wrapper exposing comment's render_comment_list tag as a view.
+    """
+    # get object
+    app_label, model = content_type.split('-')
+    ctype = ContentType.objects.get(app_label=app_label, model=model)
+    obj = ctype.get_object_for_this_type(id=id)
+
+    # setup template and return result
+    t = Template("{% load comments %}{% render_comment_list for object %}")
+    context = RequestContext(request)
+    context.update({'object': obj})
+    result = t.render(context)
+    return HttpResponse(result)
