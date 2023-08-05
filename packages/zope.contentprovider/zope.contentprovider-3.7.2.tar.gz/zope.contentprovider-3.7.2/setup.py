5ee246c321e3aed94064dc8e5b3292285a06861c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package zope.contentprovider/3.7.2/zope.contentprovider-3.7.2.tar.gz

---
+++ packages/zope.contentprovider/zope.contentprovider-3.7.2.tar.gz/zope.contentprovider-3.7.2/setup.py
@@ -0,0 +1,70 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.contentprovider package
+
+$Id: setup.py 112694 2010-05-25 14:08:28Z tseaver $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.contentprovider',
+      version = '3.7.2',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Content Provider Framework for Zope Templates',
+      long_description=(
+          read('src', 'zope', 'contentprovider', 'README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 content provider",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.contentprovider',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.browserpage>=3.12',
+                'zope.testing',
+                ]),
+      install_requires=['setuptools',
+                        'zope.component',
+                        'zope.event',
+                        'zope.interface',
+                        'zope.location',
+                        'zope.publisher',
+                        'zope.schema',
+                        'zope.tales',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
