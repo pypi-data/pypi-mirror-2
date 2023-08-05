72aabfd49bd8e61ede0fc86b49949f83a0a73559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:37 +0000

    Add package gdm2setup/0.5.2-karmic/gdm2setup-0.5.2-karmic.tar.gz

---
+++ packages/gdm2setup/gdm2setup-0.5.2-karmic.tar.gz/gdm2setup-0.5.2-karmic/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+ 
+setup(name='gdm2setup',
+	version='0.5.2-karmic',
+	author='Garth Johnson',
+	author_email='growlf@biocede.com',
+	description='GDM2 Setup utility and libraries',
+	long_description=open('README').read(),
+	license='GPL2',
+	url='https://launchpad.net/gdm2setup/',
+	packages=['gdm2'],
+	package_data = {'gdm2': ['gdm2setup.ui']},
+
+	scripts=['gdm2setup'],
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Environment :: X11 Applications :: GTK',
+		'Environment :: X11 Applications :: Gnome',
+		'Intended Audience :: End Users/Desktop',
+		'Intended Audience :: Developers',
+		'Intended Audience :: System Administrators',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python',
+		'Topic :: Desktop Environment :: Gnome',
+		'Topic :: System :: Installation/Setup',
+		'Topic :: System :: Systems Administration',
+		'Topic :: Utilities',
+		],
+	data_files=[
+		('/usr/share/applications', ['gdm2setup.desktop']),
+		('/usr/bin', ['gdm2setup']),
+		]
+
+)
