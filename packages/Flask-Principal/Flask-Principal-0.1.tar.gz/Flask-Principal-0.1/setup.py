46719dc434667fa35d050a3141f6da58e62785d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package Flask-Principal/0.1/Flask-Principal-0.1.tar.gz

---
+++ packages/Flask-Principal/Flask-Principal-0.1.tar.gz/Flask-Principal-0.1/setup.py
@@ -0,0 +1,43 @@
+"""
+Flask Principal
+---------------
+
+Identity management for Flask.
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-Principal/>`_
+* `source <http://bitbucket.org/aafshar/flask-principal-main>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Principal',
+    version='0.1',
+    url='http://packages.python.org/Flask-Principal/',
+    license='MIT',
+    author='Ali Afshar',
+    author_email='aafshar@gmail.com',
+    description='Identity management for flask',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask', 'blinker'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
