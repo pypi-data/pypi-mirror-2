776eb232a3d24330c28e43b54f26c2ae0d7ca65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:56 +0000

    Add package django-request/0.3/django-request-0.3.tar.gz

---
+++ packages/django-request/django-request-0.3.tar.gz/django-request-0.3/request/router.py
@@ -0,0 +1,37 @@
+import re
+from django.core.cache import cache
+from django.utils.hashcompat import sha_constructor
+
+class RegexPattern(object):
+    def __init__(self, regex, name=''):
+        self.regex = re.compile(regex, re.UNICODE)
+        self.name = name
+    
+    def resolve(self, string):
+        match = self.regex.search(string)
+        if match:
+            return self.name, match.groupdict()
+
+class patterns(object):
+    def __init__(self, unknown, *args):
+        self.patterns = []
+        self.unknown = unknown
+        
+        for pattern in args:
+            if pattern.__class__ == str:
+                self.patterns.append(RegexPattern(pattern))
+            else:
+                self.patterns.append(RegexPattern(*pattern))
+    
+    def resolve(self, name):
+        key = 'request.%s' % sha_constructor(name).hexdigest()
+        c = cache.get(key)
+        if (c):
+            return c
+        
+        for pattern in self.patterns:
+            match = pattern.resolve(name)
+            if match:
+                cache.set(key, match)
+                return match
+        return self.unknown
---
+++ packages/django-request/django-request-0.3.tar.gz/django-request-0.3/request/settings.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
+
+REQUEST_ONLY_ERRORS = getattr(settings, 'REQUEST_ONLY_ERRORS', False)
+REQUEST_IGNORE_AJAX = getattr(settings, 'REQUEST_IGNORE_AJAX', False)
+REQUEST_IGNORE_IP = getattr(settings, 'REQUEST_IGNORE_IP', tuple())
+REQUEST_IGNORE_USERNAME = getattr(settings, 'REQUEST_IGNORE_USERNAME', tuple())
+REQUEST_IGNORE_PATHS = getattr(settings, 'REQUEST_IGNORE_PATHS', tuple())
+
+REQUEST_USE_HOSTED_MEDIA = getattr(settings, 'REQUEST_USE_HOSTED_MEDIA', True)
+REQUEST_MEDIA_PREFIX = getattr(settings, 'REQUEST_MEDIA_PREFIX', settings.MEDIA_URL)
+
+REQUEST_TRAFFIC_MODULES = getattr(settings, 'REQUEST_TRAFFIC_MODULES', (
+    'request.traffic.UniqueVisitor',
+    'request.traffic.UniqueVisit',
+    'request.traffic.Hit',
+))
+
+REQUEST_PLUGINS = getattr(settings, 'REQUEST_PLUGINS', (
+    'request.plugins.TrafficInformation',
+    'request.plugins.LatestRequests',
+    
+    'request.plugins.ActiveUsers',
+    'request.plugins.TopPaths',
+    'request.plugins.TopErrorPaths',
+    'request.plugins.TopReferrers',
+    'request.plugins.TopSearchPhrases',
+    'request.plugins.TopBrowsers',
+))
+
+try:
+    REQUEST_BASE_URL = getattr(settings, 'REQUEST_BASE_URL', 'http://%s' % Site.objects.get_current().domain)
+except:
+    REQUEST_BASE_URL = getattr(settings, 'REQUEST_BASE_URL', 'http://127.0.0.1')
