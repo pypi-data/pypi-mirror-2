9ad617042ce2c25b67918082190066047461636c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:42 +0000

    Add package plone.indexer/1.0/plone.indexer-1.0.zip

---
+++ packages/plone.indexer/plone.indexer-1.0.zip/plone.indexer-1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+setup(name='plone.indexer',
+      version=version,
+      description="Hooks to facilitate managing custom index values in "
+                  "Zope 2/CMF applications",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='plone cmf zope catalog index',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.indexer',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+          'zope.component',
+          'Products.CMFCore',
+      ],
+      )
