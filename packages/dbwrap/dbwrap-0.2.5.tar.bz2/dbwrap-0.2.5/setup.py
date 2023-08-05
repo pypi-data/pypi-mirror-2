1fe313ab4b96de401f043981216d7972b5b01270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:08 +0000

    Add package dbwrap/0.2.5/dbwrap-0.2.5.tar.bz2

---
+++ packages/dbwrap/dbwrap-0.2.5.tar.bz2/dbwrap-0.2.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+	name = "dbwrap",
+	version = "0.2.5",
+	py_modules = ["dbwrap"],
+	author = "Pianohacker",
+	author_email = "pianohacker@gmail.com",
+	description = "a simple convenience wrapper for DB-API connections",
+	url = "http://code.google.com/p/dbwrap/",
+	license = "LGPL",
+	zip_safe = True,
+);
