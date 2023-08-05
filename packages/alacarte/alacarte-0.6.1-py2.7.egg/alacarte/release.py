4f8c2f557cd0dcc2853b6f34675602575aebb84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package alacarte/0.6.1/alacarte-0.6.1-py2.7.egg

---
+++ packages/alacarte/alacarte-0.6.1-py2.7.egg/alacarte/release.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+"""Release information about the Common Templating Interface."""
+
+
+name = "alacarte"
+version = "0.6.1"
+
+summary = "A common templating interface for Python applications."
+description = """"""
+author = "Alice Bevan-McGregor"
+email = "alice@gothcandy.com"
+url = "http://github.com/GothAlice/alacarte/"
+download_url = "http://cheeseshop.python.org/pypi/alacarte/"
+copyright = "© 2009-2010 Alice Bevan-McGregor"
+license = "MIT"
