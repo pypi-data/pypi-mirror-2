f61324ef7fca7493c27c32212b0bbf1d00b00c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:36 +0000

    Add package django-boss/0.6.2/django-boss-0.6.2.tar.gz

---
+++ packages/django-boss/django-boss-0.6.2.tar.gz/django-boss-0.6.2/src/djboss/cli.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import sys
+import textwrap
+
+from django.utils.importlib import import_module
+
+from djboss.commands import Command
+
+
+class SettingsImportError(ImportError):
+    pass
+
+
+def get_settings():
+    sys.path.append(os.getcwd())
+    if 'DJANGO_SETTINGS_MODULE' in os.environ:
+        try:
+            return import_module(os.environ['DJANGO_SETTINGS_MODULE'])
+        except ImportError, exc:
+            raise SettingsImportError(textwrap.dedent("""\
+                There was an error importing the module specified by the
+                DJANGO_SETTINGS_MODULE environment variable. Make sure that it
+                refers to a valid and importable Python module."""), exc)
+    
+    try:
+        import settings
+    except ImportError, exc:
+        raise SettingsImportError(textwrap.dedent("""\
+            Couldn't import a settings module. Make sure that a `settings.py`
+            file exists in the current directory, and that it can be imported,
+            or that the DJANGO_SETTINGS_MODULE environment variable points
+            to a valid and importable Python module."""), exc)
+    return settings
+
+
+def find_commands(app):
+    """Return a dict of `command_name: command_obj` for the given app."""
+    
+    commands = {}
+    app_module = import_module(app) # Fail loudly if an app doesn't exist.
+    try:
+        commands_module = import_module(app + '.commands')
+    except ImportError:
+        pass
+    else:
+        for command in vars(commands_module).itervalues():
+            if isinstance(command, Command):
+                commands[command.name] = command
+    return commands
+
+
+def find_all_commands(apps):
+    """Return a dict of `command_name: command_obj` for all the given apps."""
+    
+    commands = {}
+    commands.update(find_commands('djboss'))
+    for app in apps:
+        commands.update(find_commands(app))
+    return commands
+
+
+def main():
+    try:
+        settings = get_settings()
+    except SettingsImportError, exc:
+        print >> sys.stderr, exc.args[0]
+        print >> sys.stderr
+        print >> sys.stderr, "The original exception was:"
+        print >> sys.stderr, '\t' + str(exc.args[1])
+        sys.exit(1)
+    
+    from django.core import management as mgmt
+    mgmt.setup_environ(settings)
+    
+    commands = find_all_commands(settings.INSTALLED_APPS)
+    
+    from djboss.parser import PARSER
+    
+    PARSER.set_defaults(settings=settings)
+    if settings.DEBUG:
+        PARSER.set_defaults(log_level='DEBUG')
+    else:
+        PARSER.set_defaults(log_level='WARN')
+    
+    args = PARSER.parse_args()
+    logging.root.setLevel(getattr(logging, args.log_level))
+    
+    # Call the command.
+    commands[args.command](args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/django-boss/django-boss-0.6.2.tar.gz/django-boss-0.6.2/src/djboss/commands.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+
+import functools
+import re
+import sys
+
+from djboss.parser import SUBPARSERS
+
+
+__all__ = ['Command', 'command', 'argument', 'APP_LABEL', 'MODEL_LABEL']
+
+
+class Command(object):
+    
+    """Wrapper to manage creation and population of sub-parsers on functions."""
+    
+    def __init__(self, function, **kwargs):
+        self.function = function
+        self.parser = self._make_parser(**kwargs)
+        self._init_arguments()
+    
+    add_argument = property(lambda self: self.parser.add_argument)
+    
+    def __call__(self, args):
+        return self.function(args)
+    
+    def name(self):
+        """The name of this command."""
+        
+        if hasattr(self.function, 'djboss_name'):
+            return self.function.djboss_name
+        else:
+            return self.function.__name__.replace('_', '-')
+    name = property(name)
+    
+    def help(self):
+        if hasattr(self.function, 'djboss_help'):
+            return self.function.djboss_help
+        elif getattr(self.function, '__doc__', None):
+            # Just the first line of the docstring.
+            return self.function.__doc__.splitlines()[0]
+    help = property(help)
+    
+    def description(self):
+        if hasattr(self.function, 'djboss_description'):
+            return self.function.djboss_description
+        elif getattr(self.function, '__doc__', None):
+            return self.function.__doc__
+    description = property(description)
+    
+    def _make_parser(self, **kwargs):
+        """Create and register a subparser for this command."""
+        
+        kwargs.setdefault('help', self.help)
+        kwargs.setdefault('description', self.description)
+        return SUBPARSERS.add_parser(self.name, **kwargs)
+    
+    def _init_arguments(self):
+        """Initialize the subparser with arguments stored on the function."""
+        
+        if hasattr(self.function, 'djboss_arguments'):
+            while self.function.djboss_arguments:
+                args, kwargs = self.function.djboss_arguments.pop()
+                self.add_argument(*args, **kwargs)
+
+
+def APP_LABEL(label=None, **kwargs):
+    
+    """
+    argparse type to resolve arguments to Django apps.
+    
+    Example Usage:
+    
+    *   `@argument('app', type=APP_LABEL)`
+    *   `@argument('app', type=APP_LABEL(empty=False))`
+    *   `APP_LABEL('auth')` => `<module 'django.contrib.auth' ...>`
+    """
+    
+    from django.db import models
+    from django.conf import settings
+    from django.utils.importlib import import_module
+    
+    if label is None:
+        return functools.partial(APP_LABEL, **kwargs)
+    
+    # `get_app('auth')` will return the `django.contrib.auth.models` module.
+    models_module = models.get_app(label, emptyOK=kwargs.get('empty', True))
+    if models_module is None:
+        for installed_app in settings.INSTALLED_APPS:
+            # 'app' should resolve to 'path.to.app'.
+            if installed_app.split('.')[-1] == label:
+                return import_module(installed_app)
+    else:
+        # 'path.to.app.models' => 'path.to.app'
+        return import_module(models_module.__name__.rsplit('.', 1)[0])
+
+
+def MODEL_LABEL(label):
+    
+    """
+    argparse type to resolve arguments to Django models.
+    
+    Example Usage:
+    
+    *   `@argument('app.model', type=MODEL_LABEL)
+    *   `MODEL_LABEL('auth.user')` => `<class 'django.contrib.auth.models.User'>`
+    """
+    
+    from django.db import models
+    
+    match = re.match(r'^([\w_]+)\.([\w_]+)$', label)
+    if not match:
+        raise TypeError
+    
+    model = models.get_model(*match.groups())
+    if not model:
+        raise ValueError
+    return model
+
+
+def command(*args, **kwargs):
+    """Decorator to declare that a function is a command."""
+    
+    def decorator(function):
+        return Command(function, **kwargs)
+    
+    if args:
+        return decorator(*args)
+    return decorator
+
+
+def argument(*args, **kwargs):
+    """Decorator to add an argument to a command."""
+    
+    def decorator(function):
+        if isinstance(function, Command):
+            func = function.function
+        else:
+            func = function
+        
+        if not hasattr(func, 'djboss_arguments'):
+            func.djboss_arguments = []
+        func.djboss_arguments.append((args, kwargs))
+        
+        return function
+    return decorator
+
+
+def manage(args):
+    """Run native Django management commands under djboss."""
+    
+    from django.core import management as mgmt
+    
+    OldOptionParser = mgmt.LaxOptionParser
+    class LaxOptionParser(mgmt.LaxOptionParser):
+        def __init__(self, *args, **kwargs):
+            kwargs['prog'] = 'djboss manage'
+            OldOptionParser.__init__(self, *args, **kwargs)
+    mgmt.LaxOptionParser = LaxOptionParser
+    
+    utility = mgmt.ManagementUtility(['djboss manage'] + args.args)
+    utility.prog_name = 'djboss manage'
+    utility.execute()
+
+# `prefix_chars=''` will stop argparse from interpreting the management
+# sub-command options as options on this command.
+manage = Command(manage, add_help=False, prefix_chars='')
+manage.add_argument('args', nargs='*')
