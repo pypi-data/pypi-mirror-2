12221fa4c2498a5de53685aff705ab34ec020d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:07 +0000

    Add package ndg-xacml/0.2/ndg_xacml-0.3-py2.6.egg

---
+++ packages/ndg-xacml/ndg_xacml-0.3-py2.6.egg/ndg/xacml/__init__.py
@@ -0,0 +1,13 @@
+"""NDG XACML package 
+
+NERC DataGrid
+"""
+__author__ = "P J Kershaw"
+__date__ = "16/03/10"
+__copyright__ = "(C) 2010 Science and Technology Facilities Council"
+__contact__ = "Philip.Kershaw@stfc.ac.uk"
+__license__ = "BSD - see LICENSE file in top-level directory"
+__contact__ = "Philip.Kershaw@stfc.ac.uk"
+__revision__ = "$Id: __init__.py 7087 2010-06-25 11:23:09Z pjkersha $"
+class XacmlError(Exception):
+    """Base class for XACML package exception types"""
