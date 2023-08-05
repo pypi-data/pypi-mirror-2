1244e3511edfcefeb83feee91abb16b15b46e161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package django-improved-inlines/0.1.5a/django-improved-inlines-0.1.5a.tar.gz

---
+++ packages/django-improved-inlines/django-improved-inlines-0.1.5a.tar.gz/django-improved-inlines-0.1.5a/setup.py
@@ -0,0 +1,29 @@
+import os
+from distutils.core import setup
+
+setup(
+    name = "django-improved-inlines",
+    version = "0.1.5a",
+    url = 'http://github.com/issackelly/django-improved-inlines',
+    license = 'BSD',
+    description = "Inline object rendering for django, based on django-basic-apps + filters + templates",
+    long_description=open("README.rst").read(),
+    author = 'Issac Kelly',
+    author_email = 'issac@kellycreativetech.com',
+    packages = [
+        'inlines',
+        'inlines.templatetags',
+    ],
+    requires = [
+        'BeautifulSoup',
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
