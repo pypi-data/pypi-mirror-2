111c3e0bf933d0000d31ec38d68d272c042b97cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:53 +0000

    Add package vs.dashboardmanager/0.2.9/vs.dashboardmanager-0.2.9.tar.gz

---
+++ packages/vs.dashboardmanager/vs.dashboardmanager-0.2.9.tar.gz/vs.dashboardmanager-0.2.9/vs/dashboardmanager/browser/portletpage.py
@@ -0,0 +1,25 @@
+################################################################
+# vs.dashboardmanager
+# (C) 2010, Veit Schiele & Andreas Jung
+# Published under the GNU Public Licence V 2 (GPL 2)
+################################################################
+
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.permissions import ManagePortal
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class Base(BrowserView):
+    """Base class for index page views
+    """
+
+class PortletPageView(Base):
+    """Default view for PortletPage """
+
+    def isManager(self):
+        mt = getToolByName(self, 'portal_membership')
+        member = mt.getAuthenticatedMember()
+        return member.has_permission(ManagePortal, self.context)
+    
+    __call__ = ViewPageTemplateFile('portletpage.pt')
