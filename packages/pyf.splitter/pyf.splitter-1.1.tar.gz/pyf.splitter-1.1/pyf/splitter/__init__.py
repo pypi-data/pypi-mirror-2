d78e9fd19e5759a413b34449d6f053686a3ac2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package pyf.splitter/1.1/pyf.splitter-1.1.tar.gz

---
+++ packages/pyf.splitter/pyf.splitter-1.1.tar.gz/pyf.splitter-1.1/pyf/splitter/__init__.py
@@ -0,0 +1,3 @@
+from pyf.splitter.inputsplitter import (
+        InputSplitterNode, get_input_item_flow, get_input_item_str_flow,
+        EndOfFileError, tokenize, InputSplitterError, get_splitter)
---
+++ packages/pyf.splitter/pyf.splitter-1.1.tar.gz/pyf.splitter-1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='pyf.splitter',
+      version=version,
+      description="Dataflow splitting system for PyF framework",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pyf'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
