031fc0297771971d9f79ac227759d59ee602d1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package disqus/0.0.2/disqus-0.0.2.tar.gz

---
+++ packages/disqus/disqus-0.0.2.tar.gz/disqus-0.0.2/disqus/__init__.py
@@ -0,0 +1,7 @@
+"""
+A python library for interacting with the Disqus commenting system.
+
+For integrating with your applications look at the 'Disqus' class.
+"""
+from api import *
+from cmdline import *
