70ea4089fdd08fb6c13b6d1e10000c89d01fc03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:06 +0000

    Add package collective.portlettabber/0.1.0/collective.portlettabber-0.1.0.tar.gz

---
+++ packages/collective.portlettabber/collective.portlettabber-0.1.0.tar.gz/collective.portlettabber-0.1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='collective.portlettabber',
+      version=version,
+      description="A jQuery plugin for Plone layouts that merge portlets together obtaining a single portlet with tabs",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: User Interfaces",
+        ],
+      keywords='plone tab portlet jquery',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://plone.org/products/collective.portlettabber',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
