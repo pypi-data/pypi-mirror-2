2895f0256f4b18cac77895a64acb451d339c9dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:30 +0000

    Add package tox/0.6/tox-0.6.tar.gz

---
+++ packages/tox/tox-0.6.tar.gz/tox-0.6/tox/__init__.py
@@ -0,0 +1,17 @@
+#
+__version__ = "0.6"
+
+import apipkg
+
+apipkg.initpkg(__name__, dict(
+    cmdline   = '._cmdline:main',
+    exception = dict(
+        ConfigError = '._config:ConfigError',
+        InvocationError = '._exception:InvocationError',
+        MissingDependency= '._exception:MissingDependency',
+        UnsupportedInterpreter = '._exception:UnsupportedInterpreter',
+        InterpreterNotFound = '._exception:InterpreterNotFound',
+        MissingFile = '._exception:MissingFile',
+        MissingDirectory = '._exception:MissingDirectory',
+    )
+))
