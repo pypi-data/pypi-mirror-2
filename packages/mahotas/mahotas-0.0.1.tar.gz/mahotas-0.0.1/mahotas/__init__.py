80ffe37e7b030795103a7fb168ddbab88a5b8b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package mahotas/0.0.1/mahotas-0.0.1.tar.gz

---
+++ packages/mahotas/mahotas-0.0.1.tar.gz/mahotas-0.0.1/mahotas/__init__.py
@@ -0,0 +1,13 @@
+'''\
+=======
+Mahotas
+=======
+
+A set of functions for basic image processing and machine vision.
+'''
+from .histogram import fullhistogram
+from .thresholding import otsu
+from .stretch import stretch
+import morph
+from .morph import *
+__doc__=morph.__doc__
