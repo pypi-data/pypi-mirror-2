de76502c7c3d4b0ec23f86f416878eeea07d9d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:07 +0000

    Add package anz.dashboard/0.6/anz.dashboard-0.6.tar.gz

---
+++ packages/anz.dashboard/anz.dashboard-0.6.tar.gz/anz.dashboard-0.6/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.6'
+
+setup(name='anz.dashboard',
+      version=version,
+      description="Plone netvibes like dashboard implementation",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone anz dashboard',
+      author='jiangdongjin',
+      author_email='eastxing@gmail.com',
+      url='http://www.anzsoft.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['anz'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'feedparser',
+          'python-cjson',
+          'plone.memoize'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+      )
