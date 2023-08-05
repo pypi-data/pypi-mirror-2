244b60f2a14c5296dcd283dfb069194989494e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package skeleton/0.5.1/skeleton-0.5.1-py3.1.egg

---
+++ packages/skeleton/skeleton-0.5.1-py3.1.egg/skeleton/__init__.py
@@ -0,0 +1,11 @@
+"""
+Basic Template system for project skeleton.
+
+skeleton is similar to the template part of PasteScript but 
+without any dependencies; it should also be compatible with Python 3.
+
+However in this early phase of development, it only target python 2.5+,
+and tests require Mock.
+"""
+
+from skeleton.core import Skeleton, Var
