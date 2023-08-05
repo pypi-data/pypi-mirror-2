5715407fd8562aa71b0ed20eaf56ebc14cb1404f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:03 +0000

    Add package piwik/0.4/piwik-0.4.tar.gz

---
+++ packages/piwik/piwik-0.4.tar.gz/piwik-0.4/piwik/django/admin.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.db import models
+from django.forms.fields import Select
+from django import forms
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.utils.functional import update_wrapper
+
+from piwik import PiwikAPI
+from piwik.django.models import PiwikSite
+
+from django.conf import settings
+
+def get_piwik_settings():
+    try:
+        url = settings.PIWIK_URL
+    except AttributeError:
+        from django.core.exceptions import ImproperlyConfigured
+        raise ImproperlyConfigured("please set PIWIK_URL in your settings.")
+
+    try:
+        token = settings.PIWIK_TOKEN
+    except AttributeError:
+        from django.core.exceptions import ImproperlyConfigured
+        raise ImproperlyConfigured("please set PIWIK_TOKEN in your settings.")
+    return [url, token]
+
+class PiwikWidget(Select):
+    def getPiwikSites(self):
+        [url, token] = get_piwik_settings()
+        piwik = PiwikAPI(url, token)
+        sites = piwik.getAllSites()
+        choices = []
+        for site in sites:
+            site_id = int(site["idsite"])
+            choices.append((site_id, '%d - %s' % (site_id, site["name"])))
+        return tuple(choices)
+    def render(self, name, value, attrs=None, choices=()):
+        return super(PiwikWidget, self).render(name, value, attrs=attrs, choices=self.getPiwikSites())
+
+class PiwikSiteForm(forms.ModelForm):
+    id_site = forms.IntegerField(widget=PiwikWidget())
+    class Meta:
+        model = PiwikSite
+
+class PiwikSitesAdmin(admin.ModelAdmin):
+    form = PiwikSiteForm
+    list_display = ('id_site', 'site', 'view_stats')
+
+    def view_stats(self, site):
+        from django.core.urlresolvers import reverse
+        link = reverse('admin:admin_piwik_stats', kwargs={'id_site_piwik':site.id_site})
+        return '<a href="%s">Stats</a>' % link;
+
+    view_stats.allow_tags = True
+
+    def get_urls(self):
+        from django.conf.urls.defaults import patterns, url
+
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            return update_wrapper(wrapper, view)
+
+        urls = patterns('',
+            url(r'^(?P<id_site_piwik>[\d]+)/stats/$', wrap(self.stats), name='admin_piwik_stats'),
+        )
+        return urls + super(PiwikSitesAdmin, self).get_urls()
+
+    def stats(self, request, id_site_piwik=None):
+        [url, token] = get_piwik_settings()
+        piwik = PiwikAPI(url, token)
+        period = request.GET.get('period', 'day')
+        site = PiwikSite.objects.get(id_site=id_site_piwik)
+        title = 'Stats: %s' % site.site.name
+        visits = piwik.call('VisitsSummary.get', params={'idSite': id_site_piwik,
+                                                         'period': period,
+                                                         'date': 'yesterday'}, format='Html')
+        return render_to_response('admin/piwik/stats.html', {'stats': visits,
+                                                             'period': period,
+                                                             'title': title,
+                                                             'piwik_url': url},
+                                  RequestContext(request))
+
+admin.site.register(PiwikSite, PiwikSitesAdmin)
---
+++ packages/piwik/piwik-0.4.tar.gz/piwik-0.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='piwik',
+    version='0.4',
+    description='Access Piwik in python',
+    long_description=open('README.rst').read(),
+
+    author='François de Metz',
+    author_email='francois@2metz.fr',
+    url='http://forge.2metz.fr/p/python-piwik/',
+    
+    license='BSD',
+    packages = find_packages(),
+    package_data = {'piwik': ['django/templates/admin/piwik/*.html']},
+    
+    install_requires = [
+        'simplejson'
+    ],
+    extras_require = {
+        'django':  ["Django>=1.0"],
+    },
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: Log Analysis',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+)
