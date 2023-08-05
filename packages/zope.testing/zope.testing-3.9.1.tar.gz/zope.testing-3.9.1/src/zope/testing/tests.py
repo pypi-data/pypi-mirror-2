044e695b56e6000c36c88f04617673482a8f8c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:19 +0000

    Add package zope.testing/3.9.1/zope.testing-3.9.1.tar.gz

---
+++ packages/zope.testing/zope.testing-3.9.1.tar.gz/zope.testing-3.9.1/src/zope/testing/tests.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests for the testing framework.
+
+$Id: tests.py 104880 2009-10-07 17:53:44Z sidnei $
+"""
+
+import re
+import unittest
+from zope.testing import doctest, renormalizing
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocTestSuite('zope.testing.loggingsupport'),
+        doctest.DocTestSuite('zope.testing.renormalizing'),
+        doctest.DocTestSuite('zope.testing.server'),
+        doctest.DocFileSuite('doctest.txt'),
+        doctest.DocFileSuite('formparser.txt'),
+        doctest.DocFileSuite(
+            'module.txt',
+            # when this test is run in isolation, the error message shows the
+            # module name as fully qualified; when it is run as part of the
+            # full test suite, the error message shows the module name as
+            # relative.
+            checker=renormalizing.RENormalizing([
+                (re.compile('No module named zope.testing.unlikelymodulename'),
+                 'No module named unlikelymodulename')])),
+        doctest.DocFileSuite('setupstack.txt'),
+        doctest.DocTestSuite(doctest, optionflags=doctest.INTERPRET_FOOTNOTES),
+        ))
