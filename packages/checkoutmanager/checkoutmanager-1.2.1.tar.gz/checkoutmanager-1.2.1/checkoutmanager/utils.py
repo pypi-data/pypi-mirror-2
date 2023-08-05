3758c5b17be897142c74f2d4ad90d47f047eb8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package checkoutmanager/1.2.1/checkoutmanager-1.2.1.tar.gz

---
+++ packages/checkoutmanager/checkoutmanager-1.2.1.tar.gz/checkoutmanager-1.2.1/checkoutmanager/utils.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import sys
+
+# For zc.buildout's system() method:
+MUST_CLOSE_FDS = not sys.platform.startswith('win')
+# When you set '-v', this constant is changed.  A bit hacky.
+VERBOSE = False
+
+
+def system(command, input=None):
+    """commands.getoutput() replacement that also works on windows
+
+    Code copied from zc.buildout.
+
+    """
+    if VERBOSE:
+        print '[%s] %s' % (os.getcwd(), command)
+    p = subprocess.Popen(command,
+                         shell=True,
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         close_fds=MUST_CLOSE_FDS)
+    stdoutdata, stderrdata = p.communicate(input=input)
+    result = stdoutdata + stderrdata
+    if p.returncode:
+        # An error occured!  Notify and bail out directly.
+        print "Something went wrong when executing:"
+        print "    %s" % command
+        print "while in directory:"
+        print "    %s" % os.getcwd()
+        print "Output:"
+        print result
+        sys.exit(1)
+
+    return result
