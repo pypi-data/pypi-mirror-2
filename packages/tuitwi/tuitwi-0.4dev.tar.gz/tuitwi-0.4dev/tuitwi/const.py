2b2e24180ad4ea136b08f0aee8a2d30fb013e752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package tuitwi/0.4dev/tuitwi-0.4dev.tar.gz

---
+++ packages/tuitwi/tuitwi-0.4dev.tar.gz/tuitwi-0.4dev/tuitwi/const.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# OAuthの Consumer key と Consumer secret
+CONSUMER_KEY = 'L1RrniBm7dydExU4Dzuaxg'
+CONSUMER_SECRET = 'ySNrH1dE8FXIEtjSbyg3WtX7xHHIlTMNnf9BPshPtPg'
+
+DEFAULT_CONFIG = {
+    'access_token': {
+        'key': '',
+        'secret': ''
+        },
+    'options': {
+        'ambiguous_width': 2,
+        'browser_command': 'firefox %s',
+        'full_status_area_height': 6,
+        'max_log': 200,
+        'reply_check_interval': 20,
+        'update_interval': 60
+        },
+    'tabs': [ {
+            'keyword': '',
+            'title': 'all',
+            'users': []
+        }]
+    }
+
