ebcc1b24ab01ee93809ef420e3335a65d05cc4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:16 +0000

    Add package winpexpect/1.2/winpexpect-1.2.zip

---
+++ packages/winpexpect/winpexpect-1.2.zip/winpexpect-1.2/setup.py
@@ -0,0 +1,27 @@
+#
+# This file is part of WinPexpect. WinPexpect is free software that is made
+# available under the MIT license. Consult the file "LICENSE" that is
+# distributed together with this file for the exact licensing terms.
+#
+# WinPexpect is copyright (c) 2008-2010 by the WinPexpect authors. See the
+# file "AUTHORS" for a complete overview.
+
+from setuptools import setup
+
+setup(
+    name = 'winpexpect',
+    version = '1.2',
+    description = 'A version of pexpect that works under Windows.',
+    author = 'Geert Jansen',
+    author_email = 'geert@boskant.nl',
+    url = 'http://bitbucket.org/geertj/winpexpect',
+    license = 'MIT',
+    classifiers = ['Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: Microsoft :: Windows'],
+    package_dir = {'': 'lib'},
+    py_modules = ['pexpect', 'winpexpect'],
+    test_suite = 'nose.collector'
+)
