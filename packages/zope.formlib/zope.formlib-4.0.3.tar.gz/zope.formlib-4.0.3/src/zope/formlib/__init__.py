41c64d297707c41312b54a596e4a3b2c24ad93c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package zope.formlib/4.0.3/zope.formlib-4.0.3.tar.gz

---
+++ packages/zope.formlib/zope.formlib-4.0.3.tar.gz/zope.formlib-4.0.3/src/zope/formlib/__init__.py
@@ -0,0 +1,15 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: __init__.py 112015 2010-05-05 18:27:23Z tseaver $
+"""
---
+++ packages/zope.formlib/zope.formlib-4.0.3.tar.gz/zope.formlib-4.0.3/src/zope/formlib/namedtemplate.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2005-2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id: namedtemplate.py 112015 2010-05-05 18:27:23Z tseaver $
+"""
+
+# BBB
+from zope.browserpage.namedtemplate import (
+    INamedTemplate, NamedTemplateImplementation, implementation,
+    NamedTemplate, NamedTemplatePathAdapter)
