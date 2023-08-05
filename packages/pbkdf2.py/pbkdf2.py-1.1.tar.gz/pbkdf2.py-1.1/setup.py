b5205bfe5f7572815f413293e80a1c25452a9291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:31 +0000

    Add package pbkdf2.py/1.1/pbkdf2.py-1.1.tar.gz

---
+++ packages/pbkdf2.py/pbkdf2.py-1.1.tar.gz/pbkdf2.py-1.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pbkdf2.py',
+      version='1.1',
+      description='Simple Library for PBKDF2, with cxor support',
+      author='Tobias Ammann',
+      packages=['pbkdf2'],
+     )
+
