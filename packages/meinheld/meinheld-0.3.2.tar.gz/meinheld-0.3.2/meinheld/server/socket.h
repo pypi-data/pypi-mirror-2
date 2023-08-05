182e7ea3fc244db01dd43be5d59efc0329349172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package meinheld/0.3.2/meinheld-0.3.2.tar.gz

---
+++ packages/meinheld/meinheld-0.3.2.tar.gz/meinheld-0.3.2/meinheld/server/socket.h
@@ -0,0 +1,38 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include "server.h"
+#include "client.h"
+
+typedef struct {
+    PyObject_HEAD
+    int fd;
+    ClientObject *client;
+    buffer *recv_buf;
+    buffer *send_buf;
+} NSocketObject;
+
+extern PyTypeObject NSocketObjectType;
+
+inline PyObject* 
+NSocketObject_New(int fd, ClientObject *client);
+
+inline int 
+CheckNSocketObject(PyObject *obj);
+
+inline void 
+setup_nsocket(void);
+
+inline void 
+setup_listen_sock(int fd);
+
+inline void 
+setup_sock(int fd);
+
+inline void 
+enable_cork(client_t *client);
+
+inline void 
+disable_cork(client_t *client);
+
+#endif
