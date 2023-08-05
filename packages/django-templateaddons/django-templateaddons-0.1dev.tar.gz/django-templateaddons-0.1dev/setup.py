5c2d83e9e5de7789b7fabb64dbc5dc18f58d99f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package django-templateaddons/0.1dev/django-templateaddons-0.1dev.tar.gz

---
+++ packages/django-templateaddons/django-templateaddons-0.1dev.tar.gz/django-templateaddons-0.1dev/setup.py
@@ -0,0 +1,26 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-templateaddons',
+    version='0.1dev',
+    url='http://bitbucket.org/benoitbryon/django-templateaddons/',
+    author='Benoit Bryon',
+    author_email='benoit@marmelune.net',
+    license='LICENSE',
+    description = "A set of tools for use with templates of the Django " \
+                  "framework: additional template tags, context processors " \
+                  "and utilities for template tag development.",
+    long_description=open('README.txt').read(),
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+    ],
+    packages=find_packages(),
+    include_package_data = True,
+)
