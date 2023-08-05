879b82c8635620dce6865553f6c25d465399f15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:22 +0000

    Add package meinheld/0.1/meinheld-0.1.tar.gz

---
+++ packages/meinheld/meinheld-0.1.tar.gz/meinheld-0.1/meinheld/gmeinheld.py
@@ -0,0 +1,19 @@
+from gunicorn.workers.base import Worker
+from meinheld import server
+import os
+
+class MeinheldWorker(Worker):
+    
+    def watchdog(self):
+        self.notify()
+
+        if self.ppid != os.getppid():
+            self.log.info("Parent changed, shutting down: %s" % self)
+            server.stop()
+
+    def run(self):
+        fd = self.socket.fileno()
+        server.set_watchdog(self.watchdog)
+        server.set_listen_socket(fd)
+        server.run(self.wsgi)
+
