ab539ccea805ce8dc3d410b1ee695f789af56774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:25 +0000

    Add package pilas/0.1/pilas-0.1.tar.gz

---
+++ packages/pilas/pilas-0.1.tar.gz/pilas-0.1/pilas/ventana.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+# Pilas engine - A video game framework.
+#
+# Copyright 2010 - Hugo Ruscitti
+# License: LGPLv3 (see http://www.gnu.org/licenses/lgpl.html)
+#
+# Website - http://www.pilas-engine.com.ar
+
+from PySFML import sf
+
+def iniciar(ancho=640, alto=480, titulo="Pilas"):
+    ventana = sf.RenderWindow(sf.VideoMode(ancho, alto), titulo)
+
+    # Define que la coordenada (0, 0) sea el centro de la ventana.
+    view = ventana.GetDefaultView()
+    view.SetCenter(0, 0)
+
+    return ventana
