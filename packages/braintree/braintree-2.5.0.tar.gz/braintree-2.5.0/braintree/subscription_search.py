0c46d1f6e54279782f9b7c291ee13452a5378b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:02 +0000

    Add package braintree/2.5.0/braintree-2.5.0.tar.gz

---
+++ packages/braintree/braintree-2.5.0.tar.gz/braintree-2.5.0/braintree/subscription_search.py
@@ -0,0 +1,13 @@
+from braintree.util import Constants
+from braintree import Subscription
+from braintree.search import Search
+
+class SubscriptionSearch:
+    billing_cycles_remaining = Search.RangeNodeBuilder("billing_cycles_remaining")
+    days_past_due = Search.RangeNodeBuilder("days_past_due")
+    id = Search.TextNodeBuilder("id")
+    ids = Search.MultipleValueNodeBuilder("ids")
+    merchant_account_id = Search.MultipleValueNodeBuilder("merchant_account_id")
+    plan_id = Search.MultipleValueOrTextNodeBuilder("plan_id")
+    price = Search.RangeNodeBuilder("price")
+    status = Search.MultipleValueNodeBuilder("status", Constants.get_all_constant_values_from_class(Subscription.Status))
