c65c4c2176b802b3b89f6ad8ff59657c70c8935d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:26 +0000

    Add package collective.atimage.transformtoolbar/1.1/collective.atimage.transformtoolbar-1.1.tar.gz

---
+++ packages/collective.atimage.transformtoolbar/collective.atimage.transformtoolbar-1.1.tar.gz/collective.atimage.transformtoolbar-1.1/README.txt
@@ -0,0 +1,13 @@
+Introduction
+============
+The solely intention of this product is to replace the default "Transform" tab in Image content types with an icon-toolbar viewlet in the plone.belowcontent viewlet manager.
+
+By keeping the user in the same page and requiring less clicks and page loads we intend to ease applying transformations in the images.
+
+Features
+========
+
+- Performs image transformations with AJAX to prevent reloading the page
+- Degrades gracefully in non-JavaScript browsers
+- i18n support by using atcontentypes and plone i18n domains.
+- Tested in Plone 3.3.5 and Plone 4.0b3
---
+++ packages/collective.atimage.transformtoolbar/collective.atimage.transformtoolbar-1.1.tar.gz/collective.atimage.transformtoolbar-1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='collective.atimage.transformtoolbar',
+      version=version,
+      description="Replaces 'Transform' tab in ATImage with an icon toolbar in 'View' tab",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "INSTALL.txt")).read()  + "\n" +
+                       open(os.path.join("docs", "AUTHORS.txt")).read()  + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone"
+        ],
+      keywords='',
+      author='Marcos F. Romero',
+      author_email='marcos.romero {at} inter-cultura {dot} com',
+      url='http://svn.plone.org/svn/collective/collective.atimage.transformtoolbar',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.atimage'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
+
