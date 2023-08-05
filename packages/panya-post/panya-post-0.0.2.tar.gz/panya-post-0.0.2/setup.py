c63f4a3dc6f11869127929d4140c2b4b6d1c236b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:50 +0000

    Add package panya-post/0.0.2/panya-post-0.0.2.tar.gz

---
+++ packages/panya-post/panya-post-0.0.2.tar.gz/panya-post-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panya-post',
+    version='0.0.2',
+    description='Panya post app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/panya-post',
+    packages = find_packages(),
+    install_requires = [
+        'django-ckeditor',
+        'panya',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
