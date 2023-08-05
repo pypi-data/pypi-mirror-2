ce55b37386401400d6ef93c493c672f67ab809be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:47 +0000

    Add package asm.workflow/0.1/asm.workflow-0.1.tar.gz

---
+++ packages/asm.workflow/asm.workflow-0.1.tar.gz/asm.workflow-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(name='asm.workflow',
+      version = '0.1',
+      description="Workflow extension for the Assembly CMS",
+      author="Webcrew",
+      author_email="web@assembly.org",
+      url="",
+      license="proprietary",
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'grok',
+                        'zope.deferredimport',
+                        'asm.cms',
+                        'zope.component',
+                        'zope.event'])
