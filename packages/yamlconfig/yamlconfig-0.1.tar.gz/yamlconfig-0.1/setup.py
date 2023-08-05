ed7860bcafcc94636b5b62dfcf1835b821e15700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package yamlconfig/0.1/yamlconfig-0.1.tar.gz

---
+++ packages/yamlconfig/yamlconfig-0.1.tar.gz/yamlconfig-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='yamlconfig',
+    version='0.1',
+    description='YAML configuration file reader with basic variable interpolation.',
+    author='Kelsey Hightower',
+    author_email='kelsey.hightower@gmail.com',
+    url='https://bitbucket.org/khightower/yamlconfig',
+    py_modules=['yamlconfig'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
