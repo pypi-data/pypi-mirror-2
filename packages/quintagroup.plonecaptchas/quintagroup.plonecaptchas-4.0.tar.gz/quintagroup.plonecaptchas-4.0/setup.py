31d9819a2a7785dc6addf8106d7feeaacefa2a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package quintagroup.plonecaptchas/4.0/quintagroup.plonecaptchas-4.0.tar.gz

---
+++ packages/quintagroup.plonecaptchas/quintagroup.plonecaptchas-4.0.tar.gz/quintagroup.plonecaptchas-4.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '4.0'
+
+setup(name='quintagroup.plonecaptchas',
+      version=version,
+      description="quintagroup.plonecaptchas is simple captchas implementation for Plone, designed for validating human input in insecure forms.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone captcha',
+      author='Quintagroup',
+      author_email='support@quintagroup.com',
+      url='http://svn.quintagroup.com/products/quintagroup.plonecaptchas',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone>=4.0dev',
+          'quintagroup.captcha.core>=0.2',
+          'quintagroup.formlib.captcha',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
