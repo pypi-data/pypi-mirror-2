53597196e653f941e1b2bcb9d11337266ad4ca4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:56 +0000

    Add package plone.formwidget.captcha/1.0b1/plone.formwidget.captcha-1.0b1.tar.gz

---
+++ packages/plone.formwidget.captcha/plone.formwidget.captcha-1.0b1.tar.gz/plone.formwidget.captcha-1.0b1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '1.0b1'
+
+setup(name='plone.formwidget.captcha',
+      version=version,
+      description="Captcha widget for Plone.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Timo Stollenwerk',
+      author_email='timo@zmag.de',
+      url='http://pypi.python.org/pypi/plone.formwidget.captcha',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'skimpyGimpy',
+          'plone.keyring',
+          'plone.z3cform',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
