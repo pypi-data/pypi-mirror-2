da2ea251e4edb33534a21b7f5039a248591e3fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package langacore.thingsweforget/0.1.0/langacore.thingsweforget-0.1.0.tar.gz

---
+++ packages/langacore.thingsweforget/langacore.thingsweforget-0.1.0.tar.gz/langacore.thingsweforget-0.1.0/src/langacore/thingsweforget/__init__.py
@@ -0,0 +1,12 @@
+"""\
+Synchronization of thingsweforget.blogspot.com content for offline use
+----------------------------------------------------------------------
+
+This project requires Python 3.1 or later.
+"""
+
+# UTF-8 in the variables below is PyPI incompatible
+__version__ = "0.1"
+__release__ = "0.1.0"
+__author__ = "Lukasz Langa"
+__contact__ = "lukasz@langa.pl"
