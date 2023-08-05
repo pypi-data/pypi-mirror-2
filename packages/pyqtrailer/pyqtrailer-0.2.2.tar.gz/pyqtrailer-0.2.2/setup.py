56252e8b63e347e622cee3f53049be21dd2b1dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:05 +0000

    Add package pyqtrailer/0.2.2/pyqtrailer-0.2.2.tar.gz

---
+++ packages/pyqtrailer/pyqtrailer-0.2.2.tar.gz/pyqtrailer-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import pyqtrailer
+
+setup(name='pyqtrailer',
+      version=pyqtrailer.__version__,
+      description='PyQt4 application to download trailers from www.apple.com/trailers',
+      author='Stanislav Ochotnicky',
+      author_email='sochotnicky@gmail.com',
+      url='http://code.google.com/p/pyqtrailer',
+      requires=["pytrailer"],
+      install_requires=['pytrailer>=0.1'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+                   'Programming Language :: Python :: 2.6',
+                   'Topic :: Software Development :: User Interfaces',
+                   'Topic :: Multimedia :: Video'],
+      keywords="movie trailer apple module pyqt qt4",
+      license="GPLv3",
+      platforms=["any"],
+      packages=["pyqtrailer",
+                "pyqtrailer.qtcustom"],
+      scripts=["scripts/pyqtrailer"],
+     )
