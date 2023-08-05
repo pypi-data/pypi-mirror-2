4219054a23046ba5c8704935e1f6eeb5c3f20c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package drainers/0.0.3/drainers-0.0.3.tar.gz

---
+++ packages/drainers/drainers-0.0.3.tar.gz/drainers-0.0.3/drainers/__init__.py
@@ -0,0 +1,16 @@
+"""
+drainers -- Event-based process monitoring.
+"""
+
+VERSION = (0, 0, 3)
+
+__version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
+__author__ = "Vincent Driessen"
+__contact__ = "vincent@datafox.nl"
+__homepage__ = "http://github.com/nvie/python-drainers/"
+#__docformat__ = "restructuredtext"
+
+from base import Drainer
+from buffered import BufferedDrainer
+
+__all__ = [ 'Drainer', 'BufferedDrainer' ]
