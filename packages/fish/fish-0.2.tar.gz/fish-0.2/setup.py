b7e585d8ce6101c7b542f5f21973b40aa78b0bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package fish/0.2/fish-0.2.tar.gz

---
+++ packages/fish/fish-0.2.tar.gz/fish-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from os import path
+
+long_desc = path.join(path.dirname(__file__), "README.rst")
+
+setup(name="fish", version="0.2",
+      url="http://sendapatch.se/",
+      author="Ludvig Ericson",
+      author_email="ludvig@sendapatch.se",
+      description="Animating fishes for progress bars",
+      long_description=long_desc,
+      py_modules=["fish"])
