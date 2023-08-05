4e1976c07767fb1a3a84d0a9c501473cba5d091b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:42 +0000

    Add package five.intid/0.5.1/five.intid-0.5.1.zip

---
+++ packages/five.intid/five.intid-0.5.1.zip/five.intid-0.5.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os.path
+
+version = '0.5.1'
+
+setup(name='five.intid',
+      version=version,
+      description="Zope2 support for zope.intid",
+      long_description=open("README.txt").read() + "\n" +
+               open(os.path.join("five", "intid", "README.txt")).read() + "\n\n" +
+               open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Zope2"],
+      keywords="'zope2 Five zope3 UID'",
+      author='Whit Morris',
+      author_email='whit@openplans.org',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['five'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'zope.intid',
+        'zope.app.intid',
+        'zope.app.zapi',
+        'zope.component',
+        'zope.event',
+        'zope.interface',
+        'zope.lifecycleevent',
+        'zope.keyreference',
+        'zope.site',
+        'zope.location',
+        'five.localsitemanager',
+        ],
+      extras_require = dict(
+        test = ['interlude',
+                'zope.testing',
+                'zope.app.testing',
+                ],
+        ),
+      )
