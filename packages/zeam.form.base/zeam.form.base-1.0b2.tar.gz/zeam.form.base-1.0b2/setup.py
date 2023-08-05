99eb2ba5cf4b6ff656d37b743f4ff5b364077759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package zeam.form.base/1.0b2/zeam.form.base-1.0b2.tar.gz

---
+++ packages/zeam.form.base/zeam.form.base-1.0b2.tar.gz/zeam.form.base-1.0b2/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b2'
+
+tests_require = [
+    'zope.app.authentication',
+    'zope.app.testing',
+    'zope.app.zcmlfiles',
+    'zope.configuration',
+    'zope.securitypolicy',
+    'zope.testbrowser',
+    'zope.testing',
+    ]
+
+
+setup(name='zeam.form.base',
+      version=version,
+      description="Grok based form framework",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='grok form framework',
+      author='Sylvain Viollon',
+      author_email='thefunny@gmail.com',
+      url='http://pypi.python.org/pypi/zeam.form.base',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['zeam', 'zeam.form'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'grokcore.component',
+          'grokcore.security',
+          'grokcore.view',
+          'martian',
+          'megrok.chameleon',
+          'megrok.pagetemplate',
+          'setuptools',
+          'zope.component',
+          'zope.i18n',
+          'zope.interface',
+          'zope.pagetemplate',
+          'zope.publisher',
+          ],
+      tests_require = tests_require,
+      extras_require = {'test': tests_require},
+      )
---
+++ packages/zeam.form.base/zeam.form.base-1.0b2.tar.gz/zeam.form.base-1.0b2/src/zeam/form/base/components.py
@@ -0,0 +1,128 @@
+
+import re
+
+from zeam.form.base import interfaces
+from zope.interface import implements
+from zope import component
+
+_valid_identifier = re.compile('[A-Za-z][A-Za-z0-9_-]*$')
+
+def createId(name):
+    # Create a valid id from any string.
+    id = unicode(name).encode('utf-8')
+    id = id.replace(' ', '-')
+    if _valid_identifier.match(id):
+        return id.lower()
+    return id.encode('hex')
+
+
+class Component(object):
+    implements(interfaces.IComponent)
+
+    def __init__(self, title, identifier=None):
+        if identifier is None:
+            identifier = createId(title)
+        self.identifier = identifier
+        if not title:
+            # If the title is empty, use the identifier as title
+            title = identifier
+        self.title = title
+
+    def clone(self, new_identifier=None):
+        return self.__class__(self.title, new_identifier)
+
+    def __repr__(self):
+        return "<%s %s>" % (self.__class__.__name__, self.title)
+
+
+_marker = object()
+
+
+class Collection(object):
+    implements(interfaces.ICollection)
+
+    type = interfaces.IComponent
+    factory = None
+
+    def __init__(self, *components, **options):
+        self.__options = {}
+        for name, value in options.items():
+            if name not in interfaces.ICollection:
+                self.__options[name] = value
+        self.__dict__.update(self.__options)
+        self.__ids = []
+        self.__components = []
+        self.extend(*components)
+
+    def get(self, id, default=_marker):
+        try:
+            return self.__components[self.__ids.index(id)]
+        except ValueError:
+            if default is _marker:
+                raise KeyError(id)
+            return default
+
+    def append(self, component):
+        if self.type.providedBy(component):
+            if component.identifier not in self.__ids:
+                self.__ids.append(component.identifier)
+                self.__components.append(component)
+            else:
+                raise ValueError(
+                    u"Duplicate identifier", component.identifier)
+        else:
+            raise TypeError(u"Invalid type", component)
+
+    def extend(self, *components):
+        for cmp in components:
+            if self.type.providedBy(cmp):
+                self.append(cmp)
+            elif interfaces.ICollection.providedBy(cmp):
+                for item in cmp:
+                    self.append(item)
+            else:
+                if self.factory is not None:
+                    factory = component.queryAdapter(cmp, self.factory)
+                    if factory is not None:
+                        for item in factory.produce():
+                            self.append(item)
+                        continue
+                raise TypeError(u'Invalid type', cmp)
+
+    def select(self, *ids):
+        components = (c for c in self.__components if c.identifier in ids)
+        return self.__class__(*components, **self.__options)
+
+    def omit(self, *ids):
+        components = (c for c in self.__components if c.identifier not in ids)
+        return self.__class__(*components, **self.__options)
+
+    def copy(self):
+        return self.__class__(*self.__components, **self.__options)
+
+    def keys(self):
+        return list(self.__ids)
+
+    def __add__(self, other):
+        if interfaces.ICollection.providedBy(other):
+            copy = self.copy()
+            for component in other:
+                copy.append(component)
+            return copy
+        if interfaces.IComponent.providedBy(other):
+            copy = self.copy()
+            copy.append(other)
+            return copy
+        raise NotImplementedError
+
+    def __getitem__(self, id):
+        return self.get(id)
+
+    def __iter__(self):
+        return self.__components.__iter__()
+
+    def __len__(self):
+        return self.__components.__len__()
+
+    def __repr__(self):
+        return "<%s>" % (self.__class__.__name__)
