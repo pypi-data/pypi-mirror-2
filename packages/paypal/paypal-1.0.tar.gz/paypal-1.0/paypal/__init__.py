a672c21adbe5688e4637a2b8997b39bd01913840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:31 +0000

    Add package paypal/1.0/paypal-1.0.tar.gz

---
+++ packages/paypal/paypal-1.0.tar.gz/paypal-1.0/paypal/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+from interface import PayPalInterface
+from settings import PayPalConfig
+from exceptions import PayPalError, PayPalConfigError, PayPalAPIResponseError
+
+VERSION = '1.0'
