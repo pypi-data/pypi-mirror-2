30d0804d929821bf4b04d5dca4f03a478282bac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:11 +0000

    Add package zc.zlibstorage/0.1.1/zc.zlibstorage-0.1.1.tar.gz

---
+++ packages/zc.zlibstorage/zc.zlibstorage-0.1.1.tar.gz/zc.zlibstorage-0.1.1/setup.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+name, version = 'zc.zlibstorage', '0.1.1'
+
+install_requires = ['ZODB3 >=3.10.0b1', 'setuptools']
+extras_require = dict(test=['zope.testing', 'manuel'])
+
+entry_points = """
+"""
+
+from setuptools import setup
+import os
+
+long_description = open(os.path.join('src', *name.split('.')+['README.txt'])
+                        ).read()
+
+setup(
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    license = 'ZPL 2.1',
+
+    name = name, version = version,
+    long_description=long_description,
+    description = long_description.split('\n')[1],
+    packages = [name.split('.')[0], name],
+    namespace_packages = [name.split('.')[0]],
+    package_dir = {'': 'src'},
+    install_requires = install_requires,
+    zip_safe = False,
+    entry_points=entry_points,
+    include_package_data=True,
+    extras_require = extras_require,
+    tests_require = extras_require['test'],
+    test_suite = name+'.tests.test_suite',
+    )
