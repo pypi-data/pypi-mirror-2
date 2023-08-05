c23cd98cfb81d242b65bc6925b336d3e582848ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package accessible_output/0.5.3/accessible_output-0.5.3.zip

---
+++ packages/accessible_output/accessible_output-0.5.3.zip/accessible_output-0.5.3/speech/outputs/__init__.py
@@ -0,0 +1,7 @@
+from jaws import Jaws
+from nvda import NVDA
+from sa import SystemAccess
+from sapi5 import Sapi5
+from we import WindowEyes
+from dolphin import Dolphin
+__all__ = ["Dolphin", "Jaws", "NVDA", "Sapi5", "SystemAccess", "WindowEyes"]
