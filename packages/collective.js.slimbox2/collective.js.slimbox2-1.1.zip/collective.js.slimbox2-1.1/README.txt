a1a84e97e6f39840bb879f7c0b37dc37c86859f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:55 +0000

    Add package collective.js.slimbox2/1.1/collective.js.slimbox2-1.1.zip

---
+++ packages/collective.js.slimbox2/collective.js.slimbox2-1.1.zip/collective.js.slimbox2-1.1/README.txt
@@ -0,0 +1,28 @@
+Introduction
+============
+
+This egg do not provide any new feature to plone. It add slimbox2 to Plone
+and give official slimbox2 full release available throw browser resource directory slimbox2.
+
+Version of slimbox2: 2.04
+
+
+Credits
+=======
+
+Companies
+---------
+
+|makinacom|_
+
+  * `Planet Makina Corpus <http://www.makina-corpus.org>`_
+  * `Contact us <mailto:python@makina-corpus.org>`_
+
+
+Authors
+
+  - JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
+
+Contributors
+
+  - Johannes RAGGAM
---
+++ packages/collective.js.slimbox2/collective.js.slimbox2-1.1.zip/collective.js.slimbox2-1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='collective.js.slimbox2',
+      version=version,
+      description="Slimbox is a 4 KB visual clone of the popular Lightbox 2 script by Lokesh Dhakar, written using the jQuery javascript library.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='jeanmichel.francois@makina-corpus.com',
+      url='http://svn.plone.org/svn/collective/collective.js.slimbox2',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
