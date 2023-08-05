e6e40d092817c33153b2cdc89567e34ce44de191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package sqlbean/0.505/sqlbean-0.505.tar.gz

---
+++ packages/sqlbean/sqlbean-0.505.tar.gz/sqlbean-0.505/sqlbean/mc_total.py
@@ -0,0 +1,55 @@
+#coding:utf-8
+from sqlbean.db.mc_connection import mc
+class McTotal(object):
+    def __init__(self, get_count, mc_key):
+        self.mc_key = mc_key
+        self.get_count = get_count
+
+    def __call__(self, key):
+        mk = self.mc_key%key
+        count = mc.get(mk)
+        if count is None:
+            count = self.get_count(key) or 0
+            mc.set(mk, count, 36000)
+        return count
+
+    def get_multi(self, keys):
+        mc_key = self.mc_key
+        mc_key_list = dict([(key,mc_key%key) for key in keys])
+        result = mc.get_multi(mc_key_list.itervalues())
+        r = {}
+        for k, mck in mc_key_list.iteritems():
+            v = result.get(mck)
+            if v is None:
+                v = self.get_count(key) or 0
+                mc.set(mck, v)
+            r[k] = v
+        return r
+
+    def bind_total(self,ins):
+        d = self.get_multi(i.id for i in ins)
+        for i in ins:
+            i.total = d[i.id]
+        return ins
+
+
+    def get_list(self, keys):
+        r = self.get_multi(keys)
+        
+        return [
+            r.get(i,0) for i in keys
+        ]
+
+
+    def incr(self, key):
+        mk = self.mc_key%key
+        if mc.get(mk) is not None:
+            mc.incr(mk)
+
+    def decr(self, key):
+        mk = self.mc_key%key
+        if mc.get(mk) is not None:
+            mc.decr(mk)
+
+
+
