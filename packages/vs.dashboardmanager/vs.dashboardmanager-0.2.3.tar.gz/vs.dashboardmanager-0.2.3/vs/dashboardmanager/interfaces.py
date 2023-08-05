6def297c5aa9f325c0fd0d0897d297def501a241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package vs.dashboardmanager/0.2.3/vs.dashboardmanager-0.2.3.tar.gz

---
+++ packages/vs.dashboardmanager/vs.dashboardmanager-0.2.3.tar.gz/vs.dashboardmanager-0.2.3/vs/dashboardmanager/interfaces.py
@@ -0,0 +1,24 @@
+################################################################
+# vs.dashboardmanager
+# (C) 2010, Veit Schiele & Andreas Jung
+# Published under the GNU Public Licence V 2 (GPL 2)
+################################################################
+
+from plone.app.portlets.interfaces import IColumn
+from plone.portlets.interfaces import IPortletManager
+
+class IPortletPageColumn(IColumn):
+    """Marker interface describing columns on a portlet page
+    """
+
+class IPortletPageColumn1(IPortletManager):
+    """ Portlet manager for vs.dashboardmanager """
+
+class IPortletPageColumn2(IPortletManager):
+    """ Portlet manager for vs.dashboardmanager """
+
+class IPortletPageColumn3(IPortletManager):
+    """ Portlet manager for vs.dashboardmanager """
+
+class IPortletPageColumn4(IPortletManager):
+    """ Portlet manager for vs.dashboardmanager """
