d1a8192d37bb61b4f6d1d219b3666b10186c5a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:51 +0000

    Add package v2.theme/4.3/v2.theme-4.3.tar.gz

---
+++ packages/v2.theme/v2.theme-4.3.tar.gz/v2.theme-4.3/v2/theme/Extensions/Install.py
@@ -0,0 +1,16 @@
+from Products.CMFCore.utils import getToolByName
+
+def uninstall(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.setImportContext('profile-v2.theme:default')
+    setup_tool.runAllImportSteps()
+    return "Ran all uninstall steps."
+
+
+def uninstall(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.setImportContext('profile-v2.theme:uninstall')
+    setup_tool.runAllImportSteps()
+#    setup_tool.setImportContext('profile-CMFPlone:plone')
+    return "Ran all uninstall steps."
+
