39652e5f2f1149f81b0866d4010ffcf484dd034e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package ccy/0.3.5/ccy-0.3.5.tar.gz

---
+++ packages/ccy/ccy-0.3.5.tar.gz/ccy-0.3.5/ccy/__init__.py
@@ -0,0 +1,35 @@
+import os
+import sys
+ 
+VERSION = (0, 3, 5)
+ 
+def get_version():
+    if len(VERSION) == 3:
+        try:
+            int(VERSION[2])
+            v  = '%s.%s.%s' % VERSION
+        except:
+            v = '%s.%s_%s' % VERSION
+    else:
+        v = '%s.%s' % VERSION[:2]
+    return v
+ 
+__version__ = get_version()
+
+
+from core import currency as _currency
+
+from data.currency import make_ccys
+_currency.ccydb.load = make_ccys
+
+from core.currency import currencydb, currency, ccypair, ccypairsdb
+from core.country import country, countryccy, set_new_country, \
+                         countries, set_country_map, country_map, \
+                         CountryError
+
+# dates utilities
+from dates import *
+
+# Shortcuts
+cross     = lambda code : currency(code).as_cross()
+crossover = lambda code : currency(code).as_cross('/')
