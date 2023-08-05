d1c7ad392e5a55b564251969518170dbca39ea0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:51 +0000

    Add package tiddlywebplugins.devstore/0.7.3/tiddlywebplugins.devstore-0.7.3.tar.gz

---
+++ packages/tiddlywebplugins.devstore/tiddlywebplugins.devstore-0.7.3.tar.gz/tiddlywebplugins.devstore-0.7.3/setup.py
@@ -0,0 +1,31 @@
+import os
+
+try: # development environment
+	import mangler
+except ImportError:
+	pass
+
+from setuptools import setup, find_packages
+
+
+VERSION = "0.7.3" # N.B.: duplicate of tiddlywebplugins.devstore
+
+
+setup(
+	name = "tiddlywebplugins.devstore",
+	version = VERSION,
+	url = "http://pypi.python.org/pypi/tiddlywebplugins.devstore",
+	description = "TiddlyWeb store implementation supporting client-side development",
+	long_description = file(os.path.join(os.path.dirname(__file__), "README")).read(),
+	platforms = "Posix; MacOS X; Windows",
+	author = "FND",
+	author_email = "FNDo@gmx.net",
+	scripts = ['twinstance_dev'],
+	namespace_packages = ["tiddlywebplugins"],
+	packages = find_packages(exclude=["test"]),
+	install_requires = [
+		"setuptools",
+		"tiddlywebwiki",
+		"tiddlywebplugins.twimport"
+	]
+)
