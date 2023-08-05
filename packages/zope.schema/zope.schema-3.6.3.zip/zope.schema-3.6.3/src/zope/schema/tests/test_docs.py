7e53fef94a62a11a9306d052a65ce6a53ad954d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package zope.schema/3.6.3/zope.schema-3.6.3.zip

---
+++ packages/zope.schema/zope.schema-3.6.3.zip/zope.schema-3.6.3/src/zope/schema/tests/test_docs.py
@@ -0,0 +1,36 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests for the schema package's documentation files
+
+$Id: test_docs.py 111756 2010-04-30 21:41:24Z hannosch $
+"""
+import doctest
+import re
+import unittest
+
+from zope.testing import renormalizing
+
+def test_suite():
+    checker = renormalizing.RENormalizing([
+        (re.compile(r"\[\(None, Invalid\('8<=10',\)\)\]"),
+                    r"[(None, <zope.interface.exceptions.Invalid instance at 0x...>)]",)
+      ])
+    return unittest.TestSuite((
+        doctest.DocFileSuite('../sources.txt', optionflags=doctest.ELLIPSIS),
+        doctest.DocFileSuite('../fields.txt'),
+        doctest.DocFileSuite('../README.txt'),
+        doctest.DocFileSuite(
+            '../validation.txt', checker=checker,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS),
+        ))
