e306fac3a5cac5b15bd0b40d882b58a9c186b715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package redturtle.smartlink/0.7.3/redturtle.smartlink-0.7.3-py2.4.egg

---
+++ packages/redturtle.smartlink/redturtle.smartlink-0.7.3-py2.4.egg/redturtle/smartlink/Extensions/install.py
@@ -0,0 +1,11 @@
+def install(portal):
+    setup_tool = portal.portal_setup
+    setup_tool.setImportContext('profile-redturtle.smartlink:default')
+    setup_tool.runAllImportSteps()
+
+
+def uninstall(portal, reinstall=False):
+    if not reinstall:
+        setup_tool = portal.portal_setup
+        setup_tool.setImportContext('profile-redturtle.smartlink:uninstall')
+        setup_tool.runAllImportSteps()
