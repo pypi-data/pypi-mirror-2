4f82de72c2bce8cb0aebf3f9ad187edff54930d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:43 +0000

    Add package Products.UserAndGroupSelectionWidget/2.0.2/Products.UserAndGroupSelectionWidget-2.0.2.tar.gz

---
+++ packages/Products.UserAndGroupSelectionWidget/Products.UserAndGroupSelectionWidget-2.0.2.tar.gz/Products.UserAndGroupSelectionWidget-2.0.2/src/Products/UserAndGroupSelectionWidget/__init__.py
@@ -0,0 +1,6 @@
+from widget import UserAndGroupSelectionWidget
+from interfaces import IGenericGroupTranslation
+from interfaces import IGenericFilterTranslation
+
+from Products.CMFCore.DirectoryView import registerDirectory
+registerDirectory('skins', globals())
