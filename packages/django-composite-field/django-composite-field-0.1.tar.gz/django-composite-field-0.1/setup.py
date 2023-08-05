3d7fccb08300dec28d8d0cc6d31835ef8b0db896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package django-composite-field/0.1/django-composite-field-0.1.tar.gz

---
+++ packages/django-composite-field/django-composite-field-0.1.tar.gz/django-composite-field-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-composite-field',
+      version='0.1',
+      description='CompositeField implementation for Django',
+      keywords='django composite field',
+      author='Michael P. Jung',
+      author_email='mpjung@terreon.de',
+      url='http://bitbucket.org/mp/django-composite-field',
+      packages=['composite_field'],
+     )
