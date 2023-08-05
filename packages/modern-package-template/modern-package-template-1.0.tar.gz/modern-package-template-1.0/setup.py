c08cfaa6cc44ff5a222304805ee5d3744b2269d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:44 +0000

    Add package modern-package-template/1.0/modern-package-template-1.0.tar.gz

---
+++ packages/modern-package-template/modern-package-template-1.0.tar.gz/modern-package-template-1.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+version = '1.0'
+
+
+setup(name='modern-package-template',
+      version=version,
+      description="PasteScript template to create a Python project with distribute and buildout support",
+      long_description=README + '\n\n' + NEWS,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Environment :: Plugins',
+          'Framework :: Paste',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Code Generators',
+      ],
+      keywords='',
+      author='Sridhar Ratnakumar',
+      author_email='srid@nearfar.org',
+      url='http://bitbucket.org/srid/modern-package-template/',
+      license='GNU General Public License v3',
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "Paste",
+          "PasteScript",
+      ],
+      entry_points="""
+      [paste.paster_create_template]
+      modern_package = modern_package:ModernPackageTemplate
+      """,
+      )
---
+++ packages/modern-package-template/modern-package-template-1.0.tar.gz/modern-package-template-1.0/src/modern_package/__init__.py
@@ -0,0 +1,59 @@
+import os
+import shutil
+
+from paste.util.template import paste_script_template_renderer
+from paste.script.templates import Template, var
+
+
+class ModernPackageTemplate(Template):
+    _template_dir = 'templates'
+    summary = "Package with distribute and buildout support"
+    vars = [
+        var('version', 'Version (like 0.1)', default='0.1'),
+        var('description', 'One-line description of the package'),
+        var('keywords', 'Space-separated keywords/tags'),
+        var('author', 'Author name'),
+        var('author_email', 'Author email'),
+        var('url', 'URL of homepage'),
+        var('license_name', 'License name'),
+        ]
+
+    template_renderer = staticmethod(paste_script_template_renderer)
+
+    def pre(self, command, output_dir, vars):
+        if vars['package'].find('.') > 0:
+            vars['namespace'] = vars['package'].split('.')
+        else:
+            vars['namespace'] = [ vars['package'] ]
+
+    def post(self, command, output_dir, vars):
+        template_dir = self.template_dir()
+        current_dir = os.path.join(output_dir, "src")
+            
+        # apparently .dotfiles are not copied by Paster
+        hidden_files = ['.hgignore', '.gitignore']
+        for hf in hidden_files:
+            shutil.copyfile(
+                os.path.join(template_dir, hf),
+                os.path.join(output_dir, hf)
+            )
+        
+        for part in vars['namespace'][:-1]:
+            current_dir = os.path.join(current_dir, part)
+            if not os.path.exists(current_dir):
+                os.makedirs(current_dir)
+
+            shutil.copyfile(
+                os.path.join(template_dir, '.namespace__init__.py'),
+                os.path.join(current_dir, '__init__.py')
+            )
+            
+        part = vars['namespace'][-1]
+        current_dir = os.path.join(current_dir, part)
+        if not os.path.exists(current_dir):
+            os.makedirs(current_dir)
+
+        shutil.copyfile(
+            os.path.join(template_dir, '.normal__init__.py'),
+            os.path.join(current_dir, '__init__.py')
+        )
