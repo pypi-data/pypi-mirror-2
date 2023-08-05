54980b3766a614d12ee5fe00f4961674b4932915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package Flask-Markdown/0.2/Flask-Markdown-0.2.tar.gz

---
+++ packages/Flask-Markdown/Flask-Markdown-0.2.tar.gz/Flask-Markdown-0.2/flaskext/markdown.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.markdown
+    ~~~~~~~~~~~~~~~~~
+
+    Markdown filter class for Flask
+    To use::
+       from flaskext.markdown import Markdown
+       # reconfigure app jinja enviroment
+       md = Markdown(app)
+
+    Then in your template::
+        {% filter markdown %}
+        Your Markdown
+        =============
+        {% endfilter %}
+
+    You can also do::
+        {{ mymarkdown | markdown}}
+
+    :copyright: (c) 2010 by Dan Colish.
+    :license: BSD, MIT see LICENSE for more details.
+"""
+from __future__ import absolute_import
+from flask import current_app, Markup
+import markdown2 as md2
+
+
+class Markdown(md2.Markdown):
+
+    def __init__(self, app, *args, **kw):
+        super(Markdown, self).__init__(*args, **kw)
+        app.jinja_env.filters.update({'markdown': self})
+
+    def __call__(self, stream):
+        if hasattr(current_app, 'genshi_instance'):
+            return self.convert(stream)
+        else:
+            return Markup(self.convert(stream))
