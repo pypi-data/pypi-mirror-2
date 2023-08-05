31ca66a7a846e44a33b043b8ca73f718d24630ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:35 +0000

    Add package medialog.subskinsiii/1.9b5/medialog.subskinsiii-1.9b5.tar.gz

---
+++ packages/medialog.subskinsiii/medialog.subskinsiii-1.9b5.tar.gz/medialog.subskinsiii-1.9b5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.9b5'
+
+
+setup(name='medialog.subskinsiii',
+      version=version,
+      description="An installable theme for Plone 3.0",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme rthemingtool ',
+      author='Espen Moe-Nilssen',
+      author_email='espen@medialog.no',       
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['medialog'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.PloneSubSkins',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
