a2ada47eccded3cd27ad1e7fda5808ba84152b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package cobe/1.1.0/cobe-1.1.0.tar.gz

---
+++ packages/cobe/cobe-1.1.0.tar.gz/cobe-1.1.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+    name = "cobe",
+    version = "1.1.0",
+    author = "Peter Teichman",
+    author_email = "peter@teichman.org",
+    url = "http://wiki.github.com/pteichman/cobe/",
+    description = "A conversation simulator similar to MegaHAL",
+    packages = ["cobe"],
+    test_suite = "tests",
+    install_requires = ["argparse>=1.1"],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    entry_points = {
+        "console_scripts" : [
+            "cobe = cobe.control:main"
+        ]
+    }
+)
