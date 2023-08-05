750d985db79e0ac81a58c7edda75866fca4a7e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package medialog.roundskin/0.1/medialog.roundskin-0.1.tar.gz

---
+++ packages/medialog.roundskin/medialog.roundskin-0.1.tar.gz/medialog.roundskin-0.1/medialog/roundskin/skins/medialog_roundskin_styles/corners.js
@@ -0,0 +1,4 @@
+jq(document).ready(function(){
+jq("#portal-column-content, #portal-column-two div.visualPadding ").corners("10px transparent");
+jq("dt.portletHeader").corners("10px transparent top");
+});
