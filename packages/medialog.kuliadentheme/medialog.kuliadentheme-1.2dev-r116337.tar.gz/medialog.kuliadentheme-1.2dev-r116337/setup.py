f7ab52874a8483b867a14c6bbee8f4f15bfc512f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package medialog.kuliadentheme/1.2dev-r116337/medialog.kuliadentheme-1.2dev-r116337.tar.gz

---
+++ packages/medialog.kuliadentheme/medialog.kuliadentheme-1.2dev-r116337.tar.gz/medialog.kuliadentheme-1.2dev-r116337/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2'
+
+setup(name='medialog.kuliadentheme',
+      version=version,
+      description="An installable theme for Plone 3.0",   
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Espen Moe-Nilssen',
+      author_email='espen@medialog.no',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['medialog'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
