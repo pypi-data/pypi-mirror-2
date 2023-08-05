dd898441ace7842aa6f14477b87fb6403f983f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:41 +0000

    Add package RestrictedPython/3.6.0a1/RestrictedPython-3.6.0a1.zip

---
+++ packages/RestrictedPython/RestrictedPython-3.6.0a1.zip/RestrictedPython-3.6.0a1/setup.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for RestrictedPython package
+
+$Id: setup.py 113155 2010-06-05 16:36:11Z hannosch $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='RestrictedPython',
+      version='3.6.0a1',
+      url='http://pypi.python.org/pypi/RestrictedPython',
+      license='ZPL 2.1',
+      description='RestrictedPython provides a restricted execution '
+      'environment for Python, e.g. for running untrusted code.',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(read('src', 'RestrictedPython', 'README.txt')
+                        + '\n' +
+                        read('CHANGES.txt')),
+
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+      install_requires = ['setuptools'],
+      include_package_data = True,
+      zip_safe = False,
+      )
