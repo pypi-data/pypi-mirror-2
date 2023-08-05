50d3f6b6df801c779e71a6f6d02c0ddaa2243b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package hgblog/0.3/hgblog-0.3.tar.gz

---
+++ packages/hgblog/hgblog-0.3.tar.gz/hgblog-0.3/hgblog/utils.py
@@ -0,0 +1,14 @@
+import os
+
+def get_repo(path=None):
+    from mercurial.cmdutil import findrepo
+    from mercurial import hg
+    from mercurial.ui import ui
+
+    if path is None:
+        path = os.getcwd()
+
+    repo_root = findrepo(path)
+    repo = hg.repository(ui(), path=repo_root)
+    return repo
+
