00291df1dfa009862eee2566593aeb592b91a62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:52 +0000

    Add package zope.app.pagetemplate/3.11.1/zope.app.pagetemplate-3.11.1.tar.gz

---
+++ packages/zope.app.pagetemplate/zope.app.pagetemplate-3.11.1.tar.gz/zope.app.pagetemplate-3.11.1/src/zope/app/pagetemplate/__init__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Zope-specific Page Templates
+
+$Id: __init__.py 106892 2009-12-22 18:40:31Z hannosch $
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.browserpage import ViewPageTemplateFile
---
+++ packages/zope.app.pagetemplate/zope.app.pagetemplate-3.11.1.tar.gz/zope.app.pagetemplate-3.11.1/src/zope/app/pagetemplate/namedtemplate.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (c) 2005-2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id: namedtemplate.py 107626 2010-01-04 14:00:19Z janwijbrand $
+"""
+
+# BBB
+from zope.browserpage.namedtemplate import implementation
+from zope.browserpage.namedtemplate import INamedTemplate
+from zope.browserpage.namedtemplate import NamedTemplate
+from zope.browserpage.namedtemplate import NamedTemplateImplementation
+from zope.browserpage.namedtemplate import NamedTemplatePathAdapter
