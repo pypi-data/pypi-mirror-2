ad3863ccb43c8c2f1746fc778c7ff5191d82a610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:09 +0000

    Add package zeam.form.viewlet/1.0/zeam.form.viewlet-1.0.tar.gz

---
+++ packages/zeam.form.viewlet/zeam.form.viewlet-1.0.tar.gz/zeam.form.viewlet-1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='zeam.form.viewlet',
+      version=version,
+      description="zeam.form forms in a viewlet",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zeam form viewlet',
+      author='Sylvain Viollon',
+      author_email='thefunny@gmail.com',
+      url='http://pypi.python.org/pypi/zeam.form.viewlet',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['zeam', 'zeam.form'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'zeam.form.base',
+        'grokcore.viewlet',
+        'megrok.pagetemplate',
+        ],
+      )
