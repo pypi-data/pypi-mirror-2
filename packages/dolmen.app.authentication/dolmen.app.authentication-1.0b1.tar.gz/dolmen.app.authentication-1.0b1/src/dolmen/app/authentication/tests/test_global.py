1afe4f66309ab75e17041fe55c622050ef4906a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:45 +0000

    Add package dolmen.app.authentication/1.0b1/dolmen.app.authentication-1.0b1.tar.gz

---
+++ packages/dolmen.app.authentication/dolmen.app.authentication-1.0b1.tar.gz/dolmen.app.authentication-1.0b1/src/dolmen/app/authentication/tests/test_global.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import re
+import doctest
+import unittest
+
+from dolmen.app.authentication import tests
+from zope.app.wsgi.testlayer import BrowserLayer
+from zope.testing import renormalizing
+
+
+FunctionalLayer = BrowserLayer(tests)
+
+
+checker = renormalizing.RENormalizing([
+    # Accommodate to exception wrapping in newer versions of mechanize
+    (re.compile(r'httperror_seek_wrapper:', re.M), 'HTTPError:')])
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    files = ('../README.txt',)
+    for filename in files:
+        docfile = doctest.DocFileSuite(
+            filename,
+            checker=checker,
+            globs={'getRootFolder': FunctionalLayer.getRootFolder},
+            optionflags=(doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS))
+        docfile.layer = FunctionalLayer
+        suite.addTest(docfile)
+    return suite
