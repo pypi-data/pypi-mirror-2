ff4285f39d4321fe273346565474cced462df10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:41 +0000

    Add package pendrell/0.2.0/pendrell-0.2.0.tar.gz

---
+++ packages/pendrell/pendrell-0.2.0.tar.gz/pendrell-0.2.0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+assert __name__ == "__main__", "I don't think you want to import this..."
+
+from distutils import core
+
+
+def getReadme(path="README"):
+    f = open(path)
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+
+def getVersion():
+    import os
+    packageSeedFile = os.path.join("lib", "_version.py")
+    ns = {}
+    execfile(packageSeedFile, ns)
+    return ns
+
+pkgVer = getVersion()
+author, author_email = pkgVer["author"].rsplit(None, 1)
+
+
+core.setup(
+    name = pkgVer["version"].package.replace(".", "_"),
+    version = pkgVer["version"].short(),
+
+    description = "An HTTP 1.1 user agent for the programming web.",
+    long_description = getReadme(),
+    url = "http://pendrell.olix0r.net/",
+
+    license = "MIT",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        ],
+
+    packages = ["pendrell", "pendrell.cases", ],
+    package_dir = {
+        "pendrell": "lib",
+        "pendrell.cases": "lib/cases",
+        },
+
+    provides = ["pendrell", ],
+    requires = ["twisted.web", ],
+
+    author = author,
+    author_email = author_email,
+    maintainer = author,
+    maintainer_email = author_email,
+    )
+
+
