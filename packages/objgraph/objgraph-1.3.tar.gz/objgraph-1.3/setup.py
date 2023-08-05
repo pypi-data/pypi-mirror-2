1624a1234ea99ecf933fd615f0c26751bc9abf55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:40 +0000

    Add package objgraph/1.3/objgraph-1.3.tar.gz

---
+++ packages/objgraph/objgraph-1.3.tar.gz/objgraph-1.3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import os, sys, doctest
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def relative(filename):
+    here = os.path.dirname('__file__')
+    return os.path.join(here, filename)
+
+
+def get_version():
+    d = {}
+    exec open(relative('objgraph.py')).read() in d
+    return d['__version__']
+
+
+def build_images(sources=['README.txt', 'examples.txt']):
+    for fn in sources:
+        doctest.testfile(fn, optionflags=doctest.ELLIPSIS)
+
+
+if len(sys.argv) > 1 and sys.argv[1] == '--build-images':
+    build_images()
+    sys.exit(0)
+
+
+setup(name='objgraph',
+      version=get_version(),
+      author='Marius Gedminas',
+      author_email='marius@gedmin.as',
+      url='http://mg.pov.lt/objgraph/',
+      license='MIT',
+      description='Draws Python object reference graphs with graphviz',
+      py_modules=['objgraph'])
