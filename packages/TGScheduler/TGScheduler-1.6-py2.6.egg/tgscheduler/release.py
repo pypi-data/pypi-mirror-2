671d40301f45a73fef176f0794085a698acaaf76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:03 +0000

    Add package TGScheduler/1.6/TGScheduler-1.6-py2.6.egg

---
+++ packages/TGScheduler/TGScheduler-1.6-py2.6.egg/tgscheduler/release.py
@@ -0,0 +1,11 @@
+# Release information about TGScheduler
+
+version = "1.6"
+
+description = "Turbogears Scheduler"
+long_description = "This scheduler that is based TG1's scheduler which was based on Kronos by Irmen de Jong. This scheduler makes it easy to have one time or recurring tasks run as needed"
+author = "Vince Spicer"
+email = "vinces1979@gmail.com"
+
+url = "http://tgscheduler.googlecode.com"
+license = "MIT"
