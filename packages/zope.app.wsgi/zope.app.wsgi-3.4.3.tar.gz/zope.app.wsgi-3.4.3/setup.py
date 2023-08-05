f8a0881b53d5d57bb8ac120bb8defb79401e9454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package zope.app.wsgi/3.4.3/zope.app.wsgi-3.4.3.tar.gz

---
+++ packages/zope.app.wsgi/zope.app.wsgi-3.4.3.tar.gz/zope.app.wsgi-3.4.3/setup.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.wsgi package
+
+$Id: setup.py 111099 2010-04-19 14:48:41Z adamg $
+"""
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.wsgi',
+      version = '3.4.3',
+      url='http://pypi.python.org/pypi/zope.app.wsgi',
+      license='ZPL 2.1',
+      description='WSGI application for the zope.publisher',
+      long_description=open('README.txt').read(),
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      classifiers=['Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Zope Public License',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+                   'Framework :: Zope3',
+                   ],
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=['zope.app.testing',
+                                  'zope.app.securitypolicy',
+                                  'zope.app.zcmlfiles',
+                                  'zope.testbrowser']),
+      install_requires=['setuptools',
+                        'ZConfig',
+                        'zope.app.appsetup',
+                        'zope.app.publication',
+                        'zope.app.wsgi',
+                        'zope.event',
+                        'zope.interface',
+                        'zope.publisher',
+                        'zope.security',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
