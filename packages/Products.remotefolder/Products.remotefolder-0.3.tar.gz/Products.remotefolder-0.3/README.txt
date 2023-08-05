66bd1a5bc3dd2b0ff912f77c530b8a331c18cc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package Products.remotefolder/0.3/Products.remotefolder-0.3.tar.gz

---
+++ packages/Products.remotefolder/Products.remotefolder-0.3.tar.gz/Products.remotefolder-0.3/README.txt
@@ -0,0 +1,9 @@
+Itroduction
+============
+
+Remote Folder is a new type, extended from ATFolder, that automatically creates and updates content as Plone types, from remote sources through RSS Feeds.
+
+When you create a Remote folder you provide one or more RSS feed URI and data will be extracted from those feeds into new Plone objects such as Pages (Document type) and Images.
+
+
+
---
+++ packages/Products.remotefolder/Products.remotefolder-0.3.tar.gz/Products.remotefolder-0.3/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of Products.remotefolder
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.3'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('docs/HISTORY.txt')
+    )
+
+tests_require=['zope.testing']
+
+setup(name='Products.remotefolder',
+      version=version,
+      description="Folderish type that loads content from an external source like, for example, RSS feeds",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='plone folderish folder type remote RSS feed',
+      author='David Jonas',
+      author_email='david@v2.nl',
+      url='http://svn.collective.org/Products.remotefolder',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'Products.remotefolder.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*- 
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
