25716aa27d5f6d0ac7027c66df4824b3874cc0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package redisco/0.1.dev3/redisco-0.1.dev3.tar.gz

---
+++ packages/redisco/redisco-0.1.dev3.tar.gz/redisco-0.1.dev3/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+version = '0.1.dev3'
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+
+setup(name='redisco',
+      version=version,
+      description='Python Containers and Simple Models for Redis',
+      url='http://github.com/iamteem/redisco',
+      download_url='',
+      long_description=long_description,
+      author='Tim Medina',
+      author_email='iamteem@gmail.com',
+      maintainer='Tim Medina',
+      maintainer_email='iamteem@gmail.com',
+      keywords=['Redis', 'model', 'container'],
+      license='MIT',
+      packages=['redisco', 'redisco.models'],
+      test_suite='tests.all_tests',
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'],
+    )
+
