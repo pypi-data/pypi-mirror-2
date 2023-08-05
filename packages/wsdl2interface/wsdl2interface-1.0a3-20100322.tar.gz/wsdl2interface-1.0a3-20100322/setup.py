f540ee17cb10dd9364040a8ba6c829f600707635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:16 +0000

    Add package wsdl2interface/1.0a3-20100322/wsdl2interface-1.0a3-20100322.tar.gz

---
+++ packages/wsdl2interface/wsdl2interface-1.0a3-20100322.tar.gz/wsdl2interface-1.0a3-20100322/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='wsdl2interface',
+    version='1.0a3',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='http://pypi.python.org/pypi/wsdl2interface',
+    description='Create zope.interface style interfaces from a WSDL file',
+    long_description=open('README.txt').read(),
+    py_modules = ['wsdl2interface'],
+    license='ZPL',
+    install_requires=[
+        'suds',
+    ],
+    entry_points = {'console_scripts': ['wsdl2interface = wsdl2interface:main',]},
+)
