94741b2b104e40bfdc660058b3323edc9d5f6552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:06 +0000

    Add package ccy/0.3.2/ccy-0.3.2.zip

---
+++ packages/ccy/ccy-0.3.2.zip/ccy-0.3.2/CHANGELOG.rst
@@ -0,0 +1,21 @@
+ccy 0.3.2  - 2010 March 30
+=============================
+ * Added `ccy.basket` module, a django application for managing basket of currencies.
+
+ccy 0.3
+==============
+ * Added as_cross to ccy class to display the currency as a cross FX string
+ * Added 2 shortcuts to display crosses: cross(eur) -> EURUSD, and crossover(eur) -> EUR/USD
+ 
+ccy 0.2
+==============
+ * THIS VERSION IS NOT COMPATIBLE WITH PREVIOUS ONE
+ * Rearranged the modules so that data-structures and data are separated.
+ * Added symbol property to ccy
+ * bug fixes and refactoring for python 3.1 compatibility
+ * Added another 2 tests
+ 
+ccy 0.1.2
+============
+first official release
+ 
---
+++ packages/ccy/ccy-0.3.2.zip/ccy-0.3.2/ccy/__init__.py
@@ -0,0 +1,32 @@
+import os
+import sys
+ 
+VERSION = (0, 3, 2)
+ 
+def get_version():
+    if len(VERSION) == 3:
+        try:
+            int(VERSION[2])
+            v  = '%s.%s.%s' % VERSION
+        except:
+            v = '%s.%s_%s' % VERSION
+    else:
+        v = '%s.%s' % VERSION[:2]
+    return v
+ 
+__version__ = get_version()
+
+
+from core import currency as _currency
+
+from data.currency import make_ccys
+_currency.ccydb.load = make_ccys
+
+from core.currency import currencydb, currency, ccypair, ccypairsdb
+from core.country import country, countryccy, set_new_country, \
+                         countries, set_country_map, country_map, \
+                         CountryError
+
+# Shortcuts
+cross     = lambda code : currency(code).as_cross()
+crossover = lambda code : currency(code).as_cross('/')
