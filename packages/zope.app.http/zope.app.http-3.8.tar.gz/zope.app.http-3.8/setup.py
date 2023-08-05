874b2e504a1dc9b4052eff9c17639f47a776a711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package zope.app.http/3.8/zope.app.http-3.8.tar.gz

---
+++ packages/zope.app.http/zope.app.http-3.8.tar.gz/zope.app.http-3.8/setup.py
@@ -0,0 +1,71 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.app.http package
+
+$Id: setup.py 111129 2010-04-19 17:50:32Z faassen $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.app.http',
+      version = '3.8',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='HTTP Behavior for the Zope Publisher',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords="zope3 http publisher view",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.app.http',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require=dict(
+          test=['zope.app.testing',
+                'zope.app.securitypolicy',
+                'zope.app.file',
+                'zope.app.zcmlfiles',
+                'zope.app.wsgi >= 3.9.0',
+                'zope.site',
+                'zope.login',]),
+      install_requires=['setuptools',
+                        'zope.interface',
+                        'zope.publisher>=3.12',
+                        'zope.container',
+                        'zope.filerepresentation',
+                        ],
+      include_package_data=True,
+      zip_safe=False,
+      )
