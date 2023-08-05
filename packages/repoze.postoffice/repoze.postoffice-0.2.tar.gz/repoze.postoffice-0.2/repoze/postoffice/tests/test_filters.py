5a1a692d48b0b4274749c46e73da0101ea5e5699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package repoze.postoffice/0.2/repoze.postoffice-0.2.tar.gz

---
+++ packages/repoze.postoffice/repoze.postoffice-0.2.tar.gz/repoze.postoffice-0.2/repoze/postoffice/tests/test_filters.py
@@ -0,0 +1,44 @@
+import unittest
+
+class TestToHostnameFilter(unittest.TestCase):
+    def _make_one(self, expr):
+        from repoze.postoffice.filters import ToHostnameFilter
+        return ToHostnameFilter(expr)
+
+    def test_absolute(self):
+        fut = self._make_one('example.com')
+        msg = {}
+        self.failIf(fut(msg))
+        msg['To'] = 'chris@foo.com'
+        self.failIf(fut(msg))
+        msg['To'] = 'chris@foo.example.com'
+        self.failIf(fut(msg))
+        msg['To'] = 'chris@example.com'
+        self.failUnless(fut(msg))
+        msg['To'] = 'Chris <chris@example.com>'
+        self.failUnless(fut(msg))
+
+    def test_relative(self):
+        fut = self._make_one('.example.com')
+        msg = {}
+        self.failIf(fut(msg))
+        msg['To'] = 'chris@foo.com'
+        self.failIf(fut(msg))
+        msg['To'] = 'chris@foo.example.com'
+        self.failUnless(fut(msg))
+        msg['To'] = 'chris@example.com'
+        self.failUnless(fut(msg))
+        msg['To'] = 'Chris <chris@example.com>'
+        self.failUnless(fut(msg))
+
+    def test_case_insensitive(self):
+        fut = self._make_one('example.com')
+        msg = {}
+        self.failIf(fut(msg))
+        msg['To'] = 'chris@Example.com'
+        self.failUnless(fut(msg))
+
+    def test_not_an_address(self):
+        fut = self._make_one('example.com')
+        msg = {'To': 'undisclosed recipients;;'}
+        self.failIf(fut(msg))
