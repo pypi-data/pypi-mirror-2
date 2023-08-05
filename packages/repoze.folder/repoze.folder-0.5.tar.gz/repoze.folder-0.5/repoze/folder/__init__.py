bcd0c607b40eff832ff3d12fb8dde1ec4fed93b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:13 +0000

    Add package repoze.folder/0.5/repoze.folder-0.5.tar.gz

---
+++ packages/repoze.folder/repoze.folder-0.5.tar.gz/repoze.folder-0.5/repoze/folder/__init__.py
@@ -0,0 +1,268 @@
+import sys
+
+from zope.interface import implements
+
+from zope.component.event import objectEventNotify
+
+from persistent import Persistent
+
+from repoze.folder.interfaces import IFolder
+from repoze.folder.interfaces import marker
+
+from repoze.folder.events import ObjectAddedEvent
+from repoze.folder.events import ObjectWillBeAddedEvent
+from repoze.folder.events import ObjectRemovedEvent
+from repoze.folder.events import ObjectWillBeRemovedEvent
+
+from BTrees.OOBTree import OOBTree
+from BTrees.Length import Length
+
+sysencoding = sys.getdefaultencoding()
+
+class Folder(Persistent):
+    """
+    A folder implementation which acts much like a Python dictionary.
+    The keys are Unicode strings; the values are arbitrary Python
+    objects.
+    """
+
+    # _num_objects=None below is b/w compat for older instances of
+    # folders which don't have a BTrees.Length object as a
+    # _num_objects attribute.
+    _num_objects = None 
+
+    __name__ = None
+    __parent__ = None
+
+    implements(IFolder)
+
+    def __init__(self, data=None):
+        if data is None:
+            data = {}
+        self.data = OOBTree(data)
+        self._num_objects = Length(len(data))
+
+    def keys(self):
+        """
+        Returns an iterable object containing the names associated
+        with the objects that appear in the folder.  Each name is
+        guaranteed to be a Unicode string.
+        """
+        return self.data.keys()
+
+    __iter__ = keys
+
+    def __len__(self):
+        """
+        Returns the number of objects in the container.
+        """
+        if self._num_objects is None:
+            # can be arbitrarily expensive
+            return len(self.data)
+        return self._num_objects()
+
+    def __nonzero__(self):
+        """ This is defined so code that does 'if folder:' will avoid
+        calling __len__ (which can be arbitrarily expensive). """
+        return True
+
+    def values(self):
+        """
+        Return a sequence-like object containing the objects that
+        appear in the folder.
+        """
+        return self.data.values()
+
+    def items(self):
+        """
+        Return a sequence-like object containing tuples of the form
+        (name, object) for the objects that appear in the folder.
+        Each name is guaranteed to be a Unicode string.
+        """
+        return self.data.items()
+
+    def __getitem__(self, name):
+        """
+        Return the named object, or raise ``KeyError`` if the object
+        is not found.
+
+        """
+        name = unicodify(name)
+        return self.data[name]
+
+    def get(self, name, default=None):
+        """
+        Return the named object, or the value of the `default`
+        argument if the object is not found.  Return the named object,
+        or raise ``KeyError`` if the object is not found.
+
+        `name`` will optimally be a Unicode string, because keys used
+        to look up objects are stored as Unicode.  However, if
+        ``name`` is a byte string, the system attempts to decode it to
+        Unicode using the default system encoding; if it cannot be
+        decoded using the system encoding, the system attempts to
+        decode it using the UTF-8 codec; and if that fails, a
+        TypeError is raised.
+        """
+        name = unicodify(name)
+        return self.data.get(name, default)
+
+    def __contains__(self, name):
+        """
+        Return true if the named object appears in the folder.
+
+        `name`` will optimally be a Unicode string, because keys used
+        to look up objects are stored as Unicode.  However, if
+        ``name`` is a byte string, the system attempts to decode it to
+        Unicode using the default system encoding; if it cannot be
+        decoded using the system encoding, the system attempts to
+        decode it using the UTF-8 codec; and if that fails, a
+        TypeError is raised.
+        """
+        name = unicodify(name)
+        return self.data.has_key(name)
+
+    def __setitem__(self, name, other):
+        """
+        Add the given object to the folder under the given name.  The
+        name must not be the empty string nor may it be of a type other
+        than ``basestring`` (a TypeError is raised if either case is true).
+
+        `name`` will optimally be a Unicode string, because keys used
+        to look up objects are stored as Unicode.  However, if
+        ``name`` is a byte string, the system attempts to decode it to
+        Unicode using the default system encoding; if it cannot be
+        decoded using the system encoding, the system attempts to
+        decode it using the UTF-8 codec; and if that fails, a
+        TypeError is raised.
+
+        This method sends a ``ObjectWillBeAddedEvent`` before the
+        object is set into the folder, and an ``ObjectAddedEvent``
+        after the object has been set into the folder.
+        """
+        return self.add(name, other)
+
+    def add(self, name, other, send_events=True):
+        """ Does the same thing as ``__delitem__`` but provides the
+        ``send_events`` argument to allow a user to suppress the
+        sending of folder events.  By default, the ``send_events``
+        flag is ``True``, meaning this method behaves exactly like
+        ``__setitem__`` (events are sent).  If ``send_events`` is
+        ``False`` or any other value that evaluates to false, folder
+        events will not be sent.
+        """
+        if not isinstance(name, basestring):
+            raise TypeError("Name must be a string rather than a %s" %
+                            name.__class__.__name__)
+        if not name:
+            raise TypeError("Name must not be empty")
+
+        name = unicodify(name)
+
+        if self.data.has_key(name):
+            raise KeyError('An object named %s already exists' % name)
+
+        if send_events:
+            objectEventNotify(ObjectWillBeAddedEvent(other, self, name))
+        other.__parent__ = self
+        other.__name__ = name
+
+        # backwards compatibility: add a Length _num_objects to folders that
+        # have none
+        if self._num_objects is None:
+            self._num_objects = Length(len(self.data))
+
+        self.data[name] = other
+        self._num_objects.change(1)
+        if send_events:
+            objectEventNotify(ObjectAddedEvent(other, self, name))
+
+    def __delitem__(self, name):
+        """
+        Delete the named object from the folder. Raises a KeyError if
+        the object is not found.
+
+        `name`` will optimally be a Unicode string, because keys used
+        to look up objects are stored as Unicode.  However, if
+        ``name`` is a byte string, the system attempts to decode it to
+        Unicode using the default system encoding; if it cannot be
+        decoded using the system encoding, the system attempts to
+        decode it using the UTF-8 codec; and if that fails, a
+        TypeError is raised.
+
+        This method sends a ``ObjectWillBeRemovedEvent`` before the
+        object is set into the folder, and an ``ObjectRemovedEvent``
+        after the object has been set into the folder.
+        """
+        return self.remove(name)
+
+    def remove(self, name, send_events=True):
+        """ Does the same thing as ``__delitem__`` but provides the
+        ``send_events`` argument to allow a user to suppress the
+        sending of folder events.  By default, the ``send_events``
+        flag is ``True``, meaning this method behaves exactly like
+        ``__delitem__`` (events are sent).  If ``send_events`` is
+        ``False`` or any other value that evaluates to false, folder
+        events will not be sent. The removed object is returned.
+        """
+        name = unicodify(name)
+        other = self.data[name]
+
+        if send_events:
+            objectEventNotify(ObjectWillBeRemovedEvent(other, self, name))
+
+        if hasattr(other, '__parent__'):
+            del other.__parent__
+        if hasattr(other, '__name__'):
+            del other.__name__
+
+        # backwards compatibility: add a Length _num_objects to folders that
+        # have none
+        if self._num_objects is None:
+            self._num_objects = Length(len(self.data))
+
+        del self.data[name]
+        self._num_objects.change(-1)
+        if send_events:
+            objectEventNotify(ObjectRemovedEvent(other, self, name))
+
+        return other
+
+    def pop(self, name, default=marker):
+        try:
+            result = self.remove(name)
+        except KeyError:
+            if default is marker:
+                raise
+            return default
+        return result
+
+    def __repr__(self):
+        klass = self.__class__
+        classname = '%s.%s' % (klass.__module__, klass.__name__)
+        return '<%s object %r at %#x>' % (classname,
+                                          self.__name__,
+                                          id(self))
+    
+def unicodify(name, encoding=None):
+    if encoding is None:
+        encoding = sysencoding
+    try:
+        name = unicode(name)
+    except UnicodeError:
+        if encoding in ('utf-8', 'utf8'):
+            raise TypeError(
+                'Byte string names must be decodeable using the system '
+                'encoding of "utf-8" (%s)' % name
+                )
+        try:
+            name = unicode(name, 'utf-8')
+        except UnicodeError:
+            raise TypeError(
+                'Byte string names must be decodeable using either the system '
+                'encoding of "%s" or the "utf-8" encoding (%s)' % (
+                sysencoding, name)
+                )
+
+    return name
+
---
+++ packages/repoze.folder/repoze.folder-0.5.tar.gz/repoze.folder-0.5/repoze/folder/interfaces.py
@@ -0,0 +1,126 @@
+from zope.component.interfaces import IObjectEvent
+from zope.interface import Interface
+from zope.interface import Attribute
+
+marker = object()
+
+class IObjectWillBeAddedEvent(IObjectEvent):
+    """ An event type sent when an before an object is added """
+    object = Attribute('The object being added')
+    parent = Attribute('The folder to which the object is being added')
+    name = Attribute('The name which the object is being added to the folder '
+                     'with')
+
+class IObjectAddedEvent(IObjectEvent):
+    """ An event type sent when an object is added """
+    object = Attribute('The object being added')
+    parent = Attribute('The folder to which the object is being added')
+    name = Attribute('The name which the object is being added to the folder '
+                     'with')
+
+class IObjectWillBeRemovedEvent(IObjectEvent):
+    """ An event type sent before an object is removed """
+    object = Attribute('The object being added')
+    parent = Attribute('The folder to which the object is being added')
+    name = Attribute('The name which the object is being added to the folder '
+                     'with')
+
+class IObjectRemovedEvent(IObjectEvent):
+    """ An event type sent when an object is removed """
+    object = Attribute('The object being added')
+    parent = Attribute('The folder to which the object is being added')
+    name = Attribute('The name which the object is being added to the folder '
+                     'with')
+
+class IFolder(Interface):
+    """ A Folder implementation which stores objects using Unicode
+    keys.  All methods which accept a ``name`` argument expect the
+    name to either be Unicode or a byte string decodable using the
+    default system encoding or the UTF-8 encoding."""
+    def keys():
+        """ Return a ``BTreeItems`` sequence representing the keys
+        (object names) present in the folder.  Use list() against this
+        value to expand eagerly."""
+    def __iter__():
+        """ An alias for ``keys`` """
+    def values():
+        """ Return a ``BTreeItems`` sequence representing the values
+        (object values) present in the folder."""
+    def items():
+        """ Return a ``BTreeItems`` sequence representing the values
+        (object values) present in the folder."""
+    def get(name, default=None):
+        """ Return the object value named by ``name`` or the default
+        if the object name does not exist.  ``name`` must be a Unicode
+        object or a bytestring object; if ``name`` is a bytestring
+        object, it must be decodable using the system default encoding
+        or the UTF-8 encoding."""
+    def __contains__(name):
+        """ Return ``True`` if the container contains the object value
+        named by name, ``False`` otherwise. ``name`` must be a Unicode
+        object or a bytestring object; if ``name`` is a bytestring
+        object, it must be decodable using the system default encoding
+        or the UTF-8 encoding."""
+    def __nonzero__():
+        """ Always return True """
+    def __len__():
+        """ Return the number of subobjects in this folder. """
+    def __setitem__(name, other):
+        """ Set an object into this folder using the name ``name`` and
+        the value ``other``.  ``name`` must be a Unicode object or a
+        bytestring object; if ``name`` is a bytestring object, it must
+        be decodable using the system default encoding or the UTF-8
+        encoding.  ``name`` cannot be the empty string.  When
+        ``other`` is seated into this folder, it will also be
+        decorated with a ``__parent__`` attribute (a reference to the
+        folder into which it is being seated) and ``__name__``
+        attribute (the name passed in to this function.  If a value
+        already exists with the name ``name`` within this folder, a
+        KeyError will be raised.  When this method is called, an
+        ``ObjectWillBeAddedEvent`` event will be emitted before the
+        object obtains a ``__name__`` or ``__parent__`` value.  After
+        the object obtains a ``__name__`` and ``__parent__`` value, an
+        ``ObjectAddedEvent`` will be emitted."""
+    def add(name, other, send_events=True):
+        """ Does the same thing as ``__delitem__`` but provides the
+        ``send_events`` argument to allow a user to suppress the
+        sending of folder events.  By default, the ``send_events``
+        flag is ``True``, meaning this method behaves exactly like
+        ``__setitem__`` (events are sent).  If ``send_events`` is
+        ``False`` or any other value that evaluates to false, folder
+        events will not be sent.
+        """
+    def pop(name, default=marker):
+        """ Remove the item named ``name`` and return it.  If ``name``
+        doesn't exist in the folder, raise a :exc:`KeyError` unless
+        ``default`` was provided, in which case return the default
+        value.  This method will cause events to be sent
+        uncondtionally.  This method is new in repoze.folder 0.5."""
+        
+    def __delitem__(name):
+        """
+        Delete an object value from this folder that is already
+        present in the folder with the name ``name``.  ``name`` must
+        be a Unicode object or a bytestring object; if ``name`` is a
+        bytestring object, it must be decodable using the system
+        default encoding or the UTF-8 encoding.  If the object value
+        with the name ``name`` does not exist in the folder a
+        ``KeyError`` will be raised.  When the object value referred
+        to by ``name`` is deleted from this folder, its ``__parent__``
+        and ``__name__`` values will be removed from the object.  When
+        this method is called, an ``ObjectWillBeRemovedEvent`` event
+        will be emitted before the object loses its ``__name__`` or
+        ``__parent__`` values.  After the object loses its
+        ``__name__`` and ``__parent__`` value, an
+        ``ObjectRemovedEvent`` will be emitted.
+        """
+    def remove(name, send_events=True):
+        """ Does the same thing as ``__delitem__`` but provides the
+        ``send_events`` argument to allow a user to suppress the
+        sending of folder events.  By default, the ``send_events``
+        flag is ``True``, meaning this method behaves exactly like
+        ``__delitem__`` (events are sent).  If ``send_events`` is
+        ``False`` or any other value that evaluates to false, folder
+        events will not be sent.  The result is the removed object in
+        repoze.folder 0.5+; in earlier versions it will be ``None``.
+        """
