845ab27cae8a03dad648c19b7659fc9338363226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package libleipzig/1.2/libleipzig-1.2.tar.gz

---
+++ packages/libleipzig/libleipzig-1.2.tar.gz/libleipzig-1.2/libleipzig/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2009, 2010 Robert Lehmann
+
+__version__ = "1.2"
+
+from libleipzig.protocol import *
+from libleipzig.transport import services
+from suds import WebFault
+
+__all__ = services.keys() + ['WebFault']
