5e74c24a5f9b37be0a045b9e675eb39997e4b2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package django-reversetag/0.3.1/django-reversetag-0.3.1.tar.gz

---
+++ packages/django-reversetag/django-reversetag-0.3.1.tar.gz/django-reversetag-0.3.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-reversetag",
+    version = "0.3.1",
+    packages = find_packages(),
+    author = "Ulrich Petri",
+    author_email = "mail@ulo.pe",
+    description = "A new, improved (and renamed) implementation of " \
+                  "Django's 'url' template tag.",
+    long_description = """
+    """,
+    license = "MIT",
+    keywords = "django",
+    url = "http://github.com/ulope/django-reversetag",
+)
+
