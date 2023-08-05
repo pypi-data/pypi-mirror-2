dae12d4f598579759aa37cd7bbb2d69439b83ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package garlicsim_py3/0.4/garlicsim_py3-0.4-py3.1.egg

---
+++ packages/garlicsim_py3/garlicsim_py3-0.4-py3.1.egg/garlicsim_py3/bundled/simulation_packages/life/settings.py
@@ -0,0 +1,6 @@
+from .life import *
+from garlicsim_py3.misc import settings
+
+DETERMINISM_FUNCTION = determinism_function
+SCALAR_STATE_FUNCTIONS = [live_cells]
+SCALAR_HISTORY_FUNCTIONS = [changes]
