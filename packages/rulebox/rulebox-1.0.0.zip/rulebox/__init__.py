0180c8f2651d2db1f5bd40e097b8f070f8151c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:31 +0000

    Add package rulebox/1.0.0/rulebox-1.0.0.zip

---
+++ packages/rulebox/rulebox-1.0.0.zip/rulebox/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright (C) 2008-2010 Brandon Evans and Chris Santiago.
+# http://www.suitframework.com/
+# http://www.suitframework.com/docs/credits
+
+"""
+A package containing various sets of rules for use with SUIT.
+
+-----------------------------
+Example Usage
+-----------------------------
+
+::
+
+    import suit # easy_install suit
+    from rulebox import templating
+    template = open('template.tpl').read()
+    # Template contains "Hello, <strong>[var]username[/var]</strong>!"
+    templating.var.username = 'Brandon'
+    print suit.execute(templating.rules, template)
+    # Result: Hello, <strong>Brandon</strong>!
+
+Basic usage; see http://www.suitframework.com/docs/ for other uses.
+"""
+
+__version__ = '1.0.0'
