b858cd30e0a6e1995636a84c0c4fff200643214a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:55 +0000

    Add package django-socialregistration/0.2/django-socialregistration-0.2.tar.gz

---
+++ packages/django-socialregistration/django-socialregistration-0.2.tar.gz/django-socialregistration-0.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+METADATA = dict(
+    name='django-socialregistration',
+    version='0.2',
+    author='Alen Mujezinovic',
+    author_email='alen@caffeinehit.com',
+    description='Django application enabling registration through a variety of APIs',
+    long_description=open('README').read(),
+    url='http://github.com/flashingpumpkin/django-socialregistration',
+    keywords='django facebook twitter oauth openid registration',
+)
+SETUPTOOLS_METADATA = dict(
+    install_requires=['setuptools', 'django', 'oauth', 'python-openid'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Web Environment',
+        'Topic :: Internet',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    packages=['socialregistration', 'socialregistration.templatetags'],
+    package_data={'socialregistration': ['templates/socialregistration/*.html'],}
+)
+
+if __name__ == '__main__':
+    try:
+        import setuptools
+        METADATA.update(SETUPTOOLS_METADATA)
+        setuptools.setup(**METADATA)
+    except ImportError:
+        import distutils.core
+        distutils.core.setup(**METADATA)
