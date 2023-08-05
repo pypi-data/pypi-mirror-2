6930c17327d3ab3a3097d20397781e4335eb4a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:47 +0000

    Add package Products.RedirectionTool/1.3b1/Products.RedirectionTool-1.3b1.zip

---
+++ packages/Products.RedirectionTool/Products.RedirectionTool-1.3b1.zip/Products.RedirectionTool-1.3b1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '1.3b1'
+
+setup(name='Products.RedirectionTool',
+      version=version,
+      description="The Redirection Tool allows the management of the aliases stored in plone.app.redirector",
+      long_description = open('README.txt').read() + '\n' +
+                         open('CHANGES.txt').read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+      ],
+      keywords='Redirection Alias Zope Plone',
+      author='Jarn AS',
+      author_email='',
+      url='http://pypi.python.org/pypi/Products.RedirectionTool',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
