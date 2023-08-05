d530f5c24c774a9f65df6813d3773e334a0f4cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:52 +0000

    Add package htmlpetrol/0.3-2/htmlpetrol-0.3-2.tar.gz

---
+++ packages/htmlpetrol/htmlpetrol-0.3-2.tar.gz/htmlpetrol-0.3-2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="htmlpetrol",
+    version="0.3-2",
+    packages=find_packages("."),
+
+    author="Derek McTavish Mounce",
+    author_email="d.mounce@silkseed.com",
+    description="Apocalyptic HTML form procreator.",
+    license="MIT"
+)
