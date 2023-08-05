08f30177b6c089ff26219800685641015e8fde0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:42 +0000

    Add package alacarte/0.6.0/alacarte-0.6.0-py2.7.egg

---
+++ packages/alacarte/alacarte-0.6.0-py2.7.egg/alacarte/template/mako_.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+from alacarte.core import Engine
+from alacarte.middleware import resolve
+
+try:
+    from mako.template import Template
+
+except ImportError:
+    raise ImportError('You must install the mako package.')
+
+
+__all__ = ['Mako']
+
+
+class Mako(Engine):
+    def prepare(self, filename, **options):
+        return self.get_template(filename)
+    
+    def render(self, template, data, **options):
+        return template.render_unicode(**data)
+
+    def get_template(self, uri):
+        filename = resolve(uri)[1]
+        return Template(filename=filename, lookup=self)
+    
+    def adjust_uri(self, uri, relativeto):
+        return uri
