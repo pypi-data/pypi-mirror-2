cd7dfb9960765fbe58f2eb9d131204d2ad595a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package quintagroup.formlib.captcha/1.3/quintagroup.formlib.captcha-1.3-py2.6.egg

---
+++ packages/quintagroup.formlib.captcha/quintagroup.formlib.captcha-1.3-py2.6.egg/quintagroup/formlib/captcha/example.py
@@ -0,0 +1,32 @@
+from zope.schema import TextLine
+from zope.component import adapts
+from zope.interface import Interface, implements
+from zope.formlib.form import FormFields
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.form.base import EditForm
+
+from quintagroup.formlib.captcha import Captcha
+
+# Define CaptchaFormlibForm form schema
+
+class ICaptchaFormlibFormSchema(Interface):
+    label = TextLine(title=u'Label',
+                     required=False)
+    captcha = Captcha(title=u'Type the code')
+      
+# Create adapter for any object to ICaptchaFormlibFormSchema
+# schema interface
+
+class CaptchaFormlibFormAdapter(object):
+    implements(ICaptchaFormlibFormSchema)
+
+    def __init__(self, context):
+        self.context = context
+
+    label = u''
+    captcha = None
+      
+# And at the last define the CaptchaFormlibForm form
+
+class CaptchaFormlibForm(EditForm):
+    form_fields = FormFields(ICaptchaFormlibFormSchema)
