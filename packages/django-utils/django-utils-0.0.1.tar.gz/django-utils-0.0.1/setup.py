b47f0f360d4c3bffe3ee758f7f1ec3d036b9d7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package django-utils/0.0.1/django-utils-0.0.1.tar.gz

---
+++ packages/django-utils/django-utils-0.0.1.tar.gz/django-utils-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = __import__('utils').__version__
+
+setup(
+    author="Jonas Obrist",
+    name="django-utils",
+    version=version,
+    url="http://github.com/ojii/django-utils",
+    description="Utilities for Django",
+    long_description="Utilities for Django",
+    platforms=['OS Independent'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Development Status :: 4 - Beta'
+    ],
+    packages=find_packages(),
+    package_dir={
+        'utils': 'utils',
+    },
+    zip_safe = False
+)
