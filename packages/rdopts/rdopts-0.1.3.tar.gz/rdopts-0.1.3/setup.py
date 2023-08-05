8303d4898a873c804efe67f9f7a558cd7748a377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:21 +0000

    Add package rdopts/0.1.3/rdopts-0.1.3.tar.gz

---
+++ packages/rdopts/rdopts-0.1.3.tar.gz/rdopts-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+README_TEXT = open('./README.rst').read()
+
+setup(
+    name='rdopts',
+    version='0.1.3',
+    py_modules=['rdopts'],
+    long_description=README_TEXT,
+    description='A custom setuptools command for reading from setup.cfg',
+    entry_points= {
+        'distutils.commands' : [
+            'rdopts = rdopts:Rdopts']}
+)
