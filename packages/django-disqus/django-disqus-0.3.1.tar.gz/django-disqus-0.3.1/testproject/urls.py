7e7f52d63e9d347f7f0d67b15b1338de207c6177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package django-disqus/0.3.1/django-disqus-0.3.1.tar.gz

---
+++ packages/django-disqus/django-disqus-0.3.1.tar.gz/django-disqus-0.3.1/testproject/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^testproject/', include('testproject.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    (r'^foo/$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
+    (r'^comments/', include('django.contrib.comments.urls')),
+)
