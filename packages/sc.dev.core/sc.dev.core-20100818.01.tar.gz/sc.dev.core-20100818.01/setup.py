0f746119d0c5a6eb25dddd076f77a8a6798c156b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:56 +0000

    Add package sc.dev.core/20100818.01/sc.dev.core-20100818.01.tar.gz

---
+++ packages/sc.dev.core/sc.dev.core-20100818.01.tar.gz/sc.dev.core-20100818.01/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '20100818.01'
+
+setup(name='sc.dev.core',
+      version=version,
+      description="Developement core packages used by Simples Consultoria",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Simples Consultoria',
+      author_email='products@simplesconsultoria.com.br',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['sc', 'sc.dev'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'sc.paster.package==0.5.2',
+          'sc.paster.buildout==0.5.2',
+          'sc.paster.policy==0.6.1',
+          'sc.paster.theme==0.6.4',
+          'setuptools-git==0.3.4',
+          'collective.dist==0.2.5',
+          'zest.releaser==3.12',
+          'collective.dist==0.2.5'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
