b1e7040e34ea29df6673f137e92be7d8e3ebe38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package strainer/0.1rc3/strainer-0.1rc3-py2.6.egg

---
+++ packages/strainer/strainer-0.1rc3-py2.6.egg/strainer/doctypes.py
@@ -0,0 +1,12 @@
+DOCTYPE_XHTML1_STRICT = (
+    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" '
+    '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n')
+
+DOCTYPE_XHTML1_TRANSITIONAL = (
+   '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" '
+   '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n')
+
+DOCTYPE_XHTML1_FRAMESET = (
+    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" '
+    '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">\n')
+
