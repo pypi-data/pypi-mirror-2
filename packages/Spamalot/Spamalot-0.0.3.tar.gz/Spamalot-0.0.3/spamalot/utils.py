0d8684a52bd5581d0483bf432bda2ead451bd769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:06 +0000

    Add package Spamalot/0.0.3/Spamalot-0.0.3.tar.gz

---
+++ packages/Spamalot/Spamalot-0.0.3.tar.gz/Spamalot-0.0.3/spamalot/utils.py
@@ -0,0 +1,16 @@
+import csv
+
+class UnicodeCSVDictReader(csv.DictReader):
+    '''Used to convert each line to the specified encoding'''
+
+    def __init__(self, f, encoding='utf-8', *args, **kw):
+        self.encoding = encoding
+        csv.DictReader.__init__(self, f, *args, **kw)
+
+    def next(self):
+        line = csv.DictReader.next(self)
+        line = dict(zip(
+            line.keys(),
+            map(lambda v: v.decode(self.encoding).trim(), line.values())))
+        return line
+
