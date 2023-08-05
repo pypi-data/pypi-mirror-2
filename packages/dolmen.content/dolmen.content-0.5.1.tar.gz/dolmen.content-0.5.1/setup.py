e2f614b7b50745ef56c053af26b3b23a9797fdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package dolmen.content/0.5.1/dolmen.content-0.5.1.tar.gz

---
+++ packages/dolmen.content/dolmen.content-0.5.1.tar.gz/dolmen.content-0.5.1/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+from os.path import join
+
+name = 'dolmen.content'
+version = '0.5.1'
+readme = open(join('src', 'dolmen', 'content', 'README.txt')).read()
+history = open(join('docs', 'HISTORY.txt')).read()
+
+install_requires = [
+    'dolmen.field >= 0.3',
+    'grokcore.component',
+    'grokcore.content',
+    'grokcore.security',
+    'martian',
+    'setuptools',
+    'zope.annotation',
+    'zope.component',
+    'zope.dublincore',
+    'zope.formlib',
+    'zope.interface',
+    'zope.schema',
+    ]
+
+tests_require = [
+    'zope.container',
+    'zope.security',
+    'zope.site',
+    'zope.testing',
+    'zope.traversing',
+    'zope.location',
+    ]
+
+setup(name = name,
+      version = version,
+      description = 'Dolmen content type framework',
+      long_description = readme + '\n\n' + history,
+      keywords = 'Grok Zope3 CMS Dolmen Content',
+      author = 'Souheil Chelfouh',
+      author_email = 'souheil@chelfouh.com',
+      url = 'http://gitweb.dolmen-project.org/',
+      download_url = '',
+      license = 'GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages = ['dolmen'],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = False,
+      tests_require = tests_require,
+      install_requires = install_requires,
+      extras_require = {'test': tests_require},
+      classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Zope3',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+)
