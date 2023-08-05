8779490d3c93c47a88cf334db31e5da931624c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package liveplots/0.4/liveplots-0.4.tar.gz

---
+++ packages/liveplots/liveplots-0.4.tar.gz/liveplots-0.4/setup.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 22/03/2010
+
+@author: fccoelho
+'''
+
+#from ez_setup import use_setuptools
+#use_setuptools()
+
+from setuptools import setup, find_packages
+
+
+setup(name='liveplots', 
+        version  = '0.4', 
+        author = 'Flavio Codeco Coelho', 
+        author_email = 'fccoelho@gmail.com', 
+        url = 'http://code.google.com/p/liveplots/',
+        description = 'Real-time live plot server',
+        zip_safe = True,
+        packages = find_packages(),
+        install_requires = ["numpy >= 1.2","pyinotify >= 0.8.9"], 
+        test_suite = 'nose.collector', 
+        license = 'GPL',  
+      )
