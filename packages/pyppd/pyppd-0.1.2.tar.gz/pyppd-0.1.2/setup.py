df1522bbf600acd28a08d9f93612d183d995a2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:07 +0000

    Add package pyppd/0.1.2/pyppd-0.1.2.tar.gz

---
+++ packages/pyppd/pyppd-0.1.2.tar.gz/pyppd-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pyppd',
+    version='0.1.2',
+    author='Vitor Baptista',
+    author_email='vitor@vitorbaptista.com',
+    packages=['pyppd'],
+    package_data={'pyppd': ['*.in']},
+    scripts=['bin/pyppd'],
+    url='http://gitorious.org/vitorbaptista/pyppd/',
+    license='GPLv3',
+    long_description=open('README.txt').read(),
+)
