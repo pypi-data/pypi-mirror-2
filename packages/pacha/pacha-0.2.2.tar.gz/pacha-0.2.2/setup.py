e79e1b247f23f7dfb25ecaaf737ad3695be8b336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:52 +0000

    Add package pacha/0.2.2/pacha-0.2.2.tar.gz

---
+++ packages/pacha/pacha-0.2.2.tar.gz/pacha-0.2.2/setup.py
@@ -0,0 +1,51 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup
+
+tests_require = ['nose', 'unittest2']
+
+setup(
+    name = "pacha",
+    version = "0.2.2",
+    packages = ['pacha'],
+    install_requires = ['supay>=0.0.6', 'mercurial==1.5.3'],
+    entry_points = {
+        'console_scripts': [
+            'pacha = pacha:main'
+            ]
+        },
+    include_package_data=True,
+    package_data = {
+        '': ['distribute_setup.py'],
+        },
+
+    # metadata 
+    author = "Alfredo Deza",
+    author_email = "alfredodeza [at] gmail [dot] com",
+    description = "Systems configuration/management engine",
+    long_description = """\
+Pacha was designed from the ground up to be a simple way to backup and 
+manage software configuration files from single or multiple server 
+instances across the network.
+
+Pacha's approach is to easily deploy an instance and capture any changes 
+via a version control, giving the System Administrator the ability 
+to rollback and safe guard valid, working configurations and rebuild a 
+host from those same configurations.
+""",
+   classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+      ],
+
+    license = "MIT",
+    keywords = "systems configuration management engine hg mercurial",
+    url = "http://code.google.com/p/pacha",   
+
+)
+
