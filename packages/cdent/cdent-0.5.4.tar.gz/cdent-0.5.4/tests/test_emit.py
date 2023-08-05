0edec24d785954e2e71da961f5ffd13b55f133a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package cdent/0.5.4/cdent-0.5.4.tar.gz

---
+++ packages/cdent/cdent-0.5.4.tar.gz/cdent-0.5.4/tests/test_emit.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import StringIO
+
+from package.unittest import *
+
+class TestPythonParser(TestCase):
+    def setUp(self):
+        import cdent.parser.cdent.yaml
+
+        parser = cdent.parser.cdent.yaml.Parser()
+        input = file('tests/modules/world.cd.yaml', 'r')
+        parser.open(input)
+        self.ast = parser.parse()
+
+    def test_emit_python(self):
+        import cdent.emitter.python
+
+        emitter = cdent.emitter.python.Emitter()
+        output = StringIO.StringIO()
+        emitter.open(output)
+        emitter.emit_ast(self.ast)
+        got = output.getvalue()
+        expected = file('tests/modules/world.py').read()
+
+        self.assertTextEquals(got, expected, 'Emit world.py')
+
+    def test_emit_perl(self):
+        import cdent.emitter.perl
+
+        emitter = cdent.emitter.perl.Emitter()
+        output = StringIO.StringIO()
+        emitter.open(output)
+        emitter.emit_ast(self.ast)
+        got = output.getvalue()
+        expected = file('tests/modules/World.pm').read()
+
+        self.assertTextEquals(got, expected, 'Emit world.pm')
+
+    def test_emit_go(self):
+        import cdent.emitter.go
+
+        emitter = cdent.emitter.go.Emitter()
+        output = StringIO.StringIO()
+        emitter.open(output)
+        emitter.emit_ast(self.ast)
+        got = output.getvalue()
+        expected = file('tests/modules/World.go').read()
+
+        self.assertTextEquals(got, expected, 'Emit world.go')
+
+if __name__ == '__main__':
+    main()
+
