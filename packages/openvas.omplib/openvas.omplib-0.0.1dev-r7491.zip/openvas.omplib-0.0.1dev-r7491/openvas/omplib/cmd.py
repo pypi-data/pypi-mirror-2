bb9c900cda4aba830bde59f36a4f59b619b00a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:55 +0000

    Add package openvas.omplib/0.0.1dev-r7491/openvas.omplib-0.0.1dev-r7491.zip

---
+++ packages/openvas.omplib/openvas.omplib-0.0.1dev-r7491.zip/openvas.omplib-0.0.1dev-r7491/openvas/omplib/cmd.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+openvas.omplib.cmd - An OMP (OpenVAS Management Protocol) command line client.
+"""
+#
+# Copyright 2010 by Hartmut Goebel <h.goebel@goebel-consult.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = "Hartmut Goebel <h.goebel@goebel-consult.de>"
+__copyright__ = "Copyright 2010 by Hartmut Goebel <h.goebel@goebel-consult.de>"
+__licence__ = "GNU General Public License version 3 (GPL v3)"
+__version__ = '0.0.1'
+
+from openvas.omplib import *
+# use the same etree implementations as omplib
+from openvas.omplib import etree
+
+import inspect
+import sys
+import time
+import argparse
+import textwrap
+
+try:
+    from gettext import gettext
+except ImportError:
+    def gettext(message):
+        return message
+_ = gettext
+
+
+class OMPCommandClient(object):
+    def __init__(self, args):
+        self.client = OMPClient(args.host, args.port,
+                                args.username, args.password)
+
+    def run_command(self, optparser, args):
+        mod = inspect.getmodule(self)
+        cmd = getattr(mod, args.command_name.replace('-', '_'))
+        self.client.open()
+        try:
+            res = cmd.run(self.client, args)
+        except Error, e:
+            sys.exit('Error: %s' % e)
+        finally:
+            self.client.close()
+        return res
+        
+    @classmethod
+    def _find_commands(klass):
+        def is_OMPCommand(c):
+            return inspect.isclass(c) and OMPCommand in c.__bases__
+        
+        classes = inspect.getmembers(inspect.getmodule(klass), is_OMPCommand)
+        return (c[1] for c in classes)
+
+
+class OMPCommand(object):
+    @classmethod
+    def _argparse(klass, subparsers):
+        cmd_name = klass.__name__.replace('_', '-')
+        description = klass.__doc__.rstrip()
+        help = description.lstrip().split('\n', 1)[0]
+        description = textwrap.dedent(description)
+        sp = subparsers.add_parser(cmd_name,
+                                   formatter_class=argparse.RawDescriptionHelpFormatter,
+                                   help=help,
+                                   description=description)
+        klass._add_arguments(sp)
+
+
+class create_task(OMPCommand):
+    """Create a task"""
+    
+    @staticmethod
+    def _add_arguments(sp):
+        sp.add_argument('-n', '--name', default="unnamed task",
+                        help="Name for the new task")
+        sp.add_argument('-m', '--comment', default="",
+                        help="Comment for the new task ")
+        #sp.add_argument('-r', '--rc',
+        #                help="Create task with RC read from stdin")
+        sp.add_argument('-c', '--config',
+                        default=DEFAULT_CONFIG,
+                        help=("Name of config to use for this task"
+                              " (default: %(default)r)"))
+        sp.add_argument('-t', '--target',
+                        default=DEFAULT_TARGET,
+                        help=("Name of target to use for this task"
+                              " (default: %(default)r)"))
+
+    @staticmethod
+    def run(omp_client, args):
+        print omp_client.create_task(name=args.name, comment=args.comment,
+                                     config=args.config, target=args.target)
+
+
+class delete_task(OMPCommand):
+    """Delete one or more tasks"""
+    
+    @staticmethod
+    def _add_arguments(sp):
+        sp.add_argument('uuid', nargs='+', help='Uuids for tasks to delete')
+
+    @staticmethod
+    def run(omp_client, args):
+        for uuid in args.uuid:
+            omp_client.delete_task(uuid)
+
+
+class delete_report(OMPCommand):
+    """Delete one or more reports"""
+    
+    @staticmethod
+    def _add_arguments(sp):
+        sp.add_argument('uuid', nargs='+', help='Uuids for reports to delete')
+
+    @staticmethod
+    def run(omp_client, args):
+        for uuid in args.uuid:
+            omp_client.delete_report(uuid)
+
+
+class get_report(OMPCommand):
+    """Get report of one task"""
+    
+    @staticmethod
+    def _add_arguments(sp):
+        sp.add_argument('uuid', nargs='+', help='Uuids for reports to fetch')
+        sp.add_argument('-f', '--format', help="Report format (default: XML).",
+                        choices=REPORT_FORMATS,
+                        default=REPORT_FORMAT_XML)
+        sp.add_argument('-o', '--output-file',
+                        help=("This specifies the output filename to write to "
+                              "or standard output by default."))
+
+    @staticmethod
+    def run(omp_client, args):
+        for uuid in args.uuid:
+            res = omp_client.get_report(uuid, format=args.format)
+            if args.format == REPORT_FORMAT_XML:
+                res = etree.tostring(res)
+            if args.output_file is not None:
+                out = open(args.output_file, 'wb')
+                out.write(res)
+                out.close()
+            else:
+                sys.stderr.write(res)
+
+
+class get_status(OMPCommand):
+    """
+    Get status of one, many or all tasks.
+    
+    If any uuid is given, the output includes a list of all reports
+    for the requested tasks. If no uuid is given, only the first,
+    second last and last report is listed (without duplicates). The
+    list of reports is indented below the respective task. Each line
+    conists of the report-uuid, number of holes, warnings, infos and
+    log and the timestamp.
+    """
+    
+    @staticmethod
+    def _add_arguments(sp):
+        sp.add_argument('uuid', nargs='*',
+                        help='Uuids of tasks to get status for')
+        
+    @staticmethod
+    def run(omp_client, args):
+
+        def print_report(report):
+            print "  %(id)s  " % report,
+            print "%(hole)3s %(warning)3s %(info)3s %(log)3s" % report['messages'],
+            try:
+                timestamp = time.strptime(report['timestamp'])
+                print time.strftime(" %Y-%m-%d %H:%M:%S", timestamp)
+            except:
+                # failed to parse time format, dump what we got
+                print " %(timestamp)s" % report
+
+        def print_task(task):
+            if not first:
+                print
+            if task['status'] == 'Running':
+                print ('%(id)s\t%(status)s %(progress)s%%\t%(name)s' % task)
+            else:
+                print ('%(id)s\t%(status)s\t%(name)s' % task)
+            # Print any reports indented under the task.
+            printed = set()
+            if task.get('reports', None):
+                for report in task['reports']:
+                    print_report(report)
+            else:
+                for report in ('first', 'second_last', 'last'):
+                    # second_lst may be the same as first, thus we need to track
+                    report = task.get(report+'_report', None)
+                    if report and not report['id'] in printed:
+                        print_report(report)
+                        printed.add(report['id'])
+          
+        first = True
+        if args.uuid:
+            for uuid in args.uuid:
+                task = omp_client.get_task_status(uuid)
+                print_task(task)
+                first = False
+        else:
+            for task in omp_client.get_task_status():
+                print_task(task)
+                first = False
+
+
+class start_task(OMPCommand):
+    "Start one or more tasks"
+    
+    @staticmethod
+    def _add_arguments(sp):
+        sp.add_argument('uuid', nargs='+', help='Uuids of tasks to start')
+
+    @staticmethod
+    def run(omp_client, args):
+        for uuid in args.uuid:
+            omp_client.start_task(uuid)
+
+
+class modify_task(OMPCommand):
+    "Modify a task"
+    
+    @staticmethod
+    def _add_arguments(sp):
+        sp.add_argument('uuid', nargs=1, help='Uuid of task to modify')
+
+    @staticmethod
+    def run(omp_client, args):
+        raise NotImplementedError
+        omp_client.modify_task(args[0], args.name, args.file)
+
+
+class help(OMPCommand):
+    "Get help message from OMP server"
+    
+    @staticmethod
+    def _add_arguments(sp): pass
+
+    @staticmethod
+    def run(omp_client, args):
+        res = omp_client.help()
+        print res.strip('\n')
+
+
+class xml(OMPCommand):
+    "Send plain XML command"
+    
+    @staticmethod
+    def _add_arguments(sp):
+        sp.add_argument('xml', nargs=1, help='XML text/document to send')
+
+    @staticmethod
+    def run(omp_client, args):
+        res = omp_client.xml(args.xml[0], xml_result=True)
+        print etree.tostring(res)
+
+
+def run():
+    import argparse
+    import getpass
+    
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--help", action="help",
+                        help=_("show this help message and exit"))
+    parser.add_argument("--version", action='version', version=__version__)
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help="Verbose messages.")
+    parser.add_argument('-P', '--prompt', action='store_true',
+                        help="Prompt to exit.")
+
+    group = parser.add_argument_group('Options for connecting to the manager')
+    group.add_argument('-h', '--host', default=MANAGER_ADDRESS,
+                       help=("Connect to manager on host HOST "
+                             "(default: %(default)s)"))
+    group.add_argument('-p', '--port', type=int, default=MANAGER_PORT,
+                       help="Use port number PORT (default: %(default)s)")
+    group.add_argument('-u', '--username', default=getpass.getuser(),
+                       help="OMP username (default: current user's name)")
+    group.add_argument('-w', '--password',
+                       help="OMP password (default: same as username)")
+
+    subparsers = parser.add_subparsers(dest='command_name',
+                                       title='Available OMP commands',
+                                       help='Get help for sub-commands with <cmd> --help')
+
+    for cmd in OMPCommandClient._find_commands():
+        cmd._argparse(subparsers)
+
+    args = parser.parse_args()
+    
+    if not (0 < args.port < 65536):
+        parser.error('Manager port must be a number between 1 and 65535.')
+    if args.password is None:
+        args.password = args.username
+
+    client = OMPCommandClient(args)
+    client.run_command(parser, args)
+
+if __name__ == '__main__':
+    run()
---
+++ packages/openvas.omplib/openvas.omplib-0.0.1dev-r7491.zip/openvas.omplib-0.0.1dev-r7491/tests/test_omplib.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import os
+import inspect
+
+import openvas.omplib
+from openvas.omplib import etree
+
+# uuids of samples delivered by gsa, see gsa/src/html/src/help.xsl
+SAMPLE_TASK_ID = '343435d6-91b0-11de-9478-ffd71f4c6f29'
+SAMPLE_REPORT_ID = '343435d6-91b0-11de-9478-ffd71f4c6f30'
+
+
+def tests_path(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+
+class OMPTestBase(unittest.TestCase):
+    _OMP_HOST = None
+    _OMP_PORT = None
+    _OMP_USER = None
+    _OMP_PASSWD = None
+
+    def setUp(self):
+        self.client = openvas.omplib.OMPClient(self._OMP_HOST, self._OMP_PORT,
+                                               self._OMP_USER, self._OMP_PASSWD)
+        self.client.open()
+
+    def tearDown(self):
+        self.client.close()
+
+
+class TestOMP(OMPTestBase):
+
+    def test_help(self):
+        self.client.help()
+
+    def test_xml_0(self):
+        text = self.client.xml('<help/>')
+        self.assert_(isinstance(text, basestring))
+        self.assert_(text != '')
+
+    def test_xml_1(self):
+        xml = self.client.xml('<help/>', xml_result=True)
+        self.assert_(etree.iselement(xml))
+
+    #--- agent ---
+
+    def test_agent(self):
+        installerdata = "#!/bin sh\necho 'agent'"
+        name = "omp_test_agent"
+        try:
+            self.client.create_agent(name, installerdata, 'Some Agent')
+            agents = self.client.get_agents()
+            self.assert_(inspect.isgenerator(agents))
+            for agent in agents:
+                self.assert_(isinstance(agent, dict))
+                pass
+            agent = self.client.get_agents(name)
+            self.assert_(isinstance(agent, dict))
+            self.assert_(agent['name'] == name)
+        finally:
+            self.client.delete_agent(name)
+
+    #--- config ---
+
+    def test_config_get_all(self):
+        configs = self.client.get_configs()
+        self.assert_(inspect.isgenerator(configs))
+        for cfg in configs:
+            self.assert_(isinstance(cfg, dict))
+
+    def test_config_get_one(self):
+        cfg = self.client.get_configs('empty')
+        self.assert_(isinstance(cfg, dict))
+        self.assert_(cfg['name'] == 'empty')
+
+    def _test_config(self):
+        rcdata = open(tests_path('new_task_empty_rc.xml')).read()
+        name = "omp_test_config"
+        try:
+            self.client.create_config(name, rcdata)
+        finally:
+            self.client.delete_config(name)
+
+    #--- lsc_credentials ---
+
+    def _test_lsc_credentials(self):
+        name = "omp_test_lsc_credential"
+        #self.client.create_lsc_credential(name, 'LoGinInOut', 'Kommentar')
+        #self.client.create_lsc_credential(name+'2', 'LoGinInOutx', 'Kommentarx')
+        creds = self.client.get_lsc_credentials()
+        self.assert_(inspect.isgenerator(creds))
+        creds = list(self.client.get_lsc_credentials(name))
+        self.assert_(len(creds) == 1)
+        #self.client.delete_lsc_credential(name)
+        #self.client.delete_lsc_credential(name+'2')
+
+    #--- targets ---
+
+    def test_targets(self):
+        name = "omp_test_targets"
+        hosts = '10.10.10.10\nwww.example.com'
+        try:
+            self.client.create_target(name, hosts, 'Kommentar on Targets')
+
+            targets = self.client.get_targets()
+            self.assert_(inspect.isgenerator(targets))
+            for target in targets:
+                self.assert_(isinstance(target, dict))
+
+            target = self.client.get_targets(name)
+            self.assert_(isinstance(target, dict))
+            self.assert_(target['name'] == name)
+        finally:
+            self.client.delete_target(name)
+
+    #--- escalators ---
+
+    def test_escalator(self):
+        # can't test since creating escalators is not yet implemented
+        name = "omp_test_escalators"
+        # until creating escalators is not yet implemented simply create
+        # using_xml()
+        xml = """
+        <create_escalator>
+        <name>%s</name>
+        <comment>hjhjklh</comment>
+        <condition>Threat level at least<data><name>level</name>Medium</data></condition>
+        <event>Task run status changed<data><name>status</name>Done</data></event>
+        <method>Email<data><name>to_address</name>aaaa@example.com</data><data><name>from_address</name>vaaa@example.com</data><data><name>notice</name>1</data></method>
+        </create_escalator>
+        """ % name.encode('utf-8')
+        try:
+            self.client.xml(xml)
+            escalators = self.client.get_escalators()
+            self.assert_(inspect.isgenerator(escalators))
+            for escalator in escalators:
+                self.assert_(isinstance(escalator, dict))
+
+            escalator = self.client.get_escalators(name)
+            self.assert_(isinstance(escalator, dict))
+            self.assert_(escalator['name'] == name)
+        finally:
+            self.client.delete_escalator(name)
+            pass
+
+    #--- misc ---
+
+    def test_get_preferences(self):
+        prefs = self.client.get_preferences()
+        self.assert_(isinstance(prefs, dict))
+
+    def _test_get_certificates(self):
+        certs = self.client.get_certificates()
+        print certs
+
+    def test_get_report(self):
+        report = self.client.get_report(SAMPLE_REPORT_ID)
+        #print etree.tostring(report[3])
+        #print etree.tostring(report)
+
+
+class main(unittest.main):
+    def parseArgs(self, argv):
+        """
+
+Examples:
+  %prog                            - run default set of tests
+  %prog MyTestSuite                - run suite 'MyTestSuite'
+  %prog MyTestCase.testSomething   - run MyTestCase.testSomething
+  %prog MyTestCase                 - run all 'test*' test methods
+                                               in MyTestCase
+        """
+        import getpass
+        import optparse
+        parser = optparse.OptionParser('usage: %prog [options] [test] [...]' +
+                                       self.parseArgs.__doc__.rstrip(),
+                                       add_help_option=False)
+        parser.add_option("--help", action="help",
+                          help="show this help message and exit")
+        parser.set_defaults(verbosity=1)
+        parser.add_option('-v', '--verbose', dest='verbosity',
+                          action='store_const', const=2)
+        parser.add_option('-q', '--quiet', dest='verbosity',
+                          action='store_const', const=0)
+
+        group = parser.add_option_group('Options for connecting to the manager')
+        group.add_option('-h', '--host',
+                          default=openvas.omplib.MANAGER_ADDRESS,
+                          help="Connect to manager on host HOST (default: %default)")
+        group.add_option('-p', '--port', type=int,
+                          default=openvas.omplib.MANAGER_PORT,
+                          help="Use port number PORT (default: %default)")
+        group.add_option('-u', '--username', default=getpass.getuser(),
+                          help="OMP username (default: current user's name)")
+        group.add_option('-w', '--password',
+                          help="OMP password (use '-' to be prompted, default: same as username)")
+
+        opts, args = parser.parse_args(argv[1:])
+
+        if not (0 < opts.port < 65536):
+            parser.error('Manager port must be a number between 1 and 65535.')
+        if opts.password is None:
+            opts.password = opts.username
+        elif opts.password == '-':
+            opts.password = getpass.getpass('Enter password for openvas-manager: ')
+
+        self.verbosity = int(opts.verbosity)
+        OMPTestBase._OMP_HOST = opts.host
+        OMPTestBase._OMP_PORT = opts.port
+        OMPTestBase._OMP_USER = opts.username
+        OMPTestBase._OMP_PASSWD = opts.password
+        
+        if len(args) == 0 and self.defaultTest is None:
+            self.test = self.testLoader.loadTestsFromModule(self.module)
+            return
+        if len(args) > 0:
+            self.testNames = args
+        else:
+            self.testNames = (self.defaultTest,)
+        self.createTests()
+
+if __name__ == '__main__':
+    main()
