67f8bb3e4f7a8f97b91643dff470fe20d7bd78c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:04 +0000

    Add package collective.colorbox/0.1a/collective.colorbox-0.1a.zip

---
+++ packages/collective.colorbox/collective.colorbox-0.1a.zip/collective.colorbox-0.1a/README.txt
@@ -0,0 +1,8 @@
+Documentation
+=============
+Very useful for creating an image gallery
+This product loads the colorbox plugin for Plone.
+
+to see it in action after installation visit the @@colorbox.demo page
+
+Based on http://colorpowered.com/colorbox/
---
+++ packages/collective.colorbox/collective.colorbox-0.1a.zip/collective.colorbox-0.1a/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1a'
+
+setup(name='collective.colorbox',
+      version=version,
+      description="A jQuery plugin that provides lightboxes in Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: User Interfaces",
+        ],
+      keywords='plone slideshow lightbox jquery colorbox',
+      author='David Bain',
+      author_email='pigeonflight@gmail.com',
+      url='http://plone.org/products/collective.colorbox',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
