ec79fae1b8d9b9d479a98784da0628c6cd4ba00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:40 +0000

    Add package plone.memoize/1.1/plone.memoize-1.1.zip

---
+++ packages/plone.memoize/plone.memoize-1.1.zip/plone.memoize-1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '1.1'
+
+setup(name='plone.memoize',
+      version=version,
+      description="Decorators for caching the values of functions and methods",
+      long_description=open("README.txt").read() + '\n' + \
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='plone memoize decorator cache',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.memoize',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+          test=[
+            'zope.configuration',
+            'zope.publisher',
+            'zope.testing',
+          ]
+      ),
+      install_requires=[
+        'setuptools',
+        'zope.annotation',
+        'zope.component',
+        'zope.interface',
+        'zope.ramcache',
+      ],
+      )
