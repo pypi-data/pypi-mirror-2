f224779234cd54b0b30ccee0eb6bb7b79682851f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:22 +0000

    Add package collective.portletpage/1.0b3/collective.portletpage-1.0b3.tar.gz

---
+++ packages/collective.portletpage/collective.portletpage-1.0b3.tar.gz/collective.portletpage-1.0b3/README.txt
@@ -0,0 +1,19 @@
+Overview
+========
+
+After installing this product, you will be able to add a "Portlet Page". This
+is a standard Plone "Page", but it also has a "Portlets" tab, from which you
+may assign portlets into four slots. The portlets will be shown on the main
+view of the content object.
+
+
+Installation
+============
+
+Install it as normal, see http://plone.org/documentation/tutorial/buildout.
+
+
+Credits
+=======
+
+This work was generously sponsored by Centrepoint (http://centrepoint.org.uk)
---
+++ packages/collective.portletpage/collective.portletpage-1.0b3.tar.gz/collective.portletpage-1.0b3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b3'
+
+setup(name='collective.portletpage',
+      version=version,
+      description="A Plone page that can contain portlets",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Martin Aspeli',
+      author_email='optilude@gmail.com',
+      url='http://plone.org',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
