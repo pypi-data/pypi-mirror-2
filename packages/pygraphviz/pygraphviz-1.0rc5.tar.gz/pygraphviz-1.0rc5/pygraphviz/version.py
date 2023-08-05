6035baf4a76d50097a3926e7e7755e845ef89dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package pygraphviz/1.0rc5/pygraphviz-1.0rc5.tar.gz

---
+++ packages/pygraphviz/pygraphviz-1.0rc5.tar.gz/pygraphviz-1.0rc5/pygraphviz/version.py
@@ -0,0 +1,11 @@
+"""
+Version information for PyGraphviz, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+__version__ = '1.0.dev1780'
+__revision__ = '1780'
+__date__ = 'Thu Jul 22 15:46:24 2010'
+
