ad829c3c9fc626f4feaa74dd20d566a0efcb995e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package zope.contentprovider/3.7/zope.contentprovider-3.7.tar.gz

---
+++ packages/zope.contentprovider/zope.contentprovider-3.7.tar.gz/zope.contentprovider-3.7/src/zope/contentprovider/tests.py
@@ -0,0 +1,61 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Content provider tests
+
+$Id: tests.py 106952 2009-12-22 22:10:15Z hannosch $
+"""
+__docformat__ = 'restructuredtext'
+import os.path
+import unittest
+from zope.component import eventtesting
+from zope.testing import doctest, cleanup
+from zope.testing.doctest import DocFileSuite
+
+counter = 0
+mtime_func = None
+
+def setUp(test):
+    cleanup.setUp()
+    eventtesting.setUp()
+
+    from zope.browserpage.metaconfigure import registerType
+    from zope.contentprovider import tales
+    registerType('provider', tales.TALESProviderExpression)
+
+    # Make sure we are always reloading page template files ;-)
+    global mtime_func
+    mtime_func = os.path.getmtime
+    def number(x):
+        global counter
+        counter += 1
+        return counter
+    os.path.getmtime = number
+
+
+def tearDown(test):
+    cleanup.tearDown()
+    os.path.getmtime = mtime_func
+    global counter
+    counter = 0
+
+def test_suite():
+    return unittest.TestSuite((
+        DocFileSuite('README.txt',
+                     setUp=setUp, tearDown=tearDown,
+                     optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+                     ),
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
