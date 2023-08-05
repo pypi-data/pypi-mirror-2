3e32e080972ccbfa3c864f56ae621aebca69c558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:06 +0000

    Add package python-nagext/0.1/python-nagext-0.1.tar.gz

---
+++ packages/python-nagext/python-nagext-0.1.tar.gz/python-nagext-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='python-nagext',
+    version='0.1',
+    description='Python interface to Nagios external commands',
+    license='LGPL3',
+    author='Alexander Duryagin',
+    author_email='daa@vologda.ru',
+    url='http://github.com/daa/nagext',
+    py_modules=['nagext'])
+
