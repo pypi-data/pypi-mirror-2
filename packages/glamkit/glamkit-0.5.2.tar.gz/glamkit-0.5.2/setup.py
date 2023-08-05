789781976228d991473de68c59fd3f08d9ebc31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:06 +0000

    Add package glamkit/0.5.2/glamkit-0.5.2.tar.gz

---
+++ packages/glamkit/glamkit-0.5.2.tar.gz/glamkit-0.5.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glamkit',
+    version='0.5.2',
+    author='Thomas Ashelford',
+    author_email='thomas@interaction.net.au',
+    description='A Django toolkit for building websites for the Galleries, Libraries, Archives and Museums sector.',
+    url='http://github.com/glamkit/glamkit',
+    packages=find_packages(exclude=[]),
+    package_data={'glamkit': ['docs/*']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
