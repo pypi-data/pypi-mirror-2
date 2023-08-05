70594491ce7440bed69557ff11c370702591cd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:55 +0000

    Add package jogging/0.2/jogging-0.2.tar.gz

---
+++ packages/jogging/jogging-0.2.tar.gz/jogging-0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='jogging',
+      version='0.2',
+      description='Jogging makes logging in django easier',
+      author='Zain',
+      author_email='zain@inzain.net',
+      url='',
+      packages = ['jogging',],
+      package_dir = {'jogging':'jogging'},
+      test_suite='tests.main',
+     )
+
