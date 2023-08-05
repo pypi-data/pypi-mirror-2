22c74e6a4521bc4bdf51061e41c74cbab4e1127a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package plone.i18n/1.0.10/plone.i18n-1.0.10.zip

---
+++ packages/plone.i18n/plone.i18n-1.0.10.zip/plone.i18n-1.0.10/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+version = '1.0.10'
+
+setup(name='plone.i18n',
+      version=version,
+      description="Advanced i18n/l10n features",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Zope2',
+      ],
+      keywords='i18n l10n Plone',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.i18n',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      )
