159bd70388fbcbf2359455ed47e07fbdfb6507a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:25 +0000

    Add package redturtle.imagedevent/0.6.0/redturtle.imagedevent-0.6.0.tar.gz

---
+++ packages/redturtle.imagedevent/redturtle.imagedevent-0.6.0.tar.gz/redturtle.imagedevent-0.6.0/README.txt
@@ -0,0 +1,40 @@
+Imaged event
+============
+
+An enhanced version of the base Plone event content type.
+
+Image field
+-----------
+
+After intalling this you'll see that the Plone event will have a new **image field** and **caption**
+like the News Item content type.
+
+As News Item do, the event now behave **image resize** functionality.
+
+Event type field
+----------------
+
+It this kind of event the *Subject* and *Event type* fields has been splitted and are now two
+distinct data.
+
+If you are using an older version of this product, you can run the proper upgrade step to fix all
+old values.
+
+Credits
+=======
+
+Developed with the support of `Camera di Commercio di Ferrara`__; Camera di Commercio supports
+the `PloneGov initiative`__.
+
+__ http://www.fe.camcom.it/
+__ http://www.plonegov.it/
+
+Authors
+=======
+
+This product was developed by RedTurtle Technology team.
+
+.. image:: http://www.redturtle.net/redturtle_banner.png
+   :alt: RedTurtle Technology Site
+   :target: http://www.redturtle.net/
+
---
+++ packages/redturtle.imagedevent/redturtle.imagedevent-0.6.0.tar.gz/redturtle.imagedevent-0.6.0/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.6.0'
+
+tests_require=['zope.testing']
+
+setup(name='redturtle.imagedevent',
+      version=version,
+      description="A redefinition of the Plone Event content type, having an image field like News Item",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='plone event image content',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://svn.plone.org/svn/collective/redturtle.imagedevent',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['redturtle', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'redturtle.imagedevent.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*- 
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
