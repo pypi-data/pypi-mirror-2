92a846cd6cfd0b8670fa3cdb3b51c8a68c16516e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:14 +0000

    Add package zc.recipe.testrunner/1.0.6/zc.recipe.testrunner-1.0.6.tar.gz

---
+++ packages/zc.recipe.testrunner/zc.recipe.testrunner-1.0.6.tar.gz/zc.recipe.testrunner-1.0.6/src/zc/recipe/testrunner/tests.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import re
+import zc.buildout.testing
+
+import unittest
+import zope.testing
+from zope.testing import doctest, renormalizing
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install_develop('zc.recipe.testrunner', test)
+    zc.buildout.testing.install_develop('zc.recipe.egg', test)
+    zc.buildout.testing.install('zope.testing', test)
+
+def test_suite():
+    return unittest.TestSuite((
+        #doctest.DocTestSuite(),
+        doctest.DocFileSuite(
+            'README.txt',
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            checker=renormalizing.RENormalizing([
+               zc.buildout.testing.normalize_path,
+               zc.buildout.testing.normalize_script,
+               zc.buildout.testing.normalize_egg_py,
+               (re.compile('#!\S+py\S*'), '#!python'),
+               #kill duration
+               (re.compile('\d[.]\d+ seconds'), '0.001 seconds'),
+               #kill zope.testing version
+               (re.compile('zope.testing-[^-]+-'), 'zope.testing-X-'),
+               #change distribute to setuptools:
+               (re.compile('/distribute-'), '/setuptools-'),
+               #kill setuptools version
+               (re.compile('setuptools-[^-]+-'), 'setuptools-X-'),
+               ])
+            ),
+
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
