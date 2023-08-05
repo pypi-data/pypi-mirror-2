0965ab987f79083ab6b2b725c79adef831e1c212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:46 +0000

    Add package should_dsl/2.0a2/should_dsl-2.0a2.tar.gz

---
+++ packages/should_dsl/should_dsl-2.0a2.tar.gz/should_dsl-2.0a2/run_all_examples.py
@@ -0,0 +1,21 @@
+import doctest
+import unittest
+import os
+import sys
+
+flags = doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS
+
+if __name__ == '__main__':
+    doctests_path = os.path.join(os.path.dirname(__file__),  'src', 'doctests')
+    suite = unittest.TestSuite()
+    runner = unittest.TextTestRunner()
+
+    suite.addTest(doctest.DocFileSuite('README.rst', optionflags=flags))
+    for doctest_file in os.listdir(doctests_path):
+        if doctest_file.endswith('.txt'):
+            suite.addTest(doctest.DocFileSuite(os.path.join(doctests_path,
+                                                            doctest_file),
+                                               optionflags=flags))
+    result = runner.run(suite)
+    sys.exit(int(bool(result.failures or result.errors)))
+
---
+++ packages/should_dsl/should_dsl-2.0a2.tar.gz/should_dsl-2.0a2/setup.py
@@ -0,0 +1,34 @@
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+from setuptools import setup, find_packages
+
+version = '2.0a2'
+
+readme = open('README.rst').read()
+
+setup(name='should_dsl',
+      version=version,
+      description='Should assertions in Python as clear and readable as possible',
+      long_description=readme,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Documentation',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Testing',
+      ],
+      keywords='should dsl assertion bdd python expectation',
+      author='Hugo Lopes Tavares',
+      author_email='hltbra@gmail.com',
+      url='http://github.com/hugobr/should-dsl',
+      license='MIT License',
+      packages=['should_dsl'],
+      package_dir={'should_dsl': 'src'},
+      install_requires=[],
+      entry_points="",
+      )
