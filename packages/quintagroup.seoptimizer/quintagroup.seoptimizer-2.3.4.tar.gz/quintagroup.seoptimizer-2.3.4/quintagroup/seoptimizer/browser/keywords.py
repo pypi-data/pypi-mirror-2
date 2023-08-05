9026338e81786e909ba90d6e1a8426ef67d717dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:10 +0000

    Add package quintagroup.seoptimizer/2.3.4/quintagroup.seoptimizer-2.3.4.tar.gz

---
+++ packages/quintagroup.seoptimizer/quintagroup.seoptimizer-2.3.4.tar.gz/quintagroup.seoptimizer-2.3.4/quintagroup/seoptimizer/browser/keywords.py
@@ -0,0 +1,83 @@
+import urllib, re, os, commands
+from xml.dom import minidom, Node
+
+from zope.interface import implements
+from Products.Five.browser import BrowserView
+
+from Products.CMFCore.utils import getToolByName
+
+from interfaces import IValidateSEOKeywordsView
+from quintagroup.seoptimizer import SeoptimizerMessageFactory as _
+
+class ValidateSEOKeywordsView(BrowserView):
+
+    implements(IValidateSEOKeywordsView)
+
+    def validateKeywords(self, text):
+        """ see interface """
+        ts = getToolByName(self.context, 'translation_service')
+        # extract keywords from text
+        if text.lower().strip():
+            keywords = map(lambda x: x.strip(), text.lower().strip().split('\n'))
+        else:
+            return ts.utranslate(None, _(u'Keywords list is empty!'), context=self.context)
+        # request html page of context object
+        url = '%s?without_metatag_keywords=1' % self.context.absolute_url()
+
+        # extract words from url page using lynx browser (test page by 'url' randered without metatag keywords)
+        page_text = commands.getoutput('lynx --dump --nolist %s' % url).lower()
+        if page_text and page_text != 'sh: lynx: command not found':
+            page_text = page_text.decode('utf8')
+        else:
+            return _(u'Could not find lynx browser!')
+
+        # check every keyword on appearing in body of html page
+        missing = []
+        finding = []
+        added = {}
+        finded = {}
+        for keyword in keywords:
+            keyword = keyword.decode('utf8')
+            if keyword:
+                keyword_on_page =  len(re.findall(u'\\b%s\\b' % keyword, page_text, re.I|re.U))
+                if keyword not in added.keys() and not keyword_on_page:
+                    missing.append(keyword+u' - 0')
+                    added[keyword] = 1
+                if keyword not in finded.keys() and keyword_on_page:
+                    finding.append(keyword+u' - '+repr(keyword_on_page))
+                    finded[keyword] = 1
+        # return list of missing and fount keywords
+        if missing or finding:
+            msg = ts.utranslate(None, _('number_keywords', default=u'Number of keywords at page:\n${found}\n${missing}',
+                                mapping={'missing':'\n'.join(missing), 'found': '\n'.join(finding)}), context=self.context)
+        else:
+            msg = ''
+        return msg
+
+    def walkTextNodes(self, parent, page_words=[]):
+        for node in parent.childNodes:
+            if node.nodeType == Node.ELEMENT_NODE:
+                self.walkTextNodes(node, page_words)
+            elif node.nodeType == Node.TEXT_NODE:
+                value = node.nodeValue
+                if value is not None:
+                    page_words.extend(map(lambda x: x.lower(), value.split()))
+
+    def strip_tags(self, in_text):
+        s_list = list(in_text)
+        i,j = 0,0
+
+        while i < len(s_list):
+            if s_list[i] == '<':
+                while s_list[i] != '>':
+                    # pop everything from the the left-angle bracket until the right-angle bracket
+                    s_list.pop(i)
+
+                # pops the right-angle bracket, too
+                s_list.pop(i)
+            else:
+                i=i+1
+
+        # convert the list back into text
+        join_char=''
+        return join_char.join(s_list)
---
+++ packages/quintagroup.seoptimizer/quintagroup.seoptimizer-2.3.4.tar.gz/quintagroup.seoptimizer-2.3.4/quintagroup/seoptimizer/setuphandlers.py
@@ -0,0 +1,84 @@
+import logging
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.Expression import Expression
+
+logger = logging.getLogger('quintagroup.seoptimizer')
+
+def migrationActions(site):
+    old = 'qseo_properties_edit_form'
+    new = '@@seo-context-properties'
+    condition = "python:exists('portal/@@seo-context-properties')"
+    for ptype in site.portal_types.objectValues():
+        acts = filter(lambda x: x.id == 'seo_properties' , ptype.listActions())
+        for act in acts:
+            log = 0
+            if not act.condition:
+                act.condition = Expression(condition)
+                log = 1
+            ac_exp = act.getActionExpression()
+            if old in ac_exp:
+                act.setActionExpression(ac_exp.replace(old, new))
+                log = 1
+            if log:
+                logger.log(logging.INFO, "Updated \"SEO Properties\" action in %s type." % ptype.id)
+
+def removeSkin(self, layer):
+    """ Remove layers.
+    """
+    skinstool = getToolByName(self, 'portal_skins')
+    for skinName in skinstool.getSkinSelections():
+        original_path = skinstool.getSkinPath(skinName)
+        original_path = [l.strip() for l in original_path.split(',')]
+        new_path= []
+        for l in original_path:
+            if (l == layer) or (l.startswith(layer+'/')):
+                logger.log(logging.INFO, "Removed %s layer from %s skin." % (l, skinName))
+                continue
+            new_path.append(l)
+        skinstool.addSkinSelection(skinName, ','.join(new_path))
+
+def removeActions(self):
+    """ Remove actions.
+    """
+    tool = getToolByName(self, 'portal_types')
+    for ptype in tool.objectValues():
+        if ptype.getId() in ['File','Document','News Item']:
+            acts = filter(lambda x: x.id == 'seo_properties', ptype.listActions())
+            action = acts and acts[0] or None
+            if action != None:
+                acts = list(ptype.listActions())
+                ptype.deleteActions([acts.index(a) for a in acts if a.getId()=='seo_properties'])
+                logger.log(logging.INFO, "Deleted \"SEO Properties\" action for %s type." % ptype.id)
+
+def remove_configlets(context, conf_ids):
+    """ Remove configlets.
+    """
+    configTool = getToolByName(context, 'portal_controlpanel', None)
+    if configTool:
+        for id in conf_ids:
+            configTool.unregisterConfiglet(id)
+            logger.log(logging.INFO, "Unregistered \"%s\" configlet." % id)
+
+def importVarious(context):
+    """ Do customized installation.
+    """
+    if context.readDataFile('seo_install.txt') is None:
+        return
+
+def reinstall(context):
+    """ Do customized reinstallation.
+    """
+    if context.readDataFile('seo_reinstall.txt') is None:
+        return
+    site = context.getSite()
+    migrationActions(site)
+
+def uninstall(context):
+    """ Do customized uninstallation.
+    """
+    if context.readDataFile('seo_uninstall.txt') is None:
+        return
+    site = context.getSite()
+    removeSkin(site, 'quintagroup.seoptimizer' )
+    removeActions(site)
+    remove_configlets(site, ('quintagroup.seoptimizer',))
