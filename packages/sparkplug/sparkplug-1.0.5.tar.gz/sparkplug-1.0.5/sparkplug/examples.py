160fac65e3eee44647b4f0d2564354ed679d10cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package sparkplug/1.0.5/sparkplug-1.0.5.tar.gz

---
+++ packages/sparkplug/sparkplug-1.0.5.tar.gz/sparkplug-1.0.5/sparkplug/examples.py
@@ -0,0 +1,15 @@
+class EchoConsumer(object):
+    def __init__(self, channel, format):
+        self.channel = channel
+        self.format = format
+    
+    def __call__(self, msg):
+        print self.format % {'body': msg.body}
+        self.channel.basic_ack(msg.delivery_tag)
+
+class Broken(object):
+    def __init__(self, channel):
+        self.channel = channel
+    
+    def __call__(self, msg):
+        raise ValueError(msg)
