07f5c6d4818c3cc57e8a8d54d21d27c62af47c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:10 +0000

    Add package gocept.reference/0.8.0/gocept.reference-0.8.0.tar.gz

---
+++ packages/gocept.reference/gocept.reference-0.8.0.tar.gz/gocept.reference-0.8.0/src/gocept/reference/tests.py
@@ -0,0 +1,44 @@
+# -*- coding: latin-1 -*-
+# Copyright (c) 2007-2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+"""Tests for gocept.reference"""
+
+import unittest
+import zope.interface.verify
+
+import gocept.reference.content
+import gocept.reference.interfaces
+import gocept.reference.manager
+import gocept.reference.testing
+
+
+class TestInterfaces(unittest.TestCase):
+
+    def test_IReferenceSource(self):
+        zope.interface.verify.verifyObject(
+            gocept.reference.interfaces.IReferenceSource,
+            gocept.reference.content.ReferenceSource(object))
+
+    def test_IReferenceTarget(self):
+        zope.interface.verify.verifyObject(
+            gocept.reference.interfaces.IReferenceTarget,
+            gocept.reference.content.ReferenceTarget(object))
+
+    def test_IReferenceManager(self):
+        zope.interface.verify.verifyObject(
+            gocept.reference.interfaces.IReferenceManager,
+            gocept.reference.manager.ReferenceManager())
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestInterfaces))
+    suite.addTest(gocept.reference.testing.FunctionalDocFileSuite(
+            'reference.txt',
+            'collection.txt',
+            'verify.txt',
+            'regression.txt',
+            'field.txt',
+            'fix.txt',
+            ))
+    return suite
