c7783ace1c8e5d591f92d769480d6b42f4298900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:32 +0000

    Add package dolmen.app.authentication/0.1.1/dolmen.app.authentication-0.1.1.tar.gz

---
+++ packages/dolmen.app.authentication/dolmen.app.authentication-0.1.1.tar.gz/dolmen.app.authentication-0.1.1/src/dolmen/app/authentication/browser/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from dolmen.app.authentication.browser.actions import (
+    AnonymousActionsMenu, AnonymousMenuEntry, UserActionsMenu, UserMenuEntry)
+
+from dolmen.app.authentication.browser.login import ILoginForm, Login
+from dolmen.app.authentication.browser.logout import LogoutAction, LoggedOut
