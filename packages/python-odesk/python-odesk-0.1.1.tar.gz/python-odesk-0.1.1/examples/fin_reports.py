d81b6c9c7e7a69b57e02267230908e09a46ad929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:53 +0000

    Add package python-odesk/0.1.1/python-odesk-0.1.1.tar.gz

---
+++ packages/python-odesk/python-odesk-0.1.1.tar.gz/python-odesk-0.1.1/examples/fin_reports.py
@@ -0,0 +1,44 @@
+"""
+Python bindings to odesk API
+python-odesk version 0.1
+(C) 2010 oDesk
+"""
+import odesk
+
+from datetime import date
+
+PUBLIC_KEY = None
+SECRET_KEY = None
+
+
+#TODO: Desktop app example (check if it's working at all - wasn't last time)
+
+def fin_reports(public_key, secret_key):
+    print "Emulating web-based app"
+    #Instantiating a client without an auth token
+    client = odesk.Client(public_key, secret_key)
+    print "Please to this URL (authorize the app if necessary):"
+    print client.auth.auth_url()
+    print "After that you should be redirected back to your app URL with " + \
+          "additional ?frob= parameter"
+    frob = raw_input('Enter frob: ') 
+    auth_token, user = client.auth.get_token(frob)
+    print "Authenticated user:"
+    print user
+    #Instantiating a new client, now with a token. 
+    #Not strictly necessary here (could just set `client.auth_token`), but 
+    #typical for web apps, which wouldn't probably keep client instances 
+    #between requests
+    client = odesk.Client(public_key, secret_key, auth_token)
+    print client.finreports.get_provider_billings('1111',  
+                    odesk.Query(select=['date', 'type',
+                                        'amount'], 
+                    where=((odesk.Q('date') <= date.today()))))
+
+ 
+if __name__ == '__main__':
+    public_key = PUBLIC_KEY or raw_input('Enter public key: ')
+    secret_key = SECRET_KEY or raw_input('Enter secret key: ')
+
+    fin_reports(public_key, secret_key)
+
