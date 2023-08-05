031ad1d3e7a149b8f69c37d0a6ed4a138e0c69e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:04 +0000

    Add package plonetheme.sunburst/1.0b6/plonetheme.sunburst-1.0b6.zip

---
+++ packages/plonetheme.sunburst/plonetheme.sunburst-1.0b6.zip/plonetheme.sunburst-1.0b6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '1.0b6'
+
+setup(name='plonetheme.sunburst',
+      version=version,
+      description="New theme for Plone 4.0",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='web zope plone theme',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plonetheme.sunburst',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      extras_require={'test': ['collective.testcaselayer']},
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
