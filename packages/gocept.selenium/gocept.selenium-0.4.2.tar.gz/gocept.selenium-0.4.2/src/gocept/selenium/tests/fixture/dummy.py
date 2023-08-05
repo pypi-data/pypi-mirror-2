1aba35f9a018e950c20d46de4613e339de955649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package gocept.selenium/0.4.2/gocept.selenium-0.4.2.tar.gz

---
+++ packages/gocept.selenium/gocept.selenium-0.4.2.tar.gz/gocept.selenium-0.4.2/src/gocept/selenium/tests/fixture/dummy.py
@@ -0,0 +1,36 @@
+#############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import zope.security.proxy
+
+
+class Set(object):
+
+    def __call__(self):
+        c = zope.security.proxy.removeSecurityProxy(self.context)
+        c.foo = 1
+        return 'setting done'
+
+
+class Get(object):
+
+    def __call__(self):
+        c = zope.security.proxy.removeSecurityProxy(self.context)
+        return str(getattr(c, 'foo', 0))
+
+
+class Error(object):
+
+    def __call__(Self):
+        raise ValueError()
