e0689324e4c2599891bf642ba50e2041166b7319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package pbkdf2.py/1.0/pbkdf2.py-1.0.tar.gz

---
+++ packages/pbkdf2.py/pbkdf2.py-1.0.tar.gz/pbkdf2.py-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pbkdf2.py',
+      version='1.0',
+      description='Simple Library for Using PBKDF2',
+      author='Tobias Ammann',
+      packages=['pbkdf2'],
+     )
+
