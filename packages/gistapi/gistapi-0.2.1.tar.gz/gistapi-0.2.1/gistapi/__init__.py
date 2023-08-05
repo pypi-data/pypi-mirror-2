3a134c7aa0fc03df617ff86fd6ea889c0b0fa150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:54 +0000

    Add package gistapi/0.2.1/gistapi-0.2.1.tar.gz

---
+++ packages/gistapi/gistapi-0.2.1.tar.gz/gistapi-0.2.1/gistapi/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+GistAPI.py -- A Python wrapper for GitHub's Gist API
+(c) 2010 Kenneth Reitz. MIT License.
+"""
+
+
+from core import *
+
+
+__author__ = 'Kenneth Reitz'
+__version__ = '0.2.1'
+__build__ = 0x000201
+__license__ = 'MIT'
