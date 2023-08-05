0aea4ca92b262c8cb8c8c723e18308315ddd5611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:04 +0000

    Add package svg.charts/2.0.4/svg.charts-2.0.4.zip

---
+++ packages/svg.charts/svg.charts-2.0.4.zip/svg.charts-2.0.4/setup.py
@@ -0,0 +1,40 @@
+#!python
+
+# $Id: setup.py 95 2010-04-14 17:14:52Z jaraco $
+
+from setuptools import setup, find_packages
+
+setup(name = "svg.charts",
+	version = "2.0.4",
+	description = "Python SVG Charting Library",
+	long_description = open('README.txt').read().strip(),
+	author = "Jason R. Coombs",
+	author_email = "jaraco@jaraco.com",
+	url = "http://py-svg.sourceforge.net",
+	packages = find_packages(exclude=['tests', 'docs']),
+	zip_safe=True,
+	namespace_packages=['svg'],
+	include_package_data = True,
+	install_requires=[
+		'python-dateutil>=1.4',
+		'cssutils>=0.9.6b3',
+		'lxml>=2.0',
+	],
+	license = "MIT",
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		"Programming Language :: Python",
+		"License :: OSI Approved :: MIT License",
+		"Topic :: Multimedia :: Graphics :: Presentation",
+	],
+	entry_points = {
+	},
+	tests_require=[
+		'nose>=0.10',
+	],
+	test_suite = "nose.collector",
+	
+	)
+
