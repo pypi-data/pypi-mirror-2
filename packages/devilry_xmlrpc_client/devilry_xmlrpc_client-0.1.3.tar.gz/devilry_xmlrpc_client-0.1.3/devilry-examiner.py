4117eb2415390fabc2db85e9be3214c17ad52be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:16 +0000

    Add package devilry_xmlrpc_client/0.1.3/devilry_xmlrpc_client-0.1.3.tar.gz

---
+++ packages/devilry_xmlrpc_client/devilry_xmlrpc_client-0.1.3.tar.gz/devilry_xmlrpc_client-0.1.3/devilry-examiner.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from cli import Cli, Login, Init, FormLogin
+from examinercmd import ListAssignmentGroups, Sync, ListAssignments, \
+        Feedback, UnpublishFeedback, PublishFeedback, InfoCmd, Guide
+
+
+# TODO: make sure SESSION_COOKIE_SECURE is enabled by default or something
+#       see: http://docs.djangoproject.com/en/dev/topics/http/sessions/#settings
+
+
+if __name__ == '__main__':
+    Cli([
+        Guide,
+        Init,
+        Login,
+        FormLogin,
+        ListAssignments,
+        ListAssignmentGroups,
+        Sync,
+        InfoCmd,
+        Feedback,
+        UnpublishFeedback,
+        PublishFeedback]).cli()
---
+++ packages/devilry_xmlrpc_client/devilry_xmlrpc_client-0.1.3.tar.gz/devilry_xmlrpc_client-0.1.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+
+
+setup(name = 'devilry_xmlrpc_client',
+      version = '0.1.3',
+      description = 'The devilry xmlrpc client and shell scripts.',
+      url = 'http://devilry.github.com',
+      author = 'Devilry',
+      author_email = 'devilry-support@ifi.uio.no',
+      package_dir = {'devilry_xmlrpc_client': ''},
+      packages = ['devilry_xmlrpc_client'],
+      scripts = ['devilry-examiner.py']
+     )
