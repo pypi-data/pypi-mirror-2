243913662fb612fd0d97490e43e6535ad007b3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package zope.testbrowser/3.8.1/zope.testbrowser-3.8.1.tar.gz

---
+++ packages/zope.testbrowser/zope.testbrowser-3.8.1.tar.gz/zope.testbrowser-3.8.1/setup.py
@@ -0,0 +1,88 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.testbrowser package
+
+$Id: setup.py 111044 2010-04-19 05:59:53Z tseaver $
+"""
+import os
+from setuptools import setup, find_packages
+
+long_description = (
+    '.. contents::\n\n'
+    + open('README.txt').read()
+    + '\n\n'
+    + open(os.path.join('src', 'zope', 'testbrowser', 'README.txt')).read()
+    + '\n\n'
+    + open('CHANGES.txt').read()
+    )
+
+setup(
+    name = 'zope.testbrowser',
+    version = '3.8.1',
+    url = 'http://pypi.python.org/pypi/zope.testbrowser',
+    license = 'ZPL 2.1',
+    description = 'Programmable browser for functional black-box tests',
+    author = 'Zope Corporation and Contributors',
+    author_email = 'zope-dev@zope.org',
+    long_description = long_description,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Internet :: WWW/HTTP',
+        ],
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['zope',],
+    tests_require = ['zope.testing'],
+    install_requires = [
+        'ClientForm',
+        # mechanize 0.2.0 folds in ClientForm, makes incompatible API changes
+        'mechanize<0.2dev',
+        'setuptools',
+        'zope.interface',
+        'zope.schema',
+        'pytz',
+        ],
+    extras_require = {
+        'test': [
+            'zope.browserpage',
+            'zope.browserresource',
+            'zope.component',
+            'zope.container',
+            'zope.principalregistry',
+            'zope.ptresource',
+            'zope.publisher',
+            'zope.security',
+            'zope.site',
+            'zope.traversing',
+            'zope.app.appsetup',
+            'zope.app.publication',
+            'zope.app.testing',
+            ],
+        'zope-functional-testing': [
+            'zope.app.testing',
+            ],
+        },
+    include_package_data = True,
+    zip_safe = False,
+    )
---
+++ packages/zope.testbrowser/zope.testbrowser-3.8.1.tar.gz/zope.testbrowser-3.8.1/src/zope/testbrowser/__init__.py
@@ -0,0 +1,17 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Browser Simulator for Functional DocTests
+
+$Id: __init__.py 93446 2008-11-29 19:09:38Z benji_york $
+"""
