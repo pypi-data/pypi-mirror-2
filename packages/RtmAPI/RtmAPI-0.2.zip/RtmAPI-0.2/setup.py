800954d4e9fca259c8456d8bf855d58124c397f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:58 +0000

    Add package RtmAPI/0.2/RtmAPI-0.2.zip

---
+++ packages/RtmAPI/RtmAPI-0.2.zip/RtmAPI-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='RtmAPI',
+    author='Michael Gruenewald',
+    author_email='mail@michaelgruenewald.eu',
+    description='API package for rememberthemilk.com',
+    long_description=open('README.txt').read(),
+    license='License :: OSI Approved :: MIT License',
+    url='https://bitbucket.org/michaelgruenewald/rtmapi',
+    version='0.2',
+    packages=['rtmapi',],
+    requires=['httplib2 (>=0.6.0)'],
+)
