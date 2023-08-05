db70ab8bbcb04b38828839a46bc55e72eb80514b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package PyHBase/0.0.12/PyHBase-0.0.12.tar.gz

---
+++ packages/PyHBase/PyHBase-0.0.12.tar.gz/PyHBase-0.0.12/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name="PyHBase",
+      version='0.0.12',
+      description="High-level Python interface to HBase",
+      url="http://github.com/hammer/pyhbase/",
+      packages=['pyhbase'],
+      package_dir={'pyhbase': 'pyhbase'},
+      package_data={'pyhbase': ['schema/*.avpr']},
+      author="Jeff Hammerbacher",
+      author_email="hammer@cloudera.com",
+      keywords="database hbase avro",
+      scripts=['examples/pyhbase-cli'],
+      install_requires=['avro'])
