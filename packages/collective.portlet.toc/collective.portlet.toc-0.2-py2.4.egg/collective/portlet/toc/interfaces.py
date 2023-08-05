25706aa68a75f28ee273a86fc3ce1a6ffc544c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:44 +0000

    Add package collective.portlet.toc/0.2/collective.portlet.toc-0.2-py2.4.egg

---
+++ packages/collective.portlet.toc/collective.portlet.toc-0.2-py2.4.egg/collective/portlet/toc/interfaces.py
@@ -0,0 +1,6 @@
+from zope.viewlet.interfaces import IViewletManager
+
+class IFullViewManager(IViewletManager):
+    """Viewlet manager on top of the full view on the expanded view
+       used to show the table of contents
+    """
