f0dbaa9c9da0ad3574a483f17f39caded8ed613f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:43 +0000

    Add package pybdist/0.1.1/pybdist-0.1.1.zip

---
+++ packages/pybdist/pybdist-0.1.1.zip/pybdist-0.1.1/docs/RELEASE.rst
@@ -0,0 +1,6 @@
+Release Notes
+=============
+
+June 19th, 2010 v 0.1.1
+-----------------------
+* Initial release
---
+++ packages/pybdist/pybdist-0.1.1.zip/pybdist-0.1.1/src/pybdist/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#
+# Copyright 2010 Scott Kirkwood All Rights Reserved.
+
+__author__ = 'scott@forusers.com (Scott Kirkwood)'
+
+
