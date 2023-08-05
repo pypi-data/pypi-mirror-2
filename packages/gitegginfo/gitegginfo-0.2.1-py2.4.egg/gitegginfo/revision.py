cbd55d1a1a432d787f99c30ed34998dd28d48a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:05 +0000

    Add package gitegginfo/0.1/gitegginfo-0.2.1-py2.4.egg

---
+++ packages/gitegginfo/gitegginfo-0.2.1-py2.4.egg/gitegginfo/revision.py
@@ -0,0 +1,40 @@
+"""gitegginfo
+
+Create a distribution's .egg-info directory and contents, pulling version
+information from git or "git svn" if possible."""
+
+from subprocess import Popen, PIPE
+from setuptools.command.egg_info import get_pkg_info_revision
+
+
+def get_git_revision(self):
+    """Try to pull a suitable revision number from 'git-describe'."""
+    try:
+        p = Popen(['git-describe'], stdout=PIPE, stderr=PIPE)
+        p.stderr.close()
+        return p.stdout.readlines()[0]
+    except:
+        return None
+
+
+def get_gitsvn_info():
+    """Returns a dict containing the output from "git svn info"."""
+    try:
+        d = dict()
+        p = Popen(['git', 'svn', 'info'], stdout=PIPE, stderr=PIPE)
+        p.stderr.close()
+        for line in p.stdout.readlines():
+            try:
+                (name,value) = line.strip().split(":",1)
+                d[ name.strip() ] = value.strip()
+            except ValueError:
+                pass
+        return d
+    except:
+        return dict()
+
+def get_gitsvn_revision():
+    """Pull the revision number from 'git svn info'."""
+    rev = get_gitsvn_info().get("Revision",0)
+    return str(rev or get_pkg_info_revision())
+
