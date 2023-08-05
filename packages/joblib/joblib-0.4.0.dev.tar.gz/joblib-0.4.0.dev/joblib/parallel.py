2a90262316b80605fc22ad1af2ff3d4b5e279f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:02 +0000

    Add package joblib/0.4.0.dev/joblib-0.4.0.dev.tar.gz

---
+++ packages/joblib/joblib-0.4.0.dev.tar.gz/joblib-0.4.0.dev/joblib/parallel.py
@@ -0,0 +1,191 @@
+"""
+Helpers for embarassingly parallel code.
+"""
+# Author: Gael Varoquaux < gael dot varoquaux at normalesup dot org >
+# Copyright: 2010, Gael Varoquaux
+# License: BSD 3 clause
+
+import sys
+import functools
+import time
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+try:
+    import multiprocessing
+except ImportError:
+    multiprocessing = None
+
+from .format_stack import format_exc, format_outer_frames
+from .logger import Logger, short_format_time
+
+################################################################################
+
+class JoblibException(Exception):
+    """ A simple exception with an error message that you can get to.
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __reduce__(self):
+        # For pickling
+        return self.__class__, (self.message,), {}
+
+    def __repr__(self):
+        return '%s\n%s\n%s\n%s' % (
+                    self.__class__.__name__,
+                    75*'_',
+                    self.message,
+                    75*'_')
+
+    __str__ = __repr__
+
+
+class SafeFunction(object):
+    """ Wraps a function to make it exception with full traceback in
+        their representation.
+        Useful for parallel computing with multiprocessing, for which 
+        exceptions cannot be captured.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+
+    def __call__(self, *args, **kwargs):
+        try:
+            return self.func(*args, **kwargs)
+        except:
+            e_type, e_value, e_tb = sys.exc_info()
+            text = format_exc(e_type, e_value, e_tb, context=10,
+                             tb_offset=1)
+            raise JoblibException(text)
+
+def print_progress(self, index, total, start_time, n_jobs=1):
+    # XXX: Not using the logger framework: need to
+    # learn to use logger better.
+    if total > 2*n_jobs:
+        # Report less often
+        if not index % n_jobs == 0:
+            return
+    elapsed_time = time.time() - start_time
+    remaining_time = (elapsed_time/(index + 1)*
+                (total - index - 1.))
+    sys.stderr.write('[%s]: Done %3i out of %3i |elapsed: %s remaining: %s\n'
+            % (self,
+                index+1, 
+                total, 
+                short_format_time(elapsed_time),
+                short_format_time(remaining_time),
+                ))
+
+
+################################################################################
+def delayed(function):
+    """ Decorator used to capture the arguments of a function.
+    """
+    # Try to pickle the input function, to catch the problems early when
+    # using with multiprocessing
+    pickle.dumps(function)
+
+    @functools.wraps(function)
+    def delayed_function(*args, **kwargs):
+        return function, args, kwargs
+    return delayed_function
+
+
+
+class Parallel(Logger):
+    """ Helper class for readable parallel mapping.
+
+        Parameters
+        -----------
+        n_jobs: int
+            The number of jobs to use for the computation. If -1 all CPUs
+            are used.
+
+        Example
+        --------
+
+        >>> from math import sqrt
+        >>> Parallel(n_jobs=1)(delayed(sqrt)(i**2) for i in range(10))
+        [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+    """
+
+    def __init__(self, n_jobs=None, verbose=0):
+        self.verbose = verbose
+        self.n_jobs = n_jobs
+        # Not starting the pool in the __init__ is a design decision, to
+        # be able to close it ASAP, and not burden the user with closing
+        # it.
+
+
+    def __call__(self, iterable):
+        n_jobs = self.n_jobs
+        if n_jobs is None or multiprocessing is None or n_jobs == 1:
+            n_jobs = 1
+            from __builtin__ import apply
+        else:
+            if n_jobs == -1:
+                n_jobs = multiprocessing.cpu_count()
+            pool = multiprocessing.Pool(n_jobs)
+            apply = pool.apply_async
+
+        output = list()
+        start_time = time.time()
+        try:
+            for index, (function, args, kwargs) in enumerate(iterable):
+                if n_jobs > 1:
+                    function = SafeFunction(function)
+                output.append(apply(function, args, kwargs))
+                if self.verbose and n_jobs == 1:
+                    print '[%s]: Done job %3i | elapsed: %s' % (
+                            self, index, 
+                            short_format_time(time.time() - start_time)
+                        )
+            if n_jobs > 1:
+                start_time = time.time()
+                jobs = output
+                output = list()
+                for index, job in enumerate(jobs):
+                    try:
+                        output.append(job.get())
+                        if self.verbose:
+                            print_progress(self, index, len(jobs), start_time,
+                                           n_jobs=n_jobs)
+                    except JoblibException, exception:
+                        # Capture exception to add information on 
+                        # the local stack in addition to the distant
+                        # stack
+                        this_report = format_outer_frames(
+                                                context=10,
+                                                stack_start=1,
+                                                )
+                        report = """Multiprocessing exception:
+%s
+---------------------------------------------------------------------------
+Sub-process traceback: 
+---------------------------------------------------------------------------
+%s""" % (
+                                    this_report,
+                                    exception.message,
+                                )
+                        raise JoblibException(report)
+        finally:
+            if n_jobs > 1:
+                pool.close()
+                pool.join()
+        return output
+
+
+    def __repr__(self):
+        return '%s(n_jobs=%s)' % (
+                    self.__class__.__name__,
+                    self.n_jobs,
+                )
+
+
+
