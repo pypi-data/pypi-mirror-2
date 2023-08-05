7566969cb3fc629afa5183e19294c009773d219f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:00 +0000

    Add package squawk/0.2.3/squawk-0.2.3.tar.gz

---
+++ packages/squawk/squawk-0.2.3.tar.gz/squawk-0.2.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from squawk import __version__ as version
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name = 'squawk',
+    version = version,
+    description = 'SQL query tool and library for static files',
+    long_description = long_description,
+    author = 'Samuel Stauffer',
+    author_email = 'samuel@descolada.com',
+    url = 'http://github.com/samuel/squawk',
+    packages = ['squawk', 'squawk/parsers'],
+    license = "BSD",
+    scripts = ['bin/squawk'],
+    requires = ["pyparsing"],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
