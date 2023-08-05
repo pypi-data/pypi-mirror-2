38271c542ef9aab1ca8a8dd92e0a5be0375cfbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:54 +0000

    Add package apps-sdk/0.2.2/apps_sdk-0.2.2-py2.6.egg

---
+++ packages/apps-sdk/apps_sdk-0.2.2-py2.6.egg/griffin/project.py
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2010 BitTorrent Inc.
+#
+
+"""
+A griffin project.
+"""
+
+import json
+import os
+
+class Project(object):
+
+    def __init__(self, path):
+        self.path = path
+        self.read_metadata()
+
+    def read_metadata(self):
+        try:
+            self.metadata = json.load(open(os.path.join(self.path,
+                                                        'package.json'), 'r'))
+        except IOError, err:
+            self.metadata = {
+                'name': self.path,
+                'version': '0.1',
+                'description': 'The default project.',
+                'site': 'http://griffin.bittorrent.com',
+                'author': 'Default Author <default@example.com>',
+                'keywords': [ 'default' ],
+                'bt:publisher': 'Default Publisher',
+                'bt:update_url': "http://localhost/default",
+                'bt:release_date': '00/00/0000',
+                'bt:description': 'This is the default app.',
+                'bt:libs': [
+                    { 'name': 'griffin',
+                      'url': 'http://staging.apps.bittorrent.com/pkgs/griffin.pkg' }
+                    ]
+                }
+
+
