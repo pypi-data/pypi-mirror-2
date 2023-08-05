b398cf36427587576a09f3ababa24a048649acba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:18 +0000

    Add package html2data/0.1/html2data-0.1.tar.gz

---
+++ packages/html2data/html2data-0.1.tar.gz/html2data-0.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "html2data",
+    version = "0.1",
+    author = "Daniel Perez Rada",
+    author_email = "daniel@zappedy.com",
+    description = ("A simple way to transform a HTML file or URL to structured data."),
+    license = "BSD",
+    keywords = "html2data html data xpath crawler transform",
+    url = "http://packages.python.org/html2data",
+    packages=['html2data', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
---
+++ packages/html2data/html2data-0.1.tar.gz/html2data-0.1/tests/test.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+import os
+from unittest import TestCase
+from html2data import html2data
+from lxml import etree
+
+class TestHtml2Data(TestCase):
+    def setUp(self):
+        self.handler = html2data()
+    
+    def test_load_html2xml(self):
+        html = """<!DOCTYPE html><html lang="en"><head>
+                <meta charset="utf-8" />
+                <title>Example Page</title>
+                <link rel="stylesheet" href="css/main.css" type="text/css" />
+            </head>
+            <body>
+                <h1><b>Title</b></h1>
+                <div class="description">This is not a valid HTML
+            </body>
+        </html>
+        """
+        
+        config = {
+            'map': [
+                ['header_title', u'//head/title/text()'],
+                ['body_title', u'//h1/b/text()'],
+                ['description', u'//div[@class="description"]/text()'],
+            ]
+        }
+
+        expected_obj = {
+            "header_title": "Example Page",
+            "body_title": "Title",
+            "description": "This is not a valid HTML"
+        }
+        
+        received_obj = self.handler.load(html = html, config=config)
+        self.assertEqual(expected_obj, received_obj)
