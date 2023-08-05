6765791b306f3170c198d7e80439236eef983da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package Pythomnic3k/1.1/pythomnic3k-postgresql-1.1.tar.gz

---
+++ packages/Pythomnic3k/pythomnic3k-postgresql-1.1.tar.gz/pythomnic3k/cages/.shared/config_resource_postgresql_1.py
@@ -0,0 +1,37 @@
+# configuration file for resource "postgresql_1"
+#
+# this file exists as a reference for configuring postgresql resources
+# and to support self-test run of module protocol_postgresql_pg8000.py
+#
+# copy this file to your own cage, possibly renaming into
+# config_resource_YOUR_RESOURCE_NAME.py, then modify the copy
+
+config = dict \
+(
+protocol = "postgresql_pg8000",             # meta
+decimal_precision = (10, 2),                # sql
+server_address = ("db.domain.com", 5432),   # postgresql
+connect_timeout = 3.0,                      # postgresql
+database = "database",                      # postgresql
+username = "user",                          # postgresql
+password = "pass",                          # postgresql
+)
+
+# self-tests of protocol_postgresql.py depend on the following configuration
+
+self_test_config = dict \
+(
+server_address = ("test.postgres", 5432),
+database = "test_db",
+username = "user",
+password = "pass",
+)
+
+# DO NOT TOUCH BELOW THIS LINE
+
+__all__ = [ "get", "copy" ]
+
+get = lambda key, default = None: pmnc.config.get_(config, self_test_config, key, default)
+copy = lambda: pmnc.config.copy_(config, self_test_config)
+
+# EOF
