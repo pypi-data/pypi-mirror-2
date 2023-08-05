b3e563c1a2ee8597c7af0e69c027d0dbf89d93a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package alacarte/0.6.2/alacarte-0.6.2-py2.7.egg

---
+++ packages/alacarte/alacarte-0.6.2-py2.7.egg/alacarte/core.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+from alacarte.engines import Engines
+from alacarte.resolver import Resolver
+from alacarte.template.engine import Engine
+from alacarte.util import Cache
+
+
+__all__ = ['Engine', 'Engines', 'Resolver', 'Cache']
