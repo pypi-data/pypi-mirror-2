8b9ed431e992d6521a4b3119cd4efb0e049383c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:08 +0000

    Add package django-native-tags/0.4.1/django-native-tags-0.4.1.tar.gz

---
+++ packages/django-native-tags/django-native-tags-0.4.1.tar.gz/django-native-tags-0.4.1/native_tags/contrib/hash.py
@@ -0,0 +1,38 @@
+from django.utils.hashcompat import md5_constructor, sha_constructor
+from native_tags.decorators import function, filter
+
+def hexd(algo, value):
+    lookup = {
+        'md5': md5_constructor,
+        'sha1': sha_constructor,
+        'sha': sha_constructor,
+    }
+    try:
+        import hashlib
+        lookup.update({
+            'sha224': hashlib.sha224,
+            'sha256': hashlib.sha256,
+            'sha384': hashlib.sha384,
+            'sha512': hashlib.sha512,
+        })
+    except ImportError:
+        pass
+    try:
+        return lookup[algo](value).hexdigest()
+    except IndexError:
+        return ''
+
+def hashtag(algo, lib=False):
+    def inner(value):
+        return hexd(algo, value)
+    return filter(function(inner),
+        doc='Returns %s hexadecimal hash of the value. %s' % \
+            (algo.upper(), lib and 'Requires the :mod:`hashlib` module' or ''),
+        name=algo, test={'args':('wtf',),'result':hexd(algo, 'wtf')})
+    
+md5 = hashtag('md5')
+sha1 = hashtag('sha1')
+sha224 = hashtag('sha224',True)
+sha256 = hashtag('sha256',True)
+sha384 = hashtag('sha384',True)
+sha512 = hashtag('sha512',True)
