871bb6ef1bc7b76cd88060a927fe85dd8cdfcf49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package Products.TinyMCE/1.1rc11/Products.TinyMCE-1.1rc11.zip

---
+++ packages/Products.TinyMCE/Products.TinyMCE-1.1rc11.zip/Products.TinyMCE-1.1rc11/Products/TinyMCE/browser/browser.py
@@ -0,0 +1,87 @@
+from zope.interface import implements
+from zope.component import getUtility
+from Products.Five.browser import BrowserView
+from Acquisition import aq_inner
+
+from Products.TinyMCE.adapters.interfaces.JSONFolderListing import IJSONFolderListing
+from Products.TinyMCE.adapters.interfaces.JSONSearch import IJSONSearch
+from Products.TinyMCE.adapters.interfaces.JSONDetails import IJSONDetails
+from Products.TinyMCE.adapters.interfaces.Upload import IUpload
+from Products.TinyMCE.adapters.interfaces.Save import ISave
+from Products.TinyMCE.browser.interfaces.browser import ITinyMCEBrowserView
+from Products.TinyMCE.interfaces.utility import ITinyMCE
+
+class TinyMCEBrowserView(BrowserView):
+    """TinyMCE Browser View"""
+    implements(ITinyMCEBrowserView)
+
+    def upload(self):
+        """Upload a file to the zodb"""
+
+        context = aq_inner(self.context)
+        object = IUpload(self.context)
+        return object.upload()
+
+    def save(self, text, fieldname):
+        """Saves the specified richedit field"""
+
+        context = aq_inner(self.context)
+        object = ISave(self.context)
+        return object.save(text, fieldname)
+
+    def jsonLinkableFolderListing(self, rooted, document_base_url):
+        """Returns the folderlisting of linkable objects in JSON"""
+
+        utility = getUtility(ITinyMCE)
+        linkable_portal_types = utility.linkable.split('\n')
+
+        context = aq_inner(self.context)
+        object = IJSONFolderListing(self.context)
+        results = object.getListing(linkable_portal_types, rooted, document_base_url, 'File') 
+        return results
+
+    def jsonImageFolderListing(self, rooted, document_base_url):
+        """Returns the folderlisting of image objects in JSON"""
+
+        utility = getUtility(ITinyMCE)
+        image_portal_types = utility.imageobjects.split('\n')
+        image_portal_types.extend(utility.containsobjects.split('\n'))
+
+        context = aq_inner(self.context)
+        object = IJSONFolderListing(self.context)
+        results = object.getListing(image_portal_types, rooted, document_base_url, 'Image')
+        return results
+
+    def jsonLinkableSearch(self, searchtext):
+        """Returns the search results of linkable objects in JSON"""
+
+        utility = getUtility(ITinyMCE)
+        linkable_portal_types = utility.linkable.split('\n')
+        linkable_portal_types.extend(utility.containsobjects.split('\n'))
+
+        context = aq_inner(self.context)
+        object = IJSONSearch(self.context)
+        results = object.getSearchResults(linkable_portal_types, searchtext)
+        return results
+
+    def jsonImageSearch(self, searchtext):
+        """Returns the search results of image objects in JSON"""
+
+        utility = getUtility(ITinyMCE)
+        image_portal_types = utility.imageobjects.split('\n')
+        image_portal_types.extend(utility.containsobjects.split('\n'))
+
+        context = aq_inner(self.context)
+        object = IJSONSearch(self.context)
+        results = object.getSearchResults(image_portal_types, searchtext)
+        return results
+
+    def jsonDetails(self):
+        """Returns the details of an object in JSON"""
+
+        context = aq_inner(self.context)
+        object = IJSONDetails(self.context)
+        return object.getDetails()
+    
+    
+        
---
+++ packages/Products.TinyMCE/Products.TinyMCE-1.1rc11.zip/Products.TinyMCE-1.1rc11/Products/TinyMCE/utils/compile-mo.py
@@ -0,0 +1,19 @@
+#!/opt/local/bin/python2.4
+
+import os
+
+try:
+    from xml.etree import ElementTree as ET
+except ImportError:
+    from elementtree import ElementTree as ET
+
+# Skip .ko for now
+AVAILABLE_LANGUAGES = set(
+    'ar bs ch da el es fa fr he hu ii it lv ms nl pl ro sc si sl sr '
+    'tr tw vi bg ca cs de en et fi gl hr ia is ja lt mk nb nn pt ru se '
+    'sk sq sv tt uk'.split())
+
+for x in AVAILABLE_LANGUAGES:
+
+    cmd = "msgfmt -o ../locales/%s/LC_MESSAGES/tinymce.mo ../locales/%s/LC_MESSAGES/tinymce.po" % (x, x)
+    os.system(cmd)
