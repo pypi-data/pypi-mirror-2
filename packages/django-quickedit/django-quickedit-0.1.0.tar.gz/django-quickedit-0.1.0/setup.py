f517cdbf4710a91825505a75f5a5c2e133d45464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package django-quickedit/0.1.0/django-quickedit-0.1.0.tar.gz

---
+++ packages/django-quickedit/django-quickedit-0.1.0.tar.gz/django-quickedit-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import quickedit
+
+
+setup(name='django-quickedit',
+      version=quickedit.__version__,
+      description='Quick editing of fields in the django admin',
+      author='Justin Quick',
+      author_email='justquick@gmail.com',
+      url='http://github.com/washingtontimes/django-quickedit',
+      packages=find_packages(),
+      #package_data={'quickedit':['quickedit/templates/quickedit/*.html']},
+      include_package_data=True,
+      classifiers=['Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'License :: OSI Approved :: Apache Software License',
+          ],
+      )
