4c9d7a52848e23a32d02a5920dbd6a701683c4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package acted.projects/0.1dev/acted.projects-0.1dev.tar.gz

---
+++ packages/acted.projects/acted.projects-0.1dev.tar.gz/acted.projects-0.1dev/acted/projects/config.py
@@ -0,0 +1,71 @@
+"""Common configuration constants
+"""
+from Products.Archetypes.public import DisplayList
+
+###########################################################################
+## ACTED only change what is between the ######## lines !!!!!
+## ivan.price@acted.org   14/6/2010
+## 
+## After changing this file you need to restart plone !
+
+theDonors = (
+'US AID',
+'ECHO',
+'GTZ'
+)
+
+
+theSectors = (
+'NFI',
+'Shelter',
+'WASH'
+)
+
+
+theCountries = (
+'Afghanistan',
+'Central African Republic',
+'Chad',
+'Democratic Republic of Congo',
+'France',
+'Haiti',
+'India',
+'Indonesia',
+'Iraq',
+'Jordan',
+'Kenya',
+'Kyrgyzstan',
+'Lebanon',
+'Myanmar',
+'Nicaragua',
+'Occupied Palestinian Territories',
+'Pakistan',
+'Republic of Congo',
+'Somalia',
+'Sri Lanka',
+'Sudan',
+'Tajikistan',
+'Uganda'
+)
+
+
+
+############################################################################
+
+ACTED_DONORS = DisplayList()
+for donor in theDonors:
+   ACTED_DONORS.add(donor,donor)
+
+ACTED_COUNTRIES = DisplayList()
+for country in theCountries:
+   ACTED_COUNTRIES.add(country,country)
+
+ACTED_SECTORS = DisplayList()
+for sector in theSectors:
+   ACTED_SECTORS.add(sector,sector)
+
+
+PROJECTNAME = 'acted.projects'
+ADD_PERMISSIONS = {
+    'ACTEDProject': 'acted.projects: Add ACTED Project',
+}
