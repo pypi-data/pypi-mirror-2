10db5d6fa75dbd8d380e51841b4ce4955b3baf9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package monocle/0.1/monocle-0.1.tar.gz

---
+++ packages/monocle/monocle-0.1.tar.gz/monocle-0.1/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from monocle import VERSION
+
+setup(name="monocle",
+      version=VERSION,
+      description="An async programming framework with a blocking look-alike syntax",
+      author="Greg Hazel and Steven Hazel",
+      author_email="sah@awesame.org",
+      maintainer="Steven Hazel",
+      maintainer_email="sah@awesame.org",
+      url="http://github.com/saucelabs/monocle",
+      packages=['monocle', 'monocle.stack', 'monocle.twisted_stack',
+                'monocle.tornado_stack'],
+      license='MIT'
+      )
