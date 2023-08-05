db9b6c4aeee7782a11be8046b3cc134d30b5ef05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:51 +0000

    Add package transmogrify.htmltesting/0.2dev/transmogrify.htmltesting-0.2dev.tar.gz

---
+++ packages/transmogrify.htmltesting/transmogrify.htmltesting-0.2dev.tar.gz/transmogrify.htmltesting-0.2dev/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='transmogrify.htmltesting',
+      version=version,
+      description="utils and transmogrifier blueprints for testing",
+#      long_description=open( #os.path.join("transmogrify", "htmltesting", "webcrawler.txt")).read() + "\n" +
+#                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='transmogrifier blueprint funnelweb source plone import conversion microsoft office',
+      author='Dylan Jay',
+      author_email='software@pretaweb.com',
+      url='http://www.pretaweb.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['transmogrify'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'lxml',
+          'BeautifulSoup',
+          'collective.transmogrifier',
+          'z3c.autoinclude'
+          ],
+      entry_points="""
+            [z3c.autoinclude.plugin]
+            target = transmogrify
+            """,
+            )
