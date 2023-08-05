c03bf831c771752846e58948c7575ae764f993c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:59 +0000

    Add package edpath/0.1.2/edpath-0.1.2.zip

---
+++ packages/edpath/edpath-0.1.2.zip/edpath-0.1.2/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from os.path import dirname, join
+from distutils.core import setup
+
+from edpath import VERSION
+
+
+NAME = 'edpath'
+
+
+def read_file(filename):
+    filename = join(dirname(__file__), filename)
+    with open(filename) as fp:
+        # return all lines apart from the first
+        # it is the same as the 'description'
+        lines = fp.readlines()
+    return lines[0], ''.join(lines[1:])
+
+
+description, long_description = read_file('README.txt')
+setup(
+    name=NAME,
+    version=VERSION,
+    description=description,
+    long_description=long_description,
+    keywords='terminal command-line windows path environment',
+    author='Jonathan Hartley',
+    author_email='tartley@tartley.com',
+    url='http://code.google.com/p/edpath/',
+    license='BSD',
+    packages=[NAME],
+    scripts=['edpath-script.py', 'edpath.bat'],
+    #data_files=[('package', ['files'])],
+    # see classifiers http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Terminals',
+    ]
+)
+
