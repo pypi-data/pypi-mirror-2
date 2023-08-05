5bd425eb5fbd6dbd08b28df8e581a129aa5cf963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package zettwerk.clickmap/0.1/zettwerk.clickmap-0.1.tar.gz

---
+++ packages/zettwerk.clickmap/zettwerk.clickmap-0.1.tar.gz/zettwerk.clickmap-0.1/zettwerk/clickmap/__init__.py
@@ -0,0 +1,15 @@
+from zope.i18nmessageid import MessageFactory
+clickmapMessageFactory = MessageFactory('zettwerk.clickmap')
+
+from zettwerk.clickmap import config
+import ClickmapTool
+
+from Products.CMFCore import utils
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
+
+    utils.ToolInit('Zettwerk Clickmap', tools=(ClickmapTool.ClickmapTool,),
+                   icon='z.png'
+                   ).initialize(context)
