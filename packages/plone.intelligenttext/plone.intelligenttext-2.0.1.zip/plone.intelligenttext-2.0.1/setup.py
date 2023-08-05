d1891995235243c0039840d4dfd1538c35e85373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:36 +0000

    Add package plone.intelligenttext/2.0.1/plone.intelligenttext-2.0.1.zip

---
+++ packages/plone.intelligenttext/plone.intelligenttext-2.0.1.zip/plone.intelligenttext-2.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '2.0.1'
+
+setup(name='plone.intelligenttext',
+      version=version,
+      description="Provides transforms from text/x-web-intelligent to "
+                  "text/html and vice versa.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.intelligenttext',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      )
