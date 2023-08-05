abf93c943edd21eb3ba2e6692c9f6cf5650a4d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package dingo/0.2.1/dingo-0.2.1.tar.gz

---
+++ packages/dingo/dingo-0.2.1.tar.gz/dingo-0.2.1/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = "dingo",
+    version = "0.2.1",
+    url = 'http://gitorious.org/dingo',
+
+    long_description=(
+         file('README.txt','r').read()
+         + '\n'),
+
+    packages = ['dingo',],
+    package_dir = {'':'src'},
+    
+    install_requires = ['django'],
+
+    include_package_data = True,
+    zip_safe = True,
+
+    author = 'Nathan R. Yergler',
+    author_email = 'nathan@yergler.net',
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        ],
+
+    )
