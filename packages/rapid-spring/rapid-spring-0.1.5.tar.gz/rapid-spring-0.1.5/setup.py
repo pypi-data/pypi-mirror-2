f75e04a82902da3233ccf42a6a0d2ccc43e677f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:57 +0000

    Add package rapid-spring/0.1.5/rapid-spring-0.1.5.tar.gz

---
+++ packages/rapid-spring/rapid-spring-0.1.5.tar.gz/rapid-spring-0.1.5/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='rapid-spring',
+    version='0.1.5',
+    author='Tobi Vollebregt',
+    author_email='tobivollebregt@gmail.com',
+    packages=['rapid'],
+    scripts=['bin/rapid','bin/rapid-gui'],
+    url='http://pypi.python.org/pypi/rapid-spring/',
+    license='LICENSE.txt',
+    description='spring content downloading',
+    long_description=open('README.txt').read(),
+    requires=['bitarray'],
+)
