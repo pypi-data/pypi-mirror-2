b56b1256e0344b2f36dbb62a7ca3161bbfcc0592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package CoilMQ/0.3.1/CoilMQ-0.3.1.tar.gz

---
+++ packages/CoilMQ/CoilMQ-0.3.1.tar.gz/CoilMQ-0.3.1/coilmq/store/__init__.py
@@ -0,0 +1,143 @@
+"""
+Storage containers for durable queues and (planned) durable topics.
+"""
+import logging
+import threading
+
+from coilmq.util.concurrency import synchronized
+
+__authors__ = ['"Hans Lellelid" <hans@xmpl.org>']
+__copyright__ = "Copyright 2009 Hans Lellelid"
+__license__ = """Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License."""
+
+class QueueStore(object):
+    """
+    Abstract base class for queue storage. 
+    
+    Extensions/implementations of this class must be thread-safe.
+    
+    @ivar log: A logger for this class.
+    @type log: C{logging.Logger}
+    """
+    
+    def __init__(self):
+        """
+        A base constructor that sets up logging and the lock used by synchronized decorator.
+        
+        If you extend this class, you should either call this method or at minimum make sure these values
+        get set.
+        """
+        self.log = logging.getLogger('%s.%s' % (self.__module__, self.__class__.__name__))
+        self._lock = threading.RLock()
+    
+    @synchronized
+    def enqueue(self, destination, frame):
+        raise NotImplementedError()
+    
+    @synchronized
+    def dequeue(self, destination):
+        raise NotImplementedError()
+    
+    @synchronized
+    def requeue(self, destination, frame):
+        self.enqueue(destination, frame)
+
+    @synchronized
+    def size(self, destination):
+        """
+        Size of the queue for specified destination.
+        
+        @param destination: The queue destination (e.g. /queue/foo)
+        @type destination: C{str}
+        
+        @return: The number of frames in specified queue.
+        @rtype: C{int}
+        """
+        raise NotImplementedError()
+
+    @synchronized
+    def has_frames(self, destination):
+        """
+        Whether specified destination has any frames.
+        
+        Default implementation uses L{QueueStore.size} to determine if there
+        are any frames in queue.  Subclasses may choose to optimize this.
+        
+        @param destination: The queue destination (e.g. /queue/foo)
+        @type destination: C{str}
+        
+        @return: The number of frames in specified queue.
+        @rtype: C{int}
+        """
+        return self.size(destination) > 0
+    
+    @synchronized
+    def close(self):
+        """
+        May be implemented to perform any necessary cleanup operations when store is closed.
+        """
+        pass
+    
+    # This is intentionally not synchronized, since it does not directly
+    # expose any shared data.
+    def frames(self, destination):
+        """
+        Returns an iterator for frames in specified queue.
+        
+        The iterator simply wraps calls to L{dequeue} method, so the order of the 
+        frames from the iterator will be the reverse of the order in which the
+        frames were enqueued.
+        
+        @param destination: The queue destination (e.g. /queue/foo)
+        @type destination: C{str}
+        """
+        return QueueFrameIterator(self, destination)
+    
+class QueueFrameIterator(object):
+    """
+    Provides an C{iterable} over the frames for a specified destination in a queue.
+    
+    @ivar store: The queue store.
+    @type store: L{coilmq.store.QueueStore}
+    
+    @ivar destination: The destination for this iterator.
+    @type destination: C{str}
+    """
+    def __init__(self, store, destination):
+        self.store = store
+        self.destination = destination
+        
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        frame = self.store.dequeue(self.destination)
+        if not frame:
+            raise StopIteration()
+        return frame
+    
+    def __len__(self):
+        return self.store.size(self.destination)
+    
+    def __nonzero__(self):
+        return self.store.has_frames(self.destination)
+    
+class TopicStore(object):
+    """
+    Abstract base class for non-durable topic storage.
+    """
+    
+class DurableTopicStore(TopicStore):
+    """
+    Abstract base class for durable topic storage.
+    """
---
+++ packages/CoilMQ/CoilMQ-0.3.1.tar.gz/CoilMQ-0.3.1/setup.py
@@ -0,0 +1,60 @@
+'''
+CoilMQ is a STOMP message broker written in Python.
+
+The provided server implementation for CoilMQ uses the Python SocketServer libraries;
+however, CoilMQ is only loosely coupled to this server implementation.  It could be used
+with other socket implementations.
+
+Two things worth noting:
+
+  1. The CoilMQ core classes and bundled storage implementations are designed to be thread-safe.
+
+  2. While this project could be used with asynchronous frameworks like Twisted, it is not 
+     explicitly designed to use the async patterns (e.g. using Twisted Deffered), which may 
+     make it impractical to use this with slow/blocking storage mechanisms (e.g. using db 
+     storage will cause all clients to block).
+'''
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='CoilMQ',
+    version='0.3',
+    description='STOMP message broker',
+    long_description=__doc__,
+    keywords='stomp server broker',
+    license='Apache',
+    author='Hans Lellelid',
+    author_email='hans@xmpl.org',
+    url='http://code.google.com/p/coilmq',
+    packages=find_packages(exclude=['ez_setup', 'distribute_setup', 'tests', 'tests.*']),
+    package_data={'coilmq': ['config/*.cfg*', 'tests/resources/*']},
+    zip_safe=False, # We use resource_filename for logging configuration and some unit tests.
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['nose', 'coverage'],
+    install_requires=[
+          'distribute',
+          'stomper==0.2.2',
+    ],
+    extras_require={
+        'daemon': ['python-daemon'],
+        'sqlalchemy': ['SQLAlchemy']
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    entry_points="""
+    [console_scripts]
+    coilmq = coilmq.start:main
+    """,
+)
