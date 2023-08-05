c97b5f02b1501b2701c3f574dc01dbfdf4973a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package z3c.language.negotiator/1.0.3/z3c.language.negotiator-1.0.3.tar.gz

---
+++ packages/z3c.language.negotiator/z3c.language.negotiator-1.0.3.tar.gz/z3c.language.negotiator-1.0.3/src/z3c/language/negotiator/app.py
@@ -0,0 +1,139 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+"""
+$Id: app.py 115479 2010-08-05 08:19:23Z adamg $
+"""
+__docformat__ = 'restructuredtext'
+
+import persistent
+import zope.interface
+
+from zope.schema.fieldproperty import FieldProperty
+from zope.i18n.interfaces import IUserPreferredLanguages
+from zope.i18n.interfaces import INegotiator
+from zope.i18n.negotiator import negotiator
+
+from zope.app.container import contained
+
+from z3c.language.session.interfaces import ILanguageSession
+
+from z3c.language.negotiator.interfaces import INegotiatorManager
+from z3c.language.negotiator.interfaces import language_policies
+from z3c.language.negotiator.interfaces import LANGUAGE_CACHE_KEY
+
+
+
+class Negotiator(persistent.Persistent, contained.Contained):
+    """Loacal negotiator implementation.
+
+    The negotiator let you change the policy, which is a alias
+    for the lookup mechanism.
+
+    'server' -- The server defines the language
+
+    'session' -- The session defines the language (usefull for testing)
+
+    'browser' -- The browser defines the language (usefull for testing)
+
+    'browser --> session --> server' -- Left criteria first
+
+    'browser --> server' -- Left criteria first
+
+    'session --> browser --> server' -- Left criteria first (default use case)
+
+    'session --> server' -- Left criteria first
+
+    The 'server' criteria is used as a fallback criteria in most use cases.
+
+    """
+
+    zope.interface.implements(INegotiator, INegotiatorManager)
+
+    serverLanguage = FieldProperty(INegotiatorManager['serverLanguage'])
+    offeredLanguages = FieldProperty(INegotiatorManager['offeredLanguages'])
+    cacheEnabled = FieldProperty(INegotiatorManager['cacheEnabled'])
+
+    def __init__(self):
+        self._policy = 'session --> browser --> server'
+
+    @apply
+    def policy():
+        def get(self):
+            """Returns the language policy."""
+            return self._policy
+
+        def set(self, policy):
+            """Set the language policy."""
+            if policy not in language_policies:
+                policies = str(language_policies)
+                raise ValueError('Not a valid policy name.', policy)
+            self._policy = policy
+        return property(get, set)
+
+    def _getLanguage(self, languages, request):
+        """Returns the language dependent on the policy."""
+        policyList = self._policy.split(' --> ')
+
+        for policy in policyList:
+
+            # the server is handling the language
+            if policy == 'server':
+                if self.serverLanguage:
+                    return self.serverLanguage
+
+            # the language is handled by a session
+            elif policy == 'session':
+                session = ILanguageSession(request)
+                lang = session.getLanguage()
+                if lang is not None:
+                    return lang
+
+            # the language is handled by the browsers language settings
+            elif policy == 'browser':
+                lang = negotiator.getLanguage(languages, request)
+                if lang is not None:
+                    return lang
+
+        return None
+
+    def getLanguage(self, languages, request):
+        if self.cacheEnabled:
+            try:
+                cached = request.annotations[LANGUAGE_CACHE_KEY]
+                if cached in languages:
+                    return cached
+
+                # If the user asked for a specific variation, but we don't
+                # have it available we may serve the most generic one,
+                # according to the spec (eg: user asks for ('en-us',
+                # 'de'), but we don't have 'en-us', then 'en' is preferred
+                # to 'de').
+                parts = cached.split('-')
+                if len(parts) > 1 and parts[0] in languages:
+                    return parts[0]
+
+                #if there's still no match, we have to run through _getLanguage
+            except KeyError:
+                lang = self._getLanguage(languages, request)
+                request.annotations[LANGUAGE_CACHE_KEY] = lang
+                return lang
+
+        return self._getLanguage(languages, request)
+
+    def clearCache(self, request):
+        try:
+            del request.annotations[LANGUAGE_CACHE_KEY]
+        except KeyError:
+            pass
---
+++ packages/z3c.language.negotiator/z3c.language.negotiator-1.0.3.tar.gz/z3c.language.negotiator-1.0.3/src/z3c/language/negotiator/tests.py
@@ -0,0 +1,167 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+"""
+$Id: tests.py 115479 2010-08-05 08:19:23Z adamg $
+"""
+__docformat__ = 'restructuredtext'
+
+import unittest
+
+import zope.component
+from zope.testing import doctest
+from zope.testing.doctestunit import DocFileSuite
+
+from z3c.language.negotiator import app
+from z3c.language.negotiator import testing
+
+
+class NegotiatorBaseTest(testing.BaseTestINegotiator):
+
+    def getTestClass(self):
+        return app.Negotiator
+
+
+class NegotiatorTest(zope.component.testing.PlacelessSetup,
+    unittest.TestCase):
+
+    def setUp(self):
+        super(NegotiatorTest, self).setUp()
+        self.negotiator = app.Negotiator()
+        zope.component.provideAdapter(testing.LanguageSessionStub)
+
+    def test_policy(self):
+        default = 'session --> browser --> server'
+        self.assertEqual(self.negotiator.policy, default)
+        self.negotiator.policy = 'server'
+        self.assertEqual(self.negotiator.policy, 'server')
+
+    def test_serverLanguage(self):
+        self.assertEqual(self.negotiator.serverLanguage, u'en')
+        self.negotiator.serverLanguage = u'de'
+        self.assertEqual(self.negotiator.serverLanguage, u'de')
+
+    def test_offeredLanguages(self):
+        self.assertEqual(self.negotiator.offeredLanguages, [])
+        self.negotiator.offeredLanguages = [u'de', u'en']
+        self.assertEqual(self.negotiator.offeredLanguages, [u'de', u'en'])
+
+    def test_getLanguagesBrowser(self):
+        # first set the default policy to 'browser'
+        self.negotiator.policy = 'browser'
+        self.assertEqual(self.negotiator.policy, 'browser')
+
+        _cases = (
+            (('en','de'), ('en','de','fr'),  'en'),
+            (('en'),      ('it','de','fr'),  None),
+            (('pt-br','de'), ('pt_BR','de','fr'),  'pt_BR'),
+            (('pt-br','en'), ('pt', 'en', 'fr'),  'pt'),
+            (('pt-br','en-us', 'de'), ('de', 'en', 'fr'),  'en'),
+            )
+
+        for user_pref_langs, obj_langs, expected in _cases:
+            env = testing.EnvStub(user_pref_langs)
+            self.assertEqual(self.negotiator.getLanguage(obj_langs, env),
+                             expected)
+
+    def test_getLanguagesServer(self):
+        self.negotiator.policy = 'server'
+        self.assertEqual(self.negotiator.policy, 'server')
+
+        self.negotiator.serverLanguage = u'de'
+        self.assertEqual(self.negotiator.serverLanguage, u'de')
+
+        _cases = (
+            (('en','de'), ('en','de','fr'),  'de'),
+            (('en'),      ('it','de','fr'),  'de'),
+            (('pt-br','de'), ('pt_BR','de','fr'),  'de'),
+            (('pt-br','en'), ('pt', 'en', 'fr'),  'de'),
+            (('pt-br','en-us', 'de'), ('de', 'en', 'fr'),  'de'),
+            )
+
+        for user_pref_langs, obj_langs, expected in _cases:
+            env = testing.EnvStub(user_pref_langs)
+            self.assertEqual(self.negotiator.getLanguage(obj_langs, env),
+                             expected)
+
+    def test_getLanguagesSession(self):
+        self.negotiator.policy = 'session'
+        self.assertEqual(self.negotiator.policy, 'session')
+
+        _cases = (
+            (('en','de'), ('en','de','fr'),  'fr'),
+            (('en'),      ('it','de','fr'),  'fr'),
+            (('pt-br','de'), ('pt_BR','de','fr'),  'fr'),
+            (('pt-br','en'), ('pt', 'en', 'fr'),  'fr'),
+            (('pt-br','en-us', 'de'), ('de', 'en', 'fr'),  'fr'),
+            )
+
+        for user_pref_langs, obj_langs, expected in _cases:
+            env = testing.EnvStub(user_pref_langs)
+            self.assertEqual(self.negotiator.getLanguage(obj_langs, env),
+                             expected)
+
+    def test_getLanguagesCached(self):
+        self.negotiator.cacheEnabled = True
+
+        self.negotiator.policy = 'server'
+        self.assertEqual(self.negotiator.policy, 'server')
+
+        self.negotiator.serverLanguage = u'de'
+
+        env = testing.EnvStub(('pt-br','en'))
+        self.assertEqual(self.negotiator.getLanguage(('en', 'de'), env), 'de')
+
+        self.negotiator.serverLanguage = u'en'
+
+        self.assertEqual(self.negotiator.getLanguage(('en', 'de'), env), 'de')
+
+        env = testing.EnvStub(('pt-br','en'))
+
+        self.assertEqual(self.negotiator.getLanguage(('en', 'de'), env), 'en')
+
+        self.negotiator.serverLanguage = u'de'
+
+        self.assertEqual(self.negotiator.getLanguage(('en', 'de'), env), 'en')
+
+        self.negotiator.clearCache(env)
+
+        self.assertEqual(self.negotiator.getLanguage(('en', 'de'), env), 'de')
+
+        self.negotiator.clearCache(env)
+        self.negotiator.clearCache(env)
+
+        #edge case, cache has a more specific language than available
+        self.negotiator.policy = 'browser'
+        env = testing.EnvStub(('de-de','de'))
+
+        self.assertEqual(self.negotiator.getLanguage(('de-de', 'de'), env), 'de-de')
+
+        self.assertEqual(self.negotiator.getLanguage(('de', 'en'), env), 'de')
+
+        self.negotiator.clearCache(env)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(NegotiatorBaseTest),
+        unittest.makeSuite(NegotiatorTest),
+        DocFileSuite('README.txt',
+            setUp=testing.doctestSetUp,
+            tearDown=testing.doctestTearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS),
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
