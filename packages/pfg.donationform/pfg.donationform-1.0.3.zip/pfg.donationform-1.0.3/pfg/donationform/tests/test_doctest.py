661ef09f1a035150d79eee65c8b0ddb22658cce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package pfg.donationform/1.0.3/pfg.donationform-1.0.3.zip

---
+++ packages/pfg.donationform/pfg.donationform-1.0.3.zip/pfg.donationform-1.0.3/pfg/donationform/tests/test_doctest.py
@@ -0,0 +1,27 @@
+import unittest
+import doctest
+
+#from zope.testing import doctestunit
+#from zope.component import testing, eventtesting
+
+from Testing import ZopeTestCase as ztc
+
+from pfg.donationform.tests import base
+
+test_files = ('one-page-checkout.txt', 'add-to-cart.txt')
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Demonstrate the main content types
+        ztc.ZopeDocFileSuite(
+            f, package='pfg.donationform.tests',
+            test_class=base.FunctionalTestCase,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
+                doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
+
+        for f in test_files
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
