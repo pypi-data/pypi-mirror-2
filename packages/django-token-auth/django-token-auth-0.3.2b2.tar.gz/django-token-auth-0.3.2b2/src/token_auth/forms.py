92cd387ef3c992157785849965bad836470f11fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:01 +0000

    Add package django-token-auth/0.3.2b2/django-token-auth-0.3.2b2.tar.gz

---
+++ packages/django-token-auth/django-token-auth-0.3.2b2.tar.gz/django-token-auth-0.3.2b2/src/token_auth/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from models import Token, ProtectedURL
+
+        
+class TokenAddForm(forms.ModelForm):
+    class Meta:
+        model = Token
+        fields = ('url', 'name', 'email', 'forward_count', 'valid_until')
+
+class ProtectedURLForm(forms.ModelForm):
+    class Meta:
+        model = ProtectedURL
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        if not url[0] is '/':
+            url = '/%s' % url
+        return url
+
+class ForwardProtectedURLForm(forms.Form):
+    def __init__(self, token, *args, **kwargs):
+        super(ForwardProtectedURLForm, self).__init__(*args, **kwargs)
+        self.token = token
+    
+    emails = forms.CharField(max_length=255)
+    
+    def clean_emails(self):
+        emails = self.cleaned_data['emails'].split(';')
+        if self.token.forward_count and len(emails) > self.token.forward_count:
+                raise forms.ValidationError("You can only forward to %i emails." % (self.token.forward_count or 0))
+        return emails
---
+++ packages/django-token-auth/django-token-auth-0.3.2b2.tar.gz/django-token-auth-0.3.2b2/src/token_auth/middleware.py
@@ -0,0 +1,68 @@
+import datetime
+import sys
+
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseForbidden
+from django.core.urlresolvers import reverse, resolve
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth import login, get_backends
+
+from views import get_tokens_from_cookie
+from signals import signal_token_visited
+from models import ProtectedURL, Token
+
+
+class ProtectedURLMiddleware(object):
+    """
+    This middleware requires a login or associated token for protected urls.
+    """
+
+    def check_for_user_or_token(self, request):
+        # check user is not anonymous and is logged in
+        if not isinstance(request.user, AnonymousUser) and request.user.is_authenticated():
+            return True
+        # see if a protected url token exists
+        if getattr(request, 'valid_token', None):
+            return True
+        return False
+    
+    def process_request(self, request):
+        CHECK_TOKEN = False
+        try:
+            view, args, kwargs = resolve(request.path)
+            if 'use_token' is view.__name__: CHECK_TOKEN = True
+        except:
+            pass
+        if CHECK_TOKEN:
+            pass
+        elif request.path == reverse('login_form'):
+            pass
+        else:
+            where_sql = 'SUBSTR(%s, 1, LENGTH(url)) = url'
+            if ProtectedURL.objects.extra(where=[where_sql], params=[request.path]):
+                user_tokens = get_tokens_from_cookie(request) # get the user's tokens
+                tokens = Token.active_objects.filter(token__in=user_tokens).order_by('url')
+                if tokens:
+                    for token in tokens:
+                        if request.path.startswith(token.url):
+                            signal_token_visited.send(sender=self.__class__, request=request, token=token)
+                            request.valid_token = token
+                            break
+                allowed = self.check_for_user_or_token(request)
+                if not allowed:
+                    return HttpResponseRedirect(reverse('login_form'))
+
+class TokenAuthLoginMiddleware(object):
+    def process_request(self, request):
+        if isinstance(request.user, AnonymousUser):
+            token = getattr(request, 'valid_token', None) # found from cookies (valid)
+            if token:
+                try:
+                    user = User.objects.get(email=token.email)
+                    backend = get_backends()[0]
+                    user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name__)
+                    login(request, user)
+                except User.DoesNotExist:
+                    pass
+                    
+            
