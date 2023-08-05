6e8aa1832665118134f18906ce7f84194a75e8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:48 +0000

    Add package pgmagick/0.0.4/pgmagick-0.0.4.tar.gz

---
+++ packages/pgmagick/pgmagick-0.0.4.tar.gz/pgmagick-0.0.4/src/_main.cpp
@@ -0,0 +1,22 @@
+#include <boost/python.hpp>
+#include <boost/python/module.hpp>
+
+void __Blob();
+void __Color();
+void __FilterTypes();
+void __Geometry();
+void __Image();
+void __Montage();
+void __Pixels();
+
+
+BOOST_PYTHON_MODULE(_pgmagick)
+{
+    __Blob();
+    __Color();
+    __FilterTypes();
+    __Geometry();
+    __Image();
+    __Montage();
+    __Pixels();
+}
---
+++ packages/pgmagick/pgmagick-0.0.4.tar.gz/pgmagick-0.0.4/test/test_pgmagick_import.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../')
+sys.path.append('./')
+from pgmagick import Blob
+from pgmagick import Color
+from pgmagick import ColorHSL
+from pgmagick import ColorGray
+from pgmagick import ColorMono
+from pgmagick import ColorRGB
+from pgmagick import ColorYUV
+from pgmagick import Geometry
+from pgmagick import Image
+from pgmagick import Montage
