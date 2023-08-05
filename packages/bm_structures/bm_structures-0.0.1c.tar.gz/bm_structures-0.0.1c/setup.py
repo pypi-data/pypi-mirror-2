4dd789689d275de4b664a47ac888108a52aa09ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package bm_structures/0.0.1c/bm_structures-0.0.1c.tar.gz

---
+++ packages/bm_structures/bm_structures-0.0.1c.tar.gz/bm_structures-0.0.1c/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+VERSION = "0.0.1c"
+setup(
+    name = "bm_structures",
+    version = VERSION,
+    packages = [
+    'bm_structures',
+    #'bm_structures.finite_automaton',
+    #'bm_structures.proxy',
+    #'bm_structures.singleton',
+    #'bm_structures.trees'
+    ],
+    
+    package_dir = {'bm_structures' : 'src'},
+    zip_safe=False,
+    include_package_data=True,
+    license='GPL',
+    author='Alex Toney',
+    author_email='toneyalex@gmail.com',
+    description='Common structures for python',
+    test_suite="nose.collector",
+    tests_require="nose",
+)
