7520a4897558d1aec8b7dee2b65ab7b16cfa7ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package bm_structures/0.0.1.5/bm_structures-0.0.1.5.tar.gz

---
+++ packages/bm_structures/bm_structures-0.0.1.5.tar.gz/bm_structures-0.0.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+VERSION = "0.0.1.5"
+setup(
+    name = "bm_structures",
+    version = VERSION,
+    packages = [
+    'bm_structures',
+
+    ],
+    
+    package_dir = {'bm_structures' : 'src'},
+    zip_safe=False,
+    include_package_data=True,
+    license='GPL',
+    author='Alex Toney',
+    author_email='toneyalex@gmail.com',
+    description='Common structures for python',
+    test_suite="nose.collector",
+    tests_require="nose",
+)
