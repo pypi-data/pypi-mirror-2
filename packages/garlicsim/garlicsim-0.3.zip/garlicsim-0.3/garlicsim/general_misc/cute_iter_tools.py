3a9167ae7490c9630ea0dd34b157f790d2ff7126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:07 +0000

    Add package garlicsim/0.3/garlicsim-0.3.zip

---
+++ packages/garlicsim/garlicsim-0.3.zip/garlicsim-0.3/garlicsim/general_misc/cute_iter_tools.py
@@ -0,0 +1,43 @@
+# Copyright 2009-2010 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+This module defines functions that may be useful when working with iterators.
+'''
+
+def pairs(iterable):
+    '''
+    Iterate over successive pairs from the iterable.
+    
+    Example:
+    if the iterable is [0, 1, 2, 3], then its `pairs` would be
+    [(0, 1), (1, 2), (2, 3)]. (Except it would be an iterator and not an actual
+    list.)
+    '''
+    
+    first_run = True
+    old = None
+    for current in iterable:
+        if not first_run:
+            yield (old, current)
+        else:
+            first_run = False
+        old = current
+        
+def shorten(iterable, n):
+    '''
+    Shorten an iterator to length n.
+    
+    Iterate over the given iterable, but stop after n iterations (Or when the
+    iterable stops iteration by itself.)
+    
+    todo: make possible for n to be infinite.
+    '''
+    
+    assert isinstance(n, int)
+    counter = 0
+    for thing in iterable:
+        if counter >= n: raise StopIteration
+        yield thing
+        counter += 1
+        
---
+++ packages/garlicsim/garlicsim-0.3.zip/garlicsim-0.3/garlicsim/general_misc/dict_tools.py
@@ -0,0 +1,47 @@
+# Copyright 2009-2010 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+This module defines several functions that may be useful when working with
+dicts.
+'''
+
+import copy
+
+'''
+def set_or_raise(dict_, key, value)
+'''
+
+'''
+def deepcopy_values(d):
+    new_d = d.copy()
+    for key in new_d:
+        new_d[key] = copy.deepcopy(new_d[key])
+    return new_d
+'''
+
+
+def fancy_string(d, indent=0):
+    '''Show a dict as a string, slightly nicer than dict.__repr__.'''
+
+    small_space = ' ' * indent
+    
+    big_space = ' ' * (indent + 4)
+    
+    huge_space = ' ' * (indent + 8)
+    
+    def show(thing, indent=0):
+        space = ' ' * indent
+        enter_then_space = '\n' + space
+        return repr(thing).replace('\n', enter_then_space)
+    
+    temp1 = (
+        (big_space + repr(key) + ':\n' + huge_space + show(value, indent + 8))
+        for(key, value) in d.items())
+    
+    temp2 = small_space + '{\n' + ',\n'.join(temp1) + '\n' + small_space +'}'
+    
+    return temp2
+    
+    
+    
