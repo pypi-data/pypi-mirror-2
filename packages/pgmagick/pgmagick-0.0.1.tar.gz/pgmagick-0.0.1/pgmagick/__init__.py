ec3c30e2307aaf70d35a81348149137f420a61ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package pgmagick/0.0.1/pgmagick-0.0.1.tar.gz

---
+++ packages/pgmagick/pgmagick-0.0.1.tar.gz/pgmagick-0.0.1/pgmagick/__init__.py
@@ -0,0 +1,40 @@
+import _pgmagick
+
+
+class Blob(_pgmagick.Blob):
+    pass
+
+
+class Image(_pgmagick.Image):
+    pass
+
+
+FilterTypes = _pgmagick.FilterTypes
+
+
+class Color(_pgmagick.Color):
+    pass
+
+class ColorHSL(_pgmagick.ColorHSL):
+    pass
+
+class ColorGray(_pgmagick.ColorGray):
+    pass
+
+class ColorMono(_pgmagick.ColorMono):
+    pass
+
+class ColorRGB(_pgmagick.ColorRGB):
+    pass
+
+class ColorYUV(_pgmagick.ColorYUV):
+    pass
+
+#class Blob(_pgmmagick.Blob):
+#    def __init__(self, *args):
+#        if len(args) == 1 and type(args[0]) == type(''):
+#            _pgmagick.Blob.__init__(self)
+#            self.update(args[0])
+#        else:
+#            _pgmagick.Blob.__init__(self, *args)
+#    data = property(_pgmagick.get_blob_data, _pgmagick.Blob.update)
