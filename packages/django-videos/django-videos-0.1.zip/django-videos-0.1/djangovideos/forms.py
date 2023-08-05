4dc67040f66da6ca749e59c3704bdd084e96b160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package django-videos/0.1/django-videos-0.1.zip

---
+++ packages/django-videos/django-videos-0.1.zip/django-videos-0.1/djangovideos/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes.models import ContentType
+from djangovideos.models import Video
+
+class VideoForm(forms.ModelForm):
+    url = forms.CharField(label=_('Video url'))
+
+    def clean_url(self):
+        data = self.cleaned_data['url'].strip()
+        if 'src=' in data:
+            data = data.split('src=', 1)[1]
+            data = data.split(' ')[0]
+            data = data.strip(''''"''')
+        if data.startswith('http://www.youtube.com/watch?v='):
+            data = data.replace('http://www.youtube.com/watch?v=', 'http://www.youtube.com/v/')
+        if data.startswith('http'):
+            return data
+        else:
+            raise forms.ValidationError("No valid link found")
+
+    class Meta:
+        model = Video
+        fields = ('url',)
+
+    def save(self, request, obj, *args, **kwargs):
+        self.instance.creator = request.user
+        self.instance.content_type = ContentType.objects.get_for_model(obj)
+        self.instance.object_id = obj.id
+        super(VideoForm, self).save(*args, **kwargs)
