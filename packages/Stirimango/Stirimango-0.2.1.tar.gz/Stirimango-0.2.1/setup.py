4d6c57ef22d7f7f838374c5f3dbf77a49f1d7bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package Stirimango/0.2.1/Stirimango-0.2.1.tar.gz

---
+++ packages/Stirimango/Stirimango-0.2.1.tar.gz/Stirimango-0.2.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+classifiers = [ 'Development Status :: 4 - Beta'
+              , 'Intended Audience :: Developers'
+              , 'License :: OSI Approved :: BSD License'
+              , 'Operating System :: MacOS :: MacOS X'
+              , 'Operating System :: POSIX :: Linux'
+              , 'Operating System :: Unix'
+              , 'Programming Language :: Python :: 3'
+              ]
+
+setup(name='Stirimango',
+      version='0.2.1',
+      author='Robert Escriva (rescrv)',
+      author_email='stirimango@mail.robescriva.com',
+      packages=['stirimango'
+               ],
+      scripts=['bin/stirimango'],
+      license='3-clause BSD',
+      url='http://robescriva.com/',
+      description='Database migrations for Postgresql.',
+      long_description=open('README.rst').read(),
+      classifiers=classifiers,
+      )
