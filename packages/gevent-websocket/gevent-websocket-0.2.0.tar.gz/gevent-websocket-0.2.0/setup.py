275333b7c3cfa8789eb3d00e74852e1919290673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:07 +0000

    Add package gevent-websocket/0.2.0/gevent-websocket-0.2.0.tar.gz

---
+++ packages/gevent-websocket/gevent-websocket-0.2.0.tar.gz/gevent-websocket-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gevent-websocket",
+    version="0.2.0",
+    description="Websocket handler for the gevent pywsgi server, a Python network library",
+    long_description=open("README.rst").read(),
+    author="Jeffrey Gelens",
+    author_email="jeffrey@noppo.pro",
+    license="BSD",
+    url="http://www.gelens.org/code/gevent-websocket/",
+    download_url="http://www.gelens.org/code/gevent-websocket/",
+    install_requires=("gevent", "greenlet"),
+    packages=find_packages(exclude=["examples","tests"]),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+)
