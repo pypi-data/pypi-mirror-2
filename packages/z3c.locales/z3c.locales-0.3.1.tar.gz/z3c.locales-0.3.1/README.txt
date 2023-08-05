8079b6fbbc713202b709193ef165e864541d51c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:59 +0000

    Add package z3c.locales/0.3.1/z3c.locales-0.3.1.tar.gz

---
+++ packages/z3c.locales/z3c.locales-0.3.1.tar.gz/z3c.locales-0.3.1/README.txt
@@ -0,0 +1,22 @@
+This package provides the translation for the `z3c` packages which are
+using the shared `z3c` domain.
+
+.. contents::
+
+==========
+ Contents
+==========
+
+This package provides translations for:
+
+- z3c.authviewlet
+- z3c.contents
+- z3c.layer.pagelet
+- z3c.table
+- z3c.wizard
+
+=====
+Usage
+=====
+
+Include the ``configure.zcml`` of this package in your project.
---
+++ packages/z3c.locales/z3c.locales-0.3.1.tar.gz/z3c.locales-0.3.1/setup.py
@@ -0,0 +1,74 @@
+##############################################################################
+#
+# Copyright (c) 2008-2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup"""
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name = 'z3c.locales',
+    version='0.3.1',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description = "Shared z3c domain translations",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    keywords = "Zope z3c locale locales i18n internationalisation",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Natural Language :: German',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://pypi.python.org/pypi/z3c.locales',
+    license='ZPL 2.1',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require=dict(
+        test=[
+            'zope.testing',
+            ],
+        extract = [
+            'z3c.authviewlet',
+            'z3c.contents',
+            'z3c.csvvocabulary',
+            'z3c.layer.pagelet',
+            'z3c.table',
+            'z3c.wizard',
+            ],
+        compile=[
+            'python-gettext'
+            ],
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.i18n',
+        ],
+    zip_safe = False,
+    )
