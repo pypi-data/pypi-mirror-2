9a5514cf043683b7f714fd4a0681265780f6dffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:59 +0000

    Add package django-planet/0.1/django-planet-0.1.tar.gz

---
+++ packages/django-planet/django-planet-0.1.tar.gz/django-planet/sample_project/planet/urls.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+from django.conf.urls.defaults import patterns, url
+
+
+# HTML view's urls
+urlpatterns = patterns('planet.views',
+    url(r'^blogs/(?P<blog_id>\d+)/$', "blog_detail", name="planet_blog_detail"),
+    url(r'^blogs/$', "blogs_list", name="planet_blog_list"),
+
+    url(r'^feeds/(?P<feed_id>\d+)/tags/(?P<tag>.*)/$', "feed_detail", name="planet_by_tag_feed_detail"),
+    url(r'^feeds/(?P<feed_id>\d+)/$', "feed_detail", name="planet_feed_detail"),
+    url(r'^feeds/$', "feeds_list", name="planet_feed_list"),
+
+    url(r'^authors/(?P<author_id>\d+)/tags/(?P<tag>.*)/$', "author_detail", name="planet_by_tag_author_detail"),
+    url(r'^authors/(?P<author_id>\d+)/$', "author_detail", name="planet_author_detail"),
+    url(r'^authors/$', "authors_list", name="planet_author_list"),
+    
+    url(r'^tags/(?P<tag>.*)/feeds/$', "tag_feeds_list", name="planet_tag_feed_list"),
+    url(r'^tags/(?P<tag>.*)/authors/$', "tag_authors_list", name="planet_tag_author_list"),
+    url(r'^tags/(?P<tag>.*)/$', "tag_detail", name="planet_tag_detail"),
+    url(r'^tags/$', "tags_cloud", name="planet_tag_cloud"),
+    
+    url(r'^opml/$', "opml", name="planet_opml"),
+    url(r'^foaf/$', "foaf", name="planet_foaf"),
+    
+    url(r'^posts/(?P<post_id>\d+)/$', "post_detail", name="planet_post_detail"),
+    url(r'^posts/$', "posts_list", name="planet_post_list"),
+    
+    url(r'^search/$', "search", name="planet_search"),
+
+    url(r'^$', "index", name="planet_index"),
+)
+
+# Feed's urls
+urlpatterns += patterns('planet.feeds',
+    url(r'^posts/feeds/rss/$', "rss_feed", name="planet_rss_feed"),
+    url(r'^feeds/rss/tags/(?P<tag>.*)/$', "rss_feed", name="planet_tag_rss_feed"),
+    url(r'^feeds/rss/authors/(?P<author_id>\d+)/$', "rss_feed", name="planet_author_rss_feed"),
+    url(r'^feeds/rss/authors/(?P<author_id>\d+)/tags/(?P<tag>.*)/$', "rss_feed", name="planet_tag_author_rss_feed"),
+)
