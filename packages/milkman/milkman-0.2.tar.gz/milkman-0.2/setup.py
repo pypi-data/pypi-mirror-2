701dfd7ad76c8bc51a77e0ac031aae86c8ef76ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:49 +0000

    Add package milkman/0.2/milkman-0.2.tar.gz

---
+++ packages/milkman/milkman-0.2.tar.gz/milkman-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name="milkman",
+    version="0.2",
+    packages = ["milkman"],
+    author="Wilkes Joiner, Chuck Collins",
+    author_email="chuck.collins@gmail.com",
+    install_requires=["docutils>=0.3","django"],
+    license = "MIT",
+    keywords = "django testing mock stub",
+)
