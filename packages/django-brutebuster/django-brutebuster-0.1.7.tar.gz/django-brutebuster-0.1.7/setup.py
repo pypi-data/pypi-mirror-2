17875a33d2ec507c3208c261b02a89e09b08c217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package django-brutebuster/0.1.7/django-brutebuster-0.1.7.tar.gz

---
+++ packages/django-brutebuster/django-brutebuster-0.1.7.tar.gz/django-brutebuster-0.1.7/setup.py
@@ -0,0 +1,14 @@
+#import ez_setup
+#ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "django-brutebuster",
+    version = "0.1.7",
+    packages = find_packages(),
+    author = "Cyber Security Consulting Ltd.",
+    author_email = "contact@csc.bg",
+    description = "Pluggable Django application providing password bruteforce protection",
+    url = "http://code.google.com/p/django-brutebuster/",
+    include_package_data = True
+)
+
