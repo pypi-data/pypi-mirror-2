91e5ab21d537f12f46612f642d29c008adbd1117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package Products.PloneGetPaid/0.10.0/Products.PloneGetPaid-0.10.0.zip

---
+++ packages/Products.PloneGetPaid/Products.PloneGetPaid-0.10.0.zip/Products.PloneGetPaid-0.10.0/setup.py
@@ -0,0 +1,56 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.10.0'
+
+setup(name='Products.PloneGetPaid',
+      version=version,
+      description="E-commerce framework for Plone",
+      long_description = (
+        read('README.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Software Development :: Libraries",
+      ],
+      keywords='commerce donation zope plone getpaid',
+      author='GetPaid Team',
+      author_email='getpaid-dev@googlegroups.com',
+      url='http://www.plonegetpaid.com/',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'five.intid',
+          'getpaid.core>=0.9.0',
+          'getpaid.nullpayment>=0.5.0',
+          'getpaid.wizard',
+          'ore.viewlet',
+          'yoma.batching',
+          'zc.table',
+          'zope.interface',
+          'zope.component',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
