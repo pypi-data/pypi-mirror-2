d2ce7c742c6f08fc7fac4ecb43220390af2912a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:34 +0000

    Add package collective.autoresizetextarea/1.0b2/collective.autoresizetextarea-1.0b2.tar.gz

---
+++ packages/collective.autoresizetextarea/collective.autoresizetextarea-1.0b2.tar.gz/collective.autoresizetextarea-1.0b2/README.txt
@@ -0,0 +1,49 @@
+Introduction
+============
+
+collective.autoresizetextarea is a textarea auto-resizer for Plone.
+It is based on James Padolsey's jQuery `autoResize`_ plugin. Check
+his website_ for a demo.
+
+.. _autoResize: http://james.padolsey.com/javascript/jquery-plugin-autoresize/
+
+.. _website: http://james.padolsey.com/javascript/jquery-plugin-autoresize/
+
+Buildout Installation
+---------------------
+
+Add the following code to your buildout.cfg::
+
+    [buildout]
+    ...
+    eggs =
+        ...
+        collective.autoresizetextarea
+        ...
+
+    ...
+    [instance]
+    ...
+    zcml =
+        ...
+        collective.autoresizetextarea
+    ...
+
+Usage
+-----
+
+To add the auto-resize feature to a textarea you can either add an
+"autoresize" CSS class to the textarea::
+
+    <textarea class="autoresize" />
+
+or change the jQuery selector in the second line of the javascripts/autoresize.js file::
+
+    ...
+    jq('textarea.mycssclass').autoResize({
+       ...
+    ...
+
+Consult the `jQuery documentation`_ for more about jQuery selectors.
+
+.. _`jQuery documentation`: http://docs.jquery.com/Selectors
---
+++ packages/collective.autoresizetextarea/collective.autoresizetextarea-1.0b2.tar.gz/collective.autoresizetextarea-1.0b2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b2'
+
+setup(name='collective.autoresizetextarea',
+      version=version,
+      description="jQuery textarea auto-resizer for Plone.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='jQuery textarea auto-resize',
+      author='Timo Stollenwerk',
+      author_email='timo@zmag.de',
+      url='http://pypi.python.org/pypi/collective.autoresizetextarea',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
