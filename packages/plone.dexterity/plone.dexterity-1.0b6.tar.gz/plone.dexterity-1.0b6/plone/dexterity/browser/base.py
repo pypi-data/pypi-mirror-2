00ab807d9cd2844cf98d6c8e4f40c1be42c2a96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package plone.dexterity/1.0b6/plone.dexterity-1.0b6.tar.gz

---
+++ packages/plone.dexterity/plone.dexterity-1.0b6.tar.gz/plone.dexterity-1.0b6/plone/dexterity/browser/base.py
@@ -0,0 +1,43 @@
+import logging
+from zope.component import getUtility
+
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.autoform.form import AutoExtensibleForm
+
+from plone.dexterity.i18n import MessageFactory as _
+from plone.dexterity.interfaces import IDexterityFTI
+from plone.dexterity.utils import resolveDottedName
+
+log = logging.getLogger(__name__)
+
+class DexterityExtensibleForm(AutoExtensibleForm):
+    """Mixin class for Dexterity forms that support updatable fields
+    """
+
+    default_fieldset_label = _(u"Content")
+
+    @property
+    def description(self):
+        fti = getUtility(IDexterityFTI, name=self.portal_type)
+        return fti.Description()
+    
+    # AutoExtensibleForm contract
+    
+    @property
+    def schema(self):
+        fti = getUtility(IDexterityFTI, name=self.portal_type)
+        return fti.lookupSchema() 
+    
+    @property
+    def additionalSchemata(self):
+        fti = getUtility(IDexterityFTI, name=self.portal_type)
+        for behavior_name in fti.behaviors:
+            try:
+                behavior_interface = resolveDottedName(behavior_name)
+            except (ValueError, ImportError):
+                log.warning("Error resolving behaviour %s", behavior_name)
+                continue
+            if behavior_interface is not None:
+                behavior_schema = IFormFieldProvider(behavior_interface, None)
+                if behavior_schema is not None:
+                    yield behavior_schema
