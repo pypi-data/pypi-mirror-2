54c8bd69b60f7c209adcfaf0a0b51f537fcaa409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package django-navigation/0.5.0/django-navigation-0.5.0.tar.gz

---
+++ packages/django-navigation/django-navigation-0.5.0.tar.gz/django-navigation-0.5.0/navigation/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2008-2009 Andy Mikhailenko and contributors
+#
+#  Django Navigation is free software under terms of the GNU Lesser
+#  General Public License version 3 (LGPLv3) as published by the Free
+#  Software Foundation. See the file README for copying conditions.
+#
+
+" A breadcrumbs navigation application for Django framework. "
+
+__author__  = 'Andy Mikhailenko'
+__license__ = 'GNU Lesser General Public License (GPL), Version 3'
+__url__     = 'http://bitbucket.org/neithere/django-navigation/'
+__version__ = '0.5.0'
