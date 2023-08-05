6d79a0094719215530df8bae67c593769b48b9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package python-cassandra/0.6.1/python-cassandra-0.6.1.tar.gz

---
+++ packages/python-cassandra/python-cassandra-0.6.1.tar.gz/python-cassandra-0.6.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# © 2009, 2010 Digg, Inc. All rights reserved.
+# Author: Ian Eure <ian@digg.com>
+#
+
+from setuptools import setup, find_packages
+
+setup(name="python-cassandra",
+      version='0.6.1',
+      description="Thrift client for Cassandra",
+      url="http://incubator.apache.org/cassandra/",
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      author="Ian Eure",
+      author_email="ian.eure@gmail.com",
+      scripts=['scripts/Cassandra-remote'],
+      keywords="database cassandra",
+      install_requires=['Thrift'])
