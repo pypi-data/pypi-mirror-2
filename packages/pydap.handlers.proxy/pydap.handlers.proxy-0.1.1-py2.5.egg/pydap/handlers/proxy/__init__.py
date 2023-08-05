128b468781976ca21ffb29c97e44b1650d56b509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:06 +0000

    Add package pydap.handlers.proxy/0.1.1/pydap.handlers.proxy-0.1.1-py2.5.egg

---
+++ packages/pydap.handlers.proxy/pydap.handlers.proxy-0.1.1-py2.5.egg/pydap/handlers/proxy/__init__.py
@@ -0,0 +1,36 @@
+import re
+
+from configobj import ConfigObj
+from paste.httpexceptions import HTTPSeeOther
+
+from pydap.handlers.lib import BaseHandler
+from pydap.exceptions import OpenFileError
+from pydap.client import open_url
+from pydap.handlers.helper import constrain
+
+
+class Handler(BaseHandler):
+
+    extensions = re.compile(r"^.*\.url$", re.IGNORECASE)
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def parse_constraints(self, environ):
+        try:
+            config = ConfigObj(self.filepath)
+        except:
+            message = 'Unable to open file %s.' % self.filepath
+            raise OpenFileError(message)
+    
+        url = config['dataset']['url']
+        pass_ = config['dataset']['pass']
+        response = environ['pydap.response']
+
+        if response in pass_:
+            # forward to the requested response
+            raise HTTPSeeOther(url + '.' + response)
+        else:
+            # load a dataset and return it
+            dataset = open_url(url)
+            return constrain(dataset, environ.get('QUERY_STRING', ''))
