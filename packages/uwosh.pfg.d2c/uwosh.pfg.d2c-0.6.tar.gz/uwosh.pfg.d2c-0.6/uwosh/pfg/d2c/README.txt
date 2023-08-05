8dd121a24f1f2bf4c4b6eaee85785cb21a049548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:01 +0000

    Add package uwosh.pfg.d2c/0.6/uwosh.pfg.d2c-0.6.tar.gz

---
+++ packages/uwosh.pfg.d2c/uwosh.pfg.d2c-0.6.tar.gz/uwosh.pfg.d2c-0.6/uwosh/pfg/d2c/README.txt
@@ -0,0 +1,15 @@
+Introduction
+============
+
+uwosh.pfg.(d)ata(2)(c)ontent
+
+This product provides a dynamic content type to store PloneFormGen form
+data into. It leverages schemaextenders ability to dynamically add extra
+fields on a content type so that you essentially get a persistent copy of
+your form.
+
+The product adds a "Save Data to Content Adapter" item to the "Add new.." drop
+down for the PloneFormGen Form. Once enabled, when a user submits a form,
+a new content item is created with that data and located in the adapter.
+
+Compatible with versions of PloneFormGen >= 1.2.2 and Plone 3.x -> 4.0.
---
+++ packages/uwosh.pfg.d2c/uwosh.pfg.d2c-0.6.tar.gz/uwosh.pfg.d2c-0.6/uwosh/pfg/d2c/content/dataentry.py
@@ -0,0 +1,29 @@
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.public import *
+from Products.ATContentTypes.content.base import registerATCT
+from uwosh.pfg.d2c.config import PROJECTNAME
+from Products.ATContentTypes.content.base import ATCTContent
+from Products.ATContentTypes.content.schemata import ATContentTypeSchema
+from uwosh.pfg.d2c.interfaces import IFormSaveData2ContentEntry
+from zope.interface import implements
+
+
+FormSaveData2ContentEntrySchema = ATContentTypeSchema.copy()
+FormSaveData2ContentEntrySchema.delField('title')
+FormSaveData2ContentEntrySchema.delField('description')
+
+class FormSaveData2ContentEntry(ATCTContent):
+    implements(IFormSaveData2ContentEntry)
+    
+    schema = FormSaveData2ContentEntrySchema
+
+    meta_type = portal_type = 'FormSaveData2ContentEntry'
+    archetype_name = 'Save Data to Content Entry'
+    
+    security       = ClassSecurityInfo()
+
+    
+    def Title(self):
+        return self.getId()
+    
+registerATCT(FormSaveData2ContentEntry, PROJECTNAME)
