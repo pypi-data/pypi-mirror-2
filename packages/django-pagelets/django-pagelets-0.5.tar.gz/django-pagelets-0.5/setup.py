8d63e574751b883aa61a955ac3a35f7befa2467e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:52 +0000

    Add package django-pagelets/0.5/django-pagelets-0.5.tar.gz

---
+++ packages/django-pagelets/django-pagelets-0.5.tar.gz/django-pagelets-0.5/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+packages = find_packages()
+packages.remove('sample_project')
+
+classifiers = """
+Topic :: Internet :: WWW/HTTP :: Dynamic Content
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+Development Status :: 4 - Beta
+Operating System :: OS Independent
+"""
+
+setup(
+    name='django-pagelets',
+    version='0.5',
+    author='Caktus Consulting Group',
+    author_email='solutions@caktusgroup.com',
+    packages=packages,
+    install_requires = [],
+    include_package_data = True,
+    exclude_package_data={
+        '': ['*.sql', '*.pyc'],
+        'pagelets': ['media/*'],
+    },
+    url='http://http://github.com/caktus/django-pagelets',
+    license='LICENSE.txt',
+    description='Simple, flexible app for integrating static, unstructured '
+                'content in a Django site',
+    classifiers = filter(None, classifiers.split("\n")),
+    long_description=open('README.rst').read(),
+)
