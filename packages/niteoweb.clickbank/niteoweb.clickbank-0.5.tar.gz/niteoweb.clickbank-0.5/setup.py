35f42b79a937956a604c8f17f64cd2c74b82f062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:57 +0000

    Add package niteoweb.clickbank/0.5/niteoweb.clickbank-0.5.tar.gz

---
+++ packages/niteoweb.clickbank/niteoweb.clickbank-0.5.tar.gz/niteoweb.clickbank-0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.5'
+
+setup(name='niteoweb.clickbank',
+      version=version,
+      description="Integrates ClickBank digital products retailer system with Plone for paid memberships.",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone clickbank',
+      author='NiteoWeb Ltd.',
+      author_email='info@niteoweb.com',
+      url='http://svn.plone.org/svn/collective/niteoweb.clickbank',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['niteoweb'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
