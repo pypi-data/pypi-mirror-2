f74e1330e7c6abe82d01f520acd300345b21c01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:51 +0000

    Add package zope.app.interface/3.5.1/zope.app.interface-3.5.1.zip

---
+++ packages/zope.app.interface/zope.app.interface-3.5.1.zip/zope.app.interface-3.5.1/setup.py
@@ -0,0 +1,58 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.interface package
+
+$Id: setup.py 114291 2010-07-07 16:48:24Z adamg $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.app.interface',
+      version='3.5.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Zope Interface Extensions',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 interface persistent type",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.app.interface',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      install_requires=['setuptools',
+                        'ZODB3',
+                        'zodbcode',
+                        'zope.componentvocabulary',
+                        'zope.security',
+                       ],
+      include_package_data = True,
+      zip_safe = False,
+      )
