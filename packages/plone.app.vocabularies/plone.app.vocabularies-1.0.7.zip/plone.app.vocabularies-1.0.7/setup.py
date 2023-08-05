3c27da977f418f633688138a9be74ac17f8da6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:45 +0000

    Add package plone.app.vocabularies/1.0.7/plone.app.vocabularies-1.0.7.zip

---
+++ packages/plone.app.vocabularies/plone.app.vocabularies-1.0.7.zip/plone.app.vocabularies-1.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.7'
+
+setup(name='plone.app.vocabularies',
+      version=version,
+      description="A collection of generally useful vocabularies for usage in zope.formlib.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        ],
+      keywords='Plone Zope formlib vocabularies',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.vocabularies',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      )
