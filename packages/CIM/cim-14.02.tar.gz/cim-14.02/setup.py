3f101daa1670f3746465f86c205166b748f52c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package CIM/14.02/cim-14.02.tar.gz

---
+++ packages/CIM/cim-14.02.tar.gz/cim-14.02/setup.py
@@ -0,0 +1,15 @@
+__author__ = "Richard Lincoln"
+
+from setuptools import setup, find_packages
+
+setup(name="cim",
+      description="Python implementation of the Common Information Model.",
+      long_description = open('README').read().strip(),
+      author="Richard Lincoln",
+      author_email="r.w.lincoln@gmail.com",
+      url="http://rwl.github.com/cim",
+      version="14.02",
+      license="Apache License version 2.0",
+#      include_package_data=True,
+      packages=find_packages(),
+      zip_safe=True)
