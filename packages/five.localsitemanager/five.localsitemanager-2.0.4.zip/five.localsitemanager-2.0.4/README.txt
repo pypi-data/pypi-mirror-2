c92b2205be27c87e5a25f03b5f267da228456022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package five.localsitemanager/2.0.4/five.localsitemanager-2.0.4.zip

---
+++ packages/five.localsitemanager/five.localsitemanager-2.0.4.zip/five.localsitemanager-2.0.4/README.txt
@@ -0,0 +1,17 @@
+Introduction
+============
+
+Overview
+--------
+
+five.localsitemanager attempts to provide a local site manager implementation
+that is as close to the zope.interface / zope.component implementation as
+possible. Some reservations that do not conflict with the original API have
+been made to ease the path with CMF.
+
+Developer Resources
+-------------------
+
+- Subversion browser:
+
+  http://svn.zope.org/five.localsitemanager
---
+++ packages/five.localsitemanager/five.localsitemanager-2.0.4.zip/five.localsitemanager-2.0.4/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+version = '2.0.4'
+
+setup(name='five.localsitemanager',
+      version=version,
+      url='http://pypi.python.org/pypi/five.localsitemanager',
+      license='ZPL 2.1',
+      description='Local site manager implementation for Zope 2',
+      author='Zope Foundation and Contributors',
+      author_email='zope-cmf@zope.org',
+      long_description=open("README.txt").read() + "\n" + 
+                       open("INSTALL.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          'Environment :: Web Environment',
+          'Framework :: Zope2',
+          'License :: OSI Approved :: Zope Public License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP :: Site Management',
+      ],
+      keywords='zope zope2 five sitemanager',
+      packages=['five', 'five.localsitemanager'],
+      package_dir = {'': 'src'},
+      namespace_packages=['five',],
+      include_package_data = True,
+      install_requires=[
+          'Acquisition',
+          'ZODB3',
+          'setuptools',
+          'zope.component',
+          'zope.event',
+          'zope.interface',
+          'zope.location',
+          'zope.lifecycleevent',
+          'zope.site',
+          'zope.testing',
+          'zope.traversing',
+          'Zope2 >= 2.12.0',
+          ],
+      zip_safe = False,
+      )
