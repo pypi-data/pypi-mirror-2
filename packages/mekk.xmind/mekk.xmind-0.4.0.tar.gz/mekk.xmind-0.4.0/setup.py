134e86416ff15db2290b0e071f58f38108d2e2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package mekk.xmind/0.4.0/mekk.xmind-0.4.0.tar.gz

---
+++ packages/mekk.xmind/mekk.xmind-0.4.0.tar.gz/mekk.xmind-0.4.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# (c) 2008, Marcin Kasperski
+
+from setuptools import setup, find_packages
+
+version = '0.4.0'
+long_description = open("README.txt").read()
+
+classifiers = [
+    "Programming Language :: Python",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    # TODO: Development Status, Environment, Topic
+    ]
+
+setup(name='mekk.xmind',
+      version=version,
+      description="XMind data files reading and writing library.",
+      long_description=long_description,
+      classifiers=classifiers,
+      keywords='xmind',
+      author='Marcin Kasperski',
+      author_email='Marcin.Kasperski@mekk.waw.pl',
+      url='http://bitbucket.org/Mekk/mekk.xmind/',
+      license='Artistic',
+      package_dir={'':'src'},
+      packages=find_packages('src', exclude=['ez_setup', 'examples', 'tests']),
+      namespace_packages=['mekk'],
+      test_suite = 'nose.collector',
+      include_package_data = True,
+      zip_safe=False,
+      install_requires=[
+          'lxml >= 2.1.1',
+      ],
+#      entry_points="""
+#      [babel.extractors]
+#      mako = mako.ext.babelplugin:extract
+#      """,
+)
