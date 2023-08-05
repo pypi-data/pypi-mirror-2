2c73ba27ccba29b918c86914c9230fab371019bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package Products.TinyMCE/1.1/Products.TinyMCE-1.1-py2.4.egg

---
+++ packages/Products.TinyMCE/Products.TinyMCE-1.1-py2.4.egg/Products/TinyMCE/tests.py
@@ -0,0 +1,33 @@
+"""Test setup for integration and functional tests."""
+import unittest
+
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+
+from Testing import ZopeTestCase as ztc
+
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import onsetup
+
+@onsetup
+def setup_product():
+    """Set up the package and its dependencies."""
+    
+    fiveconfigure.debug_mode = True
+    import Products.TinyMCE
+    zcml.load_config('configure.zcml', Products.TinyMCE)
+    fiveconfigure.debug_mode = False
+    
+setup_product()
+ptc.setupPloneSite(products=['Products.TinyMCE'])
+
+doc_tests = ('adapters.txt','browser.txt','exportimport.txt','setuphandlers.txt','utility.txt','transforms.txt','upgrades.txt',)
+
+def test_suite():
+    """This sets up a test suite that actually runs the tests"""
+    return unittest.TestSuite(
+        [ztc.ZopeDocFileSuite(
+            'tests/%s' % f, package='Products.TinyMCE',
+            test_class=ptc.FunctionalTestCase)
+            for f in doc_tests],
+        )
