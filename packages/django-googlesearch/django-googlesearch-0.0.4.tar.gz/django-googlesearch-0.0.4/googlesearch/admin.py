1fd788bb058d8885551bd3a2118e0e921b44fff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package django-googlesearch/0.0.4/django-googlesearch-0.0.4.tar.gz

---
+++ packages/django-googlesearch/django-googlesearch-0.0.4.tar.gz/django-googlesearch-0.0.4/googlesearch/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from googlesearch.models import GoogleSearchPreferences
+
+admin.site.register(GoogleSearchPreferences)
---
+++ packages/django-googlesearch/django-googlesearch-0.0.4.tar.gz/django-googlesearch-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-googlesearch',
+    version='0.0.4',
+    description='Django Google custom search engine app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/django-googlesearch',
+    packages = find_packages(),
+    install_requires = [
+        'django-preferences',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
