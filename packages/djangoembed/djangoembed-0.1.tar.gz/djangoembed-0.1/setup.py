5f39ec60c7bc8f1a8964ca469b42d5ade62129fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:26 +0000

    Add package djangoembed/0.1/djangoembed-0.1.tar.gz

---
+++ packages/djangoembed/djangoembed-0.1.tar.gz/djangoembed-0.1/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='djangoembed',
+    version="0.1",
+    description='rich media consuming/providing for django',
+    long_description=readme,
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    url='http://github.com/worldcompany/djangoembed/tree/master',
+    packages=find_packages(),
+    package_data = {
+        'oembed': [
+            'templates/*.html',
+            'templates/*/*.html',
+            'templates/*/*/*.html',
+            'tests/templates/*.html',
+            'tests/templates/*/*.html',
+            'tests/templates/*/*/*.html',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
