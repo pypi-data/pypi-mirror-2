1a23135f5ef047298ce67b9a9fb9ba44021c1570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package zope.app.preference/3.8.0/zope.app.preference-3.8.0.tar.gz

---
+++ packages/zope.app.preference/zope.app.preference-3.8.0.tar.gz/zope.app.preference-3.8.0/setup.py
@@ -0,0 +1,68 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.app.preference package
+
+$Id: setup.py 113390 2010-06-12 11:47:50Z icemac $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name = 'zope.app.preference',
+      version='3.8.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='User Preferences Framework ZMI UI',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          '.. contents::\n\n' +
+          read('src', 'zope', 'app', 'preference', 'README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 user preference",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.app.preference',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require=dict(test=[
+          'zope.app.testing',
+          'zope.app.zcmlfiles',
+          ]),
+      install_requires = ['setuptools',
+                          'zope.preference',
+                          'zope.app.tree',
+                          ],
+      include_package_data = True,
+      zip_safe = False,
+      )
