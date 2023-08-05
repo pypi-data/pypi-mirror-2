0aa255fa6827f621605b04cace7f12945d307d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:09 +0000

    Add package langacore.kit.django/0.1.7/langacore.kit.django-0.1.7.tar.gz

---
+++ packages/langacore.kit.django/langacore.kit.django-0.1.7.tar.gz/langacore.kit.django-0.1.7/src/langacore/kit/django/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import os, os.path
+
+def publish(path):
+    return os.sep.join((os.path.split(__file__)[0], path,))
+
+current_dir_support = publish('current_dir_support.py')
+profile_support = publish('profile_support.py')
