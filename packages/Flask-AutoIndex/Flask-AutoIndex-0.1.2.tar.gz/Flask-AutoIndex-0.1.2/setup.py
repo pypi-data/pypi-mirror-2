54c59d788b3bc5436168500b287297170fddbc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:50 +0000

    Add package Flask-AutoIndex/0.1.2/Flask-AutoIndex-0.1.2.tar.gz

---
+++ packages/Flask-AutoIndex/Flask-AutoIndex-0.1.2.tar.gz/Flask-AutoIndex-0.1.2/setup.py
@@ -0,0 +1,63 @@
+"""
+Flask-AutoIndex
+---------------
+
+Flask-AutoIndex generates an index page for your `Flask`_ application
+automatically. The result just like `mod_autoindex`_, but the look is more
+awesome! Look at this:
+
+.. _Flask: http://flask.pocoo.org/
+.. _mod_autoindex: http://httpd.apache.org/docs/current/mod/mod_autoindex.html
+
+Links
+`````
+
+* `documentation <http://docs.sublee.kr/flask-autoindex>`_
+* `development version
+  <http://github.com/sublee/flask-autoindex/zipball/master>`_
+
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def run_tests():
+    from tests import suite
+    return suite()
+
+
+setup(
+    name="Flask-AutoIndex",
+    version="0.1.2",
+    url="http://github.com/sublee/flask-autoindex",
+    license="BSD",
+    author="Lee Heung-sub",
+    author_email="heung@sublee.kr",
+    description="Generates index page like mod_autoindex for Flask application",
+    long_description=__doc__,
+    packages=["flaskext", "flaskext.autoindex"],
+    include_package_data=True,
+    package_data={"flaskext.autoindex": ["static/icons/*",
+                                         "static/style.css",
+                                         "templates/*"]},
+    namespace_packages=["flaskext"],
+    zip_safe=False,
+    platforms="any",
+    install_requires=[
+        "Flask"
+    ],
+    test_suite="__main__.run_tests",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
+
