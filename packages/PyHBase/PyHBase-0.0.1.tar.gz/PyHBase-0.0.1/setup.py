329527cf92b27a6da504460cbb05adeb48f96877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:03 +0000

    Add package PyHBase/0.0.1/PyHBase-0.0.1.tar.gz

---
+++ packages/PyHBase/PyHBase-0.0.1.tar.gz/PyHBase-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="PyHBase",
+      version='0.0.1',
+      description="High-level Python interface to HBase",
+      url="http://github.com/hammer/pyhbase/tree/master",
+      packages=find_packages(),
+      include_package_data=True,
+      author="Jeff Hammerbacher",
+      author_email="hammer@cloudera.com",
+      keywords="database hbase",
+      scripts=['examples/cli.py'],
+      install_requires=['Thrift', 'python-hbase'])
