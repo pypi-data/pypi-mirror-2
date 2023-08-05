f6082c9a0cc87b84cef1d19bf5002f9e82a286dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package zopyx.smartprintng.psd/0.1.1/zopyx.smartprintng.psd-0.1.1.tar.gz

---
+++ packages/zopyx.smartprintng.psd/zopyx.smartprintng.psd-0.1.1.tar.gz/zopyx.smartprintng.psd-0.1.1/README.txt
@@ -0,0 +1,32 @@
+zopyx.smartprintng.psd
+======================
+
+A simple converter for parsing PSD (Photoshop) files into PrinceXML templates (HTML/CSS)
+based on the ``psdparse`` converter.
+
+Requirements
+============
+* requires ``psdparse`` :
+  - svn checkout http://www.telegraphics.com.au/svn/psdparse
+  - configure; make; make install
+
+Usage
+=====
+
+* bin/psd2prince --help
+* bin/psd2prince <PSD-FILENAME>
+
+Licence
+=======
+``zopyx.smartprintng.psd`` is published under the GNU Public License V2 (GPL 2)
+
+Author
+======
+
+| ZOPYX Limited
+| Andreas Jung
+| Charlottenstr. 37/1
+| 72070 Tuebingen
+| Germany
+| info@zopyx.com
+| www.zopyx.com
---
+++ packages/zopyx.smartprintng.psd/zopyx.smartprintng.psd-0.1.1.tar.gz/zopyx.smartprintng.psd-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.1'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('docs/HISTORY.txt')
+    + '\n' )
+
+
+setup(name='zopyx.smartprintng.psd',
+      version=version,
+      description="PSD -> PrinceXML converter",
+      long_description=long_description,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='http://pypi.python.org/pypi/zopyx.smartprintng.psd',
+      license='GPL 2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      namespace_packages=['zopyx', 'zopyx.smartprintng'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'lxml',
+      ],
+      entry_points=dict(console_scripts=(
+          'psd2prince=zopyx.smartprintng.psd.psd:main',
+      )),
+      )
