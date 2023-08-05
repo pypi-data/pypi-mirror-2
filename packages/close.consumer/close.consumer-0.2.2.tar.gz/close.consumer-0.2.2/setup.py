4b3ff79ddc2803c1ae8241db7e6cb79019fe4836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:03 +0000

    Add package close.consumer/0.2.2/close.consumer-0.2.2.tar.gz

---
+++ packages/close.consumer/close.consumer-0.2.2.tar.gz/close.consumer-0.2.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'close.consumer',
+    version = '0.2.2',
+    description = 'gevent based Streaming API consumer',
+    long_description = open('README.rst').read(),
+    author = 'James Arthur',
+    author_email = 'thruflo@googlemail.com',
+    url = 'http://github.com/thruflo/close.consumer',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Programming Language :: Python'
+    ],
+    license = 'Creative Commons CC0 1.0 Universal',
+    packages = ['close.consumer'],
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe = False,
+    namespace_packages = [
+        'close'
+    ],
+    install_requires=[
+        'greenlet==0.2',
+        'gevent==0.12.2',
+        #'redis==1.34.1'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'close-consume = close.consumer.consumer:main',
+            'close-process = close.consumer.process:main'
+        ]
+    }
+)
