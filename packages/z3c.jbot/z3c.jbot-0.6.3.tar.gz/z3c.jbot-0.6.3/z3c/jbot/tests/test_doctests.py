f89ddda35165e3c8e1b29bf0266a40167e190c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:27 +0000

    Add package z3c.jbot/0.6.3/z3c.jbot-0.6.3.tar.gz

---
+++ packages/z3c.jbot/z3c.jbot-0.6.3.tar.gz/z3c.jbot-0.6.3/z3c/jbot/tests/test_doctests.py
@@ -0,0 +1,31 @@
+import zope.interface
+import zope.component
+import zope.testing.doctest
+import unittest
+
+OPTIONFLAGS = (zope.testing.doctest.ELLIPSIS |
+               zope.testing.doctest.NORMALIZE_WHITESPACE)
+
+import zope.component.testing
+
+def setUp(test):
+    zope.component.testing.setUp(test)
+    import z3c.jbot.patches
+
+def test_suite():
+    globs = dict(
+        interface=zope.interface,
+        component=zope.component)
+
+    return unittest.TestSuite((
+        zope.testing.doctest.DocFileSuite(
+            'README.txt',
+            optionflags=OPTIONFLAGS,
+            setUp=setUp,
+            tearDown=zope.component.testing.tearDown,
+            globs=globs,
+            package="z3c.jbot"),
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
