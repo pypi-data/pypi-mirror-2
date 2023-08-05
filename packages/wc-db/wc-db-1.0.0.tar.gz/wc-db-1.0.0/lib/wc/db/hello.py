56289814b0951d6a7e7ec2f4317c674d18f8bf67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:40 +0000

    Add package wc-db/1.0.0/wc-db-1.0.0.tar.gz

---
+++ packages/wc-db/wc-db-1.0.0.tar.gz/wc-db-1.0.0/lib/wc/db/hello.py
@@ -0,0 +1,4 @@
+from wc import socket
+
+def main():
+    socket.write("wc.db.hello.main: hello world")
---
+++ packages/wc-db/wc-db-1.0.0.tar.gz/wc-db-1.0.0/setup.py
@@ -0,0 +1,88 @@
+"""
+WidgetCo - db which depends on wc-socket.
+
+This is demonstrating how to create namespace packages.
+
+See my blog article which explains their usage here: 
+
+    http://www.sourceweaver.com/posts/python-namespace-packages
+
+This should eggify and in theory upload to pypi without problems.
+
+This is released under the BSD license.
+
+Oisin Mulvihill
+2010-05-31
+
+"""
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+Name='wc-db'
+ProjecUrl=""
+Version='1.0.0'
+Author='Oisin Mulvihill'
+AuthorEmail='oisinmulvihill at gmail dot com'
+Maintainer=' Oisin Mulvihill'
+Summary='WidgetCo db: This is a fake web module used to teach namespace packages ( http://www.sourceweaver.com/posts/python-namespace-packages).'
+License='BSD License'
+ShortDescription=Summary
+Description=Summary
+
+needed = [
+    'wc-socket==1.0.0',
+]
+
+
+# Include everything under package dir. I needed to add a __init__.py
+# to each directory inside package. I did this using the following
+# handy command:
+#
+#  find lib/wc -type d -exec touch {}//__init__.py \;
+#
+# If new directories are added then I'll need to rerun this command.
+#
+EagerResources = [
+    'wc',
+]
+
+ProjectScripts = [
+    'scripts/rundb',
+]
+
+PackageData = {
+    '': ['*.*'],
+}
+
+# Make exe versions of the scripts:
+EntryPoints = {
+    'console_scripts': [
+        'rundb =wc.db.hello:main',
+    ]
+}
+
+setup(
+#    url=ProjecUrl,
+    name=Name,
+    zip_safe=False,
+    version=Version,
+    author=Author,
+    author_email=AuthorEmail,
+    description=ShortDescription,
+    long_description=Description,
+    license=License,
+    scripts=ProjectScripts,
+    install_requires=needed,
+    include_package_data=True,
+    packages=find_packages('lib'),
+    package_data=PackageData,
+    package_dir = {'': 'lib'},
+    eager_resources = EagerResources,
+    entry_points = EntryPoints,
+    namespace_packages = ['wc'],
+)
+
