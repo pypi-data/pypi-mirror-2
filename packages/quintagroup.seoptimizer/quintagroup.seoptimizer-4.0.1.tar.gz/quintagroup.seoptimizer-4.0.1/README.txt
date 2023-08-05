00f5fd77d0f5fc856bedcbadf14dfd1fe0da8394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:47 +0000

    Add package quintagroup.seoptimizer/4.0.1/quintagroup.seoptimizer-4.0.1.tar.gz

---
+++ packages/quintagroup.seoptimizer/quintagroup.seoptimizer-4.0.1.tar.gz/quintagroup.seoptimizer-4.0.1/README.txt
@@ -0,0 +1,104 @@
+Introduction
+============
+
+quintagroup.seoptimizer was created to enhance SE visibility of Plone sites.
+
+
+Features
+--------
+
+quintagroup.seoptimizer allows per document editing of:
+
+* HTML Title tag
+
+* META description tag
+
+* META keywords tag
+
+* HTML comment into page header
+
+* META robots tag
+
+* META Disposition tag
+
+* Canonical URl http://projects.quintagroup.com/products/wiki/qSEOptimizer#CanonicalURL
+
+For Title, description keywords and comment you have statistics
+(total/stop/used words, field length counter).
+
+IMPORTANT
+---------
+
+**Starting from 3.0 release - quintagroup.seoptimizer package does not need 'overrides.zcml' file. So please remove 'quintagroup.seoptimizer-overrides' line from your buildout's ZCML area.**
+  
+Usage
+-----
+
+* Go to Plone Control Panel, enable Plone SEO for desired content types
+
+* Go to a document (Blog entry, news item, event, etc)
+
+* Switch to *SEO Properties* tab
+
+* Select *Override* checkboxes of features you want to override
+
+* Type-in your SEO values
+
+* Save changes
+
+* Do this for all documents that need enhanced SEO properties
+
+
+Requirements
+------------
+
+* Plone 4 
+
+Notes
+-----
+
+* For Plone 4 versions - use Plone SEO 4.0 release and up http://plone.org/products/plone-seo/releases/4.0. In your buildout.cfg file's egg section set product version::
+
+   [buildout]
+   ....
+   eggs =
+        ...
+        quintagroup.seoptimizer >=4.0
+
+* For Plone 3 versions - use the latest Plone SEO 3.x release http://plone.org/products/plone-seo/releases/3.0.5. In your buildout.cfg file's egg section set product version::
+
+   [buildout]
+   ....
+   eggs =
+        ...
+        quintagroup.seoptimizer >3.0,<4.0
+
+
+* For Plone 2.x versions - use Plone SEO 1.7.1  release http://plone.org/products/plone-seo/releases/1.7.1
+
+
+Links
+-----
+
+Watch Plone SEO screencast http://quintagroup.com/cms/screencasts/plone-seo to learn how to install and set up Plone SEO on a buildout-based Plone instance for Plone 3.2 or above.
+
+
+Authors
+-------
+
+* Myroslav Opyr
+
+* Andriy Mylenkyy
+
+* Volodymyr Cherepanyak
+
+* Vitaliy Podoba
+
+* Taras Melnychuk
+
+* Mykola Kharechko
+
+* Vitaliy Stepanov
+
+* Volodymyr Romaniuk
+
---
+++ packages/quintagroup.seoptimizer/quintagroup.seoptimizer-4.0.1.tar.gz/quintagroup.seoptimizer-4.0.1/quintagroup/seoptimizer/tests/base.py
@@ -0,0 +1,102 @@
+"""Test setup for integration and functional tests.
+
+When we import PloneTestCase and then call setupPloneSite(), all of
+Plone's products are loaded, and a Plone site will be created. This
+happens at module level, which makes it faster to run each test, but
+slows down test runner startup.
+"""
+import re
+import transaction
+from zope.component import getUtility
+
+# Starting from plone.memoize v1.1a4 (used in plone4), global ram cache
+# utility provides other IRAMCache interface, than before
+try:
+    # In plone4 provides
+    from zope.ramcache.interfaces.ram import IRAMCache
+except ImportError:
+    # In plone3 provides
+    from zope.app.cache.interfaces.ram import IRAMCache
+
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+
+from Testing import ZopeTestCase as ztc
+
+from Products.CMFCore.utils import getToolByName
+
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase import setup as ptc_setup
+
+from Products.PloneTestCase.PloneTestCase import portal_owner
+from Products.PloneTestCase.PloneTestCase import default_password
+
+from quintagroup.seoptimizer.config import PROJECT_NAME
+from quintagroup.seoptimizer.config import SUPPORT_BLAYER
+
+ptc.setupPloneSite()
+
+class NotInstalled(PloneSite):
+    """ Only package register, without installation into portal
+    """
+
+    @classmethod
+    def setUp(cls):
+        fiveconfigure.debug_mode = True
+        import quintagroup.seoptimizer
+        zcml.load_config('configure.zcml', quintagroup.seoptimizer)
+        fiveconfigure.debug_mode = False
+        ztc.installPackage(PROJECT_NAME)
+
+
+class Installed(NotInstalled):
+    """ Install product into the portal
+    """
+    @classmethod
+    def setUp(cls):
+        app = ztc.app()
+        portal = app[ptc_setup.portal_name]
+
+        # Sets the local site/manager
+        ptc_setup._placefulSetUp(portal)
+        # Install PROJECT
+        qi = getattr(portal, 'portal_quickinstaller', None)
+        qi.installProduct(PROJECT_NAME)
+        transaction.commit()
+
+    @classmethod
+    def tearDown(cls):
+        ptc_setup._placefulTearDown()
+        
+
+class MixinTestCase:
+
+    def _getauth(self):
+        # Fix authenticator for the form
+        import re
+        try:
+            authenticator = self.portal.restrictedTraverse("@@authenticator")
+        except:
+            handle  = ""
+        else:
+            html = authenticator.authenticator()
+            handle = re.search('value="(.*)"', html).groups()[0]
+        return handle
+
+    def beforeTearDown(self):
+        getUtility(IRAMCache).invalidateAll()
+
+
+class TestCase(MixinTestCase, ptc.PloneTestCase):
+    layer = Installed
+
+class TestCaseNotInstalled(MixinTestCase, ptc.PloneTestCase):
+    layer = NotInstalled
+
+
+class FunctionalTestCase(MixinTestCase, ptc.FunctionalTestCase):
+    layer = Installed
+
+class FunctionalTestCaseNotInstalled(MixinTestCase, ptc.FunctionalTestCase):
+    layer = NotInstalled
