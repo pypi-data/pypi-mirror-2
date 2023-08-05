9d6260567f9c14bd1948e9c4c7d29b12ac8aaba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:43 +0000

    Add package sniffer/0.1.1/sniffer-0.1.1.tar.gz

---
+++ packages/sniffer/sniffer-0.1.1.tar.gz/sniffer-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from sniffer import __version__, __author__, __author_email__
+
+setup(
+    name='sniffer',
+    version=__version__,
+    description='An automatic test runner. Supports nose out of the box.',
+    long_description=open('README.rst').read(),
+    author=__author__,
+    author_email=__author_email__,
+    url='http://github.com/jeffh/sniffer/',
+    requires=[
+        'termstyle',
+    ],
+    entry_points = {
+        'console_scripts': ['sniffer = sniffer:main'],
+    },
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
