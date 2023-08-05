ab02a446da92918f25bbf3e9f0de3d3c138b53b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:00 +0000

    Add package Supay/0.0.5/Supay-0.0.5.tar.gz

---
+++ packages/Supay/Supay-0.0.5.tar.gz/Supay-0.0.5/setup.py
@@ -0,0 +1,30 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "Supay",
+    version = "0.0.5",
+    packages = find_packages(),
+    scripts = ['supay.py'],
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt'],
+        },
+
+    # metadata for upload to PyPI
+    author = "Alfredo Deza",
+    author_email = "alfredodeza [at] gmail [dot] com",
+    description = "A daemonizing library for Python.",
+    long_description = """\
+ An effective way to daemonize a Python script with some added
+ functionality like spawning a new child process and status verification
+ """,
+
+    license = "MIT",
+    py_modules = ['supay',],
+    keywords = "daemon daemonize start stop status spawn process processses",
+    url = "http://code.google.com/p/supay",   
+
+)
+
