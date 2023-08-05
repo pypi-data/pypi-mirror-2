d0b1ed6b30e12f0bd59a6185ff0cbfd95060ba5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:44 +0000

    Add package Mezzanine/0.2.1/Mezzanine-0.2.1.tar.gz

---
+++ packages/Mezzanine/Mezzanine-0.2.1.tar.gz/Mezzanine-0.2.1/mezzanine/forms/admin.py
@@ -0,0 +1,30 @@
+
+from copy import deepcopy
+
+from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+from mezzanine.core.admin import OrderableAdmin
+from mezzanine.forms.models import Form, Field
+from mezzanine.pages.admin import PageAdmin
+
+
+form_fieldsets = deepcopy(PageAdmin.fieldsets)
+form_fieldsets[0][1]["fields"] += ("response", "send_email")
+FormMedia = deepcopy(OrderableAdmin.Media)
+FormMedia.js = PageAdmin.Media.js + [js for js in FormMedia.js if js not in 
+    PageAdmin.Media.js]
+
+class FieldAdmin(admin.TabularInline):
+    model = Field
+   
+class FormAdmin(PageAdmin, OrderableAdmin):
+
+    class Media(FormMedia):
+        pass
+
+    inlines = (FieldAdmin,)
+    fieldsets = form_fieldsets
+    
+admin.site.register(Form, FormAdmin)
