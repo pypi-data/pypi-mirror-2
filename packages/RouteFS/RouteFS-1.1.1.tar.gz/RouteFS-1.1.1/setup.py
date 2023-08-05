d2461d161b2c890a1895ee9f88bd0dfb40e27c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package RouteFS/1.1.1/RouteFS-1.1.1.tar.gz

---
+++ packages/RouteFS/RouteFS-1.1.1.tar.gz/RouteFS-1.1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="RouteFS",
+    version="1.1.1",
+    description="RouteFS: A FUSE API wrapper based on URL routing",
+    author="Evan Broder",
+    author_email="broder@mit.edu",
+    url="http://github.com/ebroder/python-routefs/wikis",
+    license="MPL, GPL",
+    packages=find_packages(),
+    install_requires=['fuse_python>=0.2a', 'Routes>=1.9'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
+        'License :: DFSG approved',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python'
+        ]
+)
