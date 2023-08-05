423ff4b666ac8eb2a01f8eaaacba5aa284e5b0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package skeleton/0.5/skeleton-0.5.tar.gz

---
+++ packages/skeleton/skeleton-0.5.tar.gz/skeleton-0.5/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# Bootstrap installation of Distribute
+import distribute_setup
+distribute_setup.use_setuptools()
+
+import sys
+
+from setuptools import setup
+
+PROJECT = 'skeleton'
+VERSION = '0.5'
+URL = 'http://github.com/dinoboff/skeleton'
+AUTHOR = 'Damien Lebrun'
+AUTHOR_EMAIL = 'dinoboff@gmail.com'
+DESC = "Basic Template system for project skeleton."
+
+EXTRAS = {}
+
+if sys.version_info > (3,):
+    EXTRAS['use_2to3'] = True
+
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+    description=DESC,
+    long_description=open('README.rst').read(),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license='BSD',
+    packages=['skeleton', 'skeleton.tests', 'skeleton.examples'],
+    test_suite='skeleton.tests',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    extras_require={
+        'virtualenv-templates':  [
+            'virtualenvwrapper>=2.1.1',
+            'virtualenvwrapper.project>=1.0'
+            ],
+    },
+    entry_points={
+        'virtualenvwrapper.project.template': [
+            'package = skeleton.examples.basicpackage:virtualenv_warpper_hook',
+            ],
+        },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        ],
+    **EXTRAS
+)
