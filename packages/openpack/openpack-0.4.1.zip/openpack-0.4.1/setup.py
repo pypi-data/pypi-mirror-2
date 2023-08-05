dff976a61dff3a1ef48529590102d55b6d5db5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package openpack/0.4.1/openpack-0.4.1.zip

---
+++ packages/openpack/openpack-0.4.1.zip/openpack-0.4.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+	name="openpack",
+	author="Christian Wyglendowski (YouGov), Jason R. Coombs (YouGov)",
+	version="0.4.1",
+	packages=['openpack'],
+	install_requires=[
+		'lxml',
+	],
+	tests_require=[
+		'py.test>=1.0',
+	],
+	entry_points = {
+		'console_scripts': [
+			'part-edit = openpack.editor:part_edit_cmd',
+			'zip-listdir = openpack.editor:pack_dir_cmd',
+			],
+	},
+)
