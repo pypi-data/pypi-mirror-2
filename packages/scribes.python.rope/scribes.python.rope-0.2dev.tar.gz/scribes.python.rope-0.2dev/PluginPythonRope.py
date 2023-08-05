1ee51bb86487836d97f6b874eb7393056b8c4f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package scribes.python.rope/0.2dev/scribes.python.rope-0.2dev.tar.gz

---
+++ packages/scribes.python.rope/scribes.python.rope-0.2dev.tar.gz/scribes.python.rope-0.2dev/PluginPythonRope.py
@@ -0,0 +1,13 @@
+name = "Python rope autocomplete"
+authors = ["Anton Bobrov <bobrov@vl.ru>"]
+languages = ["python"]
+version = 0.1
+autoload = True
+class_name = "PythonRopePlugin"
+short_description = "Autocompletes python code"
+long_description = """This plugin uses rope refactoring library
+to autocomlete python code"""
+
+from scribes.helpers import bootstrap
+
+PythonRopePlugin = bootstrap('scribes.python.rope.Plugin')
