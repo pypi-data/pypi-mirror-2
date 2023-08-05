94ab7693178dcff1a90fa5e99cf964bc10f8d72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package pyf.dataflow/1.1/pyf.dataflow-1.1.tar.gz

---
+++ packages/pyf.dataflow/pyf.dataflow-1.1.tar.gz/pyf.dataflow-1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='pyf.dataflow',
+      version=version,
+      description="Core DataFlow system of PyF framework",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta"
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pyf'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'pyjon.utils',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
