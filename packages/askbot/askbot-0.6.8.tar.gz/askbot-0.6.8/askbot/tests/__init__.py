495e3783b5aa11411286c75997a9531d33946179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:14 +0000

    Add package askbot/0.6.8/askbot-0.6.8.tar.gz

---
+++ packages/askbot/askbot-0.6.8.tar.gz/askbot-0.6.8/askbot/tests/__init__.py
@@ -0,0 +1,5 @@
+from askbot.tests.email_alert_tests import *
+from askbot.tests.on_screen_notification_tests import *
+from askbot.tests.page_load_tests import *
+from askbot.tests.permission_assertion_tests import *
+from askbot.tests.db_api_tests import *
