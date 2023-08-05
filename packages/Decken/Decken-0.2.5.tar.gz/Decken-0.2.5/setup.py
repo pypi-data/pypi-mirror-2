f5720dd7bcfd740e92ba3a3587e73a8dc302cb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package Decken/0.2.5/Decken-0.2.5.tar.gz

---
+++ packages/Decken/Decken-0.2.5.tar.gz/Decken-0.2.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from glob import glob
+import os
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+	name='Decken',
+	version='0.2.5',
+	install_requires=["BeautifulSoup<3.1.0.1"],
+	package_data = {'decken': ['*.glade']},
+	packages = find_packages(),
+	#entry_points = {
+	#	'gui_scripts' : [ 'decken=decken_script:command']
+	#},
+	scripts = ['bin/decken_script', 'bin/decken_n800'],
+	
+	
+	author='Claudio Torcato',
+	author_email='claudiotorcato@gmail.com',
+	description='Deck Organizer to Trading Card Games',
+	long_description= open('README.txt').read(),
+	license='LICENSE.txt',
+	keywords = "tcg mtg magic deck card python gnome linux mac n800"
+)
