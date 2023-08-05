1b12edf797c0fc8920af31af1de36b1d36bf3213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package z3c.sampledata/0.3.1/z3c.sampledata-0.3.1.tar.gz

---
+++ packages/z3c.sampledata/z3c.sampledata-0.3.1.tar.gz/z3c.sampledata-0.3.1/setup.py
@@ -0,0 +1,81 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id:$
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup (
+    name='z3c.sampledata',
+    version='0.3.1',
+    author = "Stephan Richter and the Zope Community",
+    author_email = "zope-dev@zope.org",
+    description = "Sampledata Generator",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        'Detailed Documentation\n'
+        '**********************'
+        + '\n\n' +
+        read('src', 'z3c', 'sampledata', 'README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 z3c sampledata",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.sampledata',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = [
+            'zope.app.testing',
+            'zope.app.zcmlfiles',
+            ],
+        ),
+    install_requires = [
+        'setuptools',
+        'ZODB3',
+        'zope.component',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.intid',
+        'zope.lifecycleevent',
+        'zope.schema',
+        'zope.site',
+        'zope.traversing',
+        'zope.viewlet',
+        'zope.app.pagetemplate>=3.6.0',
+        'zope.app.authentication>=3.7.0',
+        'zope.app.component',
+        'zope.app.security',
+        ],
+    zip_safe = False,
+)
