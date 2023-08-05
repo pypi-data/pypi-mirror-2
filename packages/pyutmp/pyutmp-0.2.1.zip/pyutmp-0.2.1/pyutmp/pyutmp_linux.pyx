76bc0f09a37a53fdbbc45704df76d67633fbe825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:15 +0000

    Add package pyutmp/0.2.1/pyutmp-0.2.1.zip

---
+++ packages/pyutmp/pyutmp-0.2.1.zip/pyutmp-0.2.1/pyutmp/pyutmp_linux.pyx
@@ -0,0 +1,126 @@
+#                                                               -*-python-*-
+# Cython input file for pyutmp_sysv module.
+#
+# $Id: 29246639589846ae6cc53ac259b317e22cd5e958 $
+# ---------------------------------------------------------------------------
+
+cdef extern from "time.h":
+    struct timeval:
+        long tv_sec
+        long tv_usec
+
+cdef extern from "utmp.h":
+    struct exit_status:
+        short e_termination
+        short e_exit
+
+    ctypedef int pid_t
+
+    cdef enum:
+         UT_LINESIZE = 1
+         UT_NAMESIZE = 1
+         UT_HOSTSIZE = 1
+         UT_UNKNOWN = 0
+         RUN_LVL = 1
+         BOOT_TIME = 2
+         NEW_TIME = 3
+         OLD_TIME = 4
+         INIT_PROCESS = 5
+         LOGIN_PROCESS = 6
+         USER_PROCESS = 7
+         DEAD_PROCESS = 8
+         ACCOUNTING = 9
+
+    struct utmp:
+        short ut_type
+        pid_t ut_pid
+        char ut_line[UT_LINESIZE]
+        char ut_id[4]
+        char ut_user[UT_NAMESIZE]
+        char ut_host[UT_HOSTSIZE]
+        exit_status ut_exit
+        long ut_session
+        timeval ut_tv
+        int ut_addr_v6[4]
+
+    void setutent()
+    void endutent()
+    utmp *getutent()
+    utmp *getutline(utmp *u)
+    void utmpname(char *path)
+
+_TYPE_MAP = {RUN_LVL: 'RUN_LVL',
+             BOOT_TIME: 'BOOT_TIME',
+             NEW_TIME: 'NEW_TIME',
+             OLD_TIME: 'OLD_TIME',
+             INIT_PROCESS: 'INIT_PROCESS',
+             LOGIN_PROCESS: 'LOGIN_PROCESS',
+             USER_PROCESS: 'USER_PROCESS',
+             DEAD_PROCESS: 'DEAD_PROCESS',
+             ACCOUNTING: 'ACCOUNTING'}
+
+class Utmp(object):
+    ut_type = 'UT_UNKNOWN'
+    ut_pid = None
+    ut_line = None
+    ut_id = None
+    ut_user = None
+    ut_host = None
+    ut_exit_code = 0
+    ut_session = None
+    ut_time = None
+    ut_addr = None
+    ut_user_process = False
+
+class _UtmpFile(object):
+    def __init__(self, path=None):
+        self._is_open = False
+        if path:
+            utmpname(path)
+
+    def __del__(self):
+        if self._is_open:
+            endutent()
+
+    def rewind(self):
+        setutent()
+
+    def _get_next_entry(self):
+        if not self._is_open:
+            setutent()
+            self._is_open = True
+
+        cdef utmp *entry = getutent()
+        if entry:
+            u = Utmp()
+            u.ut_type = _TYPE_MAP[entry.ut_type]
+            u.ut_user_process = (u.ut_type == 'USER_PROCESS')
+            u.ut_pid = entry.ut_pid
+            u.ut_id = entry.ut_id
+            u.ut_user = entry.ut_user
+            u.ut_exit_code = entry.ut_exit.e_exit
+            u.ut_time = float(entry.ut_tv.tv_sec)
+            u.ut_addr = entry.ut_addr_v6[0]
+
+            if len(entry.ut_user) > 0:
+                u.ut_user = entry.ut_user
+            else:
+                u.ut_user = None
+
+            if len(entry.ut_line) == 0:
+                u.ut_line = None
+            else:
+                u.ut_line = entry.ut_line
+                if not u.ut_line.startswith('/'):
+                    u.ut_line = '/dev/' + u.ut_line
+
+            if len(entry.ut_host) > 0:
+                u.ut_host = entry.ut_host
+            else:
+                u.ut_host = None
+
+        else:
+            u = None
+            endutent()
+
+        return u
---
+++ packages/pyutmp/pyutmp-0.2.1.zip/pyutmp-0.2.1/setup.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+#
+# Distutils setup script for pyutmp
+#
+# $Id: d4ce49e116e8196db08c38c9554f105dd9bc42e2 $
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+
+from distutils.core import setup, Extension
+import os
+import sys
+import imp
+
+# ---------------------------------------------------------------------------
+# Constants
+# ---------------------------------------------------------------------------
+
+DESCRIPTION = 'Python UTMP wrapper for Un*x systems'
+HERE = os.path.dirname(os.path.abspath(__file__))
+PACKAGE = 'pyutmp'
+
+sys.path = [HERE] + sys.path
+
+# ---------------------------------------------------------------------------
+# Functions 
+# ---------------------------------------------------------------------------
+
+def run_command(cmd):
+    print '+ %s' % cmd
+    return os.system(cmd)
+
+def die(msg):
+    print >> sys.stderr, msg
+    sys.exit(1)
+
+# ---------------------------------------------------------------------------
+# Initialization
+# ---------------------------------------------------------------------------
+
+# Get some info from the module itself.
+
+mf = os.path.join(HERE, 'pyutmp', '__init__.py')
+os.environ['__IN_SETUP_PY'] = 'True'
+m = imp.load_module('pyutmp', open(mf), mf,
+                    ('__init__.py', 'r', imp.PY_SOURCE))
+platform = m._get_platform()
+long_description = m.__doc__
+
+# Scrub the long description, for PyPI. It uses normal reStructuredText;
+# we have to remove some epydoc-isms.
+
+lines = long_description.split('\n')
+lines2 = []
+for line in lines:
+    if line.startswith('.. python::'):
+        lines2.append('::')
+        continue
+
+    line = line.replace('``', '%%--%%').replace('`', '').replace('%%--%%', '``')
+    lines2.append(line)
+
+long_description = '\n'.join(lines2)
+del lines
+del lines2
+
+# Determine whether the C file is there. If not, build it. If it's older than
+# the pyx file, build it.
+
+os.chdir('pyutmp')
+c_file = 'pyutmp_%s.c' % platform
+pyx_file = 'pyutmp_%s.pyx' % platform
+ext_module = 'pyutmp_%s' % platform
+ext_package = os.path.join(PACKAGE, ext_module)
+
+build_c_file = False
+if not os.access(c_file, os.F_OK):
+    build_c_file = True
+    print 'Can\'t find "%s". Attempting to make it.' % c_file
+else:
+    st_c = os.stat(c_file)
+    st_pyx = os.stat(pyx_file)
+    if st_c.st_mtime < st_pyx.st_mtime:
+        print '"%s" is newer than "%s". Rebuilding "%s".' %\
+              (pyx_file, c_file, c_file)
+        build_c_file = True
+if build_c_file:
+    rc = run_command('cython %s' % pyx_file)
+    if rc != 0:
+        die('Failed to build "%s" from "%s"' % (c_file, pyx_file))
+os.chdir(HERE)
+
+# ---------------------------------------------------------------------------
+# Setup
+# ---------------------------------------------------------------------------
+
+setup(
+    name='pyutmp',
+    packages = [PACKAGE],
+    version='0.2.1',
+    description=DESCRIPTION,
+    long_description=long_description,
+    url='http://bmc.github.com/pyutmp/',
+    license='BSD license',
+    author='Brian M. Clapper',
+    author_email='bmc@clapper.org',
+    py_modules=['pyutmp'],
+    ext_modules=[Extension(ext_package,
+                           [os.path.join(PACKAGE, c_file)],)],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
