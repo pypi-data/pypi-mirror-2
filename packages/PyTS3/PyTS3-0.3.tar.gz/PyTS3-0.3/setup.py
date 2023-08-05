b08f17b9d09b750e88dee1a586d53ec697471570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package PyTS3/0.3/PyTS3-0.3.tar.gz

---
+++ packages/PyTS3/PyTS3-0.3.tar.gz/PyTS3-0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="PyTS3",
+    version="0.3",
+    description="""A python modul to use the Teamspeak Query Interface.""",
+    author="Christoph Heer",
+    author_email="Christoph.Heer@googlemail.com",
+    license="MIT License",
+    url="http://bitbucket.org/ChristophHeer/pyts3/",
+    py_modules=["PyTS3"])
