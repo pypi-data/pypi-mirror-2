f14249887482745d1a20bbbe1bf97c3509c10210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:35 +0000

    Add package pyppd/0.1.6/pyppd-0.1.6.tar.gz

---
+++ packages/pyppd/pyppd-0.1.6.tar.gz/pyppd-0.1.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pyppd',
+    version='0.1.6',
+    author='Vitor Baptista',
+    author_email='vitor@vitorbaptista.com',
+    packages=['pyppd'],
+    package_data={'pyppd': ['*.in']},
+    scripts=['bin/pyppd'],
+    url='http://gitorious.org/vitorbaptista/pyppd/',
+    license='GPLv3',
+    description='A CUPS PostScript Printer Driver\'s compressor and generator',
+    long_description=open('README.txt').read(),
+)
