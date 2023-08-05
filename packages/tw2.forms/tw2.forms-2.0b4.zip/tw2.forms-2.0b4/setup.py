9fe22b34636f9eaed4030af6d9bcab37d95f1052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:21 +0000

    Add package tw2.forms/2.0b4/tw2.forms-2.0b4.zip

---
+++ packages/tw2.forms/tw2.forms-2.0b4.zip/tw2.forms-2.0b4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tw2.forms',
+    version='2.0b4',
+    description='The basic form widgets for ToscaWidgets 2.',
+    author='Paul Johnston, Christopher Perkins & contributors',
+    author_email='paj@pajhome.org.uk',
+    url='http://toscawidgets.org/docs/tw2.core/',
+    install_requires=[
+        "tw2.core>=2.0b4",
+        ],
+    packages=find_packages(exclude=['ez_setup', 'tests']),
+    namespace_packages = ['tw2'],
+    zip_safe=False,
+    include_package_data=True,
+    test_suite = 'nose.collector',
+    entry_points="""
+        [tw2.widgets]
+        # Register your widgets so they can be listed in the WidgetBrowser
+        tw2.forms = tw2.forms
+    """,
+    keywords = [
+        'toscawidgets.widgets',
+    ],
+    tests_require = ['BeautifulSoup'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: ToscaWidgets',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Widget Sets',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
