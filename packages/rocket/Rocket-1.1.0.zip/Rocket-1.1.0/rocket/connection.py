c72ebfd36db33793c78986fa289a97078316f7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:57 +0000

    Add package rocket/1.1.0/Rocket-1.1.0.zip

---
+++ packages/rocket/Rocket-1.1.0.zip/Rocket-1.1.0/rocket/connection.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of the Rocket Web Server
+# Copyright (c) 2009 Timothy Farrell
+
+# Import System Modules
+import time
+import socket
+try:
+    import ssl
+    has_ssl = True
+except ImportError:
+    has_ssl = False
+# Import Package Modules
+from . import PY3K
+
+class Connection:
+    def __init__(self, sock_tuple, port, secure=False):
+        self.client_addr, self.client_port = sock_tuple[1]
+        self.server_port = port
+        self.socket = sock_tuple[0]
+        self.start_time = time.time()
+        self.ssl = has_ssl and isinstance(self.socket, ssl.SSLSocket)
+        self.secure = secure
+
+        for x in dir(self.socket):
+            if not hasattr(self, x):
+                try:
+                    self.__dict__[x] = self.socket.__getattribute__(x)
+                except:
+                    pass
+
+    def close(self):
+        if hasattr(self.socket, '_sock'):
+            try:
+                self.socket._sock.close()
+            except socket.error:
+                info = sys.exc_info()
+                if info[1].errno != socket.EBADF:
+                    raise info[1]
+                else:
+                    pass
+        self.socket.close()
---
+++ packages/rocket/Rocket-1.1.0.zip/Rocket-1.1.0/rocket/monitor.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of the Rocket Web Server
+# Copyright (c) 2009 Timothy Farrell
+
+# Import System Modules
+import time
+import logging
+import select
+try:
+    from queue import Queue
+except ImportError:
+    from Queue import Queue
+from threading import Thread
+# Import Package Modules
+from . import IS_JYTHON, POLL_TIMEOUT
+
+class Monitor(Thread):
+    # Monitor worker base class.
+    queue = Queue() # Holds connections to be monitored
+    connections = set()
+    timeout = 0
+
+    def run(self):
+        self.name = self.getName()
+        self.log = logging.getLogger('Rocket.Monitor')
+        try:
+            self.log.addHandler(logging.NullHandler())
+        except:
+            pass
+
+        self.log.debug('Entering monitor loop.')
+
+        # Enter thread main loop
+        while True:
+            # Move the queued connections to the selection pool
+            while not self.queue.empty() or not len(self.connections):
+                self.log.debug('In "receive timed-out connections" loop.')
+                c = self.queue.get()
+
+                if not c:
+                    # A non-client is a signal to die
+                    self.log.debug('Received a death threat.')
+                    self.stop()
+                    return
+
+                self.log.debug('Received a timed out connection.')
+
+                assert(c not in self.connections)
+
+                if IS_JYTHON:
+                    # Jython requires a socket to be in Non-blocking mode in
+                    # order to select on it.
+                    c.setblocking(False)
+
+                self.log.debug('Adding connection to monitor list.')
+                self.connections.add(c)
+
+            # Wait on those connections
+            self.log.debug('Blocking on connections')
+            readable = select.select(list(self.connections),
+                                     [], [], POLL_TIMEOUT)[0]
+
+            # If we have any readable connections, put them back
+            for r in readable:
+                self.log.debug('Restoring readable connection')
+
+                if IS_JYTHON:
+                    # Jython requires a socket to be in Non-blocking mode in
+                    # order to select on it, but the rest of the code requires
+                    # that it be in blocking mode.
+                    r.setblocking(True)
+
+                r.start_time = time.time()
+                self.out_queue.put(r)
+                self.connections.remove(r)
+
+            # If we have any stale connections, kill them off.
+            if self.timeout:
+                now = time.time()
+                stale = set()
+                for c in self.connections:
+                    if now - c.start_time >= self.timeout:
+                        stale.add(c)
+
+                for c in stale:
+                    data = (c.client_addr, c.server_port, '*' if c.ssl else '')
+                    self.log.debug('Flushing stale connection: %s:%i%s' % data)
+                    self.connections.remove(c)
+                    try:
+                        c.close()
+                    finally:
+                        del c
+
+    def stop(self):
+        self.log.debug('Flushing waiting connections')
+        for c in self.connections:
+            try:
+                c.close()
+            finally:
+                del c
+
+        self.log.debug('Flushing queued connections')
+        while not self.queue.empty():
+            c = self.queue.get()
+            try:
+                c.close()
+            finally:
+                del c
