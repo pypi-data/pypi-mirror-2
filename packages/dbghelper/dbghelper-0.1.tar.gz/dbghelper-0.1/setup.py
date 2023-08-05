41e27142597cab3d7c26bdf3d8c39a5e0b0d4510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package dbghelper/0.1/dbghelper-0.1.tar.gz

---
+++ packages/dbghelper/dbghelper-0.1.tar.gz/dbghelper-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0.1'
+
+setup(
+    name="dbghelper",
+    version=VERSION,
+    author = "David Fraser",
+    author_email = 'davidf@sjsoft.com',
+    url = 'http://bitbucket.org/davidfraser/python-dbghelper',
+    description = 'Simple helper for inline winpdb/pdb debugging',
+    license='BSD',
+    py_modules=['dbg'],
+    install_requires = ['winpdb'],
+)
+
