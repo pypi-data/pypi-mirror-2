75089671231eff16317727a271539322bffbe7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package cubicweb-testcard/0.2.2/cubicweb-testcard-0.2.2.tar.gz

---
+++ packages/cubicweb-testcard/cubicweb-testcard-0.2.2.tar.gz/cubicweb-testcard-0.2.2/hooks.py
@@ -0,0 +1,46 @@
+from itertools import chain
+
+from cubicweb.selectors import implements
+from cubicweb.server import hook
+
+class MakeTestInstancesOnVersionStatusChange(hook.Hook):
+    """when a Version is going from 'planned' to 'in development', search
+    for related test cards and create a test instance for each of them
+    """
+    __regid__ = 'createtestinstances'
+    __select__ = hook.Hook.__select__ & implements('TrInfo')
+    events = ('after_add_entity',)
+
+    def __call__(self):
+        forentity = self.entity.for_entity
+        if forentity.e_schema  != 'Version':
+            return
+        if self.entity.new_state.name == 'dev':
+            self.copy_cards(forentity.eid)
+
+    def copy_cards(self, veid):
+        # get cards for past stories
+        rset1 = self._cw.execute('Any X,XT WHERE X test_case_of P, X test_case_for S, S in_state ST,'
+                                'ST name "done", V version_of P, V eid %(v)s, X title XT,'
+                                'NOT EXISTS(TI instance_of X, TI for_version V)',
+                               {'v': veid})
+        # get cards for stories in this version
+        rset2 = self._cw.execute('Any X,XT WHERE X test_case_of P, X test_case_for S,'
+                                'S done_in V, V eid %(v)s, X title XT,'
+                                'NOT EXISTS(TI instance_of X, TI for_version V)',
+                               {'v': veid})
+        # get general cards (not related to a particular story
+        rset3 = self._cw.execute('Any X,XT WHERE X test_case_of P, NOT X test_case_for S,'
+                                'V version_of P, V eid %(v)s, X title XT,'
+                                'NOT EXISTS(TI instance_of X, TI for_version V)',
+                               {'v': veid})
+        done = set()
+        for eid, title in chain(rset1, rset2, rset3):
+            if eid in done:
+                continue
+            done.add(eid)
+            self._cw.execute('INSERT TestInstance X: X name %(name)s,'
+                             'X instance_of C, X for_version V '
+                             'WHERE C eid %(c)s, V eid %(v)s',
+                             {'v': veid, 'c': eid, 'name': title})
+
---
+++ packages/cubicweb-testcard/cubicweb-testcard-0.2.2.tar.gz/cubicweb-testcard-0.2.2/views.py
@@ -0,0 +1,94 @@
+"""cube-specific forms/views/actions/components
+
+:organization: Logilab
+:copyright: 2001-2009 LOGILAB S.A. (Paris, FRANCE), license is LGPL v2.
+:contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
+:license: GNU Lesser General Public License, v2.1 - http://www.gnu.org/licenses
+"""
+from cubes.tracker.views import project as tracker
+
+from cubicweb.web import uicfg, action
+from cubicweb.web.views import primary, baseviews, tabs
+from cubicweb.selectors import match_search_state, implements
+
+_afs = uicfg.autoform_section
+_afs.tag_object_of(('TestInstance', 'for_version', 'Version'), 'main', 'hidden')
+
+_pvs = uicfg.primaryview_section
+_pvs.tag_object_of(('TestInstance', 'instance_of', '*'), 'hidden')
+_pvs.tag_object_of(('*', 'test_case_of', 'Project'), 'hidden')
+
+_abaa = uicfg.actionbox_appearsin_addmenu
+_abaa.tag_subject_of(('TestInstance', 'generate_bug', 'Ticket'), True)
+_abaa.tag_subject_of(('TestInstance', 'instance_of', 'Card'), False)
+_abaa.tag_object_of(('TestInstance', 'instance_of', 'Card'), False)
+_abaa.tag_object_of(('TestInstance', 'for_version', 'Version'), False)
+_abaa.tag_object_of(('TestInstance', 'filed_under', 'Folder'), False)
+_abaa.tag_object_of(('Card', 'test_case_for', 'Ticket'), True)
+
+
+
+class TestInstancePrimaryView(primary.PrimaryView):
+    __select__ = implements('TestInstance')
+
+    def render_entity_title(self, entity):
+        title = xml_escape('%s [%s]' % (entity.name, self._cw._(entity.state)))
+        self.w(u'<h1><span class="etype">%s</span> %s</h1>'
+               % (entity.dc_type().capitalize(), title))
+
+    def render_entity_metadata(self, entity):
+        pass
+
+    def render_entity_attributes(self, entity):
+        self.w(entity.instance_of[0].view('inlined'))
+
+
+class TestInstanceOneLineView(baseviews.OneLineView):
+    """text representation of a test instance:
+
+    display title and state
+    """
+    __select__ = implements('TestInstance')
+    def cell_call(self, row, col):
+        super(TestInstanceOneLineView, self).cell_call(row, col)
+        entity = self.entity(row, col)
+        self.wdata(u' [%s]' % self._cw._(entity.state))
+
+
+class TestInstanceGenerateBugAction(action.LinkToEntityAction):
+    __regid__ = 'submitbug'
+    __select__ = match_search_state('normal') & implements('TestInstance')
+    title = _('add TestCard generate_bug Ticket subject')
+    target_etype = 'Ticket'
+    rtype = 'generate_bug'
+    role = 'subject'
+    def url(self):
+        entity = self.cw_rset.get_entity(0, 0)
+        linkto = '__linkto=concerns:%s:object' % (entity.instance_of[0].test_case_of[0].eid)
+        linkto += '&__linkto=generate_bug:%s:subject' % entity.eid
+        linkto += '&__linkto=appeared_in:%s:object' % entity.for_version[0].eid
+        return '%s&%s' % (action.LinkToEntityAction.url(self), linkto)
+
+class ProjectTestCardsView(tabs.EntityRelationView):
+    """display project's test cards"""
+    __regid__ = title = _('projecttests')
+    __select__ = tabs.EntityRelationView.__select__ & implements('Project')
+    rtype = 'test_case_of'
+    role = 'object'
+
+class ProjectTestCardsTab(ProjectTestCardsView):
+    __regid__ = 'testcards_tab'
+    __select__ = tabs.EntityRelationView.__select__ & implements('Project')
+    title = None # should not appears in possible views
+
+if 'testcards_tab' not in tracker.ProjectPrimaryView.tabs:
+    tracker.ProjectPrimaryView.tabs.append(_('testcards_tab'))
+
+class ProjectAddTestCard(action.LinkToEntityAction):
+    __select__ = (action.LinkToEntityAction.__select__ & implements('Project'))
+    __regid__ = 'addtestcard'
+    target_etype = 'Card'
+    rtype = 'test_case_of'
+    role = 'object'
+    title = _('add Card test_case_of Project object')
+    order = 123
