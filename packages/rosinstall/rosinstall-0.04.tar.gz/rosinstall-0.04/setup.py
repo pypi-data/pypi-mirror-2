9bb3382da26310ef191f1d93766ae2a4959b2f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:53 +0000

    Add package rosinstall/0.04/rosinstall-0.04.tar.gz

---
+++ packages/rosinstall/rosinstall-0.04.tar.gz/rosinstall-0.04/setup.py
@@ -0,0 +1,20 @@
+
+from distutils.core import setup
+
+setup(name='rosinstall',
+      version='0.04',
+      packages=['rosinstall', 'rosinstall.vcs'],
+      package_dir = {'':'src'},
+      scripts = ["scripts/rosinstall"],
+      author = "Tully Foote", 
+      author_email = "tfoote@willowgarage.com",
+      url = "http://www.ros.org/wiki/rosinstall",
+      download_url = "http://pypi.python.org/packages/source/r/roslib2/roslib2-0.01.tar.gz", 
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python", 
+        "License :: OSI Approved :: BSD License" ],
+      long_description = """\
+The installer for ROS
+"""
+      )
