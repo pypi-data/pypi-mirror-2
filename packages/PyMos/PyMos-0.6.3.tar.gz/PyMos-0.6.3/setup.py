2232c8a735dc1892e9fed778fe6c37a5e7005521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:01 +0000

    Add package PyMos/0.6.3/PyMos-0.6.3.tar.gz

---
+++ packages/PyMos/PyMos-0.6.3.tar.gz/PyMos-0.6.3/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+setup(name='PyMos',
+      description='Python Mosaic Generator',
+      author='Abhishek Mishra (ideamonk), Yuvi (yuvipanda)',
+      author_email='ideamonk@gmail.com, me@yuvi.in',
+      version='0.6.3',
+      packages=['pymos'],
+      scripts=['bin/pymos'],
+      install_requires=['argparse', 'pil'],
+      license = "BSD",
+      # Thats how Apple was able to take OpenBSD, make Mac OS X :)
+      # http://bheekly.blogspot.com/2007/08/what-is-free.html
+      keywords = "mosaic imaging graphics poster"
+      )
