e02dc0146c7598bade22888129da497964e78ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:54 +0000

    Add package nonblockingloghandler/0.4.4/nonblockingloghandler-0.4.4.zip

---
+++ packages/nonblockingloghandler/nonblockingloghandler-0.4.4.zip/nonblockingloghandler-0.4.4/test/test_autoclosing_queue.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+""" Straightforward tests of Autoclosing Queue.
+
+    Does not test the real use-cases, which require the application to terminate."""
+
+import unittest
+
+import sys
+sys.path = [".."] + sys.path
+import autoclosingqueue
+
+class TestAutoClosingQueue(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_null_case(self):
+        queue = autoclosingqueue.Queue()
+
+    def test_simple_case(self):
+        queue = autoclosingqueue.Queue()
+        queue.put("Item")
+        assert queue._undone_item_count == 1 # Peeking inside private, without mutex: Doubly naughty, but safe.
+        assert queue.get() == "Item"
+        assert queue._undone_item_count == 1 # Haven't marked as done yet.
+        queue.task_done()
+        assert queue._undone_item_count == 0
+
+if __name__ == "__main__":
+    unittest.main()
