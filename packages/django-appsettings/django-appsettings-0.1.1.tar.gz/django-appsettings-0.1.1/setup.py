e4f3ced32884300973a33000304415ac742a8921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package django-appsettings/0.1.1/django-appsettings-0.1.1.tar.gz

---
+++ packages/django-appsettings/django-appsettings-0.1.1.tar.gz/django-appsettings-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from distutils.core import setup
+
+
+setup(
+    name='django-appsettings',
+    version='0.1.1',
+    description='A unified settings system for pluggable django apps',
+    author='Jared Forsyth',
+    author_email='jabapyth@gmail.com',
+    license='BSD',
+    url='http://github.com/jabapyth/django-appsettings/',
+    keywords = ['django', 'settings', 'database', 'user', 'customize'],
+    packages=[
+        'appsettings',
+    ],
+    package_data = {'feedback': ['templates/appsettings/*']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
