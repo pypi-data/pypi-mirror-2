b0c5307b2576dd0d6b939df22cccd84f193d4183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:00 +0000

    Add package vimpdb/0.3.6/vimpdb-0.3.6.tar.gz

---
+++ packages/vimpdb/vimpdb-0.3.6.tar.gz/vimpdb-0.3.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.3.6'
+
+setup(name='vimpdb',
+      version=version,
+      description="Vim and Pdb integration",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CREDITS.txt").read() + "\n" +
+                       open("CHANGES.txt").read() + "\n" +
+                       open("TODO.txt").read(),
+      classifiers=[],
+      keywords='',
+      author='Godefroid Chapelle',
+      author_email='gotcha@bubblenet.be',
+      url='http://github.com/gotcha/vimpdb',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
