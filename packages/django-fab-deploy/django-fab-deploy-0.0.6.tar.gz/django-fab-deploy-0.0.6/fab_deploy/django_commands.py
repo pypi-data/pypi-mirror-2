fc4c8818976047cd0ce8b61028b58cae3dc95bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:09 +0000

    Add package django-fab-deploy/0.0.6/django-fab-deploy-0.0.6.tar.gz

---
+++ packages/django-fab-deploy/django-fab-deploy-0.0.6.tar.gz/django-fab-deploy-0.0.6/fab_deploy/django_commands.py
@@ -0,0 +1,34 @@
+#coding: utf-8
+from __future__ import with_statement
+from fabric.api import env, cd, local, run, settings
+from fab_deploy.commands import mysqldump
+
+def manage(command, on_server=True):
+    if on_server:
+        with cd(env.conf['SRC_DIR']):
+            run ('%s/bin/python ./manage.py %s' % (env.conf['ENV_DIR'], command))
+    else:
+        local('./manage.py %s' % command)
+
+def migrate(params='', do_backup=True):
+    if do_backup:
+        mysqldump('hosting/backups/before-migrate')
+    manage('migrate --noinput %s' % params)
+
+def syncdb():
+    manage('syncdb --noinput')
+
+def compress():
+    with settings(warn_only=True):
+        manage('synccompress')
+
+def test():
+    pass
+#    with cd(env.conf['SRC_DIR']):
+#        run('source %s/bin/activate; ./bin/runtests.sh' % env.conf['ENV_DIR'])
+
+def coverage():
+    pass
+#    with cd(env.conf['SRC_DIR']):
+#        run('source %s/bin/activate; ./bin/runcoverage.sh' % env.conf['ENV_DIR'])
+
