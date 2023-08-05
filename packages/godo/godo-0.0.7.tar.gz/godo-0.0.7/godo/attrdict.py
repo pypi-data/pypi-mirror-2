d46697db8b353e8abb4c29606f812d303c91ef85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:09 +0000

    Add package godo/0.0.7/godo-0.0.7.tar.gz

---
+++ packages/godo/godo-0.0.7.tar.gz/godo-0.0.7/godo/attrdict.py
@@ -0,0 +1,16 @@
+
+class attrdict(dict):
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError:
+            raise AttributeError(key)
