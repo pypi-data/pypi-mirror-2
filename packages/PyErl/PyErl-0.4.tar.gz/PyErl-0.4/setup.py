7d58cf4bb8e9496d694f659bb625271d8c62806d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package PyErl/0.4/PyErl-0.4.tar.gz

---
+++ packages/PyErl/PyErl-0.4.tar.gz/PyErl-0.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(name = "PyErl",
+      version = "0.4",
+      description = "Erlang Interface for Python",
+      long_description = '''
+PyErl is Erlang Interface for Python. which help you integrate
+programs written in Python and Erlang.
+''',
+      author = "Tsukasa Hamano",
+      author_email = "code@cuspy.org",
+      url="http://github.com/hamano/python-erlang-interface",
+      download_url="http://pypi.python.org/pypi/PyErl/",
+      license="MPL",
+      ext_modules = [
+        Extension(
+            "pyerl",
+            ["pyerl.c", "eterm.c"],
+            libraries=["erl_interface", "ei"]
+            )
+        ],
+      classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Erlang',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      )
