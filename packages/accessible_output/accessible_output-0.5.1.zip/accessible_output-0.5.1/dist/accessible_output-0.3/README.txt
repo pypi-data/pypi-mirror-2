ed45a88ecf685080e90a6ec564d1c86383e11bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package accessible_output/0.5.1/accessible_output-0.5.1.zip

---
+++ packages/accessible_output/accessible_output-0.5.1.zip/accessible_output-0.5.1/dist/accessible_output-0.3/README.txt
@@ -0,0 +1,1 @@
+The readme should go here. But this is just a test. so it's not.
---
+++ packages/accessible_output/accessible_output-0.5.1.zip/accessible_output-0.5.1/speech/outputs/__init__.py
@@ -0,0 +1,6 @@
+from jaws import Jaws
+from nvda import NVDA
+from sa import SystemAccess
+from sapi5 import Sapi5
+from we import WindowEyes
+__all__ = ["Jaws", "NVDA", "Sapi5", "SystemAccess", "WindowEyes"]
