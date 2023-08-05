4015f63e233a1b5c816f435400813c44a313c3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package alacarte/0.6.1/alacarte-0.6.1.tar.gz

---
+++ packages/alacarte/alacarte-0.6.1.tar.gz/alacarte-0.6.1/alacarte/template/sprintf.py
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+from __future__ import with_statement
+
+from alacarte.template.engine import Engine
+
+
+__all__ = ['SprintfEngine']
+
+
+
+class SprintfEngine(Engine):
+    """A basic sprintf-based string templating language.
+    
+    Simple (string-based) usage:
+    
+        >>> from alacarte.core import Engines
+        >>> render = Engines()
+        >>> render('sprintf:', dict(hello="world"), string="Hello %(hello)s!")
+        ('text/plain', 'Hello world!')
+    
+    File-based usage:
+    
+        >>> from alacarte.core import Engines
+        >>> render = Engines()
+        >>> render('sprintf:./tests/templates/hello.txt', dict(hello="world"))
+        ('text/plain', 'Hello world!')
+    
+    """
+    
+    mapping = {
+            'sprintf': 'text/plain',
+            None: 'text/plain'
+        }
+    
+    def render(self, template, data, **options):
+        return self.mapping[None], template % data
