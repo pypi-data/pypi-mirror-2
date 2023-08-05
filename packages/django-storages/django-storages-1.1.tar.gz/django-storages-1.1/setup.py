d833d528b44c4eb58d31f99182f8b93426eb8d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:58 +0000

    Add package django-storages/1.1/django-storages-1.1.tar.gz

---
+++ packages/django-storages/django-storages-1.1.tar.gz/django-storages-1.1/setup.py
@@ -0,0 +1,26 @@
+import os, distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup, find_packages
+ 
+setup(
+    name = 'django-storages',
+    version = '1.1',
+    packages = find_packages(),
+    
+    author='David Larlet',
+    author_email='david@larlet.fr',
+    license = 'BSD',
+    description='Support for many storages (S3, MogileFS, etc) in Django.',
+    url='http://code.welldev.org/django-storages/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    py_modules=['S3'],
+    zip_safe=False,
+)
