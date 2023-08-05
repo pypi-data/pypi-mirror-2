71e18defe504b4090e38b9cf0c3b5ed62444e53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package SQLBase7-SA/0.1b1/SQLBase7_SA-0.1b1-py2.6.egg

---
+++ packages/SQLBase7-SA/SQLBase7_SA-0.1b1-py2.6.egg/sqlbase7/_version.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+################################################################################
+#
+#  PySQLBase7 -- SQLAlchemy driver/dialect for Centura SQLBase v7
+#  Copyright © 2010 Lance Edgar
+#
+#  This file is part of PySQLBase7.
+#
+#  PySQLBase7 is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  PySQLBase7 is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with PySQLBase7.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+
+__version__ = '0.1a1'
