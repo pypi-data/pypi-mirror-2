e0950fd8cc3af97f393f4b419e794a8185a3b9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:27 +0000

    Add package pumblr/0.1.1dev/pumblr-0.1.1dev-py2.5.egg

---
+++ packages/pumblr/pumblr-0.1.1dev-py2.5.egg/pumblr/errors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class PumblrError(Exception):
+    """Pumblr exception"""
+    
+    def __init__(self, msg):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
+
+
+class PumblrAuthError(PumblrError):
+    """403 Forbidden exception"""
+    pass
+
+
+class PumblrRequestError(PumblrError):
+    """400 Bad Request exception"""
+    pass
+
