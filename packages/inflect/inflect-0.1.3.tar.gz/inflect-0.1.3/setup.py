120b6f9bdd7b1258e130e514ef7b125addec2f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:53 +0000

    Add package inflect/0.1.3/inflect-0.1.3.tar.gz

---
+++ packages/inflect/inflect-0.1.3.tar.gz/inflect-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(name='inflect',
+      version='0.1.3',
+      description="Correctly generate plurals, ordinals, indefinite articles; convert numbers to words",
+      author='Paul Dyson',
+      author_email='pwdyson@yahoo.com',
+      url="http://pypi.python.org/pypi/inflect",
+      py_modules=['inflect'],
+      keywords = ['plural', 'inflect', 'participle'],
+      classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        ],
+      long_description = open('README.txt').read()
+      )
