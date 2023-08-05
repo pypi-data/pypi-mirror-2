774f9bf3bde8a313934e14041edd4087baf9ac60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package zope.preference/3.8.0/zope.preference-3.8.0.tar.gz

---
+++ packages/zope.preference/zope.preference-3.8.0.tar.gz/zope.preference-3.8.0/src/zope/preference/tests.py
@@ -0,0 +1,36 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests for the Preferences System
+
+$Id: tests.py 113366 2010-06-11 17:51:08Z icemac $
+"""
+from zope.app.testing import setup
+from zope.component import testing
+import doctest
+
+
+def setUp(test):
+    testing.setUp(test)
+    setup.setUpTestAsModule(test, 'zope.preference.README')
+
+
+def tearDown(test):
+    testing.tearDown(test)
+    setup.tearDownTestAsModule(test)
+
+
+def test_suite():
+    return doctest.DocFileSuite('README.txt',
+                                setUp=setUp, tearDown=tearDown,
+                                optionflags=doctest.NORMALIZE_WHITESPACE)
