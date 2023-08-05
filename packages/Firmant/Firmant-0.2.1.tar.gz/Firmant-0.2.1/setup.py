ce256747a71ea30c0c49aee0f736dc37b32ddce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package Firmant/0.2.1/Firmant-0.2.1.tar.gz

---
+++ packages/Firmant/Firmant-0.2.1.tar.gz/Firmant-0.2.1/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+
+classifiers = [ 'Development Status :: 4 - Beta'
+              , 'Intended Audience :: Developers'
+              , 'License :: OSI Approved :: BSD License'
+              , 'Operating System :: MacOS :: MacOS X'
+              , 'Operating System :: POSIX :: Linux'
+              , 'Operating System :: Unix'
+              , 'Programming Language :: Python :: 2.6'
+              , 'Topic :: Internet :: WWW/HTTP :: Site Management'
+              ]
+
+
+setup(name='Firmant',
+      version='0.2.1',
+      author='Robert Escriva (rescrv)',
+      author_email='firmant@mail.robescriva.com',
+      packages=['firmant'
+               ,'firmant.parsers'
+               ,'firmant.routing'
+               ,'firmant.templates'
+               ,'firmant.utils'
+               ,'firmant.writers'
+               ],
+      package_dir={'firmant': 'firmant'},
+      package_data={'firmant': ['templates/*.html',
+                                'templates/*/*.html']},
+      scripts=['bin/firmant'],
+      url='http://firmant.org/',
+      license='3-clause BSD',
+      description='A framework for static web applications.',
+      long_description=open('doc/README.rst').read(),
+      classifiers=classifiers,
+      )
