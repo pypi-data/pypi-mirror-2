37cf14158f4eb466f1c57adb2a511bd83d1af05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:01 +0000

    Add package django-blog-zinnia/0.6/django-blog-zinnia-0.6.tar.gz

---
+++ packages/django-blog-zinnia/django-blog-zinnia-0.6.tar.gz/django-blog-zinnia-0.6/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+import zinnia
+
+setup(name='django-blog-zinnia',
+      version=zinnia.__version__,
+      
+      description='A clear and powerfull weblog application powered with Django',
+      long_description=open(os.path.join('README.rst')).read(),
+      keywords='django, blog',
+
+      author=zinnia.__author__,
+      author_email=zinnia.__email__,
+      url=zinnia.__url__,
+
+      packages=find_packages(),
+      classifiers=[
+          'Framework :: Django',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Software Development :: Libraries :: Python Modules',],
+            
+      license=zinnia.__license__,
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['django-tagging',
+                        'akismet',
+                        ])
+
+
