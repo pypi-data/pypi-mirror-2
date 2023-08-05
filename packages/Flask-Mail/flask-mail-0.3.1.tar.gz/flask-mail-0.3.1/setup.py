2166389e5ac3c5dc3cab209a09e5d6cb0f049feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package Flask-Mail/0.3.1/flask-mail-0.3.1.tar.gz

---
+++ packages/Flask-Mail/flask-mail-0.3.1.tar.gz/flask-mail-0.3.1/setup.py
@@ -0,0 +1,38 @@
+"""
+flask-mail
+----------
+
+A Flask extension for sending email. 
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='flask-mail',
+    version='0.3.1',
+    url='http://bitbucket.org/danjac/flask-mail',
+    license='BSD',
+    author='Dan Jacob',
+    author_email='danjac354@gmail.com',
+    description='Flask extension for sending email',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Lamson',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
