208de0b79ef0fda03c597e562fde19055071488e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package plone.app.contentmenu/2.0b3/plone.app.contentmenu-2.0b3.zip

---
+++ packages/plone.app.contentmenu/plone.app.contentmenu-2.0b3.zip/plone.app.contentmenu-2.0b3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '2.0b3'
+
+setup(name='plone.app.contentmenu',
+      version=version,
+      description="Plone's content menu implementation",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[],
+      keywords='plone contentmenu menu',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.contentmenu',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+          'Products.PloneTestCase',
+          ]
+        ),
+      install_requires=[
+        'setuptools',
+        'plone.locking',
+        'plone.memoize',
+        'plone.app.content >=2.0a3',
+        'zope.component',
+        'zope.contentprovider',
+        'zope.interface',
+        'zope.i18n',
+        'zope.i18nmessageid',
+        'zope.publisher',
+        'zope.app.publisher',
+        'Acquisition',
+        'Products.CMFCore',
+        'Products.CMFDynamicViewFTI',
+        'Plone',
+        'Zope2',
+      ],
+      )
