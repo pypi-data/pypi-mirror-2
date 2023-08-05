c12d7cb5c519081247953a93b94d11254c7aca93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package virtualkeyring/1.5/virtualkeyring-1.5.zip

---
+++ packages/virtualkeyring/virtualkeyring-1.5.zip/virtualkeyring-1.5/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.5'
+
+setup(name='virtualkeyring',
+      version=version,
+      description="Strong domain-specific passwords generator",
+      long_description=file("README.txt").read(),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          ],
+      keywords='hash sha1 password generator keyring master',
+      author='Olivier Grisel',
+      author_email='olivier.grisel@ensta.org',
+      url='http://oliviergrisel.name/virtualkeyring',
+      license='GPLv3',
+      py_modules = ['virtualkeyring'],
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'pexpect',
+      ],
+      entry_points={
+        'console_scripts': [
+            'vkr = virtualkeyring:main',
+            'vkr-key = virtualkeyring:add_key',
+        ],
+        'setuptools.installation': [
+            'eggsecutable = virtualkeyring:main'],
+      },
+      test_suite="tests.test_suite",
+     )
