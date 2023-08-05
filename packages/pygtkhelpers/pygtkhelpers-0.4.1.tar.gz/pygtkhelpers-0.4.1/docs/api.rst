7eb85c4e1e26a11d828f4efc36e0cdaa038a2057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package pygtkhelpers/0.4.1/pygtkhelpers-0.4.1.tar.gz

---
+++ packages/pygtkhelpers/pygtkhelpers-0.4.1.tar.gz/pygtkhelpers-0.4.1/docs/api.rst
@@ -0,0 +1,20 @@
+
+API
+===
+
+.. toctree::
+    :maxdepth: 2
+
+    api_delegates
+    api_proxy
+    api_utils
+    api_gthreads
+    api_objectlist
+    api_widgets
+    api_dialogs
+    api_test
+    api_debut
+
+
+
+
---
+++ packages/pygtkhelpers/pygtkhelpers-0.4.1.tar.gz/pygtkhelpers-0.4.1/pygtkhelpers/debug/__init__.py
@@ -0,0 +1,12 @@
+"""
+    pygtkheplers.debug
+    ~~~~~~~~~~~~~~~~~~
+
+    a library for ui integrated traceback handling in pygtk
+"""
+
+from .dialogs import (
+        SimpleExceptionDialog,
+        ExtendedExceptionDialog,
+        install_hook,
+        )
