bbbc6fb408df08a7bfcbf3f61d43b0987847e3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:04 +0000

    Add package stopam/0.1.4/stopam-0.1.4.tar.gz

---
+++ packages/stopam/stopam-0.1.4.tar.gz/stopam-0.1.4/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='stopam',
+      version='0.1.4',
+      description='A python client for the stopam.com spam prevention service',
+      author='Emil Ivanov',
+      author_email='emil.vladev@gmail.com',
+      url='http://stopam.com/',
+      packages=['stopam'],
+      license='MIT')
