1e4d5f91b65de26db8d78aaa110e6702bc887b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:48 +0000

    Add package actionbar.panel/1.0/actionbar.panel-1.0.zip

---
+++ packages/actionbar.panel/actionbar.panel-1.0.zip/actionbar.panel-1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='actionbar.panel',
+      version=version,
+      description="Provides a (old) facebook style action panel at the bottom of your  Plone site",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['actionbar'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
