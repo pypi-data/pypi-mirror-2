f2c29a1651b5b73b02e49fc440d2dd4d04236130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package collective.lineage/0.5/collective.lineage-0.5.tar.gz

---
+++ packages/collective.lineage/collective.lineage-0.5.tar.gz/collective.lineage-0.5/setup.py
@@ -0,0 +1,54 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.5'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+# line breaks are needed after each block so that reST doesn't get mad
+long_description = """
+%s
+
+%s
+
+%s
+
+%s
+
+""" % (read('README.txt'),
+       read('src', 'collective', 'lineage', 'README.txt'),
+       read('docs', 'INSTALL.txt'),
+       read('docs', 'HISTORY.txt'))
+
+setup(name='collective.lineage',
+      version=version,
+      description="The microsite creation product for Plone",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone lineage',
+      author='Six Feet Up, Inc.',
+      author_email='info@sixfeetup.com',
+      url='http://plone.org/products/collective-lineage',
+      license='GPL',
+      packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'p4a.z2utils',
+          'p4a.common',
+          'p4a.subtyper',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ packages/collective.lineage/collective.lineage-0.5.tar.gz/collective.lineage-0.5/src/collective/lineage/upgrades.py
@@ -0,0 +1,66 @@
+from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
+
+import zope.component
+import transaction
+
+from p4a.subtyper import engine
+from p4a.subtyper import interfaces
+
+def migrateChildFolders(context):
+    """Migrates Child Folder objects to normal Folder objects
+    that are subtyped"""
+    # allow the Child Folder type to be addable
+    pt = getToolByName(context, "portal_types")
+    pw = getToolByName(context, "portal_workflow")
+    cf_type = pt["Child Folder"]
+    cf_type.global_allow = True
+
+    pc = getToolByName(context, 'portal_catalog')
+    brains = pc.searchResults(portal_type="Child Folder")
+    while brains:
+        child_folder = brains[0].getObject()
+        parent = child_folder.getParentNode()
+        children_ids = child_folder.objectIds()
+
+        cf_state = pw.getInfoFor(child_folder, "review_state")
+        cf_wf = pw.getDefaultChainFor(child_folder)
+        cf_wf = cf_wf and cf_wf[0]
+        cf_title = child_folder.Title()
+        cf_desc = child_folder.Description()
+        cf_id = child_folder.getId()
+        cf_new_id = "%s-old" % cf_id
+        parent.manage_renameObjects([cf_id], [cf_new_id])
+
+        parent.invokeFactory("Folder", cf_id)
+        new_folder = parent[cf_id]
+        new_folder.setTitle(cf_title)
+        new_folder.setDescription(cf_desc)
+        new_folder.processForm()
+        zope.component.provideUtility(engine.Subtyper())
+        subtyper = zope.component.getUtility(interfaces.ISubtyper)
+        subtyper.change_type(new_folder, u'collective.lineage.childsite')
+
+        if cf_wf:
+            new_state = {
+                'actor': 'Administrator',
+                'action': None,
+                'review_state':cf_state,
+                'time': DateTime(),
+                'comments': 'setting up the workflow of the item correctly',
+                }
+            pw.setStatusOf(cf_wf, new_folder, new_state)
+            new_folder.reindexObject()
+
+
+        if children_ids:
+            cut_items = child_folder.manage_cutObjects(ids=children_ids)
+            new_folder.manage_pasteObjects(cut_items)
+
+        parent.manage_delObjects([cf_new_id])
+        transaction.savepoint()
+
+        brains = pc.searchResults(portal_type="Child Folder")
+
+    cf_type.global_allow = False
+
