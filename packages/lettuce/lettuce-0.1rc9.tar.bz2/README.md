18ce9165cbe7010c9826c71ddd5e9b33db4c4245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package lettuce/0.1rc9/lettuce-0.1rc9.tar.bz2

---
+++ packages/lettuce/lettuce-0.1rc9.tar.bz2/README.md
@@ -0,0 +1,54 @@
+# lettuce
+> Version 0.1 - barium
+
+## On release names
+
+Lettuce release names will be inspired by any green stuff.
+
+Barium: In form of "barium nitrate" is commonly used to make green fireworks. Such a good name for a first version :)
+
+# What
+
+Lettuce is a BDD tool for python, 100% inspired on [cucumber](http://cukes.info/ "BDD with elegance and joy").
+
+# Motivation
+
+1. [Cucumber](http://cukes.info/) makes [Ruby](http://www.ruby-lang.org/) even more sexy. Python needed something like it.
+2. Testing must be funny and easy.
+3. Most python developers code in python, not ruby.
+4. Ruby has Capistrano, Python has Fabric. Ruby has cucumber, Python has lettuce.
+5. I personally don't like mixing many languages in small projects. Keeping all in python is better.
+6. I love python, and ever did. But I also ever missed something that make writing tests easier and funnier.
+7. I like [nose](http://code.google.com/p/python-nose/), which is a unittest pythonic framework. However, as the project I work on grows, so do the tests, and it becomes harder to understand them.
+
+# Dependencies
+
+**you will need to install these dependencies in order to** *hack* **lettuce** :)
+all them are used within lettuce tests
+
+* [nose](http://code.google.com/p/python-nose/)
+    > [sudo] pip install nose
+* [mox](http://code.google.com/p/pymox/)
+    > [sudo] pip install mox
+* [sphinx](http://sphinx.pocoo.org/)
+    > [sudo] pip install sphinx
+* [lxml](http://codespeak.net/lxml/)
+    > [sudo] pip install lxml
+* [django](http://djangoproject.com/)
+    > [sudo] pip install django
+
+# Contributing
+
+1. fork and clone the project
+2. install the dependencies above
+3. run the tests with make:
+    > make unit functional integration doctest
+4. hack at will
+5. commit, push etc
+6. send a pull request
+
+# Special thanks
+
+1. [Cucumber](http://cukes.info/) crew, for creating such a AWESOME project, and for inspiring [Lettuce](http://lettuce.it/).
+2. [Tatiana](http://github.com/tatiana) for helping a lot with documentation.
+3. [Django](http://djangoproject.com) which documentation structure was borrowed.
---
+++ packages/lettuce/lettuce-0.1rc9.tar.bz2/lettuce/__init__.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+# <Lettuce - Behaviour Driven Development for python>
+# Copyright (C) <2010>  Gabriel Falcão <gabriel@nacaolivre.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+version = '0.1rc9'
+release = 'barium'
+
+import os
+import sys
+
+from lettuce import fs
+from lettuce.core import Feature, TotalResult
+
+from lettuce.terrain import after
+from lettuce.terrain import before
+from lettuce.terrain import world
+
+from lettuce.decorators import step
+from lettuce.registry import CALLBACK_REGISTRY
+from lettuce.registry import STEP_REGISTRY
+
+from lettuce.exceptions import LettuceSyntaxError
+
+__all__ = ['after', 'before', 'step', 'world', 'STEP_REGISTRY', 'CALLBACK_REGISTRY']
+
+def _import(name):
+    return __import__(name)
+
+class Runner(object):
+    """ Main lettuce's test runner
+
+    Takes a base path as parameter (string), so that it can look for
+    features and step definitions on there.
+    """
+    def __init__(self, base_path, verbosity=0):
+        """ lettuce.Runner will try to find a terrain.py file and
+        import it from within `base_path`
+        """
+
+        self.terrain = None
+        self.single_feature = None
+        if os.path.isfile(base_path) and os.path.exists(base_path):
+            self.single_feature = base_path
+            base_path = os.path.dirname(base_path)
+
+        sys.path.insert(0, base_path)
+        self.loader = fs.FeatureLoader(base_path)
+        self.verbosity = verbosity
+        try:
+            self.terrain = _import("terrain")
+        except Exception, e:
+            if not "No module named terrain" in str(e):
+                string = 'Lettuce has tried to load the conventional environment ' \
+                    'module "terrain"\nbut it has errors, check its contents and ' \
+                    'try to run lettuce again.\n'
+
+                sys.stderr.write(string)
+                raise SystemExit(1)
+
+        sys.path.remove(base_path)
+
+        if verbosity is 0:
+            from lettuce.plugins import non_verbose as output
+        elif verbosity is 3:
+            from lettuce.plugins import shell_output as output
+        else:
+            from lettuce.plugins import colored_shell_output as output
+
+        reload(output)
+
+        self.output = output
+
+    def run(self):
+        """ Find and load step definitions, and them find and load
+        features under `base_path` specified on constructor
+        """
+
+        self.loader.find_and_load_step_definitions()
+
+        for callback in CALLBACK_REGISTRY['all']['before']:
+            callback()
+
+        results = []
+        if self.single_feature:
+            features_files = [self.single_feature]
+        else:
+            features_files = self.loader.find_feature_files()
+
+        if not features_files:
+            self.output.print_no_features_found(self.loader.base_dir)
+            return
+
+        try:
+            for filename in features_files:
+                feature = Feature.from_file(filename)
+                results.append(feature.run())
+        except LettuceSyntaxError, e:
+            sys.stderr.write(e.msg)
+            raise SystemExit(2)
+
+        total = TotalResult(results)
+
+        for callback in CALLBACK_REGISTRY['all']['after']:
+            callback(total)
+
+        return total
