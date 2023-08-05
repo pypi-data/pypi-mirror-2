5f8905f4d251363eebd56400132fec90416f350e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package langacore.kit.common/0.1.6/langacore.kit.common-0.1.6.tar.gz

---
+++ packages/langacore.kit.common/langacore.kit.common-0.1.6.tar.gz/langacore.kit.common-0.1.6/setup.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+# Copyright (C) 2010 LangaCore
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+from doc.conf import release 
+
+setup (
+    name = 'langacore.kit.common',
+    version = release,
+    author = 'LangaCore, Lukasz Langa',
+    author_email = 'support@langacore.org, lukasz@langa.pl',
+    description = "A library of various simple common routines that keep being " \
+                  "rewritten all over again in every project we're working on.",
+    long_description = '',
+    url = 'http://packages.python.org/langacore.kit.common/',
+    keywords = '',
+    platforms = ['any'],
+    license = 'GPL v3', 
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['langacore', 'langacore.kit'],
+    zip_safe = True,
+    install_requires = [
+        'setuptools',
+        'lockfile',
+        ],
+    
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows :: Windows NT/2000',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    )
