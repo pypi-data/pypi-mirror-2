9c5fe3e346815b1129c3146d608a6dc1233fe658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package panya-competition/0.0.3/panya-competition-0.0.3.tar.gz

---
+++ packages/panya-competition/panya-competition-0.0.3.tar.gz/panya-competition-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panya-competition',
+    version='0.0.3',
+    description='Panya competition app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt International',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/panya-competition',
+    packages = find_packages(),
+    dependency_links = [
+        'https://github.com/praekelt/panya/tarball/master#egg=panya',
+    ],
+    install_requires = [
+        'panya',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
+
