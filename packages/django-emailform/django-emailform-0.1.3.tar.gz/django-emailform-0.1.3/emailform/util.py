742f7d32537dcf03f5c7aa8c381680f0a9f00097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:25 +0000

    Add package django-emailform/0.1.3/django-emailform-0.1.3.tar.gz

---
+++ packages/django-emailform/django-emailform-0.1.3.tar.gz/django-emailform-0.1.3/emailform/util.py
@@ -0,0 +1,21 @@
+import itertools
+import re
+
+from django.utils.importlib import import_module
+
+
+class ChoiceEnum(object):
+    def __init__(self, choices):
+        if isinstance(choices, basestring):
+            choices=choices.split()
+        if isinstance(choices, (list,tuple)) and all(isinstance(x, tuple) and len(x)==2 for x in choices):
+            values=choices
+        else:
+            values=zip(itertools.count(1), choices)
+        for v, n in values:
+            name=re.sub('[- ]', '_', n.upper())
+            setattr(self, name, v)
+        self._choices=values
+
+    def __getitem__(self, idx):
+        return self._choices[idx]
