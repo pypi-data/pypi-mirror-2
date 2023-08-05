126667695f5485522fabd2fe7f2efdb99f93f1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:04 +0000

    Add package collective.wowlichking/0.3/collective.wowlichking-0.3.zip

---
+++ packages/collective.wowlichking/collective.wowlichking-0.3.zip/collective.wowlichking-0.3/README.txt
@@ -0,0 +1,6 @@
+.. contents::
+
+Introduction
+============
+
+A World of Warcraft Plone3 theme based on reteeps wordpress theme "WoW Lich King".
---
+++ packages/collective.wowlichking/collective.wowlichking-0.3.zip/collective.wowlichking-0.3/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.3'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
+setup(name='collective.wowlichking',
+      version=version,
+      description="An installable WoW theme for Pone 3",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='web zope plone theme wow warcraft lich king',
+      author='Marc Goetz',
+      author_email='goetz.marc@googlemail.com',
+      url='https://svn.inquant.de/inquant/develop/praktikum/collective.wowlichking/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
