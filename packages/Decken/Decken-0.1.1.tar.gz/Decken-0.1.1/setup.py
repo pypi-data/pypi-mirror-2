7cf2608797f15b51c7a46365ce606fb3a846e474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package Decken/0.1.1/Decken-0.1.1.tar.gz

---
+++ packages/Decken/Decken-0.1.1.tar.gz/Decken-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+
+setup(
+	name='Decken',
+	version='0.1.1',
+	scripts = ['decken.py', 'decken_n800.py', 'ez_setup.py'],
+	package_data={'' : ['*.csv', '*.glade'], 'images' : [ '*.jpg' ] },
+	install_requires=["BeautifulSoup"],
+	entry_points = {
+		'gui_scripts' : [
+			'decken = decken:command'
+		]
+	},
+		
+	author='Claudio Torcato',
+	author_email='claudiotorcato@gmail.com',
+	description='Deck Organizer to Trading Card Games',
+	license="PSF",
+	keywords = "tcg mtg magic deck card python gnome linux mac n800",
+)
