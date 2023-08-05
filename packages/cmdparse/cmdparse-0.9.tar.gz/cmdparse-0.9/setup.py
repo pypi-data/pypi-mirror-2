a0390fb9b1ca7eedd82770b23ca895d0fed9169b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:11 +0000

    Add package cmdparse/0.9/cmdparse-0.9.tar.gz

---
+++ packages/cmdparse/cmdparse-0.9.tar.gz/cmdparse-0.9/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+    name = "cmdparse",
+    version = "0.9",
+    author = "Peter Teichman",
+    author_email = "peter@teichman.org",
+    url = "http://wiki.github.com/pteichman/python-cmdparse/",
+    download_url = "http://github.com/pteichman/python-cmdparse/downloads",
+    description = "A command line parser for CVS-style subcommands",
+    packages = ["cmdparse"],
+    test_suite = "tests.cmdparse_suite",
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: User Interfaces"
+    ]
+)
