36e5a431312216be97f2ac0f7d9f8512e3b37401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:18 +0000

    Add package django-simple-news/1.0/django-simple-news-1.0.tar.gz

---
+++ packages/django-simple-news/django-simple-news-1.0.tar.gz/django-simple-news-1.0/django_simple_news/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django_simple_news.models import NewsItem, NewsAuthor
+
+
+class NewsItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ['title', 'date', 'site']
+    list_filter = ['date']
+    search_fields = ['title', 'body']
+    date_hierarchy = 'date'
+    ordering = ['-date']
+    fieldsets = (
+        ('Article info', {
+            'fields': ('title', 'body', 'date')
+        }),
+        ('Advanced', {
+            'classes': ['collapse'],
+            'fields': ('snippet', 'slug',)
+        }),
+    )
+
+    def queryset(self, request):
+        return NewsItem.on_site.all()
+
+admin.site.register(NewsItem, NewsItemAdmin)
+
