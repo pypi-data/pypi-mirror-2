af74cc7f518963a5746daa4c610adc7f2add9149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package Flask-Markdown/0.2.1/Flask-Markdown-0.2.1.tar.gz

---
+++ packages/Flask-Markdown/Flask-Markdown-0.2.1.tar.gz/Flask-Markdown-0.2.1/flaskext/markdown.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+flaskext.markdown
+~~~~~~~~~~~~~~~~~
+
+Markdown filter class for Flask
+To use::
+
+    from flaskext.markdown import Markdown
+    md = Markdown(app)
+
+Then in your template::
+
+    {% filter markdown %}
+    Your Markdown
+    =============
+    {% endfilter %}
+
+You can also do::
+
+    {{ mymarkdown | markdown}}
+
+:copyright: (c) 2010 by Dan Colish.
+:license: BSD, MIT see LICENSE for more details.
+"""
+from __future__ import absolute_import
+from flask import Markup
+import markdown as md
+
+
+class Markdown(object):
+    """
+    Simple wrapper class for Markdown objects, any options that are available
+    for markdown may be passed as keyword arguments like so::
+
+      Markdown(app,
+               extensions=['footnotes'],
+               extension_configs={'footnotes': ('PLACE_MARKER','~~~~~~~~')},
+               safe_mode=True,
+               output_format='html4',
+               )
+    """
+
+    def __init__(self, app, **markdown_options):
+        """Markdown uses old style classes"""
+        self._instance = md.Markdown(**markdown_options)
+        app.jinja_env.filters.setdefault('markdown', self)
+
+    def __call__(self, stream):
+        return Markup(self._instance.convert(stream))
