f467e2f47e1285a9704d06bd67c48e1a774ab25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package slc.publications/1.1.17/slc.publications-1.1.17.zip

---
+++ packages/slc.publications/slc.publications-1.1.17.zip/slc.publications-1.1.17/slc/publications/config.py
@@ -0,0 +1,12 @@
+
+__author__ = """SYSLAB.COM <info@syslab.com>"""
+__docformat__ = 'plaintext'
+
+PROJECTNAME = "slc.publications"
+product_globals = globals()
+
+DEFAULT_ADD_CONTENT_PERMISSION = "Add portal content"
+
+DEPENDENCIES = [
+        'LinguaPlone',
+        ]
