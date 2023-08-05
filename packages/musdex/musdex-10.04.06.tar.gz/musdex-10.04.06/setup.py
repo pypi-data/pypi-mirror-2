14ff89247e9fde0b7a9013ffa231af8a9ffa3e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:56 +0000

    Add package musdex/10.04.06/musdex-10.04.06.tar.gz

---
+++ packages/musdex/musdex-10.04.06.tar.gz/musdex-10.04.06/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(
+    name='musdex',
+    version='10.04.06',
+    description='musdex -- VCS-aware zip archive tool',
+    author='Max Battcher',
+    author_email='me@worldmaker.net',
+    url='http://musdex.code.worldmaker.net',
+    packages=['musdex'],
+    scripts=['scripts/musdex', 'scripts/xedsum'],
+    requires=[
+        'argparse',
+        'yaml',
+    ],
+    license='Microsoft Reciprocal License (Ms-RL)',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Version Control',
+    ],
+)
+
+# vim: ai et ts=4 sts=4 sw=4
