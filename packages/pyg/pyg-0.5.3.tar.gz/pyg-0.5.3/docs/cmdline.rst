f8c0463935fae7335e5fca640242686d828580f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:38 +0000

    Add package pyg/0.5.3/pyg-0.5.3.tar.gz

---
+++ packages/pyg/pyg-0.5.3.tar.gz/pyg-0.5.3/docs/cmdline.rst
@@ -0,0 +1,21 @@
+Using Pyg from the command line
+===============================
+
+Pyg should be used mostly from the command line, as it requires root's privileges to install and remove packages.
+
+On some systems (e.g. on Unix systems), you may need root privileges to execute some commands such as :command:`install`, :command:`uninstall`, or :command:`upgrade`.
+In this case you should use the :command:`sudo` command.
+
+Main commands:
+
+.. toctree::
+   :maxdepth: 2
+
+   main_cmd.rst
+
+Minor commands:
+
+.. toctree::
+   :maxdepth: 2
+
+   minor_cmd.rst
---
+++ packages/pyg/pyg-0.5.3.tar.gz/pyg-0.5.3/pyg/__init__.py
@@ -0,0 +1,26 @@
+__version__ = '0.5.3'
+
+
+def main():
+    import sys
+    import urllib2
+
+    from .parser.parser import init_parser, load_options
+    from .core import PygError, InstallationError, AlreadyInstalled
+
+    try:
+        parser = init_parser(__version__)
+        load_options()
+        parser.dispatch()
+    except (PygError, InstallationError, ValueError):
+        sys.exit(1)
+    except AlreadyInstalled:
+        sys.exit(0)
+    except urllib2.HTTPError as e:
+        sys.exit('HTTPError: {0}'.format(e.msg))
+    except urllib2.URLError as e:
+        sys.exit('urllib error: {0}'.format(e.reason))
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
