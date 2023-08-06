d971e72a8efabcae76fca71cdf35f2763ffc12f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:04 +0000

    Add package Jug/0.8.1/Jug-0.8.1.tar.gz

---
+++ packages/Jug/Jug-0.8.1.tar.gz/Jug-0.8.1/jug/tests/test_tasklet.py
@@ -0,0 +1,26 @@
+from jug.options import default_options
+from jug.tests.task_reset import task_reset
+import jug.jug
+
+@task_reset
+def test_tasklets():
+    store, space = jug.jug.init('jug/tests/jugfiles/tasklets.py', 'dict_store')
+    jug.jug.execute(store, default_options)
+    assert space['t0'].value() == 0
+    assert space['t2'].value() == 4
+    assert space['t0_2'].value() == 4
+    assert space['t0_2_1'].value() == 5
+
+@task_reset
+def test_iteratetask():
+    store, space = jug.jug.init('jug/tests/jugfiles/iteratetask.py', 'dict_store')
+    jug.jug.execute(store, default_options)
+    assert space['t0'].value() == 0
+    assert space['t1'].value() == 2
+    assert space['t2'].value() == 4
+
+@task_reset
+def test_tasklet_dependencies():
+    store, space = jug.jug.init('jug/tests/jugfiles/tasklets.py', 'dict_store')
+    assert not space['t0_2'].can_run()
+
