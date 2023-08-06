ad11fb2eab0c5a241f83a8f6d3f50758300267a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:09 +0000

    Add package killproc/1.0.1/killproc-1.0.1.tar.gz

---
+++ packages/killproc/killproc-1.0.1.tar.gz/killproc-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='killproc',
+    version='1.0.1',
+    author='Jeff Fischer',
+    author_email='jeffrey.fischer@genforma.com',
+    url='https://github.com/jfischer/killproc',
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+            'killproc = killproc.killproc:main'
+            ]},
+    install_requires=[],
+    license='Apache V2.0',
+    description='Kill unix processes by name',
+    long_description=open('README.rst').read(),
+    classifiers=[
+    "Environment :: Console",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: POSIX",
+    "Topic :: System :: Systems Administration"
+    ],
+    )
