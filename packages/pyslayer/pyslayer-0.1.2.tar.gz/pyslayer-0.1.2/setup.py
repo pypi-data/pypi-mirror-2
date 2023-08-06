f2ced4bbdfb9b213a63709f1906130ccbe7660df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:17 +0000

    Add package pyslayer/0.1.2/pyslayer-0.1.2.tar.gz

---
+++ packages/pyslayer/pyslayer-0.1.2.tar.gz/pyslayer-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='pyslayer',
+      version='0.1.2',
+      packages=['pyslayer'],
+      scripts=['scripts/pyslayer'],
+      install_requires = [],
+      extras_require = {'process title': ["setproctitle"],'daemonize':["python-daemon"]},
+      author = "Uriel Katz",
+      author_email = "uriel.katz@gmail.com",
+      description = "a python clone of dbslayer - JSON HTTP abstraction for DBAPI",
+      license = "MIT",
+      keywords = "dbslayer json dbapi",
+      url = "https://github.com/urielka/pyslayer",
+      download_url = "https://github.com/urielka/pyslayer/downloads",
+      zip_safe = False,
+      entry_points = '''
+      [console_scripts]
+      pyslayer = pyslayer.main:cli_start
+      ''',
+)
