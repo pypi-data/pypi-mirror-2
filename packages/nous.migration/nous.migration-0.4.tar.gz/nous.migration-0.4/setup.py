297c4c765f1645696aa922908a390dbe61ff3dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:28 +0000

    Add package nous.migration/0.4/nous.migration-0.4.tar.gz

---
+++ packages/nous.migration/nous.migration-0.4.tar.gz/nous.migration-0.4/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+def get_long_description():
+    dirname = os.path.dirname(__file__)
+    readme = os.path.join(dirname, 'README.txt')
+    f = open(readme, 'rb')
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+setup(
+    name='nous.migration',
+    version='0.4',
+    description='Yet another sqlalchemy based database schema migration tool',
+    long_description=get_long_description(),
+    author='Ignas Mikalajunas',
+    author_email='ignas@nous.lt',
+    url='http://github.com/Ignas/nous.migration/',
+    classifiers=["Development Status :: 4 - Beta",
+                 "Intended Audience :: Developers",
+                 "License :: OSI Approved :: GNU General Public License (GPL)",
+                 "Programming Language :: Python"],
+    install_requires=[
+        'sqlalchemy',
+        'PasteDeploy'
+        ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+      entry_points = """\
+    [console_scripts]
+    migrate = nous.migration:main
+    """,
+    license="GPL"
+)
