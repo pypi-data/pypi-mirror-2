7ab67a7a344493627f100fad227baca691d24b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:11 +0000

    Add package django-universaltag/0.1rc2/django-universaltag-0.1rc2.tar.gz

---
+++ packages/django-universaltag/django-universaltag-0.1rc2.tar.gz/django-universaltag-0.1rc2/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# author:        alisue
+# date:            2011/03/22
+#
+from setuptools import setup, find_packages
+
+version = "0.1rc2"
+
+def read(filename):
+    import os.path
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name="django-universaltag",
+    version=version,
+    description = "Universal tagging library for Django",
+    long_description=read('README.rst'),
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    keywords = "django tagging universal tag",
+    author = "Alisue",
+    author_email = "lambdalisue@hashnote.net",
+    url=r"https://github.com/lambdalisue/django-universaltag",
+    download_url = r"https://github.com/lambdalisue/django-universaltag/tarball/master",
+    license = 'BSD',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires=['distribute'],
+    dependency_links = [
+        r"https://bitbucket.org/lambdalisue/django-piston/get/a40885f1da15.tar.gz#egg=django-piston",
+    ]
+)
+
