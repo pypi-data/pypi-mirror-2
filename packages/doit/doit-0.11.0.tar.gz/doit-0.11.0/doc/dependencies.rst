9f6e10f60e0d88277fab2a60a098074ca3ccf0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:10 +0000

    Add package doit/0.11.0/doit-0.11.0.tar.gz

---
+++ packages/doit/doit-0.11.0.tar.gz/doit-0.11.0/doc/dependencies.rst
@@ -0,0 +1,226 @@
+======================
+Dependencies & Targets
+======================
+
+up-to-date
+----------
+
+One of the main ideas of `doit` and other build-tools is to check if the tasks/targets are **up-to-date**. In case there is no modification in the dependencies and the targets already exist, it skip the task execution to save time, as it would produce the same output from the previous run.
+
+Dependency
+  A dependency indicates an input to the task execution.
+
+Target
+  A *target* is the result/output file produced by the task execution.
+
+
+i.e. In a compilation task the source file is a *file_dep*, the object file is a *target*.
+
+.. literalinclude:: tutorial/compile.py
+
+
+`doit` automatically keeps track of dependencies. It saves the signature (MD5) of the dependencies every time the task is completed successfully.
+
+So if there are no modifications to the dependencies and you run `doit` again. The execution of the task's actions is skipped.
+
+
+.. code-block:: console
+
+  $ doit
+  .  compile
+  $ doit
+  -- compile
+
+Note the ``--`` (2 dashes, one space) on the command output on the second time it is executed. It means, this task was up-to-date and not executed.
+
+
+file_dep
+---------
+
+Different from most build-tools dependencies are on tasks not on targets. So `doit` can take advantage of the "execute only if not up-to-date" feature even for tasks that not define targets.
+
+Lets say you work with a dynamic language (python in this example). You don't need to compile anything but you probably wants to apply a lint-like tool (`PyChecker <http://pychecker.sourceforge.net/>`_) your source code files. You can define the source code as a dependency to the task.
+
+
+.. literalinclude:: tutorial/checker.py
+
+.. code-block:: console
+
+   $ doit
+   .  checker
+   $ doit
+   -- checker
+
+Note the ``--`` again to indicate the execution was skipped.
+
+Traditional build-tools can only handle files as "dependencies". `doit` has several ways to check for dependencies.
+
+
+targets
+-------
+
+Targets can be any file path (a file or folder). If a target doesn't exist the task will be executed. There is no limitation on the number of targets a task may define.
+
+Lets take the compilation example again.
+
+.. literalinclude:: tutorial/compile.py
+
+If there are no changes in the dependency the task execution is skipped. But if the target is removed the task is also executed again. But only if does not exist. If the target is modified but the dependencies do not change the task is not executed again.
+
+.. code-block:: console
+
+    $ doit
+    .  compile
+    $ doit
+    -- compile
+    $ rm main.o
+    $ doit
+    .  compile
+    $ echo xxx > main.o
+    $ doit
+    -- compile
+
+
+result-dependency
+----------------------
+
+In some cases you can not determine if a task is "up-to-date" only based on some input files, the input could come from a database or an extrernal process. *doit* defines a "result-dependency" to deal with these cases without need to create an intermediate file with the reulsts of the process.
+
+i.e. Suppose you want to send an email everytime you run *doit* on a bazaar repository that contains a new revision number.
+
+.. literalinclude:: tutorial/taskresult.py
+
+Note the `result_dep` with the name of the task (version). `doit` will keep track of the output of the task *version* and will execute *send_email* only when the bazaar repository has a new version since last time *doit* was executed.
+
+The "result" from the dependent task compared between different runs is given by its last action. The content for python-action is the value of the returned string. For cmd-actions is the output send to stdout plus stderr.
+
+
+
+uptodate
+----------
+
+`doit` also supports checking if a task is up-to-date directly by the parameter `uptodate`.
+
+This can be used in cases where you do not check for changes in dependencies. i.e. you check if a value in a database is smaller than 10.
+
+`uptodate` is a list where each element can be True, False, or None.
+
+ * ``False`` indicates that the task is NOT up-to-date
+ * ``True`` indicates that the task is up-to-date
+ * ``None`` values will just be ignored. This is used when the value is dinamically calculated
+
+.. note::
+
+  This often used with calc_deps (see below).
+
+
+
+run-once
+--------
+
+Sometimes there is no dependency for a task but you do not want to execute it all the time. If you use ``True`` in "run_once" the task will not be executed again after the first successful run. This is mostly used together with targets.
+
+Suppose you need to download something from internet. There is no dependency though you do not want to download it many times.
+
+.. literalinclude:: tutorial/download.py
+
+Note that even with *run_once* the file will be downloaded again in case the target is removed.
+
+.. code-block:: console
+
+    $ doit
+    .  get_pylogo
+    $ doit
+    -- get_pylogo
+    $ rm python-logo.gif
+    $ doit
+    .  get_pylogo
+
+
+.. note::
+
+  A task is **not** up-to-date if any of `file_dep`, `result_dep`, `run_once` or `uptodate` is not up-to-date. If a task does not define any of these dependencies it will always be executed.
+
+
+calculated-dependencies
+------------------------
+
+Calculation of dependencies might be an expensive operation, so not suitable to be done on task-generators. For this situation is better to delegate the calculation of dependencies to another task. The task calcutating dependencies must have a python-action returning a dictionary with `file_dep`, `task_dep`, `result_dep`, `uptodate` or another `calc_dep`.
+
+On the example below ``mod_deps`` prints on the screen all direct dependencies from a module. The dependencies itself are calculated on task ``get_dep`` (note: get_dep has a fake implementation where the results are taken from a dict).
+
+
+.. literalinclude:: tutorial/calc_dep.py
+
+
+task-dependency
+---------------
+
+It is used to enforce tasks are executed on the desired order. By default tasks are executed on the same order as they were defined in the `dodo` file. To define a dependency on another task use the task name (whatever comes after ``task_`` on the function name) in the "task_dep" attribute.
+
+
+.. note::
+
+  A *task-dependency* **only** indicates that another task should be "executed" before itself. The task-dependency might not really be executed if it is *up-to-date*.
+
+.. note::
+
+  *task-dependency* s are **not** used to determine if a task is up-to-date or not. If a task defines only *task-dependency* it will always be executed.
+
+
+This example we make sure we include a file with the latest revision number of the bazaar repository on the tar file.
+
+.. literalinclude:: tutorial/tar.py
+
+.. code-block:: console
+
+    $ doit
+    .  version
+    .  tar
+
+
+groups
+^^^^^^^
+
+You can define group of tasks by adding tasks as dependencies and setting its `actions` to ``None``.
+
+.. literalinclude:: tutorial/group.py
+
+Note that tasks are never executed twice in the same "run".
+
+
+getargs
+--------
+
+`getargs` provides a way to use values computed in one task from another task.
+
+.. literalinclude:: tutorial/getargs.py
+
+
+
+keywords on actions
+--------------------
+
+It is common situation to use task information such as *targets*, *dependencies*, or *changed* in its own actions. Note: Dependencies are only *file-dependencies*.
+
+For *cmd-action* you can use the python notation for keyword substitution on strings. The string will contain all values separated by a space (" ").
+
+For *python-action* create a parameter in the function `doit` will take care of passing the value when the function is called. The values are passed as list of strings.
+
+.. literalinclude:: tutorial/hello.py
+
+
+
+Environment setup
+---------------------
+
+Some tasks require some kind of environment setup. Tasks may have a list of "setup" task.
+
+* the setup-task will be executed only if the task is to be executed (not up-to-date)
+* setup-tasks are just normal tasks that follow all other task behavior
+
+Task may also define 'teardown' actions. These actions are executed after all tasks have finished their execution.
+
+Example:
+
+.. literalinclude:: tutorial/tsetup.py
---
+++ packages/doit/doit-0.11.0.tar.gz/doit-0.11.0/doit/task.py
@@ -0,0 +1,420 @@
+"""Tasks are the main abstractions managed by doit"""
+
+import types
+import os
+import copy
+
+from doit import cmdparse
+from doit.exceptions import CatchedException, InvalidTask
+from doit.action import create_action
+
+
+class Task(object):
+    """Task
+
+    @ivar name string
+    @ivar actions: list - L{BaseAction}
+    @ivar clean_actions: list - L{BaseAction}
+    @ivar teardown (list - L{BaseAction})
+    @ivar targets: (list -string)
+    @ivar task_dep: (list - string)
+    @ivar wild_dep: (list - string) task dependency using wildcard *
+    @ivar file_dep: (set - string)
+    @ivar result_dep: (list -string)
+    @ivar calc_dep: (list - string) reference to a task
+    @ivar calc_dep_stack: (list - string) unprocessed calc_dep
+    @ivar dep_changed (list - string): list of file-dependencies that changed
+          (are not up_to_date). this must be set before
+    @ivar run_once: (bool) task without dependencies should run
+    @ivar uptodate: (list - bool/None) use bool/computed value instead of
+                                       checking dependencies
+    @ivar setup_tasks (list - string): references to task-names
+    @ivar is_subtask: (bool) indicate this task is a subtask.
+    @ivar result: (str) last action "result". used to check task-result-dep
+    @ivar values: (dict) values saved by task that might be used by other tasks
+    @ivar getargs: (dict) values from other tasks
+    @ivar doc: (string) task documentation
+
+    @ivar options: (dict) calculated params values (from getargs and taskopt)
+    @ivar taskopt: (cmdparse.Command)
+    @ivar custom_title: function reference that takes a task object as
+                        parameter and returns a string.
+    @ivar run_status (str): contains the result of Dependency.get_status
+            modified by runner, value can be:
+           - None: not processed yet
+           - run: task is selected to be executed (it might be running or
+                   waiting for setup)
+           - ignore: task wont be executed (user forced deselect)
+           - up-to-date: task wont be executed (no need)
+           - done: task finished its execution
+    """
+
+    DEFAULT_VERBOSITY = 1
+
+    # list of valid types/values for each task attribute.
+    valid_attr = {'name': ([str], []),
+                  'actions': ([list, tuple], [None]),
+                  'file_dep': ([list, tuple], []),
+                  'task_dep': ([list, tuple], []),
+                  'result_dep': ([list, tuple], []),
+                  'uptodate': ([list, tuple], []),
+                  'run_once': ([bool], []),
+                  'calc_dep': ([list, tuple], []),
+                  'targets': ([list, tuple], []),
+                  'setup': ([list, tuple], []),
+                  'clean': ([list, tuple], [True]),
+                  'teardown': ([list, tuple], []),
+                  'doc': ([str], [None]),
+                  'params': ([list, tuple], []),
+                  'verbosity': ([], [None,0,1,2]),
+                  'getargs': ([dict], []),
+                  'title': ([types.FunctionType], [None]),
+                  }
+
+
+    def __init__(self, name, actions, file_dep=(), targets=(),
+                 task_dep=(), result_dep=(), uptodate=(), run_once=False,
+                 calc_dep=(), setup=(), clean=(), teardown=(), is_subtask=False,
+                 doc=None, params=(), verbosity=None, title=None, getargs=None):
+        """sanity checks and initialization
+
+        @param params: (list of option parameters) see cmdparse.Command.__init__
+        """
+
+        getargs = getargs or {} #default
+        # check task attributes input
+        my_locals = locals()
+        for attr, valid_list in self.valid_attr.iteritems():
+            self.check_attr_input(name, attr, my_locals[attr], valid_list)
+
+        self.name = name
+        self.targets = targets
+        self.uptodate = list(uptodate)
+        self.run_once = run_once
+        self.is_subtask = is_subtask
+        self.result = None
+        self.values = {}
+        self.verbosity = verbosity
+        self.custom_title = title
+        self.getargs = getargs
+        self.setup_tasks = list(setup)
+
+        # options
+        self.taskcmd = cmdparse.TaskOption(name, params, None, None)
+        # put default values on options. this will be overwritten, if params
+        # options were passed on the command line.
+        self.options = self.taskcmd.parse('')[0] # ignore positional parameters
+
+        # actions
+        if actions is None:
+            self.actions = []
+        else:
+            self.actions = [create_action(a, self) for a in actions]
+
+        # clean
+        if clean is True:
+            self._remove_targets = True
+            self.clean_actions = ()
+        else:
+            self._remove_targets = False
+            self.clean_actions = [create_action(a, self) for a in clean]
+
+        # teardown
+        self.teardown = [create_action(a, self) for a in teardown]
+
+        # dependencies
+        self.dep_changed = None
+
+        # file_dep
+        self.file_dep = set()
+        self._expand_file_dep(file_dep)
+
+        # task_dep
+        self.task_dep = []
+        self.wild_dep = []
+        self._expand_task_dep(task_dep)
+
+        # result_dep
+        self.result_dep = []
+        self._expand_result_dep(result_dep)
+
+        # calc_dep
+        self.calc_dep = []
+        self.calc_dep_stack = []
+        self._expand_calc_dep(calc_dep)
+
+        self._init_getargs()
+        self.doc = self._init_doc(doc)
+
+        self.run_status = None
+
+
+    def _expand_file_dep(self, file_dep):
+        """put input into file_dep"""
+        for dep in file_dep:
+
+            if not isinstance(dep, basestring):
+                raise InvalidTask("%s. file_dep must be a str got '%r' (%s)" %
+                                  (self.name, dep, type(dep)))
+
+            if dep not in self.file_dep:
+                self.file_dep.add(dep)
+
+        # run_once can't be used together with file dependencies
+        if self.run_once and self.file_dep:
+            msg = ("%s. task cant have file and dependencies and run_once " +
+                   "at the same time. (just remove run_once)")
+            raise InvalidTask(msg % self.name)
+
+
+    def _expand_task_dep(self, task_dep):
+        """convert task_dep input into actaul task_dep and wild_dep"""
+        for dep in task_dep:
+            if "*" in dep:
+                self.wild_dep.append(dep)
+            else:
+                self.task_dep.append(dep)
+
+    def _expand_result_dep(self, result_dep):
+        """convert ressult_dep input into restul_dep and task_dep"""
+        self.result_dep.extend(result_dep)
+        # result_dep are also task_dep
+        self.task_dep.extend(result_dep)
+
+
+    def _expand_calc_dep(self, calc_dep):
+        """calc_dep input"""
+        for dep in calc_dep:
+            if dep not in self.calc_dep:
+                self.calc_dep.append(dep)
+                self.calc_dep_stack.append(dep)
+
+
+    def update_deps(self, deps):
+        """expand all kinds of dep input"""
+        #if "test_action" in self.name:
+        #    from doit.tools import set_trace;set_trace()
+        for dep, dep_values in deps.iteritems():
+            if dep == 'task_dep':
+                self._expand_task_dep(dep_values)
+            elif dep == 'file_dep':
+                self._expand_file_dep(dep_values)
+            elif dep == 'result_dep':
+                self._expand_result_dep(dep_values)
+            elif dep == 'calc_dep':
+                self._expand_calc_dep(dep_values)
+            elif dep == 'uptodate':
+                self.uptodate.extend(dep_values)
+
+    def _init_getargs(self):
+        """task getargs attribute define implicit task dependencies"""
+        for key, desc in self.getargs.iteritems():
+            # check format
+            parts = desc.rsplit('.', 1)
+            if len(parts) != 2:
+                msg = ("Taskid '%s' - Invalid format for getargs of '%s'.\n" %
+                       (self.name, key) +
+                       "Should be <taskid>.<argument-name> got '%s'\n" % desc)
+                raise InvalidTask(msg)
+            if parts[0] not in self.task_dep:
+                self.task_dep.append(parts[0])
+
+
+    @staticmethod
+    def _init_doc(doc):
+        """process task "doc" attribute"""
+        # store just first non-empty line as documentation string
+        if doc is not None:
+            for line in doc.splitlines():
+                striped = line.strip()
+                if striped:
+                    return striped
+        return ''
+
+
+    @staticmethod
+    def check_attr_input(task, attr, value, valid):
+        """check input task attribute is correct type/value
+
+        @param task (string): task name
+        @param attr (string): attribute name
+        @param value: actual input from user
+        @param valid (list): of valid types/value accepted
+        @raises InvalidTask if invalid input
+        """
+        value_type = type(value)
+        if value_type in valid[0]:
+            return
+        if value in valid[1]:
+            return
+
+        # input value didnt match any valid type/value, raise execption
+        msg = "Task '%s' attribute '%s' must be " % (task, attr)
+        accept = ", ".join([getattr(v, '__name__', str(v)) for v in
+                            (valid[0] + valid[1])])
+        msg += "{%s} got:%r %s" % (accept, value, type(value))
+        raise InvalidTask(msg)
+
+
+    def _get_out_err(self, out, err, verbosity):
+        """select verbosity to be used"""
+        priority = (verbosity, # use command line option
+                    self.verbosity, # or task default from dodo file
+                    self.DEFAULT_VERBOSITY) # or global default
+        use_verbosity = [v for v in  priority if v is not None][0]
+
+        out_err = [(None, None), # 0
+                   (None, err),  # 1
+                   (out, err)]   # 2
+        return out_err[use_verbosity]
+
+
+    def execute(self, out=None, err=None, verbosity=None):
+        """Executes the task.
+        @return failure: see CmdAction.execute
+        """
+        task_stdout, task_stderr = self._get_out_err(out, err, verbosity)
+        for action in self.actions:
+            action_return = action.execute(task_stdout, task_stderr)
+            if isinstance(action_return, CatchedException):
+                return action_return
+            self.result = action.result
+            self.values.update(action.values)
+
+
+    def execute_teardown(self, out=None, err=None, verbosity=None):
+        """Executes task's teardown
+        @return failure: see CmdAction.execute
+        """
+        task_stdout, task_stderr = self._get_out_err(out, err, verbosity)
+        for action in self.teardown:
+            action_return = action.execute(task_stdout, task_stderr)
+            if isinstance(action_return, CatchedException):
+                return action_return
+
+
+    def clean(self, outstream, dryrun):
+        """Execute task's clean
+        @ivar outstream: 'write' output into this stream
+        @ivar dryrun (bool): if True clean tasks are not executed
+                             (just print out what would be executed)
+        """
+        # if clean is True remove all targets
+        if self._remove_targets is True:
+            files = [path for path in self.targets if os.path.isfile(path)]
+            dirs = [path for path in self.targets if os.path.isdir(path)]
+
+            # remove all files
+            for file_ in files:
+                msg = "%s - removing file '%s'\n" % (self.name, file_)
+                outstream.write(msg)
+                if not dryrun:
+                    os.remove(file_)
+
+            # remove all directories (if empty)
+            for dir_ in dirs:
+                if os.listdir(dir_):
+                    msg = "%s - cannot remove (it is not empty) '%s'\n"
+                    outstream.write(msg % (self.name, dir_))
+                else:
+                    msg = "%s - removing dir '%s'\n"
+                    outstream.write(msg % (self.name, dir_))
+                    if not dryrun:
+                        os.rmdir(dir_)
+
+        else:
+            # clean contains a list of actions...
+            for action in self.clean_actions:
+                msg = "%s - executing '%s'\n"
+                outstream.write(msg % (self.name, action))
+                if not dryrun:
+                    action.execute()
+
+
+    def title(self):
+        """String representation on output.
+
+        @return: (str) Task name and actions
+        """
+        if self.custom_title:
+            return self.custom_title(self)
+        return self.name
+
+
+    def __repr__(self):
+        return "<Task: %s>"% self.name
+
+
+    # when using multiprocessing Tasks are pickled.
+    def __getstate__(self):
+        """remove attributes that might contain unpickleble content
+        mostly probably closures
+        """
+        to_pickle = self.__dict__.copy()
+        del to_pickle['actions']
+        del to_pickle['clean_actions']
+        del to_pickle['teardown']
+        del to_pickle['custom_title']
+        return to_pickle
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def update_from_pickle(self, pickle_obj):
+        """update self with data from pickled Task"""
+        self.__dict__.update(pickle_obj.__dict__)
+
+    def clone(self):
+        """create a deep copy of this task"""
+        inst =  self.__class__.__new__(self.__class__)
+        inst.name = self.name
+        inst.targets = self.targets[:]
+        inst.uptodate = self.uptodate[:]
+        inst.run_once = self.run_once
+        inst.is_subtask = self.is_subtask
+        inst.result = self.result
+        inst.values = self.values.copy()
+        inst.verbosity = self.verbosity
+        inst.custom_title = self.custom_title
+        inst.getargs = copy.copy(self.getargs)
+        inst.setup_tasks = self.setup_tasks[:]
+        inst.taskcmd = self.taskcmd
+        inst.options = self.options.copy()
+        inst.actions = [a.clone(inst) for a in self.actions]
+        inst._remove_targets = self._remove_targets
+        inst.clean_actions = [a.clone(inst) for a in self.clean_actions]
+        inst.teardown = [a.clone(inst) for a in self.teardown]
+        inst.dep_changed = self.dep_changed
+        inst.file_dep = copy.copy(self.file_dep)
+        inst.task_dep = self.task_dep[:]
+        inst.wild_dep = self.wild_dep[:]
+        inst.result_dep = self.result_dep[:]
+        inst.calc_dep = self.calc_dep[:]
+        inst.calc_dep_stack = self.calc_dep_stack[:]
+        inst.doc = self.doc
+        inst.run_status = self.run_status
+        return inst
+
+def dict_to_task(task_dict):
+    """Create a task instance from dictionary.
+
+    The dictionary has the same format as returned by task-generators
+    from dodo files.
+
+    @param task_dict (dict): task representation as a dict.
+    @raise InvalidTask: If unexpected fields were passed in task_dict
+    """
+    # check required fields
+    if 'actions' not in task_dict:
+        raise InvalidTask("Task %s must contain 'actions' field. %s" %
+                          (task_dict['name'],task_dict))
+
+    # user friendly. dont go ahead with invalid input.
+    task_attrs = task_dict.keys()
+    valid_attrs = set(Task.valid_attr.iterkeys())
+    for key in task_attrs:
+        if key not in valid_attrs:
+            raise InvalidTask("Task %s contains invalid field: '%s'"%
+                              (task_dict['name'],key))
+
+    return Task(**task_dict)
