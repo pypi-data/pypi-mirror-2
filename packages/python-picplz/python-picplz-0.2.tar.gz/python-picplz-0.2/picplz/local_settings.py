50f4c30ac108d2979ae350a6c01fcfa47e4e9caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:57 +0000

    Add package python-picplz/0.2/python-picplz-0.2.tar.gz

---
+++ packages/python-picplz/python-picplz-0.2.tar.gz/python-picplz-0.2/picplz/local_settings.py
@@ -0,0 +1,3 @@
+PICPLZ_KEY='3Qs8UWCW3Q5d4EYMwTjqUR5zg3yu63AR'
+PICPLZ_SECRET='prswetgqHMzuCcUjcnGxPE3b9ca6JfKB'
+PICPLZ_REDITECT_URI='http://trove.whitestar.jemery.com:8000/identity/picplz/login/complete'
