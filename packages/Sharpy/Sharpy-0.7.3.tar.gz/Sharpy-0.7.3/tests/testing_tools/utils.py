56f8acaf1ec4438b9427dd88fe11c93d0409af15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:39 +0000

    Add package Sharpy/0.7.3/Sharpy-0.7.3.tar.gz

---
+++ packages/Sharpy/Sharpy-0.7.3.tar.gz/Sharpy-0.7.3/tests/testing_tools/utils.py
@@ -0,0 +1,20 @@
+import httplib2
+from time import time
+
+from testconfig import config
+
+def clear_users():
+    username = config['cheddar']['username']
+    password = config['cheddar']['password']
+    product = config['cheddar']['product_code']
+    endpoint = config['cheddar']['endpoint']
+
+    h = httplib2.Http()
+    h.add_credentials(username, password)
+    
+    url = '%s/customers/delete-all/confirm/%d/productCode/%s' % (endpoint, int(time()), product)
+    
+    response, content = h.request(url, 'POST')
+    
+    if response.status != 302:
+        raise Exception('Could not clear users.  Recieved a response of %s %s ' % response.status, response.reason)
