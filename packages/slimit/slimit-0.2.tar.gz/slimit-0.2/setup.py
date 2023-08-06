fb5b2f70f6c04d02a1bc0a32f561fd20396f58ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:46 +0000

    Add package slimit/0.2/slimit-0.2.tar.gz

---
+++ packages/slimit/slimit-0.2.tar.gz/slimit-0.2/setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+classifiers = """\
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Software Development :: Compilers
+Operating System :: Unix
+"""
+
+def read(*rel_names):
+    return open(os.path.join(os.path.dirname(__file__), *rel_names)).read()
+
+
+setup(
+    name='slimit',
+    version='0.2',
+    url='http://slimit.org',
+    license='MIT',
+    description='SlimIt - JavaScript minifier',
+    author='Ruslan Spivak',
+    author_email='ruslan.spivak@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['distribute', 'ply>=3.4'],
+    zip_safe=False,
+    entry_points="""\
+    [console_scripts]
+    """,
+    classifiers=filter(None, classifiers.split('\n')),
+    long_description=read('README.rst') + '\n\n' + read('CHANGES'),
+    extras_require={'test': []}
+    )
