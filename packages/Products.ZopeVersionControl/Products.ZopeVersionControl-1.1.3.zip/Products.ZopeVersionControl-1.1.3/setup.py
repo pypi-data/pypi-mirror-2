2bc01c2cf250fb76eab5017266c83d434fa85109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:25 +0000

    Add package Products.ZopeVersionControl/1.1.3/Products.ZopeVersionControl-1.1.3.zip

---
+++ packages/Products.ZopeVersionControl/Products.ZopeVersionControl-1.1.3.zip/Products.ZopeVersionControl-1.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(name='Products.ZopeVersionControl',
+      version='1.1.3',
+      description="Zope Version Control",
+      long_description=open("CHANGES.txt").read(),
+      classifiers=[
+        'Framework :: Zope2',
+      ],
+      license='ZPL',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      url='http://pypi.python.org/pypi/Products.ZopeVersionControl',
+      packages=find_packages(),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'zope.interface',
+        'Acquisition',
+        'DateTime',
+        'transaction',
+        'ZODB3',
+        'Zope2',
+      ],
+      )
