72c726520aab5bf3d02cb212814b9cdd6a0043a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:51 +0000

    Add package askbot/0.6.73/askbot-0.6.73.tar.gz

---
+++ packages/askbot/askbot-0.6.73.tar.gz/askbot-0.6.73/askbot/tests/db_api_tests.py
@@ -0,0 +1,214 @@
+"""Tests database api - the basic data entry 
+functions that happen on behalf of users
+
+e.g. ``some_user.do_something(...)``
+"""
+from askbot.tests.utils import AskbotTestCase
+from askbot import models
+from askbot.conf import settings as askbot_settings
+import datetime
+
+class DBApiTests(AskbotTestCase):
+
+    def setUp(self):
+        self.create_user()
+        self.create_user(username = 'other_user')
+        self.question = self.post_question()
+        self.now = datetime.datetime.now()
+
+    def post_answer(self, user = None, question = None):
+        if user is None:
+            user = self.user
+        if question is None:
+            question = self.question
+
+        self.answer = super(DBApiTests, self).post_answer(
+                                                user = user,
+                                                question = question,
+                                            )
+
+    def assert_post_is_deleted(self, post):
+        self.assertTrue(post.deleted == True)
+        self.assertTrue(isinstance(post.deleted_by, models.User))
+        self.assertTrue(post.deleted_at is not None)
+
+    def assert_post_is_not_deleted(self, post):
+        self.assertTrue(post.deleted == False)
+        self.assertTrue(post.deleted_by == None)
+        self.assertTrue(post.deleted_at == None)
+
+    def test_flag_question(self):
+        self.user.set_status('m')
+        self.user.flag_post(self.question)
+        self.assertEquals(
+            self.user.get_flags().count(),
+            1
+        )
+
+    def test_flag_answer(self):
+        self.post_answer()
+        self.user.set_status('m')
+        self.user.flag_post(self.answer)
+        self.assertEquals(
+            self.user.get_flags().count(),
+            1
+        )
+
+    def ask_anonymous_question(self):
+        q = self.user.post_question(
+                        is_anonymous = True,
+                        body_text = 'hahahah',
+                        title = 'aouaouaosuoa',
+                        tags = 'test'
+                    )
+        return self.reload_object(q)
+
+    def test_post_anonymous_question(self):
+        q = self.ask_anonymous_question()
+        self.assertTrue(q.is_anonymous)
+        rev = q.revisions.all()[0]
+        self.assertTrue(rev.is_anonymous)
+
+    def test_reveal_asker_identity(self):
+        q = self.ask_anonymous_question()
+        self.other_user.set_status('m')
+        self.other_user.save()
+        self.other_user.edit_question(
+                            question = q,
+                            title = 'hahah',
+                            body_text = 'hoeuaoea',
+                            tags = 'aoeuaoeu',
+                            revision_comment = 'hahahah'
+                        )
+        q.remove_author_anonymity()
+        q = self.reload_object(q)
+        self.assertFalse(q.is_anonymous)
+        for rev in q.revisions.all():
+            self.assertFalse(rev.is_anonymous)
+
+    def test_accept_best_answer(self):
+        self.post_answer(user = self.other_user)
+        self.user.accept_best_answer(self.answer)
+
+    def test_delete_question(self):
+        self.user.delete_question(self.question)
+        self.assert_post_is_deleted(self.question)
+
+    def test_restore_question(self):
+        self.question.deleted = True
+        self.question.deleted_by = self.user
+        self.question.deleted_at = self.now
+        self.question.save()
+        self.user.restore_post(self.question)
+        self.assert_post_is_not_deleted(self.question)
+
+    def test_delete_answer(self):
+        self.post_answer(question = self.question)
+        self.user.delete_answer(self.answer)
+        self.assert_post_is_deleted(self.answer)
+        saved_question = models.Question.objects.get(id = self.question.id)
+        self.assertEquals(
+                saved_question.answer_count,
+                0
+            )
+
+    def test_restore_answer(self):
+        self.post_answer()
+        self.answer.deleted = True
+        self.answer.deleted_by = self.user
+        self.answer.deleted_at = self.now
+        self.answer.save()
+        self.user.restore_post(self.answer)
+        self.assert_post_is_not_deleted(self.answer)
+
+    def test_delete_question_with_answer_by_other(self):
+        self.post_answer(user = self.other_user)
+        self.user.delete_question(self.question)
+        self.assert_post_is_deleted(self.question)
+        answer_count = self.question.get_answers(user = self.user).count()
+        answer = self.question.answers.all()[0]
+        self.assert_post_is_not_deleted(answer)
+        self.assertTrue(answer_count == 1)
+        saved_question = models.Question.objects.get(id = self.question.id)
+        self.assertTrue(saved_question.answer_count == 1)
+
+    def test_unused_tag_is_auto_deleted(self):
+        self.user.retag_question(self.question, tags = 'one-tag')
+        tag = models.Tag.objects.get(name='one-tag')
+        self.assertEquals(tag.used_count, 1)
+        self.assertEquals(tag.deleted, False)
+        self.user.retag_question(self.question, tags = 'two-tag')
+
+        count = models.Tag.objects.filter(name='one-tag').count()
+        self.assertEquals(count, 0)
+
+class UserLikeTests(AskbotTestCase):
+    def setUp(self):
+        self.create_user()
+        self.question = self.post_question(tags = 'one two three')
+
+    def test_user_likes_question_via_tags(self):
+        truth_table = (
+            ('good', 'like', True),
+            ('good', 'dislike', False),
+            ('bad', 'like', False),
+            ('bad', 'dislike', True),
+        )
+        tag = models.Tag.objects.get(name = 'one')
+        for item in truth_table:
+            reason = item[0]
+            mt = models.MarkedTag(user = self.user, tag = tag, reason = reason)
+            mt.save()
+            self.assertEquals(
+                self.user.has_affinity_to_question(
+                    question = self.question,
+                    affinity_type = item[1]
+                ),
+                item[2]
+            )
+            mt.delete()
+
+    def setup_wildcard(self, wildcard = None, reason = None):
+        if reason == 'good':
+            self.user.interesting_tags = wildcard
+            self.user.ignored_tags = ''
+        else:
+            self.user.ignored_tags = wildcard
+            self.user.interesting_tags = ''
+        self.user.save()
+        askbot_settings.update('USE_WILDCARD_TAGS', True)
+
+    def assert_affinity_is(self, affinity_type, expectation):
+        self.assertEquals(
+            self.user.has_affinity_to_question(
+                question = self.question,
+                affinity_type = affinity_type
+            ),
+            expectation
+        )
+
+    def test_user_likes_question_via_wildcards(self):
+        self.setup_wildcard('on*', 'good')
+        self.assert_affinity_is('like', True)
+        self.assert_affinity_is('dislike', False)
+
+        self.setup_wildcard('aouaou* o* on* oeu*', 'good')
+        self.assert_affinity_is('like', True)
+        self.assert_affinity_is('dislike', False)
+
+        self.setup_wildcard('on*', 'bad')
+        self.assert_affinity_is('like', False)
+        self.assert_affinity_is('dislike', True)
+
+        self.setup_wildcard('aouaou* o* on* oeu*', 'bad')
+        self.assert_affinity_is('like', False)
+        self.assert_affinity_is('dislike', True)
+        
+        self.setup_wildcard('one*', 'good')
+        self.assert_affinity_is('like', True)
+        self.assert_affinity_is('dislike', False)
+
+        self.setup_wildcard('oneone*', 'good')
+        self.assert_affinity_is('like', False)
+        self.assert_affinity_is('dislike', False)
+
