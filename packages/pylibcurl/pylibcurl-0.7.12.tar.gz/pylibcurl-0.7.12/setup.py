d4031a7dfb7c69cc6ba4c0a98b0d301cdebd819d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:40 +0000

    Add package pylibcurl/0.7.12/pylibcurl-0.7.12.tar.gz

---
+++ packages/pylibcurl/pylibcurl-0.7.12.tar.gz/pylibcurl-0.7.12/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '0.7.12'
+
+setup(
+    name='pylibcurl',
+    version=version,
+    description='python ctypes curl library',
+    author='Askar Yusupov',
+    author_email='devex.soft@gmail.com',
+    url='http://bitbucket.org/jungle/pylibcurl',
+    download_url='http://bitbucket.org/jungle/pylibcurl/downloads',
+    license='MIT',
+    packages=['pylibcurl'],
+    keywords='curl ctypes',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
+
