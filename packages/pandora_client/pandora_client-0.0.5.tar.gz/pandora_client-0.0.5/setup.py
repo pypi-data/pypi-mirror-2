a609b379026c039315c6e035ef05a830240a1950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:51 +0000

    Add package pandora_client/0.0.5/pandora_client-0.0.5.tar.gz

---
+++ packages/pandora_client/pandora_client-0.0.5.tar.gz/pandora_client-0.0.5/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# vi:si:et:sw=4:sts=4:ts=4
+# encoding: utf-8
+from distutils.core import setup
+
+def get_bzr_version():
+    return 5 #import os
+    rev = int(os.popen('bzr revno').read())
+    if rev:
+        return u'%s' % rev
+    return u'unknown'
+
+setup(
+    name="pandora_client",
+    version="0.0.%s" % get_bzr_version() ,
+    description='''pandora_client - commandline client and python api for pan.do/ra
+
+can be used interact with pan.do/ra instance via its api.
+or instead of OxFF to keep archive and pan.do/ra instance in sync.
+''',
+    author="j",
+    author_email="j@mailb.org",
+    url="http://code.0x2620.org/pandora_client",
+    download_url="http://code.0x2620.org/pandora_client/download",
+    license="GPLv3",
+    scripts = [
+        'bin/pandora_client',
+    ],
+    packages=[
+        'pandora_client'
+    ],
+    keywords = [
+],
+    classifiers = [
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+    ],
+)
+
