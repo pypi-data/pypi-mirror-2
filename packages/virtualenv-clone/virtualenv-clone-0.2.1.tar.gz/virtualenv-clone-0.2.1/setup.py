9a45acf6e072796bc96099687c7bd164a88b6143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:58 +0000

    Add package virtualenv-clone/0.2.1/virtualenv-clone-0.2.1.tar.gz

---
+++ packages/virtualenv-clone/virtualenv-clone-0.2.1.tar.gz/virtualenv-clone-0.2.1/setup.py
@@ -0,0 +1,25 @@
+import sys
+
+from setuptools import setup
+
+if __name__ == '__main__' and sys.version_info < (2, 5):
+    raise SystemExit("Python >= 2.5 required for virtualenv-clone")
+
+
+setup(name="virtualenv-clone",
+    version='0.2.1',
+    description='script to clone virtualenvs.',
+    author='Edward George',
+    author_email='edwardgeorge@gmail.com',
+    url='http://github.com/edwardgeorge/virtualenv-clone',
+    py_modules=["clonevirtualenv"],
+    entry_points = {
+        'console_scripts': [
+            'virtualenv-clone=clonevirtualenv:main',
+    ]},
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+    ], )
