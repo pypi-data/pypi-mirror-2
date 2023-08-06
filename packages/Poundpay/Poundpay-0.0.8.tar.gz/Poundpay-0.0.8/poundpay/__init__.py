f19a0205f2d7467244980ab57649d4e0ce35697d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:31 +0000

    Add package Poundpay/0.0.8/Poundpay-0.0.8.tar.gz

---
+++ packages/Poundpay/Poundpay-0.0.8.tar.gz/Poundpay-0.0.8/poundpay/__init__.py
@@ -0,0 +1,11 @@
+from .client import Client
+from .developers import Developer
+from .resource import Resource
+from .payments import Payment
+
+
+__all__ = ['Client', 'Developer', 'Resource', 'Payment']
+
+
+def configure(developer_sid, auth_token, api_url=None, api_version=None):
+    Resource.client = Client(developer_sid, auth_token, api_url, api_version)
