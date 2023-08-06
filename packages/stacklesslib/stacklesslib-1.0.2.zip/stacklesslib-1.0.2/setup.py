481307291718c8f438ac4b09c0018ed26100605b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:28 +0000

    Add package stacklesslib/1.0.2/stacklesslib-1.0.2.zip

---
+++ packages/stacklesslib/stacklesslib-1.0.2.zip/stacklesslib-1.0.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+setup(
+    name = "stacklesslib",
+    packages = ["stacklesslib"],
+    version = "1.0.2",
+    description = "Standard Stackless Python supporting functionality",
+    author = "Richard Tew",
+    author_email = "richard.m.tew@gmail.com",
+    url = "http://code.google.com/p/sake/",
+    keywords = ["microthreads", "coroutines", "stackless", "monkeypatching"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+stacklesslib
+============
+
+This is a simple framework that layers useful functionality around the
+Stackless scheduler.  It's sole purpose is to implement the logic that
+an developer needs to implement themselves, to make best use of
+Stackless.
+
+ * A sleep function, to allow a tasklet to sleep for a period of time.
+ * Monkeypatching to make blocking IO block tasklets not threads.
+
+Changes
+-------
+
+Version 1.0.2 introduces the following changes:
+- Monkey-patching has been cleaned up.
+- Better integration of stacklessio networking resource.
+- Fixed source distribution archive which was missing subdirectories.
+- Isn't broken bad code, like 1.0.1.
+ """
+)
