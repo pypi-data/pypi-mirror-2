f275b8f4eddc256f6a374e197aab3b189fb30786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:13 +0000

    Add package Products.Poi/2.0.2/Products.Poi-2.0.2.zip

---
+++ packages/Products.Poi/Products.Poi-2.0.2.zip/Products.Poi-2.0.2/Products/Poi/browser/watchers.py
@@ -0,0 +1,20 @@
+from collective.watcherlist.interfaces import IWatcherList
+from Acquisition import aq_inner
+from Products.Five.browser import BrowserView
+
+
+class WatcherView(BrowserView):
+
+    def __call__(self):
+        context = aq_inner(self.context)
+        # Old style
+        #context.toggleWatching()
+        # New style
+        watchers = IWatcherList(context)
+        watchers.toggle_watching()
+        self.request.RESPONSE.redirect(context.absolute_url())
+
+    def is_watching(self):
+        context = aq_inner(self.context)
+        watchers = IWatcherList(context)
+        return watchers.isWatching()
