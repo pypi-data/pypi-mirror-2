bc823ecc8a5fd187e699cbf5687bb30bc7a18064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:05 +0000

    Add package makesite/0.6.2/makesite-0.6.2.tar.gz

---
+++ packages/makesite/makesite-0.6.2.tar.gz/makesite-0.6.2/makesite/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+VERSION_INFO = (0, 6, 2)
+
+__project__ = PROJECT = __name__
+__version__ = VERSION = '.'.join( str(i) for i in VERSION_INFO )
+__author__ = AUTHOR = "Kirill Klenov <horneds@gmail.com>"
+__license__ = LICENSE = "GNU LGPL"
+
+INI_FILENAME = "makesite.ini"
+TEMPLATES_FILE = '.makesite'
---
+++ packages/makesite/makesite-0.6.2.tar.gz/makesite-0.6.2/makesite/templates/celeryd/service/celeryd_install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Variables
+PROGRAMM_NAME={{ project }}.{{ branch }}.celeryd
+DEPLOY_DIR={{ deploy_dir }}
+SUPERVISOR_CONFPATH={{ supervisor_confpath }}.celeryd.conf
+
+# Check supervisor installed
+which supervisord 1>/dev/null || {
+        echo "  * SUPERVISOR not found! Attempting to install..."
+        if [ -f /etc/lsb-release ] ; then
+                sudo apt-get install supervisor -y
+        elif [ -f /etc/fedora-release ] ; then
+                sudo yum install supervisor
+        elif [ -f /etc/debian_version ] ; then
+                sudo apt-get install supervisor
+        fi
+}
+
+echo '  * Create link to supervisor conf:'$SUPERVISOR_CONFPATH
+sudo ln -sf $DEPLOY_DIR/deploy/supervisor.celeryd.conf $SUPERVISOR_CONFPATH
+
+if [ -f /etc/init.d/supervisor ]; then
+    echo '  * Update supervisord for celeryd'
+    sudo supervisorctl reread
+    sudo supervisorctl reload $PROGRAMM_NAME
+fi
