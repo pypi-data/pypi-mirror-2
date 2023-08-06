1181bbc6792e91abd47288b11a12d1639260f9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:05 +0000

    Add package psphere/0.1.4/psphere-0.1.4.tar.gz

---
+++ packages/psphere/psphere-0.1.4.tar.gz/psphere-0.1.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='psphere',
+      version='0.1.4',
+      description='vSphere SDK for Python',
+      long_description=read('README'),
+      author='Jonathan Kinred',
+      author_email='jonathan.kinred@gmail.com',
+      url='http://bitbucket.org/jkinred/psphere',
+      packages=['psphere'],
+      install_requires=['suds'],
+      keywords=['vsphere', 'vmware'],
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+      ],
+     )
