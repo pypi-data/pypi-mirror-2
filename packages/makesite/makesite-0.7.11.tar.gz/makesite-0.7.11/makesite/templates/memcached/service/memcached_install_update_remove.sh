6d0c9a22d9e09502c084394210240c65c5bddac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:23 +0000

    Add package makesite/0.7.11/makesite-0.7.11.tar.gz

---
+++ packages/makesite/makesite-0.7.11.tar.gz/makesite-0.7.11/makesite/templates/memcached/service/memcached_install_update_remove.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Variables
+MEMCACHED_HOST={{ memcached_host }}
+MEMCACHED_PORT={{ memcached_port }}
+
+if [ -z "$MEMCACHED_HOST" ] || [ -z "$MEMCACHED_PORT" ]; then
+    # pass
+    exit 0
+fi
+
+# Install memcached on localhost
+if [ $MEMCACHED_HOST = 'localhost' ]; then
+    sudo which memcached 1>/dev/null || {
+            echo "  * Memcache not found! Attempting to install..."
+            # Ubuntu
+            if [ -f /etc/lsb-release ] ; then
+                sudo apt-get install memcached -y
+
+            # Debian
+            elif [ -f /etc/debian_version ] ; then
+                sudo apt-get install memcached
+
+            # Fedora
+            elif [ -f /etc/fedora-release ] ; then
+                sudo yum install memcached
+            fi
+    }
+fi
+
+python -c "import memcache" 2>/dev/null || {
+    echo "  * Python memcache not found! Attempting to install..."
+    sudo pip install python-memcached
+}
+
+echo "  * Flush memcahed '$MEMCACHED_HOST:$MEMCACHED_PORT'"
+python -c "import memcache; memcache.Client(['$MEMCACHED_HOST:$MEMCACHED_PORT']).flush_all()"
