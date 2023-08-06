2725839d876d7dd07da34b2c7d5d84e17e81b470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:28 +0000

    Add package mongate/1.0.1/mongate-1.0.1-py2.6.egg

---
+++ packages/mongate/mongate-1.0.1-py2.6.egg/mongate/database.py
@@ -0,0 +1,29 @@
+"""
+A database within MongoDB.
+"""
+
+from mongate.collection import Collection
+
+class Database(object):
+    """
+    A database within MongoDB.
+    """
+    
+    def __init__(self, name, connection):
+        self.__name = name
+        self.__connection = connection
+        
+    def get_name(self):
+        return self.__name
+        
+    def drop_collection(self, collection_name):
+        self[collection_name].remove()
+        
+    def __getitem__(self, key):
+        return self._return_collection(key)
+        
+    def __getattr__(self, key):
+        return self._return_collection(key)
+        
+    def _return_collection(self, key):
+        return Collection(key, self.__connection, self)
