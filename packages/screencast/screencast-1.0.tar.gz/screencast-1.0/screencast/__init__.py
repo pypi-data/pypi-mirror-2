3a83ecba15509f68580644d3af6578ad1e127ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:21 +0000

    Add package screencast/1.0/screencast-1.0.tar.gz

---
+++ packages/screencast/screencast-1.0.tar.gz/screencast-1.0/screencast/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+`webbrowser` Documentation:  http://docs.python.org/library/webbrowser.html
+
+This file should open up your web browser, create a new tab, and load my
+website.
+"""
+
+import webbrowser
+
+webbrowser.open('http://www.zachwill.com', new=2)
