0e36ac6e1dc1e83dcd26b4ac7318af0a86431a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:30 +0000

    Add package django-hudson-25/0.9.0/django-hudson-25-0.9.0.tar.gz

---
+++ packages/django-hudson-25/django-hudson-25-0.9.0.tar.gz/django-hudson-25-0.9.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-hudson-25',
+    version = '0.9.0',
+    author = 'Mikhail Podgurskiy',
+    author_email = 'kmmbvnr@gmail.com',
+    description = 'Plug and play continuous integration with django and hudson',
+    license = 'LGPL',
+    platforms = ['Any'],
+    keywords = ['pyunit', 'unittest', 'testrunner', 'hudson', 'django'],
+    url = 'http://github.com/kmmbvnr/django-hudson',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing'
+    ],
+    install_requires=[
+        'Django>=1.2',
+        'coverage>=3.4b1',
+        'pylint',
+    ],
+    packages = ['django_hudson', 'django_hudson.management', 'django_hudson.management.commands'],
+    package_data={'django_hudson': ['management/commands/pylint.rc']},
+    zip_safe = False,
+    include_package_data = True
+)
