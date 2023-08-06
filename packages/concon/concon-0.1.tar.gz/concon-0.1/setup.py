7962a319a8b1e44b31ea6c6173bbef33b823301d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:41 +0000

    Add package concon/0.1/concon-0.1.tar.gz

---
+++ packages/concon/concon-0.1.tar.gz/concon-0.1/setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'concon',
+      description = 'CONstrained CONtainers: immutable and append-only container subclasses and utilities.',
+      url = 'https://bitbucket.org/nejucomo/concon',
+      license = 'MIT (see LICENSE.txt)',
+      version = '0.1',
+      author = 'Nathan Wilcox',
+      author_email = 'nejucomo@gmail.com',
+      py_modules = ['concon'],
+      )
