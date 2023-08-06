1ac00492cb7ad4a59eb5b295b8202ba4e0964428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:24 +0000

    Add package fabtest/0.0.2/fabtest-0.0.2.tar.gz

---
+++ packages/fabtest/fabtest-0.0.2.tar.gz/fabtest-0.0.2/fabtest/utils.py
@@ -0,0 +1,21 @@
+from fabric import state
+from fabric.main import get_hosts
+from fabric.network import interpret_host_string, HostConnectionCache
+
+def fab(command, *args, **kwargs):
+    """ Runs fab command. Accepts callable. """
+
+    # clean the connection cache
+    state.connections = HostConnectionCache()
+
+    # collect results
+    results = []
+
+    # partially implement the logic from fabric.main.main
+    state.env.command = command.__name__
+    state.env.all_hosts = hosts = get_hosts(command, None, None)
+    for host in hosts:
+        interpret_host_string(host)
+        res = command(*args, **kwargs)
+        results.append(res)
+    return results
