1051cdc62660f26d4e8e14b5076dbcf3d62e31a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:27 +0000

    Add package pylcdui/0.5.8/pylcdui-0.5.8.tar.gz

---
+++ packages/pylcdui/pylcdui-0.5.8.tar.gz/pylcdui-0.5.8/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "pylcdui",
+    version = "0.5.8",
+    description = "Library for CrystalFontz and Matrix-Orbital LCD displays",
+    author = "mike wakerly",
+    author_email = "opensource@hoho.com",
+    url = "http://code.google.com/p/pylcdui/",
+    packages = [
+      'lcdui',
+      'lcdui.core',
+      'lcdui.ui',
+      'lcdui.devices',
+    ],
+)
