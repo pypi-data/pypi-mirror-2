8039b669e44b46858b2ca68453051ac749fa8c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:07 +0000

    Add package qtdjango/1.5.2/qtdjango-1.5.2.tar.gz

---
+++ packages/qtdjango/qtdjango-1.5.2.tar.gz/qtdjango-1.5.2/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import glob
+from distutils.core import setup
+
+"""
+py2app/py2exe build script for MyApplication.
+
+Will automatically ensure that all build prerequisites are available
+via ez_setup
+
+Usage (Mac OS X):
+    python setup.py py2app
+
+Usage (Windows):
+    python setup.py py2exe
+
+Usage (Ubuntu/Debian):
+    python setup.py --command-packages=stdeb.command bdist_deb
+
+Usage (RPM-based linux distros):
+    python setup.py bdist_rpm
+"""
+
+
+
+
+execfile('src/qtdjango/__init__.py')
+
+
+
+
+base_options = dict (name='qtdjango',
+      install_requires = ["django", ],
+      version=__version__,
+      description='''Library for connection of PyQt application to Django server,
+also reusable app.''',
+      author='Sergey Klimov',
+      author_email='dcdarv@gmail.com',
+      url='http://github.com/darvin/qtdjango',
+      package_dir = {'qtdjango': 'src/qtdjango'},
+      packages=['qtdjango',
+                'qtdjango.restclient',
+                'qtdjango.restclient.httplib2',
+                'qtdjango.django_qtdjango'],
+      license="GPL",
+      maintainer="Sergey Klimov",
+      maintainer_email="dcdarv@gmail.com",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          ],
+      
+     )
+
+options = base_options 
+
+
+
+setup( **options)
---
+++ packages/qtdjango/qtdjango-1.5.2.tar.gz/qtdjango-1.5.2/src/qtdjango/baseview.py
@@ -0,0 +1,35 @@
+
+# -*- coding: utf-8 -*-
+class BaseView(object):
+    '''
+    classdocs
+    '''
+    fields = ()
+    widgets = {}
+    model = None
+
+
+    exclude_fields = ("id")
+    """@cvar: Tuple of fields that must exclude from view"""
+
+    def __init__(self, **kwargs):
+        '''
+        Constructor
+        '''
+        self.model.add_notify(self)
+        if self.fields == ():
+            self.fields = [x for x in self.model.get_fields()]
+
+
+    def refresh(self):
+        """docstring for dataChanged"""
+        pass
+
+    def clean(self):
+        """Cleans temporary models of view"""
+        raise NotImplementedError
+
+
+    def save(self):
+        """Saves models changes in view"""
+        raise NotImplementedError
