f0b490f48c48cc2fada72115dd0475277d7cafee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:33 +0000

    Add package parameterized_testcase/0.1/parameterized_testcase-0.1.tar.gz

---
+++ packages/parameterized_testcase/parameterized_testcase-0.1.tar.gz/parameterized_testcase-0.1/setup.py
@@ -0,0 +1,19 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'parameterized_testcase',
+    version = '0.1',
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = 'Austin Bingham',
+    author_email = 'austin.bingham@gmail.com',
+    description = 'Create unittest-compatible parameterized testcases.',
+    license = 'MIT',
+    keywords = 'unittest',
+    url = 'http://code.google.com/p/parameterized-testcase/',
+    )
+
