b0e3c59f675484a4e2ae884c3a93664bdc02b5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:07 +0000

    Add package thebestspinner/0.1.1/thebestspinner-0.1.1.tar.gz

---
+++ packages/thebestspinner/thebestspinner-0.1.1.tar.gz/thebestspinner-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='thebestspinner',
+    description='A class aid use of The Best Spinner API',
+    version='0.1.1',
+    packages=['tbs',],
+    license='MIT',
+    long_description=open('README.rst').read(),
+    author='Peter Flood',
+    author_email='info@whywouldwe.com',
+    url='http://bitbucket.org/johnsmith/thebestspinner',
+)
+
