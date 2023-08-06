d0723fab1346b2565809287e628d4e5c5e11fafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:52 +0000

    Add package Datat/1.0.1/Datat-1.0.1.zip

---
+++ packages/Datat/Datat-1.0.1.zip/Datat-1.0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "Datat",
+      version = "1.0.1",
+      description = "Intuitive data tables, with translation to R data frames.",
+      author = "Thomas Kluyver",
+      author_email = "takowl@gmail.com",
+      url = "http://packages.python.org/Datat/",
+      packages = ["datat"])
