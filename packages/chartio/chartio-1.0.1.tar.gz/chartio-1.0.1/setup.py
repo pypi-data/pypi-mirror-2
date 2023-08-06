0c33b469fc790f3904d8fd984bde9d36df1b0298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:51 +0000

    Add package chartio/1.0.1/chartio-1.0.1.tar.gz

---
+++ packages/chartio/chartio-1.0.1.tar.gz/chartio-1.0.1/setup.py
@@ -0,0 +1,43 @@
+import os
+
+from distutils.core import setup
+from distutils.command.install import install
+
+
+CONF_LOCATION = '/etc/chartio.cfg'
+PIDFILE_LOCTATION = '/var/run/chartio_connect.pid'
+LOGFILE_LOCATION = '/var/log/chartio_connect.log'
+
+class PostInstall(install):
+
+    def run(self):
+        install.run(self)
+        # Touch files as root and make it writeable
+        for fi in [CONF_LOCATION, PIDFILE_LOCTATION, LOGFILE_LOCATION]:
+            os.system('touch %s' % fi)
+            os.system('chmod 666 %s' % fi)
+
+
+setup(  cmdclass=dict(install=PostInstall),
+        name='chartio',
+        version='1.0.1',
+        scripts=['chartio_setup.py', 'chartio_connect.py'],
+        classifiers = ['Environment :: Console',
+                       'Intended Audience :: System Administrators',
+                       'License :: OSI Approved :: BSD License',
+                       'Natural Language :: English',
+                       'Operating System :: POSIX',
+                       'Programming Language :: Python',
+                       'Programming Language :: Python :: 2.4',
+                       'Programming Language :: Python :: 2.5',
+                       'Programming Language :: Python :: 2.6',
+                       'Programming Language :: Python :: 2.7',
+                       'Topic :: System :: Monitoring',
+                       'Topic :: Database',
+                       'Topic :: Database :: Database Engines/Servers',
+                       ],
+        url="http://chart.io/", 
+        author="chart.io",
+        author_email="support@chart.io",
+        description="Setup wizard and connection client for connecting MySQL databases to chart.io",
+)
