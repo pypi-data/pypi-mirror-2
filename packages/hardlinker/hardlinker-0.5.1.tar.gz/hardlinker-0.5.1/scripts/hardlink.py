134a65bb4400bde028bbee3a6b8cc27e1f77a7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:10 +0000

    Add package hardlinker/0.5.1/hardlinker-0.5.1.tar.gz

---
+++ packages/hardlinker/hardlinker-0.5.1.tar.gz/hardlinker-0.5.1/scripts/hardlink.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os,sys
+major, minor = sys.version_info[:2]
+
+loc = os.path.abspath(__file__)
+path = '/'.join(loc.split('/')[:-2])
+path += 'lib/python%i.%i/site-packages' % (major, minor)
+
+import site
+site.addsitedir(path)
+
+import hardlinker.tools
+
+if __name__ == '__main__':
+    hardlinker.tools.main()
