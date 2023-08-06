224a3c2c934621d9c3d56b401ee3cdd55191c8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:33 +0000

    Add package curate/0.10/curate-0.10.tar.gz

---
+++ packages/curate/curate-0.10.tar.gz/curate-0.10/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.10'
+requirements=[
+    "ckanclient",
+    "pycurl",
+    "rdflib",
+    "FuXi>=1.2",
+    ]
+
+if sys.version_info.major == 2 and sys.version_info.minor < 7:
+    requirements.append("argparse")
+
+setup(
+    name='curate',
+    version=version,
+    description="Curation Utilities for CKAN",
+    long_description="""\
+Curation Utilities for CKAN""",
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Internet",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="ckan rdf rdflib",
+    author='Open Knowledge Foundation',
+    author_email='okfn-help@lists.okfn.org',
+    url="http://packages.python.org/curate/",
+    license='AGPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requirements,
+    entry_points="""
+        # -*- Entry points: -*-
+        [console_scripts]
+        curate=curate.command:curate
+
+        [curate.builtins]
+
+        [curate.actions]
+        httpReq=curate.actions:httpReq
+        curlReq=curate.actions:curlReq
+#        sparqlCheck=curate.builtins:sparqlCheck
+        addTag=curate.actions:addTag
+        delTag=curate.actions:delTag
+        addGroup=curate.actions:addGroup
+    """,
+)
