da097aabc6fb8edc44851af08f4f0dda683fb043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:38 +0000

    Add package ielectric.fab/0.1.4/ielectric.fab-0.1.4.tar.gz

---
+++ packages/ielectric.fab/ielectric.fab-0.1.4.tar.gz/ielectric.fab-0.1.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.4'
+
+setup(name='ielectric.fab',
+      version=version,
+      description="Personal fabric file",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGELOG").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://www.fubar.si',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ielectric'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'fabric',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
