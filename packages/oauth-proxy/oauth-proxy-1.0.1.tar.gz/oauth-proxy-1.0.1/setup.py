d7c547806a3a880000d404c1489a0e0083cba8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package oauth-proxy/1.0.1/oauth-proxy-1.0.1.tar.gz

---
+++ packages/oauth-proxy/oauth-proxy-1.0.1.tar.gz/oauth-proxy-1.0.1/setup.py
@@ -0,0 +1,33 @@
+"""OAuth Proxy"""
+
+from setuptools import setup, find_packages
+
+def refresh_plugin_cache():
+    from twisted.plugin import IPlugin, getPlugins
+    list(getPlugins(IPlugin))
+
+setup(
+    name="oauth-proxy",
+    version="1.0.1",
+    url="http://github.com/mojodna/oauth-proxy",
+    license="BSD License",
+    description="OAuth HTTP proxy",
+    long_description="An OAuth proxy server that signs requests w/ provided tokens and passes them on to their original destination.",
+    keywords="oauth proxy twisted",
+    packages=find_packages(),
+    package_data={
+        "twisted": ["plugins/proxy.py"]
+    },
+    scripts=["bin/oauth-proxy"],
+    install_requires=["twisted>=8.2.0", "oauth>=1.0.1"],
+    author="Seth Fitzsimmons",
+    author_email="seth@mojodna.net",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Topic :: Internet :: Proxy Servers",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
+
+refresh_plugin_cache()
