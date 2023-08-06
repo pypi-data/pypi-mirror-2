ea1e1ebeba368de5acc7918d4f017a85d1bb9f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:02 +0000

    Add package kombu/1.1.1/kombu-1.1.1.tar.gz

---
+++ packages/kombu/kombu-1.1.1.tar.gz/kombu-1.1.1/funtests/tests/test_beanstalk.py
@@ -0,0 +1,10 @@
+from funtests import transport
+
+
+class test_beanstalk(transport.TransportCase):
+    transport = "beanstalk"
+    prefix = "beanstalk"
+    event_loop_max = 10
+
+    def after_connect(self, connection):
+        connection.channel().client
