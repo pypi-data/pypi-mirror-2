a90d50aa87820e38e021aa2586d747d1527786d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:14 +0000

    Add package cclib/1.0.1b/cclib-1.0.1b.zip

---
+++ packages/cclib/cclib-1.0.1b.zip/cclib-1.0.1b/test/methods.py
@@ -0,0 +1,29 @@
+__revision__ = "$Rev: 628 $"
+
+import sys
+import unittest
+
+testmodules = ['testpopulation', 'testcda']
+
+def importname(modulename, name):
+    """Import from a module whose name is determined at runtime.
+
+    (Python Cookbook 2nd ed.)
+    """
+    module = __import__(modulename, globals(), locals(), [name])
+    if not module:
+        raise ImportError
+    return getattr(module, name)
+
+if __name__=="__main__":
+    fullsuite = unittest.TestSuite()
+    for testmodule in testmodules:
+        try:
+            tests = importname(testmodule, "tests")
+            for test in tests:
+                test = unittest.TestLoader().loadTestsFromTestCase(test)
+                fullsuite.addTest(test)
+        except ImportError:
+            print "%s failed!" % testmodule
+    unittest.TextTestRunner(verbosity=2).run(fullsuite)
+        
