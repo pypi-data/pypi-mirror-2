ff9df56f0a2a5a0fb371d3818f2edcaa147ef299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:58 +0000

    Add package scriptloader/0.1.0/scriptloader-0.1.0.tar.gz

---
+++ packages/scriptloader/scriptloader-0.1.0.tar.gz/scriptloader-0.1.0/scriptloader.py
@@ -0,0 +1,36 @@
+import imp
+
+import nose.loader
+import nose.plugins
+
+class ScriptLoader(nose.plugins.Plugin):
+    """Load tests from scripts that may not have a .py extension."""
+
+    def options(self, parser, env):
+        return nose.plugins.Plugin.options(self, parser, env)
+
+    def configure(self, options, conf):
+        return nose.plugins.Plugin.configure(self, options, conf)
+
+    def prepareTestLoader(self, loader):
+        """Save the *loader*.
+
+        This instance will be used later by :meth:`loadTestsFromFile` if
+        necessary.
+        """
+        self.loader = loader
+
+    def loadTestsFromFile(self, filename):
+        """Load tests from *filename*.
+
+        Attempt to load the file using :func:`imp.load_source`. If that
+        succeeds, pass the loaded module to the :attr:`loader`
+        :meth:`loadTestsFromModule`. If the file can't be loaded, return False
+        so other plugins can try loading it.
+        """
+        try:
+            module = imp.load_source("module", filename)
+        except SyntaxError:
+            return False
+
+        return self.loader.loadTestsFromModule(module)
---
+++ packages/scriptloader/scriptloader-0.1.0.tar.gz/scriptloader-0.1.0/setup.py
@@ -0,0 +1,38 @@
+import sys
+
+from setuptools import setup
+
+meta = dict(
+    name="scriptloader",
+    version="0.1.0",
+    description="scriptloader loads scripts (into nose)",
+    author="Will Maier",
+    author_email="willmaier@ml1.net",
+    py_modules=["scriptloader"],
+    test_suite="tests",
+    install_requires=["setuptools"],
+    keywords="nose testing scripts",
+    url="http://packages.python.org/scriptloader",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+    ],
+    entry_points = {
+        'nose.plugins.0.10': [
+            'scriptloader = scriptloader:ScriptLoader'
+        ]
+    },
+)
+
+# Automatic conversion for Python 3 requires distribute.
+if False and sys.version_info >= (3,):
+    meta.update(dict(
+        use_2to3=True,
+    ))
+
+setup(**meta)
