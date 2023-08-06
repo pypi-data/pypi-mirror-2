753158a76137eabacac30219c75719851f3916e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:09 +0000

    Add package imagestore/2.1.1/imagestore-2.1.1.tar.gz

---
+++ packages/imagestore/imagestore-2.1.1.tar.gz/imagestore-2.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = 'imagestore',
+        version = '2.1.1',
+        packages = find_packages(),
+        install_requires = [
+            'django',
+            'sorl-thumbnail',
+            'south',
+            'django-mptt',
+            'pil',
+            'django-fancy-autocomplete',
+            'django-tagging',
+            ],
+        author = 'Pavel Zhukov',
+        author_email = 'gelios@gmail.com',
+        description = 'Django simple image gallery',
+        long_description = open('README.rst').read(),
+        license = 'GPL',
+        keywords = 'django gallery',
+        url = 'http://bitbucket.org/zeus/imagestore/'
+     )
+ 
