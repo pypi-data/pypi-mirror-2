b28d9822a79f61e020855cc5ad50289c3dd711f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:56 +0000

    Add package djangohttpdigest/0.2.3/djangohttpdigest-0.2.3-py2.6.egg

---
+++ packages/djangohttpdigest/djangohttpdigest-0.2.3-py2.6.egg/djangohttpdigest/authentication.py
@@ -0,0 +1,111 @@
+"""
+Various authentication cases, used by decorators.
+"""
+
+__all__ = ("SimpleHardcodedAuthenticator", "ModelAuthenticator", "ClearTextModelAuthenticator")
+
+from hashlib import md5
+
+class Authenticator(object):
+    """ Authenticator """
+    
+    def __init__(self):
+        object.__init__(self)
+        
+        self.server_secret = None
+        self.a1 = None
+        self.a2 = None
+    
+    def get_a1(self, *args, **kwargs):
+        """ Get server's a1 hash for later comparison """
+        raise NotImplementedError("This (sub)class does not support this method, see another one.")
+    
+    def secret_passed(self, digestor):
+        """ Compare computed secret to secret from authentication backend.
+        If get_a1 was not called before, it's called with digestor as an argument.
+        Return bool whether it matches.
+        """
+        if not self.a1:
+            try:
+                self.get_a1(digestor=digestor)
+            except ValueError:
+                return False
+        
+        assert self.a1 is not None
+        
+        client_secret = digestor.get_client_secret()
+        server_secret = digestor.get_server_secret(a1=self.a1)
+        return client_secret == server_secret
+
+def check_hardcoded_authentication(parsed_header, method, path, params, realm, username, password):
+    """ Do information sent in header authenticates against given credentials? """
+    
+    return parsed_header['response'] == result_secret 
+
+class SimpleHardcodedAuthenticator(Authenticator):
+    """ Compares secret to explicitly given credentials """
+    
+    def __init__(self, server_realm, server_username, server_password):
+        Authenticator.__init__(self)
+        
+        self.server_realm = server_realm
+        self.server_username = server_username
+        self.server_password = server_password
+
+    def get_a1(self, digestor):
+        self.a1 = digestor.get_a1(realm=self.server_realm, username=self.server_username, password=self.server_password)
+        return self.a1 
+    
+
+class ModelAuthenticator(Authenticator):
+    def __init__(self, model, realm, realm_field, username_field, secret_field):
+        Authenticator.__init__(self)
+        
+        self.model = model
+        self.realm = realm
+        self.realm_field = realm_field
+        self.username_field = username_field
+        self.secret_field = secret_field
+    
+    
+    def get_a1(self, digestor):
+        try:
+            username = digestor.get_client_username()
+            pars = {
+                self.username_field : username
+            }
+            if self.realm_field:
+                pars[self.realm_field] = self.realm
+            inst = self.model.objects.get(**pars)
+            self.a1 = getattr(inst, self.secret_field)
+            return self.a1
+        
+        except self.model.DoesNotExist:
+            raise ValueError()
+
+class ClearTextModelAuthenticator(Authenticator):
+    def __init__(self, model, realm, realm_field, username_field, password_field):
+        Authenticator.__init__(self)
+
+        self.model = model
+        self.realm = realm
+        self.realm_field = realm_field
+        self.username_field = username_field
+        self.password_field = password_field
+
+
+    def get_a1(self, digestor):
+        try:
+            username = digestor.get_client_username()
+            pars = {
+                self.username_field : username
+            }
+            if self.realm_field:
+                pars[self.realm_field] = self.realm
+            inst = self.model.objects.get(**pars)
+            password = getattr(inst, self.password_field)
+            self.a1 = md5("%s:%s:%s" % (username, self.realm, password)).hexdigest()
+            return self.a1
+
+        except self.model.DoesNotExist, e:
+            raise ValueError(e)
---
+++ packages/djangohttpdigest/djangohttpdigest-0.2.3-py2.6.egg/djangohttpdigest/decorators.py
@@ -0,0 +1,61 @@
+from django.http import HttpResponseBadRequest
+
+from djangohttpdigest.http import HttpResponseNotAuthorized
+from djangohttpdigest.digest import Digestor, parse_authorization_header
+from djangohttpdigest.authentication import SimpleHardcodedAuthenticator, ClearTextModelAuthenticator, ModelAuthenticator
+
+__all__ = ("protect_digest", "protect_digest_model")
+
+def protect_digest(realm, username, password):
+    def _innerDecorator(function):
+        def _wrapper(request, *args, **kwargs):
+            
+            digestor = Digestor(method=request.method, path=request.get_full_path(), realm=realm)
+            
+            if request.META.has_key('HTTP_AUTHORIZATION'):
+                try:
+                    parsed_header = digestor.parse_authorization_header(request.META['HTTP_AUTHORIZATION'])
+                except ValueError, err:
+                    return HttpResponseBadRequest(err)
+
+                if parsed_header['realm'] == realm:
+                    authenticator = SimpleHardcodedAuthenticator(server_realm=realm, server_username=username, server_password=password)
+
+                    if authenticator.secret_passed(digestor):
+                        return function(request, *args, **kwargs)
+                
+            # nothing received, return challenge
+            response = HttpResponseNotAuthorized("Not Authorized")
+            response['www-authenticate'] = digestor.get_digest_challenge()
+            return response
+        return _wrapper
+    return _innerDecorator
+
+def protect_digest_model(model, realm, realm_field='realm', username_field='username', secret_field='secret_field', password_field=None):
+    def _innerDecorator(function):
+        def _wrapper(request, *args, **kwargs):
+            
+            digestor = Digestor(method=request.method, path=request.get_full_path(), realm=realm)
+            
+            if request.META.has_key('HTTP_AUTHORIZATION'):
+                try:
+                    parsed_header = digestor.parse_authorization_header(request.META['HTTP_AUTHORIZATION'])
+                except ValueError, err:
+                    return HttpResponseBadRequest(err)
+
+                if parsed_header['realm'] == realm:
+                    if password_field:
+                        authenticator = ClearTextModelAuthenticator(model=model, realm=realm, realm_field=realm_field, username_field=username_field, password_field=password_field)
+                    else:
+                        authenticator = ModelAuthenticator(model=model, realm=realm, realm_field=realm_field, username_field=username_field, secret_field=secret_field)
+
+                    if authenticator.secret_passed(digestor):
+                        return function(request, *args, **kwargs)
+                
+            # nothing received, return challenge
+            response = HttpResponseNotAuthorized("Not Authorized")
+            response['www-authenticate'] = digestor.get_digest_challenge()
+            return response
+        return _wrapper
+    return _innerDecorator
+
