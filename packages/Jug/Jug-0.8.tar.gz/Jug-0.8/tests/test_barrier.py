f732331c94e3946fe2574e52ce2958eec0fe7ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:08 +0000

    Add package Jug/0.8/Jug-0.8.tar.gz

---
+++ packages/Jug/Jug-0.8.tar.gz/Jug-0.8/tests/test_barrier.py
@@ -0,0 +1,30 @@
+import jug.jug
+from tests.task_reset import task_reset
+from jug.options import default_options
+
+@task_reset
+def test_barrier():
+    store, space = jug.jug.init('tests/jugfiles/wbarrier.py', 'dict_store')
+    assert 'four' not in space
+    jug.jug.execute(store, default_options)
+    store, space = jug.jug.init('tests/jugfiles/wbarrier.py', store)
+    assert 'four' in space
+
+    # This is a regression test:
+    # a test version of jug would fail here:
+    jug.jug.execute(store, default_options)
+
+def product(vals):
+    import operator
+    return reduce(operator.mul, vals)
+
+@task_reset
+def test_mapreduce_barrier():
+    store, space = jug.jug.init('tests/jugfiles/barrier_mapreduce.py', 'dict_store')
+    assert 'values' not in space
+    jug.jug.execute(store, default_options)
+    store, space = jug.jug.init('tests/jugfiles/barrier_mapreduce.py', store)
+    assert 'values' in space
+    assert space['values'] == product(range(20))
+    jug.jug.execute(store, default_options)
+
