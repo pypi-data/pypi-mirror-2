659d0480653da3511f0ac2941d0b4839627ede08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:34 +0000

    Add package django-richcomments/0.0.2/django-richcomments-0.0.2.tar.gz

---
+++ packages/django-richcomments/django-richcomments-0.0.2.tar.gz/django-richcomments-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-richcomments',
+    version='0.0.2',
+    description='Django app extending the builtin comments framework for AJAX style commenting.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/django-richcomments',
+    packages = find_packages(),
+    include_package_data=True,
+    test_suite="setuptest.SetupTestSuite",
+    tests_require=[
+        'django-setuptest>=0.0.6',
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
