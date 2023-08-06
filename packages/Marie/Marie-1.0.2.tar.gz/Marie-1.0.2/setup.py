a91c4e7b28cdd94d4feacf89431ec675a3a9c61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:50 +0000

    Add package Marie/1.0.2/Marie-1.0.2.tar.gz

---
+++ packages/Marie/Marie-1.0.2.tar.gz/Marie-1.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+sdict = {
+    'name' : 'Marie',
+    'version' : '1.0.2',
+    'description' : 'A WSGI micro-framework for websites in Python',
+    'long_description' : 'A WSGI micro-framework for websites in Python',
+    'url': 'http://www.trydyingtolive.com/marie/',
+    'author' : 'Mark W. Lee',
+    'author_email' : 'trydyingtolive@gmail.com',
+    'maintainer' : 'Mark W. Lee',
+    'maintainer_email' : 'trydyingtolive',
+    'keywords' : ['Marie', 'WSGI', 'micro', 'framework', 'Redis', 'Mako'],
+    'license' : 'GPLv3',
+    'packages' : find_packages(),
+    'classifiers' : [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'],
+}
+    
+setup(**sdict)
