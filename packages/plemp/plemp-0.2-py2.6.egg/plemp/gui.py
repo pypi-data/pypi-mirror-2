ff59615919f1029d21475eef4e0e3f9f6ce007c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:33 +0000

    Add package plemp/0.2/plemp-0.2-py2.6.egg

---
+++ packages/plemp/plemp-0.2-py2.6.egg/plemp/gui.py
@@ -0,0 +1,126 @@
+# Copyright 2010 Arjan Scherpenisse <arjan@scherpenisse.net>
+# See LICENSE for details.
+
+import os
+import gtk
+import gobject
+import dbus
+import dbus.service
+if getattr(dbus, 'version', (0,0,0)) >= (0,41,0):
+	import dbus.glib
+
+
+class GUI (object):
+
+    def __init__(self, uploader):
+        self.uploader = uploader
+
+        builder = gtk.Builder()
+        builder.add_from_file(os.path.join(os.path.dirname(__file__), "plemp.ui"))
+        for b in builder.get_objects():
+            setattr(self, gtk.Buildable.get_name(b), b)
+        builder.connect_signals(self)
+
+        self.uploader.waitForAuthentication = self.waitForAuthentication
+        self.uploader.authorizationError = self.authorizationError
+        self.uploader.uploadCallback = self.uploadCallback
+
+        self.goButton.hide()
+        if not self.uploader.photoset:
+            self.window.set_size_request(320, 70)
+            self.setBox.hide()
+        else:
+            self.window.set_size_request(320, 120)
+            self.setEntry.set_sensitive(False)
+            self.setEntry.set_text(self.uploader.photoset)
+
+        self.status("Will upload %d files." % len(self.uploader.files))
+        gobject.idle_add(self.checkStart)
+
+
+    def status(self, text):
+        self.progressLabel.set_text(text)
+
+
+    def waitForAuthentication(self):
+        d = gtk.MessageDialog(self.window, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_OK,
+                              "You need to allow plemp to access your account at Flickr. A webpage has been opened for you to do the authorization.\n\nClick OK when done.")
+        d.run()
+        d.destroy()
+
+
+    def authorizationError(self):
+        d = gtk.MessageDialog(self.window, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK,
+                              "Authorization failed. Please retry.")
+        d.run()
+        d.destroy()
+
+
+    def uploadCallback(self, filenum, progress, done):
+        total = len(self.uploader.files)
+        if done and filenum == total:
+            self.status("Done!")
+            self.progressBar.set_fraction(1.)
+            gobject.idle_add(gtk.main_quit)
+        else:
+            self.status("Uploading file %d of %d" % (filenum, total))
+            self.progressBar.set_fraction((100*(filenum-1)+progress)/(total*100.))
+
+        self.window.queue_draw()
+        self.window.get_window().process_updates(True)
+
+
+    def checkStart(self):
+
+        session_bus = dbus.SessionBus()
+        try:
+            o = session_bus.get_object("net.scherpenisse.Plemp", '/')
+            # This is the second copy, add the files to the other copy.
+            for f in self.uploader.files:
+                o.addFile(f)
+            gtk.main_quit()
+            return
+        except dbus.DBusException, e: # No other copy running
+            print e
+            pass
+
+        # setup dbus service
+        self.remote = RemoteControl(self, session_bus, self.uploader.profile)
+
+        self.window.show()
+        self.window.queue_draw()
+        self.window.get_window().process_updates(True)
+
+        if self.uploader.canStart():
+            self.uploader.start()
+        else:
+            if self.uploader.photoset == "ask":
+                self.setEntry.set_text("")
+            self.setEntry.set_sensitive(True)
+            self.setEntry.grab_focus()
+            self.goButton.grab_default()
+            self.goButton.show()
+
+
+    def on_goAction_activate(self, b):
+        self.uploader.photoset = self.setEntry.get_text()
+        self.setEntry.set_sensitive(False)
+        self.goButton.hide()
+        self.uploader.start()
+
+
+    def addFile(self, f):
+        self.uploader.addFile(f)
+        self.status("Will upload %d files." % len(self.uploader.files))
+
+
+
+class RemoteControl(dbus.service.Object):
+    def __init__(self, gui, session_bus, profile):
+        self.gui = gui
+        bus_name = dbus.service.BusName("net.scherpenisse.Plemp", bus=session_bus)
+        dbus.service.Object.__init__(self, object_path='/', bus_name=bus_name)
+
+    @dbus.service.method('net.scherpenisse.Plemp.Uploader')
+    def addFile(self, f):
+        self.gui.addFile(f)
---
+++ packages/plemp/plemp-0.2-py2.6.egg/plemp/scripts.py
@@ -0,0 +1,67 @@
+# Copyright 2010 Arjan Scherpenisse <arjan@scherpenisse.net>
+# See LICENSE for details.
+
+import gtk
+
+from optparse import OptionParser
+
+from plemp import __version__
+from plemp.base import Uploader
+
+
+def main():
+    parser = OptionParser(description="Utility to upload photos to Flickr, with graphical feedback.")
+    parser.add_option("-p", "--profile", help="Profile name, for connecting multiple Flickr accounts", default=None, action='store')
+    parser.add_option("", "--photoset", help="Upload to set ('ask' means interactive prompt)", default=None, action='store')
+    parser.add_option("", "--collection", help="Upload to collection ('ask' means interactive prompt)", default=None, action='store')
+    parser.add_option("-n", "--no-gui", help="Upload to collection ('ask' means interactive prompt)", action='store_true')
+
+    parser.add_option("", "--content-type", help="Content type; 1 for Photo, 2 for Screenshot, or 3 for Other.", action='store')
+    parser.add_option("", "--hidden", help="Hide; set to 1 to keep the photo in global search results, 2 to hide from public searches.", action='store')
+    parser.add_option("-a", "--access", help="Access (public,private,family; comma separated)", action='store')
+    parser.add_option("-v", "--version", help="Get version info.", action='store_true')
+
+    (options, filenames) = parser.parse_args()
+
+    if options.version:
+        print "plemp", __version__
+        print
+        exit()
+
+    uploader = Uploader(options.profile)
+    uploader.photoset = options.photoset
+
+    for opt in ['hidden', 'content_type']:
+        uploader.setUploadOption(opt, getattr(options, opt))
+
+    if options.access:
+        access = [s.strip() for s in options.access.split(",")]
+        if "public" not in access:
+            uploader.setUploadOption('is_public', 0)
+            for k in access:
+                try:
+                    uploader.setUploadOption('is_'+k, 1)
+                except:
+                    print "Invalid access option: " + k
+                    exit(1)
+        else:
+            # public picture (the default)
+            pass
+
+    for f in filenames:
+        uploader.addFile(f)
+
+    if options.no_gui:
+        n = uploader.start()
+        print "%d file(s) uploaded." % n
+    else:
+        if not uploader.files:
+            exit()
+
+        from dbus.mainloop.glib import DBusGMainLoop
+        DBusGMainLoop(set_as_default=True)
+
+        from plemp.gui import GUI
+        GUI(uploader)
+        gtk.main()
+
