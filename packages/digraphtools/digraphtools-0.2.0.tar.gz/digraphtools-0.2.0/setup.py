924297ba82d8692fb99f1260a68eba5da6621413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:01 +0000

    Add package digraphtools/0.2.0/digraphtools-0.2.0.tar.gz

---
+++ packages/digraphtools/digraphtools-0.2.0.tar.gz/digraphtools-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+	name='digraphtools',
+	version='0.2.0',
+	author='David Basden',
+	author_email='davidb-python@rcpt.to',
+	packages=['digraphtools','digraphtools.test'],
+	url='https://github.com/dbasden/python-digraphtools',
+	description='Some tools for working with digraphs, partial orders and topological sorting with Python',
+	long_description=open('README.txt').read(),
+	platforms = ['any'],
+	classifiers = [
+		'Intended Audience :: Developers',
+		'Development Status :: 3 - Alpha',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+	],
+)
