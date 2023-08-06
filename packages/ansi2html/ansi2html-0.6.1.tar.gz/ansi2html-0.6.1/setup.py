c4506284f521c2af464e290ac669b28d92bd54b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:05 +0000

    Add package ansi2html/0.6.1/ansi2html-0.6.1.tar.gz

---
+++ packages/ansi2html/ansi2html-0.6.1.tar.gz/ansi2html-0.6.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+f = open('README.rst')
+long_description = f.read().strip()
+long_description = long_description.split('split here', 1)[1]
+f.close()
+
+setup(
+    name='ansi2html',
+    version='0.6.1',
+    description="Convert text with ANSI color codes to HTML",
+    long_description=long_description,
+    author='Ralph Bean',
+    author_email='ralph.bean@gmail.com',
+    url='http://github.com/ralphbean/ansi2html/',
+    license='GPL',
+    install_requires=[
+        "mako",
+        "tw2.core", # for dottedtemplatelookup... for realsies.
+    ],
+    packages=['ansi2html'],
+    package_data={ 'ansi2html' : [ 'ansi2html.sh', 'templates/*' ], },
+)
