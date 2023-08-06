834a5e5c4e82f01dfd6e2b26fb7e1b1f131cbeb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:14 +0000

    Add package van.contactology/1.0/van.contactology-1.0.tar.gz

---
+++ packages/van.contactology/van.contactology-1.0.tar.gz/van.contactology-1.0/van/contactology/__init__.py
@@ -0,0 +1,90 @@
+"""A generic API for contactology"""
+
+import urllib
+from pprint import pformat
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from twisted.web.client import getPage
+from twisted.internet import defer
+from twisted.python import log
+
+__version__ = "1.0"
+
+class APIError(Exception):
+    """Base class for all api errors from contactology"""
+
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+        super(APIError, self).__init__("API Error: %s (%s)" % (message, code))
+
+
+class Contactology(object):
+    """Proxy object"""
+    host = "api.emailcampaigns.net"
+    path = "/2/REST/"
+    _logio = False
+    
+    def __init__(self, key, useHTTPS=True):
+        self.key = key
+        self.useHTTPS = useHTTPS
+
+    def _log_query(self, r):
+        log.msg("SENT: %s" % pformat(r))
+        return r
+
+    def __getattr__(self, name):
+        def call_wrapper(**args):
+            return self._call(name, **args)
+        return call_wrapper
+
+    @defer.inlineCallbacks
+    def _call(self, method, **kw):
+        if self._logio:
+            self._log_query(kw)
+        # serialize non-strings using json
+        for k, v in kw.items():
+            if isinstance(v, unicode):
+                kw[k] = v = v.encode('utf-8')
+            if not isinstance(v, str):
+                v = json.dumps(v)
+                kw[k] = v
+        # add our preset arguments
+        kw.update({'key': self.key, 'method': method})
+        # construct request data
+        postdata = urllib.urlencode(kw)
+        schema = self.useHTTPS and 'https' or 'http'
+        url = '%s://%s%s' % (schema, self.host, self.path)
+        headers = {"Content-type": "application/x-www-form-urlencoded",
+                   "User-Agent": "Twisted Wrapper %s" % __version__}
+        resp = yield getPage(url, method='POST', headers=headers, postdata=postdata)
+        # de-serialize response
+        resp = json.loads(resp)
+        if self._logio:
+            log.msg("RECEIVED: %s" % pformat(resp))
+        # check for errors
+        if isinstance(resp, dict):
+            if resp.get('result', None) == 'error':
+                raise APIError(resp['code'], resp['message'])
+        yield defer.returnValue(resp)
+
+if __name__ == '__main__':
+    from twisted.internet import reactor
+    from pprint import pprint
+    proxy = Contactology('Your API key here')
+    @defer.inlineCallbacks
+    def test():
+        try:
+            resp = yield proxy.List_Get_Active_Lists()
+            print resp
+            resp = yield proxy.List_Get_Active_Lists(optionalParameters={'offset': 1})
+            print resp
+            resp = yield proxy.List_Get_Info(listId=1)
+            print resp
+        finally:
+            reactor.stop()
+    reactor.callWhenRunning(test)
+    reactor.run()
---
+++ packages/van.contactology/van.contactology-1.0.tar.gz/van.contactology-1.0/van/contactology/tests/test_contactology.py
@@ -0,0 +1,56 @@
+import unittest
+from cgi import parse_qsl
+from simplejson import dumps
+from twisted.trial.unittest import TestCase
+from twisted.internet import defer
+
+from mock import patch, Mock
+from van.contactology import Contactology, APIError, __version__
+
+def _parse_post(callargs):
+    return sorted(parse_qsl(callargs['postdata'], True, True))
+
+class TestProxy(TestCase):
+
+    @defer.inlineCallbacks
+    def _call_once(self, api_key, result, method, *args, **kw):
+        patcher = patch('van.contactology.getPage')
+        getPage = patcher.start()
+        try:
+            getPage.return_value = dumps(result)
+            proxy = Contactology(api_key)
+            method = getattr(proxy, method)
+            out = yield method(*args, **kw)
+        finally:
+            patcher.stop()
+        defer.returnValue((getPage, out))
+
+    @defer.inlineCallbacks
+    def test_list_return(self):
+        getPage, out = yield self._call_once('API Key', [], 'Campaign_Find')
+        self.assertEquals(out, [])
+
+    @defer.inlineCallbacks
+    def test_call_args(self):
+        getPage, out = yield self._call_once('API Key', [], 'Campaign_Find')
+        self.assertEquals(getPage.call_count, 1)
+        self.assertEquals(getPage.call_args, (('https://api.emailcampaigns.net/2/REST/',),
+                                                 {'headers': {'Content-type': 'application/x-www-form-urlencoded',
+                                                              'User-Agent': 'Twisted Wrapper %s' % __version__},
+                                                  'method': 'POST',
+                                                  'postdata': 'method=Campaign_Find&key=API+Key'}))
+
+    @defer.inlineCallbacks
+    def test_api_error(self):
+        d = self._call_once('API Key', {'code': 221, 'message': 'Key not found', 'result': 'error'}, 'List_Get_Active_Lists')
+        yield self.failUnlessFailure(d, APIError)
+
+    @defer.inlineCallbacks
+    def test_unicode_api_key(self):
+        getPage, out = yield self._call_once(u'unicode API Key', [], 'Campaign_Find')
+        self.assertEquals(getPage.call_args[1]['postdata'], 'method=Campaign_Find&key=unicode+API+Key')
+
+    @defer.inlineCallbacks
+    def test_unicode_argument(self):
+        getPage, out = yield self._call_once('API Key', [], 'Contact_Get', email=u"a@b.c")
+        self.assertEquals(_parse_post(getPage.call_args[1]), [('email', 'a@b.c'), ('key', 'API Key'), ('method', 'Contact_Get')])
