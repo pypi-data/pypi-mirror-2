98635d6cbf30584d2f63bb5967ad7f548c592aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package Stencil/0.2.1/Stencil-0.2.1.tar.gz

---
+++ packages/Stencil/Stencil-0.2.1.tar.gz/Stencil-0.2.1/HISTORY.rst
@@ -0,0 +1,29 @@
+History
+-------
+
+0.2.1 (2011-08-10)
+^^^^^^^^^^^^^^^^^^
+
+* Better handling of target directory creation
+* Fix stencils order in command output
+
+0.2 (2011-08-05)
+^^^^^^^^^^^^^^^^
+
+* Overridable resources
+* Use Jinja2 for templates
+
+0.1.2 (2011-08-05)
+^^^^^^^^^^^^^^^^^^
+
+* Fix resource target path rendering
+
+0.1.1 (2011-07-28)
+^^^^^^^^^^^^^^^^^^
+
+* Add possibility to collect resources from multiple dirs
+
+0.1 (2011-04-14)
+^^^^^^^^^^^^^^^^
+
+* Initial release
---
+++ packages/Stencil/Stencil-0.2.1.tar.gz/Stencil-0.2.1/stencil/main.py
@@ -0,0 +1,25 @@
+import sys
+import argparse
+from pkg_resources import iter_entry_points
+
+
+stencils = {}
+for entry_point in iter_entry_points('stencils'):
+    stencils[entry_point.name] = entry_point.load()
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--use-defaults', dest='use_defaults',
+                        action='store_true', default=False,
+                        help="don't ask for variables with defaults if set")
+    subparsers = parser.add_subparsers(title='available stencils',
+                                       metavar='<stencil>')
+    for name in sorted(stencils):
+        stencils[name].add_to_subparsers(name, subparsers)
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == '__main__':
+    run()
