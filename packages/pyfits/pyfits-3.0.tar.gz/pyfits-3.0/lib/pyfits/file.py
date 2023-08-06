09ce438d7efe6f65bd31ac117cd2c48c8eee92ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:25 +0000

    Add package pyfits/3.0/pyfits-3.0.tar.gz

---
+++ packages/pyfits/pyfits-3.0.tar.gz/pyfits-3.0/lib/pyfits/file.py
@@ -0,0 +1,324 @@
+import gzip
+import os
+import sys
+import tempfile
+import urllib
+import warnings
+import zipfile
+
+import numpy as np
+from numpy import memmap as Memmap
+
+from pyfits.util import Extendable, _fromfile, _tofile, _write_string, \
+                        deprecated
+
+
+PYTHON_MODES = {'readonly': 'rb', 'copyonwrite': 'rb', 'update': 'rb+',
+                'append': 'ab+', 'ostream': 'w'}  # open modes
+MEMMAP_MODES = {'readonly': 'r', 'copyonwrite': 'c', 'update': 'r+'}
+
+
+class _File(object):
+    """
+    Represents a FITS file on disk (or in some other file-like object).
+    """
+
+    __metaclass__ = Extendable
+
+    def __init__(self, fileobj=None, mode='copyonwrite', memmap=False):
+        if fileobj is None:
+            self.__file = None
+            self.closed = False
+            self.mode = mode
+            self.memmap = memmap
+            self.compressed = False
+            self.readonly = False
+            self.writeonly = False
+            self.simulateonly = True
+            return
+        else:
+            self.simulateonly = False
+
+        if mode not in PYTHON_MODES:
+            raise ValueError("Mode '%s' not recognized" % mode)
+
+        # Determine what the _File object's name should be
+        if isinstance(fileobj, file):
+            self.name = fileobj.name
+        elif isinstance(fileobj, basestring):
+            if mode != 'append' and not os.path.exists(fileobj) and \
+               not os.path.splitdrive(fileobj)[0]:
+                #
+                # Not writing file and file does not exist on local machine and
+                # name does not begin with a drive letter (Windows), try to
+                # get it over the web.
+                #
+                self.name, fileheader = urllib.urlretrieve(fileobj)
+            else:
+                self.name = fileobj
+        else:
+            if hasattr(fileobj, 'name'):
+                self.name = fileobj.name
+            elif hasattr(fileobj, 'filename'):
+                self.name = fileobj.filename
+            elif hasattr(fileobj, '__class__'):
+                self.name = str(fileobj.__class__)
+            else:
+                self.name = str(type(fileobj))
+
+        self.closed = False
+        self.mode = mode
+        self.memmap = memmap
+
+        # Underlying fileobj is a file-like object, but an actual file object
+        self.file_like = False
+
+        self.compressed = False
+        if isinstance(fileobj, (gzip.GzipFile, zipfile.ZipFile)):
+            self.compressed = True
+
+        self.readonly = False
+        self.writeonly = False
+        if mode in ('readonly', 'copyonwrite') or \
+                (isinstance(fileobj, gzip.GzipFile) and mode == 'update'):
+            self.readonly = True
+        elif mode == 'ostream' or \
+                (isinstance(fileobj, gzip.GzipFile) and mode == 'append'):
+            self.writeonly = True
+
+        if memmap and mode not in ('readonly', 'copyonwrite', 'update'):
+            raise ValueError(
+                   "Memory mapping is not implemented for mode `%s`." % mode)
+        else:
+            # Initialize the internal self.__file object
+            if isinstance(fileobj, (file, gzip.GzipFile)):
+                if hasattr(fileobj, 'fileobj'):
+                    closed = fileobj.fileobj.closed
+                    fileobj_mode = fileobj.fileobj.mode
+                elif hasattr(fileobj, 'closed'):
+                    closed = fileobj.closed
+                    fileobj_mode = fileobj.mode
+                else:
+                    closed = True
+                    fileobj_mode = PYTHON_MODES[mode]
+
+                if not closed:
+                    if PYTHON_MODES[mode] != fileobj_mode:
+                        raise ValueError(
+                            "Input mode '%s' (%s) does not match mode of the "
+                            "input file (%s)." % (mode, PYTHON_MODES[mode],
+                                                  fileobj_mode))
+                    self.__file = fileobj
+                elif isinstance(fileobj, file):
+                    self.__file = open(self.name, PYTHON_MODES[mode])
+                    # Return to the beginning of the file--in Python 3 when
+                    # opening in append mode the file pointer is at the end of
+                    # the file
+                    self.__file.seek(0)
+                else:
+                    self.__file = gzip.open(self.name, PYTHON_MODES[mode])
+            elif isinstance(fileobj, basestring):
+                if os.path.splitext(self.name)[1] == '.gz':
+                    # Handle gzip files
+                    if mode in ['update', 'append']:
+                        raise IOError(
+                              "Writing to gzipped fits files is not supported")
+                    zfile = gzip.GzipFile(self.name)
+                    self.__file = tempfile.NamedTemporaryFile(suffix='.fits')
+                    self.name = self.__file.name
+                    self.__file.write(zfile.read())
+                    zfile.close()
+                elif os.path.splitext(self.name)[1] == '.zip':
+                    # Handle zip files
+                    if mode in ['update', 'append']:
+                        raise IOError(
+                              "Writing to zipped fits files is not supported")
+                    zfile = zipfile.ZipFile(self.name)
+                    namelist = zfile.namelist()
+                    if len(namelist) != 1:
+                        raise IOError(
+                          "Zip files with multiple members are not supported.")
+                    self.__file = tempfile.NamedTemporaryFile(suffix='.fits')
+                    self.name = self.__file.name
+                    self.__file.write(zfile.read(namelist[0]))
+                    zfile.close()
+                else:
+                    self.__file = open(self.name, PYTHON_MODES[mode])
+                # Make certain we're back at the beginning of the file
+                self.__file.seek(0)
+            else:
+                # We are dealing with a file like object.
+                # Assume it is open.
+                self.file_like = True
+                self.__file = fileobj
+
+                # If there is not seek or tell methods then set the mode to
+                # output streaming.
+                if not hasattr(self.__file, 'seek') or \
+                   not hasattr(self.__file, 'tell'):
+                    self.mode = mode = 'ostream'
+
+                if (self.mode in ('copyonwrite', 'update', 'append') and
+                    not hasattr(self.__file, 'write')):
+                    raise IOError("File-like object does not have a 'write' "
+                                  "method, required for mode '%s'."
+                                  % self.mode)
+
+                if self.mode == 'readonly' and \
+                   not hasattr(self.__file, 'read'):
+                    raise IOError("File-like object does not have a 'read' "
+                                  "method, required for mode 'readonly'."
+                                  % self.mode)
+
+            # For 'ab+' mode, the pointer is at the end after the open in
+            # Linux, but is at the beginning in Solaris.
+            if mode == 'ostream':
+                # For output stream start with a truncated file.
+                self.size = 0
+            elif isinstance(self.__file, gzip.GzipFile):
+                pos = self.__file.tell()
+                self.__file.fileobj.seek(0, 2)
+                self.size = self.__file.fileobj.tell()
+                self.__file.fileobj.seek(0)
+                self.__file.seek(pos)
+            elif hasattr(self.__file, 'seek'):
+                pos = self.__file.tell()
+                self.__file.seek(0, 2)
+                self.size = self.__file.tell()
+                self.__file.seek(pos)
+            else:
+                self.size = 0
+
+    def __repr__(self):
+        return '<%s.%s %s>' % (self.__module__, self.__class__.__name__,
+                               self.__file)
+
+    # Support the 'with' statement
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    @deprecated
+    def getfile(self):
+        """**Deprecated** Will be going away as soon as I figure out how."""
+        return self.__file
+
+    def read(self, size=None):
+        if not hasattr(self.__file, 'read'):
+            raise EOFError
+        return self.__file.read(size)
+
+    def readarray(self, size=None, offset=0, dtype=np.uint8, shape=None):
+        """
+        Similar to file.read(), but returns the contents of the underlying
+        file as a numpy array (or mmap'd array if memmap=True) rather than a
+        string.
+
+        Usually it's best not to use the `size` argument with this method, but
+        it's provided for compatibility.
+        """
+
+        if not hasattr(self.__file, 'read'):
+            raise EOFError
+
+        if not isinstance(dtype, np.dtype):
+            dtype = np.dtype(dtype)
+
+        if size and size % dtype.itemsize != 0:
+            raise ValueError('size %d not a multiple of %s' % (size, dtype))
+
+        if isinstance(shape, int):
+            shape = (shape,)
+
+        if size and shape:
+            actualsize = sum(dim * dtype.itemsize for dim in shape)
+            if actualsize < size:
+                raise ValueError('size %d is too few bytes for a %s array of '
+                                 '%s' % (size, shape, dtype))
+            if actualsize < size:
+                raise ValueError('size %d is too many bytes for a %s array of '
+                                 '%s' % (size, shape, dtype))
+
+        if size and not shape:
+            shape = (size / dtype.itemsize,)
+
+        if not (size or shape):
+            warnings.warn('No size or shape given to readarray(); assuming a '
+                          'shape of (1,)')
+            shape = (1,)
+
+        if self.memmap:
+            return Memmap(self.__file, offset=offset,
+                          mode=MEMMAP_MODES[self.mode], dtype=dtype,
+                          shape=shape)
+        else:
+            count = reduce(lambda x, y: x * y, shape)
+            pos = self.__file.tell()
+            self.__file.seek(offset)
+            data = _fromfile(self.__file, dtype, count, '')
+            data.shape = shape
+            self.__file.seek(pos)
+            return data
+
+    def write(self, string):
+        if hasattr(self.__file, 'write'):
+            _write_string(self.__file, string)
+
+    def writearray(self, array):
+        """
+        Similar to file.write(), but writes a numpy array instead of a string.
+
+        Also like file.write(), a flush() or close() may be needed before
+        the file on disk reflects the data written.
+        """
+
+        if hasattr(self.__file, 'write'):
+            _tofile(array, self.__file)
+
+    def flush(self):
+        if hasattr(self.__file, 'flush'):
+            self.__file.flush()
+
+    def seek(self, offset, whence=0):
+        # In newer Python versions, GzipFiles support the whence argument, but
+        # I don't think it was added until 2.6; instead of assuming it's
+        # present, we implement our own support for it here
+        if not hasattr(self.__file, 'seek'):
+            return
+        if isinstance(self.__file, gzip.GzipFile):
+            if whence:
+                if whence == 1:
+                    offset = self.__file.offset + offset
+                else:
+                    raise ValueError('Seek from end not supported')
+            self.__file.seek(offset)
+        else:
+            self.__file.seek(offset, whence)
+
+        pos = self.__file.tell()
+        if pos > self.size:
+            warnings.warn('Warning: File may have been truncated: actual '
+                          'file length (%i) is smaller than the expected '
+                          'size (%i)' % (self.size, pos))
+
+    def tell(self):
+        if not hasattr(self.__file, 'tell'):
+            raise EOFError
+        return self.__file.tell()
+
+    def truncate(self, size=None):
+        if hasattr(self.__file, 'truncate'):
+            self.__file.truncate(size)
+
+    def close(self):
+        """
+        Close the 'physical' FITS file.
+        """
+
+        if hasattr(self.__file, 'close'):
+            self.__file.close()
+
+        self.closed = True
+
---
+++ packages/pyfits/pyfits-3.0.tar.gz/pyfits-3.0/lib/pyfits/py3compat.py
@@ -0,0 +1,147 @@
+import sys
+
+if sys.version_info[0] >= 3:
+    # Stuff to do if Python 3
+    import io
+    import builtins
+
+    # Make io.FileIO available as the 'file' builtin as Go^H^HPython 2 intended
+    builtins.file = io.FileIO
+
+
+    # Make the decode_ascii utility function actually work
+    import pyfits.util
+    import numpy
+
+    def encode_ascii(s):
+        if isinstance(s, str):
+            return s.encode('ascii')
+        elif isinstance(s, numpy.ndarray) and \
+             issubclass(s.dtype.type, numpy.str_):
+            ns = numpy.char.encode(s, 'ascii').view(type(s))
+            if ns.dtype.itemsize != s.dtype.itemsize / 4:
+                ns = ns.astype((numpy.bytes_, s.dtype.itemsize / 4))
+            return ns
+        return s
+    pyfits.util.encode_ascii = encode_ascii
+
+    def decode_ascii(s):
+        if isinstance(s, bytes):
+            return s.decode('ascii')
+        elif isinstance(s, numpy.ndarray) and \
+             issubclass(s.dtype.type, numpy.bytes_):
+            # np.char.encode/decode annoyingly don't preserve the type of the
+            # array, hence the view() call
+            # It also doesn't necessarily preserve widths of the strings,
+            # hence the astype()
+            ns = numpy.char.decode(s, 'ascii').view(type(s))
+            if ns.dtype.itemsize / 4 != s.dtype.itemsize:
+                ns = ns.astype((numpy.str_, s.dtype.itemsize))
+            return ns
+        return s
+    pyfits.util.decode_ascii = decode_ascii
+
+
+    # Here we monkey patch (yes, I know) numpy to fix a few numpy Python 3
+    # bugs.  The only behavior that's modified is that bugs are fixed, so that
+    # should be OK.
+
+    # Fix chararrays; this is necessary in numpy 1.5.1 and below--hopefully
+    # should not be necessary later.  See
+    # http://projects.scipy.org/numpy/ticket/1817
+    # TODO: Maybe do a version check on numpy for this?  (Note: the fix for
+    # this hasn't been accepted in Numpy yet, so a version number check would
+    # not be helpful yet...)
+    import pyfits.file
+
+    _chararray = numpy.char.chararray
+    class chararray(_chararray):
+        def __getitem__(self, obj):
+                val = numpy.ndarray.__getitem__(self, obj)
+                if isinstance(val, numpy.character):
+                    temp = val.rstrip()
+                    if numpy.char._len(temp) == 0:
+                        val = ''
+                    else:
+                        val = temp
+                return val
+    for m in [numpy.char, numpy.core.defchararray, numpy.record]:
+        m.chararray = chararray
+
+
+    # Fix recarrays with sub-array fields.  See
+    # http://projects.scipy.org/numpy/ticket/1766
+    # TODO: Same as above, though the fix to this problem hasn't made it into
+    # any Numpy release yet either, so we'll have to hold off on a version
+    # check
+    def _fix_dtype(dtype):
+        """
+        Numpy has a bug (in Python3 only) that causes a segfault when
+        accessing the data of arrays containing nested arrays.  Specifically,
+        this happens if the shape of the subarray is not given as a tuple.
+        See http://projects.scipy.org/numpy/ticket/1766.
+        """
+
+        if dtype.fields is None:
+            return dtype
+
+        new_dtype = {}
+        for name in dtype.names:
+            field = dtype.fields[name]
+            shape = field[0].shape
+            if not isinstance(shape, tuple):
+                shape = (shape,)
+            new_dtype[name] = ((field[0].base, shape), field[1])
+
+        return numpy.dtype(new_dtype)
+
+    _recarray = numpy.recarray
+    class recarray(_recarray):
+         def __new__(subtype, shape, dtype=None, buf=None, offset=0,
+                     strides=None, formats=None, names=None, titles=None,
+                     byteorder=None, aligned=False, order='C'):
+             if dtype is not None:
+                 dtype = _fix_dtype(dtype)
+
+             return _recarray.__new__(
+                     subtype, shape, dtype, buf, offset, strides, formats,
+                     names, titles, byteorder, aligned, order)
+    numpy.recarray = recarray
+
+
+    # We also need to patch pyfits.file._File which can also be affected by the
+    # #1766 bug
+    old_File = pyfits.file._File
+    class _File(old_File):
+        def readarray(self, size=None, offset=None, dtype=numpy.uint8,
+                      shape=None):
+            if isinstance(dtype, numpy.dtype):
+                dtype = _fix_dtype(dtype)
+            return old_File.readarray(self, size, offset, dtype, shape)
+        readarray.__doc__ = old_File.readarray.__doc__
+    pyfits.file._File = _File
+
+
+    # Replace pyfits.util.maketrans and translate with versions that work
+    # with Python 3 unicode strings
+    pyfits.util.maketrans = str.maketrans
+
+    def translate(s, table, deletechars):
+        if deletechars:
+            table = table.copy()
+            for c in deletechars:
+                table[ord(c)] = None
+        return s.translate(table)
+    pyfits.util.translate = translate
+else:
+    # Stuff to do if not Python 3
+
+    # We want a bytes stand-in so we can do stuff like isinstance(..., bytes)
+    # and have it work correctly
+    import __builtin__
+    if not hasattr(__builtin__, 'bytes'):
+        __builtin__.bytes = str
+
+    import string
+    import pyfits.util
+    pyfits.util.maketrans = string.maketrans
