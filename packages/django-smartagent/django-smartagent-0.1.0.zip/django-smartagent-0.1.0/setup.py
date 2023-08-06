ea67ff647c910063b14d20dd5598bcb7d4c9758b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:10 +0000

    Add package django-smartagent/0.1.0/django-smartagent-0.1.0.zip

---
+++ packages/django-smartagent/django-smartagent-0.1.0.zip/django-smartagent-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-smartagent',
+    version = '0.1.0',
+    description = 'django-smartagent is the fastest and most complete user agent parser',
+    long_description = open('README.rst').read(),
+    keywords = 'django apps',
+    license = 'BSD License',
+    author = 'James Pacileo',
+    author_email = 'jamespacileo@gmail.com',
+    url = 'http://github.com/jamespacileo/django-smartagent/',
+    dependency_links = [],
+    classifiers = [
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages = ['smartagent'],
+    #packages = find_packages(exclude=['ez_setup', 'test_project']),
+    include_package_data = True,
+    zip_safe = False,
+    package_data = {
+        'smartagent': ['smartagent/agents_basic.pkl',],
+      },
+)
