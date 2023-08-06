9a359cf4c0dcbd312a4ab3b80d646c6c5758381d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:48 +0000

    Add package quintagroup.analytics/1.0/quintagroup.analytics-1.0.zip

---
+++ packages/quintagroup.analytics/quintagroup.analytics-1.0.zip/quintagroup.analytics-1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='quintagroup.analytics',
+      version=version,
+      description="Plone site's statistics",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Quintagroup',
+      author_email='support@quintagroup.com',
+      url='http://svn.quintagroup.com/products/quintagroup.analytics',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'GChartWrapper'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      """,
+      )
