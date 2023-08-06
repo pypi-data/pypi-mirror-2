5da857a9ba2777bd7415cb86c78c45a0d413b9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package plone.formwidget.recaptcha/1.0b3/plone.formwidget.recaptcha-1.0b3.zip

---
+++ packages/plone.formwidget.recaptcha/plone.formwidget.recaptcha-1.0b3.zip/plone.formwidget.recaptcha-1.0b3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = '1.0b3'
+
+setup(name='plone.formwidget.recaptcha',
+      version=version,
+      description="ReCaptcha widget for Plone.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(), 
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone discussion plone.app.discussion spam captcha recaptcha',
+      author='Timo Stollenwerk - Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.formwidget.recaptcha',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.z3cform',
+          'plone.registry',
+          'plone.app.registry',
+          'recaptcha-client != 1.0.4',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
