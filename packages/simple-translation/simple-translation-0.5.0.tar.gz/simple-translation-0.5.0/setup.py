7735a8804a46fb05c70195dae067582db8c8645f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:35 +0000

    Add package simple-translation/0.5.0/simple-translation-0.5.0.tar.gz

---
+++ packages/simple-translation/simple-translation-0.5.0.tar.gz/simple-translation-0.5.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name='simple-translation',
+    version='0.5.0',
+    description='This is a translation app/plugin with support for django-cms 2.0 plugins in the admin',
+    author='Oyvind Saltvik',
+    author_email='oyvind@gmail.com',
+    url='http://bitbucket.org/fivethreeo/simple-translation/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
+
