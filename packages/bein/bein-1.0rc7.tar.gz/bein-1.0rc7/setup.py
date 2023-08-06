a989b63f42cbdd3daa7a422f05d4829c50239cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:35 +0000

    Add package bein/1.0rc7/bein-1.0rc7.tar.gz

---
+++ packages/bein/bein-1.0rc7.tar.gz/bein-1.0rc7/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='bein',
+      version='1.0rc7',
+      url='http://madhadron.com/bein',
+      description='Miniature LIMS and workflow manager for bioinformatics',
+      author='Fred Ross',
+      author_email='madhadron@gmail.com',
+      packages=['bein'],
+      scripts=['beinclient','add_nh_flag'],
+      classifiers=['Topic :: System :: Shells', 'Topic :: Scientific/Engineering :: Bio-Informatics'],
+      install_requires = ['cherrypy', 'unittest2'],
+      )
