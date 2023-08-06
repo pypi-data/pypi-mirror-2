f64adbf20b28feb56398f1e7833cfd121b4f7565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:58 +0000

    Add package pyawsbuckets/1.0.2/pyawsbuckets-1.0.2.tar.gz

---
+++ packages/pyawsbuckets/pyawsbuckets-1.0.2.tar.gz/pyawsbuckets-1.0.2/pyawsbuckets/exceptions.py
@@ -0,0 +1,7 @@
+class AwsError(Exception):
+    """Base error class."""
+    pass
+
+class AwsRequestFailureError(AwsError):
+    """A packaged request was met with an unexpected response."""
+    pass
---
+++ packages/pyawsbuckets/pyawsbuckets-1.0.2.tar.gz/pyawsbuckets-1.0.2/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pyawsbuckets',
+    version='1.0.2',
+    author='Mark Henwood',
+    author_email='mark@mcbh.co.uk',
+    description='Handle Amazon S3 PUT/DELETE/sign interactions',
+    license='MIT',
+    keywords='amazon aws s3 buckets',
+    url='http://packages.python.org/pyawsbuckets',
+    packages=['pyawsbuckets'],
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Utilities'
+    ]
+)
