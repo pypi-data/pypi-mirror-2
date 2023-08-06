bfee05803ec8df59bbcb12d9a96c37681d95e247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:14 +0000

    Add package Products.ECAutoAssessmentBox/1.4/Products.ECAutoAssessmentBox-1.4.tar.gz

---
+++ packages/Products.ECAutoAssessmentBox/Products.ECAutoAssessmentBox-1.4.tar.gz/Products.ECAutoAssessmentBox-1.4/Products/ECAutoAssessmentBox/browser/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# $Id: __init__.py 1547 2011-04-01 07:34:35Z amelung $
+#
+# Copyright (c) 2006-2011 Otto-von-Guericke-UniversitŠt Magdeburg
+#
+# This file is part of ECAutoAssessmentBox.
+#
+__author__ = """Mario Amelung <mario.amelung@gmx.de>"""
+__docformat__ = 'plaintext'
+
+# make me a python module
