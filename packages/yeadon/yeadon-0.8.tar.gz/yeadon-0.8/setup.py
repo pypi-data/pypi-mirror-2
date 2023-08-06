58fa9c95b002bd9136186efbd9e8f0a923b06be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:21 +0000

    Add package yeadon/0.8/yeadon-0.8.tar.gz

---
+++ packages/yeadon/yeadon-0.8.tar.gz/yeadon-0.8/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='yeadon',
+    author='Chris Dembia',
+    author_email='chris530d@gmail.com',
+    version='0.8',
+    packages=['yeadon','dtk'],
+    license='LICENSE.txt',
+    long_description=open('README.rst').read(),
+)
