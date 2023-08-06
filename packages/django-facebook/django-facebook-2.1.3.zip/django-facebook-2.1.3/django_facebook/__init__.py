20ded2199d50a5957610700096f48b8da29f8240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:19 +0000

    Add package django-facebook/2.1.3/django-facebook-2.1.3.zip

---
+++ packages/django-facebook/django-facebook-2.1.3.zip/django-facebook-2.1.3/django_facebook/__init__.py
@@ -0,0 +1,34 @@
+__author__ = 'Thierry Schellenbach'
+__copyright__ = 'Copyright 2010, Thierry Schellenbach'
+__credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
+
+
+__license__ = 'BSD'
+__version__ = '2.1.3'
+__maintainer__ = 'Thierry Schellenbach'
+__email__ = 'thierryschellenbach@gmail.com'
+__status__ = 'Production'
+
+'''
+Some links which help me with publishing this code
+
+rest editor
+http://rst.ninjs.org/
+
+updating pypi
+python setup.py sdist upload
+http://pypi.python.org/pypi
+
+setting up pip for editing
+http://www.pip-installer.org/en/latest/index.html
+pip install -e ./
+
+TODO
+- password is required (wtf)
+- paco error, login not authenticating somehow
+/group/2086
+/group/2076
+- refactor profile updating and validation (one flow for register and connect)
+- Retry on facebook connection errors
+- errors op email already in use op register flow (auth backend issues?)
+'''
