89899537a6a278a1c289edeae7cd1aec16520cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:08 +0000

    Add package Flask-Celery/2.2.5/Flask-Celery-2.2.5.tar.gz

---
+++ packages/Flask-Celery/Flask-Celery-2.2.5.tar.gz/Flask-Celery-2.2.5/README.rst
@@ -0,0 +1,11 @@
+==============================
+ Flask <-> Celery Integration
+==============================
+:Version: 2.2.5
+
+Requires `Celery`_ 2.2.0 or later.
+
+.. _Celery: http://celeryproject.org
+
+
+See ``example.py`` for example usage.
---
+++ packages/Flask-Celery/Flask-Celery-2.2.5.tar.gz/Flask-Celery-2.2.5/setup.py
@@ -0,0 +1,42 @@
+"""
+Flask-Celery
+------------
+
+Celery integration for Flask
+
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-Celery',
+    version='2.2.5',
+    url='http://github.com/ask/flask-celery/',
+    license='BSD',
+    author='Ask Solem',
+    author_email='ask@celeryproject.org',
+    description='Celery integration for Flask',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    test_suite="nose.collector",
+    install_requires=[
+        'Flask',
+        'Flask-Script',
+        'celery>=2.2.0',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
