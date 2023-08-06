594e7034f90cf5f1ca62550a315ec3b8fd65e206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package django-adminbrowse/0.1.2/django-adminbrowse-0.1.2.tar.gz

---
+++ packages/django-adminbrowse/django-adminbrowse-0.1.2.tar.gz/django-adminbrowse-0.1.2/adminbrowse/__init__.py
@@ -0,0 +1,8 @@
+from adminbrowse.base import (ChangeListColumn, ChangeListTemplateColumn,
+                              ChangeListModelFieldColumn, template_column,
+                              model_field)
+from adminbrowse.related import (link_to_change, link_to_changelist,
+                                 related_list)
+from adminbrowse.columns import link_to_url, truncated_field
+from adminbrowse.admin import AutoBrowseModelAdmin
+
