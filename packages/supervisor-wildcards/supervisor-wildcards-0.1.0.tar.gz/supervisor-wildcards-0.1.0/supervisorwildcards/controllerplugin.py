960dd56bd7bebe4de37b6ea97d64910bb3945299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:58 +0000

    Add package supervisor-wildcards/0.1.0/supervisor-wildcards-0.1.0.tar.gz

---
+++ packages/supervisor-wildcards/supervisor-wildcards-0.1.0.tar.gz/supervisor-wildcards-0.1.0/supervisorwildcards/controllerplugin.py
@@ -0,0 +1,49 @@
+import fnmatch
+
+from supervisor.supervisorctl import ControllerPluginBase
+
+class WildCardsControllerPlugin(ControllerPluginBase):
+    name = 'wildcards'
+
+    def __init__(self, controller, **config):
+        self.ctl = controller
+
+    def _expand_wildcards(self, arg, command):
+        patterns = arg.split()
+        supervisor = self.ctl.get_supervisor()
+        matched = False
+        if 'all' in patterns:
+            self.ctl.onecmd('%s all' % command)
+            return
+        for process in supervisor.getAllProcessInfo():
+            for pattern in patterns:
+                if fnmatch.fnmatch(process['name'], pattern):
+                    self.ctl.onecmd('%s %s' % (command, process['name']))
+                    matched = True
+        if not matched:
+            self.ctl.output('No process matched given expression.')
+
+    def _wrap_help(self, command):
+        self.ctl.output('The same as %s, but accepts wildcard expressions to match the process name.' % command)
+        self.ctl.output('m%s a* - %ss all processes begining with "a".' % (command, command))
+
+
+    def do_mstop(self, arg):
+        self._expand_wildcards(arg, command='stop')
+    def do_mstart(self, arg):
+        self._expand_wildcards(arg, command='start')
+    def do_mrestart(self, arg):
+        self._expand_wildcards(arg, command='restart')
+
+    def help_mstop(self):
+        return self._wrap_help('stop')
+    def help_mstart(self):
+        return self._wrap_help('start')
+    def help_mrestart(self):
+        return self._wrap_help('restart')
+
+
+
+
+def make_wildcards_controllerplugin(controller, **config):
+    return WildCardsControllerPlugin(controller, **config)
