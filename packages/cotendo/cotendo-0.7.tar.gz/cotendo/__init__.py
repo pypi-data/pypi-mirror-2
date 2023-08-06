fb0f1ea59caa2315cf3d8ffd07353b4c53917fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:30 +0000

    Add package cotendo/0.7/cotendo-0.7.tar.gz

---
+++ packages/cotendo/cotendo-0.7.tar.gz/cotendo/__init__.py
@@ -0,0 +1,212 @@
+import logging
+import suds
+
+from suds.client import Client
+from suds.plugin import MessagePlugin
+from suds.sax.text import Text
+
+from cotendohelper import CotendoDNS, CotendoCDN, UnescapedText
+
+class Cotendo(object):
+    """
+    Variable information:
+
+    * cname
+        The CNAME of the origin
+
+    * domainName
+        Name of the domain
+
+    * domainConf
+        [Domain XML Configuration](http://help.cotendo.net/display/Manual22/ADNS+XML+Reference+Guide?undefined)
+
+    * environment
+        Staging or Production configuration. (0 for staging, 1 for production)
+
+    * expressions
+        The expression can contain multiple flush jobs.
+        The delimiter is by a new line.
+
+    * originConf
+        [Origin XML Configuration](http://help.cotendo.net/display/Manual22/CDN+XML+Reference+Guide?undefined)
+
+    * token
+        Last origin configuration token.
+        (Call cdn_get_conf to retrieve the latest token)
+
+    * type
+        Flush type is either 'hard' or 'soft'
+
+    * variables
+        Container for the variable tags
+
+        Ex:
+
+            <variables>
+               <variable name="ny_weight" value="10"/>
+            </variables>
+
+    * variable tag
+        Ex:
+
+            <variable name="ny_weight" value="10"/>
+
+    """
+    def __init__(self, username, password, debug=False):
+        logging.basicConfig(level=logging.INFO)
+        self.client = Client('https://api.cotendo.net/cws?wsdl',
+                             location = 'http://api.cotendo.net/cws?ver=1.0',
+                             username=username, password=password,
+                             plugins=[CotendoPlugin()])
+        if debug:
+            logging.getLogger('suds.client').setLevel(logging.DEBUG)
+            logging.getLogger('suds.transport').setLevel(logging.DEBUG)
+            logging.getLogger('suds.xsd.schema').setLevel(logging.DEBUG)
+            logging.getLogger('suds.wsdl').setLevel(logging.DEBUG)
+
+    def cdn_get_conf(self, cname, environment):
+        """
+        Returns the existing origin configuration and token from the CDN
+        """
+        response = self.client.service.cdn_get_conf(cname, environment)
+        cdn_config = CotendoCDN(response)
+        return cdn_config
+
+    def cdn_publish_conf(self, cname):
+        """
+        Publishes a requested origin staging configuration
+        """
+        return self.client.service.cdn_publish_conf(cname)
+
+    def cdn_set_conf(self, cname, originConf, environment, token):
+        """
+        The cdn_set_conf method enables the user to update an existing
+        origin configuration in the CDN.
+
+        The cdn_get_conf returns the token in the response and the
+        cdn_set_conf requires a token as one of the parameters.
+
+        The set action is valid only if the token returned is equal to
+        the token representing the current version of the configuration file.
+        """
+        return self.client.service.cdn_set_conf(
+            cname, originConf, environment, token)
+
+    def dns_get_conf(self, domainName, environment):
+        """
+        Returns the existing domain configuration and token from the ADNS
+        """
+        response = self.client.service.dns_get_conf(domainName, environment)
+        dns_config = CotendoDNS(response)
+        return dns_config
+
+    def dns_publish_conf(self, domainName):
+        """
+        Publishes a requested origin staging configuration
+        """
+        return self.client.service.dns_publish_conf(domainName)
+
+    def dns_set_conf(self, domainName, domainConf, environment, token):
+        """
+        The cdn_set_conf method enables the user to update an existing
+        origin configuration in the CDN.
+
+        The cdn_get_conf returns the token in the response and the
+        cdn_set_conf requires a token as one of the parameters.
+
+        The set action is valid only if the token returned is equal to
+        the token representing the current version of the configuration file.
+        """
+        return self.client.service.dns_set_conf(
+            domainName, domainConf, environment, token)
+
+    def dns_set_variables(self, variables):
+        """
+        This API sets one or more variable values in the DNS configuration.
+        """
+        return self.client.service.dns_set_variables(variables)
+
+    def doFlush(self, cname, flushExpression, flushType):
+        """
+        doFlush method enables specific content to be "flushed" from the
+        cache servers.
+
+        * Note: The flush API is limited to 1,000 flush invocations per hour
+        (each flush invocation may include several objects). *
+        """
+        return self.client.service.doFlush(
+            cname, flushExpression, flushType)
+
+class CotendoPlugin(MessagePlugin):
+    def marshalled(self, context):
+        # Adjust prefixes
+        context.envelope.refitPrefixes()
+        context.envelope.expns = None
+
+        context.envelope = context.envelope.setPrefix(
+                'soap', 'http://schemas.xmlsoap.org/soap/envelope/')
+
+        # Update the Envelope name
+        context.envelope.rename('soap:Envelope')
+
+        # Remove Excess header
+        context.envelope.remove(context.envelope.getChildren()[0])
+
+        # Update the Body name
+        context.envelope.getChildren()[0].rename('soap:Body')
+
+        # Add prefixes
+        context.envelope.addPrefix('xsi',
+                'http://www.w3.org/2001/XMLSchema-instance')
+        context.envelope.addPrefix('soapenc',
+                'http://schemas.xmlsoap.org/soap/encoding/')
+        context.envelope.addPrefix('xsd',
+                'http://www.w3.org/2001/XMLSchema')
+
+        # Set Encoding Style
+        context.envelope.set('soap:encodingStyle',
+                'http://schemas.xmlsoap.org/soap/encoding/')
+
+        body = context.envelope.getChild('Body')
+
+        # Remove xmlns from body
+        body.expns = None
+
+        # Add param xsi types and wrap data in <![CDATA[data]]>
+        method = body.getChildren()[0]
+        for param in method.getChildren():
+            param.set('xsi:type', 'xsd:string')
+            param.text = UnescapedText('<![CDATA[' + Text(param.text) + ']]>')
+
+class CotendoHelper(Cotendo):
+    def __init__(self, username, password, debug=False):
+        super(CotendoHelper, self).__init__(
+            username, password, debug=False)
+        # DNS Helper
+        self.dns = None
+        # CDN Helper
+        self.cdn = None
+
+    def GrabDNS(self, domain, environment):
+        self.dns = self.dns_get_conf(domain, environment)
+
+    def UpdateDNS(self, domain, environment):
+        """Pushes DNS updates"""
+        self.dns_set_conf(domain, self.dns.config,
+                          environment, self.dns.token)
+
+    def ImportDNS(self, config, token=None):
+        """
+        Import a dns configuration file into the helper
+
+        Note: This requires that you have the latest token.
+        To get the latest token, run the GrabDNS command first.
+        """
+        if not token:
+            raise Exception("You must have the dns token set first.")
+        self.dns = CotendoDNS([token, config])
+        return True
+
+    def ExportDNS(self):
+        """Export a dns configuration file from the helper"""
+        return self.dns.config
---
+++ packages/cotendo/cotendo-0.7.tar.gz/cotendo/cotendohelper.py
@@ -0,0 +1,161 @@
+from lxml import etree
+from BeautifulSoup import BeautifulSoup
+from records import *
+
+dns_tag_lookup = {
+    'soa': SOARecord,
+    'ns': NSRecord,
+    'a': ARecord,
+    'cname': CNAMERecord,
+    'mx': MXRecord,
+    'ptr': PTRRecord,
+    'txt': TXTRecord
+    }
+
+self_closing_tags = [
+    'result'
+    ]
+
+class CotendoObject(object):
+    def __init__(self, response):
+        self.token = response[0]
+        self._add_config(response[1])
+
+    def _add_config(self, config):
+        soup = BeautifulSoup(
+            config, selfClosingTags=self_closing_tags)
+        self._data = etree.XML(soup.prettify())
+
+class CotendoDNS(CotendoObject):
+    def __init__(self, response):
+        super(CotendoDNS, self).__init__(response)
+        self._entries = self._get_entries()
+        self.sort()
+
+    def _get_entries(self):
+        records = self._data.getchildren()[0]
+        record_list = []
+        for record in records:
+            # Do not show SOA/NS Records
+            if record.tag in ['soa', 'ns', 'comment']:
+                continue
+
+            # Do not show comments
+            if record.tag == etree.Comment:
+                continue
+
+            recordObj = dns_tag_lookup[record.tag](record)
+            record_list.append(recordObj)
+
+        return record_list
+
+    def show(self):
+        """This could use some love, it's currently here as reference"""
+        for entry in self._entries:
+            print "{'%s': %s, 'records': %s}" % (
+                entry._record_type, entry.host, entry.records)
+        print
+
+    def add_record(self, record):
+        """Add or update a given DNS record"""
+        rec = self.get_record(record._record_type, record.host)
+        if rec:
+            rec = record
+            for i,r in enumerate(self._entries):
+                if r._record_type == record._record_type \
+                        and r.host == record.host:
+                            self._entries[i] = record
+        else:
+            self._entries.append(record)
+        self.sort()
+        return True
+
+    def diff_record(self, record):
+        """Return the removed and added diffs"""
+        rec = self.get_record(record._record_type, record.host)
+        if rec is not None and record is not None:
+            return {'removed': tuple(set(rec.results) - set(record.results)),
+                    'added': tuple(set(record.results) - set(rec.recults))}
+        else:
+            return False
+
+    def get_record(self, dns_record_type, host):
+        """Fetch a DNS record"""
+        record_list = self._entries
+        for record in record_list:
+            if record._record_type == dns_record_type \
+                    and record.host == host:
+                return record
+        return False
+
+    def del_record(self, dns_record_type, host):
+        """Remove a DNS record"""
+        rec = self.get_record(dns_record_type, host)
+        if rec:
+            self._entries = list(set(self._entries) - set([rec]))
+        return True
+
+    def sort(self):
+        unsorted = {
+            'a': [],
+            'cname': [],
+            'mx': [],
+            'ptr': [],
+            'txt': []
+        }
+        for record in self._entries:
+            if record._record_type in unsorted.keys():
+                unsorted[record._record_type].append(record)
+
+        for key in unsorted.keys():
+            unsorted[key] = sorted(
+                unsorted[key], key=lambda result: result.host)
+
+        sorted_entries = []
+        for key in ('a', 'cname', 'mx', 'ptr', 'txt'):
+            for record in unsorted[key]:
+                sorted_entries.append(record)
+
+        self._entries = sorted_entries
+
+    @staticmethod
+    def CreateRecord(record_type, host, results):
+        """
+        Create a record of type (record_type) and fill it with the given
+        dict of results
+        """
+        record_class = dns_tag_lookup[record_type]
+        record = etree.Element("a")
+        record.set("host", host)
+        for params in results:
+            result = etree.Element("result")
+            for k,v in params.iteritems():
+                result.set(k, v)
+            record.append(result)
+
+        return record_class(record)
+
+    @property
+    def config(self):
+        """Create the finalized configuration"""
+        root = etree.Element("xml")
+        resource_records = etree.SubElement(root, "recouce_records")
+
+        # Append SOA and NS records
+        resource_records.append(SOARecord()._etree)
+        resource_records.append(NSRecord()._etree)
+
+        # Append the rest
+        for record in self._entries:
+            resource_records.append(record._etree)
+
+        return etree.tostring(root, encoding="utf-8",
+                              pretty_print=True)
+
+class CotendoCDN(CotendoObject):
+    def entries(self):
+        return etree.tostring(self._data)
+
+class UnescapedText(unicode):
+    def escape(self):
+        return self
