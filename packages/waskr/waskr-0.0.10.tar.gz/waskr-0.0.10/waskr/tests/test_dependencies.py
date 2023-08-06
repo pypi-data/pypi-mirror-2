4c92e0847824cbc37e8f646fcd03b36237e2d536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:30 +0000

    Add package waskr/0.0.10/waskr-0.0.10.tar.gz

---
+++ packages/waskr/waskr-0.0.10.tar.gz/waskr-0.0.10/waskr/tests/test_dependencies.py
@@ -0,0 +1,46 @@
+import unittest
+
+"""Make sure we are able to import everything we need for waskr"""
+
+
+class TestImportDependencies(unittest.TestCase):
+
+    def test_import_bottle(self):
+        "Import Bottle"
+        try:
+            import bottle
+            imported = True
+        except ImportError:
+            imported = False
+        self.assertTrue(imported)
+
+    def test_import_nose(self):
+        "Import Nose"
+        try:
+            import nose
+            imported = True
+        except ImportError:
+            imported = False
+        self.assertTrue(imported)
+
+    def test_import_webtest(self):
+        "Import webtest"
+        try:
+            import webtest
+            imported = True
+        except ImportError:
+            imported = False
+        self.assertTrue(imported)
+
+    def test_import_pymongo(self):
+        "Import pymongo"
+        try:
+            import pymongo
+            imported = True
+        except ImportError:
+            imported = False
+        self.assertTrue(imported)
+
+
+if __name__ == '__main__':
+    unittest.main()
