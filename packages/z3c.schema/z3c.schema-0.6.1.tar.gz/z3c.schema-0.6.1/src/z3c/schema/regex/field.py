13f88d8f06e5161cf4c034af9f93c9ae349971bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package z3c.schema/0.6.1/z3c.schema-0.6.1.tar.gz

---
+++ packages/z3c.schema/z3c.schema-0.6.1.tar.gz/z3c.schema-0.6.1/src/z3c/schema/regex/field.py
@@ -0,0 +1,44 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: field.py 91149 2008-09-14 22:39:34Z rogerineichen $
+"""
+__docformat__ = "reStructuredText"
+
+import re
+import zope.interface
+import zope.schema
+
+from z3c.schema.regex import interfaces
+
+
+class Regex(zope.schema.ASCIILine):
+    """Regex schema field.
+
+    Must be a compilable regular expression
+    """
+
+    zope.interface.implements(interfaces.IRegex)
+
+    def _validate(self, value):
+        super(Regex, self)._validate(value)
+        try:
+            re.compile(value)
+        except re.error, e:
+            raise interfaces.InvalidRegex, '%r, %s' % (value, e)
+
+    def fromUnicode(self, value):
+        v = str(value.strip())
+        self.validate(v)
+        return v
