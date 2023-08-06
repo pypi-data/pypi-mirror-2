546589da77c5a6ff91bfb8b96453fd94948f58da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:15 +0000

    Add package mvob.InfoBlad/0.9/mvob.InfoBlad-0.9.tar.gz

---
+++ packages/mvob.InfoBlad/mvob.InfoBlad-0.9.tar.gz/mvob.InfoBlad-0.9/mvob/InfoBlad/config.py
@@ -0,0 +1,9 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'mvob.InfoBlad'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'InfoBlad': 'mvob.InfoBlad: Add InfoBlad',
+}
---
+++ packages/mvob.InfoBlad/mvob.InfoBlad-0.9.tar.gz/mvob.InfoBlad-0.9/mvob/InfoBlad/interfaces/infoblad.py
@@ -0,0 +1,12 @@
+from zope.interface import Interface
+# -*- Additional Imports Here -*-
+from zope import schema
+
+from mvob.InfoBlad import InfoBladMessageFactory as _
+
+
+class IInfoBlad(Interface):
+    """Type that is similar to Document but is folderish and has a view to display images in a photoalbum"""
+
+    # -*- schema definition goes here -*-
+#
