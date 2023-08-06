c5eb03637fc967f0a6da23a67330ddf827f45f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:12 +0000

    Add package django-dbtemplates/1.2.1/django-dbtemplates-1.2.1.tar.gz

---
+++ packages/django-dbtemplates/django-dbtemplates-1.2.1.tar.gz/django-dbtemplates-1.2.1/setup.py
@@ -0,0 +1,39 @@
+import codecs
+from os import path
+from setuptools import setup, find_packages
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+
+setup(
+    name='django-dbtemplates',
+    version=':versiontools:dbtemplates:',
+    description='Template loader for templates stored in the database',
+    long_description=read(path.join(path.dirname(__file__), 'README.rst')),
+    author='Jannis Leidel',
+    author_email='jannis@leidel.info',
+    url='http://django-dbtemplates.readthedocs.org/',
+    packages=find_packages(exclude=['example']),
+    zip_safe=False,
+    package_data = {
+        'dbtemplates': [
+            'locale/*/LC_MESSAGES/*',
+            'static/dbtemplates/css/*.css',
+            'static/dbtemplates/js/*.js',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+    ],
+    install_requires=['django-appconf >= 0.4'],
+    setup_requires=['versiontools >= 1.5'],
+)
