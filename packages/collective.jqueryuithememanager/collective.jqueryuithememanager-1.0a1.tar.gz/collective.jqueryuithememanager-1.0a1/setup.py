87341bf3c5debd07b369c0336ab390ffe6fdf9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:04 +0000

    Add package collective.jqueryuithememanager/1.0a1/collective.jqueryuithememanager-1.0a1.tar.gz

---
+++ packages/collective.jqueryuithememanager/collective.jqueryuithememanager-1.0a1.tar.gz/collective.jqueryuithememanager-1.0a1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a1'
+
+setup(name='collective.jqueryuithememanager',
+      version=version,
+      description="JQueryUI theme manager for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='jeanmichel.francois@makina-corpus.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.js.jqueryui',
+          'collective.z3cform.colorpicker',
+          'plone.app.registry',
+          'plone.resource',
+          'plone.namedfile',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require = dict(
+          tests=['plone.app.testing'],
+      ),
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
