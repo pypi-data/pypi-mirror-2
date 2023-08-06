085e934372594ef004d45fec5b082e05ab6fdefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:41 +0000

    Add package zopyx.sharepoint/0.2.0/zopyx.sharepoint-0.2.0.zip

---
+++ packages/zopyx.sharepoint/zopyx.sharepoint-0.2.0.zip/zopyx.sharepoint-0.2.0/zopyx/sharepoint/tests/test_connector.py
@@ -0,0 +1,13 @@
+import unittest
+
+from zopyx.sharepoint.connection import Connector
+
+class ConnectorTests(unittest.TestCase):
+
+    def testConnector(self):
+        connector = Connector()
+    
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(ConnectorTests))
+    return suite
