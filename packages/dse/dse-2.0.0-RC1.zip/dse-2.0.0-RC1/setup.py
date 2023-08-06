83fa1528760ac2b48bab860ed13cb9a8d9de6dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:01 +0000

    Add package dse/2.0.0-RC1/dse-2.0.0-RC1.zip

---
+++ packages/dse/dse-2.0.0-RC1.zip/dse-2.0.0-RC1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='dse',
+      version='2.0.0-RC1',
+      description='DSE - Simplified "bulk" insert/update/delete for Django.',
+      author='Thomas Weholt',
+      author_email='thomas@weholt.org',
+      long_description=open('README.txt').read(),
+      packages = ['dse'],
+      install_requires = ['django'],
+      url = "https://bitbucket.org/weholt/dse2",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Framework :: Django',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Database',
+          ],
+      )
