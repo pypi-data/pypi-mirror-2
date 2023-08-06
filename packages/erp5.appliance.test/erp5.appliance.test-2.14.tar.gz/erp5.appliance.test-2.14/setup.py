a916b092a98e83e7fe722ccd4af29f6a289deabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:43 +0000

    Add package erp5.appliance.test/2.14/erp5.appliance.test-2.14.tar.gz

---
+++ packages/erp5.appliance.test/erp5.appliance.test-2.14.tar.gz/erp5.appliance.test-2.14/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+name = "erp5.appliance.test"
+version = '2.14'
+
+def r(n): return open(n).read()
+
+setup(
+    name=name,
+    version=version,
+    author='Lukasz Nowak',
+    author_email='luke@nexedi.com',
+    description="ERP5 Appliance testing system",
+    long_description=r('README.txt') + '\n' + r('CHANGES.txt'),
+    license="GPL",
+    keywords="erp5 appliance test",
+    classifiers=[
+      "Environment :: Console",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: GNU General Public License (GPL)",
+      "Operating System :: POSIX",
+      "Topic :: Software Development :: Testing",
+      "Programming Language :: Python :: 2.4",
+      "Programming Language :: Python :: 2.5",
+      "Programming Language :: Python :: 2.6",
+      "Programming Language :: Python :: 2.7",
+      ],
+    package_dir={'':'src'},
+    url='http://svn.erp5.org/erp5/trunk/utils/%s' % name,
+    py_modules=['test'],
+    install_requires=['pexpect'],
+    entry_points="""
+    [console_scripts]
+    erp5apptest212 = test:main212
+    erp5apptest28 = test:main28
+    """,
+    )
