1ab44753dc025c3616256951874466dde80b160b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:46 +0000

    Add package jmbo-contact/0.0.6/jmbo-contact-0.0.6.tar.gz

---
+++ packages/jmbo-contact/jmbo-contact-0.0.6.tar.gz/jmbo-contact-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-contact',
+    version='0.0.6',
+    description='Jmbo contact form app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt International',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-contact',
+    packages = find_packages(),
+    install_requires = [
+        'django-preferences',
+        'jmbo',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
