321036b41c63b914a61801e2fb30bf746bad3fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:58 +0000

    Add package json-store/1.0/json-store-1.0.tar.gz

---
+++ packages/json-store/json-store-1.0.tar.gz/json-store-1.0/setup.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+from distutils.core import setup
+
+setup(
+    name='json-store',
+    version='1.0',
+    packages=['json_store',],
+    scripts=['bin/shelve2json.py',],
+    license='MIT License',
+    long_description="A JSON store to use in place of shelve.",
+    author='jeremy avnet',
+    author_email='brainsik-code@theory.org',
+    url='https://github.com/brainsik/brainsik',
+)
