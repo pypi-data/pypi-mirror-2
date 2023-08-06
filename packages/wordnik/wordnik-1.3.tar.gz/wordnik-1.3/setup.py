afd7a3632073ddbb2cc3744314035e0897e8da64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:15 +0000

    Add package wordnik/1.3/wordnik-1.3.tar.gz

---
+++ packages/wordnik/wordnik-1.3.tar.gz/wordnik-1.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='wordnik',
+    version="1.3",
+    description='Simple wrapper around the wordnik API',
+    author='Robin Walsh',
+    author_email='robin@wordnik.com',
+    url='http://developer.wordnik.com',
+    packages = ['wordnik' ],
+)
