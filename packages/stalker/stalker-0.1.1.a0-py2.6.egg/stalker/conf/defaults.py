417780eb75fc61793946795f3f93b3bd6f9d6bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:30 +0000

    Add package stalker/0.1.1.a0/stalker-0.1.1.a0-py2.6.egg

---
+++ packages/stalker/stalker-0.1.1.a0-py2.6.egg/stalker/conf/defaults.py
@@ -0,0 +1,50 @@
+#-*- coding: utf-8 -*-
+import tempfile
+
+#
+# The default database addres
+# 
+DATABASE = "sqlite:///:memory:"
+
+
+#
+# The default settings for the database, see sqlalchemy.create_engine for
+# possible parameters
+# 
+DATABASE_ENGINE_SETTINGS = {
+    "echo":False,
+}
+
+
+DATABASE_SESSION_SETTINGS ={}
+
+STUDIO_DATABASE= "sqlite:///:memory:"
+PROJECT_DATABASE = "sqlite:///:memory:"
+
+#
+# Tells Stalker to create an admin by default
+#
+AUTO_CREATE_ADMIN = True
+
+# 
+# these are for new projects
+# after creating the project you can change them from the interface
+# 
+ADMIN_NAME = "admin"
+ADMIN_PASSWORD = "admin"
+ADMIN_EMAIL = "admin@admin.com"
+ADMIN_DEPARTMENT_NAME = "admins"
+
+
+# the default keyword which is going to be used in password scrambling
+KEY = "stalker_default_key"
+
+
+#
+# The default mapper module, see docs for mappers for complete description of
+# mappers
+#
+MAPPERS = [
+"stalker.db.mapper"
+]
+
