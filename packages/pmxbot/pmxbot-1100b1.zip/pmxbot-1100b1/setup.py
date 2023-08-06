a2fcb22aa8698a4bbbba0f03ef6f31def699dc6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:57 +0000

    Add package pmxbot/1100b1/pmxbot-1100b1.zip

---
+++ packages/pmxbot/pmxbot-1100b1.zip/pmxbot-1100b1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+setup(
+	name="pmxbot",
+	version="1100b1",
+	packages=["pmxbot", "pmxbotweb", "pmxbot.popquotes"],
+	package_data={
+		'pmxbot' : ["popquotes.sqlite",],
+		'pmxbotweb' : ["templates/*.html", "templates/pmxbot.png",],
+	},
+	entry_points=dict(
+		console_scripts = [
+			'pmxbot=pmxbot.pmxbot:run',
+			'pmxbotweb=pmxbotweb.pmxbotweb:run',
+		],
+	),
+	install_requires=[
+		"pyyaml",
+		"python-irclib",
+		"httplib2",
+		"feedparser",
+		"pytz",
+		#for viewer
+		"jinja2",
+		"cherrypy",
+	],
+	description="IRC bot - full featured, yet extensible and customizable",
+	license = 'MIT',
+	author="You Gov, Plc. (jamwt, mrshoe, cperry, chmullig, and others)",
+	author_email="open.source@yougov.com",
+	maintainer = 'chmullig',
+	maintainer_email = 'chmullig@gmail.com',
+	url = 'http://bitbucket.org/yougov/pmxbot',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: POSIX',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: MacOS :: MacOS X',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		'Topic :: Communications :: Chat :: Internet Relay Chat',
+		'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+	],
+	long_description = open('README').read(),
+)
