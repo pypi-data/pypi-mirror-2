33ccc7cf4df5e50e075ead015925c33c3ba1359d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:21 +0000

    Add package panya-banner/0.0.4/panya-banner-0.0.4.tar.gz

---
+++ packages/panya-banner/panya-banner-0.0.4.tar.gz/panya-banner-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panya-banner',
+    version='0.0.4',
+    description='Panya banner app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/panya-banner',
+    packages = find_packages(),
+    install_requires = [
+        'django-preferences',
+        'panya',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
