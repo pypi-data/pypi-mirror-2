66b7343567262a1ec3e19fbbe865a702742b2625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:13 +0000

    Add package PloneTerminal/0.3/PloneTerminal-0.3.zip

---
+++ packages/PloneTerminal/PloneTerminal-0.3.zip/PloneTerminal-0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.3'
+
+setup(name='PloneTerminal',
+      version=version,
+      description="A Terminal for Plone",
+      long_description=open('README.txt').read(),
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Framework :: Plone',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          ],
+      keywords='plone terminal',
+      author='Bearstech',
+      author_email='py@bearstech.com',
+      url='https://github.com/bearstech/PloneTerminal',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
