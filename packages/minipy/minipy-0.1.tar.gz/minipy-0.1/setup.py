5a013139c74521e02e1ff6bcb029d1ae7a54a975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:16 +0000

    Add package minipy/0.1/minipy-0.1.tar.gz

---
+++ packages/minipy/minipy-0.1.tar.gz/minipy-0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import minipy
+
+setup(
+    author = minipy.__author__,
+    author_email = minipy.__email__,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Pre-processors',
+        ],
+    description = 'Minify Python 2 source code',
+    license = minipy.__license__,
+    long_description = open('README.rst').read(),
+    maintainer = minipy.__maintainer__,
+    maintainer_email = minipy.__email__,
+    name = 'minipy',
+    py_modules = ['minipy', 'test_minipy'],
+    url = 'https://github.com/gareth-rees/minipy',
+    version = minipy.__version__,
+)
