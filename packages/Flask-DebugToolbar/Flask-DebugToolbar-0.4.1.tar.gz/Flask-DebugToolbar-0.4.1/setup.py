bc77f1c0a54e90ad3b3ef62061be9848cb118026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:59 +0000

    Add package Flask-DebugToolbar/0.4.1/Flask-DebugToolbar-0.4.1.tar.gz

---
+++ packages/Flask-DebugToolbar/Flask-DebugToolbar-0.4.1.tar.gz/Flask-DebugToolbar-0.4.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Flask-DebugToolbar',
+    version='0.4.1',
+    url='http://github.com/mvantellingen/flask-debugtoolbar',
+    license='BSD',
+    author='Michael van Tellingen',
+    author_email='michaelvantellingen@gmail.com',
+    description='A port of the Django debug toolbar to Flask',
+    long_description=__doc__,
+    packages=find_packages(),
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'setuptools',
+        'Flask',
+        'blinker'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
+
