9a0501b2cfea4388810e801bc61250130ac61633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:24 +0000

    Add package django-likes/0.0.2/django_likes-0.0.2-py2.6.egg

---
+++ packages/django-likes/django_likes-0.0.2-py2.6.egg/likes/views.py
@@ -0,0 +1,15 @@
+from secretballot import views
+from secretballot.models import Vote
+
+def can_vote(request, content_type, object_id, vote):
+    return content_type.model_class().objects.get(id=object_id).can_vote(request)[0]
+  
+def like(request, content_type, id, vote):
+    url_friendly_content_type = content_type
+    content_type = content_type.replace("-", ".")
+    if request.is_ajax():
+        return views.vote(request, content_type=content_type, object_id=id, vote=vote, template_name='likes/inclusion_tags/likes.html', can_vote_test=can_vote, extra_context={'can_vote': False, 'vote_status': 'voted', "content_type": url_friendly_content_type})
+    else:
+        # Redirect to referer but append unique number(determined from global vote count) to end of URL to bypass local cache.
+        redirect_url = '%s?v=%s' % (request.META['HTTP_REFERER'], Vote.objects.count() + 1)
+        return views.vote(request, content_type=content_type, object_id=id, vote=vote, redirect_url=redirect_url, can_vote_test=can_vote)
