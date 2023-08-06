908f6cde65239f9f2c40c9a82238eb1feb12cddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:37 +0000

    Add package pysmbc/1.0.11.1/pysmbc-1.0.11.1.tar.bz2

---
+++ packages/pysmbc/pysmbc-1.0.11.1.tar.bz2/pysmbc-1.0.11.1/tests/settings.py
@@ -0,0 +1,13 @@
+# This is setting template.
+# You must change these parameter if you perform test.
+# The following configration will create \\server\share\testdir
+# directory, so user should have write permission.
+
+WORKGROUP = "WORKGROUP"
+SERVER = "hamano201"
+SHARE = "test"
+USERNAME = "user1"
+PASSWORD = "password"
+TESTDIR = "testdir"
+
+
