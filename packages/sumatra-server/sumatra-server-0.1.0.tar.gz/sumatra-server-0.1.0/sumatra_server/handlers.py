b1f59f4387fb2aba2f36202c925ef7ac876e1c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:04 +0000

    Add package sumatra-server/0.1.0/sumatra-server-0.1.0.tar.gz

---
+++ packages/sumatra-server/sumatra-server-0.1.0.tar.gz/sumatra-server-0.1.0/sumatra_server/handlers.py
@@ -0,0 +1,226 @@
+from piston.handler import BaseHandler
+from piston.utils import rc, validate
+from sumatra.recordstore.django_store import models
+import datetime
+from django.core.urlresolvers import reverse
+from django.db.models import ForeignKey
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from forms import PermissionsForm
+
+"""
+Note that in the Wikipedia REST article, the verb table differs slightly from
+what is in the Piston documentation. In Wikipedia, it states that for an
+element URI (e.g. a simulation record here), PUT should be used to create a
+new element with a specified ID, while POST should be used to treat the resource
+as a collection and add a subordinate. Piston says use POST to an element URI
+to create a new resource. The Wikipedia approach seems more logical to me, so I'm
+using it.
+"""
+
+
+def build_timestamp(**kwargs):
+    D = {}
+    for key in 'year', 'month', 'day', 'hour', 'minute', 'second':
+        D[key] = int(kwargs[key])
+    return datetime.datetime(**D)
+
+def keys2str(D):
+    """Keywords cannot be unicode."""
+    E = {}
+    for k,v in D.items():
+        E[str(k)] = v
+    return E
+
+def check_permissions(func):
+    def wrapper(self, request, project, *args, **kwargs):
+        if request.user.projectpermission_set.filter(project__id=project).count():
+            return func(self, request, project, *args, **kwargs)
+        else:
+            return rc.FORBIDDEN
+    return wrapper
+
+
+class RecordHandler(BaseHandler):
+    allowed_methods = ('GET', 'PUT', 'DELETE')
+    model = models.Record
+    fields = ('label', 'timestamp', 'reason', 'outcome', 'duration',
+              'executable', 'repository', 'main_file', 'version', 'diff',
+              'dependencies', 'parameters', 'launch_mode', 'datastore',
+              'data_key', 'platforms', 'tags', 'user', 'project_id',
+              'script_arguments', 'input_data')
+    template = "record_detail.html"
+    
+    def queryset(self, request): # this is already defined in more recent versions of Piston
+        return self.model.objects.all()
+    
+    @classmethod
+    def project_id(self, record):
+        return record.project.id
+    
+    @check_permissions
+    def read(self, request, project, label):
+        filter = {'project': project, 'label': label}
+        try:
+            return self.queryset(request).get(**filter)
+        except ObjectDoesNotExist:
+            return rc.NOT_FOUND
+        
+    @check_permissions
+    def update(self, request, project, label):
+        # this performs update if the record already exists, and create otherwise
+        filter = {'project': project, 'label': label}
+        attrs = self.flatten_dict(request.data)
+        #print attrs
+        try:
+            # need to check consistency between URL project, group, timestamp
+            # and the same information in request.data
+            # we should also limit the fields that can be updated
+            updatable_fields = ('reason', 'outcome', 'tags')
+            inst = self.queryset(request).get(**filter)
+            for field_name in updatable_fields:
+                setattr(inst, field_name, attrs[field_name])
+            inst.save()
+            return rc.ALL_OK
+        except self.model.DoesNotExist:
+            # check consistency between URL project, label
+            # and the same information in attrs. Remove those items from attrs
+            assert label == attrs["label"]
+            prj, created = models.Project.objects.get_or_create(id=filter["project"])
+            if created:
+                prj.projectpermission_set.create(user=request.user)
+            inst = self.model(project=prj, label=label)
+            fields = [field for field in self.model._meta.fields if field.name not in ('project', 'label', 'db_id')]
+            for field in fields:
+                if isinstance(field, ForeignKey):
+                    fk_model = field.rel.to
+                    obj_attrs = keys2str(attrs[field.name])
+                    ##print field.name, fk_model, obj_attrs
+                    fk_inst, created = fk_model.objects.get_or_create(**obj_attrs)
+                    setattr(inst, field.name, fk_inst)
+                else:
+                    ##print field.name, attrs[field.name], type(attrs[field.name])
+                    setattr(inst, field.name, attrs[field.name])
+            inst.save()
+            for field in self.model._meta.many_to_many:
+                for obj_attrs in attrs[field.name]:
+                    getattr(inst, field.name).get_or_create(**keys2str(obj_attrs))
+            inst.save()
+            return rc.CREATED
+        except self.model.MultipleObjectsReturned: # this should never happen
+            return rc.DUPLICATE_ENTRY
+
+    @check_permissions
+    def delete(self, request, project, label):
+        filter = {'project': project, 'label': label}
+        response = BaseHandler.delete(self, request, **filter)
+        if response.status_code == 410: # Using 404 instead of 410 if resource doesn't exist
+            response = rc.NOT_FOUND # to me 'Gone' implies it used to exist, and we can't say that for sure
+        return response
+
+
+class ProjectHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    template = "project_detail.html"
+    
+    def read(self, request, project):
+        try:
+            prj = models.Project.objects.get(id=project, projectpermission__user=request.user)
+        except models.Project.DoesNotExist:
+            return rc.FORBIDDEN
+        records = prj.record_set.all()
+        tags = request.GET.get("tags", None)
+        if tags:
+            records = records.filter(tags__contains=tags)
+        protocol = request.is_secure() and "https" or "http"
+        project_uri = "%s://%s%s" % (protocol, request.get_host(), reverse("sumatra-project", args=[prj.id]))
+        return {
+                    'id': prj.id,
+                    'name': prj.get_name(),
+                    'description': prj.description,
+                    'records': [ "%s%s/" % (project_uri, rec.label)
+                                for rec in records],
+                    'access': [perm.user.username for perm in prj.projectpermission_set.all()],
+                    'tags': tags,
+                }
+
+
+class PermissionListHandler(BaseHandler):
+    allowed_methods = ('GET', 'POST',)
+    template = "project_permissions.html"
+    
+    def read(self, request, project):
+        try:
+            prj = models.Project.objects.get(id=project, projectpermission__user=request.user)
+        except models.Project.DoesNotExist:
+            return rc.FORBIDDEN
+        return {
+                    'id': prj.id,
+                    'name': prj.get_name(),
+                    'access': [perm.user for perm in prj.projectpermission_set.all()],
+                }
+
+    @validate(PermissionsForm)
+    def create(self, request, project):
+        try:
+            prj = models.Project.objects.get(id=project, projectpermission__user=request.user)
+        except models.Project.DoesNotExist:
+            return rc.FORBIDDEN
+        prj.projectpermission_set.create(user=request.form.cleaned_data["user"])
+        return HttpResponseRedirect(reverse("sumatra-project", args=[prj.id]))
+
+
+class ProjectListHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    template = "project_list.html"
+    
+    def read(self, request):
+        if request.user.is_anonymous():
+            user, _ = User.objects.get_or_create(username="anonymous")
+            user.set_password("")
+        else:
+            user = request.user
+        protocol = request.is_secure() and "https" or "http"
+        return [ {
+                    "id": prj.id,
+                    "name": prj.get_name(),
+                    "description": prj.description,
+                    "uri": "%s://%s%s" % (protocol, request.get_host(), reverse("sumatra-project", args=[prj.id])),
+                  }
+                  for prj in models.Project.objects.filter(projectpermission__user=user) ]
+        
+
+# the following are currently defined only to suppress the 'id'
+# in the output
+
+class ExecutableHandler(BaseHandler):
+    allowed_methods = []
+    model = models.Executable
+    
+class ParameterSetHandler(BaseHandler):
+    allowed_methods = []
+    model = models.ParameterSet
+    
+class RepositoryHandler(BaseHandler):
+    allowed_methods = []
+    model = models.Repository
+    
+class LaunchModeHandler(BaseHandler):
+    allowed_methods = []
+    model = models.LaunchMode
+    
+class DatastoreHandler(BaseHandler):
+    allowed_methods = []
+    model = models.Datastore
+    
+class PlatformHandler(BaseHandler):
+    allowed_methods = []
+    model = models.PlatformInformation
+    
+class DependencyHandler(BaseHandler):
+    allowed_methods = []
+    model = models.Dependency
+
+# note: if we start to look at the Accept header, should send 406 response if we can't send the requested mimetype (RFC 2616)
---
+++ packages/sumatra-server/sumatra-server-0.1.0.tar.gz/sumatra-server-0.1.0/sumatra_server/tests.py
@@ -0,0 +1,277 @@
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+from django.conf import settings
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import base64
+#from pprint import pprint
+
+OK = 200
+CREATED = 201
+UNAUTHORIZED = 401
+NOT_FOUND = 404
+NO_CONTENT = 204
+
+
+def deunicode(D):
+    """Convert unicode keys and values to str"""
+    E = {}
+    for k,v in D.items():
+        if isinstance(v, dict):
+            v = deunicode(v)
+        elif isinstance(v, unicode):
+            v = str(v)
+        elif isinstance(v, list):
+            v = [deunicode(d) for d in v]
+        E[str(k)] = v
+    return E
+
+class BaseTestCase(TestCase):
+    fixtures = ['haggling', 'permissions']
+
+    def setUp(self):
+        # thanks to Thomas Pelletier for this function
+        # http://thomas.pelletier.im/OK9/12/test-your-django-piston-api-with-auth/
+        self.client = Client()
+        auth = '%s:%s' % ('testuser', 'abc123')
+        auth = 'Basic %s' % base64.encodestring(auth)
+        auth = auth.strip()
+        self.extra = {
+            'HTTP_AUTHORIZATION': auth,
+        }
+
+    def assertMimeType(self, response, desired_mimetype):
+        mimetype, charset = response["Content-Type"].split(";")
+        self.assertEqual(mimetype, desired_mimetype)
+
+
+class ProjectListHandlerTest(BaseTestCase):    
+    
+    def setUp(self):
+        self.client.login(username="testuser", password="abc123")
+    
+    def test_GET_no_data(self):
+        prj_list_uri = reverse("sumatra-project-list")
+        response = self.client.get(prj_list_uri)
+        self.failUnlessEqual(response.status_code, OK)
+        self.assertMimeType(response, "application/json")
+        data = json.loads(response.content)
+        assert isinstance(data, list)
+        self.assertEqual(len(data), 1)
+        prj = data[0]
+        self.assertEqual(prj["uri"], "http://testserver%sTestProject/" % prj_list_uri)
+        
+    def test_GET_format_html(self):
+        prj_list_uri = reverse("sumatra-project-list")
+        response = self.client.get(prj_list_uri, {"format": "html"})
+        self.failUnlessEqual(response.status_code, OK)
+        self.assertMimeType(response, "text/html")
+        
+        
+class ProjectHandlerTest(BaseTestCase):
+    
+    #def setUp(self):
+    #    self.client.login(username="testuser", password="abc123")
+        
+    def test_GET_no_data(self):
+        prj_uri = reverse("sumatra-project",
+                          kwargs={"project": "TestProject"})
+        response = self.client.get(prj_uri, {}, **self.extra)
+        #print response.content
+        self.assertEqual(response.status_code, OK)
+        self.assertMimeType(response, "application/json")
+        data = json.loads(response.content)
+        assert isinstance(data, dict)
+        self.assertEqual(data["id"], "TestProject")
+        assert "testuser" in data["access"]
+        assert isinstance(data["records"], list)
+        for record in data["records"]:
+            touch = self.client.get(record, {}, **self.extra) # should perhaps add support for HEAD to the server
+            self.assertEqual(touch.status_code, OK)
+        
+        
+class RecordHandlerTest(BaseTestCase):
+    
+    def test_GET_no_data(self):
+        #label =  "20100709-154255"
+        label = "haggling"
+        rec_uri = reverse("sumatra-record",
+                          kwargs={"project": "TestProject",
+                                  "label": label})
+        response = self.client.get(rec_uri, {}, **self.extra)
+        #print response.content
+        self.assertEqual(response.status_code, OK)
+        self.assertMimeType(response, "application/json")
+        data = json.loads(response.content)
+        assert isinstance(data, dict)
+        self.assertEqual(data["label"], label)
+        self.assertEqual(set(data.keys()),
+                         set(("executable","label","reason",
+                              "duration","executable","repository",
+                              "main_file","version","parameters",
+                              "launch_mode","datastore","outcome",
+                              "data_key","timestamp","tags","diff",
+                              "user","dependencies", "platforms",
+                              "project_id", "input_data", "script_arguments")))
+        self.assertEqual(data["tags"], "foobar")
+        
+    def test_GET_format_html(self):
+        self.extra = {} # use Django auth, not HTTP Basic
+        self.client.login(username="testuser", password="abc123")
+        label = "haggling"
+        rec_uri = reverse("sumatra-record",
+                          kwargs={"project": "TestProject",
+                                  "label": label})
+        response = self.client.get(rec_uri, {"format": "html"}, **self.extra)
+        self.failUnlessEqual(response.status_code, OK)
+        self.assertMimeType(response, "text/html")
+        
+    def test_GET_not_authenticated(self):
+        label = "haggling"
+        rec_uri = reverse("sumatra-record",
+                          kwargs={"project": "TestProject",
+                                  "label": label})
+        response = self.client.get(rec_uri, {})
+        print response.content
+        self.assertEqual(response.status_code, UNAUTHORIZED)
+        
+    def test_GET_nonexistent_record(self):
+        label = "iquegfxnqiuehfiomehxgo"
+        rec_uri = reverse("sumatra-record",
+                          kwargs={"project": "TestProject",
+                                  "label": label})
+        response = self.client.get(rec_uri, {}, **self.extra)
+        self.failUnlessEqual(response.status_code, NOT_FOUND)
+    
+    def test_GET_Accept_html(self):
+        self.extra = {} # use Django auth, not HTTP Basic
+        self.client.login(username="testuser", password="abc123")
+        label = "haggling"
+        rec_uri = reverse("sumatra-record",
+                          kwargs={"project": "TestProject",
+                                  "label": label})
+        response = self.client.get(rec_uri, {}, Accept="text/html", **self.extra)
+        self.failUnlessEqual(response.status_code, OK)
+        self.assertMimeType(response, "text/html")
+    
+    def test_PUT_new_record_json(self):
+        new_record = {
+            "label": "abcdef",
+            "reason": "uygnougy",
+            "duration": 32.1,
+            "executable": {
+                "path": "lgljgkjhk",
+                "version": "iugnogn",
+                "name": "kljhnhn",
+                "options": "dfgdfg"
+             },
+            "repository": {
+                "url": "iuhnhc;<s",
+                "type": "iufvbfgbjlml",
+            },
+            "main_file": "OIYUGUIYFU",
+            "version": "LUGNYGNYGu",
+            "parameters": {
+                "content": "oignuguygnug",
+                "type": "hjgjgn65878",
+            },
+            "input_data": "sfgshaeth",
+            "script_arguments": "p8yupyrprutot",
+            "launch_mode": {
+                "type": "OIUNIU6nkjgbun", 
+                "parameters": "GUNYGU76565",
+            },
+            "datastore": {
+                "type": "mosigcqpoejf;",
+                "parameters": "oscih,spoirghosgc",
+            },
+            "outcome": "mihiuhpoip",
+            "data_key": "iugbufuyfiutyfitfy",
+            "timestamp": "2010-07-11 22:50:00",
+            "tags": "abcd,efgh,ijklm",
+            "diff": "iugoiug,ihg",
+            "user": "gnugynygy",
+            "dependencies": [{
+                "path": "moh,oh",
+                "version": "liuhiuhiu",
+                "name": "mohuuyfbn",
+                "module": "ouitfvbtfky",
+                "diff": "liugnig,lug",
+                }],
+            "platforms": [{
+                "system_name": "liugiuyhiuyg", 
+                "ip_addr": "igng,iihih,i", 
+                "architecture_bits": "vmsilughcqioej;", 
+                "machine": "cligcnquefgx", 
+                "architecture_linkage": "uygbytfkg", 
+                "version": "luyhtdkguhl,h", 
+                "release": "lufuytdydy", 
+                "network_name": "ouifbf67", 
+                "processor": "iugonuyginugugu"
+            }],
+        }
+        prj_uri = reverse("sumatra-project",
+                          kwargs={"project": "TestProject"})
+        rec_uri = "%s%s/" % (prj_uri, new_record["label"])
+        response = self.client.put(rec_uri, data=json.dumps(new_record),
+                                   content_type="application/json",**self.extra)
+        print response.content
+        self.assertEqual(response.status_code, CREATED)
+        
+        response = self.client.get(rec_uri, {}, **self.extra)
+        self.assertEqual(response.status_code, OK)
+        #pprint(deunicode(json.loads(response.content)))
+        new_record.update(project_id="TestProject")
+        self.maxDiff = None
+        self.assertEqual(new_record, deunicode(json.loads(response.content)))
+        
+    def test_PUT_existing_record_json(self):
+        prj_uri = reverse("sumatra-project",
+                          kwargs={"project": "TestProject"})
+        rec_uri = "%s%s/" % (prj_uri, "haggling")
+        update = {
+            "reason": "reason goes here",
+            "outcome": "comments on outcome go here",
+            "tags": "tagA,tagB,tagC",
+            "version": "this should not be updated"
+        }
+        response = self.client.put(rec_uri, data=json.dumps(update),
+                                   content_type="application/json",**self.extra)
+        print response.content
+        self.assertEqual(response.status_code, OK)
+        
+        response = self.client.get(rec_uri, {}, **self.extra)
+        self.assertEqual(response.status_code, OK)
+        data = json.loads(response.content)
+        assert isinstance(data, dict)
+        self.assertEqual(data["label"], "haggling")
+        self.assertEqual(data["reason"], update["reason"])
+        self.assertEqual(data["outcome"], update["outcome"])
+        self.assertEqual(data["tags"], update["tags"])
+        self.assertNotEqual(data["version"], update["version"])
+    
+    def test_DELETE_existing_record(self):
+        label =  "20100709-154255"
+        rec_uri = reverse("sumatra-record",
+                          kwargs={"project": "TestProject",
+                                  "label": label})
+        response = self.client.get(rec_uri, {}, **self.extra)
+        self.assertEqual(response.status_code, OK)
+        
+        response = self.client.delete(rec_uri, {}, **self.extra)
+        self.assertEqual(response.status_code, NO_CONTENT)
+        
+        response = self.client.get(rec_uri, {}, **self.extra)
+        self.assertEqual(response.status_code, NOT_FOUND)
+        
+    def test_DELETE_nonexistent_record(self):
+        label =  "mochirsueghcnisuercgheiosg"
+        rec_uri = reverse("sumatra-record",
+                          kwargs={"project": "TestProject",
+                                  "label": label})
+        response = self.client.delete(rec_uri, {}, **self.extra)
+        self.assertEqual(response.status_code, NOT_FOUND)
