fa53a38fa80cd7b4d516d84132a844be50f9b393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:48 +0000

    Add package misaka/0.3.1/misaka-0.3.1.tar.gz

---
+++ packages/misaka/misaka-0.3.1.tar.gz/misaka-0.3.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup, Extension
+
+setup(
+    name='misaka',
+    version='0.3.1',
+    description='A Python binding for Upskirt.',
+    author='Frank Smit',
+    author_email='frank@61924.nl',
+    url='http://misaka.61924.nl/',
+    license='MIT',
+    long_description=open('README.txt').read(),
+    ext_modules=[Extension('misaka', [
+        'src/misaka.c',
+        'src/upskirt/array.c',
+        'src/upskirt/buffer.c',
+        'src/upskirt/markdown.c',
+        'src/upskirt/html.c',
+        'src/upskirt/html_smartypants.c',
+        'src/upskirt/autolink.c',
+        'src/upskirt/html_autolink.c'
+    ])]
+)
