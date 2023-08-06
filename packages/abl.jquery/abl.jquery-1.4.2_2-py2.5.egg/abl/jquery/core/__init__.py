8d4698a9d40ed05cb68219ea954588746ae4bb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:14 +0000

    Add package abl.jquery/1.4.2-2/abl.jquery-1.4.2_2-py2.5.egg

---
+++ packages/abl.jquery/abl.jquery-1.4.2_2-py2.5.egg/abl/jquery/core/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from .base import (
+    jquery_js,
+    jquery_cookie_js,
+    jquery_dimensions_js,
+    jquery_bgiframe_js,
+    jQuery,
+    )
+
+from .logging import logging_js
+
