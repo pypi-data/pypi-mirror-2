8671395875b06fc1ed8c1110366a23b96ad71a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:16 +0000

    Add package tendo/0.0.6/tendo-0.0.6.tar.gz

---
+++ packages/tendo/tendo-0.0.6.tar.gz/tendo-0.0.6/setup.py
@@ -0,0 +1,39 @@
+# Setuptools is required for the use_2to3 option below. You should install it
+# from the Distribute home page, http://packages.python.org/distribute/
+import sys
+from setuptools import setup
+
+options = {}
+if sys.version_info > (3, 0):
+    # Automatically run 2to3 when installing under Python 3
+    #options['use_2to3'] = True
+    pass
+
+setup(
+	name = 'tendo',
+	py_modules = ['tendo.colorer', 'tendo.execfile2', 'tendo.singleton', 'tendo.tee', 'tendo.unicode'],
+	version = '0.0.6',
+	description = 'A Python library that extends some core functionality',
+	author = 'Sorin Sbarnea',
+	author_email = 'sorin.sbarnea@gmail.com',
+	url = 'https://github.com/ssbarnea/tendo',
+	download_url = 'https://github.com/ssbarnea/tendo/archives/master',
+	keywords = ['tendo', 'tee', 'unicode', 'colorer', 'singleton'],
+	classifiers = [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Development Status :: 4 - Beta',
+		'Environment :: Other Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Internet',
+	],
+	long_description = '''Update: for complete list of changes read https://github.com/ssbarnea/tendo/commits/master''',
+        **options
+)
+  
