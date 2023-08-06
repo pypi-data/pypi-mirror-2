bcccd73837609144fe700b4f93cbf49f346693e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:15 +0000

    Add package pyfspot/0.2/pyfspot-0.2.tar.gz

---
+++ packages/pyfspot/pyfspot-0.2.tar.gz/pyfspot-0.2/pyfspot/controller.py
@@ -0,0 +1,153 @@
+import os
+import sys
+import logging
+import shutil
+import time
+import urllib
+from gettext import gettext as _
+
+from models import NotFoundError, create_engine, metadata, session, Photo, Tag
+
+
+DEFAULT_DB_FILE = os.path.join(os.path.expanduser('~'), '.config', 'f-spot', 'photos.db')
+
+logger = logging.getLogger(__name__)
+
+
+def backupdb(*args):
+    def wrapper(func):
+        def wrapped(self, *args, **kwargs):
+            self.create_backup()
+            return func(self, *args, **kwargs)
+        return wrapped
+    return wrapper
+
+
+def normalize(*args):
+    def wrapper(func):
+        def wrapped(self, *args, **kwargs):
+            self.normalize_paths()
+            return func(self, *args, **kwargs)
+        return wrapped
+    return wrapper
+
+
+class FSpotController(object):
+
+    def __init__(self, **kwargs):
+        self.backup = kwargs.get('backup', True)
+        self.normalize = kwargs.get('normalize', True)
+        self.dbpath = kwargs.get('dbpath')
+        if not self.dbpath:
+            self.dbpath = DEFAULT_DB_FILE
+        logger.debug(_("Using database file '%s'") % self.dbpath)
+        
+        engine = kwargs.get('engine')
+        if not engine:
+            engine = create_engine('sqlite:///%s' % self.dbpath)
+            metadata.bind = engine
+            session.configure(bind=engine)
+
+        self._photoset = None
+
+    @property
+    def photoset(self):
+        if not self._photoset:
+            return session.query(Photo)
+        return self._photoset
+
+    @photoset.setter
+    def photoset(self, p):
+        self._photoset = p
+
+    def create_backup(self):
+        if self.backup:
+            new = self.dbpath + '~%s' % time.strftime("%Y%m%d%H%M%S")
+            shutil.copyfile(self.dbpath, new)
+            logger.info(_("Database backup created '%s'") % new)
+            # Do it once.
+            self.backup = False
+
+    def normalize_paths(self):
+        if self.normalize:
+            queryset = self.photoset.filter(~Photo.base_uri.endswith(os.sep)).all()
+            if len(queryset):
+                self.create_backup()
+                for p in queryset:
+                    p.base_uri += os.sep
+                session.commit()
+                logger.info(_("Normalized path separator on %s photos.") % len(queryset))
+            
+            total = 0
+            # Look for photo without '%' in their path (i.e. not encoded)
+            queryset = self.photoset.filter(~Photo.base_uri.like('%\\%%', escape="\\")).all()
+            if len(queryset):
+                self.create_backup()
+                for p in queryset:
+                    # Compare encoded version of base_uri to actual version
+                    # (slice file:// part)
+                    base_uri_encoded = urllib.quote(p.base_uri[7:].encode('utf-8'))
+                    if base_uri_encoded != p.base_uri[7:]:
+                        p.base_uri = p.base_uri[:7] + base_uri_encoded
+                        total += 1
+                session.commit()
+                logger.info(_("Normalized path encoding on %s photos.") % total)
+            self.normalize = False
+
+    def find_by_tag(self, tag):
+        if isinstance(tag, basestring):
+            t = session.query(Tag).filter(Tag.name.like(tag)).first()
+        if not t:
+            raise NotFoundError(Tag, tag)
+        return self.photoset.filter(Photo.tags.any(id=t.id))
+
+    @normalize()
+    def find_by_path(self, path):
+        condition = urllib.quote(path)
+        condition = condition.replace('%', "\\%")  # encoding char is not wildchar
+        condition = condition.replace('_', "\\_")  # underscore neither
+        condition = condition.replace("\\%2A", '%')  # Replace * by %
+        condition = condition.replace("\\%3F", '_')  # Replace ? by _
+        return self.photoset.filter(Photo.uri.like(condition, escape="\\"))
+
+    @backupdb()
+    def change_rating(self, rating, safe=False):
+        total = 0
+        for p in self.photoset:
+            if safe and rating < p.rating:
+                continue
+            p.rating = rating
+            total += 1
+        logger.info(_("Set rating %s to %s photos.") % (rating, total))
+        session.commit()
+
+    def find_missing_on_disk(self):
+        # Cannot use hybrid attributes yet.
+        missing = [p.id for p in self.photoset if not p.exists()]
+        return self.photoset.filter(Photo.id.in_(missing))
+
+    def find_missing_in_catalog(self):
+        raise NotImplementedError
+
+    def find_corrupted(self):
+        raise NotImplementedError
+
+    def find_by_time(self):
+        raise NotImplementedError
+
+    def find_duplicates(self):
+        raise NotImplementedError
+
+    def change_path(self, old, new):
+        raise NotImplementedError
+
+    def apply_tag(self):
+        raise NotImplementedError
+
+    def remove(self):
+        raise NotImplementedError
+
+    def delete(self):
+        raise NotImplementedError
+
+
---
+++ packages/pyfspot/pyfspot-0.2.tar.gz/pyfspot-0.2/pyfspot/main.py
@@ -0,0 +1,94 @@
+import sys
+import logging
+import codecs 
+import locale 
+from optparse import OptionParser, OptionGroup
+from gettext import gettext as _
+
+from controller import FSpotController
+
+logger = logging.getLogger(__name__)
+
+
+def main(args=None):
+    # Parse command-line arguments
+    parser = OptionParser()
+    parser.add_option("--database",
+                      dest="database", default=None,
+                      help=_("Path to F-Spot database"))
+    parser.add_option("--log-level",
+                      dest="log_level", default=logging.INFO, type='int',
+                      help=_("Logging level for messages (1:debug 2:info, 3:warning, 4:errors, 5:critical)"))
+    # Find
+    lookupgrp = OptionGroup(parser, _("Queries"))
+    lookupgrp.add_option("--find-path",
+                      dest="find_path", default=None,
+                      help=_("Find by path"))
+    lookupgrp.add_option("--find-tag",
+                      dest="find_tag", default=None,
+                      help=_("Find by tag"))
+    lookupgrp.add_option("--find-missing",
+                      dest="find_missing", default=False, action="store_true",
+                      help=_("Find photos missing on disk"))
+    # Actions
+    actionsgrp = OptionGroup(parser, _("Actions"))
+    actionsgrp.add_option("--list",
+                      dest="list", default=False, action="store_true",
+                      help=_("List photos matching set"))
+    actionsgrp.add_option("--rating",
+                      dest="rating", default=None, type='int',
+                      help=_("Change rating"))
+    actionsgrp.add_option("--safe-rating",
+                      dest="safe_rating", default=False, action="store_true",
+                      help=_("Change rating only if superior to current"))
+    actionsgrp.add_option("--tag",
+                      dest="tag", default=None,
+                      help=_("Apply specified tag"))
+    actionsgrp.add_option("--untag",
+                      dest="untag", default=None,
+                      help=_("Remove specified tag"))
+    parser.add_option_group(lookupgrp)
+    parser.add_option_group(actionsgrp)
+    (options, args) = parser.parse_args(args)
+    
+    logging.basicConfig(level = options.log_level)
+
+    # Start using the controller
+    fm = FSpotController(dbpath=options.database)
+
+    # Chain find queries
+    if options.find_path:
+        path = unicode(options.find_path, 'utf8')
+        fm.photoset = fm.find_by_path(path)
+    if options.find_tag:
+        tagname = unicode(options.find_tag, 'utf8')
+        fm.photoset = fm.find_by_tag(tagname)
+    if options.find_missing:
+        fm.photoset = fm.find_missing_on_disk()
+
+    if options.rating:
+        fm.change_rating(options.rating, options.safe_rating)
+    if options.tag:
+        fm.apply_tag(options.tag)
+    if options.untag:
+        fm.remove_tag(options.untag)
+
+    # List photoset in stdout
+    if options.list:
+        # Force UTF-8 encoding of stdout
+        #sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
+        logger.debug(_("Default locale: %s") % locale.getdefaultlocale()[1])
+        logger.debug(_("Terminal encoding (stdout): %s") % sys.stdout.encoding)
+        for p in fm.photoset:
+            print p.path
+
+    if not any([options.list, 
+                options.rating.
+                options.tag,
+                options.untag]):
+        logger.warning(_("No action was specified."))
+    return 0
+
+if __name__ == "__main__":
+    code = main(sys.argv)
+    sys.exit(code)
