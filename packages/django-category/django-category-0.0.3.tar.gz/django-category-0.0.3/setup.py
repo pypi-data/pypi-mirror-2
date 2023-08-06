0d1f43023dd0e96ad03afa78bcd21af476dfbea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:16 +0000

    Add package django-category/0.0.3/django-category-0.0.3.tar.gz

---
+++ packages/django-category/django-category-0.0.3.tar.gz/django-category-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-category',
+    version='0.0.3',
+    description='Django categorize content app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/django-category',
+    packages = find_packages(),
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
