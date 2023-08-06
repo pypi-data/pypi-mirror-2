f0f5c7f7baf402a71136160dd1954467ca539e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package nimbstor/0.4.1/nimbstor-0.4.1.tar.gz

---
+++ packages/nimbstor/nimbstor-0.4.1.tar.gz/nimbstor-0.4.1/nimbstor/__init__.py
@@ -0,0 +1,9 @@
+"""Store data streams in different backends.
+
+"""
+
+from stor import stor_output, stor_input, stor_util
+from directory_backend import directory_backend
+from combine_backend import combine_backend
+from imap_backend import imap_backend
+import adler64
