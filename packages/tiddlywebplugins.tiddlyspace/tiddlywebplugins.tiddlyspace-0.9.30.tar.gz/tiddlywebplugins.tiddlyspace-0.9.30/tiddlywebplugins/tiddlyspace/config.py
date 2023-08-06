81b9fcd61104e626d257009222ccc72bc0ecaa0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:06 +0000

    Add package tiddlywebplugins.tiddlyspace/0.9.30/tiddlywebplugins.tiddlyspace-0.9.30.tar.gz

---
+++ packages/tiddlywebplugins.tiddlyspace/tiddlywebplugins.tiddlyspace-0.9.30.tar.gz/tiddlywebplugins.tiddlyspace-0.9.30/tiddlywebplugins/tiddlyspace/config.py
@@ -0,0 +1,29 @@
+"""
+Base configuration for TiddlySpace.
+
+This provides the basics which may be changed in tidlywebconfig.py.
+"""
+from tiddlywebplugins.instancer.util import get_tiddler_locations
+
+from tiddlywebplugins.tiddlyspace.instance import store_contents
+
+
+PACKAGE_NAME = 'tiddlywebplugins.tiddlyspace'
+
+
+config = {
+    'instance_tiddlers': get_tiddler_locations(store_contents, PACKAGE_NAME),
+    'atom.default_filter': 'select=tag:!excludeLists;sort=-modified;limit=20',
+    'auth_systems': ['cookie_form', 'tiddlywebplugins.tiddlyspace.openid'],
+    'bag_create_policy': 'ANY',
+    'recipe_create_policy': 'ANY',
+    'css_uri': '/bags/common/tiddlers/tiddlyweb.css',
+    'socialusers.reserved_names': ['www', 'about', 'help', 'announcements',
+        'dev', 'info', 'api', 'status', 'login', 'frontpage'],
+    'cookie_age': '2592000', # 1 month
+    'server_store': ['tiddlywebplugins.tiddlyspace.store', {
+        'db_config': 'mysql:///tiddlyspace?charset=utf8&use_unicode=0'}],
+    'indexer': 'tiddlywebplugins.mysql',
+    'tiddlywebwiki.binary_limit': 1048576, # 1MB
+    'lazy.titles': ['SiteIcon', 'ColorPalette'],
+}
