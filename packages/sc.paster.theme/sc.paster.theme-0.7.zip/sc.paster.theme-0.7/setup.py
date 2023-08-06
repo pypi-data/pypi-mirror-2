ba3b067bb2d9a72451f9c5e35e10274006e979cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:33 +0000

    Add package sc.paster.theme/0.7/sc.paster.theme-0.7.zip

---
+++ packages/sc.paster.theme/sc.paster.theme-0.7.zip/sc.paster.theme-0.7/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+version = '0.7'
+
+setup(name='sc.paster.theme',
+      version=version,
+      description="Simples Consultoria's project skeleton for a Plone Theme (skin). ",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone development simples_consultoria',
+      author='Erico Andrei',
+      author_email='erico@simplesconsultoria.com.br',
+      url='http://www.simplesconsultoria.com.br/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['sc', 'sc.paster'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'ZopeSkel==2.19'
+      ],
+      tests_require=['zope.testing', 'zc.buildout', 'Cheetah', 'PasteScript','ZopeSkel'],
+      test_suite='sc.paster.theme.tests.test_templates.test_suite',
+      entry_points="""
+        [paste.paster_create_template]
+        portal_theme = sc.paster.theme:Theme
+      """,
+      )
