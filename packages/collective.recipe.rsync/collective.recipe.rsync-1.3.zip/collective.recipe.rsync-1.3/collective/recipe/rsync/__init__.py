cb1e7d9ea75dfed5e3be5c57ec45d5fedea820ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:44 +0000

    Add package collective.recipe.rsync/1.3/collective.recipe.rsync-1.3.zip

---
+++ packages/collective.recipe.rsync/collective.recipe.rsync-1.3.zip/collective.recipe.rsync-1.3/collective/recipe/rsync/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Recipe rsync"""
+
+from commands import getoutput
+
+line = ('----------------------------------------' +
+        '----------------------------------------')
+
+
+class Recipe(object):
+    """zc.buildout recipe"""
+
+    def __init__(self, buildout, name, options):
+        self.buildout, self.name, self.options = buildout, name, options
+        source = options['source']
+        target = options['target']
+        cmd = '  rsync -av --partial --progress %s %s' % (source, target)
+
+        print line
+        print 'Running rsync...'
+        print cmd
+        print '  this may take a while!'
+        print getoutput('rsync -av --partial --progress %s %s' % (
+            source, target))
+        print 'Done.'
+        print line
+
+    def install(self):
+        """Installer"""
+        # XXX Implement recipe functionality here
+
+        # Return files that were created by the recipe. The buildout
+        # will remove all returned files upon reinstall.
+        return tuple()
+
+    def update(self):
+        """Updater"""
+        pass
---
+++ packages/collective.recipe.rsync/collective.recipe.rsync-1.3.zip/collective.recipe.rsync-1.3/collective/recipe/rsync/tests.py
@@ -0,0 +1,10 @@
+
+import unittest
+
+class FooTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testFoo(self):
+        pass
