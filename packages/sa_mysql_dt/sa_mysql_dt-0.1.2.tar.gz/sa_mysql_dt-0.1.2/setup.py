7a4f4787a95bbe56c7c21772f22d730e6162e885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:21 +0000

    Add package sa_mysql_dt/0.1.2/sa_mysql_dt-0.1.2.tar.gz

---
+++ packages/sa_mysql_dt/sa_mysql_dt-0.1.2.tar.gz/sa_mysql_dt-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os, sys
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
+setup(name='sa_mysql_dt',
+      version='0.1.2',
+      description="Alternative implementation of DateTime column for MySQL.",
+      long_description=long_description,
+      classifiers=[], 
+      keywords="sqlalchemy MySQL",
+      author="Martijn Faassen",
+      author_email="faassen@startifact.com",
+      url="",
+      license="",
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'sqlalchemy',
+        'MySQL_python',
+        ],
+      )
