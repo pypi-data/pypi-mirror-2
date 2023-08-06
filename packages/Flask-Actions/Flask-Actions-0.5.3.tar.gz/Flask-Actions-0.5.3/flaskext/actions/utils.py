f08afaa044bbbf25e15db0deb0b42c8fa5f3ec98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:02 +0000

    Add package Flask-Actions/0.5.3/Flask-Actions-0.5.3.tar.gz

---
+++ packages/Flask-Actions/Flask-Actions-0.5.3.tar.gz/Flask-Actions-0.5.3/flaskext/actions/utils.py
@@ -0,0 +1,8 @@
+# -*- encoding:utf-8 -*-
+import string
+import random
+KEY_CHARS = string.letters + string.digits + string.punctuation
+
+def generate_secret_key(length=32):
+    return  repr(''.join(random.choice(KEY_CHARS) for _ in xrange(length)))
+
