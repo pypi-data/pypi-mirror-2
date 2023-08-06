dc1ef51ea227df0b03ae6efd2fe703839c54b168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:42 +0000

    Add package webcouturier.dropdownmenu/2.2/webcouturier.dropdownmenu-2.2.zip

---
+++ packages/webcouturier.dropdownmenu/webcouturier.dropdownmenu-2.2.zip/webcouturier.dropdownmenu-2.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+version = "2.2"
+
+setup(name='webcouturier.dropdownmenu',
+      version=version,
+      description="Dropdown menus for global navigation in Plone",
+      long_description=open("README.txt").read()+ '\n' +
+                       open("docs/INSTALL.txt").read()+ '\n' +
+                       open('CHANGES.txt').read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web couturier dropdown menu navigation',
+      author='Denys Mishunov',
+      author_email='denys.mishunov@gmail.com',
+      url='http://plone.org/products/webcouturier-dropdownmenu',
+      license='GPL',
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['webcouturier'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.testing',
+          'unittest2',
+          'plone.browserlayer',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
