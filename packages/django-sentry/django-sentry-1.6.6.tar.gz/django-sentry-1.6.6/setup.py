147e60717cf9db7a0b7151beda70f5b0491612d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:22 +0000

    Add package django-sentry/1.6.6/django-sentry-1.6.6.tar.gz

---
+++ packages/django-sentry/django-sentry-1.6.6.tar.gz/django-sentry-1.6.6/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+
+
+class mytest(test):
+    def run(self, *args, **kwargs):
+        from runtests import runtests
+        runtests()
+        # Upgrade().run(dist=True)
+        # test.run(self, *args, **kwargs)
+
+
+setup(
+    name='django-sentry',
+    version='1.6.6',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://github.com/dcramer/django-sentry',
+    description = 'Exception Logging to a Database in Django',
+    packages=find_packages(exclude="example_project"),
+    zip_safe=False,
+    install_requires=[
+        'django',
+        'django-paging>=0.2.2',
+        'django-indexer==0.2.1',
+        'uuid',
+    ],
+    test_suite = 'sentry.tests',
+    include_package_data=True,
+    cmdclass={"test": mytest},
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
