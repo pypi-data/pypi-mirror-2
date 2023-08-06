03a8a3d605286b6b2bf1f1d382cb601ad3963ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:41 +0000

    Add package zopyx.tinymceplugins.tinyautosave/1.0/zopyx.tinymceplugins.tinyautosave-1.0.tar.gz

---
+++ packages/zopyx.tinymceplugins.tinyautosave/zopyx.tinymceplugins.tinyautosave-1.0.tar.gz/zopyx.tinymceplugins.tinyautosave-1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='zopyx.tinymceplugins.tinyautosave',
+      version=version,
+      description="TinyMCE auto-save plugin for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='TinyMCE Plone',
+      maintainer='Andreas Jung',
+      maintainer_email='info@zopyx.com',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='http://pypi.python.org/pypi/zopyx.tinymceplugins.tinyautosave',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zopyx', 'zopyx.tinymceplugins'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
