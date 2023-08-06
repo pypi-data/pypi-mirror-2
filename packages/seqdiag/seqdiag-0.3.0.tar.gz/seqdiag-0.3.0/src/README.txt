79c4bd7c5e506f71ae78654f305dfe80418d0607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:25 +0000

    Add package seqdiag/0.3.0/seqdiag-0.3.0.tar.gz

---
+++ packages/seqdiag/seqdiag-0.3.0.tar.gz/seqdiag-0.3.0/src/README.txt
@@ -0,0 +1,163 @@
+`seqdiag` generate sequence-diagram image file from spec-text file.
+
+Features
+========
+
+* Generate sequence-diagram from dot like text (basic feature).
+* Multilingualization for node-label (utf-8 only).
+
+You can get some examples and generated images on 
+`tk0miya.bitbucket.org <http://tk0miya.bitbucket.org/seqdiag/build/html/index.html>`_ .
+
+Setup
+=====
+
+by easy_install
+----------------
+Make environment::
+
+   $ easy_install seqdiag
+
+by buildout
+------------
+Make environment::
+
+   $ hg clone http://bitbucket.org/tk0miya/seqdiag
+   $ cd seqdiag
+   $ python bootstrap.py
+   $ bin/buildout
+
+Copy and modify ini file. example::
+
+   $ cp <seqdiag installed path>/blockdiag/examples/simple.diag .
+   $ vi simple.diag
+
+Please refer to `spec-text setting sample`_ section for the format of the
+`simpla.diag` configuration file.
+
+spec-text setting sample
+========================
+
+Few examples are available.
+You can get more examples at
+`tk0miya.bitbucket.org <http://tk0miya.bitbucket.org/seqdiag/build/html/index.html>`_ .
+
+simple.diag
+------------
+
+simple.diag is simply define nodes and transitions by dot-like text format::
+
+    diagram {
+      browser  -> webserver [label = "GET /index.html"];
+      browser <-- webserver;
+      browser  -> webserver [label = "POST /blog/comment"];
+                  webserver  -> database [label = "INSERT comment"];
+                  webserver <-- database;
+      browser <-- webserver;
+    }
+
+
+Usage
+=====
+
+Execute seqdiag command::
+
+   $ seqdiag simple.diag
+   $ ls simple.png
+   simple.png
+
+
+Requirements
+============
+
+* Python 2.4 or later (not support 3.x)
+* Python Imaging Library 1.1.5 or later.
+* funcparserlib 0.3.4 or later.
+* setuptools or distriubte.
+
+
+License
+=======
+Python Software Foundation License.
+
+
+History
+=======
+
+0.3.0 (2011-04-22)
+------------------
+* Add group syntax
+
+0.2.7 (2011-04-15)
+------------------
+* Adjust start coordinates of edges
+
+0.2.6 (2011-04-14)
+------------------
+* Fix bugs
+* Allow unquoted utf8 characters
+
+0.2.5 (2011-03-26)
+------------------
+* Fix seqdiag could not run under blockdiag 0.7.6
+
+0.2.4 (2011-03-20)
+------------------
+* Fix bugs
+
+0.2.3 (2011-03-09)
+------------------
+* Fix bugs
+
+0.2.2 (2011-03-07)
+------------------
+* Fix could not run under python 2.4
+* Support edge colors
+
+0.2.1 (2011-02-28)
+------------------
+* Add default_shape attribute to diagram
+
+0.2.0 (2011-02-27)
+------------------
+* Add metrix parameters for edge label: edge_height, edge_length
+* Fix bugs
+
+0.1.7 (2011-01-21)
+------------------
+* Fix TeX exporting in Sphinx extension
+
+0.1.6 (2011-01-15)
+------------------
+* Support blockdiag-0.6.3
+* Fix bugs
+
+0.1.5 (2011-01-15)
+------------------
+* Draw activity on lifelines
+* Support both direction edge with '=>' operator
+
+0.1.4 (2011-01-13)
+------------------
+* Change synxtax around edges
+
+0.1.3 (2011-01-12)
+------------------
+* Support diagonal edge
+* Fix bugs
+
+0.1.2 (2011-01-11)
+------------------
+* Support nested edges
+* Add edge attributes; return, dir
+* Add sphinx extention module(sphinxcontrib_seqdiag)
+* Fix bugs
+
+0.1.1 (2011-01-11)
+------------------
+* Fix bugs about layouting
+
+0.1.0 (2011-01-08)
+------------------
+* first release
+
---
+++ packages/seqdiag/seqdiag-0.3.0.tar.gz/seqdiag-0.3.0/src/seqdiag/command.py
@@ -0,0 +1,107 @@
+#!bin/py
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+from ConfigParser import SafeConfigParser
+from optparse import OptionParser
+from DiagramDraw import DiagramDraw
+import diagparser
+from builder import ScreenNodeBuilder
+
+
+def parse_option():
+    usage = "usage: %prog [options] infile"
+    p = OptionParser(usage=usage)
+    p.add_option('-a', '--antialias', action='store_true',
+                 help='Pass diagram image to anti-alias filter')
+    p.add_option('-c', '--config',
+                 help='read configurations from FILE', metavar='FILE')
+    p.add_option('-o', dest='filename',
+                 help='write diagram to FILE', metavar='FILE')
+    p.add_option('-f', '--font', default=[], action='append',
+                 help='use FONT to draw diagram', metavar='FONT')
+    p.add_option('-P', '--pdb', dest='pdb', action='store_true', default=False,
+                 help='Drop into debugger on exception')
+    p.add_option('-T', dest='type', default='PNG',
+                 help='Output diagram as TYPE format')
+    options, args = p.parse_args()
+
+    if len(args) == 0:
+        p.print_help()
+        sys.exit(0)
+
+    options.type = options.type.upper()
+    if not options.type in ('SVG', 'PNG', 'PDF'):
+        msg = "ERROR: unknown format: %s\n" % options.type
+        sys.stderr.write(msg)
+        sys.exit(0)
+
+    if options.type == 'PDF':
+        try:
+            import reportlab.pdfgen.canvas
+        except ImportError:
+            msg = "ERROR: colud not output PDF format; Install reportlab\n"
+            sys.stderr.write(msg)
+            sys.exit(0)
+
+    if options.config and not os.path.isfile(options.config):
+        msg = "ERROR: config file is not found: %s\n" % options.config
+        sys.stderr.write(msg)
+        sys.exit(0)
+
+    configpath = options.config or "%s/.seqdiagrc" % os.environ.get('HOME')
+    if os.path.isfile(configpath):
+        config = SafeConfigParser()
+        config.read(configpath)
+
+        if config.has_option('seqdiag', 'fontpath'):
+            fontpath = config.get('seqdiag', 'fontpath')
+            options.font.append(fontpath)
+
+    return options, args
+
+
+def detectfont(options):
+    fonts = options.font + \
+            ['c:/windows/fonts/VL-Gothic-Regular.ttf',  # for Windows
+             'c:/windows/fonts/msmincho.ttf',  # for Windows
+             '/usr/share/fonts/truetype/ipafont/ipagp.ttf',  # for Debian
+             '/usr/local/share/font-ipa/ipagp.otf',  # for FreeBSD
+             '/System/Library/Fonts/AppleGothic.ttf']  # for MaxOS
+
+    fontpath = None
+    for path in fonts:
+        if path and os.path.isfile(path):
+            fontpath = path
+            break
+
+    return fontpath
+
+
+def main():
+    options, args = parse_option()
+
+    infile = args[0]
+    if options.filename:
+        outfile = options.filename
+    else:
+        outfile = re.sub('\..*', '', infile) + '.' + options.type.lower()
+
+    if options.pdb:
+        sys.excepthook = utils.postmortem
+
+    fontpath = detectfont(options)
+
+    tree = diagparser.parse_file(infile)
+    diagram = ScreenNodeBuilder.build(tree)
+
+    draw = DiagramDraw(options.type, diagram, outfile, font=fontpath,
+                       antialias=options.antialias)
+    draw.draw()
+    draw.save()
+
+
+if __name__ == '__main__':
+    main()
