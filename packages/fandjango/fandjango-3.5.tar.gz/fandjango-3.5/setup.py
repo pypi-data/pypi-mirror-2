407398e4dca6a4eb661514bbec7a02c175d7534c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:12 +0000

    Add package fandjango/3.5/fandjango-3.5.tar.gz

---
+++ packages/fandjango/fandjango-3.5.tar.gz/fandjango-3.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'fandjango',
+  version = '3.5',
+  description = "Fandjango makes it easy to create Facebook applications powered by Django",
+  author = "Johannes Gorset",
+  author_email = "jgorset@gmail.com",
+  url = "http://github.com/jgorset/fandjango",
+  packages = ['fandjango', 'fandjango.migrations']
+)
