b780414b9b8b7357af09c7ca64e1a238593539b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:30 +0000

    Add package plone.cachepurging/1.0b2/plone.cachepurging-1.0b2.zip

---
+++ packages/plone.cachepurging/plone.cachepurging-1.0b2.zip/plone.cachepurging-1.0b2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b2'
+
+tests_require = ['unittest2',
+                 'plone.app.testing',
+                 ]
+
+setup(name='plone.cachepurging',
+      version=version,
+      description="Cache purging support for Zope 2 applications",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone cache purge',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.cachepurging',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+          'zope.component',
+          'zope.event',
+          'zope.annotation',
+          'zope.lifecycleevent',
+          'zope.i18nmessageid',
+          'five.globalrequest',
+          'plone.registry',
+          'Zope2',
+      ],
+      tests_require=tests_require,
+      extras_require={
+        'test': tests_require,
+      },
+      )
