e1fc01d3dc74f1244c232e4fd19e125d3efffa5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:28 +0000

    Add package django-cachebot/0.2.1/django-cachebot-0.2.1.tar.gz

---
+++ packages/django-cachebot/django-cachebot-0.2.1.tar.gz/django-cachebot-0.2.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import cachebot
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README.rst')
+
+
+setup(
+    name = "django-cachebot",
+    version = cachebot.__version__,
+    description = 'Automated caching and invalidation for the Django ORM',
+    long_description = README,
+    url = 'http://github.com/dziegler/django-cachebot',
+    author = 'David Ziegler',
+    author_email = 'david.ziegler@gmail.com',
+    license = 'BSD',
+    zip_safe = False,
+    packages = find_packages(),
+    install_requires = [
+        'django>=1.2',
+    ],
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
