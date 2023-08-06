800e41b25327baee145bdd21e1829722b89a46d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:07 +0000

    Add package ClusterShell/1.5.1/clustershell-1.5.1.tar.gz

---
+++ packages/ClusterShell/clustershell-1.5.1.tar.gz/clustershell-1.5.1/scripts/clush.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# $Id: clush.py 373 2010-09-26 23:20:10Z st-cea $
+
+"""
+clush command-line tool
+"""
+
+from ClusterShell.CLI.Clush import main
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ClusterShell/clustershell-1.5.1.tar.gz/clustershell-1.5.1/scripts/nodeset.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# $Id: nodeset.py 373 2010-09-26 23:20:10Z st-cea $
+
+"""
+nodeset command-line tool
+"""
+
+from ClusterShell.CLI.Nodeset import main
+
+if __name__ == '__main__':
+    main()
