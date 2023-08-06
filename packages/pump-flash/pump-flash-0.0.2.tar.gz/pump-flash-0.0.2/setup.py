6a0acb23d2416238b0089c76d3f0b4b0a218aad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:28 +0000

    Add package pump-flash/0.0.2/pump-flash-0.0.2.tar.gz

---
+++ packages/pump-flash/pump-flash-0.0.2.tar.gz/pump-flash-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="pump-flash",
+  version="0.0.2",
+  description="A Pump middleware for adding flash message support via sessions.",
+  author="Adeel Ahmad Khan",
+  author_email="adeel@adeel.ru",
+  py_modules=["pump_flash"],
+  license="MIT")
