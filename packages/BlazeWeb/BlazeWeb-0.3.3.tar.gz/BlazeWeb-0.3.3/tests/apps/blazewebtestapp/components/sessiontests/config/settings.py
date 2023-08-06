fee75aff2fc71ea18b331c60429d9b251723033d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:55 +0000

    Add package BlazeWeb/0.3.3/BlazeWeb-0.3.3.tar.gz

---
+++ packages/BlazeWeb/BlazeWeb-0.3.3.tar.gz/BlazeWeb-0.3.3/tests/apps/blazewebtestapp/components/sessiontests/config/settings.py
@@ -0,0 +1,8 @@
+from blazeweb.config import ComponentSettings
+
+class Settings(ComponentSettings):
+
+    def init(self):
+
+        self.add_route('/sessiontests/setfoo', 'sessiontests:SetFoo')
+        self.add_route('/sessiontests/getfoo', 'sessiontests:GetFoo')
