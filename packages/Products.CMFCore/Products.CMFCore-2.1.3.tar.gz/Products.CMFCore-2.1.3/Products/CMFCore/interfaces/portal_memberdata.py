f96ed0de67b8867b797cf35288e26f748e9c5915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:56 +0000

    Add package Products.CMFCore/2.1.3/Products.CMFCore-2.1.3.tar.gz

---
+++ packages/Products.CMFCore/Products.CMFCore-2.1.3.tar.gz/Products.CMFCore-2.1.3/Products/CMFCore/interfaces/portal_memberdata.py
@@ -0,0 +1,21 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Foundation and Contributors.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" Memberdata storage tool interface.
+
+$Id: portal_memberdata.py 110655 2010-04-08 15:38:57Z tseaver $
+"""
+
+# BBB: module will be removed in CMF 2.2
+#      zope2 interfaces created on runtime:
+#      - portal_memberdata
+#      - MemberData
