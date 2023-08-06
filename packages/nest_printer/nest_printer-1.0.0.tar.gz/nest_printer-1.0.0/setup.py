187f5777477eee243eb5d5a9052576a3b85ac86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:36 +0000

    Add package nest_printer/1.0.0/nest_printer-1.0.0.tar.gz

---
+++ packages/nest_printer/nest_printer-1.0.0.tar.gz/nest_printer-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+	name = 'nest_printer',
+	version = '1.0.0',
+	py_modules = ['nest_printer'],
+	author = 'prashu',
+	description = 'print list of lists'
+	)
+
