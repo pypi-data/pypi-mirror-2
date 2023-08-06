080612b829816e461a4f1a37583b6c65582c097a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:13 +0000

    Add package firebirdsql/0.3.5/firebirdsql-0.3.5.tar.gz

---
+++ packages/firebirdsql/firebirdsql-0.3.5.tar.gz/firebirdsql-0.3.5/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import firebirdsql
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Topic :: Database',
+    'License :: OSI Approved :: BSD License',
+]
+
+setup(name='firebirdsql', 
+        version=firebirdsql.__version__,
+        description = 'python dbapi for firebird rdbms.', 
+        url='http://github.com/nakagami/pyfirebirdsql/',
+        classifiers=classifiers,
+        keywords=['Firebird'],
+        license='BSD',
+        author='Hajime Nakagami',
+        author_email='nakagami@gmail.com',
+        packages = ['firebirdsql'],
+)
+
