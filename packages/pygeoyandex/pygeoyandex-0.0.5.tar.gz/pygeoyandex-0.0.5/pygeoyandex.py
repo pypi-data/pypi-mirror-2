24c19b001e655783eeb7ad6e4fcc3afb004e157d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:50 +0000

    Add package pygeoyandex/0.0.5/pygeoyandex-0.0.5.tar.gz

---
+++ packages/pygeoyandex/pygeoyandex-0.0.5.tar.gz/pygeoyandex-0.0.5/pygeoyandex.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+import urllib
+import json
+from collections import namedtuple
+
+YANDEX_GEO_URL = 'http://geocode-maps.yandex.ru/1.x/'
+
+class GeoError(Exception):
+    pass
+
+def force_str(s, encoding='utf-8'):
+    if isinstance(s, unicode):
+        return s.encode(encoding)
+    return str(s)
+
+class Country(object):
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __repr__(self):
+        return '<Country: %s>' % str(self)
+
+
+Position = namedtuple('Position', ('lat', 'lon'))
+
+
+class GeoResult(object):
+    def __init__(self, country, locality, pos, area=None, subarea=None):
+        self.country = country
+        self.locality = locality
+        self.pos = pos or ()
+        self.area = area
+        self.subarea = subarea
+
+    def __unicode__(self):
+        pieces = map(unicode, filter(None, [self.country, self.area, self.subarea, self.locality]))
+        return u', '.join(pieces) + (' (%f, %f)' % self.pos)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __repr__(self):
+        return '<GeoResult: %s>' % str(self)
+
+
+class Geocoder(object):
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def geocode(self, geoquery, results=1, plng='ru', kind='locality'):
+        query = {
+            'results': results,
+            'format': 'json',
+            'plng': plng,
+            'kind': kind,
+            'key': self.api_key,
+        }
+
+        if isinstance(geoquery, (tuple, list)):
+            query['geocode'] = ','.join(map(force_str, reversed(geoquery[:2])))
+        else:
+            query['geocode'] = force_str(geoquery)
+
+        url = YANDEX_GEO_URL + ('?%s' % urllib.urlencode(query))
+        res = json.load(urllib.urlopen(url))
+        if u'response' in res:
+            return create_result(res)
+        else:
+            raise GeoError(res[u'error'][u'message'])
+
+
+def create_result(res):
+    r = res[u'response'][u'GeoObjectCollection']
+    result_list = []
+
+    for geoobj in (o[u'GeoObject'] for o in r[u'featureMember']):
+        try:
+            pos = Position(*reversed(map(float, geoobj[u'Point'][u'pos'].split())))
+            address_dict = geoobj[u'metaDataProperty'][u'GeocoderMetaData'][u'AddressDetails']
+            country = Country(address_dict[u'Country'][u'CountryName'],
+                              address_dict[u'Country'][u'CountryNameCode'])
+
+            if u'AdministrativeArea' in address_dict[u'Country']:
+                area_dict = address_dict[u'Country'][u'AdministrativeArea']
+                area = area_dict[u'AdministrativeAreaName']
+                if u'SubAdministrativeArea' in area_dict:
+                    subarea = area_dict[u'SubAdministrativeArea'][u'SubAdministrativeAreaName']
+                    locality = area_dict[u'SubAdministrativeArea'][u'Locality'][u'LocalityName']
+                else:
+                    subarea = None
+                    locality = area_dict[u'Locality'][u'LocalityName']
+            else:
+                area = None
+                subarea = None
+                locality = address_dict[u'Country'][u'Locality'][u'LocalityName']
+
+            area_dict = address_dict[u'Country']
+            result_list.append(GeoResult(country, locality, pos, area=area, subarea=subarea))
+        except KeyError:
+            pass
+    return result_list
+
+
+if __name__ == '__main__':
+    g = Geocoder('API-KEY')
+    res = g.geocode((59.93772, 30.313622)) # lat, lon
+    print res
+
+
