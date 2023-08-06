74fdd4983a4c1f14e24f530c9159815e1ec39e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:27 +0000

    Add package truenight_nester/1.0.0/truenight_nester-1.0.0.tar.gz

---
+++ packages/truenight_nester/truenight_nester-1.0.0.tar.gz/truenight_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name         = 'truenight_nester',
+		version      = '1.0.0',
+		py_modules   = ['truenight_nester'],
+		author       = 'truenight',
+		author_email = 'hfpython@headfirstlabs.com',
+		url          = 'http://www.headfirstlabs.com',
+		description  = 'A simple printer of nested lists',
+	 )
