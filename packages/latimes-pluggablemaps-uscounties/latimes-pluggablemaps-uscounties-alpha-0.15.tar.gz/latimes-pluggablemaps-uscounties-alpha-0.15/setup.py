ef1813390c31cfada048d44ab0e90aaaf1977223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:00 +0000

    Add package latimes-pluggablemaps-uscounties/alpha-0.15/latimes-pluggablemaps-uscounties-alpha-0.15.tar.gz

---
+++ packages/latimes-pluggablemaps-uscounties/latimes-pluggablemaps-uscounties-alpha-0.15.tar.gz/latimes-pluggablemaps-uscounties-alpha-0.15/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='latimes-pluggablemaps-uscounties',
+      version='alpha-0.15',
+      description='L.A. Times Pluggable Maps: U.S. Counties',
+      author='Ben Welsh',
+      author_email='ben.welsh@gmail.com',
+      url='http://github.com/datadesk/latimes-pluggablemaps-uscounties',
+      download_url='http://github.com/datadesk/latimes-pluggablemaps-uscounties.git',
+      packages=find_packages(),
+      license='MIT',
+      keywords='gis geographical maps earth usa counties boundaries',
+      classifiers=[
+       "Development Status :: 3 - Alpha",
+       "Intended Audience :: Developers",
+       "Intended Audience :: Science/Research",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+       "Programming Language :: Python",
+       "Topic :: Scientific/Engineering :: GIS",
+       "Topic :: Software Development :: Libraries :: Python Modules"
+       ],
+     )
