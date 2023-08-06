2b6dbb18c2b99a12dc11d6f4c0aa8cbe267ab8a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package Flask-Actions/0.5.1/Flask-Actions-0.5.1.tar.gz

---
+++ packages/Flask-Actions/Flask-Actions-0.5.1.tar.gz/Flask-Actions-0.5.1/setup.py
@@ -0,0 +1,48 @@
+"""
+Flask-Actions
+-------------
+
+custom actions for flask
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-Actions>`_
+* `development version
+  <http://bitbucket.org/youngking/flask-actions/get/tip.gz#egg=Flask-Actions-dev>`_
+
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Actions',
+    version='0.5.1',
+    url='http://blog.flyzen.com',
+    license='BSD',
+    author='Young King',
+    author_email='yanckin@gmail.com',
+    description='custom actions for flask to help manage your application',
+    long_description=__doc__,
+    packages=['flaskext','flaskext.actions'],
+    namespace_packages=['flaskext'],
+    test_suite='nose.collector',
+    tests_require=['Nose'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Werkzeug'
+    ],
+    scripts=['flaskext/actions/flask_admin.py'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
