8533011c920abb70e1a1d5a8014c9e61e6cf116d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:48 +0000

    Add package redsolutioncms.template-classic/0.1.0/redsolutioncms.template-classic-0.1.0.tar.gz

---
+++ packages/redsolutioncms.template-classic/redsolutioncms.template-classic-0.1.0.tar.gz/redsolutioncms.template-classic-0.1.0/classic/make.py
@@ -0,0 +1,31 @@
+from redsolutioncms.make import BaseMake
+from redsolutioncms.models import CMSSettings
+from classic.models import TemplateSettings
+import os
+
+
+class Make(BaseMake):
+    def premake(self):
+        super(Make, self).premake()
+        cms_settings = CMSSettings.objects.get_settings()
+        template_settings = TemplateSettings.objects.get_settings()
+        cms_settings.render_to(['..', 'templates', 'base_template.html'], 'classic/templates/base_template.html', {
+            'template_settings': template_settings,
+        }, 'w')
+
+    def make(self):
+        super(Make, self).make()
+        cms_settings = CMSSettings.objects.get_settings()
+
+        cms_settings.copy_dir(
+            os.path.join(cms_settings.project_dir, 'media',),
+            os.path.join(os.path.dirname(__file__), 'templates', 'classic', 'media'),
+            merge=True
+        )
+        cms_settings.copy_dir(
+            os.path.join(cms_settings.project_dir, 'templates',),
+            os.path.join(os.path.dirname(__file__), 'templates', 'classic', 'templates'),
+            merge=True
+        )
+
+make = Make()
