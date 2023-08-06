061b200fb4e56a122cf7829fc4155d29e9e564a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:01 +0000

    Add package python-creole/0.7.0.pre/python-creole-0.7.0.pre.tar.gz

---
+++ packages/python-creole/python-creole-0.7.0.pre.tar.gz/python-creole-0.7.0.pre/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+    distutils setup
+    ~~~~~~~~~~~~~~~
+
+    :copyleft: 2009-2011 by the python-creole team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+from creole import VERSION_STRING, creole2html, html2rest
+
+
+PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_authors():
+    try:
+        f = file(os.path.join(PACKAGE_ROOT, "AUTHORS"), "r")
+        authors = [l.strip(" *\r\n") for l in f if l.strip().startswith("*")]
+        f.close()
+    except Exception, err:
+        authors = "[Error: %s]" % err
+    return authors
+
+
+def get_long_description():
+    try:
+        f = file(os.path.join(PACKAGE_ROOT, "README.creole"), "r")
+        desc_creole = f.read()
+        f.close()
+
+        desc_creole = unicode(desc_creole, 'utf-8').strip()
+
+        desc_html = creole2html(desc_creole)
+        long_description = html2rest(desc_html)
+    except Exception, err:
+        long_description = "[Error: %s]" % err
+        if len(sys.argv) > 1:
+            print sys.argv
+            if sys.argv[1] in ("--long-description", "sdist"):
+                raise
+
+    return long_description
+
+
+setup(
+    name='python-creole',
+    version=VERSION_STRING,
+    description='python-creole is an open-source creole2html and html2creole converter in pure Python.',
+    long_description=get_long_description(),
+    author=get_authors(),
+    maintainer="Jens Diemer",
+    url='http://code.google.com/p/python-creole/',
+    packages=find_packages(),
+    include_package_data=True, # include package data under svn source control
+    zip_safe=True,
+    classifiers=[
+#        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Operating System :: OS Independent",
+    ],
+    test_suite="tests.run_all_tests",
+)
