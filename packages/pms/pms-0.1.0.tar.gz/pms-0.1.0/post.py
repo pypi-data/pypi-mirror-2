627e667722afffbaa4a65f53f2b1dfb71bdb5eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:55 +0000

    Add package pms/0.1.0/pms-0.1.0.tar.gz

---
+++ packages/pms/pms-0.1.0.tar.gz/pms-0.1.0/post.py
@@ -0,0 +1,8 @@
+from jsonrequester import JsonRequester
+
+requester = JsonRequester('http://localhost:5000')
+
+print requester.post('/record', {
+    'type': 'test',
+    'priority': 'critical',
+})
