964e83b9e87f10a90dbac98a3d7d4e1013a821dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:55 +0000

    Add package django-guts/0.1.3/django-guts-0.1.3.tar.gz

---
+++ packages/django-guts/django-guts-0.1.3.tar.gz/django-guts-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-guts',
+    version = '0.1.3',
+    description = '''Funny application to show sources of your Django project.''',
+    keywords = 'django apps',
+    license = 'New BSD License',
+    author = 'Alexander Artemenko',
+    author_email = 'svetlyak.40wt@gmail.com',
+    url = 'http://github.com/svetlyak40wt/django-guts/',
+    install_requires = [],
+    dependency_links = [],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+)
+
