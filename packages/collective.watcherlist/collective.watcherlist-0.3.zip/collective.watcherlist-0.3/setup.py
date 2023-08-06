eaaccf23f623d9bc6fea5861f26e8e3e4056e583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:08 +0000

    Add package collective.watcherlist/0.3/collective.watcherlist-0.3.zip

---
+++ packages/collective.watcherlist/collective.watcherlist-0.3.zip/collective.watcherlist-0.3/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+readme = open(os.path.join('collective', 'watcherlist', 'README.txt')).read()
+
+version = '0.3'
+
+setup(name='collective.watcherlist',
+      version=version,
+      description="Send emails from Plone to interested members (watchers)",
+      long_description=readme + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Maurits van Rees',
+      author_email='maurits@vanrees.org',
+      url='http://plone.org/products/collective.watcherlist',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
