77903ad5c5c012d070c79b4fd6c94350c876ae6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:42 +0000

    Add package django-impersonate/0.4/django-impersonate-0.4.tar.gz

---
+++ packages/django-impersonate/django-impersonate-0.4.tar.gz/django-impersonate-0.4/impersonate/helpers.py
@@ -0,0 +1,59 @@
+import re
+from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage
+
+ 
+def get_redir_path():        
+    return getattr(
+        settings,
+        'IMPERSONATE_REDIRECT_URL',
+        getattr(settings, 'LOGIN_REDIRECT_URL', '/'),
+    )       
+        
+    
+def get_paginator(request, qs):
+    try:
+        page_number = int(request.GET.get('page', 1))
+    except ValueError:
+        page_number = 1
+
+    paginator = Paginator(
+        qs,
+        int(getattr(settings, 'IMPERSONATE_PAGINATE_COUNT', 20)),
+    )   
+    try:
+        page = paginator.page(page_number)
+    except EmptyPage:
+        page = None
+
+    return (paginator, page, page_number)
+
+
+def check_allow_staff():
+    return (not getattr(settings, 'IMPERSONATE_REQUIRE_SUPERUSER', False))
+
+
+def check_allow_for_user(start_user, end_user):
+    if not start_user.is_superuser:
+        allow_staff = check_allow_staff()
+        if start_user.is_staff and allow_staff:
+            if not end_user.is_superuser:
+                return True
+    else:
+        return True
+
+    return False
+
+
+def check_allow_for_uri(uri):
+    uri = uri.lstrip('/')
+
+    exclusions = getattr(settings, 'IMPERSONATE_URI_EXCLUSIONS', (r'^admin/',))
+    if not isinstance(exclusions, (list, tuple)):
+        exclusions = (exclusions,)
+
+    for exclusion in exclusions:
+        if re.search(exclusion, uri):
+            return False
+
+    return True
