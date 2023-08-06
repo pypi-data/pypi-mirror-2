a61940400deff31a8c85e8a90a0918a42a4f1f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:01 +0000

    Add package message/0.0.2/message-0.0.2.zip

---
+++ packages/message/message-0.0.2.zip/message-0.0.2/message/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__all__ = [
+		'__version__',
+		'__author__',
+		'sub',
+		'unsub',
+		'pub',
+]
+
+from message import sub, unsub, pub
+
+__version__ = '0.0.2'
+__author__ = 'LaiYonghao'
