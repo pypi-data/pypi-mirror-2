32218eb9aa4708701dafaa11d417b9d5dc9fb681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:11 +0000

    Add package telephonie/0.3.1/telephonie-0.3.1.tar.gz

---
+++ packages/telephonie/telephonie-0.3.1.tar.gz/telephonie-0.3.1/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+try:
+    from setuptools import find_packages
+except:
+    from distutils.core import find_packages
+
+version = open('VERSION.txt').read().strip()
+author = "Telephonie Team"
+author_email = "telephonie@miglu.com"
+maintainer = "Telephonie Team"
+maintainer_email = "telephonie@miglu.com"
+licence = "MPL 1.1"
+
+setup_args = {
+      'name':'telephonie',
+      'version':version,
+      'description':'Telephonie framework',
+      'url':'http://github.com/miglu/Telephonie',
+      'author':author,
+      'author_email':author_email,
+      'maintainer':maintainer,
+      'maintainer_email':maintainer_email,
+      'platforms':['linux'],
+      'long_description':'Framework to create telephony applications using FreeSWITCH',
+      'packages':find_packages('src'),
+      'package_dir':{'': 'src'},
+      'include_package_data':True,
+      'scripts':['src/scripts/telephonied'],
+      'keywords':"telecom voip telephony freeswitch ivr",
+      'license':licence,
+      'zip_safe':False,
+      'classifiers':[
+        "Programming Language :: Python",
+        "Operating System :: POSIX",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Communications",
+        "Topic :: Multimedia",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)",
+        "Development Status :: 4 - Beta"]
+}
+
+
+try:
+    from setuptools import setup
+    setup_args['install_requires'] = ['gevent']
+except ImportError:
+    from distutils.core import setup
+    setup_args['requires'] = ['gevent']
+  
+setup(**setup_args)
---
+++ packages/telephonie/telephonie-0.3.1.tar.gz/telephonie-0.3.1/src/telephonie/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Telephonie  - Framework to create telephony applications using FreeSWITCH 
+
+Primary Authors - Michael Ricordeau (tamiel) and Miglu (miglu)
+
+Telephonie is inspired by and uses code from - https://github.com/fiorix/eventsocket
+"""
+
+__name__ = "telephonie"
+
+__all__ = ['core', 'utils']
+
