6540b57ca9779105f453b0d9d9c6e80f97e80fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:36 +0000

    Add package tw.uitheme/0.2.2/tw.uitheme-0.2.2.tar.gz

---
+++ packages/tw.uitheme/tw.uitheme-0.2.2.tar.gz/tw.uitheme-0.2.2/tw/uitheme/release.py
@@ -0,0 +1,11 @@
+# Metadata for the widget EGG. Should leave it for the (unimplemented) widget
+# tracker to track it
+
+__DISTRIBUTION__ = 'tw.uitheme'
+__DESCRIPTION__ = 'theming for jquery plugins packaged as tosca widgets ressources, based on 1.8.9'
+__URL__ = 'http://jqueryui.com/themeroller/'
+__VERSION__ = '0.2.2'
+__AUTHOR__ = 'Nicolas Laurance'
+__EMAIL__ = 'nicolas[dot]laurance[at]gmail[dot]com'
+__COPYRIGHT__ = "Copyright 2010 Nicolas Laurance"
+__LICENSE__ = 'GPL'
