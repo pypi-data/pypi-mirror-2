dd5b18451c8992e35c06712c8bec653e1ef0fe44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:39 +0000

    Add package parse2plone/0.9.3/parse2plone-0.9.3.zip

---
+++ packages/parse2plone/parse2plone-0.9.3.zip/parse2plone-0.9.3/typeswap.py
@@ -0,0 +1,12 @@
+
+###############################################################################
+#                                                                             #
+# Adds "typeswap" feature to ``parse2plone``.                                 #
+#                                                                             #
+# This feature allows the user to specify customize content types for use     #
+# when importing content by specifying a "default" content type followed by   #
+# its replacement "custom" content type (e.g.                                 #
+# --typeswap=Document:MyCustomPageType).                                      #
+#                                                                             #
+###############################################################################
+
