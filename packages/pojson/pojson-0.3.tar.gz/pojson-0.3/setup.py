bd9d36eca485bfe7f0c5d9728956ec16ddf49fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:43 +0000

    Add package pojson/0.3/pojson-0.3.tar.gz

---
+++ packages/pojson/pojson-0.3.tar.gz/pojson-0.3/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='pojson',
+    version='0.3',
+    description="Convert PO files to JSON",
+    long_description=long_description,
+    keywords='PO i18n internationalisation JSON gettext',
+    author='Martijn Faassen',
+    author_email='faassen@startifact.com',
+    license='BSD',
+    url='https://bitbucket.org/faassen/pojson',
+    packages=['pojson'],
+    include_package_data = True,
+    zip_safe=False,
+    setup_requires=['hgtools'],
+    install_requires=[
+        'argh',
+        'polib',
+        'simplejson',
+        'argparse',
+        'pytest >= 2.0', # XXX test require
+    ],
+    entry_points= {
+        'console_scripts': [
+            'pojson = pojson.main:main',
+        ]
+        },
+    
+    )
