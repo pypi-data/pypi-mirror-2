13403c4a46659efdafe176bc8b2b44308e356aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:57 +0000

    Add package NoseKay/0.1.1/NoseKay-0.1.1.tar.gz

---
+++ packages/NoseKay/NoseKay-0.1.1.tar.gz/NoseKay-0.1.1/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+VERSION = "0.1.1"
+DESCRIPTION = "nose plugin for Kay framework testing"
+README = os.path.join(os.path.dirname(__file__), "README.rst")
+LONG_DESCRIPTION = open(README).read() + "\n\n"
+
+setup(
+    name = "NoseKay",
+    version = VERSION,
+    author = "Yuku Takahashi",
+    author_email = "taka84u9@gmail.com",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    license = "BSD",
+    entry_points = {
+        "nose.plugins.0.10": ["nosekay = nosekay:NoseKay"]
+    },
+    keywords="nose kay-framework",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+		"Programming Language :: Python",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Testing",
+    ],
+    py_modules = ["nosekay"],
+    install_requires = ["nose>=0.10.1"],
+)
