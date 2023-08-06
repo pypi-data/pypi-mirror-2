42068832523d01281b0ebcea018ec379bdb2be57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:01 +0000

    Add package qtdjango/1.5.6/qtdjango-1.5.6.tar.gz

---
+++ packages/qtdjango/qtdjango-1.5.6.tar.gz/qtdjango-1.5.6/src/qtdjango/django_qtdjango/handlers.py
@@ -0,0 +1,126 @@
+'''
+@author: darvin
+This module must be imported from django enviroment
+'''
+
+
+from piston.handler import AnonymousBaseHandler, BaseHandler
+from piston.utils import rc
+from piston.utils import validate
+
+from forms import create_form_type
+import qtdjango
+from django.conf import settings
+import inspect
+
+
+class InfoHandler(AnonymousBaseHandler):
+    allowed_methods = ('GET',)
+
+    models = []
+    server_package = None
+
+    @staticmethod
+    def create_model_info(model):
+        fields = {}
+        for field in model._meta.fields:
+            try:
+                f = {}
+                f["type"]=field.__class__.__name__
+                fields[field.name] = f
+            except AttributeError:
+                pass
+
+        info = {"name":model.__name__,
+                "fields":fields,
+                }
+
+        try:
+            info["dump_order"]=model.dump_order
+        except AttributeError:
+            pass
+        return info
+
+    @classmethod
+    def set_models(cls, models):
+        for model in models:
+            cls.models.append(cls.create_model_info(model))
+
+    def read(self, request):
+        #FIXME!
+        project_module = __import__(getattr(settings, "ROOT_URLCONF").split(".")[0])
+        project_version = project_module.__version__
+        d = {
+            "qtdjango_version":qtdjango.__version__,
+            "server_version":project_version,
+            "qtdjango_apps_setting":getattr(settings, "QTDJANGO_APPS" ),
+            "models":self.models
+        }
+        return d
+
+
+
+class MetaHandler(BaseHandler):
+    exclude = ()
+#    fields = ("machinemark",)
+    allowed_methods = ('GET','POST')
+    
+    def has_model(self):
+        return hasattr(self, 'model') or hasattr(self, 'queryset')
+
+    def create(self, request, *args, **kwargs):
+        if not self.has_model():
+            return rc.NOT_IMPLEMENTED
+
+        # get the actual data that wants to be created from the api request
+        attrs = self.flatten_dict(request.POST)
+
+        for field in self.model._meta.fields:
+            if field.rel:
+                # this is a ForeignKey (as far as I can tell... _meta isn't documented)
+                # so we need to convert it to it's pk equivilent... there's most likely
+                # a better way of doing this than hardcoding stuff...
+                if field.name in attrs:
+                    try:
+                        attrs[field.name] =  field.rel.to.objects.get(\
+                            **{field.rel.field_name: attrs[field.name]})
+                    except:
+                        pass
+
+
+        try:
+            inst = self.model.objects.get(**attrs)
+            return rc.DUPLICATE_ENTRY
+        except self.model.DoesNotExist:
+            inst = self.model()
+            inst = self.model(**attrs)
+            inst.save()
+            return inst
+        except self.model.MultipleObjectsReturned:
+            return rc.DUPLICATE_ENTRY
+
+
+def create_handler_type(model):
+    """Builds handler class from model
+    @param model: Model class"""
+    f = []
+    for field in model._meta.fields:
+        f.append(field.name)
+
+    for methodname in dir(model):
+        try:
+            if getattr(model, methodname).method_as_field:
+                f.append(methodname)
+        except AttributeError:
+            pass
+
+    for field in model._meta.many_to_many:
+        f.append(field.name)
+    handler_type = type(model.__name__+"Handler", (MetaHandler,),\
+                        {"model":model, "fields":f})
+    valid_decor = validate(create_form_type(model))
+    handler_type.create = valid_decor(handler_type.create)
+
+
+
+    return handler_type
---
+++ packages/qtdjango/qtdjango-1.5.6.tar.gz/qtdjango-1.5.6/src/qtdjango/widgets.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+
+
+'''
+@author: darvin
+'''
+from qtmodels import ListModel
+from models import *
+from PyQt4.QtGui import *
+from PyQt4 import QtCore, Qt
+from PyQt4.QtCore import QDateTime
+
+
+class Widget(object):
+    def __init__(self, field):
+        self.field = field
+
+        
+    def getData(self):
+        raise NotImplementedError
+    
+    def setData(self, data):
+        raise NotImplementedError
+
+
+class LabelWidget(QLabel, Widget):
+    def __init__(self, field):
+        Widget.__init__(self, field)
+        QLabel.__init__(self)
+        
+    def getData(self):
+        return unicode(self.text())
+    
+    def setData(self, data):
+        if data is not None:
+            self.setText(str(data))
+
+class ForeignKeyWidget(QComboBox, Widget):
+    def __init__(self, field):
+        Widget.__init__(self, field)
+        QComboBox.__init__(self)
+        self.model = self.field.model
+
+
+    def set_filter(self, filter):
+        self.qtmodel.set_filter(filter)
+
+    def getData(self):
+        try:
+            return self.qtmodel.get_model_instance_by_int_index(\
+                self.currentIndex())
+        except IndexError:
+            return None
+
+    def setData(self, data):
+        self.qtmodel = ListModel(parent=self, filter=None,\
+                                model=self.model, blank_variant=self.field.blank)
+        self.setModel(self.qtmodel)
+        if data is not None:
+            self.setCurrentIndex(self.qtmodel.get_index_of_model(data))
+
+
+
+class SpinBoxWidget(QSpinBox, Widget):
+    def __init__(self, field):
+        Widget.__init__(self, field)
+        QLabel.__init__(self)
+        self.setMinimum(0)
+        self.setMaximum(999999)
+
+    def getData(self):
+        return self.value()
+    
+    def setData(self, data):
+        if data is not None:
+            self.setValue = data
+
+class DateTimeEditWidget(QDateTimeEdit, Widget):
+    def __init__(self, field):
+        Widget.__init__(self, field)
+        QDateTimeEdit.__init__(self)
+
+    def getData(self):
+        return self.dateTime().toPyDateTime()
+
+    def setData(self, data):
+        if data is not None:
+            self.setDateTime(data)
+        else:
+            self.setDateTime(QDateTime.currentDateTime())
+
+class CheckBoxWidget(QCheckBox, Widget):
+    def __init__(self, field):
+       Widget.__init__(self, field)
+       QCheckBox.__init__(self)
+
+    def getData(self):
+        return self.isChecked()
+    def setData(self, data):
+        if data is None:
+            data = False
+        self.stateChanged.emit(data)
+
+class ComboBoxWidget(QComboBox, Widget):
+    def __init__(self, field):
+        Widget.__init__(self, field)
+        QComboBox.__init__(self)
+        
+    def getData(self):
+        return self.value
+    
+    def setData(self, data):
+        if data is not None:
+            self.value = data
+
+
+class TextEditWidget(QTextEdit, Widget):
+    def __init__(self, field):
+        Widget.__init__(self, field)
+        QTextEdit.__init__(self)
+        self.setMinimumHeight(30)
+        self.setMaximumHeight(1000)
+
+    def getData(self):
+        return unicode(self.toPlainText())
+    
+    def setData(self, data):
+        if data is not None:
+            self.setPlainText(QtCore.QString(data))
+
+
+class LineEditWidget(QLineEdit, Widget):
+    def __init__(self, field):
+        Widget.__init__(self, field)
+        QLineEdit.__init__(self)
+
+    def getData(self):
+        return unicode(self.text())
+    
+    def setData(self, data):
+        if data is not None:
+            self.setText(QtCore.QString(data))
+
+
+class IdLabelWidget(QLabel, Widget):
+    def __init__(self, field):
+        Widget.__init__(self, field)
+        QLabel.__init__(self)
+
+    def getData(self):
+        res = self.text().toInt()
+        if res[1]:
+            return res[0]
+        else:
+            return None
+    
+    def setData(self, data):
+        if data is not None:
+            self.setText(str(data))
+
+
