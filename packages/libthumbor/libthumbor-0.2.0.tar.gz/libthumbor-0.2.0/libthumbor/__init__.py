1d5a8a2154a56407a14a8119b2d3feca1eccedd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:08 +0000

    Add package libthumbor/0.2.0/libthumbor-0.2.0.tar.gz

---
+++ packages/libthumbor/libthumbor-0.2.0.tar.gz/libthumbor-0.2.0/libthumbor/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# libthumbor - python extension to thumbor
+# https://github.com/heynemann/libthumbor/wiki
+
+# Licensed under the MIT license: 
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
+
+__version__ = "0.2.0"
+
+from libthumbor.core import *
