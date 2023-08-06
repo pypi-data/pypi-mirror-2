46686b808b6991efe6c929765d372a12f45eaf3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:27 +0000

    Add package Flask-SQLAlchemy/0.11/Flask-SQLAlchemy-0.11.tar.gz

---
+++ packages/Flask-SQLAlchemy/Flask-SQLAlchemy-0.11.tar.gz/Flask-SQLAlchemy-0.11/setup.py
@@ -0,0 +1,47 @@
+"""
+Flask-SQLAlchemy
+----------------
+
+Adds SQLAlchemy support to your Flask application.
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-SQLAlchemy>`_
+* `development version
+  <http://github.com/mitsuhiko/flask-sqlalchemy/zipball/master#egg=Flask-SQLAlchemy-dev>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-SQLAlchemy',
+    version='0.11',
+    url='http://github.com/mitsuhiko/flask-sqlalchemy',
+    license='BSD',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    description='Adds SQLAlchemy support to your Flask application',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'setuptools',
+        'Flask',
+        'SQLAlchemy'
+    ],
+    test_suite='test_sqlalchemy',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
