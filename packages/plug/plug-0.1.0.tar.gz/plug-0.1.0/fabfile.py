eca8d4e15033b65e881f62c82e39c0e7133be307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:51 +0000

    Add package plug/0.1.0/plug-0.1.0.tar.gz

---
+++ packages/plug/plug-0.1.0.tar.gz/plug-0.1.0/fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import *
+
+env.hosts = ['33.33.33.10']
+env.password = 'vagrant'
+env.user = 'vagrant'
+
+def bootstrap():
+    sudo('apt-get update')
+
+    put('puppet.tgz', '/tmp/puppet.tgz')
+
+    sudo('rm -rf /etc/puppet')
+    with cd('/etc'):
+        sudo('tar xfz /tmp/puppet.tgz')
+    sudo('puppet apply /etc/puppet/manifests/server.pp')
+
+    sudo('easy_install -U distribute')
+
+def deploy():
+    put('dist/plug-latest.tar.gz', '/tmp/plug.tar.gz')
+    sudo('pip install /tmp/plug.tar.gz')
+
+def test():
+    #sudo('rm -rf /srv/plug')
+    put('*.plug', '/tmp')
+    sudo('plug install --plug=/tmp/plug-0.1.0.server1.plug')
+    sudo('plug setup --plug=plug-0.1.0.server1.plug')
+    #sudo('plug install --plug=/tmp/plug-0.1.0.server2.plug')
+    #sudo('plug setup --plug=plug-0.1.0.server2.plug')
+    sudo('plug list')
+    sudo('plug status')
