13233ca1a301957a9db36889d680b0dd04f8e7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package rstex/0.11/rstex-0.11.zip

---
+++ packages/rstex/rstex-0.11.zip/rstex-0.11/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2.7",
+    "License :: OSI Approved :: BSD License"
+    ]
+
+setup(
+    name="rstex",
+    version="0.11",
+    description="An extension of docutils' rst2latex with inline math, "
+                "equations, references, and raw latex",
+    long_description=open('README.txt', 'r').read(),
+    license="BSD License",
+    keywords="restructuredtext rst docutils latex tex math rst2latex",
+    scripts=["src/rstex.py"],
+    author="Marcin Cieslik",
+    author_email="mpc4p@virginia.edu",
+    url="http://muralab.org/rstex",
+    zip_safe=False,
+    install_requires=["setuptools", "docutils"]
+)
---
+++ packages/rstex/rstex-0.11.zip/rstex-0.11/test/short.rst
@@ -0,0 +1,13 @@
+:math:`\frac{\log 23}{\pi}`
+:ref:`my_label`
+
+.. math::
+    :label: my_label
+      
+    \pi \neq \sigma
+
+.. latex::
+    :usepackage: url
+            
+    \emph{a link} \url{http://muralab.org/rstex}
+        
