1ed43cdc7ab36aeb2391e557dc80c866653b9ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:02 +0000

    Add package pyramid_simpleform/0.3.1/pyramid_simpleform-0.3.1.tar.gz

---
+++ packages/pyramid_simpleform/pyramid_simpleform-0.3.1.tar.gz/pyramid_simpleform-0.3.1/pyramid_simpleform/__init__.py
@@ -0,0 +1,217 @@
+from formencode import htmlfill
+from formencode import variabledecode
+from formencode import Invalid
+
+from pyramid.renderers import render
+
+class Form(object):
+
+    """
+    `request` : Pyramid request instance
+
+    `schema`  : FormEncode Schema class or instance
+
+    `validators` : a dict of FormEncode validators i.e. { field : validator }
+
+    `defaults`   : a dict of default values
+
+    `obj`        : instance of an object (e.g. SQLAlchemy model)
+
+    `state`      : state passed to FormEncode validators.
+
+    `method`        : HTTP method
+
+    `variable_decode` : will decode dict/lists
+
+    `dict_char`       : variabledecode dict char
+
+    `list_char`       : variabledecode list char
+
+    """
+
+    def __init__(self, request, schema=None, validators=None, defaults=None, 
+                 obj=None, state=None, method="POST", variable_decode=False, 
+                 dict_char=".", list_char="-", multipart=False):
+
+        self.request = request
+        self.schema = schema
+        self.validators = validators
+        self.method = method
+        self.variable_decode = variable_decode
+        self.dict_char = dict_char
+        self.list_char = list_char
+        self.multipart = multipart
+        self.state = state
+
+        self.is_validated = False
+
+        self.errors = {}
+        self.data = {}
+
+        if defaults:
+            self.data.update(defaults)
+
+        if obj:
+            self.data.update(obj.__dict__)
+
+
+        assert self.schema or self.validators, \
+                "validators and/or schema required"
+
+    def is_error(self, field):
+        """
+        Checks if individual field has errors.
+        """
+        return field in self.errors
+
+    def errors_for(self, field):
+        """
+        Returns any errors for a given field as a list.
+        """
+        errors = self.errors.get(field, [])
+        if isinstance(errors, basestring):
+            errors = [errors]
+        return errors
+
+    def validate(self):
+        """
+        Runs validation and returns True/False whether form is 
+        valid.
+        
+        This will check if the form should be validated (i.e. the
+        request method matches) and the schema/validators validate.
+
+        Validation will only be run once; subsequent calls to 
+        validate() will have no effect, i.e. will just return
+        the original result.
+
+        The errors and data values will be updated accordingly.
+
+        """
+
+        if self.is_validated:
+            return not(self.errors)
+
+        if self.method and self.method != self.request.method:
+            return False
+
+        if self.method == "POST":
+            params = self.request.POST
+        else:
+            params = self.request.params
+        
+        if self.variable_decode:
+            decoded = variabledecode.variable_decode(
+                        params, self.dict_char, self.list_char)
+
+        else:
+            decoded = params
+
+        self.data.update(params)
+
+        if self.schema:
+            try:
+                self.data = self.schema.to_python(decoded, self.state)
+            except Invalid, e:
+                self.errors = e.unpack_errors(self.variable_decode,
+                                              self.dict_char,
+                                              self.list_char)
+
+
+        if self.validators:
+            for field, validator in self.validators.iteritems():
+                try:
+                    self.data[field] = validator.to_python(decoded.get(field),
+                                                           self.state)
+
+                except Invalid, e:
+                    self.errors[field] = unicode(e)
+
+        self.is_validated = True
+
+        return not(self.errors)
+
+    def bind(self, obj, include=None, exclude=None):
+        """
+        Binds validated field values to an object instance, for example a
+        SQLAlchemy model instance.
+
+        `include` : list of included fields. If field not in this list it 
+        will not be bound to this object.
+
+        `exclude` : list of excluded fields. If field is in this list it 
+        will not be bound to the object.
+
+        Returns the `obj` passed in.
+
+        Calling bind() before running validate() will result in a RuntimeError
+        """
+
+        if not self.is_validated:
+            raise RuntimeError, \
+                    "Form has not been validated. Call validate() first"
+
+        if self.errors:
+            raise RuntimeError, "Cannot bind to object if form has errors"
+
+        for k, v in self.data.items():
+
+            if include and k not in include:
+                continue
+
+            if exclude and k in exclude:
+                continue
+
+            setattr(obj, k, v)
+
+        return obj
+
+    def htmlfill(self, content, **htmlfill_kwargs):
+        """
+        Runs FormEncode **htmlfill** on content.
+        """
+
+        charset = getattr(self.request, 'charset', 'utf-8')
+        htmlfill_kwargs.setdefault('encoding', charset)
+        return htmlfill.render(content, 
+                               defaults=self.data,
+                               errors=self.errors,
+                               **htmlfill_kwargs)
+
+    def render(self, template, extra_info=None, htmlfill=True,
+              **htmlfill_kwargs):
+        """
+        Renders the form directly to a template,
+        using Pyramid's **render** function. 
+
+        `template` : name of template
+
+        `extra_info` : dict of extra data to pass to template
+
+        `htmlfill` : run htmlfill on the result.
+
+        By default the form itself will be passed in as `form`.
+
+        htmlfill is automatically run on the result of render if
+        `htmlfill` is **True**.
+
+        This is useful if you want to use htmlfill on a form,
+        but still return a dict from a view. For example::
+
+            @view_config(name='submit', request_method='POST')
+            def submit(request):
+
+                form = Form(request, MySchema)
+                if form.validate():
+                    # do something
+                return dict(form=form.render("my_form.html"))
+
+        """
+        
+        extra_info = extra_info or {}
+        extra_info.setdefault('form', self)
+
+        result = render(template, extra_info, self.request)
+        if htmlfill:
+            result = self.htmlfill(result, **htmlfill_kwargs)
+        return result
---
+++ packages/pyramid_simpleform/pyramid_simpleform-0.3.1.tar.gz/pyramid_simpleform-0.3.1/pyramid_simpleform/renderers.py
@@ -0,0 +1,161 @@
+
+from webhelpers.html import tags
+from webhelpers.html.builder import HTML
+
+class FormRenderer(object):
+    """
+    A simple form helper. Uses WebHelpers to render individual
+    form widgets: see the WebHelpers library for more information
+    on individual widgets.
+    """
+
+    def __init__(self, form, csrf_field='_csrf'):
+
+        self.form = form
+        self.data = self.form.data
+        self.csrf_field = csrf_field
+
+    def value(self, name, default=None):
+        return self.data.get(name, default)
+
+    def begin(self, url, **attrs):
+        """
+        Creates the opening <form> tags.
+        """
+        return tags.form(url, multipart=self.form.multipart, **attrs)
+
+    def end(self):
+        """
+        Closes the form, i.e. outputs </form>.
+        """
+        return tags.end_form()
+    
+    def csrf(self):
+        """
+        Returns the CSRF hidden input. Creates new CSRF token
+        if none has been assigned yet.
+
+        The name of the hidden field is **_csrf** by default.
+        """
+        token = self.form.request.session.get_csrf_token()
+        if token is None:
+            token = self.form.request.session.new_csrf_token()
+
+        return self.hidden(self.csrf_field, value=token)
+
+    def csrf_token(self):
+        """
+        Convenience function. Returns CSRF hidden tag inside hidden DIV.
+        """
+        return HTML.tag("div", self.csrf(), style="display:none;")
+
+    def text(self, name, value=None, id=None, **attrs):
+        """
+        Outputs text input.
+        """
+        return tags.text(name, self.value(name, value), id, **attrs)
+
+    def file(self, name, value=None, id=None, **attrs):
+        """
+        Outputs file input.
+        """
+        return tags.file(name, self.value(name, value), id, **attrs)
+
+    def hidden(self, name, value=None, id=None, **attrs):
+        """
+        Outputs hidden input.
+        """
+        return tags.hidden(name, self.value(name, value), id, **attrs)
+
+    def radio(self, name, value=None, checked=False, label=None, **attrs):
+        """
+        Outputs radio input.
+        """
+        checked = self.data.get(name) == value or checked
+        return tags.radio(name, value, checked, label, **attrs)
+
+    def submit(self, name, value=None, id=None, **attrs):
+        """
+        Outputs submit button.
+        """
+        return tags.submit(name, self.value(name, value), id, **attrs)
+
+    def select(self, name, options, selected_value=None, id=None, **attrs):
+        """
+        Outputs <select> element.
+        """
+        return tags.select(name, self.value(name, selected_value), 
+                           options, id, **attrs)
+
+    def checkbox(self, name, value="1", checked=False, label=None, id=None, 
+                 **attrs):
+        """
+        Outputs checkbox input.
+        """
+    
+        return tags.checkbox(name, value, self.value(name), label, id, **attrs)
+
+    def textarea(self, name, content="", id=None, **attrs):
+        """
+        Outputs <textarea> element.
+        """
+        return tags.textarea(name, self.value(name, content), id, **attrs)
+
+    def password(self, name, value=None, id=None, **attrs):
+        """
+        Outputs a password input.
+        """
+        return tags.password(name, self.value(name, value), id, **attrs)
+
+    def is_error(self, name):
+        """
+        Shortcut for self.form.is_error(name)
+        """
+        return self.form.is_error(name)
+
+    def errors_for(self, name):
+        """
+        Shortcut for self.form.errors_for(name)
+        """
+        return self.form.errors_for(name)
+
+    def errorlist(self, name=None, **attrs):
+        """
+        Renders errors in a <ul> element. Unless specified in attrs, class
+        will be "error".
+
+        If no errors present returns an empty string.
+
+        `name` : errors for name. If **None** all errors will be rendered.
+        """
+
+        if name is None:
+            errors = self.form.errors.values()
+        else:
+            errors = self.errors_for(name)
+
+        if not errors:
+            return ''
+
+        content = "\n".join(HTML.tag("li", error) for error in errors)
+        
+        if 'class_' not in attrs:
+            attrs['class_'] = "error"
+
+        return HTML.tag("ul", tags.literal(content), **attrs)
+
+    def label(self, name, label=None, **attrs):
+        """
+        Outputs a <label> element. 
+
+        `name`  : field name. Automatically added to "for" attribute.
+
+        `label` : if **None**, uses the capitalized field name.
+        """
+        if 'for_' not in attrs:
+            attrs['for_'] = name
+        label = label or name.capitalize()
+        return HTML.tag("label", label, **attrs)
+        
+
+
