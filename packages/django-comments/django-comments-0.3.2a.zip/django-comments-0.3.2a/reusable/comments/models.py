d8ab828aa149fa39ce22c299f9897d8547722b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:30 +0000

    Add package django-comments/0.3.2a/django-comments-0.3.2a.zip

---
+++ packages/django-comments/django-comments-0.3.2a.zip/django-comments-0.3.2a/reusable/comments/models.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+from reusable.comments.managers import GenericObjectManager
+
+# --------------------------------------------------------------------------- #
+
+class GenericObject(models.Model):
+    """
+    Базовая модель, обеспечивающая функциональность generic relation
+    """
+    object_id      = models.CharField(max_length=255)
+    content_type   = models.ForeignKey(ContentType)
+    content_object = generic.GenericForeignKey(
+        ct_field   = 'content_type',
+        fk_field   = 'object_id',
+    )
+
+    objects = GenericObjectManager()
+
+    class Meta:
+        abstract = True
+
+# --------------------------------------------------------------------------- #
+
+class AbstractComment(GenericObject):
+    """
+    Базовая модель комментария
+    """
+    parent_comment = models.ForeignKey('self', related_name='replies', null=True, blank=True)
+    date_created   = models.DateTimeField(default=datetime.now, editable=False)
+    content        = models.TextField(default='')
+    remote_addr    = models.IPAddressField(blank=True, null=True)
+    forwarded_for  = models.IPAddressField(blank=True, null=True)
+
+    def has_replies(self):
+        return self.rght - self.lft > 1
+
+    def parent_id(self):
+        return self.__dict__['parent_comment_id']
+
+    def __unicode__(self):
+        return self.content
+
+    class Meta:
+        abstract = True
+        app_label = 'comments'
+        verbose_name = _('comment')
+        verbose_name_plural = _('comments')
---
+++ packages/django-comments/django-comments-0.3.2a.zip/django-comments-0.3.2a/reusable/comments/tests/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from django.template import Template, Context
+
+class CommentViewTest(TestCase):
+    view_name = 'comments_new'
+
+    def setUp(self):
+        self.client = client.Client()
+        self.url    = reverse(self.view_name)
+
+    def get(self, data=dict(), meta=dict(), url=None):
+        return self.client.get(url or self.url, data, **meta)
+
+    def post(self, data=dict(), meta=dict(), url=None):
+        return self.client.post(url or self.url, data, **meta)
+
+    def ajax_get(self, data=dict(), meta=dict(), url=None):
+        meta.update(HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        return self.get(data, meta, url)
+
+    def ajax_post(self, data=dict(), meta=dict(), url=None):
+        meta.update(HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        return self.post(data, meta, url)
+
+    def render(self, template, context):
+        return Template(template).render(Context(context))
+
+# --------------------------------------------------------------------------- #
+
+from .environment import *
+from .views import *
+from .templatetags import *
+
+from reusable.comments_plugins.guest.tests import *
+from reusable.comments_plugins.authorized.tests import *
