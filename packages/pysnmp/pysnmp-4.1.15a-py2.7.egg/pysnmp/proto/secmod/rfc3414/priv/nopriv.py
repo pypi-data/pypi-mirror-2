cb11c260e543a2cedf0f09a1aa6249808b5b10c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:19 +0000

    Add package pysnmp/4.1.15a/pysnmp-4.1.15a-py2.7.egg

---
+++ packages/pysnmp/pysnmp-4.1.15a-py2.7.egg/pysnmp/proto/secmod/rfc3414/priv/nopriv.py
@@ -0,0 +1,10 @@
+from pysnmp.proto.secmod.rfc3414.priv import base
+from pysnmp.proto import error
+
+class NoPriv(base.AbstractEncryptionService):
+    serviceID = (1, 3, 6, 1, 6, 3, 10, 1, 2, 1) # usmNoPrivProtocol
+    def encryptData(self, encryptKey, privParameters, dataToEncrypt):
+        raise error.StatusInformation(errorIndication='no encryption')
+    
+    def decryptData(self, decryptKey, privParameters, encryptedData):
+        raise error.StatusInformation(errorIndication='no encryption')
