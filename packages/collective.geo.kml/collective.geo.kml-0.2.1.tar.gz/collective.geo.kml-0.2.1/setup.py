ef3d2d44165f44bc3dbb371997255a89a94185c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:16 +0000

    Add package collective.geo.kml/0.2.1/collective.geo.kml-0.2.1.tar.gz

---
+++ packages/collective.geo.kml/collective.geo.kml-0.2.1.tar.gz/collective.geo.kml-0.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2.1'
+
+setup(name='collective.geo.kml',
+      version=version,
+      description="Kml view for collective.geo",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone geo gis kml google earth',
+      author='Giorgio Borelli',
+      author_email='giorgio@giorgioborelli.it',
+      url='https://svn.plone.org/svn/collective/collective.geo.kml',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.geo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'collective.geo.mapwidget',
+          'collective.geo.geographer',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
