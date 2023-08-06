84207f518266d71e4b027b9702cce7e38bb8a7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:40 +0000

    Add package sigprofiler/0.0.3/sigprofiler-0.0.3.tar.gz

---
+++ packages/sigprofiler/sigprofiler-0.0.3.tar.gz/sigprofiler-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='sigprofiler',
+      version='0.0.3',
+      py_modules=['sigprofiler'],
+      description='Statistical profiler using signal',
+      author='INADA Naoki',
+      author_email='songofacandy@gmail.com',
+      classifiers=[
+          "License :: OSI Approved :: Python Software Foundation License",
+          "Operating System :: POSIX",
+          "Programming Language :: Python :: 2",
+          ],
+      )
