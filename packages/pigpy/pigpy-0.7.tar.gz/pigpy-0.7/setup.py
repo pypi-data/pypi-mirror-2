5139372dd20bf287bee64eb0fc065b987d73442f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:49 +0000

    Add package pigpy/0.7/pigpy-0.7.tar.gz

---
+++ packages/pigpy/pigpy-0.7.tar.gz/pigpy-0.7/setup.py
@@ -0,0 +1,22 @@
+import os
+from distutils.core import setup
+
+setup(
+    name="pigpy",
+    packages=['pigpy'],
+    version="0.7",
+    license="MIT",
+    url="http://pypi.python.org/pypi/pigpy",
+    author="Marshall Weir",
+    author_email="marshall.weir+pigpy@gmail.com",
+
+    classifiers=[
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Software Development",
+    ],
+    description="a python tool to manage Pig reports",
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.txt")).read()
+)
