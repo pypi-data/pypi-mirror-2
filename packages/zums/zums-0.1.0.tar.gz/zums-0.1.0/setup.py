75a259a3fb3529d383816946004db5321d05018a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:09 +0000

    Add package zums/0.1.0/zums-0.1.0.tar.gz

---
+++ packages/zums/zums-0.1.0.tar.gz/zums-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "zums",
+    version = "0.1.0",
+    url = 'http://github.com/amitu/zums',
+    license = 'BSD',
+    description = file("README.rst").read(),
+    author = 'Amit Upadhyay',
+    packages = find_packages('ROOT'),
+    package_dir = {'': 'ROOT'},
+    install_requires = ['setuptools'],
+    entry_points={
+      'console_scripts': ['zumsd = zums.zumsd:main']
+    }
+)
