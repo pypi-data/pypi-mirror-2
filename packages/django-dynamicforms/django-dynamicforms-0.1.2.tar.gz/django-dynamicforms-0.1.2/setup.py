9afe604aa2503ccd596c663504f95621f472fe0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:46 +0000

    Add package django-dynamicforms/0.1.2/django-dynamicforms-0.1.2.tar.gz

---
+++ packages/django-dynamicforms/django-dynamicforms-0.1.2.tar.gz/django-dynamicforms-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                 = 'django-dynamicforms',
+    version              = '0.1.2',
+    packages             = find_packages(),
+    install_requires     = ['html2text'],
+    author               = 'Roald de Vries',
+    author_email         = 'roald@go2people.nl',
+    include_package_data = True,
+    url                  = 'https://github.com/roalddevries/django-dynamicforms/',
+    license              = 'LICENSE.txt',
+    description          = 'Dynamically add forms to your site through the admin interface.',
+    long_description     = open('README.rst').read(),
+)
