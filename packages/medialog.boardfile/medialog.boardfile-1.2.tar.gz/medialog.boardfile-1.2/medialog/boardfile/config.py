77f4467f86af52c349417c45914c9356305d5fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:24 +0000

    Add package medialog.boardfile/1.2/medialog.boardfile-1.2.tar.gz

---
+++ packages/medialog.boardfile/medialog.boardfile-1.2.tar.gz/medialog.boardfile-1.2/medialog/boardfile/config.py
@@ -0,0 +1,11 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'medialog.boardfile'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'Publication': 'medialog.boardfile: Add Publication',
+    'Boardfile': 'medialog.boardfile: Add Boardfile',
+}
+
---
+++ packages/medialog.boardfile/medialog.boardfile-1.2.tar.gz/medialog.boardfile-1.2/medialog/boardfile/interfaces/__init__.py
@@ -0,0 +1,5 @@
+# -*- extra stuff goes here -*-
+from publication import IPublication
+
+from boardfile import IBoardfile
+
