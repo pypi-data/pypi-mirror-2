ddb928421b103c915d0c87fbff57cc4257f73da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package HTTP4Store/0.2a/HTTP4Store-0.2a.tar.gz

---
+++ packages/HTTP4Store/HTTP4Store-0.2a.tar.gz/HTTP4Store-0.2a/setup.py
@@ -0,0 +1,19 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name="HTTP4Store",
+      version="0.2a",
+      description="HTTP4Store - python client for 4Store.org (4s-httpd)",
+      long_description="""\
+This is a python client for the 4Store httpd service - allowing for easy handling of sparql results, and adding, appending and deleting graphs.
+
+Changelog 0.2   - Now throws a "HTTP4StoreConnectionException" when it cannot connect to 4Store
+Changelog 0.1.1 - incorporated a fix to append_graph method, made by Kurt J
+""",
+      author="Ben O'Steen",
+      author_email="bosteen@gmail.com",
+      packages=find_packages(exclude='tests'),
+      install_requires=['rdflib>=2.4.2', 'simplejson', 'httplib2'],
+      )
+
