33294951120a0de612a07c04a2da205dc498e294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:56 +0000

    Add package kombu/1.0.0b4/kombu-1.0.0b4.tar.gz

---
+++ packages/kombu/kombu-1.0.0b4.tar.gz/kombu-1.0.0b4/kombu/__init__.py
@@ -0,0 +1,13 @@
+"""AMQP Messaging Framework for Python"""
+VERSION = (1, 0, 0, "b4")
+__version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
+__author__ = "Ask Solem"
+__contact__ = "ask@celeryproject.org"
+__homepage__ = "http://github.com/ask/kombu/"
+__docformat__ = "restructuredtext"
+
+import os
+if not os.environ.get("KOMBU_NO_EVAL", False):
+    from kombu.connection import BrokerConnection
+    from kombu.entity import Exchange, Queue
+    from kombu.messaging import Consumer, Producer
