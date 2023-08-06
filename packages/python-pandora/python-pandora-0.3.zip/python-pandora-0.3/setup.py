04f33bf456414090603f052fc64b66ce00b035ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:49 +0000

    Add package python-pandora/0.3/python-pandora-0.3.zip

---
+++ packages/python-pandora/python-pandora-0.3.zip/python-pandora-0.3/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='python-pandora',
+    version='0.3',
+    description='Library to access pandora.com. Based on the work from http://forum.xbmc.org/showthread.php?t=70471',
+    maintainer='Stefan Richter',
+    maintainer_email='stefan@02strich.de',
+    packages = ['pandora']
+    #install_requires=['Flask >= 1.0'],
+)
