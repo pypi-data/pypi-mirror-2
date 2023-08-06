503bcdaae2b2e7ef37576c89830c6d8db26f2dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:42 +0000

    Add package collective.rtvideo.youtube/0.1.0/collective.rtvideo.youtube-0.1.0.tar.gz

---
+++ packages/collective.rtvideo.youtube/collective.rtvideo.youtube-0.1.0.tar.gz/collective.rtvideo.youtube-0.1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='collective.rtvideo.youtube',
+      version=version,
+      description="The YouTube Plone support for RedTurtle Video",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Multimedia :: Video",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        ],
+      keywords='plone plonegov video embed youtube',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://plone.org/products/redturtle.video',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.rtvideo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'redturtle.video>=0.5.0',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
