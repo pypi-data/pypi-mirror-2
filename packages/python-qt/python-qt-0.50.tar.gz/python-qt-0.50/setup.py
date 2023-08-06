23d233bfe6a18cd9cb4503e38ef119f0440f4be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:42 +0000

    Add package python-qt/0.50/python-qt-0.50.tar.gz

---
+++ packages/python-qt/python-qt-0.50.tar.gz/python-qt-0.50/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-qt',
+      version='0.50',
+      description='Python compatibility wrapper for Qt Bindings (PySide/PyQt4)',
+      author='Christopher S. Case',
+      author_email='chris.case@g33xnexus.com',
+      url='https://bitbucket.org/morgul/python-qt',
+      packages=['Qt'],
+     )
