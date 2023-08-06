d3a9b8ad9ddbdfe3b3484ef14552618063a8e028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:34 +0000

    Add package squasher/1.3.0/squasher-1.3.0.tar.gz

---
+++ packages/squasher/squasher-1.3.0.tar.gz/squasher-1.3.0/squasher.py
@@ -0,0 +1,16 @@
+"""this is the "squasher.py" module and it contains the function "print_lol" which prints lists and any nested lists"""
+
+def print_lol(the_list, indent=False, level=0):
+
+        """this is a recursive function named "print_lol" which takes a positional argument called "the_list".
+	Each item in the list is recursively printed on its own line."""
+	
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        print_lol(each_item, indent, level+1)
+                else:
+                        if indent:
+                                for tab_stop in range(level):
+                                        print("\t", end='')
+                        print(each_item)
+
