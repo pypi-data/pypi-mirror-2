79ee2a5c50334ca0a2affbf7242cdab33c5a57b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package jquery.pyproxy/0.2/jquery.pyproxy-0.2.zip

---
+++ packages/jquery.pyproxy/jquery.pyproxy-0.2.zip/jquery.pyproxy-0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+def get_file_contents(filename):
+    file_path = os.path.join(filename)
+    this_file = open(file_path)
+    contents = this_file.read().strip()
+    this_file.close()
+    return contents
+
+version = get_file_contents('jquery/pyproxy/version.txt')
+history = get_file_contents('jquery/pyproxy/HISTORY.txt')
+readme = get_file_contents('README.txt')
+long = "%s\n\n\n%s" % (readme, history)
+
+setup(name='jquery.pyproxy',
+      version=version,
+      description="A simple python egg and jquery plugin to easily use JQuery in Django/Plone/.. websites.",
+      long_description=long,
+      classifiers=[
+        ],
+      keywords='',
+      author='Vincent Pretre',
+      author_email='vincent.pretre@gmail.com',
+      url='http://github.com/vincent-psarga/jquery.pyproxy',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      namespace_packages=['jquery'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
