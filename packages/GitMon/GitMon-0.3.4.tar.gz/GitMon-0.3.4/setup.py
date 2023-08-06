a5ed86801e68573a0ef3edb067574e624f0605ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:51 +0000

    Add package GitMon/0.3.4/GitMon-0.3.4.tar.gz

---
+++ packages/GitMon/GitMon-0.3.4.tar.gz/GitMon-0.3.4/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env/ python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name = 'GitMon',
+    version = '0.3.4',
+    packages = find_packages('lib'),
+    package_dir = {'' : 'lib'},
+    scripts = ['misc/gitmon'],
+
+    zip_safe = False,
+
+    package_data = {'': ['*.png', '*.example']},
+    requires = ('gitpython (>=0.2.9)', 'py_Growl_2_6 (>=0.0.7)'),
+    install_requires = ['gitpython >= 0.2.9', 'py_Growl_2_6 >= 0.0.7'],
+
+    #Metadata for PyPI
+    url = 'http://github.com/spajus/gitmon/',
+    author = 'Tomas Varaneckas',
+    author_email = 'tomas.varaneckas@gmail.com',
+    description = 'GitMon - The Git Repository Monitor',
+    long_description = 'GitMon - The Git Repository Monitor',
+    license = 'GPLv3',
+    keywords = ['git', 'monitor', 'scm', 'repository'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: MacOS X',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ]
+)
