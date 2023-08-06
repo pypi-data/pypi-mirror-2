ea1beb14b632d0597634b12c5fde39cbb1ae1a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:45 +0000

    Add package printIndented/0.1.2/printIndented-0.1.2.zip

---
+++ packages/printIndented/printIndented-0.1.2.zip/printIndented-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name    = "printIndented",
+    version = '0.1.2',
+    py_modules =['print_indented'],
+    author     = 'AvituTestLab',
+    author_email  = 'avitustestlab@gmail.com',
+    url           = 'http://avitustestlab.com',
+    description  = 'A sample util to print indented text'
+    
+    )
