4848eb553459a924bce9cb1ce9a6336c60a89050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:50 +0000

    Add package django-icons-tango/1.0/django-icons-tango-1.0.tar.gz

---
+++ packages/django-icons-tango/django-icons-tango-1.0.tar.gz/django-icons-tango-1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name = 'django-icons-tango',
+    version = '1.0',
+    author = 'Mikhail Korobov',
+    author_email = 'kmike84@gmail.com',
+    url = 'http://bitbucket.org/kmike/django-icons/',
+    description = 'Tango icons pack for django.contrib.staticfiles',
+    long_description = open('README.rst').read(),
+    license = 'MIT license',
+
+    packages=['icons_tango'],
+    include_package_data = True,
+    install_requires=['distribute'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
