e6e7e750255fa386ce55b4e6b31111ac120eb26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:00 +0000

    Add package objgraph/1.5.1/objgraph-1.5.1.tar.gz

---
+++ packages/objgraph/objgraph-1.5.1.tar.gz/objgraph-1.5.1/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+import os, sys, unittest, doctest
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def relative(filename):
+    here = os.path.dirname('__file__')
+    return os.path.join(here, filename)
+
+
+def read(filename):
+    f = open(relative(filename))
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+
+def unsphinx(text):
+    # remove Sphinx extensions used in CHANGES.txt from reStructuredText
+    # so that it can be handled by plain docutils
+    return text.replace(':func:', '').replace('.. currentmodule:: objgraph', '')
+
+
+def get_version():
+    d = {}
+    exec read('objgraph.py') in d
+    return d['__version__']
+
+
+def get_description():
+    readme = read('README.txt')
+    changelog = read('CHANGES.txt')
+    return unsphinx(readme + '\n\n\n' + changelog)
+
+
+def build_images(doctests=()):
+    import tests
+    if not doctests:
+        doctests = tests.find_doctests()
+    suite = doctest.DocFileSuite(optionflags=doctest.ELLIPSIS,
+                                 checker=tests.MyChecker(),
+                                 *doctests)
+    result = unittest.TextTestRunner().run(suite)
+    if not result.wasSuccessful():
+        sys.exit(1)
+
+
+if len(sys.argv) > 1 and sys.argv[1] == '--build-images':
+    build_images(sys.argv[2:])
+    sys.exit(0)
+
+
+setup(name='objgraph',
+      version=get_version(),
+      author='Marius Gedminas',
+      author_email='marius@gedmin.as',
+      url='http://mg.pov.lt/objgraph/',
+      license='MIT',
+      description='Draws Python object reference graphs with graphviz',
+      long_description=get_description(),
+      py_modules=['objgraph'])
---
+++ packages/objgraph/objgraph-1.5.1.tar.gz/objgraph-1.5.1/tests.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import unittest
+import doctest
+import tempfile
+import os
+import shutil
+import glob
+
+
+RANDOM_OUTPUT = doctest.register_optionflag('RANDOM_OUTPUT')
+
+
+class MyChecker(doctest.OutputChecker):
+
+    def check_output(self, want, got, optionflags):
+        if optionflags & RANDOM_OUTPUT:
+            return True
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
+
+
+def setUp(test):
+    test.tmpdir = tempfile.mkdtemp(prefix='test-objgraph-')
+    test.prevdir = os.getcwd()
+    test.prevtempdir = tempfile.tempdir
+    tempfile.tempdir = test.tmpdir
+    os.chdir(test.tmpdir)
+
+
+def tearDown(test):
+    tempfile.tempdir = test.prevtempdir
+    os.chdir(test.prevdir)
+    shutil.rmtree(test.tmpdir)
+
+
+def find_doctests():
+    return [fn for fn in glob.glob('*.txt')
+            if fn != 'HACKING.txt']
+
+
+def test_suite():
+    doctests = find_doctests()
+    return doctest.DocFileSuite(setUp=setUp, tearDown=tearDown,
+                                optionflags=doctest.ELLIPSIS,
+                                checker=MyChecker(),
+                                *doctests)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
