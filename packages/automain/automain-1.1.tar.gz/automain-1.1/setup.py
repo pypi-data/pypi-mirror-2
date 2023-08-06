7978a2b0e63fb917e2486d80c7ebf896e70c9b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:49 +0000

    Add package automain/1.1/automain-1.1.tar.gz

---
+++ packages/automain/automain-1.1.tar.gz/automain-1.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='automain',
+    version='1.1',
+    description='A shortcut for "if __name__ == \"main\":"',
+    author='Gerald Kaszuba',
+    author_email='automain@gakman.com',
+    packages=['automain'],
+    )
