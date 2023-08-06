7b89d725f64c91ca6fba540127e0b617071d02cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:35 +0000

    Add package GitWeb/0.3/GitWeb-0.3.tar.gz

---
+++ packages/GitWeb/GitWeb-0.3.tar.gz/GitWeb-0.3/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='GitWeb',
+      version='0.3',
+      description='WSGI application to serve a git repository',
+      long_description=open('README.txt').read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Gael Pasgrimaud',
+      author_email='gael@gawel.org',
+      url='https://github.com/gawel/GitWeb',
+      licence='GPL',
+      keywords='web',
+      py_modules = ['gitweb', 'subprocessio'],
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['WebOb'],
+      tests_require=[],
+      entry_points = """\
+      [paste.app_factory]
+      main = gitweb:make_dir_app
+      dir = gitweb:make_dir_app
+      repo = gitweb:make_app
+      """,
+      )
+
