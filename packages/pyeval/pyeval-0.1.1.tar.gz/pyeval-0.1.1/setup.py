f60a74e8cd096462b89b34161756edf73428c19a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:10 +0000

    Add package pyeval/0.1.1/pyeval-0.1.1.tar.gz

---
+++ packages/pyeval/pyeval-0.1.1.tar.gz/pyeval-0.1.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyeval',
+      description='Conveniently evaluate expressions from the shell',
+      version='0.1.1',
+      author='Nathan Wilcox',
+      author_email='nejucomo@gmail.com',
+      license='GPLv3',
+      url='https://bitbucket.org/nejucomo/pyeval/',
+      scripts=['pyeval'],
+     )
