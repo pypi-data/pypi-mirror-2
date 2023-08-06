9cb7f6a9ea1a5a9952cefe172e9c97b74806db10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package jersey/0.1.4/jersey-0.1.4.tar.gz

---
+++ packages/jersey/jersey-0.1.4.tar.gz/jersey-0.1.4/setup.py
@@ -0,0 +1,45 @@
+#/usr/bin/env python2.6
+
+from setuptools import setup
+
+
+def getVersion():
+    import os
+    packageSeedFile = os.path.join("lib", "_version.py")
+    ns = {"__name__": __name__, }
+    execfile(packageSeedFile, ns)
+    return ns["version"]
+
+version = getVersion()
+
+
+setup(
+    name = version.package,
+    version = version.short(),
+
+    description = "Jersey Core Libraries",
+    long_description = open("README").read(),
+
+    author = "Oliver Gould", author_email = "jersey@olix0r.net",
+    maintainer = "Oliver Gould", maintainer_email = "jersey@olix0r.net",
+    url = "http://jersey.olix0r.net",
+
+    license = "BSD",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Framework :: Twisted",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        ],
+
+    package_dir = {"jersey": "lib", },
+    packages = ["jersey", "jersey.cases", ],
+
+    provides = ["jersey", "jersey.cli", "jersey.inet", "jersey.log", ],
+    setup_requires = ["twisted", ],
+    install_requires = ["twisted>=9.0.0", ],
+    )
+
+
