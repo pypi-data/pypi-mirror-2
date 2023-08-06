fdc4e63f4dcb2152502f876213a4ec187c2eddd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:53 +0000

    Add package Theano/0.4.0/Theano-0.4.0.zip

---
+++ packages/Theano/Theano-0.4.0.zip/Theano-0.4.0/theano/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM THEANO SETUP.PY
+short_version = '0.4.0'
+version = '0.4.0'
+hg_revision = '8416d8b4cbde+'
+full_version = '0.4.0.dev-%(hg_revision)s' % {'hg_revision': hg_revision}
+release = True
+
+if not release:
+    version = full_version
