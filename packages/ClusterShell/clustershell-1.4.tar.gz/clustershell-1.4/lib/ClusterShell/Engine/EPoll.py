f03e6d30c4cce2afa3d11800810b28b7c6544795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:17 +0000

    Add package ClusterShell/1.4/clustershell-1.4.tar.gz

---
+++ packages/ClusterShell/clustershell-1.4.tar.gz/clustershell-1.4/lib/ClusterShell/Engine/EPoll.py
@@ -0,0 +1,227 @@
+#
+# Copyright CEA/DAM/DIF (2009, 2010)
+#  Contributor: Stephane THIELL <stephane.thiell@cea.fr>
+#
+# This file is part of the ClusterShell library.
+#
+# This software is governed by the CeCILL-C license under French law and
+# abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL-C
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL-C license and that you accept its terms.
+#
+# $Id: EPoll.py 384 2010-10-17 21:24:21Z st-cea $
+
+"""
+A ClusterShell Engine using epoll, an I/O event notification facility.
+
+The epoll event distribution interface is available on Linux 2.6, and
+has been included in Python 2.6.
+"""
+
+import errno
+import select
+import time
+
+from ClusterShell.Engine.Engine import Engine
+from ClusterShell.Engine.Engine import EngineNotSupportedError
+from ClusterShell.Engine.Engine import EngineTimeoutException
+from ClusterShell.Worker.EngineClient import EngineClientEOF
+
+
+class EngineEPoll(Engine):
+    """
+    EPoll Engine
+
+    ClusterShell Engine class using the select.epoll mechanism.
+    """
+
+    identifier = "epoll"
+
+    def __init__(self, info):
+        """
+        Initialize Engine.
+        """
+        Engine.__init__(self, info)
+        try:
+            # get an epoll object
+            self.epolling = select.epoll()
+        except AttributeError:
+            raise EngineNotSupportedError()
+
+    def _register_specific(self, fd, event):
+        """
+        Engine-specific fd registering. Called by Engine register.
+        """
+        if event & (Engine.E_READ | Engine.E_ERROR):
+            eventmask = select.EPOLLIN
+        elif event == Engine.E_WRITE:
+            eventmask = select.EPOLLOUT
+
+        self.epolling.register(fd, eventmask)
+
+    def _unregister_specific(self, fd, ev_is_set):
+        """
+        Engine-specific fd unregistering. Called by Engine unregister.
+        """
+        self._debug("UNREGSPEC fd=%d ev_is_set=%x"% (fd, ev_is_set))
+        if ev_is_set:
+            self.epolling.unregister(fd)
+
+    def _modify_specific(self, fd, event, setvalue):
+        """
+        Engine-specific modifications after a interesting event change for
+        a file descriptor. Called automatically by Engine set_events().
+        For the epoll engine, it modifies the event mask associated to a file
+        descriptor.
+        """
+        self._debug("MODSPEC fd=%d event=%x setvalue=%d" % (fd, event,
+                                                            setvalue))
+        eventmask = 0
+        if setvalue:
+            if event & (Engine.E_READ | Engine.E_ERROR):
+                eventmask = select.EPOLLIN
+            elif event == Engine.E_WRITE:
+                eventmask = select.EPOLLOUT
+
+            self.epolling.register(fd, eventmask)
+        else:
+            self.epolling.unregister(fd)
+
+    def runloop(self, timeout):
+        """
+        Run epoll main loop.
+        """
+        if timeout == 0:
+            timeout = -1
+
+        start_time = time.time()
+
+        # run main event loop...
+        while self.evlooprefcnt > 0:
+            self._debug("LOOP evlooprefcnt=%d (reg_clifds=%s) (timers=%d)" % \
+                    (self.evlooprefcnt, self.reg_clifds.keys(),
+                     len(self.timerq)))
+            try:
+                timeo = self.timerq.nextfire_delay()
+                if timeout > 0 and timeo >= timeout:
+                    # task timeout may invalidate clients timeout
+                    self.timerq.clear()
+                    timeo = timeout
+                elif timeo == -1:
+                    timeo = timeout
+
+                self.reg_clifds_changed = False
+                evlist = self.epolling.poll(timeo + 0.001)
+
+            except IOError, e:
+                # might get interrupted by a signal
+                if e.errno == errno.EINTR:
+                    continue
+
+            for fd, event in evlist:
+
+                if self.reg_clifds_changed:
+                    self._debug("REG CLIENTS CHANGED - Aborting current evlist")
+                    # Oops, reconsider evlist by calling poll() again.
+                    break
+
+                # get client instance
+                client, fdev = self._fd2client(fd)
+                if not client or fdev is None:
+                    continue
+
+                # process this client
+                client._processing = True
+
+                # check for poll error condition of some sort
+                if event & select.EPOLLERR:
+                    self._debug("EPOLLERR %s" % client)
+                    self.unregister_writer(client)
+                    client.file_writer.close()
+                    client.file_writer = None
+                    continue
+
+                # check for data to read
+                if event & select.EPOLLIN:
+                    assert fdev & (Engine.E_READ | Engine.E_ERROR)
+                    assert client._events & fdev
+                    self.modify(client, 0, fdev)
+                    try:
+                        if fdev & Engine.E_READ:
+                            client._handle_read()
+                        else:
+                            client._handle_error()
+                    except EngineClientEOF, e:
+                        self._debug("EngineClientEOF %s" % client)
+                        if fdev & Engine.E_READ:
+                            self.remove(client)
+                        continue
+
+                # or check for end of stream (do not handle both at the same
+                # time because handle_read() may perform a partial read)
+                elif event & select.EPOLLHUP:
+                    self._debug("EPOLLHUP fd=%d %s (r%s,e%s,w%s)" % (fd,
+                        client.__class__.__name__, client.reader_fileno(),
+                        client.error_fileno(), client.writer_fileno()))		
+                    client._processing = False
+
+                    if fdev & Engine.E_READ:
+                        if client._events & Engine.E_ERROR:
+                            self.modify(client, 0, fdev)
+                        else:
+                            self.remove(client)
+                    else:
+                        if client._events & Engine.E_READ:
+                            self.modify(client, 0, fdev)
+                        else:
+                            self.remove(client)
+                    continue
+
+                # check for writing
+                if event & select.EPOLLOUT:
+                    self._debug("EPOLLOUT fd=%d %s (r%s,e%s,w%s)" % (fd,
+                        client.__class__.__name__, client.reader_fileno(),
+                        client.error_fileno(), client.writer_fileno()))
+                    assert fdev == Engine.E_WRITE
+                    assert client._events & fdev
+                    self.modify(client, 0, fdev)
+                    client._handle_write()
+
+                # post processing
+                client._processing = False
+
+                # apply any changes occured during processing
+                if client.registered:
+                    self.set_events(client, client._new_events)
+
+            # check for task runloop timeout
+            if timeout > 0 and time.time() >= start_time + timeout:
+                raise EngineTimeoutException()
+
+            # process clients timeout
+            self.fire_timers()
+
+        self._debug("LOOP EXIT evlooprefcnt=%d (reg_clifds=%s) (timers=%d)" % \
+                (self.evlooprefcnt, self.reg_clifds, len(self.timerq)))
+
---
+++ packages/ClusterShell/clustershell-1.4.tar.gz/clustershell-1.4/lib/ClusterShell/Worker/Worker.py
@@ -0,0 +1,534 @@
+#
+# Copyright CEA/DAM/DIF (2007, 2008, 2009, 2010)
+#  Contributor: Stephane THIELL <stephane.thiell@cea.fr>
+#
+# This file is part of the ClusterShell library.
+#
+# This software is governed by the CeCILL-C license under French law and
+# abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL-C
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL-C license and that you accept its terms.
+#
+# $Id: Worker.py 381 2010-10-11 22:19:08Z st-cea $
+
+"""
+ClusterShell worker interface.
+
+A worker is a generic object which provides "grouped" work in a specific task.
+"""
+
+from ClusterShell.Worker.EngineClient import EngineClient
+from ClusterShell.NodeSet import NodeSet
+
+
+class WorkerException(Exception):
+    """Generic worker exception."""
+
+class WorkerError(WorkerException):
+    """Generic worker error."""
+
+# DEPRECATED: WorkerBadArgumentError exception is deprecated as of 1.4,
+# use ValueError instead.
+WorkerBadArgumentError = ValueError
+
+class Worker(object):
+    """
+    Base class Worker.
+    """
+    def __init__(self, handler):
+        """
+        Initializer. Should be called from derived classes.
+        """
+        self.eh = handler                   # associated EventHandler
+        self.task = None
+
+    def _set_task(self, task):
+        """
+        Bind worker to task. Called by task.schedule()
+        """
+        if self.task is not None:
+            # one-shot-only schedule supported for now
+            raise WorkerError("worker has already been scheduled")
+        self.task = task
+
+    def _task_bound_check(self):
+        if not self.task:
+            raise WorkerError("worker is not task bound")
+
+    def _engine_clients(self):
+        """
+        Return a list of underlying engine clients.
+        """
+        raise NotImplementedError("Derived classes must implement.")
+
+    def _invoke(self, ev_type):
+        """
+        Invoke user EventHandler method if needed.
+        """
+        if self.eh:
+            self.eh._invoke(ev_type, self)
+
+    # Base getters
+
+    def last_read(self):
+        """
+        Get last read message from event handler.
+        """
+        raise NotImplementedError("Derived classes must implement.")
+
+    def last_error(self):
+        """
+        Get last error message from event handler.
+        """
+        raise NotImplementedError("Derived classes must implement.")
+
+    def did_timeout(self):
+        """
+        Return whether this worker has aborted due to timeout.
+        """
+        self._task_bound_check()
+        return self.task._num_timeout_by_worker(self) > 0
+
+    # Base actions
+
+    def abort(self):
+        """
+        Abort processing any action by this worker.
+        """
+        raise NotImplementedError("Derived classes must implement.")
+
+    def flush_buffers(self):
+        """
+        Flush any messages associated to this worker.
+        """
+        self._task_bound_check()
+        self.task._flush_buffers_by_worker(self)
+
+    def flush_errors(self):
+        """
+        Flush any error messages associated to this worker.
+        """
+        self._task_bound_check()
+        self.task._flush_errors_by_worker(self)
+
+class DistantWorker(Worker):
+    """
+    Base class DistantWorker, which provides a useful set of setters/getters
+    to use with distant workers like ssh or pdsh.
+    """
+
+    def __init__(self, handler):
+        Worker.__init__(self, handler)
+
+        self._last_node = None
+        self._last_msg = None
+        self._last_errmsg = None
+        self._last_rc = 0
+        self.started = False
+
+    def _on_start(self):
+        """
+        Starting
+        """
+        if not self.started:
+            self.started = True
+            self._invoke("ev_start")
+
+    def _on_node_msgline(self, node, msg):
+        """
+        Message received from node, update last* stuffs.
+        """
+        self._last_node = node
+        self._last_msg = msg
+
+        self.task._msg_add((self, node), msg)
+
+        self._invoke("ev_read")
+
+    def _on_node_errline(self, node, msg):
+        """
+        Error message received from node, update last* stuffs.
+        """
+        self._last_node = node
+        self._last_errmsg = msg
+
+        self.task._errmsg_add((self, node), msg)
+
+        self._invoke("ev_error")
+
+    def _on_node_rc(self, node, rc):
+        """
+        Return code received from a node, update last* stuffs.
+        """
+        self._last_node = node
+        self._last_rc = rc
+
+        self.task._rc_set((self, node), rc)
+
+        self._invoke("ev_hup")
+
+    def _on_node_timeout(self, node):
+        """
+        Update on node timeout.
+        """
+        # Update _last_node to allow node resolution after ev_timeout.
+        self._last_node = node
+
+        self.task._timeout_add((self, node))
+
+    def last_node(self):
+        """
+        Get last node, useful to get the node in an EventHandler
+        callback like ev_timeout().
+        """
+        return self._last_node
+
+    def last_read(self):
+        """
+        Get last (node, buffer), useful in an EventHandler.ev_read()
+        """
+        return self._last_node, self._last_msg
+
+    def last_error(self):
+        """
+        Get last (node, error_buffer), useful in an EventHandler.ev_error()
+        """
+        return self._last_node, self._last_errmsg
+
+    def last_retcode(self):
+        """
+        Get last (node, rc), useful in an EventHandler.ev_hup()
+        """
+        return self._last_node, self._last_rc
+
+    def node_buffer(self, node):
+        """
+        Get specific node buffer.
+        """
+        self._task_bound_check()
+        return self.task._msg_by_source((self, node))
+        
+    def node_error(self, node):
+        """
+        Get specific node error buffer.
+        """
+        self._task_bound_check()
+        return self.task._errmsg_by_source((self, node))
+
+    node_error_buffer = node_error
+
+    def node_retcode(self, node):
+        """
+        Get specific node return code. Raises a KeyError if command on
+        node has not yet finished (no return code available), or is
+        node is not known by this worker.
+        """
+        self._task_bound_check()
+        try:
+            rc = self.task._rc_by_source((self, node))
+        except KeyError:
+            raise KeyError(node)
+        return rc
+
+    node_rc = node_retcode
+
+    def iter_buffers(self, match_keys=None):
+        """
+        Returns an iterator over available buffers and associated
+        NodeSet. If the optional parameter match_keys is defined, only
+        keys found in match_keys are returned.
+        """
+        self._task_bound_check()
+        for msg, keys in self.task._call_tree_matcher( \
+                            self.task._msgtree.walk, match_keys, self):
+            yield msg, NodeSet.fromlist(keys)
+
+    def iter_errors(self, match_keys=None):
+        """
+        Returns an iterator over available error buffers and associated
+        NodeSet. If the optional parameter match_keys is defined, only
+        keys found in match_keys are returned.
+        """
+        self._task_bound_check()
+        for msg, keys in self.task._call_tree_matcher( \
+                            self.task._errtree.walk, match_keys, self):
+            yield msg, NodeSet.fromlist(keys)
+
+    def iter_node_buffers(self, match_keys=None):
+        """
+        Returns an iterator over each node and associated buffer.
+        """
+        self._task_bound_check()
+        return self.task._call_tree_matcher(self.task._msgtree.items,
+                                            match_keys, self)
+
+    def iter_node_errors(self, match_keys=None):
+        """
+        Returns an iterator over each node and associated error buffer.
+        """
+        self._task_bound_check()
+        return self.task._call_tree_matcher(self.task._errtree.items,
+                                            match_keys, self)
+
+    def iter_retcodes(self, match_keys=None):
+        """
+        Returns an iterator over return codes and associated NodeSet.
+        If the optional parameter match_keys is defined, only keys
+        found in match_keys are returned.
+        """
+        self._task_bound_check()
+        for rc, keys in self.task._rc_iter_by_worker(self, match_keys):
+            yield rc, NodeSet.fromlist(keys)
+
+    def iter_node_retcodes(self):
+        """
+        Returns an iterator over each node and associated return code.
+        """
+        self._task_bound_check()
+        return self.task._krc_iter_by_worker(self)
+
+    def num_timeout(self):
+        """
+        Return the number of timed out "keys" (ie. nodes) for this worker.
+        """
+        self._task_bound_check()
+        return self.task._num_timeout_by_worker(self)
+
+    def iter_keys_timeout(self):
+        """
+        Iterate over timed out keys (ie. nodes) for a specific worker.
+        """
+        self._task_bound_check()
+        return self.task._iter_keys_timeout_by_worker(self)
+
+class WorkerSimple(EngineClient, Worker):
+    """
+    Implements a simple Worker being itself an EngineClient.
+    """
+
+    def __init__(self, file_reader, file_writer, file_error, key, handler,
+            stderr=False, timeout=-1, autoclose=False):
+        """
+        Initialize worker.
+        """
+        Worker.__init__(self, handler)
+        EngineClient.__init__(self, self, stderr, timeout, autoclose)
+
+        self._last_msg = None
+        self._last_errmsg = None
+
+        if key is None: # allow key=0
+            self.key = self
+        else:
+            self.key = key
+        self.file_reader = file_reader
+        self.file_writer = file_writer
+        self.file_error = file_error
+
+    def _engine_clients(self):
+        """
+        Return a list of underlying engine clients.
+        """
+        return [self]
+
+    def set_key(self, key):
+        """
+        Source key for this worker is free for use. Use this method to
+        set the custom source key for this worker.
+        """
+        self.key = key
+
+    def _start(self):
+        """
+        Start worker.
+        """
+        self._invoke("ev_start")
+
+        return self
+
+    def error_fileno(self):
+        """
+        Returns the standard error reader file descriptor as an integer.
+        """
+        if self.file_error and not self.file_error.closed:
+            return self.file_error.fileno()
+
+        return None
+
+    def reader_fileno(self):
+        """
+        Returns the reader file descriptor as an integer.
+        """
+        if self.file_reader and not self.file_reader.closed:
+            return self.file_reader.fileno()
+
+        return None
+    
+    def writer_fileno(self):
+        """
+        Returns the writer file descriptor as an integer.
+        """
+        if self.file_writer and not self.file_writer.closed:
+            return self.file_writer.fileno()
+        
+        return None
+    
+    def _read(self, size=4096):
+        """
+        Read data from process.
+        """
+        return EngineClient._read(self, size)
+
+    def _readerr(self, size=4096):
+        """
+        Read error data from process.
+        """
+        return EngineClient._readerr(self, size)
+
+    def _close(self, abort, flush, timeout):
+        """
+        Close client. See EngineClient._close().
+        """
+        if flush and self._rbuf:
+            # We still have some read data available in buffer, but no
+            # EOL. Generate a final message before closing.
+            self.worker._on_msgline(self._rbuf)
+
+        if self.file_reader != None:
+            self.file_reader.close()
+        if self.file_writer != None:
+            self.file_writer.close()
+        if self.file_error != None:
+            self.file_error.close()
+
+        if timeout:
+            assert abort, "abort flag not set on timeout"
+            self._on_timeout()
+
+        self._invoke("ev_close")
+
+    def _handle_read(self):
+        """
+        Engine is telling us there is data available for reading.
+        """
+        debug = self.task.info("debug", False)
+        if debug:
+            print_debug = self.task.info("print_debug")
+
+        for msg in self._readlines():
+            if debug:
+                print_debug(self.task, "LINE %s" % msg)
+            self._on_msgline(msg)
+
+    def _handle_error(self):
+        """
+        Engine is telling us there is error available for reading.
+        """
+        debug = self.task.info("debug", False)
+        if debug:
+            print_debug = self.task.info("print_debug")
+
+        for msg in self._readerrlines():
+            if debug:
+                print_debug(self.task, "LINE@STDERR %s" % msg)
+            self._on_errmsgline(msg)
+
+    def last_read(self):
+        """
+        Read last msg, useful in an EventHandler.
+        """
+        return self._last_msg
+
+    def last_error(self):
+        """
+        Get last error message from event handler.
+        """
+        return self._last_errmsg
+
+    def _on_msgline(self, msg):
+        """
+        Add a message.
+        """
+        # add last msg to local buffer
+        self._last_msg = msg
+
+        # update task
+        self.task._msg_add((self, self.key), msg)
+
+        self._invoke("ev_read")
+
+    def _on_errmsgline(self, msg):
+        """
+        Add a message.
+        """
+        # add last msg to local buffer
+        self._last_errmsg = msg
+
+        # update task
+        self.task._errmsg_add((self, self.key), msg)
+
+        self._invoke("ev_error")
+
+    def _on_timeout(self):
+        """
+        Update on timeout.
+        """
+        self.task._timeout_add((self, self.key))
+
+        # trigger timeout event
+        self._invoke("ev_timeout")
+
+    def read(self):
+        """
+        Read worker buffer.
+        """
+        self._task_bound_check()
+        for key, msg in self.task._call_tree_matcher(self.task._msgtree.items,
+                                                     worker=self):
+            assert key == self.key
+            return str(msg)
+
+    def error(self):
+        """
+        Read worker error buffer.
+        """
+        self._task_bound_check()
+        for key, msg in self.task._call_tree_matcher(self.task._errtree.items,
+                                                     worker=self):
+            assert key == self.key
+            return str(msg)
+
+    def write(self, buf):
+        """
+        Write to worker.
+        """
+        self._write(buf)
+
+    def set_write_eof(self):
+        """
+        Tell worker to close its writer file descriptor once flushed. Do not
+        perform writes after this call.
+        """
+        self._set_write_eof()
+
