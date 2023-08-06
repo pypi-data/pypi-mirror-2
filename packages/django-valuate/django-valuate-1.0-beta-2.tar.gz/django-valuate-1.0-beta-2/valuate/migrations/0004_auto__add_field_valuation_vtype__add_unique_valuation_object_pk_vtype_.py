dc67f41b6a3f0c5e7a68905f40760074b101d8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:13 +0000

    Add package django-valuate/1.0-beta-2/django-valuate-1.0-beta-2.tar.gz

---
+++ packages/django-valuate/django-valuate-1.0-beta-2.tar.gz/django-valuate-1.0-beta-2/valuate/migrations/0004_auto__add_field_valuation_vtype__add_unique_valuation_object_pk_vtype_.py
@@ -0,0 +1,103 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Valuation.vtype'
+        db.add_column('valuate_valuation', 'vtype', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['valuate.ValuationType']), keep_default=False)
+
+        # Adding unique constraint on 'Valuation', fields ['object_pk', 'vtype', 'user', 'content_type']
+        db.create_unique('valuate_valuation', ['object_pk', 'vtype_id', 'user_id', 'content_type_id'])
+
+        # Adding unique constraint on 'Valuation', fields ['vtype', 'object_pk', 'session', 'content_type']
+        db.create_unique('valuate_valuation', ['vtype_id', 'object_pk', 'session', 'content_type_id'])
+
+
+    def backwards(self, orm):
+        
+        # Removing unique constraint on 'Valuation', fields ['vtype', 'object_pk', 'session', 'content_type']
+        db.delete_unique('valuate_valuation', ['vtype_id', 'object_pk', 'session', 'content_type_id'])
+
+        # Removing unique constraint on 'Valuation', fields ['object_pk', 'vtype', 'user', 'content_type']
+        db.delete_unique('valuate_valuation', ['object_pk', 'vtype_id', 'user_id', 'content_type_id'])
+
+        # Deleting field 'Valuation.vtype'
+        db.delete_column('valuate_valuation', 'vtype_id')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'valuate.valuation': {
+            'Meta': {'unique_together': "(('user', 'content_type', 'object_pk', 'vtype'), ('session', 'content_type', 'object_pk', 'vtype'))", 'object_name': 'Valuation'},
+            'choice': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['valuate.ValuationChoice']"}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'content_type_set_for_valuation'", 'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.IPAddressField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            'object_pk': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'session': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['sites.Site']"}),
+            'submit_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'valuation'", 'null': 'True', 'to': "orm['auth.User']"}),
+            'vtype': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['valuate.ValuationType']"})
+        },
+        'valuate.valuationchoice': {
+            'Meta': {'object_name': 'ValuationChoice'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'value': ('django.db.models.fields.IntegerField', [], {}),
+            'vtype': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['valuate.ValuationType']"})
+        },
+        'valuate.valuationtype': {
+            'Meta': {'object_name': 'ValuationType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['valuate']
