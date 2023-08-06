77b28da35feb817647c58fdae393164bb202170c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:37 +0000

    Add package Ver/1.0.1/Ver-1.0.1.zip

---
+++ packages/Ver/Ver-1.0.1.zip/Ver-1.0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Ver',
+      version='1.0.1',
+      description='List available packages verions',
+      author='Alex Movsisyan',
+      url='http://www.python.org/',
+      py_modules=['ver']
+     )
