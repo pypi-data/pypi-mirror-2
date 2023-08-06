c1fc2b04033868ecae3054f0ca4341e1c4239063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:04 +0000

    Add package stalker/0.1.2.a3/stalker-0.1.2.a3-py2.7.egg

---
+++ packages/stalker/stalker-0.1.2.a3-py2.7.egg/stalker/__init__.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+"""
+Stalker is a Production Asset Management System (ProdAM) designed for animation
+and vfx studios. See docs for more information.
+"""
+
+
+
+__version__ = "0.1.2.a3"
+
