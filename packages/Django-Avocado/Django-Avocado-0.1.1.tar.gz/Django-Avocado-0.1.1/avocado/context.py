18fe6948a2db398b17fe10d2a3fb9fe3726400fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:22 +0000

    Add package Django-Avocado/0.1.1/Django-Avocado-0.1.1.tar.gz

---
+++ packages/Django-Avocado/Django-Avocado-0.1.1.tar.gz/Django-Avocado-0.1.1/avocado/context.py
@@ -0,0 +1,110 @@
+import sys
+import time
+import traceback
+import cStringIO
+import datetime
+from django.contrib.contenttypes.models import ContentType
+
+from models import *
+
+import dse
+dse.patch_models()
+
+def singleton(cls, context_text):
+    instances = {}
+
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls(context_text)
+        return instances[cls]
+    return getinstance
+
+
+class DelayedLoggingContext(object):
+
+    def __init__(self, context_text):
+        self.context_text = context_text
+        self.items = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.flush()
+
+    def flush(self):
+        with LogEntry.dse as d:
+            for item in self.items:
+                d.add_item(item)
+
+    def log(self, loglevel, text, user=None, instance=None):
+        item = dict(loglevel=loglevel, text=text, date_added=datetime.datetime.today(), user=user)
+        if instance:
+            item['content_type_id'] = ContentType.objects.get_for_model(instance.__class__).pk
+            item['object_id'] = instance.pk
+
+        if len(self.items) > 1000:            
+            self.flush()
+
+        self.items.append(item)
+
+    def debug(self, text, user=None, instance=None):
+        self.log('debug', text, user=user, instance=instance)
+
+    def info(self, text, user=None, instance=None):
+        self.log('info', text, user=user, instance=instance)
+
+    def warning(self, text, user=None, instance=None):
+        self.log('warning', text, user=user, instance=instance)
+
+    def critical(self, text, user=None, instance=None):
+        self.log('critical', text, user=user, instance=instance)
+
+    def error(self, text, user=None, instance=None):
+        self.log('error', text, user=user, instance=instance)
+
+    def exception(self, text, user=None, instance=None):
+        try:
+            exception_data = exc_plus() 
+        except:
+            exception_data = ''
+        self.log('exception', '%s\n\n%s' % (text, exception_data), user=user, instance=instance)
+
+
+def get_context(context_text):
+    return DelayedLoggingContext(context_text)
+
+
+def get_singleton_context(context_text):
+    if not hasattr(DelayedLoggingContext, 'singleton'):
+        setattr(DelayedLoggingContext, 'singleton', singleton(DelayedLoggingContext, context_text)())
+    return DelayedLoggingContext.singleton
+
+
+def exc_plus():
+    "Based on the recipie in Python Cookbook, #14.4"
+    tb = sys.exc_info()[2]
+    while 1:
+        if not tb.tb_next:
+            break
+        tb = tb.tb_next
+    stack = []
+    f = tb.tb_frame
+    while f:
+        stack.append(f)
+        f = f.f_back
+    stack.reverse()
+    traceback.print_exc()
+    result = []
+    for frame in stack:
+        result.append('Frame %s in %s at line %s' % (
+            frame.f_code.co_name,
+            frame.f_code.co_filename,
+            frame.f_lineno)
+                      )
+        for key, value in frame.f_locals.items():
+            try:
+                result.append('%s = %s' % (key, value))
+            except:
+                pass
+    return '\n'.join(result)
---
+++ packages/Django-Avocado/Django-Avocado-0.1.1.tar.gz/Django-Avocado-0.1.1/avocado/tests.py
@@ -0,0 +1,76 @@
+from django.utils import unittest
+from django.test.utils import setup_test_environment, teardown_test_environment
+from django.contrib.auth.models import User
+
+from avocado.context import *
+from avocado.models import *
+
+
+class BasicTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.u = User.objects.create(username='test', first_name='t', last_name='t', email='t@t.com', password='t')
+
+    def tearDown(self):
+        User.objects.all().delete()
+        LogEntry.objects.all().delete()
+
+    def test_basic(self):
+        with get_context("my context") as log:
+            log.debug("debug", instance=self.u)
+        self.assertTrue(LogEntry.objects.all().count() == 1)
+
+    def test_basic_many(self):
+        with get_context("my context") as log:
+            for i in range(0, 100):
+                log.debug("debug %s" % i, instance=self.u)
+            self.assertTrue(LogEntry.objects.all().count() == 0)
+        self.assertTrue(LogEntry.objects.all().count() == 100)
+
+    def test_get_context_debug(self):
+        with get_context("my context") as log:
+            log.debug("debug")
+
+    def test_get_context_info(self):
+        with get_context("my context") as log:
+            log.info("info")
+
+    def test_get_context_critical(self):
+        with get_context("my context") as log:
+            log.critical("critical")
+
+    def test_get_context_warning(self):
+        with get_context("my context") as log:
+            log.warning("warning")
+
+    def test_get_context_error(self):
+        with get_context("my context") as log:
+            log.error("error")
+
+    def test_get_context_exception(self):
+        with get_context("my context") as log:
+            log.exception("exception", str(Exception()), instance=self.u)
+
+    def test_get_singleton_context(self):
+        ctx1 = get_singleton_context("my context")
+        ctx2 = get_singleton_context("my context")
+        self.assertIs(ctx1,ctx2)
+
+    def test_get_singleton_context_debug(self):
+        ctx1 = get_singleton_context("my context1")
+        ctx2 = get_singleton_context("my context2")
+        ctx1.debug("debug1")
+        ctx2.debug("debug2")
+        self.assertEqual(LogEntry.objects.all().count(), 0)
+        ctx1.flush()
+        self.assertEqual(LogEntry.objects.all().count(), 2)
+
+    def test_exc_plus(self):
+        with get_context("my context") as log:
+            try:
+               a = 1
+               b = 2
+               c = a +b
+               raise Exception("fdsfd")
+            except Exception, e:
+                log.exception("exception", "Caught exception doing some math. Not uncommon.")
