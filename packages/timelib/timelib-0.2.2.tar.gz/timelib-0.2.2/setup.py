38e7b8c95d3e56383e92c308f6293552461808ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:02 +0000

    Add package timelib/0.2.2/timelib-0.2.2.tar.gz

---
+++ packages/timelib/timelib-0.2.2.tar.gz/timelib-0.2.2/setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+import os
+
+try:
+    from setuptools import setup, Extension    
+    extra = dict(zip_safe=False)
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+    extra = dict()
+    
+def read_long_description():
+    fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.txt")
+    return open(fn).read()
+    
+sources = "astro.c dow.c parse_date.c parse_tz.c timelib.c tm2unixtime.c unixtime2tm.c".split()
+sources = ["ext-date-lib/"+x for x in sources]
+sources.append("timelib.c")
+
+setup(name='timelib',
+      version='0.2.2',
+      description="parse english textual date descriptions",
+      author='Ralf Schmitt',
+      author_email='schmir@gmail.com',
+      url="http://github.com/schmir/timelib/tree/master",
+      ext_modules=[Extension("timelib", sources=sources,
+                             define_macros=[('HAVE_STRING_H',1)])],
+      include_dirs=[".",  "ext-date-lib"],
+      long_description=read_long_description(),
+      license="zlib/php",
+      **extra
+     )
