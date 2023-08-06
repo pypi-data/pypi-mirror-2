e87e53b43a09006a171b72c4c11cff485a81d8f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:09 +0000

    Add package Products.FacultyStaffDirectory/3.0/Products.FacultyStaffDirectory-3.0.zip

---
+++ packages/Products.FacultyStaffDirectory/Products.FacultyStaffDirectory-3.0.zip/Products.FacultyStaffDirectory-3.0/Products/FacultyStaffDirectory/utility.py
@@ -0,0 +1,9 @@
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implements
+
+class FSDNonInstallable(object):
+    implements(INonInstallable)
+
+    def getNonInstallableProfiles(self):
+        return [u'Products.FacultyStaffDirectory:plone3-actions-fix',
+                u'Products.FacultyStaffDirectory:uninstall']
