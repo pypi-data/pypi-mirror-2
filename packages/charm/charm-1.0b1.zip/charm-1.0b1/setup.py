404ab80050290354b4040784b4ca380a27095e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:35 +0000

    Add package charm/1.0b1/charm-1.0b1.zip

---
+++ packages/charm/charm-1.0b1.zip/charm-1.0b1/setup.py
@@ -0,0 +1,40 @@
+
+from setuptools import setup, find_packages
+
+name = 'charm'
+description = 'Import static websites on the file system into Plone.'
+version = '1.0b1'
+
+
+def read(file):
+    file = open(file)
+    data = file.read()
+    file.close()
+    return data
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=read('README.txt') + read('docs/HISTORY.txt'),
+    url='https://github.com/aclark4life/charm',
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    entry_points={
+        'zc.buildout': ['default = %s:Recipe' % name],
+    },
+    install_requires=[
+        'BeautifulSoup',
+        'lxml',
+        'xlrd',
+        'zc.buildout',
+    ],
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Buildout',
+        'Framework :: Plone'
+    ],
+    extras_require={
+        'tests': ['zope.testing', 'Plone'],
+    },
+)
