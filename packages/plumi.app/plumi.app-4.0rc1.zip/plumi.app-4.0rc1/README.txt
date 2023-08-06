1b3ce4a00bedc35ebe43719f6667492d4765cf83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:11 +0000

    Add package plumi.app/4.0rc1/plumi.app-4.0rc1.zip

---
+++ packages/plumi.app/plumi.app-4.0rc1.zip/plumi.app-4.0rc1/README.txt
@@ -0,0 +1,10 @@
+Plumi video sharing system 
+
+ - For more info see http://plumi.org
+ - For installation instructions see docs/INSTALL.txt
+ - For migration instructions see docs/MIGRATE.txt
+ - For the Plumi Manual visit http://en.flossmanuals.net/bin/view/Plumi
+ - Code repository: https://svn.plone.org/svn/collective/plumi.app/trunk
+ - Questions and comments to discuss@lists.plumi.org
+ - Report bugs at http://plumi.org/newticket
+
---
+++ packages/plumi.app/plumi.app-4.0rc1.zip/plumi.app-4.0rc1/src/plumi.app/plumi/app/Extensions/Install.py
@@ -0,0 +1,26 @@
+import transaction
+import logging
+from Products.CMFCore.utils import getToolByName
+from plumi.app.install import app_installation_tasks
+
+def install(self, reinstall=False):
+    logger = logging.getLogger('plumi.app')
+    logger.info('starting install')
+    portal_quickinstaller = getToolByName(self, 'portal_quickinstaller')
+    portal_setup = getToolByName(self, 'portal_setup')
+    logger.info('starting extension profiles')
+
+    portal_setup.runAllImportStepsFromProfile('profile-plumi.app:default', purge_old=False)
+    portal_quickinstaller.notifyInstalled('plumi.app')
+    transaction.savepoint()
+
+    #run custom setup code.
+    logger.info('starting custom setup code')
+    app_installation_tasks(self, reinstall)
+    logger.info('end install')
+
+def uninstall(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-plumi.app:uninstall')
+    return "Ran all uninstall steps."
+
