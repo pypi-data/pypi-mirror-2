3f367ddac1cd4beee7af6735e1de873883b03c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:46 +0000

    Add package django-simple-events/0.2/django-simple-events-0.2.tar.gz

---
+++ packages/django-simple-events/django-simple-events-0.2.tar.gz/django-simple-events-0.2/events/management/commands/update_occurrences.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.utils.translation import ugettext as _
+from django.db.models import Q
+
+from events.models import Event
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        q = Q(end__isnull=True)
+        q |= Q(repetitions__isnull=True)
+        for event in Event.objects.filter(q):
+            print(_('Updating occurrences for %s') % event)
+            event.save()
