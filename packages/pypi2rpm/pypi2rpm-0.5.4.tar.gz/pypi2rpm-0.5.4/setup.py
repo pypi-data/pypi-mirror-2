317ba222b2eb7d4ce427d651da3573dc2b6a4aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package pypi2rpm/0.5.4/pypi2rpm-0.5.4.tar.gz

---
+++ packages/pypi2rpm/pypi2rpm-0.5.4.tar.gz/pypi2rpm-0.5.4/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+
+f = open('README')
+try:
+    README = f.read()
+finally:
+    f.close()
+
+
+setup(name='pypi2rpm', version='0.5.4', author='Tarek Ziade',
+      author_email='tarek@ziade.org',
+      description='Script that transforms a sdist archive '
+                  'into a rpm archive',
+      long_description=README,
+      url='http://bitbucket.org/tarek/pypi2rpm',
+      packages=['pypi2rpm', 'pypi2rpm.command'],
+      scripts=['pypi2rpm/pypi2rpm.py'],
+      install_requires=['Distutils2', 'argparse'])
