a8b42c4a280a415dbf0258a0346847730d8b1ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:11 +0000

    Add package xm.charting/0.4/xm.charting-0.4.zip

---
+++ packages/xm.charting/xm.charting-0.4.zip/xm.charting-0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4'
+
+setup(name='xm.charting',
+      version=version,
+      description="For generating gantt charts",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Rocky Burt',
+      author_email='rocky@serverzen.com',
+      url='http://svn.plone.org/svn/collective/xm.charting/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['xm'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.eXtremeManagement',
+      ],
+      test_suite='xm.charting.tests.test_suite',
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
