dd1816dcc8f0a03007e0c845b5852b5cd9c2abf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:20 +0000

    Add package django-stream/0.1/django-stream-0.1.tar.gz

---
+++ packages/django-stream/django-stream-0.1.tar.gz/django-stream-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-stream',
+    description='Application providing news stream like features on django models',
+    packages=find_packages(),
+    author='Alen Mujezinovic',
+    author_email='alen@caffeinehit.com',
+    url='https://github.com/caffeinehit/django-stream',
+    version='0.1',
+)
