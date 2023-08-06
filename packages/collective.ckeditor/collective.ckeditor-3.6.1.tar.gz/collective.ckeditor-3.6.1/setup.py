b7aefc0d4127024d36b616bcd4bb373dec463889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:03 +0000

    Add package collective.ckeditor/3.6.1/collective.ckeditor-3.6.1.tar.gz

---
+++ packages/collective.ckeditor/collective.ckeditor-3.6.1.tar.gz/collective.ckeditor-3.6.1/setup.py
@@ -0,0 +1,37 @@
+
+import os
+from setuptools import setup, find_packages
+
+version = '3.6.1'
+
+setup(name='collective.ckeditor',
+      version=version,
+      description="CKEditor for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n"
+                       + open(os.path.join("docs", "FAQ.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Plone CKeditor WYSIWYG',
+      author='Alterway Solutions',
+      author_email='support@ingeniweb.com',
+      url='https://svn.plone.org/svn/collective/collective.ckeditor',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.quickupload',
+          'collective.plonefinder',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
