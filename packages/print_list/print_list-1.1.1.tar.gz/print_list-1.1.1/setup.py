e8623de7a6bb25940d66bff724ae864beeffdc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:35 +0000

    Add package print_list/1.1.1/print_list-1.1.1.tar.gz

---
+++ packages/print_list/print_list-1.1.1.tar.gz/print_list-1.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+	name		= 'print_list',
+	version 	= '1.1.1',
+	py_modules	= ['print_list'],
+	author		= 'erik',
+	author_email	= 'nindoone@gmail.com',
+	url		= 'undefined',
+	description	= 'Prints lists in lists',
+
+	)
