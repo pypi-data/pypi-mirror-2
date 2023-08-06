40536f5a7f42c65a579123e778a5884c0c7ba4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package Relo/0.5-beta/Relo-0.5-beta.tar.gz

---
+++ packages/Relo/Relo-0.5-beta.tar.gz/Relo-0.5-beta/relo/core/interfaces.py
@@ -0,0 +1,13 @@
+from relo.yapsy.IPlugin import IPlugin
+
+class DocType(IPlugin):
+    """""
+    Implements different type of docs
+    """""
+    name = ""
+    def meta(self):
+        return self.name
+    def load(self, path):
+        pass
+    def search(self, key):
+        pass
