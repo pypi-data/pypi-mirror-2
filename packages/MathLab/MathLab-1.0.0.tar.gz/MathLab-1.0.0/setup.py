af5b92a5ab29a9c50be944e04f3922533a2ee297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:51 +0000

    Add package MathLab/1.0.0/MathLab-1.0.0.tar.gz

---
+++ packages/MathLab/MathLab-1.0.0.tar.gz/MathLab-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 			= 'MathLab',
+	version 		= '1.0.0',
+	py_modules  	= ['MathLab'],
+	author 			= 'torayeff',
+	author_email 	= 'torayeff@gmail.com',
+	url 			= 'www.zehin.biz',
+	description		= 'math algorithms',
+)
