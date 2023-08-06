62ae4a3e3210fcb99f4520898f3709d462db964f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:56 +0000

    Add package django-curator/0.1/django-curator-0.1.tar.gz

---
+++ packages/django-curator/django-curator-0.1.tar.gz/django-curator-0.1/curator/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from curator.forms import DashboardWidgetForm
+from curator.models import Dashboard, DashboardWidget
+
+class DashboardWidgetAdmin(admin.ModelAdmin):
+    form = DashboardWidgetForm
+    list_display = ('model', 'datetime_field', 'dashboard',)
+    
+admin.site.register(Dashboard)
+admin.site.register(DashboardWidget, DashboardWidgetAdmin)
