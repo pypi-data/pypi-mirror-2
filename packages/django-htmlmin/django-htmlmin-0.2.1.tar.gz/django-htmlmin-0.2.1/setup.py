fc9dd6dd66a8d58fd2b10041563eade630abba8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:52 +0000

    Add package django-htmlmin/0.2.1/django-htmlmin-0.2.1.tar.gz

---
+++ packages/django-htmlmin/django-htmlmin-0.2.1.tar.gz/django-htmlmin-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from htmlmin import __version__
+
+README = open('README.rst').read()
+
+setup(name='django-htmlmin',
+      version=__version__,
+      description='html minify for django',
+      long_description=README,
+      author='CobraTeam',
+      author_email='andrewsmedina@gmail.com',
+      packages=find_packages(),
+      include_package_data=True,
+      test_suite='nose.collector',
+      install_requires=['django', 'BeautifulSoup'],
+      tests_require=['nose', 'coverage'],
+      )
