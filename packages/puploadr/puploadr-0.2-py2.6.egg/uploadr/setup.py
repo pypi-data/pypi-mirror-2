0c95f99ff01c8756c87755e775ac3442cc00d1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package puploadr/0.2/puploadr-0.2-py2.6.egg

---
+++ packages/puploadr/puploadr-0.2-py2.6.egg/uploadr/setup.py
@@ -0,0 +1,27 @@
+__author__="elek"
+__date__ ="$Oct 15, 2010 9:32:10 PM$"
+
+from setuptools import setup,find_packages
+
+setup (
+  name = 'uploader.py',
+  version = '0.1',
+  packages = find_packages(),
+
+  # Declare your packages' dependencies here, for eg:
+  install_requires=['foo>=3'],
+
+  # Fill in these to make your Egg ready for upload to
+  # PyPI
+  author = 'elek',
+  author_email = '',
+
+  summary = 'Just another Python package for the cheese shop',
+  url = '',
+  license = '',
+  long_description= 'Long description of the package',
+
+  # could also include long_description, download_url, classifiers, etc.
+
+  
+)
