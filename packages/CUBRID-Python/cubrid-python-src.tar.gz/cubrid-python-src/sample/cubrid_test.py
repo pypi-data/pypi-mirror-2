4bae8a0916af33b3c3ab9d0485a4f3993aff5831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:46 +0000

    Add package CUBRID-Python/0.5.2/cubrid-python-src.tar.gz

---
+++ packages/CUBRID-Python/cubrid-python-src.tar.gz/cubrid-python-src/sample/cubrid_test.py
@@ -0,0 +1,13 @@
+import cubriddb
+ 
+con = cubriddb.connect('localhost', 30000, 'demodb')
+c = con.cursor()
+ 
+c.execute('select * from code')
+rows = c.fetchall()
+ 
+for r in rows:
+	print r
+
+c.close()
+con.close()
