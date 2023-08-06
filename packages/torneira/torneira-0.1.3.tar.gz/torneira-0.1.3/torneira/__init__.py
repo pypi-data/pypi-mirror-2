a87d63f68c52cbf9427284676c16c93b76ba69b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package torneira/0.1.3/torneira-0.1.3.tar.gz

---
+++ packages/torneira/torneira-0.1.3.tar.gz/torneira-0.1.3/torneira/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+ 
+# Copyright Marcel Nicolay <marcel.nicolay@gmail.com>
+ 
+# Licensed under the Open Software License ("OSL") v. 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+ 
+#     http://www.opensource.org/licenses/osl-3.0.php
+ 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = '0.1.3'
+
+import logging
+
+try:
+    import settings_local as settings
+    logging.debug("Using settings_local.py as settings")
+except ImportError, ie:
+    try:
+        import settings
+    except ImportError, ie:
+        class settings(object):
+            DEBUG = True
+            PROFILING = False
+            COOKIE_SECRET = "29NbhyfgaA092ZkjMbNvCx06789jdA8iIlLqz7d1D9c8"
+        
+settings = settings
---
+++ packages/torneira/torneira-0.1.3.tar.gz/torneira-0.1.3/torneira/core/dispatcher.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Open Software License ("OSL") v. 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.opensource.org/licenses/osl-3.0.php
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from routes import Mapper
+from torneira.core import Singleton
+import logging
+
+from torneira import settings
+
+class TorneiraDispatcher(Singleton):
+
+    __mapper__ = None
+    __controllers__ = None
+    __urls__ = None
+
+    def getUrls(self):
+        if not self.__urls__:
+            exec("from %s import urls" % settings.ROOT_URLS)
+            self.__urls__ = urls
+        return self.__urls__
+
+    def getMapper(self):
+        if not self.__mapper__:
+            mapper = Mapper()
+            for name, route, controller, action in self.getUrls():
+                mapper.connect(name, route, controller=controller, action=action)
+            self.__mapper__ = mapper
+        return self.__mapper__
+
+    def getController(self, controller):
+        if not self.__controllers__:
+            self.__controllers__ = {}
+
+        ctrl_name = controller.__name__
+
+        if not ctrl_name in self.__controllers__:
+            self.__controllers__[ctrl_name] = controller()
+
+        return self.__controllers__[ctrl_name]
+
+
+def url(route=None, controller=None, action=None, name=None):
+    return [name, route, controller, action]
