d02c30dc7ca1ea2e2421756a670ea8f53759e8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:24 +0000

    Add package django-redis-cache/0.5.1/django-redis-cache-0.5.1.tar.gz

---
+++ packages/django-redis-cache/django-redis-cache-0.5.1.tar.gz/django-redis-cache-0.5.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = "django-redis-cache",
+    url = "http://github.com/sebleier/django-redis-cache/",
+    author = "Sean Bleier",
+    author_email = "sebleier@gmail.com",
+    version = "0.5.1",
+    packages = ["redis_cache"],
+    description = "Redis Cache Backend for Django",
+    install_requires=['redis',],
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
