8cc6a40ab7d53169ceefcd71b29ca076a3e792e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:01 +0000

    Add package django_navigation_manager/0.0.5dev/django_navigation_manager-0.0.5dev.tar.gz

---
+++ packages/django_navigation_manager/django_navigation_manager-0.0.5dev.tar.gz/django_navigation_manager-0.0.5dev/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages, setup
+
+setup(
+	name='django_navigation_manager',
+	version='0.0.5dev',
+	maintainer = "Zenobius Jiricek",
+	maintainer_email = "airtonix@gmail.com",
+	url="projects.airtonix.net/project/django-navigation-manager",
+	description = "Django application that helps other applications declare navigation bar items.",
+	license='LICENSE.md',
+	packages=find_packages(exclude='tests'),
+	include_package_data = True
+)
+
