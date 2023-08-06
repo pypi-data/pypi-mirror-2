096efedb8d249bbde12cc49f435b81b65a60dbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:04 +0000

    Add package http-parser/0.2.0/http-parser-0.2.0.tar.gz

---
+++ packages/http-parser/http-parser-0.2.0.tar.gz/http-parser-0.2.0/examples/httpstream.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import socket
+
+from http_parser.http import HttpStream
+from http_parser.reader import SocketReader
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect(('gunicorn.org', 80))
+        s.send("GET / HTTP/1.1\r\nHost: gunicorn.org\r\n\r\n")
+        p = HttpStream(SocketReader(s))
+        print p.headers()
+        print p.body_file().read()
+    finally:
+        s.close()
+
+if __name__ == "__main__":
+    main()
+
+
