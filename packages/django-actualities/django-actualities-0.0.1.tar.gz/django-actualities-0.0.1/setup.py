6ad241262bdc4a78a97591a49b20a3b4c00cbb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:47 +0000

    Add package django-actualities/0.0.1/django-actualities-0.0.1.tar.gz

---
+++ packages/django-actualities/django-actualities-0.0.1.tar.gz/django-actualities-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-actualities",
+    version = "0.0.1",
+    description = """
+    A django blog app
+    """,
+    author = "Platypus Creation",
+    author_email = "contact@platypus-creation.com",
+    url = "https://github.com/platypus-creation/django-actualities",
+    packages = find_packages(),
+    include_package_data=True,
+    install_requires = [
+        'django-taggit==0.9.3',
+    ],
+)
