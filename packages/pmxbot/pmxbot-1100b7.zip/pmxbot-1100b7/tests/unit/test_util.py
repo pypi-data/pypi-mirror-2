caa679246708488ddc497e4390d0b3e2da52b570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:23 +0000

    Add package pmxbot/1100b7/pmxbot-1100b7.zip

---
+++ packages/pmxbot/pmxbot-1100b7.zip/pmxbot-1100b7/tests/unit/test_util.py
@@ -0,0 +1,32 @@
+import py.test
+from pmxbot.util import *
+
+def test_MongoDBKarma(mongodb_uri):
+	k = Karma.from_URI(mongodb_uri)
+	k.db = k.db.database.connection[k.db.database.name+'_test'][k.db.name]
+	k.db.drop()
+	try:
+		k.change('foo', 1)
+		k.change('bar', 1)
+		k.set('baz', 3)
+		k.set('baz', 2)
+		k.link('foo', 'bar')
+		assert k.lookup('foo') == 2
+		k.link('foo', 'baz')
+		assert k.lookup('baz') == k.lookup('foo') == 4
+	finally:
+		k.db.drop()
+
+def test_MongoDBQuotes(mongodb_uri):
+	q = Quotes.from_URI(mongodb_uri)
+	q.lib = 'test'
+	clean = lambda: q.db.remove({'library': 'test'})
+	clean()
+	try:
+		q.quoteAdd('who would ever say such a thing')
+		q.quoteAdd('go ahead, take my pay')
+		q.quoteAdd("let's do the Time Warp again")
+		q.quoteLookup('time warp')
+		q.quoteLookup('nonexistent')
+	finally:
+		clean()
