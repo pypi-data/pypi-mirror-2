6797daa4461c534db513b53a8dd421ff2dc038ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:02 +0000

    Add package dolmen.app.content/1.0b2/dolmen.app.content-1.0b2.tar.gz

---
+++ packages/dolmen.app.content/dolmen.app.content-1.0b2.tar.gz/dolmen.app.content-1.0b2/src/dolmen/app/content/__init__.py
@@ -0,0 +1,4 @@
+from dolmen.app.content.directives import icon
+from dolmen.app.content.interfaces import IDescriptiveSchema
+from dolmen.app.content.thumbnails import BlobMiniaturizer, SquareThumbnailer
+from dolmen.app.content.indexes import *
