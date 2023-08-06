4aefa567f1eadaa29b294cabb57dae6728273f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:13 +0000

    Add package combino/0.0.1/combino-0.0.1.tar.gz

---
+++ packages/combino/combino-0.0.1.tar.gz/combino-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+	name = 'combino',
+	packages = ['combino'],
+	version = '0.0.1',
+	license = 'GPL',
+	description = 'Brute-Froce Worldlist Generator',
+	author = 'v3n0m3x',
+	author_email = 'v3n0m3x@itmix.org',
+	url = 'http://itmix.org/libraries/combino/',
+	download_url = '',
+	keywords = ['generate', 'wordlist', 'brute-force'],
+	classifiers = [
+		'Programming Language :: Python',
+		'Operating System :: OS Independent',
+		'Development Status :: 1 - Planning',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Environment :: Console'
+		],
+	long_description = """\
+Brute-Force Wordlist Generator
+------------------------------------------------------------
+
+Detects Charsets:
+
+-Non yet to be implemented
+
+Maximum Length = [Not yet implemented]
+
+This version requires Python 2; a Python 3 version is not available, but might be offered in the future.
+"""
+)
