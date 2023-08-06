a01426342bf525e390bed56ce155f70b501bba00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:54 +0000

    Add package neo4py/0.1/neo4py-0.1.tar.gz

---
+++ packages/neo4py/neo4py-0.1.tar.gz/neo4py-0.1/neo4py/__init__.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2011 "Aaron Moffatt"
+# aaronmoffatt.com
+# 
+# This file is part of Neo4py.
+# 
+# Neo4py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+__license__ = "AGPL 3"
+__version__ = '0.1'
+__author__ = 'Aaron Moffatt'
+__author_email__ = 'contact@aaronmoffatt.com'
+__description__ = "Python bindings for Neo4j graph database"
+__url__ = "https://github.com/OneSaidWho/neo4py"
+__status__ = "Development"
