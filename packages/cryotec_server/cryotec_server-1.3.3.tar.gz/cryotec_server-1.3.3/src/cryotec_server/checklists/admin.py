be74f0fdfc7426f4c83dd139629c2bef36888255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:01 +0000

    Add package cryotec_server/1.3.3/cryotec_server-1.3.3.tar.gz

---
+++ packages/cryotec_server/cryotec_server-1.3.3.tar.gz/cryotec_server-1.3.3/src/cryotec_server/checklists/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from checklists.models import ChecklistAnswer, ChecklistQuestion
+from libs.admin import LinkedInline
+from checklists.forms import ChecklistAnswerInlineForm, ChecklistAnswerInlineFormset
+
+
+#admin.site.register(ChecklistAnswer)
+admin.site.register(ChecklistQuestion)
+
+
+
+class ChecklistAnswerInline(LinkedInline):
+    model = ChecklistAnswer
+    form = ChecklistAnswerInlineForm
+    formset = ChecklistAnswerInlineFormset
