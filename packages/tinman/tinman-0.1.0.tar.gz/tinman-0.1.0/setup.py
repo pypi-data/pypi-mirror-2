73fc4af72c2c7662d38d810de1d0a8e942e2a7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:17 +0000

    Add package tinman/0.1.0/tinman-0.1.0.tar.gz

---
+++ packages/tinman/tinman-0.1.0.tar.gz/tinman-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+version = "0.1.0"
+
+long_description = """\
+Tinman is an take what you need package designed to speed development of
+Tornado applications.  It includes an application wrapper and a toolbox of
+decorators and utilities.
+"""
+setup(name='tinman',
+      version=version,
+      description="Tornado application wrapper and toolbox for \
+Tornado development",
+      long_description=long_description,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+      ],
+      keywords='tornado',
+      author='Gavin M. Roy',
+      author_email='gmr@myyearbook.com',
+      url='http://github.com/gmr/tinman',
+      license='BSD',
+      packages=['tinman'],
+      requires=['ipaddr', 'pyyaml'],
+      entry_points=dict(console_scripts=['tinman=tinman:main']),
+      zip_safe=True)
