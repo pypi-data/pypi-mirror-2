3df852b20c2973ea12c1f9bfc6a1220639eca83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:27 +0000

    Add package pyzmq/2.0.10/pyzmq-2.0.10.zip

---
+++ packages/pyzmq/pyzmq-2.0.10.zip/pyzmq-2.0.10/zmq/__init__.py
@@ -0,0 +1,40 @@
+"""Python bindings for 0MQ."""
+
+#
+#    Copyright (c) 2010 Brian E. Granger
+#
+#    This file is part of pyzmq.
+#
+#    pyzmq is free software; you can redistribute it and/or modify it under
+#    the terms of the Lesser GNU General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    pyzmq is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    Lesser GNU General Public License for more details.
+#
+#    You should have received a copy of the Lesser GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+from zmq.utils import initthreads # initialize threads
+initthreads.init_threads()
+
+from zmq import core, devices
+from zmq.core import *
+
+def get_includes():
+    """Return a list of directories to include for linking against pyzmq with cython."""
+    from os.path import join, dirname
+    base = dirname(__file__)
+    return [ join(base, subdir) for subdir in ('core', 'devices', 'utils')]
+
+
+__all__ = ['get_includes'] + core.__all__
+
