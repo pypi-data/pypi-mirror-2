9e981dd2379c6d17ac017a4ba6525e88c78f2df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:37 +0000

    Add package minitrue/1/minitrue-1.tar.gz

---
+++ packages/minitrue/minitrue-1.tar.gz/minitrue-1/minitrue/utils.py
@@ -0,0 +1,35 @@
+"""
+Generically useful utilities.
+"""
+try: # pragma: no cover
+    from cStringIO import StringIO; StringIO
+except ImportError:
+    from StringIO import StringIO; StringIO
+
+
+class Constructor(object):
+    def __init__(self):
+        self.kw = {}
+
+
+    def kwarg(self, kwargName=None):
+        """
+        Decorator to pass a function as a kwarg on construction.
+        """
+        def decorator(f):
+            name = f.__name__ if kwargName is None else kwargName
+            self.kw[name] = f
+            return f
+        return decorator
+
+
+    def __call__(self):
+        return self.factory(**self.kw)
+
+
+
+def passthrough(f):
+    def callback(result, *a, **kw):
+        f(*a, **kw)
+        return result
+    return callback
