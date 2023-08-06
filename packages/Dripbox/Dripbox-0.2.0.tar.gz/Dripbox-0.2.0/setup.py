b0c5958c9b0054b3634783901ad3a52f3250ab18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:37 +0000

    Add package Dripbox/0.2.0/Dripbox-0.2.0.tar.gz

---
+++ packages/Dripbox/Dripbox-0.2.0.tar.gz/Dripbox-0.2.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Dripbox',
+      version='0.2.0',
+      description='Smart one-way SFTP file synchronization',
+      author='Eric Allen',
+      author_email='eric@hackerengineer.net',
+      maintainer='Eric Allen',
+      maintainer_email='eric@hackerengineer.net',
+      url='http://github.com/epall/dripbox',
+      py_modules=['dripbox'],
+      scripts=['scripts/drip'],
+      requires=['fsevents', 'paramiko'],
+      install_requires=['MacFSEvents', 'paramiko'],
+      license='GPL v2.0'
+     )
+
