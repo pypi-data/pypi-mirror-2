8a5bdeb0cb5cfb1a6c9d2815d8bd7a6b15ef375c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:46 +0000

    Add package UploadView/0.2.7/UploadView-0.2.7.tar.gz

---
+++ packages/UploadView/UploadView-0.2.7.tar.gz/UploadView-0.2.7/uploader/factory.py
@@ -0,0 +1,12 @@
+from dispatcher import Dispatcher
+from paste.httpexceptions import HTTPExceptionHandler
+
+def factory(global_conf, **app_conf):
+    """create a webob view and wrap it in middleware"""
+    keystr = 'uploader.'
+    args = dict([(key.split(keystr, 1)[-1], value)
+                 for key, value in app_conf.items()
+                 if key.startswith(keystr) ])
+    app = Dispatcher(**args)
+    return HTTPExceptionHandler(app)
+    
