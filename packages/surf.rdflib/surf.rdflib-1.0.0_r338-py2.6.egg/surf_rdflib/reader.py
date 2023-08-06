3953d7da2d19f86cee3a647d8366f227110ea07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:33 +0000

    Add package surf.rdflib/1.0.0-r338/surf.rdflib-1.0.0_r338-py2.6.egg

---
+++ packages/surf.rdflib/surf.rdflib-1.0.0_r338-py2.6.egg/surf_rdflib/reader.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2009, Digital Enterprise Research Institute (DERI),
+# NUI Galway
+# All rights reserved.
+
+# author: Cosmin Basca
+# email: cosmin.basca@gmail.com
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer
+#      in the documentation and/or other materials provided with
+#      the distribution.
+#    * Neither the name of DERI nor the
+#      names of its contributors may be used to endorse or promote  
+#      products derived from this software without specific prior
+#      written permission.
+
+# THIS SOFTWARE IS PROVIDED BY DERI ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DERI BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# -*- coding: utf-8 -*-
+__author__ = 'Cosmin Basca'
+
+try:
+    from json import loads
+except Exception, e:
+    from simplejson import loads
+
+from surf.plugin.query_reader import RDFQueryReader
+from surf.rdf import ConjunctiveGraph
+
+class ReaderPlugin(RDFQueryReader):
+    def __init__(self, *args, **kwargs):
+        RDFQueryReader.__init__(self, *args, **kwargs)
+
+        self.__rdflib_store = kwargs.get("rdflib_store", "IOMemory")
+        self.__rdflib_identifier = kwargs.get("rdflib_identifier") 
+        self.__commit_pending_transaction_on_close = \
+            kwargs.get("commit_pending_transaction_on_close", True)
+
+        self.__graph = ConjunctiveGraph(store = self.__rdflib_store,
+                                        identifier = self.__rdflib_identifier)
+
+    rdflib_store = property(lambda self: self.__rdflib_store)
+    rdflib_identifier = property(lambda self: self.__rdflib_identifier)
+    graph = property(lambda self: self.__graph)
+    commit_pending_transaction_on_close = \
+        property(lambda self: self.__commit_pending_transaction_on_close)
+
+    def _to_table(self, result):
+        # Elements in result.selectionF are instances of rdflib.Variable,
+        # rdflib.Variable is subclass of unicode. We convert them to 
+        # unicode here anyway to hide rdflib internals from clients. 
+        vars = [unicode(var) for var in result.selectionF]
+
+        # Convert each row to dict: { var->value, ... }
+        return [dict(zip(vars, row)) for row in result]
+
+    def _ask(self, result):
+        # askAnswer is list with boolean values, we want first value. 
+        return result.askAnswer[0]
+
+    # execute
+    def _execute(self, query):
+        q_string = unicode(query)
+        self.log.debug(q_string)
+        return self.__graph.query(q_string)
+
+    def execute_sparql(self, q_string, format = None):
+        self.log.debug(q_string)
+
+        result = self.__graph.query(q_string)
+        return loads(result.serialize('json'))
+
+    def close(self):
+        self.__graph.close(commit_pending_transaction = self.__commit_pending_transaction_on_close)
+
---
+++ packages/surf.rdflib/surf.rdflib-1.0.0_r338-py2.6.egg/surf_rdflib/writer.py
@@ -0,0 +1,149 @@
+# Copyright (c) 2009, Digital Enterprise Research Institute (DERI),
+# NUI Galway
+# All rights reserved.
+
+# author: Cosmin Basca
+# email: cosmin.basca@gmail.com
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer
+#      in the documentation and/or other materials provided with
+#      the distribution.
+#    * Neither the name of DERI nor the
+#      names of its contributors may be used to endorse or promote  
+#      products derived from this software without specific prior
+#      written permission.
+
+# THIS SOFTWARE IS PROVIDED BY DERI ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DERI BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# -*- coding: utf-8 -*-
+__author__ = 'Cosmin Basca'
+
+import warnings
+
+from surf.plugin.writer import RDFWriter
+from surf.rdf import ConjunctiveGraph
+from reader import ReaderPlugin
+
+class WriterPlugin(RDFWriter):
+    def __init__(self, reader, *args, **kwargs):
+        RDFWriter.__init__(self, reader, *args, **kwargs)
+        if isinstance(self.reader, ReaderPlugin):
+            self.__rdflib_store = self.reader.rdflib_store
+            self.__rdflib_identifier = self.reader.rdflib_identifier
+            self.__commit_pending_transaction_on_close = \
+                self.reader.commit_pending_transaction_on_close
+
+            self.__graph = self.reader.graph
+        else:
+            self.__rdflib_store = kwargs.get("rdflib_store", "IOMemory")
+            self.__rdflib_identifier = kwargs.get("rdflib_identifier") 
+            self.__commit_pending_transaction_on_close = \
+                kwargs.get("commit_pending_transaction_on_close", True)
+                        
+            self.__graph = ConjunctiveGraph(store = self.__rdflib_store, identifier = self.__rdflib_identifier)
+            
+            warnings.warn("Graph is not readable through the reader plugin", 
+                          UserWarning)
+
+    rdflib_store = property(lambda self: self.__rdflib_store)
+    rdflib_identifier = property(lambda self: self.__rdflib_identifier)
+    graph = property(lambda self: self.__graph)
+    commit_pending_transaction_on_close = \
+        property(lambda self: self.__commit_pending_transaction_on_close)
+
+
+    def _save(self, *resources):
+        for resource in resources:
+            s = resource.subject
+            self.__remove(s)
+            for p, objs in resource.rdf_direct.items():
+                for o in objs:
+                    self.__add(s, p, o)
+
+        self.__graph.commit()
+
+    def _update(self, *resources):
+        for resource in resources:
+            s = resource.subject
+            for p in resource.rdf_direct:
+                self.__remove(s, p)
+            for p, objs in resource.rdf_direct.items():
+                for o in objs:
+                    self.__add(s, p, o)
+
+        self.__graph.commit()
+
+    def _remove(self, *resources, **kwargs):
+        inverse = kwargs.get("inverse")
+        for resource in resources:
+            self.__remove(s = resource.subject)
+            if inverse:
+                self.__remove(o = resource.subject)
+
+        self.__graph.commit()
+
+    def _size(self):
+        return len(self.__graph)
+
+    def _add_triple(self, s = None, p = None, o = None, context = None):
+        self.__add(s, p, o, context)
+
+    def _set_triple(self, s = None, p = None, o = None, context = None):
+        self.__remove(s, p, context = context)
+        self.__add(s, p, o, context)
+
+    def _remove_triple(self, s = None, p = None, o = None, context = None):
+        self.__remove(s, p, o, context)
+
+    def __add(self, s = None, p = None, o = None, context = None):
+        self.log.info('ADD: %s, %s, %s, %s' % (s, p, o, context))
+        self.__graph.add((s, p, o))
+
+    def __remove(self, s = None, p = None, o = None, context = None):
+        self.log.info('REM: %s, %s, %s, %s' % (s, p, o, context))
+        self.__graph.remove((s, p, o))
+
+    def index_triples(self, **kwargs):
+        """ Index triples if this functionality is present.  
+        
+        Return `True` if successful.
+        
+        """
+
+        # TODO: can indexing be forced ?
+        return True
+
+    def load_triples(self, source = None, publicID = None, format = "xml", **args):
+        """ Load files (or resources on the web) into the triple-store. """
+        
+        if source:
+            self.__graph.parse(source, publicID = publicID, format = format, **args)
+            return True
+        
+        return False
+
+    def _clear(self, context = None):
+        """ Clear the triple-store. """
+
+        self.__graph.remove((None, None, None))
+
+    def close(self):
+        self.__graph.close(commit_pending_transaction = self.__commit_pending_transaction_on_close)
+
