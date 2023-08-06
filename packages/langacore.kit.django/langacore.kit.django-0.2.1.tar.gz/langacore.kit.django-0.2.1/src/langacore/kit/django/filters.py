045fd1f85e42570d2150251ff601825fa7ddf762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:41 +0000

    Add package langacore.kit.django/0.2.1/langacore.kit.django-0.2.1.tar.gz

---
+++ packages/langacore.kit.django/langacore.kit.django-0.2.1.tar.gz/langacore.kit.django-0.2.1/src/langacore/kit/django/filters.py
@@ -0,0 +1,6 @@
+"""
+A set of common independent filters usable directly in Python code.
+"""
+
+from langacore.kit.django.common.templatetags.converters import numberify, nullify 
+from langacore.kit.django.common.templatetags.strings import strike_empty, slugify, title, transliterate
