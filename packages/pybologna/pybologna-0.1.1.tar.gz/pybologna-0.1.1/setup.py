d56ca65221eb387914bf7c5069f628dd35caf100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:11 +0000

    Add package pybologna/0.1.1/pybologna-0.1.1.tar.gz

---
+++ packages/pybologna/pybologna-0.1.1.tar.gz/pybologna-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "pybologna",
+    version = "0.1.1",
+    py_modules = ["bologna"],
+    author = "rogeriosantosluz",
+    author_email = "rogeriosantosluz@gmail.com",
+    url = "rogeriosantosluz.com",
+    description = "Bologna & Milano text only turn based game originally written in BASIC in the 1980's.",
+)
