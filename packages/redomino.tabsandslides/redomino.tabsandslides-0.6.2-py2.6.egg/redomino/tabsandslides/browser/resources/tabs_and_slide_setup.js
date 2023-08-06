1d3e2094a8a4532566be875c1fa44aeaccda3f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:49 +0000

    Add package redomino.tabsandslides/0.6.2/redomino.tabsandslides-0.6.2-py2.6.egg

---
+++ packages/redomino.tabsandslides/redomino.tabsandslides-0.6.2-py2.6.egg/redomino/tabsandslides/browser/resources/tabs_and_slide_setup.js
@@ -0,0 +1,82 @@
+/*
+*
+* slideshow
+*
+*/
+
+jQuery.fn.init_slideshow = function (){
+    var context = this;
+    var config = tabsandslides.slideshow.config;
+    var slideshow_config = tabsandslides.slideshow.slideshow_config
+
+    jQuery(".slideshow_tabs",context).tabs(".slideshow_panes > div",config).slideshow(slideshow_config);
+    jQuery(".slideshow_tabs",context).each(function (){
+        jQuery(this).data('slideshow').play();
+    });
+
+    return context;
+};
+
+
+/*
+*
+* tabs
+*
+*/
+
+jQuery.fn.init_tabs = function (){
+    var context = this;
+    var config = tabsandslides.tabs.config;
+
+//    jQuery(".tabs",context).tabs("div.panes > div",config);
+    jQuery(".tabs",context).tabs("> .panes > div",config);
+    return context;
+
+};
+
+/*
+*
+* imagetabs
+*
+*/
+
+jQuery.fn.init_image_tabs = function (){
+    var context = this;
+    var config = tabsandslides.image_tabs.config;
+    var config_boxscrollable = tabsandslides.image_tabs.config_boxscrollable;
+
+
+    jQuery(".imagetabs-tabs",context).tabs("> .imagetabs-panes > div",config);
+    jQuery('.imagetabs-tabs',context).boxscrollable(config_boxscrollable);
+    return context;
+
+};
+
+/*
+*
+* gallery
+*
+*/
+
+jQuery.fn.init_gallery = function (){
+    var context = this;
+
+    jQuery('.imagegallery',context).boxscrollable(tabsandslides.gallery.config_boxscrollable);
+    return context;
+
+};
+
+
+/*
+*
+* init
+*
+*/
+
+(function($){
+$(document).ready(function (){
+    $(this).init_slideshow().init_tabs().init_image_tabs().init_gallery();
+});
+})(jQuery);
+
+
