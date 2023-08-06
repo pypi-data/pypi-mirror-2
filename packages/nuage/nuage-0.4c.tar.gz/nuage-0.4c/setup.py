e7a6174e37f3af185c6f4bb058e66eb3506c4886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:45 +0000

    Add package nuage/0.4c/nuage-0.4c.tar.gz

---
+++ packages/nuage/nuage-0.4c.tar.gz/nuage-0.4c/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+
+def get_packages():
+    # setuptools can't do the job :(
+    packages = []
+    for root, dirnames, filenames in os.walk('nuage'):
+        if '__init__.py' in filenames:
+            packages.append(".".join(os.path.split(root)).strip("."))
+
+    return packages
+
+fd = open("README")
+long_description = fd.read()
+fd.close()
+
+setup(
+    name='nuage',
+    version='0.4c',
+    description='django sdk for deployment in nuage infrastructure',
+    author='Jorge Eduardo Cardona',
+    author_email='jorge.cardona@cenuage.com',
+    license="BSD",
+    keywords="cloud django deployment",
+    url="http://pypi.python.org/pypi/nuage/",
+    long_description=long_description,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        ],
+    )
