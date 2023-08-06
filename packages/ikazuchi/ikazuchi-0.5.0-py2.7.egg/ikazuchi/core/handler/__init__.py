01e21b3dd75b442768a7f1f16a639488b7338453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:48 +0000

    Add package ikazuchi/0.5.0/ikazuchi-0.5.0-py2.7.egg

---
+++ packages/ikazuchi/ikazuchi-0.5.0-py2.7.egg/ikazuchi/core/handler/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from audio import AudioHandler
+from base import LanguageHandler, NullHandler
+from text import SingleSentenceHandler
+
+__all__ = [
+    "AudioHandler",
+    "LanguageHandler",
+    "NullHandler",
+    "SingleSentenceHandler",
+]
