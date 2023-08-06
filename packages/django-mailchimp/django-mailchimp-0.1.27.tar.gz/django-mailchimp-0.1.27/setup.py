563252b412bb12b0c2eb19b0a2cec9d49a6474dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:22 +0000

    Add package django-mailchimp/0.1.27/django-mailchimp-0.1.27.tar.gz

---
+++ packages/django-mailchimp/django-mailchimp-0.1.27.tar.gz/django-mailchimp-0.1.27/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+version = __import__('mailchimp').__version__
+
+setup(
+    name = 'django-mailchimp',
+    version = version,
+    description = 'Mailchimp wrapper for Django',
+    author = 'Jonas Obrist',
+    url = 'http://github.com/ojii/django-mailchimp',
+    packages = find_packages(),
+    zip_safe=False,
+    package_data={
+        'mailchimp': [
+            'templates/mailchimp/*.html',
+            'locale/en/LC_MESSAGES/django.po',
+            'locale/en/LC_MESSAGES/django.mo',
+            'locale/de/LC_MESSAGES/django.po',
+            'locale/de/LC_MESSAGES/django.mo',
+        ],
+    },
+)
