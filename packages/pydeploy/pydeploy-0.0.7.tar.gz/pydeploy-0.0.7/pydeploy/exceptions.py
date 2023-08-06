9b152006b12118f12212107234f94aad5b71f785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:13 +0000

    Add package pydeploy/0.0.7/pydeploy-0.0.7.tar.gz

---
+++ packages/pydeploy/pydeploy-0.0.7.tar.gz/pydeploy-0.0.7/pydeploy/exceptions.py
@@ -0,0 +1,11 @@
+class DeploymentException(Exception):
+    pass
+
+class CommandFailed(DeploymentException):
+    def __init__(self, cmd, returncode, output):
+        super(CommandFailed, self).__init__()
+        self.cmd = cmd
+        self.returncode = returncode
+        self.output = output
+    def __repr__(self):
+        return "CommandFailed:\n  command: {!r}\n  returncode:{}\n  output: {!r})".format(self.cmd, self.returncode, self.output)
