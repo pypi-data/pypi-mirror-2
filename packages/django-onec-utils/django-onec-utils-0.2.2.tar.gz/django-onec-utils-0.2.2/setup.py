c776ca17255ffdf6e8a55650ce6f7d8e33aab091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:57 +0000

    Add package django-onec-utils/0.2.2/django-onec-utils-0.2.2.tar.gz

---
+++ packages/django-onec-utils/django-onec-utils-0.2.2.tar.gz/django-onec-utils-0.2.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-onec-utils',
+    version=__import__('onec_utils').__version__,
+    license="BSD",
+
+    install_requires = [],
+
+    description='A collection of misc. tools for One Cardinal\'s work.',
+    long_description=open('README').read(),
+
+    author='Colin Powell',
+    author_email='colin@onecardinal.com',
+
+    url='http://github.com/powellc/django-onec-utils',
+    download_url='http://github.com/powellc/django-onec-utils/downloads',
+
+    include_package_data=True,
+
+    packages=['onec_utils'],
+
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
