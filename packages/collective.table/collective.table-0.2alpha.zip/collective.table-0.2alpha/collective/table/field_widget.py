412ff1afdc5016e336c325465aa331f364363362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:28 +0000

    Add package collective.table/0.2alpha/collective.table-0.2alpha.zip

---
+++ packages/collective.table/collective.table-0.2alpha.zip/collective.table-0.2alpha/collective/table/field_widget.py
@@ -0,0 +1,77 @@
+# To manage expectations, the data table is managed as an archetypes field
+from zope import component, interface
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.exceptions import ObjectFieldException
+from Products.Archetypes.Field import ObjectField
+from Products.Archetypes.Widget import TypesWidget
+from Products.Archetypes.Registry import registerField, registerWidget
+
+from .interfaces import ISource, ITableField
+
+
+class DataTableWidget(TypesWidget):
+    _properties = TypesWidget._properties.copy()
+    _properties.update(dict(
+        macro='@@collective.table.widgets',
+        helper_js=('++resource++collective.table/collective.table.js',),
+        helper_css=('++resource++collective.table/collective.table.css',),
+    ))
+    security = ClassSecurityInfo()
+
+    security.declarePublic('process_form')
+    def process_form(self, instance, field, form, **kw):
+        sourceName = form.get('%s.sourceName' % field.getName())
+        if sourceName is not None:
+            field.sourceName = sourceName
+
+        # This should be handled at the source level, but until we have more
+        # than one, it'll do here.
+        columndefs = form.get('%s.local.columns' % field.getName())
+        if columndefs is not None:
+            columns = []
+            counter = 0
+            for line in columndefs.splitlines():
+                title = line.strip()
+                if not title:
+                    continue
+                columns.append(dict(id='col%02d' % counter, title=title))
+                counter += 1
+            field.getSource(instance).setColumns(columns)
+        return TypesWidget.process_form(self, instance, field, form, **kw)
+
+
+class DataTableField(ObjectField):
+    interface.implements(ITableField)
+    widget = DataTableWidget
+    security = ClassSecurityInfo()
+
+    _properties = ObjectField._properties.copy()
+    _properties.update({
+        'sourceName': 'local',
+    })
+
+    security.declarePrivate('getSource')
+    def getSource(self, instance):
+        return component.getMultiAdapter(
+            (self, instance), ISource, name=self.sourceName)
+
+    # We are not a field in the usual manner, so we need to override some
+    # things to clean things up
+    security.declarePrivate('setStorage')
+    def setStorage(self, instance, storage):
+        raise ObjectFieldException("Not supported, \
+            use field.setSource(name) instead")
+
+    security.declarePrivate('getStorage')
+    def getStorage(self, instance=None):
+        return self.getSource(instance)
+
+
+registerWidget(DataTableWidget,
+    title='Data Table',
+    description='Renders the complex data table and manage it\'s '
+                'configuration',
+    used_for=('collective.table.field_widget.DataTableField',)
+)
+registerField(DataTableField,
+    title='Data Table', description='Complex data table with flexible sources')
---
+++ packages/collective.table/collective.table-0.2alpha.zip/collective.table-0.2alpha/collective/table/interfaces.py
@@ -0,0 +1,53 @@
+from zope import schema
+from Products.Archetypes.interfaces import IObjectField, IStorage
+
+
+class ITableField(IObjectField):
+    """A data table field."""
+
+    def getSource(self, instance):
+        """Return the currently selected data source"""
+
+
+class ISource(IStorage):
+    """Base source adapter interface.
+
+    Concrete subclasses of this interface, registered as named adapters for
+    ITable, are listed on the edit tab as source types to select from.
+
+    """
+
+    title = schema.TextLine(
+        title=u'Title',
+        description=u'Human-readable title of the data source',
+        required=True)
+
+    description = schema.Text(
+        title=u'Description',
+        description=u'Human-readable longer description of the data source',
+        required=True)
+
+    configurationView = schema.ASCIILine(
+        title=u'Configuration View',
+        description=u'The name of the view used to configure this source',
+        required=True)
+
+    manageable = schema.Bool(
+        title=u'Supports manageable features',
+        description=u'True if this source supports manageable features: \
+                      sorting, filtering and pagination',
+        required=True)
+
+    def listColumns():
+        """Return a sequence of dicts specifying the columns
+
+        Each dict must have an id and a title column; ids must be ASCII, but
+        titles are unicode strings.
+
+        The columns will be displayed in the order they are listed.
+
+        """
+
+
+class ILocalSource(ISource):
+    pass
