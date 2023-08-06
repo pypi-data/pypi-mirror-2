103d6ccf28e961c1b342c8a1f317d8dbd3a98a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:50 +0000

    Add package django-guardian/0.2.2/django-guardian-0.2.2.tar.gz

---
+++ packages/django-guardian/django-guardian-0.2.2.tar.gz/django-guardian-0.2.2/guardian/tests/__init__.py
@@ -0,0 +1,8 @@
+from shortcuts_test import *
+from decorators_test import *
+from other_test import *
+from tags_test import *
+from utils_test import *
+from core_test import *
+from custompkmodel_test import *
+
