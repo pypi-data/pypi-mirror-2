2a9a7fafa779656dc7b9a506f04cd710708713b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:56 +0000

    Add package cbstats/1.0.3/cbstats-1.0.3.tar.gz

---
+++ packages/cbstats/cbstats-1.0.3.tar.gz/cbstats-1.0.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+import os
+
+if os.path.exists('README.md'):
+    long_description = open('README.md', encoding = 'UTF-8').read()
+else:
+    long_description = 'See http://github.com/jwandborg/cbstats'
+
+setup(
+    name = 'cbstats',
+    version = '1.0.3',
+    description = 'CraftBukkit Stats',
+    long_description = long_description,
+    author = 'Joar Wandborg',
+    author_email = 'python@wandborg.se',
+    url = 'https://github.com/jwandborg/cbstats',
+    packages = ['cbstats'],
+    classifiers = [
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Games/Entertainment',
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        ]
+    )
