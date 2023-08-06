ecb7b5898b6631593bd18c8c178ce8d11e017e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:34 +0000

    Add package discoball/0.1.0/discoball-0.1.0.tar.gz

---
+++ packages/discoball/discoball-0.1.0.tar.gz/discoball-0.1.0/setup.py
@@ -0,0 +1,24 @@
+try:
+  from setuptools import setup
+  kw = {'entry_points':
+  '''[console_scripts]
+  discoball = discoball:main
+  ''',
+  'zip_safe': False}
+except ImportError:
+  from distutils.core import setup
+  kw = {'scripts': ['scripts/discoball']}
+
+setup(name='discoball',
+      version='0.1.0',
+      description='A simple stream filter to highlight patterns',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        ],
+      author='Kevin Le',
+      author_email='solnovus@gmail.com',
+      py_modules=['discoball'],
+      install_requires=['fabulous','argparse'],
+      **kw)
