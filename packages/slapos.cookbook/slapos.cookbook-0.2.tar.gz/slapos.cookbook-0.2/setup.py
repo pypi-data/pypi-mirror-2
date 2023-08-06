dba44581f930a83245869235c0f21aa91772ce69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:12 +0000

    Add package slapos.cookbook/0.2/slapos.cookbook-0.2.tar.gz

---
+++ packages/slapos.cookbook/slapos.cookbook-0.2.tar.gz/slapos.cookbook-0.2/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+import glob
+import os
+
+version = '0.2'
+name = 'slapos.cookbook'
+long_description = open("README.txt").read() + "\n" + \
+    open("CHANGES.txt").read() + "\n"
+
+for f in sorted(glob.glob(os.path.join('slapos', 'recipe', 'README.*.txt'))):
+  long_description += '\n' + open(f).read() + '\n'
+
+# extras_requires are not used because of
+#   https://bugs.launchpad.net/zc.buildout/+bug/85604
+setup(name=name,
+      version=version,
+      description="SlapOS recipes.",
+      long_description=long_description,
+      classifiers=[
+          "Framework :: Buildout :: Recipe",
+          "Programming Language :: Python",
+        ],
+      keywords='slapos recipe',
+      license='GPLv3',
+      namespace_packages=['slapos', 'slapos.recipe'],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+        'PyXML', # for full blown python interpreter
+        'Zope2', # some recipes like to play with zope
+        'lxml', # for full blown python interpreter
+        'netaddr', # to manipulate on IP addresses
+        'setuptools', # namespaces
+        'slapos.slap', # uses internally
+#        'slapos.toolbox', # needed for libcloud, cloudmgr, disabled for now
+        'xml_marshaller', # need to communication with slapgrid
+        'zc.buildout', # plays with buildout
+        'zc.recipe.egg', # for scripts generation
+        ],
+      zip_safe=True,
+      entry_points={
+        'zc.buildout': [
+          'download = slapos.recipe.download:Recipe',
+          'erp5 = slapos.recipe.erp5:Recipe',
+          'erp5testnode = slapos.recipe.erp5testnode:Recipe',
+          'helloworld = slapos.recipe.helloworld:Recipe',
+          'java = slapos.recipe.java:Recipe',
+          'kvm = slapos.recipe.kvm:Recipe',
+          'libcloud = slapos.recipe.libcloud:Recipe',
+          'libcloudrequest = slapos.recipe.libcloudrequest:Recipe',
+          'nbdserver = slapos.recipe.nbdserver:Recipe',
+          'nosqltestbed = slapos.recipe.nosqltestbed:NoSQLTestBed',
+          'proactive = slapos.recipe.proactive:Recipe',
+          'sheepdogtestbed = slapos.recipe.sheepdogtestbed:SheepDogTestBed',
+          'siptester = slapos.recipe.siptester:SipTesterRecipe',
+          'slaprunner = slapos.recipe.slaprunner:Recipe',
+          'testnode = slapos.recipe.testnode:Recipe',
+          'vifib = slapos.recipe.vifib:Recipe',
+          'xwiki = slapos.recipe.xwiki:Recipe',
+      ]},
+    )
