f24c53bdec684af921ed57e58a345117c6179977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:06 +0000

    Add package nudge/0.6.3/nudge-0.6.3.tar.gz

---
+++ packages/nudge/nudge-0.6.3.tar.gz/nudge-0.6.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2011 Evite LLC
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+#
+# Run "python setup.py bdist_egg" to create an "egg" distribution.
+#
+
+from setuptools import setup
+
+setup(
+    name='nudge',
+    author = "Evite LLC",
+    author_email='opensource@evite.com',
+    description = "Nudge",
+    url='http://evite.github.com/nudge/',
+
+    version='0.6.3',
+
+    packages=['nudge',],
+    install_requires=[
+        'simplejson>=2.1.3',
+    ],
+    
+    zip_safe=False,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+    ],
+)
+
