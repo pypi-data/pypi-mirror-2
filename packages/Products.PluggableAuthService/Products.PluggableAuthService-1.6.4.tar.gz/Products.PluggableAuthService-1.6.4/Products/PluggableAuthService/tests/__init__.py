876afb52985fe27a4ebe920cee8be12d007e3c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:36 +0000

    Add package Products.PluggableAuthService/1.6.4/Products.PluggableAuthService-1.6.4.tar.gz

---
+++ packages/Products.PluggableAuthService/Products.PluggableAuthService-1.6.4.tar.gz/Products.PluggableAuthService-1.6.4/Products/PluggableAuthService/tests/__init__.py
@@ -0,0 +1,18 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Foundation and Contributors
+# Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this
+# distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" PluggableAuthService unit tests
+
+$Id: __init__.py 110542 2010-04-06 03:22:08Z tseaver $
+"""
