1ea93d90f58bdcac0351b40cc7f1a9d80b660a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:12 +0000

    Add package robotframework-databaselibrary/0.2/robotframework-databaselibrary-0.2.tar.gz

---
+++ packages/robotframework-databaselibrary/robotframework-databaselibrary-0.2.tar.gz/robotframework-databaselibrary-0.2/src/DatabaseLibrary/query.py
@@ -0,0 +1,51 @@
+#  Copyright (c) 2010 Franz Allan Valencia See
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+class Query(object):
+    """
+    Query handles all the querying done by the Database Library. 
+    """
+
+    def query(self, selectStatement):
+        """
+        Uses the input `selectStatement` to query for the values that 
+        will be returned as a list of tuples.
+        
+        Tip: Unless you want to log all column values of the specified rows, 
+        try specifying the column names in your select statements 
+        as much as possible to prevent any unnecessary surprises with schema
+        changes and to easily see what your [] indexing is trying to retrieve 
+        (i.e. instead of `"select * from my_table"`, try 
+        `"select id, col_1, col_2 from my_table"`).
+        
+        For example, given we have a table `person` with the following data:
+        | id | first_name  | last_name |
+        |  1 | Franz Allan | See       |
+        
+        When you do the following:
+        | @{queryResults} | Query | select * from person |
+        | Log Many | @{queryResults} |
+        
+        You will get the following:
+        [1, 'Franz Allan', 'See']
+        
+        Also, you can do something like this:
+        | ${queryResults} | Query | select first_name, last_name from person |
+        | Log | ${queryResults[0][1]}, ${queryResults[0][0]} |
+        
+        And get the following
+        See, Franz Allan
+        """
+        cur = self._dbconnection.cursor()
+        cur.execute (selectStatement);
+        return cur.fetchall()
