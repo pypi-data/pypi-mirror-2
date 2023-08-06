c8969c80d67b64b93ee9abb440068a71ebe12e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:10 +0000

    Add package celery-haystack/0.3/celery-haystack-0.3.tar.gz

---
+++ packages/celery-haystack/celery-haystack-0.3.tar.gz/celery-haystack-0.3/celery_haystack/utils.py
@@ -0,0 +1,22 @@
+import sys
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.functional import SimpleLazyObject
+from django.utils.importlib import import_module
+
+
+def get_update_task(task_path=None):
+    import_path = task_path or settings.CELERY_HAYSTACK_DEFAULT_TASK
+    module, attr = import_path.rsplit('.', 1)
+    try:
+        mod = import_module(module)
+    except ImportError, e:
+        raise ImproperlyConfigured('Error importing module %s: "%s"' %
+                                   (module, e))
+    try:
+        Task = getattr(mod, attr)
+    except AttributeError:
+        raise ImproperlyConfigured('Module "%s" does not define a "%s" '
+                                   'class.' % (module, attr))
+    return Task()
