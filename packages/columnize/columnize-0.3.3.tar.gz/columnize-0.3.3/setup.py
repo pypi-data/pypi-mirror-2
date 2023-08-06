6b1a39bc410d7aa6147e6fcdca3b68b6ec148905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package columnize/0.3.3/columnize-0.3.3.tar.gz

---
+++ packages/columnize/columnize-0.3.3.tar.gz/columnize-0.3.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+distutils setup (setup.py)
+
+This gets a bit of package info from __pkginfo__.py file
+"""
+# Get the required package information 
+from __pkginfo__ import \
+    author,           author_email,       classifiers,      ftp_url,      \
+    license,          long_description,   mailing_list,                   \
+    modname,          py_modules,                                         \
+    short_desc,       VERSION, web, zip_safe
+
+__import__('pkg_resources')
+from setuptools import setup
+
+setup(
+      author             = author,
+      author_email       = author_email,
+      classifiers        = classifiers,
+      description        = short_desc,
+      license            = license,
+      py_modules         = py_modules,
+      long_description   = long_description,
+      name               = modname,
+      test_suite         = 'nose.collector',
+      url                = web,
+      version            = VERSION,
+      zip_safe           = zip_safe
+      )
