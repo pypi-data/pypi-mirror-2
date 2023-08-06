bde56b9ba74f4938017ee276075637dee8514c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:20 +0000

    Add package pyf.warehouse/2.0.1/pyf.warehouse-2.0.1.tar.gz

---
+++ packages/pyf.warehouse/pyf.warehouse-2.0.1.tar.gz/pyf.warehouse-2.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0.1'
+
+setup(name='pyf.warehouse',
+      version=version,
+      description="High-level dataflow storage system for PyF framework",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://pyfproject.org',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pyf'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'pyf.splitter'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
