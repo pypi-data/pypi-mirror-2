71aaf84deb14240f7342281e873c25707e364c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package katcp/0.2.7/katcp-0.2.7-py2.5.egg

---
+++ packages/katcp/katcp-0.2.7-py2.5.egg/katcp/__init__.py
@@ -0,0 +1,19 @@
+# __init__.py
+# -*- coding: utf8 -*-
+# vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
+# Copyright 2009 SKA South Africa (http://ska.ac.za/)
+# BSD license - see COPYING for details
+
+"""Root of katcp package.
+   """
+
+from .katcp import Message, KatcpSyntaxError, MessageParser, \
+                  DeviceMetaclass, ExcepthookThread, FailReply, \
+                  AsyncReply, KatcpDeviceError, KatcpClientError, \
+                  Sensor
+
+from .server import DeviceServerBase, DeviceServer, DeviceLogger
+
+from .client import DeviceClient, BlockingClient, CallbackClient
+
+from .version import VERSION, VERSION_STR
