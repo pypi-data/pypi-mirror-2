9b9dec6fdd72e3bb8df19978b450beeec4d2b02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:16 +0000

    Add package celery/2.2.3/celery-2.2.3.tar.bz2

---
+++ packages/celery/celery-2.2.3.tar.bz2/celery-2.2.3/celery/utils/encoding.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def default_encoding():
+    if sys.platform.startswith("java"):
+        return "utf-8"
+    return sys.getfilesystemencoding()
+
+
+def safe_str(s, errors="replace"):
+    encoding = default_encoding()
+    try:
+        if isinstance(s, unicode):
+            return s.encode(encoding, errors)
+        return unicode(s, encoding, errors)
+    except Exception, exc:
+        return "<Unrepresentable %r: %r>" % (type(s), exc)
+
+
+def safe_repr(o, errors="replace"):
+    try:
+        return repr(o)
+    except Exception:
+        return safe_str(o, errors)
