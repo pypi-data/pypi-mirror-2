f03d1a2a64d934a3c11180a54c945c63047399e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:56 +0000

    Add package lupa/0.17/lupa-0.17.tar.gz

---
+++ packages/lupa/lupa-0.17.tar.gz/lupa-0.17/setup.py
@@ -0,0 +1,120 @@
+
+import sys
+import os
+from distutils.core import setup, Extension
+
+VERSION = '0.17'
+
+extra_setup_args = {}
+
+# check if Cython is installed, and use it if available
+try:
+    from Cython.Distutils import build_ext
+    import Cython.Compiler.Version
+    print("building with Cython " + Cython.Compiler.Version.version)
+    extra_setup_args['cmdclass'] = {'build_ext': build_ext}
+    source_extension = ".pyx"
+except ImportError:
+    print("building without Cython")
+    source_extension = ".c"
+
+# support 'test' target if setuptools/distribute is available
+
+if 'setuptools' in sys.modules:
+    extra_setup_args['test_suite'] = 'lupa.tests.suite'
+    extra_setup_args["zip_safe"] = False
+
+# check if LuaJIT is in a subdirectory and build statically against it
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+def find_luajit_build():
+    static_libs = []
+    include_dirs = []
+
+    os_path = os.path
+    for filename in os.listdir(basedir):
+        if filename.lower().startswith('luajit'):
+            filepath = os_path.join(basedir, filename, 'src')
+            if os_path.isdir(filepath):
+                libfile = os_path.join(filepath, 'libluajit.a')
+                if os_path.isfile(libfile):
+                    static_libs = [libfile]
+                    include_dirs = [filepath]
+                    print("found LuaJIT build in %s" % filepath)
+                    print("building statically")
+    return dict(extra_objects=static_libs, include_dirs=include_dirs)
+
+def has_option(name):
+    if name in sys.argv[1:]:
+        sys.argv.remove(name)
+        return True
+    return False
+
+ext_args = find_luajit_build()
+if has_option('--without-assert'):
+    ext_args['define_macros'] = [('PYREX_WITHOUT_ASSERTIONS', None)]
+
+ext_modules = [
+    Extension(
+        'lupa._lupa',
+        sources = ['lupa/_lupa'+source_extension] + (
+            source_extension == '.pyx' and ['lupa/lock.pxi'] or []),
+        **ext_args
+        )
+    ]
+
+def read_file(filename):
+    f = open(os.path.join(basedir, filename))
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+def write_file(filename, content):
+    f = open(os.path.join(basedir, filename), 'w')
+    try:
+        f.write(content)
+    finally:
+        f.close()
+
+long_description = '\n\n'.join([
+    read_file(text_file)
+    for text_file in ['README.txt', 'INSTALL.txt', 'CHANGES.txt']])
+
+write_file(os.path.join('lupa', 'version.py'), "__version__ = '%s'\n" % VERSION)
+
+
+# call distutils
+
+setup(
+    name = "lupa",
+    version = VERSION,
+    author = "Stefan Behnel",
+    contact = "Lupa-dev mailing list",
+    contact_email = "lupa-dev@freelists.org",
+    url = "http://pypi.python.org/pypi/lupa",
+    download_url = "http://pypi.python.org/packages/source/l/lupa/lupa-%s.tar.gz" % VERSION,
+
+    description="Python wrapper around LuaJIT",
+
+    long_description = long_description,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Other Scripting Engines',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+    ],
+
+#    package_dir = {'': 'src'},
+    packages = ['lupa'],
+#    package_data = {},
+    ext_modules = ext_modules,
+    **extra_setup_args
+)
