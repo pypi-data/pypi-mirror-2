2c6ca596a1c0ed0334bdf6dd8babd7ae8639853f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:49 +0000

    Add package rpclib/2.2.2.1-beta/rpclib-2.2.2.1-beta.tar.gz

---
+++ packages/rpclib/rpclib-2.2.2.1-beta.tar.gz/rpclib-2.2.2.1-beta/src/rpclib/test/protocol/xml/test_primitive.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="plq"
+__date__ ="$Aug 28, 2011 3:33:58 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
