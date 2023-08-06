bba69a8e87a43f55d6d2f059e6dc7002b0b0916c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:49 +0000

    Add package cmsplugin-contact/0.9.7/cmsplugin-contact-0.9.7.tar.gz

---
+++ packages/cmsplugin-contact/cmsplugin-contact-0.9.7.tar.gz/cmsplugin-contact-0.9.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmsplugin-contact',
+    version='0.9.7',
+    description='Contact form plugin for Django CMS with spam protection and i18n',
+    long_description=open('README.rst').read(),
+    author='Maccesch',
+    author_email='maccesch@gmail.com',
+    url='http://github.com/maccesch/cmsplugin-contact',
+    packages=find_packages(),
+    keywords='contact form django cms django-cms spam protection email',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
