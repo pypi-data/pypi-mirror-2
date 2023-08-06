02014d5e4efae256b8a491e6195040c68ca34959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:01 +0000

    Add package cubicweb-vcsfile/1.6.6/cubicweb-vcsfile-1.6.6.tar.gz

---
+++ packages/cubicweb-vcsfile/cubicweb-vcsfile-1.6.6.tar.gz/cubicweb-vcsfile-1.6.6/utils.py
@@ -0,0 +1,12 @@
+"""utils for vcsfile
+
+:organization: Logilab
+:copyright: 2007-2010 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+:contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
+"""
+__docformat__ = "restructuredtext en"
+
+import os
+import os.path as osp
+
+from mercurial import ui, commands as hgcmd
