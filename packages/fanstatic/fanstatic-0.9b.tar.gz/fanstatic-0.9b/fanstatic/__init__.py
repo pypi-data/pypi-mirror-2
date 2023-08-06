24cb7a6c2282caa37e919e5c74b82a6959fe2949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:48 +0000

    Add package fanstatic/0.9b/fanstatic-0.9b.tar.gz

---
+++ packages/fanstatic/fanstatic-0.9b.tar.gz/fanstatic-0.9b/fanstatic/__init__.py
@@ -0,0 +1,26 @@
+from fanstatic.core import (Library,
+                            Resource,
+                            GroupResource,
+                            NeededResources,
+                            DEFAULT_SIGNATURE,
+                            sort_resources_topological,
+                            sort_resources)
+
+from fanstatic.registry import get_library_registry, LibraryRegistry
+
+from fanstatic.codegen import generate_code
+
+from fanstatic.core import (init_needed,
+                            get_needed,
+                            clear_needed,
+                            register_inclusion_renderer,
+                            UnknownResourceExtension,
+                            NEEDED)
+
+from fanstatic.injector import Injector, make_injector
+
+from fanstatic.publisher import (Publisher, Delegator, make_publisher,
+                                 DirectoryPublisher)
+
+from fanstatic.wsgi import Fanstatic, make_fanstatic
+
