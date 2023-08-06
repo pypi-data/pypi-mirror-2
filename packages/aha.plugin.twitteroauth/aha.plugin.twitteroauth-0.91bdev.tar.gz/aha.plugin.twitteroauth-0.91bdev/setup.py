b9432e08a2f43ddd4c222d74fe79e310c98b1f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package aha.plugin.twitteroauth/0.91bdev/aha.plugin.twitteroauth-0.91bdev.tar.gz

---
+++ packages/aha.plugin.twitteroauth/aha.plugin.twitteroauth-0.91bdev.tar.gz/aha.plugin.twitteroauth-0.91bdev/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.91b'
+
+setup(name='aha.plugin.twitteroauth',
+      version=version,
+      description="A twitter auth plugin for aha",
+      long_description="""\
+A plugin that ssupplies authentication support of twitter oauth.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='web aha twitter authentication plugin',
+      author='Atsushi Shibata',
+      author_email='shibata@webcore.co.jp',
+      url='http://coreblog.org/aha',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = [
+          'aha',
+      ],
+      packages = [
+        'twitteroauth',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
