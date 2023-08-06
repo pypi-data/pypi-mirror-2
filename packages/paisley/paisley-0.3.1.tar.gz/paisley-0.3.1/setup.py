a3d406efecb043893df03a2f1dab6e6c71b4ceb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:43 +0000

    Add package paisley/0.3.1/paisley-0.3.1.tar.gz

---
+++ packages/paisley/paisley-0.3.1.tar.gz/paisley-0.3.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Copyright (c) 2007-2008
+# See LICENSE for details.
+
+from distutils.core import setup
+
+
+def main():
+    setup(
+        name="paisley",
+        version="0.3.1",
+        description=("Paisley is a CouchDB client written in Python to be used "
+                     "within a Twisted application."),
+        author="Paisley Developers",
+        author_email="",
+        license="MIT",
+        url="http://github.com/smcq/paisley",
+        download_url="http://github.com/smcq/paisley/zipball/v0.3.1",
+        py_modules=["paisley", "paisley.client", "paisley.test_paisley"],
+    )
+
+if __name__ == "__main__":
+    main()
