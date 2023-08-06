7194546b384a080e8759f5e4dc0e6439ab3c2d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:03 +0000

    Add package moa/0.10.2/moa-0.10.2.tar.gz

---
+++ packages/moa/moa-0.10.2.tar.gz/moa-0.10.2/lib/python/moa/plugin/newjob.py
@@ -0,0 +1,102 @@
+# Copyright 2009-2011 Mark Fiers
+# The New Zealand Institute for Plant & Food Research
+# 
+# This file is part of Moa - http://github.com/mfiers/Moa
+# 
+# Licensed under the GPL license (see 'COPYING')
+# 
+"""
+**newjob** - Instantiate new jobs
+---------------------------------
+"""
+
+import os
+
+import optparse
+import moa.job
+import moa.logger as l
+import moa.plugin
+import moa.ui
+from moa.sysConf import sysConf
+
+def defineCommands(data):
+    data['commands']['new'] = {
+        'desc' : "Create a new Moa job",
+        'call' : newJob,
+        'needsJob' : False
+        }
+    
+
+def defineOptions(data):
+    try:
+        parserN = optparse.OptionGroup(data['parser'], "moa new")
+        data['parser'].set_defaults(title="")
+        parserN.add_option("-t", "--title", dest="title", help="Job title")
+        data['parser'].add_option_group(parserN)
+    except optparse.OptionConflictError:
+        pass
+
+def newJob(job):
+    """
+    **moa new**
+
+    Usage::
+
+        moa new TEMPLATE_NAME -t 'a descriptive title'
+        
+    """
+    wd = job.wd
+    options = sysConf['options']
+    args = sysConf['newargs']
+    if not args:
+        moa.ui.exitError("No template specified. Try `moa new TEMPLATENAME`")
+    
+    params = []
+    template = 'empty'
+
+    title = job.conf.title
+    if options.title:
+        title = options.title
+        
+    for a in args:
+        if '=' in a:
+            k,v = a.split('=', 1)
+            if k == 'title':
+                if options.title:
+                    moa.ui.warn("duplicate title defintions, using %s" % v)
+                title = v
+            else:
+                params.append(a)
+
+        else:
+            template = a
+
+    if os.path.exists(os.path.join(
+        wd, '.moa', 'template')) and \
+        not options.force:
+        l.error("Seems that there is already a Moa job in")
+        l.error(wd)
+        l.error("use -f to override")
+        
+    if title:
+        moa.ui.warn("Please define a title for this job")
+        
+    job = moa.job.newJob(wd, template=template, title = options.title)
+    job.conf['title'] = options.title
+    
+    for p in params:
+        k,v = p.split('=', 1)
+        job.conf[k] = v
+
+    job.conf.save()
+
+    moa.ui.fprint("Created a Moa {{green}}{{bold}}%s{{reset}} job" % template,
+                  f='jinja')
+    #moa.ui.fprint('With title "%%(bold)s%s%%(reset)s"' % job.conf.title)
+
+
+
+TESTSCRIPT = """
+moa new adhoc -t 'testJob' adhoc_mode=par dummy=nonsense
+[[ -d ./.moa ]] || exer 'No job was created'
+"""
---
+++ packages/moa/moa-0.10.2.tar.gz/moa-0.10.2/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import os
+#from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup
+# see:
+# http://groups.google.com/group/comp.lang.python/browse_thread/\
+#      thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+with open('VERSION') as F:
+    version = F.read().strip()
+
+scripts  = [os.path.join('bin', x) for x in """
+moa        fastaSplitter  fastaNfinder  fastaInfo  fastaExtract
+fasta2gff  blastReport    blastInfo     blast2gff  moaprompt
+moainit
+""".split()]
+
+data_files = []
+template_data = []
+exclude = ['build', 'sphinx', 'debian', 'dist', 'util']
+for dirpath, dirnames, filenames in os.walk('.'):
+    for dirname in enumerate(dirnames):
+        if dirname[0] == '.': dirnames.remove(dirname)
+        if dirpath == '.' and  dirname in exclude: dirnames.remove(dirname)
+    for filename in filenames:
+        if filename[-1] == '~': filenames.remove(filename)
+    if '__init__.py' in filenames: continue
+    #np = os.path.join('./', dirpath)
+    if dirpath[0] == '/':
+        np = dirpath[1:]
+    elif dirpath[:2] == './':
+        np = dirpath[2:]
+    else: np = dirpath
+    data_files.append([np, [os.path.join(dirpath, f) for f in filenames]])
+
+#import pprint
+#pprint.pprint(data_files)
+#data_files.append(['/etc/bash_completion.d/', ['etc/bash_completion.d/moa']])
+#data_files.append(('/etc/moa', ['etc/config']))
+#data_files.append(('/etc/bash_completion.d/', ['etc/bash_completion.d/moa']))
+setup(name='moa',
+      version=version,
+      description='Moa - lightweight bioinformatics pipelines',
+      author='Mark Fiers',
+      author_email='mark.fiers@plantandfood.co.nz',
+      url='http://mfiers.github.com/Moa/',
+      packages=['moa', 'moa.plugin', 'moa.backend', 'Yaco', 'fist'],
+      package_dir = {'': os.path.join('lib', 'python')},
+      scripts = scripts,
+      data_files = data_files,
+      requires = [
+          'Jinja2 (>2.0)',
+          'biopython (>1.50)',
+          'GitPython (>0.3)',
+          'ruffus',
+          ],
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Unix Shell',
+          'Topic :: Scientific/Engineering :: Bio-Informatics'
+          ]
+     )
