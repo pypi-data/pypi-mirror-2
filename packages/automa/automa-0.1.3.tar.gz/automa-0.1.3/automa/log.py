0be1122ece72bbeb3a71ba1e6f80f7273647dc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:13 +0000

    Add package automa/0.1.3/automa-0.1.3.tar.gz

---
+++ packages/automa/automa-0.1.3.tar.gz/automa-0.1.3/automa/log.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: t -*-
+#
+# Copyright (C) 2010 Marco Pantaleoni. All rights reserved
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 2 as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+__author__    = "Marco Pantaleoni"
+__copyright__ = "Copyright (C) 2010 Marco Pantaleoni"
+__license__   = "GPL v2"
+
+from automa.tasks import TaskContext, get_active_context
+
+def debug(*args, **kwargs):
+    ctxt = TaskContext.Active()
+    if ctxt:
+        return ctxt.debug(*args, **kwargs)
+    return None
+def info(*args, **kwargs):
+    ctxt = TaskContext.Active()
+    if ctxt:
+        return ctxt.info(*args, **kwargs)
+    return None
+def notify(*args, **kwargs):
+    ctxt = TaskContext.Active()
+    if ctxt:
+        return ctxt.notify(*args, **kwargs)
+    return None
+def warn(*args, **kwargs):
+    ctxt = TaskContext.Active()
+    if ctxt:
+        return ctxt.warn(*args, **kwargs)
+    return None
+def error(*args, **kwargs):
+    ctxt = TaskContext.Active()
+    if ctxt:
+        return ctxt.error(*args, **kwargs)
+    return None
+def fatal(*args, **kwargs):
+    ctxt = TaskContext.Active()
+    if ctxt:
+        return ctxt.fatal(*args, **kwargs)
+    return None
