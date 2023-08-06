2c43a18140993276ad6b0b4cee26ed5006d141a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:20 +0000

    Add package camlistore-client/1.0.1dev/camlistore-client-1.0.1dev.tar.gz

---
+++ packages/camlistore-client/camlistore-client-1.0.1dev.tar.gz/camlistore-client-1.0.1dev/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='camlistore-client',
+    version='1.0.1dev',
+    author='Brett Slatkin',
+    author_email='bslatkin@gmail.com',
+    description="Client library for Camlistore.",
+    url='http://camlistore.org',
+    license='Apache v2',
+    long_description='A convience library for python developers wishing to explore camlistore.',
+    packages=['camli'],
+    install_requires=['simplejson'],
+    classifiers=['Environment :: Console', 'Topic :: Internet :: WWW/HTTP']
+)
