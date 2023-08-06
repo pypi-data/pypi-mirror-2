dd39c62c66ea6836c43a640de362e81746458184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:12 +0000

    Add package raptus.multilanguageplone/1.0b5/raptus.multilanguageplone-1.0b5.tar.gz

---
+++ packages/raptus.multilanguageplone/raptus.multilanguageplone-1.0b5.tar.gz/raptus.multilanguageplone-1.0b5/raptus/multilanguageplone/setuphandlers.py
@@ -0,0 +1,59 @@
+from archetypes.schemaextender.interfaces import ISchemaExtender
+
+from raptus.multilanguageplone.extender import folder, document, event, file, image, link, newsitem, topic
+
+from Products.CMFCore.utils import getToolByName
+
+extenders = [folder.FolderExtender,
+             document.DocumentExtender,
+             event.EventExtender,
+             file.FileExtender,
+             image.ImageExtender,
+             link.LinkExtender,
+             newsitem.NewsItemExtender,
+             topic.TopicExtender,]
+
+try:
+    from raptus.multilanguageplone.extender.file import BlobFileExtender, BlobFileModifier
+    extenders.append(BlobFileExtender)
+    extenders.append(BlobFileModifier)
+    from raptus.multilanguageplone.extender.image import BlobImageExtender, BlobImageModifier
+    extenders.append(BlobImageExtender)
+    extenders.append(BlobImageModifier)
+except ImportError:
+    pass
+
+indexes = ('SearchableText', 'Subject', 'Title', 'Description', 'sortable_title',)
+
+def reindex(portal):
+    catalog = getToolByName(portal, 'portal_catalog')
+    for index in indexes:
+        catalog.reindexIndex(index, portal.REQUEST)
+
+def install(context):
+    if context.readDataFile('raptus.multilanguageplone_install.txt') is None:
+        return
+    portal = context.getSite()
+    
+    sm = portal.getSiteManager()
+    for extender in extenders:
+        sm.unregisterAdapter(extender, name='Multilanguage%s' % extender.__name__)
+        sm.registerAdapter(extender, name='Multilanguage%s' % extender.__name__)
+    
+    reindex(portal)
+    
+def uninstall(context):
+    if context.readDataFile('raptus.multilanguageplone_uninstall.txt') is None:
+        return
+    
+    portal = context.getSite()
+    
+    sm = portal.getSiteManager()
+    for extender in extenders:
+        try:
+            sm.unregisterAdapter(extender, name='Multilanguage%s' % extender.__name__)
+        except:
+            pass
+        
+    reindex(portal)
+    
