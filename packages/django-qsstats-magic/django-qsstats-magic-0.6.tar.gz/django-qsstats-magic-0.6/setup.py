4b70b9743d055f0c036216fa34f6fe1dc0de1b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:36 +0000

    Add package django-qsstats-magic/0.6/django-qsstats-magic-0.6.tar.gz

---
+++ packages/django-qsstats-magic/django-qsstats-magic-0.6.tar.gz/django-qsstats-magic-0.6/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+for cmd in ('egg_info', 'develop'):
+    import sys
+    if cmd in sys.argv:
+        from setuptools import setup
+
+setup(
+    name='django-qsstats-magic',
+    version='0.6',
+    description='A django microframework that eases the generation of aggregate data for querysets.',
+    long_description = open('README.rst').read(),
+    author='Matt Croydon, Mikhail Korobov',
+    author_email='mcroydon@gmail.com, kmike84@gmail.com',
+    url='http://bitbucket.org/kmike/django-qsstats-magic/',
+    packages=['qsstats'],
+    requires=['dateutil(>=1.4.1, < 2.0)']
+)
