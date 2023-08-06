971ee430bd40fbf65648467fd092c490ce3d62ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package django-floppyforms/0.4.5/django-floppyforms-0.4.5.tar.gz

---
+++ packages/django-floppyforms/django-floppyforms-0.4.5.tar.gz/django-floppyforms-0.4.5/floppyforms/__init__.py
@@ -0,0 +1,16 @@
+from django.forms import (Form, ModelForm, BaseModelForm, model_to_dict,
+                          fields_for_model, save_instance, ValidationError,
+                          Media, MediaDefiningClass)
+
+from django.contrib.localflavor.generic.forms import (
+                          DEFAULT_DATE_INPUT_FORMATS,
+                          DEFAULT_DATETIME_INPUT_FORMATS,
+)
+
+# Import SelectDateWidget from extras
+from django.forms.extras import SelectDateWidget
+
+from floppyforms.fields import *
+from floppyforms.models import *
+from floppyforms.widgets import *
+from floppyforms import gis
