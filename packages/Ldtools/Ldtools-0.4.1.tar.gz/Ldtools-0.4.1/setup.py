a7ef81515821c42556ba0fcc2213cf33c1ce343b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:02 +0000

    Add package Ldtools/0.4.1/Ldtools-0.4.1.tar.gz

---
+++ packages/Ldtools/Ldtools-0.4.1.tar.gz/Ldtools-0.4.1/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+from ldtools import __version__
+
+setup(
+    name='Ldtools',
+    version=__version__,
+    url='http://github.com/dmr/ldtools/',
+    license='BSD',
+    author='Daniel Rech',
+    author_email='daniel@nwebs.de',
+    description='A lightweight orm for Linked Data',
+    long_description="Ldtools provides a usable API for RDF data. Different backends can either publish local data on the web or use data from the web within your application.",
+    py_modules=['ldtools'],
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+    ],
+    setup_requires = ['rdflib'],
+    test_requires = ['nose'],
+    test_suite = 'nose.collector',
+)
