ca4042d695d08a8a20ddbce522e84a9b556c38b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:10 +0000

    Add package newsletter/0.1.1pre/newsletter-0.1.1pre.tar.gz

---
+++ packages/newsletter/newsletter-0.1.1pre.tar.gz/newsletter-0.1.1pre/newsletter/base/base.py
@@ -0,0 +1,48 @@
+
+"""
+
+* actions
+  - create, update, delete newsletters.
+  - select people to send email to.
+    - everyone on the list.
+    - select categories of people.
+  - unsubscribe through url.  Given an email, and a secret code.
+  - unsubscribe through admin section.  Given an email.
+  - subscription form
+    - 
+  - confirmation email, for double opt in.  Sends them a url with a secret to click on.
+
+"""
+
+
+class Newsletter(object):
+    """
+    """
+
+    def create(self):
+        """
+        """
+
+    def update(self):
+        """
+        """
+
+    def delete(self):
+        """
+        """
+
+    def unsubscribe(self):
+        """
+        """
+
+    def subscribe(self):
+        """
+        """
+
+    def confirm_subscribe(self):
+        """
+        """
+
+
+
+
