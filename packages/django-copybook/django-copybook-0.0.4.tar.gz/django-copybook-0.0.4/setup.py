09c90b89bd41d42433d2da6492da2049d0b3e8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:51 +0000

    Add package django-copybook/0.0.4/django-copybook-0.0.4.tar.gz

---
+++ packages/django-copybook/django-copybook-0.0.4.tar.gz/django-copybook-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+REQUIREMENTS = [
+    'django',
+]
+
+setup(
+    name="django-copybook",
+    version='0.0.4',
+    author="Matthew J. Morrison",
+    author_email="mattj.morrison@gmail.com",
+    description="Convert Django models to/from fixed format records.",
+    long_description=open('README.txt', 'r').read(),
+    packages=("djcopybook",),
+    install_requires=REQUIREMENTS,
+    tests_require=REQUIREMENTS,
+    test_suite='runtests.runtests',
+    zip_safe=False,
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+)
