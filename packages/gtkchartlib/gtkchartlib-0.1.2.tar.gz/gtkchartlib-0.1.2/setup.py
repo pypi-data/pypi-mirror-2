629d322812fcd1ee47eab9e951f693d587e383de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:37 +0000

    Add package gtkchartlib/0.1.2/gtkchartlib-0.1.2.tar.gz

---
+++ packages/gtkchartlib/gtkchartlib-0.1.2.tar.gz/gtkchartlib-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(
+    name='gtkchartlib',
+    version='0.1.2',
+    description='Collection of GTK+ chart widgets',
+    author='Fraser Tweedale',
+    author_email='frase@frase.id.au',
+    url='https://gitorious.org/gtkchartlib',
+    packages=['gtkchartlib'],
+    data_files=[
+        (
+            'doc/gtkchartlib/demo',
+            ['demo/ringchart.py', 'demo/ringchart_single_toplevel.py']
+        ),
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Widget Sets',
+    ],
+    long_description=long_description,
+    obsoletes=['pygtkchart'],
+)
