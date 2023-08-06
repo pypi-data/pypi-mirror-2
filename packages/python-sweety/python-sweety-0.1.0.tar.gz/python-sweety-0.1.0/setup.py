08321c51dd579caaac7a08758913b079c5eef957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package python-sweety/0.1.0/python-sweety-0.1.0.tar.gz

---
+++ packages/python-sweety/python-sweety-0.1.0.tar.gz/python-sweety-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+		name = 'python-sweety',
+		version = '0.1.0',
+		description = 'Python common utility classes',
+		author = 'Chris Chou',
+		author_email = 'm2chrischou@gmail.com',
+		url = 'https://bitbucket.org/mongmong/python-sweety',
+		classifiers = [
+			'Programming Language :: Python',
+			'Development Status :: 4 - Beta',
+			'Topic :: Software Development :: Libraries :: Python Modules',
+			]
+)
+
