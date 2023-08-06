80b8dfc615dc5015f75cf766ec30a636050c342a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:37 +0000

    Add package kegbot/0.8.3/kegbot-0.8.3.tar.gz

---
+++ packages/kegbot/kegbot-0.8.3.tar.gz/kegbot-0.8.3/src/pykeg/web/context_processors.py
@@ -0,0 +1,25 @@
+from pykeg.core import features
+from pykeg.core import models
+
+def enabled_features(request):
+  """Adds a USE_FEATURENAME flags for each enabled feature.
+
+  Currently defined:
+    USE_FACEBOOK - Allow registration and fbconnect login via django-socialauth
+    USE_TWITTER - pykeg.contrib.twitter is installed.
+  """
+  # TODO(mikey): this might make it harder to diagnose why features aren't
+  # visible/being used.
+  ret = {}
+  ret['USE_FACEBOOK'] = features.use_facebook()
+  ret['USE_TWITTER'] = features.use_twitter()
+  return ret
+
+def kbsite(request):
+  ret = {}
+  kbsite = request.kbsite
+  ret['kbsite'] = kbsite
+  if kbsite:
+    kbsettings, _ = models.SiteSettings.objects.get_or_create(site=request.kbsite)
+    ret['kbsettings'] = kbsettings
+  return ret
