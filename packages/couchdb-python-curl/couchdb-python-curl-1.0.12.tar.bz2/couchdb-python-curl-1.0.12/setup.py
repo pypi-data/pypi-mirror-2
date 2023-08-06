057f824e849d4e47f7c1470f603a76cc0df06fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:04 +0000

    Add package couchdb-python-curl/1.0.12/couchdb-python-curl-1.0.12.tar.bz2

---
+++ packages/couchdb-python-curl/couchdb-python-curl-1.0.12.tar.bz2/couchdb-python-curl-1.0.12/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+version = '1.0.12'
+from setuptools import setup
+if __name__ == '__main__':
+    setup(name='couchdb-python-curl',
+          version=version,
+          description='CouchDB-python wrapper (using cURL library)',
+          author='Alexey Loshkarev',
+          author_email='elf2001@gmail.com',
+          url='http://code.google.com/p/couchdb-python-curl/',
+          packages=['couchdbcurl'],
+          license='GPL',
+          classifiers=[
+              "Development Status :: 5 - Production/Stable", 
+              "Intended Audience :: Developers",
+              "License :: OSI Approved :: GNU General Public License (GPL)",
+              "Natural Language :: English",
+              "Programming Language :: Python",
+              "Topic :: Software Development :: Libraries :: Python Modules",
+              "Topic :: Database :: Front-Ends",
+              ],
+          install_requires=['pycurl'],
+          entry_points={
+              'console_scripts': [
+                  'couchdb-curl-pinger = couchdbcurl.pinger:main',
+                  'couchdb-curl-viewserver = couchdb.view:main',
+                  'couchdb-curl-dump = couchdb.tools.dump:main',
+                  'couchdb-curl-load = couchdb.tools.load:main',
+                  'couchdb-curl-replicate = couchdb.tools.replication_helper:main'
+              ]
+          }
+          )
