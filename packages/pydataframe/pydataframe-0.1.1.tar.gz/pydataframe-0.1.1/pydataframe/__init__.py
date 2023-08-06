5b590e50e87000ae0083d504ba4f7bf4545f55fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:26 +0000

    Add package pydataframe/0.1.1/pydataframe-0.1.1.tar.gz

---
+++ packages/pydataframe/pydataframe-0.1.1.tar.gz/pydataframe-0.1.1/pydataframe/__init__.py
@@ -0,0 +1,9 @@
+from dataframe import DataFrame, DataFrameFrom2dArray, combine
+from parsers import DF2CSV, DF2ARFF, TabDialect, Access2000Dialect, SpaceDialect, DF2Excel, CommaDialect, DF2Sqlite, ParserError, DF2HTMLTable
+
+all = [
+    DataFrameFrom2dArray, DataFrame, combine,
+    DF2CSV, DF2ARFF, TabDialect, Access2000Dialect, SpaceDialect, DF2Excel, CommaDialect, DF2Sqlite, ParserError,
+    DF2HTMLTable,
+
+]
