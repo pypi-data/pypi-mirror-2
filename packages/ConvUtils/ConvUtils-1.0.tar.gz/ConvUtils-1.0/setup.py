45bce5ce7f388cfbe663b51c390bcf229be6807b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:15 +0000

    Add package ConvUtils/1.0/ConvUtils-1.0.tar.gz

---
+++ packages/ConvUtils/ConvUtils-1.0.tar.gz/ConvUtils-1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='ConvUtils',
+    version='1.0',
+    author='Christopher D. Lasher',
+    author_email='chris.lasher@gmail.com',
+    packages=['convutils', 'convutils.test'],
+    url='http://pypi.python.org/pypi/ConvUtils/',
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries'
+    ],
+    description=("A library of convenient utility functions and pure "
+        "Python data structures."),
+    long_description=open('README.rst').read(),
+)
+
