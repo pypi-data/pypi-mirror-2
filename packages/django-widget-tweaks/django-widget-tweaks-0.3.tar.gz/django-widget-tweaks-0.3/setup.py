d08ae481190291d9ac67b31ba03c44b5179be8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:06 +0000

    Add package django-widget-tweaks/0.3/django-widget-tweaks-0.3.tar.gz

---
+++ packages/django-widget-tweaks/django-widget-tweaks-0.3.tar.gz/django-widget-tweaks-0.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from distutils.core import setup
+for cmd in 'egg_info', 'develop':
+    import sys
+    if cmd in sys.argv:
+        from setuptools import setup
+
+version='0.3'
+
+setup(
+    name = 'django-widget-tweaks',
+    version = version,
+    author = 'Mikhail Korobov',
+    author_email = 'kmike84@gmail.com',
+    url = 'http://bitbucket.org/kmike/django-widget-tweaks/',
+    download_url = 'http://bitbucket.org/kmike/django-widget-tweaks/get/tip.zip',
+
+    description = 'Tweak the form field rendering in templates, not in python-level form definitions.',
+    long_description = open('README.rst').read(),
+    license = 'MIT license',
+    requires = ['django'],
+
+    packages=['widget_tweaks', 'widget_tweaks.templatetags'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
