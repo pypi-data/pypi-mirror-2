8779bfb078742904caf2b24f2ffd87af03ea2339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:03 +0000

    Add package Cartridge/0.1.2/Cartridge-0.1.2.tar.gz

---
+++ packages/Cartridge/Cartridge-0.1.2.tar.gz/Cartridge-0.1.2/cartridge/project_template/urls.py
@@ -0,0 +1,9 @@
+
+from django.conf.urls.defaults import *
+
+from mezzanine.project_template.urls import urlpatterns
+
+urlpatterns = patterns("",
+    ("^shop/", include("cartridge.shop.urls")),
+) + urlpatterns
+
---
+++ packages/Cartridge/Cartridge-0.1.2.tar.gz/Cartridge-0.1.2/cartridge/shop/urls.py
@@ -0,0 +1,20 @@
+
+from django.conf.urls.defaults import *
+from cartridge.shop.settings import CHECKOUT_ACCOUNT_ENABLED
+
+
+urlpatterns = patterns("cartridge.shop.views",
+    url("^product/(?P<slug>.*)/$", "product", name="shop_product"),
+    url("^search/$", "search", name="shop_search"),
+    url("^wishlist/$", "wishlist", name="shop_wishlist"),
+    url("^cart/$", "cart", name="shop_cart"),
+    url("^checkout/$", "checkout", name="shop_checkout"),
+    url("^checkout/complete/$", "complete", name="shop_complete"),
+)
+
+if CHECKOUT_ACCOUNT_ENABLED:
+    urlpatterns += patterns("cartridge.shop.views",
+        url("^account/$", "account", name="shop_account"),
+        url("^logout/$", "logout", name="shop_logout"),
+    )
+
