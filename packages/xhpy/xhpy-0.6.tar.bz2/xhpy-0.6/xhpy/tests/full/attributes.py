f78ade9db6728cd700732298d73fe2f7b8d479e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:55 +0000

    Add package xhpy/0.6/xhpy-0.6.tar.bz2

---
+++ packages/xhpy/xhpy-0.6.tar.bz2/xhpy-0.6/xhpy/tests/full/attributes.py
@@ -0,0 +1,13 @@
+class :foo(object):
+  attribute
+    string foo
+  def __init__(self):
+    pass
+class :bar(object):
+  attribute
+    :foo,
+    :foo,
+    string bar
+  def __init__(self):
+    pass
+print "pass"
