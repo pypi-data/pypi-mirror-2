63775863a4bd0de1b33c2de00ac2a0c1a13f6d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:44 +0000

    Add package delicious2fluid/0.1.1/delicious2fluid-0.1.1.zip

---
+++ packages/delicious2fluid/delicious2fluid-0.1.1.zip/delicious2fluid-0.1.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='delicious2fluid',
+      version='0.1.1',
+      description="Imports delicious bookmarks to FluidDB",
+      author='Nicholas Tollervey',
+      author_email='ntoll@ntoll.org',
+      url='http://fluidinfo.com',
+      license='MIT',
+      requires=['httplib2', ],
+      scripts=['bin/delicious2fluid', ],
+      py_modules=['delicious2fluid', ],
+      long_description=open('README.rst').read(),
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Database',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Software Development :: Libraries'])
