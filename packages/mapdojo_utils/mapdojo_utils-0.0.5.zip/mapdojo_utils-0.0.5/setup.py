d5d2144bc8c7d8b8f53fe7eade277afc9cc77c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:21 +0000

    Add package mapdojo_utils/0.0.5/mapdojo_utils-0.0.5.zip

---
+++ packages/mapdojo_utils/mapdojo_utils-0.0.5.zip/mapdojo_utils-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'mapdojo_utils',
+    version = '0.0.5',
+    py_modules = ['mapdojo_utils'],
+    author = 'Brad Browne',
+    author_email = 'brad@mapdojo.com',
+    url = 'http://www.mapdojo.com',
+    description = 'A set of utilities used at mapdojo.',
+)
