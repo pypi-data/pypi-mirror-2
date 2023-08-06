4b85a3ba4f2bb183fb828c164b514212eef7533f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:34 +0000

    Add package nesterffff/1.0.0/nesterffff-1.0.0.tar.gz

---
+++ packages/nesterffff/nesterffff-1.0.0.tar.gz/nesterffff-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'nesterffff',
+	version = '1.0.0',
+	py_modules = ['nesterffff'],
+	author = 'scl',
+	author_email = 'ak@ak.com',
+	url = 'http://bbs.sinounix.com',
+	description = 'a test file',
+)
