adfe085f96716028b881b31c51ffefdc96f885d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:38 +0000

    Add package django-config/0.1.3/django-config-0.1.3.tar.gz

---
+++ packages/django-config/django-config-0.1.3.tar.gz/django-config-0.1.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+version = '0.1.3'
+
+setup(
+    name='django-config',
+    version=version,
+    description='An architecture for maintaining multiple settings files in Django',
+    author='Tareque Hossain',
+    author_email='tareque@codexm.com',
+    url='http://github.com/tarequeh/django-config/',
+    packages=['djangoconfig'],
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        ],
+    )
