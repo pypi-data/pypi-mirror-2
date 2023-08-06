2f65e9be7fa98594343421ead8c682f64205023e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:38 +0000

    Add package django-lfc/1.0.5/django-lfc-1.0.5.tar.gz

---
+++ packages/django-lfc/django-lfc-1.0.5.tar.gz/django-lfc-1.0.5/lfc/feeds.py
@@ -0,0 +1,45 @@
+# django imports
+from django.contrib.syndication.feeds import Feed
+from django.core.exceptions import ObjectDoesNotExist
+
+# tagging imports
+from tagging.managers import ModelTaggedItemManager
+
+# lfc imports
+import lfc.utils
+from lfc.models import BaseContent
+
+class PageTagFeed(Feed):
+    """Provides a feed for a given object restricted by given tags url, e.g.
+
+    http://www.lfcproject.com/rss/<path/to/obj>[?tags=python]
+    """
+    def get_object(self, bits):
+        if len(bits) < 1:
+            raise ObjectDoesNotExist
+
+        return lfc.utils.traverse_object(self.request, "/".join(bits))
+
+    def title(self, obj):
+        portal = lfc.utils.get_portal()
+        return "%s - %s" % (portal.title, obj.title)
+
+    def link(self, obj):
+        return obj.get_absolute_url()
+
+    def description(self, obj):
+        return obj.description
+
+    def items(self, obj):
+        paths_objs = obj.get_descendants()
+
+        tags = self.request.GET.getlist("tags")
+        if not tags:
+            return paths_objs
+        else:
+            objs = []
+            tagged_objs = ModelTaggedItemManager().with_all(tags, BaseContent.objects.all())
+            for obj in tagged_objs:
+                if obj.get_content_object() in paths_objs:
+                    objs.append(obj)
+            return objs
