b053e4d3d2da6c4c6b62b3bfe82cf1867e8a973c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:36 +0000

    Add package django-shop-simplenotifications/0.1.1/django-shop-simplenotifications-0.1.1.tar.gz

---
+++ packages/django-shop-simplenotifications/django-shop-simplenotifications-0.1.1.tar.gz/django-shop-simplenotifications-0.1.1/CHANGELOG.rst
@@ -0,0 +1,10 @@
+Version 0.1.1
+==============
+
+  * Fixed description in setup.py
+  * Fixed a bug in MANIFEST.in (now adding AUTHORS correctly)
+
+Version 0.1.0
+==============
+
+  * Creating initial release...
---
+++ packages/django-shop-simplenotifications/django-shop-simplenotifications-0.1.1.tar.gz/django-shop-simplenotifications-0.1.1/shop_simplenotifications/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+VERSION = (0, 1, 1, 'final')
+
+if VERSION[-1] != "final": # pragma: no cover
+    __version__ = '.'.join(map(str, VERSION))
+else: # pragma: no cover
+    __version__ = '.'.join(map(str, VERSION[:-1]))
+
