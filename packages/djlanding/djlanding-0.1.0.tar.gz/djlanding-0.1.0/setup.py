da0a3eed962297c949502894299a7c28025c97c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:57 +0000

    Add package djlanding/0.1.0/djlanding-0.1.0.tar.gz

---
+++ packages/djlanding/djlanding-0.1.0.tar.gz/djlanding-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='djlanding',
+    version='0.1.0',
+    description='Basic landing page for Django projects.',
+    author='NuageHQ',
+    author_email='djlanding@nuagehq.com',
+    license='BSD',
+    keywords='landing django',
+    url='http://pypi.python.org/pypi/djlanding/',
+    packages=find_packages())
