9a099740be34dd35bc0dc9ccdcc17fce0b5ddf60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:20 +0000

    Add package haystack/0.2/haystack-0.2.tar.gz

---
+++ packages/haystack/haystack-0.2.tar.gz/haystack-0.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from glob import glob
+
+setup(name="haystack",
+    version="0.2",
+    description="Search C Structures in a process' memory",
+    long_description=open('README').read(),
+
+    url="http://packages.python.org/haystack/",
+    download_url="http://github.com/trolldbois/python-haystack/tree/master",
+    license='GPL',
+    classifiers=[
+        "Topic :: System :: Networking",
+        "Topic :: Security",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    keywords=['memory','analysis','forensics','struct','ptrace'],
+    author="Loic Jaquemet",
+    author_email="loic.jaquemet+python@gmail.com",
+    packages = ['haystack'],
+    extras_require = {
+        'ptrace':  ["python-ptrace"],
+    },
+)
