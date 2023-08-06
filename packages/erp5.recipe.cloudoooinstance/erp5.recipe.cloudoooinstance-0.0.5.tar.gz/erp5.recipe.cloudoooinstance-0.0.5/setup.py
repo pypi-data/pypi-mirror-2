815c1d577a707ab1e5ede6691517cf69617d4647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:04 +0000

    Add package erp5.recipe.cloudoooinstance/0.0.5/erp5.recipe.cloudoooinstance-0.0.5.tar.gz

---
+++ packages/erp5.recipe.cloudoooinstance/erp5.recipe.cloudoooinstance-0.0.5.tar.gz/erp5.recipe.cloudoooinstance-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+name = "erp5.recipe.cloudoooinstance"
+version = '0.0.5'
+
+setup(
+    name = name,
+    version = version,
+    author = "Gabriel M. Monnerat",
+    author_email = "gabriel@tiolive.com",
+    description = "ZC Buildout recipe for installing a cloudooo instance",
+    long_description="""\n""",
+    license = "ZPL 2.1",
+    keywords = "cloud buildout",
+    classifiers=[
+      "License :: OSI Approved :: Zope Public License",
+      "Framework :: Buildout",
+      ],
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    install_requires=["z3c.recipe.template",],
+    namespace_packages = ['erp5', 'erp5.recipe'],
+    zip_safe=False,
+    entry_points = {'zc.buildout': ['default = %s:Recipe' % name]},
+    )
---
+++ packages/erp5.recipe.cloudoooinstance/erp5.recipe.cloudoooinstance-0.0.5.tar.gz/erp5.recipe.cloudoooinstance-0.0.5/src/erp5/recipe/cloudoooinstance/__init__.py
@@ -0,0 +1,24 @@
+from os import path, chmod
+import logging
+import z3c.recipe.template as z3c_template
+
+class Recipe(object):
+  __module__ = __name__
+
+  def __init__(self, buildout, name, options):
+    options['output'] = options['conf-output']
+    options['input'] = path.join(path.abspath(path.dirname(__file__)), 
+                                  "cloudooo.conf.in" )
+    self.conf_template = z3c_template.Recipe(buildout, name, options)
+    options['input'] = path.join(path.abspath(path.dirname(__file__)), 
+                                  "cloudoooctl.in" )
+    options['output'] = options['ctl-output']
+    self.ctl_template = z3c_template.Recipe(buildout, name, options)
+
+  def install(self):
+    self.conf_template.install()
+    self.ctl_template.install()
+    chmod(self.ctl_template.output, 0755)
+    return []
+
+  update = install
