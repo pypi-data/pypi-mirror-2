6869cf27f50e4c5f578597b1d9edc494c6e9497d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:41 +0000

    Add package django-badges/0.1.1/django-badges-0.1.1.tar.gz

---
+++ packages/django-badges/django-badges-0.1.1.tar.gz/django-badges-0.1.1/setup.py
@@ -0,0 +1,31 @@
+__doc__ = """
+An easy to use app that provides Stack Overflow style badges with a minimum ammount of effort in django
+
+See the README file for details, usage info, and a list of gotchas.
+"""
+
+from setuptools import setup
+
+setup(
+    name='django-badges',
+    version='0.1.1',
+    author='Jim Robert',
+    description=('An easy to use app that provides Stack Overflow style badges'
+                'with a minimum ammount of effort in django'),
+    license='GPLv3',
+    keywords='django badges social',
+    url='http://bitbucket.org/jiaaro/django-badges/',
+    packages=['badges'],
+    long_description=__doc__,
+    classifiers=[
+    	'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities'
+    ]
+)
