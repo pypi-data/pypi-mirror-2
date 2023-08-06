e35825aa01eb314a32aa6eca8d4651227d0f5cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package raptus.header/1.0b2/raptus.header-1.0b2.tar.gz

---
+++ packages/raptus.header/raptus.header-1.0b2.tar.gz/raptus.header-1.0b2/setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of raptus.header
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0b2'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('raptus', 'header', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
+tests_require=['zope.testing']
+
+setup(name='raptus.header',
+      version=version,
+      description="Provides a new folderish content type which may contain multiple images which are then displayed by the provided viewlet.",
+      long_description=long_description,
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='',
+      author='Raptus AG',
+      author_email='dev@raptus.com',
+      url='http://raptus.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['raptus', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'plone.app.imaging',
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'raptus.header.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*- 
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
