aabb24b210909da756bc4218a6ed030e1863323f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:25 +0000

    Add package thechristmaspig/0.9.3/thechristmaspig-0.9.3.tar.gz

---
+++ packages/thechristmaspig/thechristmaspig-0.9.3.tar.gz/thechristmaspig-0.9.3/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup, find_packages
+
+ 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requirements = ['zc.buildout', 'zc.recipe.egg']
+
+setup(
+    name='thechristmaspig',
+    version="0.9.3",
+    description="Buildout recipe for Django. Sets up controls scripts and wsgi file.",
+    long_description=read("README.rst"),
+    url='http://github.com/prestontimmons/thechristmaspig',
+    license='BSD',
+    author='Preston Timmons',
+    author_email='prestontimmons@gmail.com',
+    classifiers=[
+        'Framework :: Buildout',
+        'Framework :: Django',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: BSD License',
+    ],
+    packages=find_packages(exclude=['example', 'parts', 'eggs']),
+    keywords='',
+    zip_safe=False,
+    install_requires=requirements,
+    entry_points={'zc.buildout': ['default = thechristmaspig.recipe:Recipe']},
+)
