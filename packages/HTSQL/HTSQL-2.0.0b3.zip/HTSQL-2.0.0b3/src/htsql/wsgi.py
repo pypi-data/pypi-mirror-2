47346ded643b3ba8d7819400b1d18a71f7c6536e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:27 +0000

    Add package HTSQL/2.0.0b3/HTSQL-2.0.0b3.zip

---
+++ packages/HTSQL/HTSQL-2.0.0b3.zip/HTSQL-2.0.0b3/src/htsql/wsgi.py
@@ -0,0 +1,53 @@
+#
+# Copyright (c) 2006-2010, Prometheus Research, LLC
+# Authors: Clark C. Evans <cce@clarkevans.com>,
+#          Kirill Simonov <xi@resolvent.net>
+#
+
+
+"""
+:mod:`htsql.wsgi`
+=================
+
+This module provides a handler for WSGI requests.
+"""
+
+from .adapter import Utility
+from .request import Request
+from .error import HTTPError
+
+
+class WSGI(Utility):
+    """
+    Declares the WSGI interface.
+
+    The WSGI interface is a utility to handle WSGI requests.
+
+    Usage::
+
+        wsgi = WSGI()
+        body = wsgi(environ, start_response)
+    """
+
+    def __call__(self, environ, start_response):
+        """
+        Implements the WSGI entry point.
+        """
+        # Process the query.
+        request = Request.build(environ)
+        try:
+            status, headers, body = request.render(environ)
+        except HTTPError, exc:
+            return exc(environ, start_response)
+        start_response(status, headers)
+        return body
+
+    def request(self, environ):
+        path_info = environ['PATH_INFO']
+        query_string = environ.get('QUERY_STRING')
+        uri = urllib.quote(path_info)
+        if query_string:
+            uri += '?'+query_string
+        return uri
+
+
