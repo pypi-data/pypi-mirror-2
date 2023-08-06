fd17c3647b135a4bae4aa6d9965d6ed6b35837c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:39 +0000

    Add package garlicsim_lib/0.6.1/garlicsim_lib-0.6.1.tar.gz

---
+++ packages/garlicsim_lib/garlicsim_lib-0.6.1.tar.gz/garlicsim_lib-0.6.1/garlicsim_lib/simpacks/queue/wx/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''`wx` package for the `queue` simpack.'''
+
+from . import settings
+from . import widgets
