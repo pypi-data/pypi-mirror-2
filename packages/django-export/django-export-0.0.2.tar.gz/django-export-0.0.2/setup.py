b64ecfe4be026b56a64cccbcd2382945c53a9099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:53 +0000

    Add package django-export/0.0.2/django-export-0.0.2.tar.gz

---
+++ packages/django-export/django-export-0.0.2.tar.gz/django-export-0.0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+class TestRunner(test):
+    def run(self, *args, **kwargs):
+        if self.distribution.install_requires:
+            self.distribution.fetch_build_eggs(self.distribution.install_requires)
+        if self.distribution.tests_require:
+            self.distribution.fetch_build_eggs(self.distribution.tests_require)
+        from runtests import runtests
+        runtests()
+setup(
+    name='django-export',
+    version='0.0.2',
+    description='Django app allowing for filtered exporting of model data.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    url='http://github.com/praekelt/django-export',
+    packages = find_packages(),
+    install_requires = [
+        'django-object-tools',
+        'django-snippetscream>0.0.4',
+    ],
+    tests_require = [
+        'django',
+    ],
+    include_package_data=True,
+    test_suite = "export.tests",
+    cmdclass={"test": TestRunner},
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
