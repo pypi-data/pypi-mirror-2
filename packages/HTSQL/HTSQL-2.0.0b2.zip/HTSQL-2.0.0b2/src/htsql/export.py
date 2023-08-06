74141558ddae0bb24e732e62fe9c38432ea24d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:19 +0000

    Add package HTSQL/2.0.0b2/HTSQL-2.0.0b2.zip

---
+++ packages/HTSQL/HTSQL-2.0.0b2.zip/HTSQL-2.0.0b2/src/htsql/export.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2006-2010, Prometheus Research, LLC
+# Authors: Clark C. Evans <cce@clarkevans.com>,
+#          Kirill Simonov <xi@resolvent.net>
+#
+
+
+"""
+:mod:`htsql.export`
+===================
+
+This module exports the `htsql.core` addon.
+"""
+
+
+from addon import Addon
+import htsql
+
+
+class HTSQL_CORE(Addon):
+    """
+    Declares the `htsql.core` addon.
+    """
+
+
