0c5b7caaf3db44eb116d9c610cec776cd4f7519a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package parse2plone/0.2/parse2plone-0.2.zip

---
+++ packages/parse2plone/parse2plone-0.2.zip/parse2plone-0.2/setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup, find_packages
+
+name = 'parse2plone'
+description = 'Easily import static HTML files into Plone.' 
+version = '0.2'
+
+def read(file):
+    file = open(file)
+    data = file.read()
+    file.close()
+    return data
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=read('README.txt'),
+    entry_points={
+        'zc.buildout': ['default = %s:Recipe' % name],
+    },
+    install_requires=[
+        'lxml',
+        'BeautifulSoup',
+    ],
+    packages=find_packages(),
+)
