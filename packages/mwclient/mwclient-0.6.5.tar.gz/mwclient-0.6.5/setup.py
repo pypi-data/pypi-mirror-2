6b239204b84b94a304094d3196bc7d99f5a29c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:22 +0000

    Add package mwclient/0.6.5/mwclient-0.6.5.tar.gz

---
+++ packages/mwclient/mwclient-0.6.5.tar.gz/mwclient-0.6.5/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# setup.py -- Setup file for mwclient
+# Author: Baishampayan Ghose <b.ghose@gmail.com>
+#         Yuvi Panda <yuvipanda@yuvi.in>
+from setuptools import setup
+
+setup(name='mwclient',
+      version='0.6.5',
+      license="MIT License",
+      description='MediaWiki API client',
+      author='Bryan Tong Minh',
+      author_email='btongminh@users.sourceforge.net',
+      url='http://sourceforge.net/projects/mwclient/',
+      maintainer='Baishampayan Ghose',
+      maintainer_email='b.ghose@gmail.com',
+      install_requires=['simplejson'],
+      keywords=('wikipedia', 'mediawiki'),
+      packages=['mwclient']
+     )
