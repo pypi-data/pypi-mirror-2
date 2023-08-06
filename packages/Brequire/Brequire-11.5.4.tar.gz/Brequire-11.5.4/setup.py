7218d68c1417f2853c9f664a445a5ff15721c92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:55 +0000

    Add package Brequire/11.5.4/Brequire-11.5.4.tar.gz

---
+++ packages/Brequire/Brequire-11.5.4.tar.gz/Brequire-11.5.4/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+import brequire
+
+setup(
+	name='Brequire',
+	version=brequire.__version__,
+	description="A small module that collects 'I need this non-Python " +
+		"file to operate correctly' declarations.",
+	url="https://github.com/ludios/Brequire",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Development Status :: 4 - Beta',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+	],
+	py_modules=['brequire'],
+)
