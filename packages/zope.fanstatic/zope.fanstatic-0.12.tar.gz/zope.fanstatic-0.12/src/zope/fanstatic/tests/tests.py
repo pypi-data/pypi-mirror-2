6892a253694e223cc727e050ee8d8d3eb1090683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:06 +0000

    Add package zope.fanstatic/0.12/zope.fanstatic-0.12.tar.gz

---
+++ packages/zope.fanstatic/zope.fanstatic-0.12.tar.gz/zope.fanstatic-0.12/src/zope/fanstatic/tests/tests.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2011 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import unittest
+import doctest
+
+from zope.interface import Interface
+from zope.component import getGlobalSiteManager
+from zope.publisher.interfaces.browser import IBrowserRequest
+
+import fanstatic
+from zope.fanstatic.zcml import create_factory
+from zope.fanstatic.tests.view import foo
+from zope.fanstatic.testing import ZopeFanstaticBrowserLayer
+import zope.fanstatic.tests
+
+class TestLayer(ZopeFanstaticBrowserLayer):
+
+    def testSetUp(self):
+        super(TestLayer, self).testSetUp()
+        # Because it is difficult to dynamically register a
+        # entry_point in tests, we do the setup by hand:
+        resource_factory = create_factory(foo)
+        getGlobalSiteManager().registerAdapter(
+            resource_factory, (IBrowserRequest,), Interface, foo.name)
+
+layer = TestLayer(zope.fanstatic.tests)
+
+class NoInjectorTestLayer(TestLayer):
+
+    def setup_middleware(self, app):
+        return app
+
+no_injector_layer = NoInjectorTestLayer(zope.fanstatic.tests)
+
+def test_suite():
+    readme = doctest.DocFileSuite(
+        '../README.txt',
+        optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
+    readme.layer = layer
+    return unittest.TestSuite([readme])
