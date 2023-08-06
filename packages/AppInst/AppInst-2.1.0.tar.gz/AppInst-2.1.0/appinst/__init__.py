b69e770da6edd1e0ac90231016ddf4e08cfa685a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:33 +0000

    Add package AppInst/2.1.0/AppInst-2.1.0.tar.gz

---
+++ packages/AppInst/AppInst-2.1.0.tar.gz/AppInst-2.1.0/appinst/__init__.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2008-2011 by Enthought, Inc.
+# All rights reserved.
+
+import sys
+from os.path import abspath, dirname, isfile, join
+
+from egginst.utils import bin_dir_name
+
+# The custom_tools package is importable when the Python was created by an
+# "enicab" installer, in which case the directory custom_tools contains
+# platform-independent install information in __init__.py and platform-specific
+# information about user setting chosen during the install process.
+try:
+    import custom_tools
+    menu_name = custom_tools.FULL_NAME
+except ImportError:
+    menu_name = 'Python-%i.%i' % sys.version_info[:2]
+
+
+def install(shortcuts, remove):
+    """
+    install Menu and shortcuts
+    """
+    if sys.platform == 'linux2':
+        from linux2 import Menu, ShortCut
+
+    elif sys.platform == 'darwin':
+        from darwin import Menu, ShortCut
+
+    elif sys.platform == 'win32':
+        from win32 import Menu, ShortCut
+
+    m = Menu(menu_name)
+    if remove:
+        for sc in shortcuts:
+            ShortCut(m, sc).remove()
+        m.remove()
+    else:
+        m.create()
+        for sc in shortcuts:
+            ShortCut(m, sc).create()
+
+
+def transform_shortcut(dat_dir, sc):
+    """
+    transform the shortcuts relative paths to absolute paths
+    """
+    # Make the path to the executable absolute
+    bin = sc['cmd'][0]
+    if bin.startswith('..'):
+        bin = abspath(join(dat_dir, bin))
+    elif not bin.startswith('{{'):
+        bin = join(sys.prefix, bin_dir_name, bin)
+    sc['cmd'][0] = bin
+
+    if (sys.platform == 'win32' and sc['terminal'] is False and
+             not bin.startswith('{{') and isfile(bin + '-script.py')):
+        argv = [join(sys.prefix, 'pythonw.exe'), bin + '-script.py']
+        argv.extend(sc['cmd'][1:])
+        sc['cmd'] = argv
+
+    # Make the path of to icon files absolute
+    for kw in ('icon', 'icns'):
+        if kw in sc:
+            sc[kw] = abspath(join(dat_dir, sc[kw]))
+
+
+def get_shortcuts(dat_path):
+    """
+    reads and parses the appinst data file and returns the shortcuts
+    """
+    d = {}
+    execfile(dat_path, d)
+
+    shortcuts = d['SHORTCUTS']
+    for sc in shortcuts:
+        transform_shortcut(dirname(dat_path), sc)
+    return shortcuts
+
+
+def install_from_dat(dat_path):
+    """
+    does a complete install given a data file
+    """
+    install(get_shortcuts(dat_path), remove=False)
+
+
+def uninstall_from_dat(dat_path):
+    """
+    uninstalls all items in a data file
+    """
+    install(get_shortcuts(dat_path), remove=True)
---
+++ packages/AppInst/AppInst-2.1.0.tar.gz/AppInst-2.1.0/appinst/win32.py
@@ -0,0 +1,130 @@
+# Copyright (c) 2008-2011 by Enthought, Inc.
+# All rights reserved.
+
+import os
+import sys
+from os.path import isdir, join
+
+from egginst.utils import rm_empty_dir, rm_rf
+
+import wininst
+
+
+try:
+    from custom_tools.msi_property import get
+    mode = ('user', 'system')[get('ALLUSERS') == '1']
+    addtodesktop = bool(get('ADDTODESKTOP') == '1')
+    addtolauncher = bool(get('ADDTOLAUNCHER') == '1')
+except ImportError:
+    mode = 'user'
+    addtodesktop = True
+    addtolauncher = True
+
+
+quicklaunch_dir = join(wininst.get_special_folder_path('CSIDL_APPDATA'),
+                       "Microsoft", "Internet Explorer", "Quick Launch")
+
+if mode == 'system':
+    desktop_dir = wininst.get_special_folder_path(
+                                           'CSIDL_COMMON_DESKTOPDIRECTORY')
+    start_menu = wininst.get_special_folder_path('CSIDL_COMMON_PROGRAMS')
+else:
+    desktop_dir = wininst.get_special_folder_path('CSIDL_DESKTOPDIRECTORY')
+    start_menu = wininst.get_special_folder_path('CSIDL_PROGRAMS')
+
+
+def quoted(s):
+    """
+    quotes a string if necessary.
+    """
+    # strip any existing quotes
+    s = s.strip('"')
+    if ' ' in s:
+        return '"%s"' % s
+    else:
+        return s
+
+
+class Menu(object):
+
+    def __init__(self, name):
+        self.path = join(start_menu, name)
+
+    def create(self):
+        if not isdir(self.path):
+            os.mkdir(self.path)
+
+    def remove(self):
+        rm_empty_dir(self.path)
+
+
+class ShortCut(object):
+
+    def __init__(self, menu, shortcut):
+        self.menu = menu
+        self.shortcut = shortcut
+        self.cmd = shortcut['cmd']
+
+    def remove(self):
+        self.create(remove=True)
+
+    def create(self, remove=False):
+        # Separate the arguments to the invoked command from the command
+        # itself.
+        cmd = self.cmd[0]
+        args = self.cmd[1:]
+
+        # Handle the special '{{FILEBROWSER}}' command by stripping it
+        # out since File Explorer will automatically be launched when a
+        # folder link is separated.
+        if cmd == '{{FILEBROWSER}}':
+            cmd = args[0]
+            args = args[1:]
+
+        # Otherwise, handle the special '{{WEBBROWSER}}' command by
+        # invoking the Python standard lib's 'webbrowser' script.  This
+        # allows us to specify that the url(s) should be opened in new
+        # tabs.
+        #
+        # If this doesn't work, see the following website for details of
+        # the special URL shortcut file format.  While split across two
+        # lines it is one URL:
+        #   http://delphi.about.com/gi/dynamic/offsite.htm?site= \
+        #        http://www.cyanwerks.com/file-format-url.html
+        elif cmd == '{{WEBBROWSER}}':
+            import webbrowser
+            cmd = sys.executable
+            args = [webbrowser.__file__, '-t'] + args
+
+        # Now create the actual Windows shortcut.  Note that the API to
+        # create a windows shortcut requires that a path to the icon
+        # file be in a weird place -- second in a variable length
+        # list of args.
+        icon = self.shortcut.get('icon')
+        if icon:
+            shortcut_args = ['', icon]
+        else:
+            shortcut_args = []
+
+        # Menu link
+        dst_dirs = [self.menu.path]
+
+        # Desktop link
+        if self.shortcut.get('desktop') and addtodesktop:
+            dst_dirs.append(desktop_dir)
+
+        # Quicklaunch link
+        if self.shortcut.get('quicklaunch') and addtolauncher:
+            dst_dirs.append(quicklaunch_dir)
+
+        for dst_dir in dst_dirs:
+            dst = join(dst_dir, self.shortcut['name'] + '.lnk')
+            if remove:
+                rm_rf(dst)
+            else:
+                wininst.create_shortcut(
+                    quoted(cmd),
+                    self.shortcut['comment'],
+                    dst,
+                    ' '.join(quoted(arg) for arg in args),
+                    *shortcut_args)
