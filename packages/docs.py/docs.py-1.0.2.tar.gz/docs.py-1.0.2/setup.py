96116f80d7f15c6ef3651e983f8d888bdb771e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:32 +0000

    Add package docs.py/1.0.2/docs.py-1.0.2.tar.gz

---
+++ packages/docs.py/docs.py-1.0.2.tar.gz/docs.py-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='docs.py',
+    version='1.0.2',
+    author='Eric Holscher',
+    author_email='eric@ericholscher.com',
+    url='http://github.com/ericholscher/docs.py',
+    scripts=['bin/docs.py'],
+    license='BSD',
+    description='Find documentation on Read the Docs!',
+)
+
