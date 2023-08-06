0860462e1fcca15a8e80cb383b7ca14edd49f13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:03 +0000

    Add package cryotec_server/1.3.1/cryotec_server-1.3.1.tar.gz

---
+++ packages/cryotec_server/cryotec_server-1.3.1.tar.gz/cryotec_server-1.3.1/src/cryotec_server/libs/__init__.py
@@ -0,0 +1,32 @@
+
+
+
+def django_root_url(fq=False):
+    """Returns base URL (no trailing slash) for the current project.
+
+    Setting fq parameter to a true value will prepend the base URL
+    of the current site to create a fully qualified URL.
+
+    The name django_root_url is used in favor of alternatives
+    (such as project_url) because it corresponds to the mod_python
+    PythonOption django.root setting used in Apache.
+    """
+    from django.conf import settings
+
+    url = getattr(settings, 'MY_DJANGO_URL_PATH', '')
+    if fq:
+        url = current_site_url() + url
+    return url
+
+def current_site_url():
+    """Returns fully qualified URL (no trailing slash) for the current site."""
+    from django.contrib.sites.models import Site
+    from django.conf import settings
+
+    current_site = Site.objects.get_current()
+    protocol = getattr(settings, 'MY_SITE_PROTOCOL', 'http')
+    port     = getattr(settings, 'MY_SITE_PORT', '')
+    url = '%s://%s' % (protocol, current_site.domain)
+    if port:
+        url += ':%s' % port
+    return url
---
+++ packages/cryotec_server/cryotec_server-1.3.1.tar.gz/cryotec_server-1.3.1/src/cryotec_server/machines/views.py
@@ -0,0 +1,8 @@
+from machines.models import Machine, MachineMark, MachineType
+from clients.models import Client
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from django.core import serializers
+
+
