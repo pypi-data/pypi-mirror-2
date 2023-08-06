b601e33935f2bae3f93e4b036e5af160b165f724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:06 +0000

    Add package pyforge/0.1/pyforge-0.1.tar.gz

---
+++ packages/pyforge/pyforge-0.1.tar.gz/pyforge-0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(name="pyforge",
+      classifiers = [
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python :: 2.6",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Software Development :: Testing",
+          ],
+      description="Python mocking framework",
+      license="BSD",
+      author="Rotem Yaari",
+      author_email="vmalloc@gmail.com",
+      url="http://github.com/vmalloc/forge",
+      version="0.1",
+      packages=["forge"]
+      )
