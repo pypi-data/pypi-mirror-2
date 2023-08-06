25ab980f5a5371a2c922837c96713e66ff8479ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:37 +0000

    Add package pilas/0.26/pilas-0.26.tar.gz

---
+++ packages/pilas/pilas-0.26.tar.gz/pilas-0.26/pilas/escenas.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+# pilas engine - a video game framework.
+#
+# copyright 2010 - hugo ruscitti
+# license: lgplv3 (see http://www.gnu.org/licenses/lgpl.html)
+#
+# website - http://www.pilas-engine.com.ar
+
+from pilas.actores import Actor
+import pilas
+from PySFML import sf
+
+class Escena:
+    "Escena abstracta."
+
+    def __init__(self):
+        pilas.mundo.definir_escena(self)
+
+    def iniciar(self):
+        pass
+
+    def actualizar(self):
+        pass
+
+    def dibujar(self, aplicacion):
+        pass
+
+
+class Paisaje(Actor, Escena):
+    "Muestra una escena que tiene un fondo de pantalla de paisaje."
+
+    def __init__(self):
+        Escena.__init__(self)
+        Actor.__init__(self, "volley.png")
+        self.z = 100
+
+class Normal(Escena):
+    "Representa la escena inicial mas simple."
+
+    def __init__(self, color_de_fondo=None):
+        Escena.__init__(self)
+        self.fondo = color_de_fondo or pilas.colores.gris
+
+    def dibujar(self, aplicacion):
+        aplicacion.Clear(self.fondo)
---
+++ packages/pilas/pilas-0.26.tar.gz/pilas-0.26/pilas/utils.py
@@ -0,0 +1,99 @@
+# -*- encoding: utf-8 -*-
+# Pilas engine - A video game framework.
+#
+# Copyright 2010 - Hugo Ruscitti
+# License: LGPLv3 (see http://www.gnu.org/licenses/lgpl.html)
+#
+# Website - http://www.pilas-engine.com.ar
+import os
+import interpolaciones
+import pilas
+from PySFML import sf
+import sys
+import subprocess
+import math
+
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def cargar_autocompletado():
+    "Carga los modulos de python para autocompletar desde la consola interactiva."
+    import rlcompleter
+    import readline
+
+    readline.parse_and_bind("tab: complete")
+
+def hacer_flotante_la_ventana():
+    "Hace flotante la ventana para i3 (el manejador de ventanas que utiliza hugo...)"
+    try:
+        subprocess.call(['i3-msg', 't'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except OSError:
+        pass
+
+def centrar_la_ventana(app):
+    "Coloca la ventana principal en el centro del escritorio."
+
+    vm = sf.VideoMode(100, 100)
+
+    # Obtiene la resolución del escritorio y la ventana.
+    desktop_mode = vm.GetDesktopMode()
+    w, h = app.GetWidth(), app.GetHeight()
+
+    # Calcula cual debería la coordenada para centrar la ventana.
+    to_x = desktop_mode.Width/2 - w/2
+    to_y = desktop_mode.Height/2 - h/2
+
+    app.SetPosition(to_x, to_y)
+
+def es_interpolacion(an_object):
+    "Indica si un objeto se comporta como una colisión."
+
+    return isinstance(an_object, interpolaciones.Interpolacion)
+
+
+def obtener_ruta_al_recurso(ruta):
+    """Busca la ruta a un archivo de recursos.
+
+    Los archivos de recursos (como las imagenes) se buscan en varios
+    directorios (ver docstring de image.load), así que esta
+    función intentará dar con el archivo en cuestión.
+    """
+
+    dirs = ['./', os.path.dirname(sys.argv[0]), 'data', PATH, PATH + '/data']
+
+
+    for x in dirs:
+        full_path = os.path.join(x, ruta)
+        #DEBUG: print "buscando en: '%s'" %(full_path)
+
+        if os.path.exists(full_path):
+            return full_path
+
+    # Si no ha encontrado el archivo lo reporta.
+    raise IOError("El archivo '%s' no existe." %(ruta))
+
+
+def esta_en_sesion_interactiva():
+    "Indica si pilas se ha ejecutado desde una consola interactiva de python."
+    try:
+        cursor = sys.ps1
+        return True
+    except AttributeError:
+        return False
+
+def distancia(a, b):
+    "Retorna la distancia entre dos numeros."
+    return abs(b - a)
+
+def distancia_entre_dos_puntos((x1, y1), (x2, y2)):
+    "Retorna la distancia entre dos puntos en dos dimensiones."
+    return math.sqrt(distancia(x1, x2) ** 2 + distancia(y1, y2) ** 2)
+
+def distancia_entre_dos_actores(a, b):
+    return distancia_entre_dos_puntos((a.x, a.y), (b.x, b.y))
+
+def colisionan(a, b):
+    "Retorna True si dos actores estan en contacto."
+    return distancia_entre_dos_actores(a, b) < a.radio_de_colision + b.radio_de_colision
+    
