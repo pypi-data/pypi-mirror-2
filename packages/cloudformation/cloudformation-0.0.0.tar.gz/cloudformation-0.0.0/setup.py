138f3405f2a35c76f41d0fd8957f9fdc718fb5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:29 +0000

    Add package cloudformation/0.0.0/cloudformation-0.0.0.tar.gz

---
+++ packages/cloudformation/cloudformation-0.0.0.tar.gz/cloudformation-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='cloudformation',
+      version='0.0.0',
+      description='Tools for creating CloudFormation templates.',
+      author='Richard Crowley',
+      author_email='richard@devstructure.com',
+      url='http://devstructure.com/',
+      packages=find_packages(),
+      license='BSD',
+      zip_safe=False)
