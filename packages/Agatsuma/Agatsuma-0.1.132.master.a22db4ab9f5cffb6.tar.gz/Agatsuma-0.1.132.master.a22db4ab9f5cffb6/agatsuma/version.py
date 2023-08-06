2b9bd172bebdb997eb1407909889db85f9d4ceb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package Agatsuma/0.1.132.master.a22db4ab9f5cffb6/Agatsuma-0.1.132.master.a22db4ab9f5cffb6.tar.gz

---
+++ packages/Agatsuma/Agatsuma-0.1.132.master.a22db4ab9f5cffb6.tar.gz/Agatsuma-0.1.132.master.a22db4ab9f5cffb6/agatsuma/version.py
@@ -0,0 +1,3 @@
+commits_count = 132
+branch_id = 'master'
+commit_id = 'a22db4ab9f5cffb6'
---
+++ packages/Agatsuma/Agatsuma-0.1.132.master.a22db4ab9f5cffb6.tar.gz/Agatsuma-0.1.132.master.a22db4ab9f5cffb6/agatsuma/web/pylons/interfaces/handling_spell.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class HandlingSpell(object):
+    """
+    """
+
+    def init_routes(self, map):
+        pass
+
+    def post_init_routes(self, map):
+        pass
