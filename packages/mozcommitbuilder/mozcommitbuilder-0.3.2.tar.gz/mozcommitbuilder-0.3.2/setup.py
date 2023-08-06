091833a173c7f8630a2307c2d9aa1f66288536cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:00 +0000

    Add package mozcommitbuilder/0.3.2/mozcommitbuilder-0.3.2.tar.gz

---
+++ packages/mozcommitbuilder/mozcommitbuilder-0.3.2.tar.gz/mozcommitbuilder-0.3.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+#from setuptools import setup, find_packages
+setup(name='mozcommitbuilder',
+      packages=['mozcommitbuilder'],
+      version='0.3.2',
+      description ="""Regression finder using Mozilla central repo""",
+      author="Sam Liu",
+      author_email="sam@ambushnetworks.com",
+      url='http://github.com/samliu/moz-commitbuilder',
+      entry_points="""
+          [console_scripts]
+          mozcommitbuilder = mozcommitbuilder:buildercli
+        """,
+      install_requires = ['mozrunner >= 2.5.4', 'httplib2 >= 0.6.0'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Operating System :: OS Independent'
+                  ]
+
+      )
