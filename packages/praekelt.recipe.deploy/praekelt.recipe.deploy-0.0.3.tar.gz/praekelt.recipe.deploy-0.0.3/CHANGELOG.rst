87b8f20f52b23efc6e1e9f338722205f31489853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:15 +0000

    Add package praekelt.recipe.deploy/0.0.3/praekelt.recipe.deploy-0.0.3.tar.gz

---
+++ packages/praekelt.recipe.deploy/praekelt.recipe.deploy-0.0.3.tar.gz/praekelt.recipe.deploy-0.0.3/CHANGELOG.rst
@@ -0,0 +1,15 @@
+Changelog
+=========
+
+0.0.3
+-----
+#. Added cron_commands parameter. ALlow for certain script argument overrides.
+
+0.0.2
+-----
+#. Fixed branch issue.
+
+0.0.1
+-----
+#. Initial Release
+
---
+++ packages/praekelt.recipe.deploy/praekelt.recipe.deploy-0.0.3.tar.gz/praekelt.recipe.deploy-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='praekelt.recipe.deploy',
+    version='0.0.3',
+    description='Buildout recipe making versioned remote deploys trivial.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/praekelt.recipe.deploy',
+    packages = find_packages(),
+    install_requires = [
+        'zc.recipe.egg',
+        'fabric',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Buildout",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+    entry_points = {'zc.buildout': ['default = praekelt.recipe.deploy:Deploy']},
+)
