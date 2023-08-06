8f1e00914c7e73a595646a5d5302b91a8fc3a52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:07 +0000

    Add package django-generic-helpers/0.2.1/django-generic-helpers-0.2.1.tar.gz

---
+++ packages/django-generic-helpers/django-generic-helpers-0.2.1.tar.gz/django-generic-helpers-0.2.1/generic_helpers/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.contenttypes import generic
+from django.utils.translation import ugettext_lazy as _
+from generic_helpers.managers import GenericRelationManager, ContentType
+
+class GenericRelationModel(models.Model):
+    content_type = models.ForeignKey(ContentType,
+        verbose_name = _('Content type'),
+        related_name = 'content_type_set_for_%(class)s'
+    )
+    object_pk = models.CharField(
+        verbose_name = _('Object ID'),
+        max_length = 255,
+    )
+    content_object = generic.GenericForeignKey(
+        ct_field   = 'content_type',
+        fk_field   = 'object_pk',        
+    )
+    objects = GenericRelationManager()
+
+    class Meta:
+        abstract = True
