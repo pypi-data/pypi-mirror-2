9b5dadcaedcb93c0ce1784c0abb23427598106db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:52 +0000

    Add package django-pipeline/1.1.4.2/django-pipeline-1.1.4.2.tar.gz

---
+++ packages/django-pipeline/django-pipeline-1.1.4.2.tar.gz/django-pipeline-1.1.4.2/pipeline/storage.py
@@ -0,0 +1,39 @@
+import os
+
+from datetime import datetime
+
+from django.core.files.storage import FileSystemStorage, get_storage_class
+from django.utils.functional import LazyObject
+
+from pipeline.conf import settings
+
+
+class PipelineStorage(FileSystemStorage):
+    def __init__(self, location=None, base_url=None, *args, **kwargs):
+        if location is None:
+            location = settings.PIPELINE_ROOT
+        if base_url is None:
+            base_url = settings.PIPELINE_URL
+        super(PipelineStorage, self).__init__(location, base_url, *args, **kwargs)
+
+    def get_available_name(self, name):
+        if self.exists(name):
+            self.delete(name)
+        return name
+
+    def accessed_time(self, name):
+        return datetime.fromtimestamp(os.path.getatime(self.path(name)))
+
+    def created_time(self, name):
+        return datetime.fromtimestamp(os.path.getctime(self.path(name)))
+
+    def modified_time(self, name):
+        return datetime.fromtimestamp(os.path.getmtime(self.path(name)))
+
+
+class DefaultStorage(LazyObject):
+    def _setup(self):
+        self._wrapped = get_storage_class(settings.PIPELINE_STORAGE)()
+
+
+storage = DefaultStorage()
