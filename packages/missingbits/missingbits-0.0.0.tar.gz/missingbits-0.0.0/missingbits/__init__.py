6eb9bb988e4abdc7c9c89911467fb1f4f486f579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:33 +0000

    Add package missingbits/0.0.0/missingbits-0.0.0.tar.gz

---
+++ packages/missingbits/missingbits-0.0.0.tar.gz/missingbits-0.0.0/missingbits/__init__.py
@@ -0,0 +1,79 @@
+# Copyright 2011 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class Range(object):
+
+    def __init__(self, buildout, name, options):
+        start = int(options.get("start", 0))
+        stop = int(options["stop"])
+        step = int(options.get("step", 1))
+
+        for key in options.keys():
+            if key in ("recipe", "start", "stop", "step"):
+                continue
+
+            lst = [options[key].format(i) for i in range(start, stop, step)]
+            options[key + ".forward"] = "\n".join(lst)
+
+            lst.reverse()
+            options[key + ".reverse"] = "\n".join(lst)
+
+    def install(self):
+        return ()
+
+    update = install
+
+
+class Cloner(object):
+
+    def __init__(self, buildout, name, options):
+        template = buildout._raw[options["template"]]
+
+        start = int(options.get("start", 0))
+        stop = int(options["stop"])
+        step = int(options.get("step", 1))
+
+        parts = []
+        for i in range(start, stop, step):
+            name = options["template"].format(i)
+            config = dict((k, v.format(i)) for (k, v) in template.iteritems())
+
+            buildout._raw[name] = config
+
+            parts.append(name)
+
+        options["parts"] = "\n".join(parts)
+
+        if options.get("trigger-dependencies", "true").lower() in ("true", "yes", "on"):
+            [buildout[part] for part in parts]
+
+    def install(self):
+        return ()
+
+    update = install
+
+
+
+class Echo(object):
+
+    def __init__(self, buildout, name, options):
+        self.options = options
+
+    def install(self):
+        print self.options["echo"]
+        return ()
+
+    update = install
+
