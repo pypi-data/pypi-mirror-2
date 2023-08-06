f887ada0e5eb03f408aa011bb7d23d21b6fdd05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:57 +0000

    Add package django-url-imaging/0.2/django-url-imaging-0.2.tar.gz

---
+++ packages/django-url-imaging/django-url-imaging-0.2.tar.gz/django-url-imaging-0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+		name="django-url-imaging",
+		version="0.2",
+		description="URL-based image processing for Django",
+		author="Patrick Carroll",
+		author_email="patrick@patrickomatic.com",
+		requires=['boto'],
+		url="http://urlimg.com/opensource",
+		packages=['urlimaging', 'urlimaging.backends'],
+)
---
+++ packages/django-url-imaging/django-url-imaging-0.2.tar.gz/django-url-imaging-0.2/urlimaging/__init__.py
@@ -0,0 +1,31 @@
+"""
+
+Django URL-imaging
+
+Author: Patrick Carroll <patrick@patrickomatic.com>
+Version: 0.1
+
+"""
+from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
+
+from urlimaging.backends.default import *
+
+VERSION = 0.1
+
+# set up the storage backend - default to S3
+try:
+	settings.IMAGE_STORAGE = eval('%s()' % getattr(settings, 'IMAGE_STORAGE_BACKEND'))
+except AttributeError:
+	settings.IMAGE_STORAGE = S3ImageStorage()
+
+for setting in settings.IMAGE_STORAGE.get_required_settings():
+	try:
+		getattr(settings, setting)
+	except AttributeError:
+		raise ImproperlyConfigured("You must set %s in your settings.py" % setting)
+	
+try:
+	getattr(settings, 'IMAGE_WHITELIST_FN')
+except AttributeError:
+	settings.IMAGE_WHITELIST_FN = lambda url: settings.MEDIA_URL in url
