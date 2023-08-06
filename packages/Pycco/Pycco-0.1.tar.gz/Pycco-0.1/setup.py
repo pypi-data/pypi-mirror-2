e5d0990a1939dfdde7063f37c3a41cf0c8176f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:46 +0000

    Add package Pycco/0.1/Pycco-0.1.tar.gz

---
+++ packages/Pycco/Pycco-0.1.tar.gz/Pycco-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="Pycco",
+      version="0.1",
+      description="""A Python port of Docco: the original quick-and-dirty, hundred-line-long,
+      literate-programming-style documentation generator.""",
+      author="Nick Fitzgerald",
+      author_email="fitzgen@gmail.com",
+      url="http://fitzgen.github.com/pocco",
+      packages=["pycco_resources"],
+      scripts=["pycco"], 
+      install_requires = ['markdown', 'pygments', 'pystache'])
