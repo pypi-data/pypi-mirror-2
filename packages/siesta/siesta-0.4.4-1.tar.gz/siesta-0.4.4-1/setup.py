257d6baad3e601ebf009ac5c385c7faf77d68490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:37 +0000

    Add package siesta/0.4.4-1/siesta-0.4.4-1.tar.gz

---
+++ packages/siesta/siesta-0.4.4-1.tar.gz/siesta-0.4.4-1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__version__ = "0.4.4-1"
+
+setup(
+    name='siesta',
+    version=__version__,
+    description='Sit back, relax and enjoy your python REST client',
+    author='Sebastián Castillo Builes',
+    author_email='castillobuiles@gmail.com',
+    license='GPL',
+    keywords='REST RESTful API HTTP web service',
+    url="http://scastillo.github.com/siesta/",
+    long_description="""
+    Siesta is a client library to consume RESTful web services.
+    """,
+    packages=find_packages(),
+    install_requires = ['simplejson'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        ],
+    )
