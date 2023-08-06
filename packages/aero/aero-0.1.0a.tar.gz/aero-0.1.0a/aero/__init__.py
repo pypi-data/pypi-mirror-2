5d50dd1abb92787c496119d8839e3051a239116c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:23 +0000

    Add package aero/0.1.0a/aero-0.1.0a.tar.gz

---
+++ packages/aero/aero-0.1.0a.tar.gz/aero-0.1.0a/aero/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
+
+from aero.app import AeroApp
