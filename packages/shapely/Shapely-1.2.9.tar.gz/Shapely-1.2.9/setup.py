71535ac2eb16507cb8e6ce7094e7d6c2a672babb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:55 +0000

    Add package shapely/1.2.9/Shapely-1.2.9.tar.gz

---
+++ packages/shapely/Shapely-1.2.9.tar.gz/Shapely-1.2.9/setup.py
@@ -0,0 +1,57 @@
+import glob
+import warnings
+
+try:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+except:
+    warnings.warn(
+    "Failed to import distribute_setup, continuing without distribute.", 
+    Warning)
+
+from setuptools import setup, find_packages
+import sys
+
+readme_text = file('README.txt', 'rb').read()
+changes_text = file('CHANGES.txt', 'rb').read()
+
+setup_args = dict(
+    metadata_version    = '1.2',
+    name                = 'Shapely',
+    version             = '1.2.9',
+    requires_python     = '>=2.5,<3',
+    requires_external   = 'libgeos_c (>=3.1)', 
+    description         = 'Geometric objects, predicates, and operations',
+    license             = 'BSD',
+    keywords            = 'geometry topology gis',
+    author              = 'Sean Gillies',
+    author_email        = 'sean.gillies@gmail.com',
+    maintainer          = 'Sean Gillies',
+    maintainer_email    = 'sean.gillies@gmail.com',
+    url                 = 'http://trac.gispython.org/lab/wiki/Shapely',
+    long_description    = readme_text + "\n" + changes_text,
+    packages            = ['shapely', 'shapely.geometry'],
+    test_suite          = 'shapely.tests.test_suite',
+    classifiers         = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        ],
+    )
+
+# Add DLLs for Windows
+if sys.platform == 'win32':
+    if '(AMD64)' in sys.version:
+        setup_args.update(
+            data_files=[('DLLs', glob.glob('DLLs_AMD64/*.dll'))]
+            )
+    else:
+        setup_args.update(
+            data_files=[('DLLs', glob.glob('DLLs_x86/*.dll'))]
+            )
+
+setup(**setup_args)
---
+++ packages/shapely/Shapely-1.2.9.tar.gz/Shapely-1.2.9/shapely/tests/test_dlls.py
@@ -0,0 +1,15 @@
+import unittest
+
+from shapely.geos import load_dll
+
+class LoadingTestCase(unittest.TestCase):
+    def test_load(self):
+        self.assertRaises(OSError, load_dll, 'geosh_c')
+    def test_fallbacks(self):
+        a = load_dll('geosh_c', fallbacks=[
+            '/opt/local/lib/libgeos_c.dylib',
+            'libgeos_c.so.1', 
+            'libgeos_c.so'])
+
+def test_suite():
+    return unittest.TestLoader().loadTestsFromTestCase(LoadingTestCase)
