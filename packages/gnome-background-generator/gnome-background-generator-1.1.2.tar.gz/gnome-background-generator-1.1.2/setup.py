b2bf11eb84b8274419cdcdef2a5b85fe0d2c7b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:52 +0000

    Add package gnome-background-generator/1.1.2/gnome-background-generator-1.1.2.tar.gz

---
+++ packages/gnome-background-generator/gnome-background-generator-1.1.2.tar.gz/gnome-background-generator-1.1.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import sys
+
+if sys.version_info < (2,7):
+    requires = ['argparse', ]
+else:
+    requires = []
+
+setup(
+    name = "gnome-background-generator",
+    version = '1.1.2',
+    url = 'http://github.com/ametaireau/gnome-background-generator/',
+    author = 'Alexis Metaireau',
+    author_email = 'alexis@notmyidea.org',
+    description = """A simple utility command line to generate a XML file for
+                  dynamic gnome wallpapers""",
+    long_description=open('README.rst').read(),
+    requires = requires, 
+    scripts = ['gnome-background-generator', 'gnome-wallpaper-select'],
+    classifiers = ['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: GNU Affero General Public License v3',
+                   'Environment :: X11 Applications :: Gnome',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                   ],
+)
