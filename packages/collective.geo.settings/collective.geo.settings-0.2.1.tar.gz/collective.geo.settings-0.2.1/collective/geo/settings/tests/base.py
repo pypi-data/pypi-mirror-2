1bef04a920a4f56a671a1965b29cb6729f8de811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package collective.geo.settings/0.2.1/collective.geo.settings-0.2.1.tar.gz

---
+++ packages/collective.geo.settings/collective.geo.settings-0.2.1.tar.gz/collective.geo.settings-0.2.1/collective/geo/settings/tests/base.py
@@ -0,0 +1,41 @@
+from zope.interface import implements
+from zope.publisher.browser import TestRequest
+
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import onsetup
+
+from z3c.form.interfaces import IFormLayer
+
+@onsetup
+def setup_product():
+    """
+       Set up the package and its dependencies.
+    """
+
+    fiveconfigure.debug_mode = True
+    import collective.geo.settings
+    zcml.load_config('configure.zcml', collective.geo.settings)
+
+    fiveconfigure.debug_mode = False
+
+
+setup_product()
+ptc.setupPloneSite(products=['collective.geo.settings'])
+
+
+class TestRequest(TestRequest):
+    implements(IFormLayer)
+
+
+class TestCase(ptc.PloneTestCase):
+    pass
+
+    # def afterSetUp(self):
+    #     collective.geo.settings.fields.decimalPersistentFieldAdapter
+    #     
+
+class FunctionalTestCase(ptc.FunctionalTestCase):
+    pass
