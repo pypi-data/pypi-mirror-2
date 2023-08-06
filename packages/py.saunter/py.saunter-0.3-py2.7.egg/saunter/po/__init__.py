0355ce63492becf88fcdf83cb5bb6a5fc409d33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:15 +0000

    Add package py.saunter/0.3/py.saunter-0.3-py2.7.egg

---
+++ packages/py.saunter/py.saunter-0.3-py2.7.egg/saunter/po/__init__.py
@@ -0,0 +1,13 @@
+"""
+======
+Common
+======
+"""
+__version__ = '0.1'
+
+#: timeout value in s as an integer
+timeout_seconds = 30
+#: timout value in ms as an integer
+timeout_microseconds = timeout_seconds * 1000
+#: timout value in ms as a string
+string_timeout = str(timeout_microseconds)
