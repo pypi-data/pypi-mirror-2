16b513ba55c13cd0f4ab28da4ab8ee191e993df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:17 +0000

    Add package plone.app.contentrules/1.1.7/plone.app.contentrules-1.1.7.zip

---
+++ packages/plone.app.contentrules/plone.app.contentrules-1.1.7.zip/plone.app.contentrules-1.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os.path
+
+version = '1.1.7'
+
+setup(name='plone.app.contentrules',
+      version=version,
+      description="Plone integration for plone.contentrules",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Markus Fuhrer and Martin Aspeli',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.app.contentrules',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.contentrules',
+      ],
+      )
