b33306bfe129dd252be6c5fd4b95eeaa6b5c7f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:20 +0000

    Add package rapid-spring/0.5.1/rapid-spring-0.5.1.tar.gz

---
+++ packages/rapid-spring/rapid-spring-0.5.1.tar.gz/rapid-spring-0.5.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='rapid-spring',
+    version='0.5.1',
+    author='Tobi Vollebregt',
+    author_email='tobivollebregt@gmail.com',
+    packages=['rapid', 'rapid.ui', 'rapid.ui.qt', 'rapid.ui.text', 'rapid.util', 'rapid.unitsync'],
+    scripts=['bin/rapid','bin/rapid-gui'],
+    url='http://pypi.python.org/pypi/rapid-spring/',
+    license='LICENSE.txt',
+    description='spring content downloading',
+    long_description=open('README.txt').read(),
+    # running `setup.py sdist' gives a warning about this, but still
+    # install_requires is the only thing that works with pip/easy_install...
+    install_requires=['bitarray'],
+)
