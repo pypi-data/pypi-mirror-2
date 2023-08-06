d0a9952c3691508112b7cd41bbc82c853171d0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:37 +0000

    Add package launch-control-tool/0.3/launch-control-tool-0.3.tar.gz

---
+++ packages/launch-control-tool/launch-control-tool-0.3.tar.gz/launch-control-tool-0.3/launch_control_tool/commands/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2010 Linaro Limited
+#
+# Author: Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
+#
+# This file is part of launch-control-tool.
+#
+# launch-control-tool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation
+#
+# launch-control-tool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with launch-control-tool.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Package with command line commands
+"""
