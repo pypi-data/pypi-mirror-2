3175cc6650063e9438103a9000587f8bfae75035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:46 +0000

    Add package pycloudfuse/0.02/pycloudfuse-0.02.tar.gz

---
+++ packages/pycloudfuse/pycloudfuse-0.02.tar.gz/pycloudfuse-0.02/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='pycloudfuse',
+      version="0.02",
+      description='Fuse (Filesystem in Userspace) interface to Rackspace Cloud Files and Open Stack Object Storage (Swift)',
+      author='Nick Craig-Wood',
+      author_email='nick@memset.com',
+      url="https://github.com/memset/pycloudfuse",
+      license='MIT',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['fuse-python', 'python-cloudfiles', 'ftp-cloudfs>=0.9'],
+      scripts=['pycloudfuse'],
+      #packages = find_packages(exclude=['tests', 'debian']),
+      #tests_require = ["nose"],
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Operating System :: POSIX :: Linux',
+        'Environment :: No Input/Output (Daemon)',
+        'License :: OSI Approved :: MIT License',
+        ],
+      #test_suite = "nose.collector",
+      )
