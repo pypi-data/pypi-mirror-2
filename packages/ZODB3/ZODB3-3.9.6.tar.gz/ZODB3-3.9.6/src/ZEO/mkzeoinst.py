82359b2aee984921c03297a038ccdf25147b5b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:41 +0000

    Add package ZODB3/3.9.6/ZODB3-3.9.6.tar.gz

---
+++ packages/ZODB3/ZODB3-3.9.6.tar.gz/ZODB3-3.9.6/src/ZEO/mkzeoinst.py
@@ -0,0 +1,25 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+print """
+The mkzeoinst script is no longer supported.
+
+Install the zope.mkzeoinstance package:
+
+  http://pypi.python.org/pypi/zope.mkzeoinstance
+
+and use the installed mkzeointance script that is installed by that package.
+"""
+
+import sys
+sys.exit(1)
