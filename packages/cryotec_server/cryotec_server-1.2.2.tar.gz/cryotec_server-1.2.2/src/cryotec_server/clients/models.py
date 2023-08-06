fa84d3dbfa989c008b7776e02750f09b585ae5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:35 +0000

    Add package cryotec_server/1.2.2/cryotec_server-1.2.2.tar.gz

---
+++ packages/cryotec_server/cryotec_server-1.2.2.tar.gz/cryotec_server-1.2.2/src/cryotec_server/clients/models.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+try:
+    from libs.modelmixins import UrlMixin
+except ImportError:
+    from cryotec_server.libs.modelmixins import UrlMixin
+try:
+    from django.db import models
+except ImportError:
+    from qtdjango import models
+
+class Client(models.Model):
+    """
+    Клиент
+    """
+    name = models.CharField(u"Имя", max_length=128)
+    """Имя клиента"""
+    
+    comment = models.TextField(u"Комментарий", max_length=3000)
+
+    class Meta:
+        verbose_name = u"Клиент"
+        verbose_name_plural = u"Клиенты"
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+
+class ContactFace(models.Model):
+    """
+    Контактное лицо
+    """
+    client = models.ForeignKey(Client, verbose_name="Клиент")
+    name = models.CharField("Имя", max_length=30)
+    phone = models.CharField("Телефон", max_length=30, blank=True)
+    email = models.EmailField("E-mail", blank=True)
+    
+    class Meta:
+        verbose_name = "Контактное лицо"
+        verbose_name_plural = "Контактное лицо"
