e65e93896401ef2c3e7c0fcaad16b9c2597346ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:28 +0000

    Add package django-modeler/0.2.1/django-modeler-0.2.1.tar.gz

---
+++ packages/django-modeler/django-modeler-0.2.1.tar.gz/django-modeler-0.2.1/tests/myapp/tests/test_generator.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import
+from django.test import TestCase
+from myapp.tests import data
+from django.contrib.auth.models import User
+from django_modeler.api import Modeler
+from tests.myapp.models import TestModel
+
+class TestGenerator(TestCase):
+    def setUp(self):
+        data.load()
+
+    def test_simple(self):
+        code = Modeler(query_related=0).generate(TestModel.objects.get(pk=1))
+        self.assertTrue('testmodel1' in code)
+        self.assertTrue('relatedtotestmodel' not in code)
+        exec(code)
+        TestModel.objects.get(pk=1)
+
+    def test_related(self):
+        code = Modeler(query_related=2).generate(User.objects.get(pk=1))
+        self.assertTrue('testmodel1' in code)
+        self.assertTrue('relatedtotestmodel' in code)
+        exec(code)
+        TestModel.objects.get(pk=1)
+
+    def test_exclude_related_app(self):
+        code = Modeler(query_related=2, exclude_related=['myapp']).generate(User.objects.get(pk=1))
+        self.assertFalse('testmodel1' in code)
+        self.assertFalse('relatedtotestmodel' in code)
+        exec(code)
+        TestModel.objects.get(pk=1)
+
+    def test_exclude_related_model(self):
+        code = Modeler(query_related=2, exclude_related=['myapp.relatedtotestmodel']).generate(User.objects.get(pk=1))
+        self.assertTrue('testmodel1' in code)
+        self.assertFalse('relatedtotestmodel' in code)
+        exec(code)
+        TestModel.objects.get(pk=1)
+
+    def test_exclude_fields(self):
+        code = Modeler(query_related=0, exclude_fields=['auth']).generate(TestModel.objects.get(pk=1))
+        self.assertFalse('user' in code)
+        exec(code)
+        TestModel.objects.get(pk=1)
+
+    def test_exclude_field_model(self):
+        code = Modeler(query_related=0, exclude_fields=['auth.user']).generate(TestModel.objects.get(pk=1))
+        self.assertFalse('user' in code)
+        exec(code)
+        TestModel.objects.get(pk=1)
+
+    def test_none(self):
+        '''regression'''
+        code = Modeler(query_related=2).generate(TestModel.objects.get(pk=2))
+        self.assertTrue('testmodel2' in code)
+        exec(code)
+        TestModel.objects.get(pk=2)
