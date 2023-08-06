8b18d4f46e5d1cd5b45c8626f2d8953f2b4b6658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:53 +0000

    Add package pytassium/0.1.5/pytassium-0.1.5.tar.gz

---
+++ packages/pytassium/pytassium-0.1.5.tar.gz/pytassium-0.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name = 'pytassium',
+    version = '0.1.5',
+    description = 'A Python library for working with Kasabi.com APIs',
+    author='Ian Davis',
+    author_email='nospam@iandavis.com',
+    url='https://github.com/iand/pytassium',
+    classifiers=['Programming Language :: Python','License :: Public Domain', 'Operating System :: OS Independent', 'Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Database'],
+    packages =['pytassium'],
+    install_requires=['rdfchangesets'],
+    scripts = ["scripts/pytassium"],
+    
+)
