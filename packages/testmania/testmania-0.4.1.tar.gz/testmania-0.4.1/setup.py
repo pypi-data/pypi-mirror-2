f94f1b306d0c6112d9489e3dac86014bb84a6c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:56 +0000

    Add package testmania/0.4.1/testmania-0.4.1.tar.gz

---
+++ packages/testmania/testmania-0.4.1.tar.gz/testmania-0.4.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os.path
+sys.path.insert(0, os.path.dirname(__file__))
+
+from setuptools import setup
+
+from testmania.version import __version__
+
+install_requires = []
+if sys.version_info[0:2] < (2, 7):
+    install_requires.append('unittest2')
+
+
+try:
+    readme_content = open("README.rst").read()
+except:
+    readme_content = ""
+
+
+setup(
+    name='testmania',
+    version=__version__,
+    description='Library of assert_xxx functions for more convenient testing',
+    long_description=readme_content,
+    author='Victor Nakoryakov (aka nailxx)',
+    author_email='nail.xx@gmail.com',
+    license='MIT',
+    keywords="test unittest assert",
+    url='https://github.com/nailxx/testmania',
+    packages=['testmania'],
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+    ],
+)
---
+++ packages/testmania/testmania-0.4.1.tar.gz/testmania-0.4.1/testmania/__init__.py
@@ -0,0 +1,8 @@
+
+from testmania.version import __version__
+
+from testmania.pep8 import *
+from testmania.deep import assert_deep_equal
+from testmania.xml import assert_xml_equal
+from testmania.time import assert_just_now
+from testmania.expect import Expectation
