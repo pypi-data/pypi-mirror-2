f62edfb8ac55f2d81624af6dc5ebce6fad6f2fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package django-url-reduce/0.1.3/django-url-reduce-0.1.3.tar.gz

---
+++ packages/django-url-reduce/django-url-reduce-0.1.3.tar.gz/django-url-reduce-0.1.3/urlreduce/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from urlreduce import utils
+
+class Link( models.Model ):
+    """
+    Link Holds a base URI and a shortened URI, this can be used for both local
+    and remote sites.
+    """
+    
+    url         = models.URLField( verify_exists = False, unique = True )
+    short_url   = models.URLField( verify_exists = False, blank = True, unique = True )
+
+    def key( self ):
+        return utils.base62_encode( self.pk )
+
+    def save( self, *args, **kwargs ):
+        """
+        Save Override Handles Generating the Short URL
+        """
+        
+        # -- Save to Generate PK
+        super( Link, self ).save(*args, **kwargs)
+        
+        # -- Generate Short URL
+        if self.short_url == "" or self.short_url == None:
+            self.short_url = utils.base62_encode( self.pk )
+            super( Link, self ).save(*args, **kwargs)
+
+    def __unicode__( self ):
+        return self.short_url
