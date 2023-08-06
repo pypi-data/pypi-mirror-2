bfbb7961b53856eafd4f3ef9b8214c0d19665311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:17 +0000

    Add package ctctwspylib/1.0/ctctwspylib-1.0.tar.gz

---
+++ packages/ctctwspylib/ctctwspylib-1.0.tar.gz/ctctwspylib-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "ctctwspylib",
+    packages = ["ctctwspylib"],
+    version = "1.0",
+    description = "Constant Contact Labs, Python Client Library",
+    author = "Constant Contact, Inc",
+    author_email = "http://www.ctctlabs.com/",
+    url = "http://www.ctctlabs.com/",
+)
+
+# To create the package: python setup.py register sdist upload
