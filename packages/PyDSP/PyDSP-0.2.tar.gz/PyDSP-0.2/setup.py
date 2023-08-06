fcd823f49584ad6189b16fc99153b5a31f6bab7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:24 +0000

    Add package PyDSP/0.2/PyDSP-0.2.tar.gz

---
+++ packages/PyDSP/PyDSP-0.2.tar.gz/PyDSP-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyDSP',
+    version='0.2',
+    packages=['pydsp',],
+    license='BSD',
+    long_description=open('README.txt').read(),
+    url='http://www.banasfamily.net/PyDSP.html',
+    author='David Banas',
+    author_email='dbanas@banasfamily.net',
+)
