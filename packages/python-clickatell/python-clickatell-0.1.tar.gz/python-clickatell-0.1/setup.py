cedcbf0484a6fc699083086024743a199509d627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:56 +0000

    Add package python-clickatell/0.1/python-clickatell-0.1.tar.gz

---
+++ packages/python-clickatell/python-clickatell-0.1.tar.gz/python-clickatell-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "python-clickatell",
+    version = "0.1",
+    url = 'http://github.com/smn/bellville',
+    license = 'BSD',
+    description = "Clickatell HTTP library",
+    long_description = open('README.md', 'r').read(),
+    author = 'Simon de Haan',
+    author_email = "simon@praekeltfoundation.org",
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires = [
+        'setuptools',
+        'pyOpenSSL',
+        'nose',
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
+
