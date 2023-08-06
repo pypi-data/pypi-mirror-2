e300a7cb7f37415e7fe0af4ae92a7de7b49f4832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:44 +0000

    Add package hashedassets/0.3.0/hashedassets-0.3.0.tar.gz

---
+++ packages/hashedassets/hashedassets-0.3.0.tar.gz/hashedassets-0.3.0/src/hashedassets/tests.py
@@ -0,0 +1,67 @@
+from os import listdir, remove
+from os.path import isdir
+from shutil import rmtree
+import doctest
+import unittest
+
+def setUp():
+    for afile in listdir('.'):
+        if isdir(afile):
+            rmtree(afile)
+        else:
+            remove(afile)
+
+def test_globs():
+    import os
+    import sys
+    from subprocess import Popen, STDOUT, PIPE
+    from shlex import split
+    from hashedassets import main
+
+    def system(cmd, env=None, external=False):  # pylint: disable=W0612
+        cmdline = split(cmd)
+
+        if not external and cmdline[0] == 'hashedassets':
+            _stderr = sys.stderr
+            sys.stderr = sys.stdout
+            try:
+                return main(cmdline[1:])
+            finally:
+                sys.stderr = _stderr
+
+        if env != None:
+            env.update(os.environ)
+        process = Popen(cmdline, stderr=STDOUT, stdout=PIPE, env=env)
+        # from http://stackoverflow.com/questions/1388753/how-to-get-output-from-subprocess-popen
+        while True:
+            out = process.stdout.read(1)
+            if out == '' and process.poll() != None:
+                break
+            if out != '':
+                sys.stdout.write(out)
+                sys.stdout.flush()
+
+    def write(filename, content):  # pylint: disable=W0612
+        f = open(filename, "w")
+        f.write(content)
+        f.close()
+
+    return locals()
+
+def test_suite():
+    setUp()
+
+    opts = dict(
+      globs=test_globs(),
+      optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS|doctest.REPORT_UDIFF,
+    )
+
+    return unittest.TestSuite([
+        doctest.DocTestSuite('hashedassets'),
+        doctest.DocTestSuite('hashedassets.rewrite'),
+        doctest.DocTestSuite('hashedassets.serializer'),
+
+        doctest.DocFileSuite('hashedassets.rst', **opts),
+        doctest.DocFileSuite('errors.rst', **opts),
+        doctest.DocFileSuite('regressions.rst', **opts),
+    ])
