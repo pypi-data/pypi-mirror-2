6b8263f3f0220ea7ef5959c96972161efd29e586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:53 +0000

    Add package dogapi/0.0.3/dogapi-0.0.3.tar.gz

---
+++ packages/dogapi/dogapi-0.0.3.tar.gz/dogapi-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name = "dogapi",
+    version = "0.0.3",
+    packages = find_packages("src"),
+    package_dir = {'':'src'},
+    author = "Datadog, Inc.",
+    author_email = "packages@datadoghq.com",
+    description = "Python bindings to Datadog's API",
+    license = "BSD",
+    keywords = "datadog data",
+    url = "http://datadoghq.com",
+)
+
