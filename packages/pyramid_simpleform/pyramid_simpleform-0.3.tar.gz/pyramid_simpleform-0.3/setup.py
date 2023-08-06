560abab9f97fe6c052ce9af777be776c7df3c3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:27 +0000

    Add package pyramid_simpleform/0.3/pyramid_simpleform-0.3.tar.gz

---
+++ packages/pyramid_simpleform/pyramid_simpleform-0.3.tar.gz/pyramid_simpleform-0.3/setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+requires = [
+    'pyramid',
+    'webhelpers',
+    'formencode',
+]
+
+    
+setup(name='pyramid_simpleform',
+      version='0.3',
+      description='pyramid_simpleform',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Dan Jacob',
+      author_email='danjac354@gmail.com',
+      url='http://bitbucket.org/danjac/pyramid_simpleform',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      license="LICENSE.txt",
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="pyramid_simpleform",
+      )
+
