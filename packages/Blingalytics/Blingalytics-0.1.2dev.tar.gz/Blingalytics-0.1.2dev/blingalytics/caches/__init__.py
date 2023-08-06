a975f97b2b23c95e25b92c60ff4e817eb9a9cb90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:34 +0000

    Add package Blingalytics/0.1.2dev/Blingalytics-0.1.2dev.tar.gz

---
+++ packages/Blingalytics/Blingalytics-0.1.2dev.tar.gz/Blingalytics-0.1.2dev/blingalytics/caches/__init__.py
@@ -0,0 +1,49 @@
+"""
+Cache engines are used as an intermediate store for computed report data.
+Since pulling and processing the data for a report can be computationally
+intensive, this cached storage allows us to sort and page through the
+resulting report very quickly and easily.
+
+Currently, the easiest to get up and running is :doc:`/caches/local_cache`, as
+it has no dependencies outside of Python and simply caches to the local
+filesystem. However, it cannot handle concurrent connections and is generally
+a poor choice outside of the development environment. At the moment, the
+preferred choice for deployment is :doc:`/caches/redis_cache`.
+"""
+
+class InstanceLockError(Exception):
+    """Cannot secure a lock on writing the instance to cache."""
+
+class InstanceExistsError(Exception):
+    """Cannot record the instance because it already exists in cache."""
+
+class InstanceIncompleteError(Exception):
+    """The instance has not yet finished being created in cache."""
+
+class Cache(object):
+    def create_instance(self, report_id, instance_id, rows, footer, expire):
+        raise NotImplementedError
+
+    def kill_instance_cache(self, report_id, instance_id):
+        raise NotImplementedError
+
+    def kill_report_cache(self, report_id):
+        raise NotImplementedError
+
+    def is_instance_started(self, report_id, instance_id):
+        raise NotImplementedError
+
+    def is_instance_finished(self, report_id, instance_id):
+        raise NotImplementedError
+
+    def instance_row_count(self, report_id, instance_id):
+        raise NotImplementedError
+
+    def instance_timestamp(self, report_id, instance_id):
+        raise NotImplementedError
+
+    def instance_rows(self, report_id, instance_id, selected=None, sort=None, limit=None, offset=None, alpha=False):
+        raise NotImplementedError
+
+    def instance_footer(self, report_id, instance_id):
+        raise NotImplementedError
---
+++ packages/Blingalytics/Blingalytics-0.1.2dev.tar.gz/Blingalytics-0.1.2dev/blingalytics/sources/derived.py
@@ -0,0 +1,74 @@
+"""
+The derived source allows you to perform arbitrary operations using the data
+returned in columns from other sources.
+
+For example, if you have a report with a column for gross revenue and a column
+for net revenue that are both pulled from the database, you could have a
+derived column to display the gross margin by performing the operation
+``(net revenue / gross revenue * 100)``.
+"""
+
+import decimal
+
+from blingalytics import sources
+
+
+DIVISION_BY_ZERO = (decimal.InvalidOperation, ZeroDivisionError)
+
+class DerivedSource(sources.Source):
+    def post_process(self, row, clean_inputs):
+        # Compute derived values for all columns on this row
+        for name, column in self._columns:
+            row[name] = column.get_derived_value(row)
+        return row
+
+class DerivedColumn(sources.Column):
+    source = DerivedSource
+
+class Value(DerivedColumn):
+    """
+    A column that derives its value from other columns in the row. In
+    addition to the standard column options, this takes one positional
+    argument: the function used to derive the value.
+
+    The function you provide will be passed one argument: the row, as pulled
+    from other data sources but before the ``post_process`` step. The row is
+    a dict with the column names as keys. Your function should return just the
+    derived value for this column in the row. The function is often provided
+    as a lambda, but more complex functions can be defined wherever you like.
+
+    Continuing the example from above::
+
+        derived.Value(lambda row: row['net'] / row['gross'] * Decimal('100.00'))
+
+    By default, the footer for this column performs the same operation over
+    the appropriate footer columns. This is generally the footer you want for
+    a derived column, as opposed to simply summing or averaging the values in
+    the column.
+    """
+    def __init__(self, derive_func, **kwargs):
+        self.derive_func = derive_func
+        super(Value, self).__init__(**kwargs)
+
+    def get_derived_value(self, row):
+        try:
+            return self.derive_func(row)
+        except TypeError:
+            # Got None for a value, so return None
+            return None
+        except DIVISION_BY_ZERO:
+            return decimal.Decimal('0.00')
+
+    def increment_footer(self, total, cell):
+        return None
+
+    def finalize_footer(self, total, footer):
+        # The footer is the derive function run over the other footer columns
+        if self.footer:
+            try:
+                return self.derive_func(footer)
+            except TypeError:
+                # Got None for a value, so return None
+                return None
+            except DIVISION_BY_ZERO:
+                return decimal.Decimal('0.00')
