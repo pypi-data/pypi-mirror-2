2fb8ede6f265b056e7ee281184f1e325770d36d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:02 +0000

    Add package pipe2py/0.9.2/pipe2py-0.9.2.tar.gz

---
+++ packages/pipe2py/pipe2py-0.9.2.tar.gz/pipe2py-0.9.2/modules/__init__.py
@@ -0,0 +1,47 @@
+#pipe2py modules package
+#Author: Greg Gaughan
+
+#Note: each module name must match the name used internally by Yahoo, preceded by pipe
+
+__all__ = ['pipeforever',
+
+           'pipefetch',
+           'pipefetchdata',
+           'pipefetchpage',
+           'pipeyql',
+           'pipecsv',
+           'pipefeedautodiscovery',
+           'pipefetchsitefeed',
+
+           'pipedatebuilder',
+           'pipeurlbuilder',
+           'pipeitembuilder',
+           'piperssitembuilder',
+           'pipetextinput',
+           'pipeurlinput',
+           'pipestrconcat',
+           'pipedateformat',
+           'pipestrregex',
+           'pipenumberinput',
+           'pipeprivateinput',
+
+           'pipefilter',
+           'piperename',
+           'piperegex',
+           'pipeunion',
+           'pipeloop',
+           'pipesort',
+           'pipecount',
+           'pipetruncate',
+           'pipereverse',
+           'pipeuniq',
+           'pipesubstr',
+           'pipestrreplace',
+           'pipesubelement',
+           'pipetail',
+           'pipecreaterss',
+
+           'pipeoutput',
+
+           'pipestringtokenizer',
+          ]
