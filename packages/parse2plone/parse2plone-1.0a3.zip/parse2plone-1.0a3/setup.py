e99aa0f5723b3003534589c2fe34ca2821dd54cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:42 +0000

    Add package parse2plone/1.0a3/parse2plone-1.0a3.zip

---
+++ packages/parse2plone/parse2plone-1.0a3.zip/parse2plone-1.0a3/setup.py
@@ -0,0 +1,41 @@
+
+from setuptools import setup
+
+name = 'parse2plone'
+description = 'Easily import static HTML websites into Plone.'
+version = '1.0a3'
+
+
+def read(file):
+    file = open(file)
+    data = file.read()
+    file.close()
+    return data
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=read('README.txt') + read('docs/HISTORY.txt'),
+    url='https://github.com/collective/parse2plone',
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    entry_points={
+        'zc.buildout': ['default = %s:Recipe' % name],
+    },
+    install_requires=[
+        'BeautifulSoup',
+        'Plone',
+        'lxml',
+        'xlrd',
+        'zc.buildout',
+    ],
+    py_modules=['parse2plone'],
+    classifiers=[
+        'Framework :: Buildout',
+        'Framework :: Plone'
+    ],
+    extras_require={
+        'tests': ['zope.testing'],
+    },
+)
