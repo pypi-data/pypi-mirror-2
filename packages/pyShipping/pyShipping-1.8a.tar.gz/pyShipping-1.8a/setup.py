a7fcecabe38fa39cc71d0130c457b6dab4691595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:16 +0000

    Add package pyShipping/1.8a/pyShipping-1.8a.tar.gz

---
+++ packages/pyShipping/pyShipping-1.8a.tar.gz/pyShipping-1.8a/setup.py
@@ -0,0 +1,28 @@
+long_description = """
+pyShipping provides connections to interface with shipping companies and to transport shipping related
+information.
+"""
+
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+import codecs
+
+setup(name='pyShipping',
+      maintainer='Maximillian Dornseif',
+      maintainer_email='md@hudora.de',
+      url="https://github.com/hudora/pyShipping/",
+      version='1.8a',
+      description='pyShipping - Shipping related Toolkit',
+      long_description=codecs.open('README.rst', "r", "utf-8").read(),
+      classifiers=['License :: OSI Approved :: BSD License',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python'],
+      # download_url
+      zip_safe=False,
+      install_requires=['cython'],
+      packages=find_packages(),
+      package_data={'': ['README.rst'], 'pyshipping': ['carriers/dpd/georoutetables/*']},
+      include_package_data=True,
+      # cmdclass = {'build_ext': build_ext}
+)
+
