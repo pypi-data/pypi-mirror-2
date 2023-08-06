bb5bfebd201a6d03f6b8e8cc5bda3b69f70bd250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:17 +0000

    Add package HTSQL/2.1.1/HTSQL-2.1.1.zip

---
+++ packages/HTSQL/HTSQL-2.1.1.zip/HTSQL-2.1.1/src/htsql/fmt/__init__.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2006-2011, Prometheus Research, LLC
+# See `LICENSE` for license information, `AUTHORS` for the list of authors.
+#
+
+
+"""
+:mod:`htsql.fmt`
+================
+
+This package implements product formatters.
+"""
+
+
+from .format import FindRenderer
+from .json import JSONRenderer
+from .spreadsheet import CSVRenderer, TSVRenderer
+from .html import HTMLRenderer
+from .text import TextRenderer
+
+
+class FindStandardRenderer(FindRenderer):
+
+    def get_renderers(self):
+        return ([CSVRenderer, TSVRenderer, JSONRenderer,
+                 HTMLRenderer, TextRenderer]
+                + super(FindStandardRenderer, self).get_renderers())
+
+
