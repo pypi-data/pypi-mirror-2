09d2626784b01c57ac772d0f8c9942950c435e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:51 +0000

    Add package rollout/0.1/rollout-0.1.tar.gz

---
+++ packages/rollout/rollout-0.1.tar.gz/rollout-0.1/rollout/tests/command.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.core import management
+
+class RollOutCommandTest(TestCase):
+    fixtures = ['groups', 'users']
+    
+    def test_activate_percentage(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=True, rollout_percentage=20)
+    
+    def test_deactivate_percentage(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=False, rollout_percentage=20)
+
+    def test_activate_user(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=True, rollout_user="elmo")
+    
+    def test_activate_users(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=True, rollout_user="elmo,abby")
+    
+    def test_deactivate_user(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=False, rollout_user="elmo")
+    
+    def test_deactivate_users(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=False, rollout_user="elmo,abby")
+    
+    def test_activate_group(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=True, rollout_group="Sesame Street")
+    
+    def test_activate_group(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=True, rollout_group="Sesame Street,Fairy")
+    
+    def test_deactivate_group(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=False, rollout_group="Sesame Street,Fairy")
+    
+    def test_deactivate_group(self):
+        management.call_command('rollout', 'newfeature', rollout_activate=False, rollout_group="Sesame Street")
+    
+
