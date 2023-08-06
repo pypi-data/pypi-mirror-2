232978420180b32f2b3bb2bd5b1802f0cda22c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:44 +0000

    Add package metroclient/1.01/metroclient-1.01.tar.gz

---
+++ packages/metroclient/metroclient-1.01.tar.gz/metroclient-1.01/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='metroclient',
+    version = '1.01',
+    author = 'AppHosted Team',
+    author_email = 'support@lumentica.com',
+    packages = ['metroclient'],
+    description = 'AppHosted application deployment utility',
+    url = 'https://apphosted.com',
+    license = 'License :: OSI Approved :: GNU General Public License (GPL)',
+    long_description = """
+    Metro is the application deployment utility for managing and deploying web applications
+    hosted on https://apphosted.com""",
+    download_url = 'https://metro.apphosted.com/client/get/',
+    scripts = ['metro'],
+    install_requires = ['httplib2'],
+    platforms = [
+        "All",
+        ],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities",
+        ]
+    )
+
