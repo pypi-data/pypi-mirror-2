703645aa40a01c202812fd781f0112b0e66e8e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:40 +0000

    Add package Plainsight/1.0b/Plainsight-1.0b.tar.gz

---
+++ packages/Plainsight/Plainsight-1.0b.tar.gz/Plainsight-1.0b/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Plainsight',
+      version='1.0b',
+      description='A textual steganography tool to defeat censorship.',
+      author='Robert Winslow',
+      author_email='robert.winslow@gmail.com',
+      url='http://github.com/rw/plainsight',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: End Users/Desktop',
+                   'Natural Language :: English',
+                   'Programming Language :: Python',
+                   'Topic :: Security :: Cryptography',
+                   'Topic :: Text Processing :: Filters',
+                   'Topic :: Text Processing :: Linguistic',
+                  ],
+      requires=['argparse', 'bitstring', 'progressbar'],
+      scripts=['bin/plainsight'],
+      packages=['plainsight']
+     )
