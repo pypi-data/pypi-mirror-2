6f2f83f6930b03e16ff10c0710c9da0e5ea72033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:33 +0000

    Add package monocle/0.6/monocle-0.6.tar.gz

---
+++ packages/monocle/monocle-0.6.tar.gz/monocle-0.6/monocle/callback.py
@@ -0,0 +1,23 @@
+# Sort of like Twisted's Deferred, but simplified.  We don't do
+# callback chaining, since oroutines replace that mechanism.
+class Callback(object):
+    def __init__(self):
+        self._handlers = []
+
+    def add(self, handler):
+        if hasattr(self, 'result'):
+            handler(self.result)
+        else:
+            self._handlers.append(handler)
+
+    def __call__(self, result):
+        assert not hasattr(self, 'result'), "Already called back"
+        for handler in self._handlers:
+            handler(result)
+        self.result = result
+
+
+def defer(result):
+    cb = Callback()
+    cb(result)
+    return cb
