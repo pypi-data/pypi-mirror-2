d90f8f98526fbe2d95f12982edbfebfedc1f1137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:08 +0000

    Add package aino-jstools/0.1.0.4/aino-jstools-0.1.0.4.tar.gz

---
+++ packages/aino-jstools/aino-jstools-0.1.0.4.tar.gz/aino-jstools-0.1.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='aino-jstools',
+    version='0.1.0.4',
+    description='JavaScript tools for Django',
+    long_description=open('README.rst').read(),
+    author='Mikko Hellsing',
+    author_email='mikko@aino.se',
+    license='BSD',
+    url='https://github.com/aino/aino-jstools',
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: JavaScript',
+        'Framework :: Django',
+    ],
+)
+
