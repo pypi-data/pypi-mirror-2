590e48e401026e68fee843669d7b1b074c09e029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:02 +0000

    Add package garlicsim_lib_py3/0.6.3/garlicsim_lib_py3-0.6.3.tar.gz

---
+++ packages/garlicsim_lib_py3/garlicsim_lib_py3-0.6.3.tar.gz/garlicsim_lib_py3-0.6.3/garlicsim_lib/simpacks/life/settings.py
@@ -0,0 +1,9 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''Settings module for the `life` simpack'''
+
+
+from .state import determinism_function
+
+DETERMINISM_FUNCTION = determinism_function
