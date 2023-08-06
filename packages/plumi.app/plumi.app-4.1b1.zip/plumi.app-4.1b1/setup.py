8e64137de30fce0507dc12a0e4a76a423dd8e603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:15 +0000

    Add package plumi.app/4.1b1/plumi.app-4.1b1.zip

---
+++ packages/plumi.app/plumi.app-4.1b1.zip/plumi.app-4.1b1/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+import os
+
+version = '4.1b1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('docs/CHANGES.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('docs/CONTRIBUTORS.txt')
+    )
+
+setup(name='plumi.app',
+      version=version,
+      description="Plumi application setup",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone plumi video sharing',
+      author='Andy Nicholson',
+      author_email='andy@infiniterecursion.com.au',
+      url='https://svn.plone.org/svn/collective/plumi.app/trunk',
+      license='GPL',
+      packages=find_packages('src/plumi.app',exclude=['ez_setup']),
+      package_dir = {'':'src/plumi.app'},
+      namespace_packages=['plumi', 'plumi.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plumi.content',
+          'plumi.locales',
+          'collective.transcode.star',
+          'collective.contentlicensing',
+          'collective.sharerizer',
+          'qi.portlet.TagClouds',
+          'Products.LinguaPlone',
+          'plone.contentratings',
+          'collective.seeder',
+          'collective.piwik.flowplayer',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
