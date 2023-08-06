31265d497b18e47a181735e09208f34e26e3e420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:40 +0000

    Add package huTools/0.60p1/huTools-0.60p1.tar.gz

---
+++ packages/huTools/huTools-0.60p1.tar.gz/huTools-0.60p1/huTools/protocols.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+protocols.py - handle data confoming the the Hudora standard protocols.
+See http://github.com/hudora/huTools/tree/master/doc/standards for details.
+
+Created by Maximillian Dornseif on 2009-12-27.
+Copyright (c) 2009 HUDORA. All rights reserved.
+
+Code is based on http://code.activestate.com/recipes/573463/
+"""
+
+import warnings
+warnings.warn("huTools.protocols is deprecated use cs.messaging", DeprecationWarning, stacklevel=2)
+
+from huTools.structured import *
---
+++ packages/huTools/huTools-0.60p1.tar.gz/huTools-0.60p1/huTools/structured.py
@@ -0,0 +1,311 @@
+# encoding: utf-8
+
+"""
+structured.py - handle structured data/dicts/objects
+"""
+
+# Created by Maximillian Dornseif on 2009-12-27.
+# Created by Maximillian Dornseif on 2010-06-04.
+# Copyright (c) 2009, 2010 HUDORA. All rights reserved.
+
+
+import xml.etree.cElementTree as ET
+import os.path
+import sys
+import collections
+import logging
+
+# TODO: move to hujson
+try:
+    from django.utils import simplejson as json # Google appengine
+except:
+    import simplejson as json
+
+
+# siehe http://stackoverflow.com/questions/1305532/convert-python-dict-to-object
+class Struct(object):
+    def __init__(self, entries, default=None):
+        self.__dict__.update(entries)
+        self.default = default
+
+    def __getattr__(self, name):
+        if name.startswith('_'):
+            # copy expects __deepcopy__, __getnewargs__ to raise AttributeError
+            # see http://groups.google.com/group/comp.lang.python/browse_thread/thread/6ac8a11de4e2526f/e76b9fbb1b2ee171?#e76b9fbb1b2ee171
+            raise AttributeError("'<Struct>' object has no attribute '%s'" % name)
+        return self.default
+
+    #def __setattr__(self, name, value):
+    #    raise TypeError('Struct objects are immutable')
+
+
+def make_struct(obj, default=None):
+    """Converts a dict to an object, leaves objects untouched.
+
+    Someting like obj.vars() = dict() - Read Only!
+
+    >>> obj = make_struct(dict(foo='bar'))
+    >>> obj.foo
+    'bar'
+
+    make_struct leaves objects alone.
+    >>> class MyObj(object): pass
+    >>> data = MyObj()
+    >>> data.foo = 'bar'
+    >>> obj = make_struct(data)
+    >>> obj.foo
+    'bar'
+
+    make_struct also is idempotent
+    >>> obj = make_struct(make_struct(dict(foo='bar')))
+    >>> obj.foo
+    'bar'
+
+    """
+    if (not hasattr(obj, '__dict__')) and hasattr(obj, 'iterkeys'):
+        # this should be a dict
+        struc = Struct(obj, default=default)
+        # handle recursive sub-dicts
+        for k, v in obj.items():
+            setattr(struc, k, make_struct(v))
+        return struc
+    else:
+        return obj
+
+
+# Code is based on http://code.activestate.com/recipes/573463/
+def _convert_dict_to_xml_recurse(parent, dictitem, listnames):
+    # we can't convert bare lists
+    assert not isinstance(dictitem, list)
+
+    if isinstance(dictitem, dict):
+        for (tag, child) in sorted(dictitem.iteritems()):
+            if isinstance(child, list):
+                # iterate through the array and convert
+                listelem = ET.Element(tag)
+                parent.append(listelem)
+                for listchild in child:
+                    elem = ET.Element(listnames.get(tag, 'item'))
+                    listelem.append(elem)
+                    _convert_dict_to_xml_recurse(elem, listchild, listnames)
+            else:
+                elem = ET.Element(tag)
+                parent.append(elem)
+                _convert_dict_to_xml_recurse(elem, child, listnames)
+    elif not dictitem is None:
+        parent.text = unicode(dictitem)
+
+
+def dict2et(xmldict, roottag='data', listnames=None):
+    """Converts a dict to an Elementtree.
+
+    Converts a dictionary to an XML ElementTree Element::
+
+    >>> data = {"nr": "xq12", "positionen": [{"m": 12}, {"m": 2}]}
+    >>> root = dict2et(data)
+    >>> ET.tostring(root)
+    '<data><nr>xq12</nr><positionen><item><m>12</m></item><item><m>2</m></item></positionen></data>'
+
+    Per default ecerything ins put in an enclosing '<data>' element. Also per default lists are converted
+    to collecitons of `<item>` elements. But by provding a mapping between list names and element names,
+    you van generate different elements::
+
+    >>> data = {"positionen": [{"m": 12}, {"m": 2}]}
+    >>> root = dict2et(data, roottag='xml')
+    >>> ET.tostring(root)
+    '<xml><positionen><item><m>12</m></item><item><m>2</m></item></positionen></xml>'
+
+    >>> root = dict2et(data, roottag='xml', listnames={'positionen': 'position'})
+    >>> ET.tostring(root)
+    '<xml><positionen><position><m>12</m></position><position><m>2</m></position></positionen></xml>'
+
+    >>> data = {"kommiauftragsnr":2103839, "anliefertermin":"2009-11-25", "prioritaet": 7,
+    ... "ort": u"Hücksenwagen",
+    ... "positionen": [{"menge": 12, "artnr": "14640/XL", "posnr": 1},],
+    ... "versandeinweisungen": [{"guid": "2103839-XalE", "bezeichner": "avisierung48h",
+    ...                          "anweisung": "48h vor Anlieferung unter 0900-LOGISTIK avisieren"},
+    ... ]}
+
+    >>> print ET.tostring(dict2et(data, 'kommiauftrag',
+    ... listnames={'positionen': 'position', 'versandeinweisungen': 'versandeinweisung'}))
+    ...  # doctest: +SKIP
+    '''<kommiauftrag>
+    <anliefertermin>2009-11-25</anliefertermin>
+    <positionen>
+        <position>
+            <posnr>1</posnr>
+            <menge>12</menge>
+            <artnr>14640/XL</artnr>
+        </position>
+    </positionen>
+    <ort>H&#xC3;&#xBC;cksenwagen</ort>
+    <versandeinweisungen>
+        <versandeinweisung>
+            <bezeichner>avisierung48h</bezeichner>
+            <anweisung>48h vor Anlieferung unter 0900-LOGISTIK avisieren</anweisung>
+            <guid>2103839-XalE</guid>
+        </versandeinweisung>
+    </versandeinweisungen>
+    <prioritaet>7</prioritaet>
+    <kommiauftragsnr>2103839</kommiauftragsnr>
+    </kommiauftrag>'''
+    """
+
+    if not listnames:
+        listnames = {}
+    root = ET.Element(roottag)
+    _convert_dict_to_xml_recurse(root, xmldict, listnames)
+    return root
+
+
+def list2et(xmllist, root, elementname):
+    """Converts a list to an Elementtree.
+
+        See also dict2et()
+    """
+
+    basexml = dict2et({root: xmllist}, 'xml', listnames={root: elementname})
+    return basexml.find(root)
+
+
+def dict2xml(datadict, roottag='data', listnames=None, pretty=False):
+    """Converts a dictionary to an UTF-8 encoded XML string.
+
+    See also dict2et()
+    """
+    tree = dict2et(datadict, roottag, listnames)
+    if pretty:
+        indent(tree)
+    return ET.tostring(tree, 'utf-8')
+
+
+def list2xml(datalist, root, elementname, pretty=False):
+    """Converts a list to an UTF-8 encoded XML string.
+
+    See also dict2et()
+    """
+    tree = list2et(datalist, root, elementname)
+    if pretty:
+        indent(tree)
+    return ET.tostring(tree, 'utf-8')
+
+
+# From http://effbot.org/zone/element-lib.htm
+# prettyprint: Prints a tree with each node indented according to its depth. This is 
+# done by first indenting the tree (see below), and then serializing it as usual.
+# indent: Adds whitespace to the tree, so that saving it as usual results in a prettyprinted tree.
+# in-place prettyprint formatter
+
+def indent(elem, level=0):
+    i = "\n" + level*" "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + " "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for child in elem:
+            indent(child, level+1)
+        if not child.tail or not child.tail.strip():
+            child.tail = i
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+
+def test():
+    # warenzugang
+    data = {"guid":"3104247-7",
+            "menge":7,
+            "artnr":"14695",
+            "batchnr": "3104247"}
+    xmlstr = dict2xml(data, roottag='warenzugang')
+    #print xmlstr
+    assert xmlstr == '''<warenzugang><artnr>14695</artnr><batchnr>3104247</batchnr><guid>3104247-7</guid><menge>7</menge></warenzugang>'''
+
+    data = {"kommiauftragsnr":2103839,
+     "anliefertermin":"2009-11-25",
+     "fixtermin": True,
+     "prioritaet": 7,
+     "info_kunde":"Besuch H. Gerlach",
+     "auftragsnr":1025575,
+     "kundenname":"Ute Zweihaus 400424990",
+     "kundennr":"21548",
+     "name1":"Uwe Zweihaus",
+     "name2":"400424990",
+     "name3":"",
+     u"strasse":u"Bahnhofstr. 2",
+     "land":"DE",
+     "plz":"42499",
+     "ort": u"Hücksenwagen",
+     "positionen": [{"menge": 12,
+                     "artnr": "14640/XL",
+                     "posnr": 1},
+                    {"menge": 4,
+                     "artnr": "14640/03",
+                     "posnr": 2},
+                    {"menge": 2,
+                     "artnr": "10105",
+                     "posnr": 3}],
+     "versandeinweisungen": [{"guid": "2103839-XalE",
+                              "bezeichner": "avisierung48h",
+                              "anweisung": "48h vor Anlieferung unter 0900-LOGISTIK avisieren"},
+                             {"guid": "2103839-GuTi",
+                              "bezeichner": "abpackern140",
+                              "anweisung": u"Paletten höchstens auf 140 cm Packen"}]
+    }
+
+    xmlstr = dict2xml(data, roottag='kommiauftrag')
+    # print xmlstr
+
+    # Rückmeldung
+    data = {"kommiauftragsnr":2103839,
+     "positionen": [{"menge": 4,
+                     "artnr": "14640/XL",
+                     "posnr": 1,
+                     "nve": "23455326543222553"},
+                    {"menge": 8,
+                     "artnr": "14640/XL",
+                     "posnr": 1,
+                     "nve": "43255634634653546"},
+                    {"menge": 4,
+                     "artnr": "14640/03",
+                     "posnr": 2,
+                     "nve": "43255634634653546"},
+                    {"menge": 2,
+                     "artnr": "10105",
+                     "posnr": 3,
+                     "nve": "23455326543222553"}],
+     "nves": [{"nve": "23455326543222553",
+               "gewicht": 28256,
+               "art": "paket"},
+              {"nve": "43255634634653546",
+               "gewicht": 28256,
+                "art": "paket"}]}
+
+    xmlstr = dict2xml(data, roottag='rueckmeldung')
+    #print xmlstr
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    d = make_struct({
+        'item1': 'string',
+        'item2': ['dies', 'ist', 'eine', 'liste'],
+        'item3': dict(dies=1, ist=2, ein=3, dict=4),
+        'item4': 10,
+        'item5': [dict(dict=1, in_einer=2, liste=3)]})
+    type(d)
+    d.item1
+    d.item2
+    d.item3
+    d.item3.dies
+    d.item3.ist
+    d.item3.ein
+    d.item3.dict
+    d.item4
+    d.item5
+
+    test()
