73740bdfb3a0dfc8ef391f12788b6b30c93de189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:40 +0000

    Add package django-follow/0.1/django-follow-0.1.tar.gz

---
+++ packages/django-follow/django-follow-0.1.tar.gz/django-follow-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup,find_packages
+
+setup(
+    name='django-follow',
+    description='Application which enables following features for users. Can be used for contact books or whatnot',
+    packages=find_packages(),
+    author='Alen Mujezinovic',
+    author_email='alen@caffeinehit.com',
+    version='0.1',
+)
