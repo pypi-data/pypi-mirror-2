44ef29c936c28be0ccddb71d61a5d7a3dfc93f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:38 +0000

    Add package staste/0.1.1a/staste-0.1.1a-py2.7.egg

---
+++ packages/staste/staste-0.1.1a-py2.7.egg/staste/axis.py
@@ -0,0 +1,40 @@
+from staste import redis
+
+class Axis(object):
+    def __init__(self, choices):
+        self.choices = choices
+
+    def get_field_id_parts(self, value):
+        if not value in self.choices:
+            raise ValueError('Invalid value: %s, choices are: %s'
+                             % (value, self.choices))
+        
+        return ['__all__', str(value)]
+    
+
+    def get_field_main_id(self, value):
+        if not value:
+            return '__all__'
+
+        return str(value)
+
+    def get_keys(self, key):
+        return self.choices
+
+
+
+class StoredChoiceAxis(Axis):
+    """An Axis for which you don't know values in advance
+
+    Stores all values in a set at some key in Redis"""
+
+    store_choice = True
+
+    def __init__(self):
+        pass
+
+    def get_field_id_parts(self, value):
+        return ['__all__', str(value)]
+
+    def get_keys(self, key):
+        return redis.smembers(key)
