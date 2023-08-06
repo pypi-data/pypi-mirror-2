c80b951c8b6427a00bf98131813ee96b52cfe104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:20 +0000

    Add package dailyscripts/0.0.4/dailyscripts-0.0.4.tar.gz

---
+++ packages/dailyscripts/dailyscripts-0.0.4.tar.gz/dailyscripts-0.0.4/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python 
+try: 
+    from setuptools import setup, find_packages 
+except ImportError: 
+    from ez_setup import use_setuptools 
+    use_setuptools() 
+    from setuptools import setup, find_packages 
+
+setup( name = 'dailyscripts',
+    version = '0.0.4',
+    scripts = [
+        'filesystem/unpackall', 
+        'filesystem/shuffle',
+        'filesystem/flatten'
+    ],
+
+    zip_safe = True,
+
+
+    #Metadata for PyPI
+    url = 'http://github.com/spajus/dailyscripts/',
+    author = 'Tomas Varaneckas',
+    author_email = 'tomas.varaneckas@gmail.com',
+    description = 'Daily Scripts. Utilities for every geek.',
+    long_description = 'Daily Scripts. Utilities for every geek.',
+    license = 'GPLv3',
+    keywords = ['utils'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Archiving',
+        'Topic :: Utilities'
+    ],
+)
