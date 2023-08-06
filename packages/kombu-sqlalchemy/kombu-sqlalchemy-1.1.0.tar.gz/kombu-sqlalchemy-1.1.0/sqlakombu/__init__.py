63949b0794918f0b96484903e311d9af9ccc3be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:25 +0000

    Add package kombu-sqlalchemy/1.1.0/kombu-sqlalchemy-1.1.0.tar.gz

---
+++ packages/kombu-sqlalchemy/kombu-sqlalchemy-1.1.0.tar.gz/kombu-sqlalchemy-1.1.0/sqlakombu/__init__.py
@@ -0,0 +1,8 @@
+"""Kombu transport using SQLAlchemy as the message store."""
+VERSION = (1, 1, 0)
+__version__ = ".".join(map(str, VERSION))
+__author__ = "Ask Solem"
+__contact__ = "ask@celeryproject.org"
+__homepage__ = "http://github.com/ask/kombu-sqlalchemy/"
+__docformat__ = "restructuredtext"
+__license__ = "BSD"
