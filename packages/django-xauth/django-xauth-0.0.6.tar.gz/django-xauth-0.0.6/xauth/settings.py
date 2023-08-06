f5ebe0b7c17fc8081f02664910326f6d77b62ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:00 +0000

    Add package django-xauth/0.0.6/django-xauth-0.0.6.tar.gz

---
+++ packages/django-xauth/django-xauth-0.0.6.tar.gz/django-xauth-0.0.6/xauth/settings.py
@@ -0,0 +1,11 @@
+XAUTH_SERVICES = (
+    'openid',
+    'oauth1',
+    'oauth2',
+)
+XAUTH_HOSTNAME = 'localhost:8000'
+XAUTH_SIGNUP_SUCCESS_URL = '/'
+XAUTH_SIGNIN_SUCCESS_URL = '/'
+XAUTH_LOGOUT_URL = '/'
+XAUTH_ACCOUNT_FORM = 'xauth.forms.AccountForm'
+XAUTH_PROFILE_FIELDS = ['nickname', 'email']
