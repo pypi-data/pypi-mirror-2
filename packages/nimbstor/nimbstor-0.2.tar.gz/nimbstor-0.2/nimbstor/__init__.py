cbdd85eaca3e5c14a086327decb1223ac87ade19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:16 +0000

    Add package nimbstor/0.2/nimbstor-0.2.tar.gz

---
+++ packages/nimbstor/nimbstor-0.2.tar.gz/nimbstor-0.2/nimbstor/__init__.py
@@ -0,0 +1,5 @@
+from stor import stor_output, stor_input, stor_util
+from directory_backend import directory_backend
+from combine_backend import combine_backend
+from imap_backend import imap_backend
+import adler64
---
+++ packages/nimbstor/nimbstor-0.2.tar.gz/nimbstor-0.2/nimbstor/directory_backend.py
@@ -0,0 +1,40 @@
+#!/usr/pkg/bin/python2.6 -O
+
+import os, sys, random, hashlib
+
+def read_directory_content(directory):
+  content = []
+  for fname in os.listdir(directory):
+    number, checksum1, checksum2 = fname.split('.')
+    number = int(number)
+    content.append((number, checksum1, checksum2))
+  return content
+
+class directory_backend:
+  def __init__(self, nimbstor, directory):
+    self._directory = directory
+    self._nimbstor = nimbstor
+    for number, checksum1, checksum2 in read_directory_content(directory):
+      self._nimbstor.append_block_info(number, checksum1, checksum2, self)
+
+  def activate(self):
+    self._nimbstor.open(self)
+
+  def write_buffer(self, buffer, number, checksum1, checksum2):
+    fname = os.path.join(self._directory, "%07d.%s.%s" % (number, checksum1, checksum2))
+    fd = open(fname, 'w')
+    fd.write(buffer)
+    fd.close()
+    self._nimbstor.append_block_info(number, checksum1, checksum2, self)
+
+  def read_buffer(self, number, checksum1, checksum2, decode_func):
+    fname = os.path.join(self._directory, "%07d.%s.%s" % (number, checksum1, checksum2))
+    fd = open(fname, 'r')
+    buffer = fd.read()
+    fd.close()
+    return decode_func(buffer)
+
+  def close(self):
+    pass
+
+__all__ = ('directory_backend',)
