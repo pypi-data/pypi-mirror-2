51ba3695db3bedfe47ba10f6823e6db385471241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:39 +0000

    Add package asv_utils/dev-20110624-01/asv_utils-dev-20110624-01.tar.gz

---
+++ packages/asv_utils/asv_utils-dev-20110624-01.tar.gz/asv_utils-dev-20110624-01/asv_utils/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#import os
+
+__name__ = 'asv_utils'
+__version__ = 'dev-20110624-01'
+__keywords__ = 'django asv utils'
+__description__ = '''
+This is a set of common functions for other asv.* libs.
+'''
+
+#if os.getenv('DJANGO_SETTINGS_MODULE'): 
+#        from asv_imgs.settings import Settings
+#            settings = Settings()
+#
