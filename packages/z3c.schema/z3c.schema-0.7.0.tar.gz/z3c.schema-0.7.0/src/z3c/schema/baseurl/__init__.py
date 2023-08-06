5f6acca1424d00bbd66d92c40b5f37b30eeac827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package z3c.schema/0.7.0/z3c.schema-0.7.0.tar.gz

---
+++ packages/z3c.schema/z3c.schema-0.7.0.tar.gz/z3c.schema-0.7.0/src/z3c/schema/baseurl/__init__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: __init__.py 71299 2006-11-24 04:32:24Z rogerineichen $
+"""
+
+from z3c.schema.baseurl.interfaces import *
+from z3c.schema.baseurl.field import isValidBaseURL
+from z3c.schema.baseurl.field import BaseURL
---
+++ packages/z3c.schema/z3c.schema-0.7.0.tar.gz/z3c.schema-0.7.0/src/z3c/schema/hostname/tests.py
@@ -0,0 +1,59 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from zope.schema.interfaces import RequiredMissing
+from zope.schema.tests.test_field import FieldTestBase
+import doctest
+import unittest
+
+from z3c.schema.hostname import HostName
+from z3c.schema.hostname import InvalidHostName
+
+
+class HostNameTest(FieldTestBase):
+
+    _Field_Factory = HostName
+    _convert = str
+
+    def testValidate(self):
+        field = self._Field_Factory(title=u'HostName field', description=u'',
+            readonly=False, required=False)
+        field.validate(None)
+        field.validate(self._convert('host'))
+        field.validate(self._convert('123.123.123.123'))
+        field.validate(self._convert('host:123'))
+        field.validate(self._convert('www.host.com:123'))
+        field.validate(self._convert('123.123.123.123:123'))
+
+    def testValidateRequired(self):
+        field = self._Field_Factory(title=u'HostName field', description=u'',
+            readonly=False, required=True)
+        field.validate(self._convert('host'))
+        self.assertRaises(RequiredMissing, field.validate, None)
+
+    def testBadStringType(self):
+        field = self._Field_Factory(title=u'HostName field')
+        self.assertRaises(InvalidHostName, field.validate, u'123.123')
+
+    def test_newlines(self):
+        field = self._Field_Factory(title=u'HostName field')
+        self.assertRaises(InvalidHostName, field.validate,
+            self._convert('host\nfoo'))
+
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('README.txt',
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,),
+        unittest.makeSuite(HostNameTest),
+        ))
