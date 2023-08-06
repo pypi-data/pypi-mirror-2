bfa756a21bca0df2a0f912f3417ac729add5761f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:18 +0000

    Add package gtt-python-client/0.2a1/gtt-python-client-0.2a1.tar.gz

---
+++ packages/gtt-python-client/gtt-python-client-0.2a1.tar.gz/gtt-python-client-0.2a1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gtt-python-client',
+    version='0.2a1',
+    description='Python bindings for Google Translator Toolkit API.',
+    long_description=open('README.md').read(),
+    author='Yury Yurevich',
+    author_email='yyurevich@jellycrystal.com',
+    url='https://github.com/jellycrystal/gtt-python-client',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=False,
+    zip_safe=True,
+    # Get more strings from http://www.python.org/pypi?:action=list_classifiers
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
+
