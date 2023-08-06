bf37f1f9ac0731591ccf55a03363cb08dc60b307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:13 +0000

    Add package blexnest/1.3.0/blexnest-1.3.0.tar.gz

---
+++ packages/blexnest/blexnest-1.3.0.tar.gz/blexnest-1.3.0/blexnest.py
@@ -0,0 +1,11 @@
+#The Nester module from HFPython.
+
+def print_lol(the_list, indent=False, level=0):
+    for each_item in the_list:
+        if isinstance(each_item, list) == True:
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end="")
+            print(each_item)
