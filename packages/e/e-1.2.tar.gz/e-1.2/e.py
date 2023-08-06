3d0c3dc1efe6027f7ac632b08bb178fbfdbb83ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:20 +0000

    Add package e/1.2/e-1.2.tar.gz

---
+++ packages/e/e-1.2.tar.gz/e-1.2/e.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+# Public domain
+# Idea from Georg Brandl. Foolishly implemented by Michael Foord
+# E-mail: fuzzyman AT voidspace DOT org DOT uk
+
+import sys
+
+args = sys.argv[1:]
+
+def execute(arg):
+    exec (compile(arg, '<cmdline>', 'single'))
+
+if not args:
+    print ('Idea from Georg Brandl. Foolishly implemented by Michael Foord')
+    sys.exit()
+
+first = args[0]
+try:
+    execute(first)
+except NameError:
+    try:
+        __import__(first)
+    except ImportError:
+        # easy way to re-raise the original error
+        execute(first)
+    else:
+        try:
+            mod = sys.modules[first]
+        except KeyError:
+            print ('%s is not a valid module name' % first)
+            sys.exit(1)
+        
+        location = getattr(mod, '__file__', 'None')
+        if location.endswith('.pyc'):
+            location = location[:-1]
+        print (location)
+
+
+
+for x in args[1:]:
+   execute(x)
