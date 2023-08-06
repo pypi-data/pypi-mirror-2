8935386ea9393025f1fdf45e70d7e0e9b0b7b2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:07 +0000

    Add package python-googl/0.2/python-googl-0.2.tar.gz

---
+++ packages/python-googl/python-googl-0.2.tar.gz/python-googl-0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+packages=find_packages()
+
+setup(
+    name='python-googl',
+    version='0.2',
+    description='Python goo.gl url shortener wrapper',
+    author='Vince Spicer',
+    author_email='vinces1979@gmail.com',
+    url='http://code.google.com/p/python-googl',
+    install_requires=[
+        "httplib2",
+        ],
+    zip_safe=False,
+    packages=packages,
+    keywords=['google', 'url', 'googl'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+
+
+)
+
