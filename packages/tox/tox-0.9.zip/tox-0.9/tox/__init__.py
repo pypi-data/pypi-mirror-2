bafe658ce306298330f39b8f72880541e0ebcac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:38 +0000

    Add package tox/0.9/tox-0.9.zip

---
+++ packages/tox/tox-0.9.zip/tox-0.9/tox/__init__.py
@@ -0,0 +1,17 @@
+#
+__version__ = '0.9'
+
+from py.apipkg import initpkg
+
+initpkg(__name__, dict(
+    cmdline   = '._cmdline:main',
+    exception = dict(
+        ConfigError = '._config:ConfigError',
+        InvocationError = '._exception:InvocationError',
+        MissingDependency= '._exception:MissingDependency',
+        UnsupportedInterpreter = '._exception:UnsupportedInterpreter',
+        InterpreterNotFound = '._exception:InterpreterNotFound',
+        MissingFile = '._exception:MissingFile',
+        MissingDirectory = '._exception:MissingDirectory',
+    )
+))
