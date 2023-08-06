b46d33b3c4274bf0f78d685a92b52d458d71eb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:32 +0000

    Add package ftw.dashboard.portlets.recentlymodified/1.2.1/ftw.dashboard.portlets.recentlymodified-1.2.1.tar.gz

---
+++ packages/ftw.dashboard.portlets.recentlymodified/ftw.dashboard.portlets.recentlymodified-1.2.1.tar.gz/ftw.dashboard.portlets.recentlymodified-1.2.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = open("ftw/dashboard/portlets/recentlymodified/version.txt").read().strip()
+
+setup(name='ftw.dashboard.portlets.recentlymodified',
+      version=version,
+      description="recentlymodified portlet",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='philippegross',
+      author_email='mailto:info@4teamwork.ch',
+      url='http://plone.org/products/ftw.dashboard.portlets.recentlymodified/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', 'ftw.dashboard', 'ftw.dashboard.portlets'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.pipbox',
+          'collective.js.jqsmartTruncation',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+        # -*- Entry points: -*-
+        [z3c.autoinclude.plugin]
+        target = plone
+        """,
+      )
