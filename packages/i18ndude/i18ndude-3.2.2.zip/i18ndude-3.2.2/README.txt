d0aa82135f24ef883870b816ee1d38aa37bff1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:10 +0000

    Add package i18ndude/3.2.2/i18ndude-3.2.2.zip

---
+++ packages/i18ndude/i18ndude-3.2.2.zip/i18ndude-3.2.2/README.txt
@@ -0,0 +1,7 @@
+i18ndude
+========
+
+Overview
+--------
+
+Call i18ndude with the --help argument to see what it can do for you.
---
+++ packages/i18ndude/i18ndude-3.2.2.zip/i18ndude-3.2.2/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os, sys
+
+version = '3.2.2'
+      
+install_requires = [
+  'zope.tal >= 3.5.2',
+  'zope.interface >= 3.3',
+  'zope.i18nmessageid >= 3.3',
+  'plone.i18n',
+  'ordereddict',
+]
+
+if sys.version_info < (2, 5):
+    install_requires.append('elementtree')
+
+setup(name='i18ndude',
+      version=version,
+      description="i18ndude performs various tasks related to ZPT's, Python Scripts and i18n.",
+      long_description=open("README.txt").read() + "\n" + \
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Plone',
+        'Framework :: Zope2',
+        'Framework :: Zope3',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        ],
+      keywords='Plone i18n zpt',
+      author='Daniel Nouri',
+      author_email='plone-i18n@lists.sourceforge.net',
+      maintainer='Vincent Fretin',
+      maintainer_email='vincent.fretin@gmail.com',
+      url='http://pypi.python.org/pypi/i18ndude',
+      license='GPL',
+      package_dir={'': 'src'},
+      packages=find_packages('src', exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points="""
+      [console_scripts]
+      i18ndude=i18ndude.script:main
+      """,
+      )
