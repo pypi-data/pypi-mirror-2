4772f1e8e914989a3b509a131523940ba3a78bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:48 +0000

    Add package httpagentparser/0.8.3/httpagentparser-0.8.3.tar.gz

---
+++ packages/httpagentparser/httpagentparser-0.8.3.tar.gz/httpagentparser-0.8.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='httpagentparser',
+    version='0.8.3',
+    description='Extracts OS Browser etc information from http user agent string',
+    packages=find_packages(),
+    author='Shekhar Tiwatne',
+    author_email='pythonic@gmail.com',
+    url="http://flavors.me/shon",
+    license="http://www.opensource.org/licenses/mit-license.php",
+    )
+
