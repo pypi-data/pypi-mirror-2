09cebb7b03053832c50bc9aa8e8b91202299df00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:40 +0000

    Add package pyzmq/2.0.10.1/pyzmq-2.0.10.1.tar.gz

---
+++ packages/pyzmq/pyzmq-2.0.10.1.tar.gz/pyzmq-2.0.10.1/zmq/core/version.pyx
@@ -0,0 +1,54 @@
+"""PyZMQ and 0MQ version functions."""
+
+#
+#    Copyright (c) 2010 Brian E. Granger
+#
+#    This file is part of pyzmq.
+#
+#    pyzmq is free software; you can redistribute it and/or modify it under
+#    the terms of the Lesser GNU General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    pyzmq is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    Lesser GNU General Public License for more details.
+#
+#    You should have received a copy of the Lesser GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+from czmq cimport _zmq_version
+
+#-----------------------------------------------------------------------------
+# Code
+#-----------------------------------------------------------------------------
+
+__version__ = '2.0.10.1'
+
+
+def pyzmq_version():
+    """pyzmq_version()
+
+    Return the version of pyzmq.
+    """
+    return __version__
+
+
+def zmq_version():
+    """zmq_version()
+
+    Return the version of ZeroMQ itself.
+    """
+    cdef int major, minor, patch
+    _zmq_version(&major, &minor, &patch)
+    return '%i.%i.%i' % (major, minor, patch)
+
+
+__all__ = ['zmq_version', 'pyzmq_version', '__version__']
+
