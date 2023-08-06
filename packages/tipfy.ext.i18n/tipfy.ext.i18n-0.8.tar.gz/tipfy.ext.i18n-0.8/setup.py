d65f6b9f36c5db40360dd4e60bb01c9ba6ecd40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:50 +0000

    Add package tipfy.ext.i18n/0.8/tipfy.ext.i18n-0.8.tar.gz

---
+++ packages/tipfy.ext.i18n/tipfy.ext.i18n-0.8.tar.gz/tipfy.ext.i18n-0.8/setup.py
@@ -0,0 +1,56 @@
+"""
+tipfy.ext.i18n
+==============
+
+This extension provides internationalization and localization utilities for
+`tipfy <http://www.tipfy.org/>`_.
+
+Links
+-----
+* `Source Code Repository <http://code.google.com/p/tipfy-ext-i18n/>`_
+* `Issue Tracker <http://code.google.com/p/tipfy-ext-i18n/issues/list>`_
+
+About tipfy
+-----------
+* `Home page <http://www.tipfy.org/>`_
+* `Extension list <http://www.tipfy.org/wiki/extensions/>`_
+* `Discussion Group <http://groups.google.com/group/tipfy>`_
+"""
+from setuptools import setup
+
+setup(
+    name = 'tipfy.ext.i18n',
+    version = '0.8',
+    license = 'BSD',
+    url = 'http://www.tipfy.org/',
+    description = 'Internationalization extension for tipfy',
+    long_description = __doc__,
+    author = 'Rodrigo Moraes',
+    author_email = 'rodrigo.moraes@gmail.com',
+    zip_safe = False,
+    platforms = 'any',
+    packages = [
+        'tipfy',
+        'tipfy.ext',
+    ],
+    namespace_packages = [
+        'tipfy',
+        'tipfy.ext',
+    ],
+    include_package_data = True,
+    install_requires = [
+        'tipfy',
+        'babel',
+        'gaepytz',
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
