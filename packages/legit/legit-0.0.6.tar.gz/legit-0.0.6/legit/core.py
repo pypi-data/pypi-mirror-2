71e89fbcde2e569ead620ccb0dc3a2dea4e3a3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:43 +0000

    Add package legit/0.0.6/legit-0.0.6.tar.gz

---
+++ packages/legit/legit-0.0.6.tar.gz/legit-0.0.6/legit/core.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+legit.core
+~~~~~~~~~~
+
+This module provides the basic functionality of legit.
+"""
+
+# from clint import resources
+
+__version__ = '0.0.6'
+__author__ = 'Kenneth Reitz'
+__license__ = 'BSD'
+
+
+# resources.init('kennethreitz', 'legit')
+# resources.user.write('config.ini', "we'll get there.")
