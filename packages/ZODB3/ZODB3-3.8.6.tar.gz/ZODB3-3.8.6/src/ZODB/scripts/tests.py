b26e50f28466531a540f56630867754cfb818345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:36 +0000

    Add package ZODB3/3.8.6/ZODB3-3.8.6.tar.gz

---
+++ packages/ZODB3/ZODB3-3.8.6.tar.gz/ZODB3-3.8.6/src/ZODB/scripts/tests.py
@@ -0,0 +1,28 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""XXX short summary goes here.
+
+$Id: tests.py 113732 2010-06-21 15:31:11Z ctheune $
+"""
+import unittest
+from zope.testing import doctest
+import ZODB.tests.util
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('referrers.txt', 'strip_versions.test',
+            setUp=ZODB.tests.util.setUp, tearDown=ZODB.tests.util.tearDown,
+            ),
+        ))
+
