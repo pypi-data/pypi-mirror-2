fbbaeaefc4d98c485734eea7bd04502724796341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:01 +0000

    Add package coding/0.002/coding-0.002.tar.gz

---
+++ packages/coding/coding-0.002.tar.gz/coding-0.002/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 K. Richard Pixley.
+# See LICENSE for details.
+#
+# Time-stamp: <13-Jan-2011 18:29:23 PST by rich@noir.com>
+
+import os
+
+# try:
+#     from setuptools import setup, find_packages
+# except ImportError:
+#     from ez_setup import use_setuptools
+#     use_setuptools()
+#     from setuptools import setup, find_packages
+
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='coding',
+    version='0.002',
+    author='K. Richard Pixley',
+    author_email='rich@noir.com',
+    description='An answer to the question of python enums.',
+    license='MIT',
+    keywords='enum coding',
+    url='http://bitbucket.org/krp/coding',
+    long_description=read(os.path.join('README.rst')),
+    setup_requires=[
+    	'nose>=1.0.0',
+#        'sphinx>=1.0.5',
+    ],
+    install_requires=[
+    ],
+    py_modules=['coding'],
+    test_suite='nose.collector',
+    scripts = [
+    ],
+    requires=[
+    ],
+    provides=[
+        'coding',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
