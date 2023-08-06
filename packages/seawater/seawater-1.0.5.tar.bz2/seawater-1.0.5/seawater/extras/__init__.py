4edd3f0b33dc5e0b2e3af7605d9d3e8c9879970d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:14 +0000

    Add package seawater/1.0.5/seawater-1.0.5.tar.bz2

---
+++ packages/seawater/seawater-1.0.5.tar.bz2/seawater-1.0.5/seawater/extras/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+seawater.extras
+===============
+
+Just some extra handy functions to complement the origininal seawater.
+"""
+
+import sw_extras
+
+__authors__    = ["Filipe Fernandes"]
+__copyright__  = ["left"]
+__credits__    = ["Filipe Fernandes"]
+__license__    = ["BSD"]
+__version__    = ["1.0.5"]
+__maintainer__ = ["Filipe Fernandes"]
+__email__      = ["ocefpaf@gmail.com"]
+__status__     = ["Production"]
+__all__        = ['sw_extras']
+__web__        = ['http://ocefpaf.tiddlyspot.com/']
+__created__    = ["14-Jan-2010"]
+__modified__   = ["17-Aug-2010"]
---
+++ packages/seawater/seawater-1.0.5.tar.bz2/seawater-1.0.5/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Environment :: Console
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+Intended Audience :: Education
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Scientific/Engineering
+Topic :: Education
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+setup(name             = 'seawater',
+      version          = '1.0.5',
+      author           = 'Filipe Fernandes',
+      author_email     = 'ocefpaf@gmail.com',
+      maintainer       = 'Filipe Fernandes',
+      maintainer_email = 'ocefpaf@gmail.com',
+      url              = 'http://ocefpaf.tiddlyspot.com/#python-seawater',
+      description      = 'Seawater Libray for Python',
+      long_description = """\
+This module is a translation of the original SEAWATER-3.2 MATLAB toolkit routines for calculating the properties of sea water. It consists of a self contained library and is extremely easy to use.
+""",
+      download_url     = 'http://pypi.python.org/packages/source/s/seawater/',
+      packages         = ['seawater', 'seawater.extras'],
+      classifiers      = filter(None, classifiers.split("\n")),
+      platforms        = 'any',
+      license          = 'MIT',
+      keywords         = 'oceanography seawater',
+    )
