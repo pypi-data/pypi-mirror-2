5c401941ba53e1a0c56b278bfd19d13cd3d0442c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:43 +0000

    Add package bluetrain/0.9.6.1/bluetrain-0.9.6.1.zip

---
+++ packages/bluetrain/bluetrain-0.9.6.1.zip/bluetrain-0.9.6.1/bluetrain/templatetags/page_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from django.conf import settings
+from django.template import Context, Template
+from django.template.loader import get_template
+
+register = template.Library()
+
+try:
+    FORM_TEMPLATE = settings.BLUETRAIN_DEFAULT_FORM
+except NameError:
+    FORM_TEMPLATE = 'pages/default_form.html'
+
+@register.filter
+def as_default_form(form):
+    template = get_template(FORM_TEMPLATE)
+    c = Context({'form':form})
+    return template.render(c)
