21ab2b9587d977320df607c0373739efbeeb30cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:38 +0000

    Add package python-chuck-norris/0.2/python-chuck-norris-0.2.tar.gz

---
+++ packages/python-chuck-norris/python-chuck-norris-0.2.tar.gz/python-chuck-norris-0.2/chuck.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '0.2'
+
+
+class RoundHouseKick(Exception):
+    pass
+
+
+def fail():
+    raise RoundHouseKick('Fix your test. Chuck Norris never fails.')
+
+
+def assert_true(value, msg=None):
+    pass
+
+
+def assert_false(value, msg=None):
+    pass
+
+
+def assert_raises(excClass, callableObj, *args, **kwargs):
+    pass
+
+
+def assert_equals(first, second, msg=None):
+    pass
+
+
+def assert_not_equals(first, second, msg=None):
+    pass
