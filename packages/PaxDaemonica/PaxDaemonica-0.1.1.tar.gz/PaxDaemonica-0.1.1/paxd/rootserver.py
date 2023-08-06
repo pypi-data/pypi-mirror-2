724d9bf093be88b44050a38a9df0e61c2803a51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:09 +0000

    Add package PaxDaemonica/0.1.1/PaxDaemonica-0.1.1.tar.gz

---
+++ packages/PaxDaemonica/PaxDaemonica-0.1.1.tar.gz/PaxDaemonica-0.1.1/paxd/rootserver.py
@@ -0,0 +1,8 @@
+import time
+import os.path
+# from appserver import Application
+from paxd.server.controller import Controller
+
+if __name__ == '__main__':
+    c = Controller('3489y3ngyqr87gy32r8fnyehwiufhe98')
+    c.run()
---
+++ packages/PaxDaemonica/PaxDaemonica-0.1.1.tar.gz/PaxDaemonica-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Python Application/Worker Server'
+
+setup(
+    name='PaxDaemonica',
+    version=VERSION,
+    description=DESCRIPTION,
+    author='Jeffrey Jenkins',
+    license='MIT',
+    author_email='jeff@qcircles.net',
+    url='http://github.com/jeffjenkins/PaxDemonica',
+    packages=['paxd'],
+    install_requires=['redis'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # 'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
