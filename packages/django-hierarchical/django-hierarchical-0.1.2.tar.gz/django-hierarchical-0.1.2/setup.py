b04437726552373eb0b4d76d6eec08582123827e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:46 +0000

    Add package django-hierarchical/0.1.2/django-hierarchical-0.1.2.tar.gz

---
+++ packages/django-hierarchical/django-hierarchical-0.1.2.tar.gz/django-hierarchical-0.1.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+	name = 'django-hierarchical',
+	version = '0.1.2',
+	description = 'Hierarchical models and form fields for Django',
+	author = 'Moxy Park',
+	author_email = 'moxypark@me.com',
+	url = 'http://moxypark.co.uk/',
+	packages = find_packages(),
+	package_dir = {
+		'django_hierarchy': 'django_hierarchy'
+	},
+	install_requires = [
+		'django>=1.2.1'
+	],
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+		# 'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Framework :: Django',
+    ]
+)
