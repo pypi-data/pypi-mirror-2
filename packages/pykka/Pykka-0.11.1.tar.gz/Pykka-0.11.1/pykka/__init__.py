8ca68657b83cdc49aea0a7c47d8063d9903e23b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:11 +0000

    Add package pykka/0.11.1/Pykka-0.11.1.tar.gz

---
+++ packages/pykka/Pykka-0.11.1.tar.gz/Pykka-0.11.1/pykka/__init__.py
@@ -0,0 +1,7 @@
+#: Pykka's version as a tuple that can be used for comparison
+VERSION = (0, 11, 1)
+
+
+def get_version():
+    """Returns Pykka's version as a formatted string"""
+    return '.'.join(map(str, VERSION))
