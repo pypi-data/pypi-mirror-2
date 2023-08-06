5fbdb5e78823e68bfc4e8dba0871dd098162aeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:08 +0000

    Add package HolyGrail/0.1.1.Galahad/HolyGrail-0.1.1.Galahad.tar.gz

---
+++ packages/HolyGrail/HolyGrail-0.1.1.Galahad.tar.gz/HolyGrail-0.1.1.Galahad/holygrail/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from holygrail import *
+
+VERSION="0.1.1 Galahad"
+
+if __name__ == "__main__":
+    pass
+
+# vim:set shiftwidth=4 tabstop=4 expandtab:
