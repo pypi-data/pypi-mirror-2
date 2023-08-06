c03ff0d8704dde9beeeb5f206995544ccf1db8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:26 +0000

    Add package supcut/0.5/supcut-0.5.tar.gz

---
+++ packages/supcut/supcut-0.5.tar.gz/supcut-0.5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import sys, os
+
+from supcut.supcut import __version__ as version
+
+setup(name='supcut',
+      version=version,
+      description="Simple unobtrusive Python continuous unit testing",
+      long_description="""\
+A rather hassle-free tool to run nose based unit testing locally.
+It runs nosetests upon any file change and displays changes in the sets of
+failing or successful tests via OSD.
+""",
+      classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Development Status :: 4 - Beta',
+      ],
+      keywords='nose unit-testing',
+      author='Federico Ceratto',
+      author_email='federico.ceratto@gmail.com',
+      url='http://github.com/FedericoCeratto/supcut',
+      license='GPLv3',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'pyinotify',
+      ],
+      entry_points={
+        'console_scripts': ['supcut = supcut.supcut:main'],
+      },
+)
