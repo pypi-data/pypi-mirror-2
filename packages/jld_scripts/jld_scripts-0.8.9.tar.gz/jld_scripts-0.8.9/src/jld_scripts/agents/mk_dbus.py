2b68cd61dbcc2fa7ad62715a7e00bec9353b2d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:31 +0000

    Add package jld_scripts/0.8.9/jld_scripts-0.8.9.tar.gz

---
+++ packages/jld_scripts/jld_scripts-0.8.9.tar.gz/jld_scripts-0.8.9/src/jld_scripts/agents/mk_dbus.py
@@ -0,0 +1,116 @@
+"""
+    MediaKeys Dbus Agent
+    
+    Messages Generated:
+    - "mk_key_press" (key, source, priority)
+        priority: 1 -> low, 5 -> high
+    
+    Created on 2010-10-22
+    @author: jldupont
+"""
+import dbus.service
+    
+from jld_scripts.system.base import AgentThreadedBase
+from jld_scripts.system import mswitch
+
+__all__=[]
+
+
+class MKSignalRx1(dbus.service.Object):
+    """ works under ubuntu < 10.10
+    """
+    PATH=None
+    
+    def __init__(self, agent):
+        dbus.service.Object.__init__(self, dbus.SystemBus(), self.PATH) ## not sure we need this just to receive signals...
+        self.agent=agent
+        
+        dbus.SystemBus().add_signal_receiver(self.sCondition,
+                                       signal_name="Condition",
+                                       dbus_interface="org.freedesktop.Hal.Device",
+                                       bus_name=None,
+                                       path=None
+                                       )            
+    def sCondition(self, *p):
+        """
+        DBus signal handler
+        """
+        if len(p) == 2:
+            if (p[0]=="ButtonPressed"):
+                mswitch.publish(self.agent, "mk_key_press", p[1], "source1", 5)
+
+class MKSignalRx2(dbus.service.Object):
+    """ works on ubuntu >= 10.10
+    """
+    PATH=None #"/org/gnome/SettingsDaemon/MediaKeys"
+    
+    def __init__(self, agent):
+        dbus.service.Object.__init__(self, dbus.SessionBus(), self.PATH) ## not sure we need this just to receive signals...
+        self.agent=agent
+        
+        dbus.SessionBus().add_signal_receiver(self.sCondition,
+                                       signal_name="MediaPlayerKeyPressed",
+                                       dbus_interface="org.gnome.SettingsDaemon.MediaKeys",
+                                       bus_name=None,
+                                       path="/org/gnome/SettingsDaemon/MediaKeys"
+                                       )            
+    def sCondition(self, *p):
+        """
+        DBus signal handler
+        """
+        if len(p) == 2:
+            mswitch.publish(self.agent, "mk_key_press", p[1].lower(), "source2", 1)
+
+class MKSignalRx3(dbus.service.Object):
+    """ Eventor interface
+    """
+    PATH="/Events"
+    
+    def __init__(self, agent):
+        dbus.service.Object.__init__(self, dbus.SystemBus(), self.PATH) ## not sure we need this just to receive signals...
+        self.agent=agent
+        
+        dbus.SystemBus().add_signal_receiver(self.sMsg,
+                                       signal_name="Msg",
+                                       dbus_interface="com.systemical.eventor",
+                                       bus_name=None,
+                                       path="/Events"
+                                       )            
+    def sMsg(self, *p):
+        """
+        DBus signal handler
+        """
+        #print "*** Eventor: %s" % p
+        if len(p) == 1:
+            mswitch.publish(self.agent, "eventor_msg", p[0])
+
+
+class TrackSignalTx(dbus.service.Object):
+
+    PATH="/Track"
+    
+    def __init__(self, agent):
+        dbus.service.Object.__init__(self, dbus.SessionBus(), self.PATH)
+        self.agent=agent
+        
+    @dbus.service.signal(dbus_interface="com.jldupont.squeezecenter", signature="ssss")
+    def Track(self, artist, album, title, path):
+        pass
+
+
+class DbusAgent(AgentThreadedBase):
+    
+    def __init__(self):
+        AgentThreadedBase.__init__(self)
+
+        self.srx1=MKSignalRx1(self)
+        self.srx2=MKSignalRx2(self)
+        self.srx3=MKSignalRx3(self)
+        
+        self.stx=TrackSignalTx(self)
+        
+    def h_track(self, artist, album, title, path):
+        self.stx.Track(artist, album, title, path)
+                   
+_=DbusAgent()
+_.start()
---
+++ packages/jld_scripts/jld_scripts-0.8.9.tar.gz/jld_scripts-0.8.9/src/jld_scripts/scripts/squeezecenter_utils.py
@@ -0,0 +1,78 @@
+"""
+    squeezecenter_utils
+        
+    14 Feb 2011: added "playing now" dbus signal
+        
+    Created on 2010-10-21
+    @author: jldupont
+"""
+import sys
+
+APP_VERSION="1.0"
+APP_NAME="SqueezeCenter Utils"
+ICON_NAME="squeezecenter.gif"
+HELP_URL="http://www.systemical.com/doc/opensource/squeezecenter_utils"
+TIME_BASE=5000
+
+###<<< DEVELOPMENT MODE SWITCHES
+MSWITCH_OBSERVE_MODE=False
+MSWITCH_DEBUGGING_MODE=False
+MSWITCH_DEBUG_INTEREST=False
+DEV_MODE=True
+###>>>
+
+import gobject
+import dbus.glib
+from dbus.mainloop.glib import DBusGMainLoop
+import gtk
+
+gobject.threads_init()  #@UndefinedVariable
+dbus.glib.init_threads()
+DBusGMainLoop(set_as_default=True)
+
+from jld_scripts.system import base as base
+base.debug=DEV_MODE
+base.debug_interest=MSWITCH_DEBUG_INTEREST
+
+from jld_scripts.system import mswitch #@UnusedImport
+mswitch.observe_mode=MSWITCH_OBSERVE_MODE
+mswitch.debugging_mode=MSWITCH_DEBUGGING_MODE
+
+from jld_scripts.agents.notifier import notify, NotifierAgent #@Reimport
+from jld_scripts.agents.clock import Clock #@Reimport
+
+def main(debug=False):
+    try:
+        
+        try:    
+            import pysqueezecenter #@UnresolvedImport @UnusedImport
+        except: 
+            raise Exception("package 'pysqueezecenter' is required")
+        
+        from jld_scripts.res import get_res_path
+        icon_path=get_res_path()
+        
+        from jld_scripts.agents.squeeze_tray import TrayAgent
+        _ta=TrayAgent(APP_NAME, icon_path, ICON_NAME, HELP_URL, APP_VERSION)
+
+        import jld_scripts.agents.mk_dbus #@UnusedImport
+        import jld_scripts.agents.squeeze #@UnusedImport
+        
+        _na=NotifierAgent(APP_NAME, ICON_NAME)
+        _na.start()
+        
+        clk=Clock(TIME_BASE)
+        gobject.timeout_add(TIME_BASE, clk.tick)
+        
+        mswitch.publish("__main__", "debug", debug)
+        
+        gtk.main()
+    except KeyboardInterrupt:
+        mswitch.quit()
+        sys.exit(1)        
+        
+    except Exception,e:
+        notify(APP_NAME, "There was an error: %s" % e)
+        mswitch.quit()
+        sys.exit(1)
+
