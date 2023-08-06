5378028523511773543a259f0907d49814ce3b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:17 +0000

    Add package micromodels/0.1.0/micromodels-0.1.0.tar.gz

---
+++ packages/micromodels/micromodels-0.1.0.tar.gz/micromodels-0.1.0/micromodels/fields.py
@@ -0,0 +1,128 @@
+import datetime
+
+class FieldBase(object):
+    """Base class for all field types"""
+
+    def __init__(self, source=None):
+        """Create a new instance of a field class.
+
+        Keyword arguments:
+        source -- the key holding the value for this field to use in the
+        source data. If not supplied, the name of the class property this
+        instance is assigned to will be used.
+        """
+        self.source = source
+
+    def populate(self, data):
+        """Set the value or values wrapped by this field"""
+        self.data = data
+
+
+class PassField(FieldBase):
+    """Field that just passes through the data"""
+
+    def to_python(self):
+        return self.data
+
+
+class CharField(FieldBase):
+    """Field to represent a simple Unicode string value"""
+
+    def to_python(self):
+        """Convert the data supplied to the `populate` method to a Unicode string"""
+        if self.data is None:
+            return ''
+        return unicode(self.data)
+
+
+class IntegerField(FieldBase):
+    """Field to represent an integer value"""
+
+    def to_python(self):
+        """Convert the data supplied to the `populate` method to an integer"""
+        if self.data is None:
+            return 0
+        return int(self.data)
+
+
+class BooleanField(FieldBase):
+    """Field to represent a boolean"""
+
+    def to_python(self):
+        """Convert the data supplied to the `populate` method to a boolean value.
+
+        The string "True" (case insensitive) will be converted
+        to True, as will any positive integers.
+        """
+        if isinstance(self.data, basestring):
+            return self.data.strip().lower() == 'true'
+        if isinstance(self.data, int):
+            return self.data > 0
+        return bool(self.data)
+
+
+class DateTimeField(FieldBase):
+    """Field to represent a datetime"""
+
+    def __init__(self, format, **kwargs):
+        super(DateTimeField, self).__init__(**kwargs)
+        self.format = format
+
+    def to_python(self):
+        if self.data is None:
+            return None
+        return datetime.datetime.strptime(str(self.data), self.format)
+
+
+class DateField(DateTimeField):
+    """Field to represent a datetime.date"""
+
+    def to_python(self):
+        datetime = super(DateField, self).to_python()
+        return datetime.date()
+
+
+class TimeField(DateTimeField):
+    """Field to represent a datetime.time"""
+
+    def to_python(self):
+        datetime = super(TimeField, self).to_python()
+        return datetime.time()
+
+
+class WrappedObjectField(FieldBase):
+    """Superclass for any fields that wrap an object"""
+
+    def __init__(self, wrapped_class, **kwargs):
+        self._wrapped_class = wrapped_class
+        FieldBase.__init__(self, **kwargs)
+
+
+class ModelField(WrappedObjectField):
+    """Field containing a model instance"""
+
+    def to_python(self):
+        data = self.data or {}
+        return self._wrapped_class(data)
+
+
+class ModelCollectionField(WrappedObjectField):
+    """Field containing a list of model instances"""
+
+    def to_python(self):
+        data = self.data or []
+        return [self._wrapped_class(item) for item in data]
+
+
+class FieldCollectionField(WrappedObjectField):
+    """Field containing a list of fields"""
+
+    def to_python(self):
+        data = self.data or []
+        converted = []
+        for item in data:
+            field_instance = self._wrapped_class()
+            field_instance.populate(item)
+            converted.append(field_instance.to_python())
+        return converted
+
---
+++ packages/micromodels/micromodels-0.1.0.tar.gz/micromodels-0.1.0/tests.py
@@ -0,0 +1,257 @@
+import unittest
+import micromodels
+
+class ClassCreationTestCase(unittest.TestCase):
+
+    def setUp(self):
+        class SimpleModel(micromodels.Model):
+            name = micromodels.CharField()
+            field_with_source = micromodels.CharField(source='foo')
+        self.model_class = SimpleModel
+        self.instance = SimpleModel({})
+
+    def test_class_created(self):
+        """Model instance should be of type SimpleModel"""
+        self.assertTrue(isinstance(self.instance, self.model_class))
+
+    def test_fields_created(self):
+        """Model instance should have a property called _fields"""
+        self.assertTrue(hasattr(self.instance, '_fields'))
+
+    def test_field_collected(self):
+        """Model property should be of correct type"""
+        self.assertTrue(isinstance(self.instance._fields['name'], micromodels.CharField))
+
+    def test_field_source_not_set(self):
+        """Field without a custom source should have a source of None"""
+        self.assertEqual(self.instance._fields['name'].source, None)
+
+    def test_field_source_set(self):
+        """Field with custom source specificied should have source property set correctly"""
+        self.assertEqual(self.instance._fields['field_with_source'].source, 'foo')
+
+
+class FieldBaseTestCase(unittest.TestCase):
+
+    def test_field_without_provided_source(self):
+        """If no source parameter is provided, the field's source attribute should be None"""
+        field = micromodels.fields.FieldBase()
+        self.assertTrue(hasattr(field, 'source'))
+        self.assertTrue(field.source is None)
+
+    def test_field_with_provided_source(self):
+        """If a source parameter is provided, the field's source attribute should be set to the value of this parameter"""
+        field = micromodels.fields.FieldBase(source='customsource')
+        self.assertEqual(field.source, 'customsource')
+
+
+class PassFieldTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.field = micromodels.PassField()
+
+    def test_pass_field(self):
+        data = ('some', 'data', 42)
+        self.field.populate(data)
+        self.assertEqual(self.field.to_python(), data)
+
+
+class CharFieldTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.field = micromodels.CharField()
+
+    def test_string_conversion(self):
+        self.field.populate('somestring')
+        self.assertEqual(self.field.to_python(), 'somestring')
+
+    def test_none_conversion(self):
+        """CharField should convert None to empty string"""
+        self.field.populate(None)
+        self.assertEqual(self.field.to_python(), '')
+
+
+class IntegerFieldTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.field = micromodels.IntegerField()
+
+    def test_integer_conversion(self):
+        self.field.populate(123)
+        self.assertEqual(self.field.to_python(), 123)
+
+    def test_string_conversion(self):
+        self.field.populate('123')
+        self.assertEqual(self.field.to_python(), 123)
+
+    def test_none_conversion(self):
+        """IntegerField should convert None to 0"""
+        self.field.populate(None)
+        self.assertEqual(self.field.to_python(), 0)
+
+
+class BooleanFieldTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.field = micromodels.BooleanField()
+
+    def test_true_conversion(self):
+        self.field.populate(True)
+        self.assertEqual(self.field.to_python(), True)
+
+    def test_false_conversion(self):
+        self.field.populate(False)
+        self.assertEqual(self.field.to_python(), False)
+
+    def test_string_conversion(self):
+        """BooleanField should convert the string "True" (case insensitive) to True, all other values to False"""
+        self.field.populate('true')
+        self.assertEqual(self.field.to_python(), True)
+        self.field.populate('True')
+        self.assertEqual(self.field.to_python(), True)
+        self.field.populate('False')
+        self.assertEqual(self.field.to_python(), False)
+        self.field.populate('asdfasfasfd')
+        self.assertEqual(self.field.to_python(), False)
+
+    def test_integer_conversion(self):
+        """BooleanField should convert values <= 0 to False, all other integers to True"""
+        self.field.populate(0)
+        self.assertEqual(self.field.to_python(), False)
+        self.field.populate(-100)
+        self.assertEqual(self.field.to_python(), False)
+        self.field.populate(100)
+        self.assertEqual(self.field.to_python(), True)
+
+
+class DateTimeFieldTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.format = "%a %b %d %H:%M:%S +0000 %Y"
+        self.datetimestring = "Tue Mar 21 20:50:14 +0000 2006"
+        self.field = micromodels.DateTimeField(format=self.format)
+
+    def test_format_conversion(self):
+        import datetime
+        self.field.populate(self.datetimestring)
+        converted = self.field.to_python()
+        self.assertTrue(isinstance(converted, datetime.datetime))
+        self.assertEqual(converted.strftime(self.format), self.datetimestring)
+
+
+class DateFieldTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.format = "%Y-%m-%d"
+        self.datestring = "2010-12-28"
+        self.field = micromodels.DateField(format=self.format)
+
+    def test_format_conversion(self):
+        import datetime
+        self.field.populate(self.datestring)
+        converted = self.field.to_python()
+        self.assertTrue(isinstance(converted, datetime.date))
+        self.assertEqual(converted.strftime(self.format), self.datestring)
+
+
+class TimeFieldTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.format = "%H:%M:%S"
+        self.timestring = "09:33:30"
+        self.field = micromodels.TimeField(format=self.format)
+
+    def test_format_conversion(self):
+        import datetime
+        self.field.populate(self.timestring)
+        converted = self.field.to_python()
+        self.assertTrue(isinstance(converted, datetime.time))
+        self.assertEqual(converted.strftime(self.format), self.timestring)
+
+
+class InstanceTestCase(unittest.TestCase):
+
+    def test_basic_data(self):
+        class ThreeFieldsModel(micromodels.Model):
+            first = micromodels.CharField()
+            second = micromodels.CharField()
+            third = micromodels.CharField()
+
+        data = {'first': 'firstvalue', 'second': 'secondvalue', 'third': 'thirdvalue'}
+        instance = ThreeFieldsModel(data)
+
+        self.assertEqual(instance.first, data['first'])
+        self.assertEqual(instance.second, data['second'])
+        self.assertEqual(instance.third, data['third'])
+        self.assertEqual(instance._data, data)
+
+    def test_custom_data_source(self):
+        class CustomSourceModel(micromodels.Model):
+            first = micromodels.CharField(source='custom_source')
+
+        data = {'custom_source': 'somevalue'}
+        instance = CustomSourceModel(data)
+
+        self.assertEqual(instance.first, data['custom_source'])
+
+
+class ModelFieldTestCase(unittest.TestCase):
+
+    def test_model_field_creation(self):
+        class IsASubModel(micromodels.Model):
+            first = micromodels.CharField()
+
+        class HasAModelField(micromodels.Model):
+            first = micromodels.ModelField(IsASubModel)
+
+        data = {'first': {'first': 'somevalue'}}
+        instance = HasAModelField(data)
+        self.assertTrue(isinstance(instance.first, IsASubModel))
+        self.assertEqual(instance.first.first, data['first']['first'])
+
+
+class ModelCollectionFieldTestCase(unittest.TestCase):
+
+    def test_model_collection_field_creation(self):
+        class IsASubModel(micromodels.Model):
+            first = micromodels.CharField()
+
+        class HasAModelCollectionField(micromodels.Model):
+            first = micromodels.ModelCollectionField(IsASubModel)
+
+        data = {'first': [{'first': 'somevalue'}, {'first': 'anothervalue'}]}
+        instance = HasAModelCollectionField(data)
+        self.assertTrue(isinstance(instance.first, list))
+        for item in instance.first:
+            self.assertTrue(isinstance(item, IsASubModel))
+        self.assertEqual(instance.first[0].first, data['first'][0]['first'])
+        self.assertEqual(instance.first[1].first, data['first'][1]['first'])
+
+    def test_model_collection_field_with_no_elements(self):
+        class IsASubModel(micromodels.Model):
+            first = micromodels.CharField()
+
+        class HasAModelCollectionField(micromodels.Model):
+            first = micromodels.ModelCollectionField(IsASubModel)
+
+        data = {'first': []}
+        instance = HasAModelCollectionField(data)
+        self.assertEqual(instance.first, [])
+
+
+class FieldCollectionFieldTestCase(unittest.TestCase):
+
+    def test_field_collection_field_creation(self):
+        class HasAFieldCollectionField(micromodels.Model):
+            first = micromodels.FieldCollectionField(micromodels.CharField)
+
+        data = {'first': ['one', 'two', 'three']}
+        instance = HasAFieldCollectionField(data)
+        self.assertTrue(isinstance(instance.first, list))
+        self.assertTrue(len(data['first']), len(instance.first))
+        for index, value in enumerate(data['first']):
+            self.assertEqual(instance.first[index], value)
+
+
+if __name__ == "__main__":
+    unittest.main()
