97e2e2d4ccc675aa1c5d847bac147c7fd3651b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:04 +0000

    Add package libtele/0.2/libtele-0.2-py2.7.egg

---
+++ packages/libtele/libtele-0.2-py2.7.egg/tele/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module contains the methods to query http://tele.at.
+
+All methods expects you to use ``unicode``-Strings and local aware
+``datetime.datetime``\s! Since this module depends on the ``pytz``-Module
+you can use it without adding a dependency to your application.
+"""
+
+__version__ = "0.2"
+__license__ = "GPLv3"
+
