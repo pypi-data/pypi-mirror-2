0ff94641892257bc3ab37822ac37ddc348686202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:31 +0000

    Add package marrow.script/1.0/marrow.script-1.0.tar.gz

---
+++ packages/marrow.script/marrow.script-1.0.tar.gz/marrow.script-1.0/marrow/script/__init__.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+import sys
+
+from marrow.script.core import Parser
+
+
+__all__ = ['Parser', 'execute', 'script', 'annotate', 'describe', 'short']
+
+
+
+def execute(obj): # pragma: no cover
+    sys.exit(Parser(obj)(sys.argv[1:]))
+
+
+
+def base(attr):
+    def decorator(**kw):
+        def inner(fn):
+            if not hasattr(fn, attr):
+                fn.__dict__[attr] = dict()
+            
+            fn.__dict__[attr].update(kw)
+            
+            return fn
+        
+        return inner
+    
+    return decorator
+
+
+script = base('_cmd_script')
+annotate = base('_cmd_arg_types')
+describe = base('_cmd_arg_doc')
+short = base('_cmd_shorts')
