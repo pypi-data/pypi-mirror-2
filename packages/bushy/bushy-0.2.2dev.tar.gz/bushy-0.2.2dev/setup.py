20d85c328a4b4524fa4fc91e9af533b3e49f5b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:10 +0000

    Add package bushy/0.2.2dev/bushy-0.2.2dev.tar.gz

---
+++ packages/bushy/bushy-0.2.2dev.tar.gz/bushy-0.2.2dev/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import find_packages
+from distutils.core import setup
+
+REQUIRES = [
+    'pivotal-py',
+    'httplib2',
+    ]
+
+setup(
+    name='bushy',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    version='0.2.2dev',
+    author='junkafarian',
+    author_email='junkafarian@gmail.com',
+    url='https://github.com/junkafarian/bushy',
+    install_requires=REQUIRES,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Software Development",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Bug Tracking",
+        "Topic :: Utilities",
+        "Environment :: Console",
+    ],
+    description='A git workflow plugin.',
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
+    include_package_data=True,
+    entry_points='''
+[console_scripts]
+git-feature = bushy.scripts:feature
+git-bug = bushy.scripts:bug
+git-finish = bushy.scripts:finish
+''',
+    )
