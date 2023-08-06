6970106bad58a5338bce3ef977195c5afcccf0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:47 +0000

    Add package Searchpy/1.4.2/Searchpy-1.4.2.tar.gz

---
+++ packages/Searchpy/Searchpy-1.4.2.tar.gz/Searchpy-1.4.2/searchpy/adapters/google.py
@@ -0,0 +1,48 @@
+class Engine(object):
+    def __init__(self, query, tld, country, lang):
+        self.tld     = tld
+        self.country = country
+        self.lang    = lang
+        
+        self.endpoint = 'http://www.google.%s/search' % self.tld
+        self.options  = {'q'  : query,
+                         'hl' : self.lang,
+                         'gl' : self.country,
+                         'start' : 0
+                        }
+    
+    def _get_start(self):
+        return self.options['start']
+    def _set_start(self, value):
+        self.options['start'] = value
+    
+    start = property(_get_start, _set_start, "Paging start set")
+
+def parse(results, start=0):
+    """Parse the html results into a data structure."""
+    
+    from pyquery           import PyQuery as pq
+    from searchpy.adapters import remove_tags
+    
+    doc = pq(results)
+    res = doc("#ires")
+    rec = res(".g")
+    
+    # Make each element an instance of pq
+    recs  = [pq(x) for x in rec]
+    items = [{'start' : start}]
+    
+    # Step through each element and build a dictionary of their useful values
+    for record in recs:
+        url    = record('.vsc')('.tl')('.r')('a')
+        
+        print repr(recs)
+        
+        if len(url) > 0:
+            pieces = {'url'    : url.attr('href'),
+                      'title'  : remove_tags(url.text()),
+                      'cached' : record('.vsc')('.s')('a').attr('href')}
+            
+            items.append(pieces)
+    
+    return items
