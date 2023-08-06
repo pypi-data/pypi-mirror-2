3c21cfe851db63e71565360b3f35bae250f1bb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:05 +0000

    Add package python-flot-utils/0.2.1/python-flot-utils-0.2.1.tar.gz

---
+++ packages/python-flot-utils/python-flot-utils-0.2.1.tar.gz/python-flot-utils-0.2.1/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "python-flot-utils",
+    version = "0.2.1",
+    author = "Brian Luft",
+    author_email = "brian@unbracketed.com",
+    description = ("Makes it easy to convert Python data structures" 
+        " to JSON strings suitable for flot series and options"),
+    license = "MIT",
+    keywords = "flot graphs charts javascript data timeseries time series",
+    #url = "http://packages.python.org/an_example_pypi_project",
+    packages=['pyflot', 'tests'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
