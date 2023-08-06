e25b5b4863e32e9b5679833e2ac7ed81ee4c034f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:23 +0000

    Add package drink/0.0.3/drink-0.0.3.tar.gz

---
+++ packages/drink/drink-0.0.3.tar.gz/drink-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name="drink",
+    version="0.0.3",
+    author="Fabien Devaux",
+    author_email="fdev31@gmail.com",
+    license="BSD",
+    platform="all",
+    description="my web sandbox (ZODB + Bottle)",
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points = {
+        "console_scripts": [
+            'drink = drink:startup',
+            ],
+        "setuptools.installation" : [
+            'eggsecutable = drink:startup'
+            ]
+        },
+    packages=find_packages(),
+    install_requires = ['jinja2', 'markdown', 'ZODB3'],
+)
+
