5816afa6edd7fe3acfd84663db67c642a188c61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:34 +0000

    Add package pytest/2.0.2/pytest-2.0.2.zip

---
+++ packages/pytest/pytest-2.0.2.zip/pytest-2.0.2/setup.py
@@ -0,0 +1,70 @@
+import os, sys
+if sys.version_info >= (3,0):
+    from distribute_setup import use_setuptools
+    use_setuptools()
+from setuptools import setup
+
+long_description = """
+cross-project testing tool for Python.
+
+Platforms: Linux, Win32, OSX
+
+Interpreters: Python versions 2.4 through to 3.2, Jython 2.5.1 and PyPy
+
+Bugs and issues: http://bitbucket.org/hpk42/pytest/issues/
+
+Web page: http://pytest.org
+
+(c) Holger Krekel and others, 2004-2010
+"""
+def main():
+    setup(
+        name='pytest',
+        description='py.test: simple powerful testing with Python',
+        long_description = long_description,
+        version='2.0.2',
+        url='http://pytest.org',
+        license='MIT license',
+        platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
+        author='holger krekel, Guido Wesdorp, Carl Friedrich Bolz, Armin Rigo, Maciej Fijalkowski & others',
+        author_email='holger at merlinux.eu',
+        entry_points= make_entry_points(),
+        install_requires=['py>1.4.1'],
+        classifiers=['Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: MIT License',
+                     'Operating System :: POSIX',
+                     'Operating System :: Microsoft :: Windows',
+                     'Operating System :: MacOS :: MacOS X',
+                     'Topic :: Software Development :: Testing',
+                     'Topic :: Software Development :: Libraries',
+                     'Topic :: Utilities',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 3'],
+        packages=['_pytest', ],
+        py_modules=['pytest'],
+        zip_safe=False,
+    )
+
+def cmdline_entrypoints(versioninfo, platform, basename):
+    target = 'pytest:main'
+    if platform.startswith('java'):
+        points = {'py.test-jython': target}
+    else:
+        if basename.startswith("pypy"):
+            points = {'py.test-%s' % basename: target}
+        else: # cpython
+            points = {'py.test-%s.%s' % versioninfo[:2] : target,}
+        points['py.test'] = target
+    return points
+
+def make_entry_points():
+    basename = os.path.basename(sys.executable)
+    points = cmdline_entrypoints(sys.version_info, sys.platform, basename)
+    keys = list(points.keys())
+    keys.sort()
+    l = ["%s = %s" % (x, points[x]) for x in keys]
+    return {'console_scripts': l}
+
+if __name__ == '__main__':
+    main()
