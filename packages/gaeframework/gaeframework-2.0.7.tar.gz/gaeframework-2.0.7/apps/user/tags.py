974aac2a448a4f08d767edd4d32adada08c74559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:09 +0000

    Add package gaeframework/2.0.7/gaeframework-2.0.7.tar.gz

---
+++ packages/gaeframework/gaeframework-2.0.7.tar.gz/gaeframework-2.0.7/apps/user/tags.py
@@ -0,0 +1,17 @@
+from gae import template
+from gae import webapp
+from user.forms import UserLoginForm, UserRegistrationForm
+
+register = template.create_template_register()
+
+@register.simple_tag
+def login_form():
+    """Render the user login form"""
+    form = UserLoginForm()
+    return webapp.instance.render("user/login_form", {'form': form})
+
+@register.simple_tag
+def registration_form():
+    """Render the user registration form"""
+    form = UserRegistrationForm()
+    return webapp.instance.render("user/registration_form", {'form': form})
