70d0795e92967d3125746693ea49277e07c3bbab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:40 +0000

    Add package PyGall/0.6/PyGall-0.6.tar.gz

---
+++ packages/PyGall/PyGall-0.6.tar.gz/PyGall-0.6/pygall/templates/pygall/main/constants.mako.js
@@ -0,0 +1,12 @@
+App = window.App || {};
+
+App.constants = {
+
+    urls: {
+
+        photos_editcomment: "${url(controller='photos', action='editcomment')}",
+
+        photos: "${url('photos')}"
+    }
+};
+
