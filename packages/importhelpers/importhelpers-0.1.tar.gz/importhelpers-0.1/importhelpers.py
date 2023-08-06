701ec068c3c1e14354b34df768814813e64fdb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:32 +0000

    Add package importhelpers/0.1/importhelpers-0.1.tar.gz

---
+++ packages/importhelpers/importhelpers-0.1.tar.gz/importhelpers-0.1/importhelpers.py
@@ -0,0 +1,42 @@
+def dotted_import(name):
+    """Import python object from dotted path string.
+
+    eg: dotted_import('package.module.function')
+
+    """
+    mod, attr = name.split('.'), []
+    obj = None
+    while mod:
+        try:
+            obj = __import__('.'.join(mod), {}, {}, [''])
+        except ImportError, e:
+            attr.insert(0, mod.pop())
+        else:
+            for a in attr:
+                try:
+                    obj = getattr(obj, a)
+                except AttributeError, e:
+                    raise ImportError('could not get attribute %s from %s'
+                        ' -> %s (%r)' % (a, '.'.join(mod), '.'.join(attr),
+                        obj))
+            return obj
+    raise ImportError('could not import %s' % name)
+
+
+def import_preferential(name, *morenames):
+    """return first import that can be imported successfully.
+
+    eg: import_preferential('lxml.etree', 'cElementTree',
+        'elementtree.ElementTree', 'xml.etree.ElementTree')
+
+    """
+    if not isinstance(name, (list, tuple)):
+        name = [name]
+    names = list(name) + list(morenames)
+    while names:
+        name = names.pop(0)
+        try:
+            return dotted_import(name)
+        except ImportError, e:
+            pass
+    raise ImportError('could not satisfy any imports.')
---
+++ packages/importhelpers/importhelpers-0.1.tar.gz/importhelpers-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(name="importhelpers", version="0.1",
+    py_modules=["importhelpers"],
+    author="Edward George",
+    author_email="edwardgeorge@gmail.com",
+    description="Common import convenience functions.",
+    url="http://github.com/edwardgeorge/importhelpers/",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
