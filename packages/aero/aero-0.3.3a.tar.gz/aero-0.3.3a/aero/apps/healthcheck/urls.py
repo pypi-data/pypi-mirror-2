8645e89cbf0483aa5886af3f980278222835f9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:12 +0000

    Add package aero/0.3.3a/aero-0.3.3a.tar.gz

---
+++ packages/aero/aero-0.3.3a.tar.gz/aero-0.3.3a/aero/apps/healthcheck/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
+
+from aero.apps.healthcheck.handlers import HealthcheckHandler
+
+urls = (
+    (r'/healthcheck/?', HealthcheckHandler),
+)
