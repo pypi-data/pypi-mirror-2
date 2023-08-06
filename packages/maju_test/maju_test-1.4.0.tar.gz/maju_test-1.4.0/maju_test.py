3124eb3fdc319ac6c013422a988cabdb15a839be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:05 +0000

    Add package maju_test/1.4.0/maju_test-1.4.0.tar.gz

---
+++ packages/maju_test/maju_test-1.4.0.tar.gz/maju_test-1.4.0/maju_test.py
@@ -0,0 +1,14 @@
+""" This is the “nester.py" module, and it provides one function called
+    print_lol() which prints lists that may or may not include nested lists."""
+def print_lol(the_list,indent=False,level=0,file_name=sys.stdout):
+    """ This function takes a positional argument called “the_list", which is any
+        Python list (of, possibly, nested lists). Each data item in the provided list
+        is (recursively) printed to the screen on its own line."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,indent,level+1,file_name)	
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end="", file=file_name)
+                print(each_item, file=file_name)
