8cf1079b036e1f2614ac74a4f79618fbf9808e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:39 +0000

    Add package brubeck/0.2.7/brubeck-0.2.7.tar.gz

---
+++ packages/brubeck/brubeck-0.2.7.tar.gz/brubeck-0.2.7/brubeck/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+"""The Brubeck Message Handling system."""
+
+version = "0.2.7"
+version_info = (0, 2, 7)
+__all__ = [ 'auth', 'models', 'mongrel2', 'request_handling', 'templating',]
