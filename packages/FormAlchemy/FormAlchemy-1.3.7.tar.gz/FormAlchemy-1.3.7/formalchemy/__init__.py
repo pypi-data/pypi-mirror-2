2a7388067c79db1ca8465fc95b739db7622973a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:20 +0000

    Add package FormAlchemy/1.3.7/FormAlchemy-1.3.7.tar.gz

---
+++ packages/FormAlchemy/FormAlchemy-1.3.7.tar.gz/FormAlchemy-1.3.7/formalchemy/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2007 Alexandre Conrad, alexandre (dot) conrad (at) gmail (dot) com
+#
+# This module is part of FormAlchemy and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+from formalchemy import templates
+from formalchemy import config
+from formalchemy.tables import *
+from formalchemy.forms import *
+from formalchemy.fields import *
+from formalchemy.validators import ValidationError
+import formalchemy.validators as validators
+import formalchemy.fatypes as types
+
+__all__ = ["FieldSet", "Field", "FieldRenderer", "Grid", "ValidationError", "validators", "SimpleMultiDict", "types"]
+__version__ = "1.3.7"
+
