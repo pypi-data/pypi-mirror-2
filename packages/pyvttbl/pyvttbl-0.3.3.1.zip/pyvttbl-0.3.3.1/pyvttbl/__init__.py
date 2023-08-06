65b6708fe2da80d8a045cae469295cccb4036bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:02 +0000

    Add package pyvttbl/0.3.3.1/pyvttbl-0.3.3.1.zip

---
+++ packages/pyvttbl/pyvttbl-0.3.3.1.zip/pyvttbl-0.3.3.1/pyvttbl/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2011, Roger Lew [see LICENSE.txt]
+# This software is funded in part by NIH Grant P20 RR016454.
+
+from pyvttbl import DataFrame
+from pyvttbl import PyvtTbl
+from pyvttbl import Marginals
+from pyvttbl import Histogram
+from pyvttbl import Descriptives
+
+__all__ = ["DataFrame",
+           "PyvtTbl",
+           "Marginals",
+           "Descriptives",
+           "Histogram"]
