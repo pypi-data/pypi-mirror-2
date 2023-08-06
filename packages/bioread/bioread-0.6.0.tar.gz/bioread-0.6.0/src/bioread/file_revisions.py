683299e685c9873e05e76dd4afa9f11b6463d228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:05 +0000

    Add package bioread/0.6.0/bioread-0.6.0.tar.gz

---
+++ packages/bioread/bioread-0.6.0.tar.gz/bioread-0.6.0/src/bioread/file_revisions.py
@@ -0,0 +1,31 @@
+# coding: utf8
+# Part of the bioread package for reading BIOPAC data.
+#
+# Copyright (c) 2010 Board of Regents of the University of Wisconsin System
+#
+# Written by John Ollinger <ollinger@wisc.edu> and Nate Vack <njvack@wisc.edu>
+# at the Waisman Laboratory for Brain Imaging and Behavior, University of
+# Wisconsin-Madison
+
+# This is an enumeration of known file revisions, for use in header parsing.
+
+V_ALL=0
+V_20a=30
+V_20b=31
+V_20r=32
+V_207=33
+V_30r=34
+V_303=35
+V_35x=36
+V_36x=37
+V_370=38
+V_373=39
+V_381=41
+V_37P=42
+V_382=43
+V_38P=44
+V_390=45
+V_400=78 # Unsure about exact version... but this is Post-Some-Big-Change.
+V_41a=80 # I'm honestly guessing about this one.
+V_410=83
+V_411=84
