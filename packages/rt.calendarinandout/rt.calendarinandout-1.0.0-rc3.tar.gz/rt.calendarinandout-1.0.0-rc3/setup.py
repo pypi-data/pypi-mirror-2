20ab12761119072b87d926225115efd24b471b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:48 +0000

    Add package rt.calendarinandout/1.0.0-rc3/rt.calendarinandout-1.0.0-rc3.tar.gz

---
+++ packages/rt.calendarinandout/rt.calendarinandout-1.0.0-rc3.tar.gz/rt.calendarinandout-1.0.0-rc3/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of rt.calendarinandout
+"""
+import os
+from setuptools import setup, find_packages
+
+version = '1.0.0-rc3'
+
+tests_require = ['zope.testing']
+
+setup(name='rt.calendarinandout',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://www.redturtle.it',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['rt', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        # -*- Extra requirements: -*-
+                        'collective.js.jqueryui<1.8dev'
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite='rt.calendarinandout.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
