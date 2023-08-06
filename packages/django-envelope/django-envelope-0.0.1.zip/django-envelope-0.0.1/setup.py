ca6c535a970e062b8b09639886677d6794a12953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:45 +0000

    Add package django-envelope/0.0.1/django-envelope-0.0.1.zip

---
+++ packages/django-envelope/django-envelope-0.0.1.zip/django-envelope-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-envelope',
+    version='0.0.1',
+    description='A contact form app for Django',
+    author='Zbigniew Siciarz',
+    author_email='antyqjon@gmail.com',
+    url='http://github.com/zsiciarz/django-envelope',
+    install_requires=['Django', 'django-honeypot'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+)
