664b20b06a7fa2056482903138aa3c4b91001d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package django-urlcrypt/0.1.2/django-urlcrypt-0.1.2.tar.gz

---
+++ packages/django-urlcrypt/django-urlcrypt-0.1.2.tar.gz/django-urlcrypt-0.1.2/urlcrypt/conf.py
@@ -0,0 +1,11 @@
+import hashlib
+
+from django.conf import settings
+
+SECRET_KEY = settings.SECRET_KEY
+# kind of ghetto, is there a better way to do this other than os.urandom?
+OBFUSCATE_KEY = hashlib.sha512(SECRET_KEY).digest() + hashlib.sha512(SECRET_KEY[::-1]).digest()
+
+URLCRYPT_LOGIN_URL = getattr(settings, 'URLCRYPT_LOGIN_URL', settings.LOGIN_URL)
+RUNNING_TESTS = getattr(settings, 'RUNNING_TESTS', False)
+URLCRYPT_RATE_LIMIT = getattr(settings, 'URLCRYPT_RATE_LIMIT', 60)
