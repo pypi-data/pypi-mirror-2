bbf54938007469bbacedf5f4166eb76a7a6d8c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:52 +0000

    Add package plone.app.layout/1.2.7/plone.app.layout-1.2.7.tar.gz

---
+++ packages/plone.app.layout/plone.app.layout-1.2.7.tar.gz/plone.app.layout-1.2.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os.path
+
+version = '1.2.7'
+
+setup(name='plone.app.layout',
+      version=version,
+      description="Layout mechanisms for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        ],
+      keywords='plone layout viewlet',
+      author='Martin Aspeli',
+      author_email='optilude@gmx.net',
+      url='http://svn.plone.org/svn/plone/plone.app.layout',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.app.viewletmanager>=1.2',
+        'plone.memoize',
+        'plone.portlets',
+        'Products.CMFEditions>=1.2.2',
+        'Plone>=3.2.1',
+      ],
+      )
