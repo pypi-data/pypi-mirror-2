f589ab364a367d1cefaffbb2737134b7710f9bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:08 +0000

    Add package cubicweb-forge/1.7.0/cubicweb-forge-1.7.0.tar.gz

---
+++ packages/cubicweb-forge/cubicweb-forge-1.7.0.tar.gz/cubicweb-forge-1.7.0/views/ticket.py
@@ -0,0 +1,36 @@
+"""some additional tabs for Ticket entities
+
+:organization: Logilab
+:copyright: 2006-2011 LOGILAB S.A. (Paris, FRANCE), license is LGPL.
+:contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
+"""
+__docformat__ = "restructuredtext en"
+_ = unicode
+
+from logilab.mtconverter import xml_escape
+from cubicweb.web import component, box
+from cubicweb.web.views import tabs, primary
+from cubicweb.selectors import one_line_rset, is_instance, has_related_entities
+
+class TicketTestCardCtxComponent(component.RelatedObjectsCtxComponent):
+    """display project's test cards"""
+    __regid__ = 'tickettests'
+    __select__ = component.RelatedObjectsCtxComponent.__select__ & is_instance('Ticket')
+
+    rtype = 'test_case_for'
+    target = 'subject'
+
+    title = _('Test cards')
+    context = 'navcontentbottom'
+    order = 30
+
+class TicketScreenshotsView(tabs.EntityRelationView):
+    """display ticket's screenshots """
+    __regid__ = 'ticketscreenshots'
+    __select__ = one_line_rset() & tabs.EntityRelationView.__select__ & is_instance('Ticket')
+
+    rtype = 'attachment'
+    target = 'object'
+
+    title = _('Attached Documents')
+    vid = 'gallery'
