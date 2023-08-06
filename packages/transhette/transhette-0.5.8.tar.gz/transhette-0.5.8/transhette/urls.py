963c3508313fea7897c8dbde91aa956461c391ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:07 +0000

    Add package transhette/0.5.8/transhette-0.5.8.tar.gz

---
+++ packages/transhette/transhette-0.5.8.tar.gz/transhette-0.5.8/transhette/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns('transhette.views',
+    url(r'^$', 'home', name='transhette-home'),
+    url(r'^restart/$', 'restart_server', name='transhette-restart-server'),
+    url(r'^apply_changes/$', 'do_restart', name='apply_changes'),
+    url(r'^pick/$', 'list_languages', name='transhette-pick-file'),
+    url(r'^download/$', 'download_file', name='transhette-download-file'),
+    url(r'^select/(?P<langid>[\w\-]+)/(?P<idx>\d+)/$', 'lang_sel', name='transhette-language-selection'),
+    url(r'^set_new_translation/$', 'set_new_translation', name='set_new_translation'),
+    url(r'^inline_demo/$', 'inline_demo', name='inline_demo'),
+    url(r'^update/confirmation/$', 'update_confirmation', name='transhette-confirmation-file'),
+    url(r'^update/file/((?P<no_confirmation>\w+)/)?$', 'update', name='transhette-update-file'),
+    url(r'^update/catalogue/((?P<no_confirmation>\w+)/)?$', 'update_catalogue', name='transhette-update-catalogue'),
+    url(r'^change/catalogue/$', 'change_catalogue', name='transhette-change-catalogue'),
+    url(r'^translation_conflicts/$', 'translation_conflicts', name='translation_conflicts'),
+    url(r'^ajax/$', 'ajax', name='transhette-ajax'),
+    url(r'^ajax/restart/$', 'ajax_restart', name='transhette-ajax-restart'),
+    url(r'^ajax/are_you_wakeup/$', 'ajax_is_wakeup', name='transhette-ajax-is-wakeup'),
+)
