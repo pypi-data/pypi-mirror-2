f63b8ff3c79d6f678d615c9123709c3c00d7d254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:13 +0000

    Add package jip/0.3/jip-0.3.tar.gz

---
+++ packages/jip/jip-0.3.tar.gz/jip-0.3/setup.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2011 Sun Ning<classicning@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from jip import JIP_VERSION as version
+
+setup(
+    name="jip",
+    version=version,
+    author="Sun Ning",
+    author_email="classicning@gmail.com",
+    url="https://github.com/sunng87/jip",
+    description="jip installs packages, for Jython",
+    scripts = ["scripts/jip", "scripts/jython-all"],
+    license='gpl',
+    py_modules=['jip'],
+    long_description="""
+    Due to the complexity of traditional Java dependency management tool(ivy/maven), I created jip for simple, easy and pythonic. With jip, you can download and install jars from maven central repositories, just like the way of well known python package management tool, pip.
+
+The typical usage of jip is:
+jip install [groupId]:[artifactId]:[version]
+Currently, jip must be used within virtualenv, this is also a best practice for you to create and manage portable environments of python.
+
+jip is under active development, and does not have a stable release currently.
+    """,
+    classifiers=['Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Java',
+        'Environment :: Console',
+        'Operating System :: POSIX']
+)
+
+
