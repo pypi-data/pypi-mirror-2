ee84ad3a1c1133a1fb67c5d349c1c4b3291cda2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package snaked/0.3.4/snaked-0.3.4.tar.gz

---
+++ packages/snaked/snaked-0.3.4.tar.gz/snaked-0.3.4/snaked/plugins/python/plugin.py
@@ -0,0 +1,177 @@
+import os
+
+import gtk
+import gio
+
+from snaked.signals import connect_external, connect_all
+from snaked.util import idle, refresh_gui, lazy_property
+
+from .ropehints import FileHintDb
+
+class Plugin(object):
+    def __init__(self, editor):
+        self.editor = editor
+        idle(connect_all, self, view=editor.view)
+        idle(self.init_completion)
+
+    def init_completion(self):
+        provider = self.completion_provider
+        self.editor.view.get_completion().add_provider(provider)
+        
+    def close(self):
+        if hasattr(self, '__project'):
+            self.project.close()
+            self.hints_monitor.cancel()
+    
+    def refresh_hints(self, filemonitor, file, other_file, event, db):
+        if event in (gio.FILE_MONITOR_EVENT_CHANGES_DONE_HINT, gio.FILE_MONITOR_EVENT_CREATED):        
+            db.refresh()
+    
+    @lazy_property
+    def project(self):
+        if not os.access(self.editor.uri, os.W_OK):
+            root = '/tmp'
+        else:
+            root = self.editor.project_root
+        
+        if root:
+            from rope.base.project import Project
+            project = Project(root)
+            db = FileHintDb(project)
+
+            self.hints_monitor = gio.File(db.hints_filename).monitor_file()
+            self.hints_monitor.connect('changed', self.refresh_hints, db)
+            
+            return project
+        else:
+            return None           
+    
+    @lazy_property
+    def completion_provider(self):
+        import complete
+        return complete.RopeCompletionProvider(self)
+
+    def get_rope_resource(self, project, uri=None):
+        from rope.base import libutils    
+        uri = uri or self.editor.uri
+        return libutils.path_to_resource(project, uri)
+
+    def get_source_and_offset(self):
+        offset = self.editor.cursor.get_offset()
+        source = self.editor.text
+        
+        if not isinstance(source, unicode):
+            source = source.decode('utf8')
+        
+        return source, offset
+
+    def goto_line(self, editor, line):
+        refresh_gui()
+        edit = editor.buffer
+        iterator = edit.get_iter_at_line(line - 1)
+        edit.place_cursor(iterator)
+        editor.view.scroll_to_iter(iterator, 0.001, use_align=True, xalign=1.0)
+
+    def goto_definition(self):
+        project = self.project
+        if not project:
+            project = getattr(self.editor, 'ropeproject', None)
+            if not project:
+                self.editor.message("Can't find project path")
+                return
+        
+        project.validate()
+                 
+        current_resource = self.get_rope_resource(project) 
+        
+        from rope.contrib import codeassist
+
+        try:
+            resource, line = codeassist.get_definition_location(
+                project, *self.get_source_and_offset(),
+                resource=current_resource, maxfixes=3)
+        except Exception, e:
+            import traceback
+            traceback.print_exc()
+            self.editor.message(str(e), 5000)
+            return
+        
+        if resource and resource.real_path == current_resource.real_path:
+            resource = None
+            
+        if resource:
+            uri = resource.real_path
+            editor = self.editor.open_file(uri)
+            editor.ropeproject = project 
+            self.goto_line(editor, line)
+        else:
+            if line:
+                self.goto_line(self.editor, line)
+            else:
+                self.editor.message("Unknown definition")
+
+
+    @connect_external('view', 'key-press-event')
+    def on_textview_key_press_event(self, sender, event):
+        if event.keyval != gtk.keysyms.Return:
+            return False
+            
+        cursor = self.editor.cursor
+        line_start = cursor.copy()
+        line_start.set_line(line_start.get_line())
+        
+        text = line_start.get_text(cursor).strip()
+        if text and text[-1] == ':':
+            end = line_start.copy()
+            end.forward_word_end()
+            end.backward_word_start()
+            ws = line_start.get_text(end)
+
+            if self.editor.view.get_insert_spaces_instead_of_tabs():
+                tab = u' ' * self.editor.view.get_tab_width()
+            else:
+                tab = u'\t'
+                
+            self.editor.buffer.begin_user_action()
+            self.editor.buffer.insert(cursor, u'\n' + ws + tab)
+            self.editor.buffer.end_user_action()
+            
+            if cursor.is_end():
+                idle(self.editor.view.scroll_to_iter, cursor, 0.001, use_align=True, xalign=1.0)
+            
+            return True
+        
+        return False
+
+    @connect_external('view', 'backspace')
+    def on_textview_backspace(self, *args):
+        cursor = self.editor.cursor
+        
+        if cursor.starts_line():
+            return False
+        
+        start = cursor.copy()
+        start.set_line(start.get_line())
+            
+        text = start.get_text(cursor)
+        
+        if text.strip():
+            return False
+            
+        delete_from = cursor.copy()
+        if text[-1] == u'\t': 
+            delete_from.backward_char()
+        else:
+            delete_from.backward_chars(self.editor.view.get_tab_width() - 1)
+        
+        if delete_from.get_line() != start.get_line():
+            delete_from = start
+
+        if delete_from.equal(start):
+            delete_from.forward_char()
+
+        self.editor.buffer.begin_user_action()
+        self.editor.buffer.delete(delete_from, cursor)
+        self.editor.buffer.end_user_action()
+
+        return True
---
+++ packages/snaked/snaked-0.3.4.tar.gz/snaked-0.3.4/snaked/plugins/python/ropehints.py
@@ -0,0 +1,197 @@
+import os.path
+import re
+
+import weakref
+
+import rope.base.oi.soi
+import rope.base.pyobjects
+import rope.base.pynames
+from rope.base import exceptions
+from rope.base.pyobjectsdef import PyModule, PyPackage
+
+def update_type_for(project, source, resource):
+    pass
+    
+def infer_parameter_objects_with_hints(func):
+    def inner(pyfunction):
+        params_types = func(pyfunction)
+        
+        try:
+            hintdb = pyfunction.pycore.hintdb
+        except AttributeError:
+            return params_types
+        
+        param_names = pyfunction.get_param_names(False)
+        for i, name in enumerate(param_names):
+            ptype = hintdb.get_function_param_type(pyfunction, name)
+            if ptype != None:
+                params_types[i] = ptype
+        
+        return params_types
+        
+    return inner
+
+rope.base.oi.soi.infer_parameter_objects = infer_parameter_objects_with_hints(
+    rope.base.oi.soi.infer_parameter_objects)
+
+
+def infer_returned_object_with_hints(func):
+    def inner(pyfunction, args):
+        try:
+            hintdb = pyfunction.pycore.hintdb
+        except AttributeError:
+            return func(pyfunction, args)
+        
+        rtype = hintdb.get_function_param_type(pyfunction, 'return')
+        if rtype is None:
+            rtype = func(pyfunction, args)
+        
+        return rtype
+        
+    return inner
+
+rope.base.oi.soi.infer_returned_object = infer_returned_object_with_hints(
+    rope.base.oi.soi.infer_returned_object)
+
+
+def get_module_attribute_with_hints(func, what):
+    def inner(self, name):
+        #print what, self.pycore.modname(self.resource), name
+
+        try:
+            hintdb = self.pycore.hintdb
+        except AttributeError:
+            return func(self, name)
+
+        try:
+            original_pyname = func(self, name)
+        except exceptions.AttributeNotFoundError:
+            original_pyname = None
+        
+        result = hintdb.get_module_attribute(self, name, original_pyname)
+        if not result:
+            raise exceptions.AttributeNotFoundError()
+        else:
+            return result
+        
+    return inner
+        
+PyModule.get_attribute = get_module_attribute_with_hints(PyModule.get_attribute, 'mod')
+PyPackage.get_attribute = get_module_attribute_with_hints(PyPackage.get_attribute, 'pkg')
+
+class HintDb(object):
+    def __init__(self, project):
+        self.type_cache = {}
+        self.module_attrs_cache = weakref.WeakKeyDictionary()
+        project.pycore.hintdb = self
+        
+    def get_function_param_type(self, pyfunc, name):
+        scope_path = self.get_scope_path(pyfunc.get_scope())
+        type_name = self.find_type_for(scope_path, name)
+        if type_name:
+            return self.get_type(pyfunc.pycore, type_name).get_object()
+        else:
+            return None
+        
+    def get_scope_path(self, scope):
+        result = []
+        current_scope = scope
+        while current_scope is not None:
+            pyobj = current_scope.pyobject
+            if isinstance(pyobj, PyModule):
+                name = pyobj.pycore.modname(pyobj.resource)
+            else:
+                name = pyobj.get_name()
+        
+            result.insert(0, name)
+            current_scope = current_scope.parent
+        
+        return '.'.join(result)
+        
+    def find_type_for(scope_path, name):
+        return None
+        
+    def get_type(self, pycore, type_name):
+        try:
+            return self.type_cache[type_name]
+        except KeyError:
+            pass
+        
+        module, sep, name = type_name.strip('()').rpartition('.')
+        if module:
+            module = pycore.get_module(module)
+            obj = module[name]
+        else:
+            obj = pycore.get_module(name)
+        
+        self.type_cache[type_name] = obj
+        return obj
+        
+    def get_module_attribute(self, pymodule, name, original_pyname):
+        try:
+            return self.module_attrs_cache[pymodule][name]
+        except KeyError:
+            pass
+
+        scope_path = pymodule.pycore.modname(pymodule.resource)
+        type_name = self.find_type_for(scope_path, name)
+        if type_name:
+            type = self.get_type(pymodule.pycore, type_name)
+        else:
+            type = None
+        
+        if type:
+            if type_name.endswith('()'):
+                type = type.get_object()
+                
+                obj = rope.base.pyobjects.PyObject(type)
+                if isinstance(original_pyname, rope.base.pynamesdef.AssignedName):
+                    pyname = rope.base.pynamesdef.AssignedName(original_pyname.lineno,
+                        original_pyname.module, obj)
+                else:
+                    pyname = rope.base.pynames.DefinedName(obj)
+            else:
+                pyname = type
+        else:
+            pyname = original_pyname
+        
+        self.module_attrs_cache.setdefault(pymodule, {})[name] = pyname
+        return pyname
+
+        
+class ReHintDb(HintDb):
+    def __init__(self, project):
+        super(ReHintDb, self).__init__(project)
+        self.db = []
+        
+    def add_hint(self, scope, name, object_type):
+        self.db.append((re.compile(scope), re.compile(name), object_type))  
+
+    def find_type_for(self, scope_path, name):
+        for scope, vname, otype in self.db:
+            if scope.search(scope_path) and vname.search(name):
+                return otype
+                
+        return None
+        
+class FileHintDb(ReHintDb):
+    def __init__(self, project):
+        super(FileHintDb, self).__init__(project)
+        self.hints_filename = os.path.join(project.ropefolder.real_path, 'hints')
+        self.load_hints()
+        
+    def refresh(self):
+        self.load_hints()
+        
+    def load_hints(self):
+        self.db[:] = []
+        self.module_attrs_cache.clear()
+        
+        if os.path.exists(self.hints_filename):
+            with open(self.hints_filename) as f:
+                for l in f:
+                    try:
+                        scope, name, type = l.strip().split()
+                        self.add_hint(scope, name, type)
+                    except ValueError:
+                        continue
