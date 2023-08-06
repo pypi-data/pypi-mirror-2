7175c28717727020973612064ccbabf1b190fe62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:02 +0000

    Add package django-performance-tools/0.1/django-performance-tools-0.1.tar.gz

---
+++ packages/django-performance-tools/django-performance-tools-0.1.tar.gz/django-performance-tools-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "django-performance-tools",
+    version = "0.1",
+    packages = [
+        "performance_tools",
+    ],
+    author = "Chris Adams",
+    author_email = "chris@improbable.org",
+    description = "Tools for collecting performance data from Django apps",
+    url = "http://github.com/acdha/django-performance-tools",
+)
