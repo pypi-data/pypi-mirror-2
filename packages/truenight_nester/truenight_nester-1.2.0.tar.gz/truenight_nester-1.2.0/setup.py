96151d48ad43ebd236d96af3684a8920ffdae5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:28 +0000

    Add package truenight_nester/1.2.0/truenight_nester-1.2.0.tar.gz

---
+++ packages/truenight_nester/truenight_nester-1.2.0.tar.gz/truenight_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name         = 'truenight_nester',
+		version      = '1.2.0',
+		py_modules   = ['truenight_nester'],
+		author       = 'truenight',
+		author_email = 'hfpython@headfirstlabs.com',
+		url          = 'http://www.headfirstlabs.com',
+		description  = 'This is a test, NOT a real script',
+	 )
