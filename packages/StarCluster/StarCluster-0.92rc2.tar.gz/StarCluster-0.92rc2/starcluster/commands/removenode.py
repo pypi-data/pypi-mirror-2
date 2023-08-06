37529553cdcdf037eb575cc79241f9cbc96d4256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:51 +0000

    Add package StarCluster/0.92rc2/StarCluster-0.92rc2.tar.gz

---
+++ packages/StarCluster/StarCluster-0.92rc2.tar.gz/StarCluster-0.92rc2/starcluster/commands/removenode.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from completers import ClusterCompleter
+
+
+class CmdRemoveNode(ClusterCompleter):
+    """
+    removenode [options] <cluster_tag> <node> [<node> ...]
+
+    Terminate one or more nodes in the cluster
+
+    Example:
+
+        $ starcluster removenode mynewcluster node003
+
+    This will remove node003 from mynewcluster and terminate the node.
+    """
+    names = ['removenode', 'rn']
+
+    tag = None
+
+    def execute(self, args):
+        if not len(args) >= 2:
+            self.parser.error("please specify a <cluster_tag> and <node>")
+        tag = self.tag = args[0]
+        aliases = args[1:]
+        for alias in aliases:
+            self.cm.remove_node(tag, alias)
