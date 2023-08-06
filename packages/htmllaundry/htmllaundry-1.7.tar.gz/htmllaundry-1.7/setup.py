2ac73c528bf47d3a08d480359a4d95235d2196e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:20 +0000

    Add package htmllaundry/1.7/htmllaundry-1.7.tar.gz

---
+++ packages/htmllaundry/htmllaundry-1.7.tar.gz/htmllaundry-1.7/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = "1.7"
+
+setup(name="htmllaundry",
+      version=version,
+      description="Simple HTML cleanup utilities",
+      long_description=open("README.txt").read()+"\n\n"+
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Text Processing :: Markup :: HTML",
+          ],
+      keywords="html clean",
+      author="Wichert Akkerman",
+      author_email="wichert@wiggy.net",
+      url="",
+      license="BSD",
+      packages=find_packages(exclude=["tests"]),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "lxml",
+      ],
+      extras_require={
+          "z3cform": [ "z3c.form",
+                       "zope.interface",
+                       "zope.component",
+                       "zope.schema",
+                     ],
+      },
+      tests_require="nose>=0.10.0b1",
+      test_suite="nose.collector",
+      )
