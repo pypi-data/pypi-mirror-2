93f61eec5f9cade56d2b8e539447776f4a3426a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:01 +0000

    Add package collective.examples.diazo/0.2/collective.examples.diazo-0.2.tar.gz

---
+++ packages/collective.examples.diazo/collective.examples.diazo-0.2.tar.gz/collective.examples.diazo-0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='collective.examples.diazo',
+      version=version,
+      description="Example of Diazo (xdv) usage in Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "C-X-EXAMPLE.txt")).read() + "\n" + 
+                       open(os.path.join("docs", "ORANGE-SUNSET-THEME.txt")).read() + "\n" + 
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.examples'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.xdv',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
