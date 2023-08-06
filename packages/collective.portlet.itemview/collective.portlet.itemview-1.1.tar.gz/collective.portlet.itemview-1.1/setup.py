81b7dbcdd046f76d5be93b3a04dcf2d9dc100130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:52 +0000

    Add package collective.portlet.itemview/1.1/collective.portlet.itemview-1.1.tar.gz

---
+++ packages/collective.portlet.itemview/collective.portlet.itemview-1.1.tar.gz/collective.portlet.itemview-1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='collective.portlet.itemview',
+      version=version,
+      description="A portlet where you can select one item, and render it with a view",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='jeanmichel.francois@makina-corpus.com',
+      url='https://github.com/collective/collective.portlet.itemview',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require = {'test':['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
