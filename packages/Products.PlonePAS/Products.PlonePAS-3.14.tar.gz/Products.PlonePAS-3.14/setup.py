9e66daebb4d2babbc7a099e9cc8d00df895f4194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:27 +0000

    Add package Products.PlonePAS/3.14/Products.PlonePAS-3.14.tar.gz

---
+++ packages/Products.PlonePAS/Products.PlonePAS-3.14.tar.gz/Products.PlonePAS-3.14/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '3.14'
+
+setup(name='Products.PlonePAS',
+      version=version,
+      description="PlonePAS adapts the PluggableAuthService for use by Plone.",
+      long_description=open("Products/PlonePAS/HISTORY.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+      ],
+      keywords='Zope CMF Plone PAS authentication',
+      author='Kapil Thangavelu, Wichert Akkerman',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/collective/Products.PlonePAS/branches/3.x',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'Products.GenericSetup',
+        'plone.memoize',
+        'plone.openid',
+      ],
+)
