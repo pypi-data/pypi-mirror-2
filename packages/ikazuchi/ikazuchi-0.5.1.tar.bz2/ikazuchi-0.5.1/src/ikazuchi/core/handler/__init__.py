84f5328ffe6fd8be1a152115dfbbf3e6c2859484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:23 +0000

    Add package ikazuchi/0.5.1/ikazuchi-0.5.1.tar.bz2

---
+++ packages/ikazuchi/ikazuchi-0.5.1.tar.bz2/ikazuchi-0.5.1/src/ikazuchi/core/handler/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from base import LanguageHandler, NullHandler
+from text import SingleSentenceHandler
+
+__all__ = [
+    "LanguageHandler",
+    "NullHandler",
+    "SingleSentenceHandler",
+]
