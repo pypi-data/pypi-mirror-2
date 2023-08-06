cd865044eaa505ce11b26448152c990c40f38d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:23 +0000

    Add package inputgetter/1.0.0/inputgetter-1.0.0.zip

---
+++ packages/inputgetter/inputgetter-1.0.0.zip/inputgetter-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+        name         = 'inputgetter',
+        version      = '1.0.0',
+        py_modules   = ['inputgetter'],
+        author       = 'Daniel Glus',
+        author_email = 'danielglus@aol.com'
+      )
