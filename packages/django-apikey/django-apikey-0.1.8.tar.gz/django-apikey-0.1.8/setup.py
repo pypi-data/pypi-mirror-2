d4b68f906828f946393605d8cbbd4c873c598463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:36 +0000

    Add package django-apikey/0.1.8/django-apikey-0.1.8.tar.gz

---
+++ packages/django-apikey/django-apikey-0.1.8.tar.gz/django-apikey-0.1.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os import path
+BASE_DIR = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name='django-apikey',
+    description='Simple Key authentication for Django.'\
+    'Based on  github.com/scoursen/django-apikey',
+    version='0.1.8',
+    long_description=open(path.join(BASE_DIR, 'README.rst')).read(),
+    url='https://www.github.com/jorgeecardona/django-apikey',
+    packages=find_packages(),
+    author='Jorge Eduardo Cardona',
+    author_email='jorgeecardona@gmail.com',
+    license='BSD',
+    keywords='django authentication piston',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        ],
+    )
