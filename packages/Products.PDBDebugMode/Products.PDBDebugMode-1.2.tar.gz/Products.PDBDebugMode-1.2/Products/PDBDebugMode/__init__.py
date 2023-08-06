79ed1dcdae617d025953bdf356945a42291128e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:58 +0000

    Add package Products.PDBDebugMode/1.2/Products.PDBDebugMode-1.2.tar.gz

---
+++ packages/Products.PDBDebugMode/Products.PDBDebugMode-1.2.tar.gz/Products.PDBDebugMode-1.2/Products/PDBDebugMode/__init__.py
@@ -0,0 +1,15 @@
+"""If debug-mode=on, Monkey-patch the zpublisher_exception_hook to
+call pdb.post_mortem on an error and enable import of pdb in
+unprotected code.
+"""
+
+import sys
+
+import Globals
+import AccessControl
+
+if Globals.DevelopmentMode:
+    sys.modules['Products.PDBDebugMode.Globals'] = Globals
+
+    # Allow import of pdb in unprotected code
+    AccessControl.allow_module('pdb')
---
+++ packages/Products.PDBDebugMode/Products.PDBDebugMode-1.2.tar.gz/Products.PDBDebugMode-1.2/Products/PDBDebugMode/pdblogging.py
@@ -0,0 +1,45 @@
+"""Hook the logger.error call."""
+
+import sys
+import re
+import logging
+from pdb import set_trace
+from pdb import post_mortem
+
+logger = logging.getLogger('Products.PDBDebugMode')
+
+LoggerClass = logging.getLoggerClass()
+orig_error = LoggerClass.error
+
+ignore_matchers = (
+    # There's a known error in ZCatalog where deleting a container
+    # will generate these log errors for its children
+    re.compile(
+        '^uncatalogObject unsuccessfully attempted to uncatalog an '
+        'object with a uid of ').search,
+    )
+
+def error(self, msg, *args, **kw):
+    """Drop into pdb when logging an error."""
+    result = orig_error(self, msg, *args, **kw)
+
+    if not isinstance(msg, basestring):
+        msg = str(msg)
+
+    for matcher in ignore_matchers:
+        try:
+            match = matcher(msg)
+        except:
+            logger.exception('Matcher %r failed for log message %r' %
+                             (matcher, msg))
+        else:
+            if match:
+                break
+    else:
+        if kw.get('exc_info'):
+            type, value, traceback = sys.exc_info()
+            post_mortem(traceback)
+        else:
+            set_trace()
+        
+    return result
