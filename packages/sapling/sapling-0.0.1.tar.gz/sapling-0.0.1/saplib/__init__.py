b54e9e93c1a98afff11f722ae308b0d2b42ff741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:51 +0000

    Add package sapling/0.0.1/sapling-0.0.1.tar.gz

---
+++ packages/sapling/sapling-0.0.1.tar.gz/sapling-0.0.1/saplib/__init__.py
@@ -0,0 +1,9 @@
+from config import Config, ConfigError
+from lib import find, with_line_numbers
+from split import Split
+
+__all__ = (
+    'Config',
+    'ConfigError',
+    'Split'
+)
---
+++ packages/sapling/sapling-0.0.1.tar.gz/sapling-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name = 'sapling',
+    version = '0.0.1',
+
+    author = "John Sirois",
+    author_email = "john.sirois@gmail.com",
+    description = "A git porcelain to manage bidirectional subtree syncing with foreign git "
+                  "repositories",
+    license = "Apache License Version 2.0",
+    url = "http://github.com/jsirois/sapling",
+
+    provides = "sapling",
+    requires = [
+      "GitPython (>= 0.3)"
+    ],
+
+    packages = [ 'saplib' ],
+    scripts = [ 'sapling.py' ],
+
+    classifiers = [
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2.6",
+      "Development Status :: 2 - Pre-Alpha",
+      "Environment :: Console",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: Apache Software License",
+
+      # TODO(jsirois): the sapling.py --install action is actually unix/symlink dependant -
+      # perhaps detect windows and just copy the sapling.py script to git-core/git-sap ?
+      "Operating System :: OS Independent",
+
+      "Topic :: Software Development :: Version Control"
+    ],
+)
