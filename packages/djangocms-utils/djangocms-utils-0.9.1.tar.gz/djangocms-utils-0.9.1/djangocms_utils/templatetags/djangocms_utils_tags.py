1282a22d4e035df9c22134e262ed21c69f7ec4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:04 +0000

    Add package djangocms-utils/0.9.1/djangocms-utils-0.9.1.tar.gz

---
+++ packages/djangocms-utils/djangocms-utils-0.9.1.tar.gz/djangocms-utils-0.9.1/djangocms_utils/templatetags/djangocms_utils_tags.py
@@ -0,0 +1,40 @@
+from django import template
+from classytags.arguments import Argument
+from classytags.parser import Parser
+from classytags.core import Options
+from cms.templatetags.placeholder_tags import RenderPlaceholder
+from cms.models import Placeholder
+register = template.Library()
+ 
+class RenderPlaceholderAs(RenderPlaceholder):
+    name = 'render_placeholder_as'
+    options = Options(
+        Argument('placeholder'),
+        'as',
+        Argument('as_var', default=None, required=False),
+        Argument('width', default=None, required=False)
+    )
+
+    def render_tag(self, context, placeholder, as_var, width):
+        rendered = super(RenderPlaceholderAs, self).render_tag(context, placeholder, width)
+        if as_var:
+            context[as_var] = rendered
+            return u''
+        return rendered
+
+register.tag(RenderPlaceholderAs)
+
+def choose_placeholder(placeholders, placeholder):
+    try:
+        return placeholders.get(slot=placeholder)
+    except Placeholder.DoesNotExist:
+        return None
+register.filter('choose_placeholder', choose_placeholder)
+
+def get_placeholder_group(placeholder):
+    placeholder_group = 'none'
+    for group, placeholders in settings.PLACEHOLDER_GROUPS.items():
+        if placeholder in placeholders:
+            placeholder_group = group
+    return placeholder_group
+register.filter('get_placeholder_group', get_placeholder_group)
