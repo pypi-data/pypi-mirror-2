2378c473cf25464c504bf4f05de57b374aefbc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:22 +0000

    Add package pyslha/1.0.3/pyslha-1.0.3.tar.gz

---
+++ packages/pyslha/pyslha-1.0.3.tar.gz/pyslha-1.0.3/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+#from distutils.core import setup
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+## Setup definition
+import pyslha
+__doc__ = pyslha.__doc__
+
+setup(name = 'pyslha',
+      version = pyslha.__version__,
+      py_modules = ["pyslha"],
+      scripts = ["slhaplot", "slha2isawig", "isawig2slha"],
+      author = 'Andy Buckley',
+      author_email = 'andy@insectnation.org',
+      url = 'http://www.insectnation.org/projects/pyslha',
+      description = 'Parsing, manipulating, and visualising SUSY Les Houches Accord data',
+      long_description = __doc__,
+      keywords = 'supersymmetry susy slha simulation mass decay hep physics particle',
+      license = 'GPL',
+      )
