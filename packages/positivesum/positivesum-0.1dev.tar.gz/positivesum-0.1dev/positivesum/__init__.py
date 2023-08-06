da3c6403f78f108b7f5c78b8586e0d08df3848af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:47 +0000

    Add package positivesum/0.1dev/positivesum-0.1dev.tar.gz

---
+++ packages/positivesum/positivesum-0.1dev.tar.gz/positivesum-0.1dev/positivesum/__init__.py
@@ -0,0 +1,16 @@
+from fabric.api import env
+
+class Environment:
+
+    def __init__(self, name, exclude, site_url, locked_db=True):
+        self.name = name
+        self.exclude = exclude
+        self.site_url = site_url
+        self.locked_db = locked_db
+        self.__doc__ = self.site_url
+
+    def __call__(self):
+        env.name = self.name
+        env.exclude = self.exclude
+        env.site_url = self.site_url
+        env.locked_db = self.locked_db
