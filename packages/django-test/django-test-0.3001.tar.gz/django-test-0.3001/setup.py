b6a755235448336468a58ef7aa86323646830c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:46 +0000

    Add package django-test/0.3001/django-test-0.3001.tar.gz

---
+++ packages/django-test/django-test-0.3001.tar.gz/django-test-0.3001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='django-test',
+  version='0.3001',
+  packages=find_packages(),
+  license='GPL',
+  author="Andrew Magee",
+  author_email="amagee@gmail.com",
+  long_description="Awesome replacement for django test runner",
+  keywords = "django test",
+  url = "https://bitbucket.org/amagee/django-test",
+)
