f7a137b95330e8ba0a2056362eec510c0a7c1049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:37 +0000

    Add package simplesms/0.1.0/simplesms-0.1.0.tar.gz

---
+++ packages/simplesms/simplesms-0.1.0.tar.gz/simplesms-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(name = 'simplesms',
+      version = '0.1.0',
+      author = 'Emmanuel Okyere',
+      author_email = 'chief@hutspace.net',
+      url = 'http://github.com/eokyere/simplesms',
+      description = 'Build SMS apps on top of a simple SMS/USSD gateway',
+      packages = ['simplesms', 'simplesms.contrib'],
+      keywords = ['gsm', 'sms', 'communication'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Other Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python'],
+      long_description= """
+      
+      """,
+          )
