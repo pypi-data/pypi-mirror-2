caa262d427e25253414c26891a07004a64bcdd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:28 +0000

    Add package zenpad/0.0.7/zenpad-0.0.7.tar.gz

---
+++ packages/zenpad/zenpad-0.0.7.tar.gz/zenpad-0.0.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7'
+try:
+    README = open('README.rst').read()
+except IOError:
+    pass
+
+setup(
+    name     = 'zenpad',
+    version  = VERSION,
+    author   = 'Imbolc',
+    author_email = 'imbolc@imbolc.name',
+    description = 'Tree-structured notepad',
+    long_description = README,
+    zip_safe   = False,
+    packages = find_packages(),
+    include_package_data = True,
+    url = 'http://zenpad.ru/',    
+    entry_points = {
+        'gui_scripts': [
+            'zenpad = zenpad.zenpad:main',
+        ]
+    },
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: Russian",
+        "Topic :: Text Editors",
+    ],
+)
