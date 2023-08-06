f4a2b1480677349deb99952c066615eae7c13ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:54 +0000

    Add package PyHeat/0.2/PyHeat-0.2.tar.gz

---
+++ packages/PyHeat/PyHeat-0.2.tar.gz/PyHeat-0.2/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Python OpenGL Heatmap Library"""
+
+from distutils.core import setup
+
+
+classifiers = """\
+Programming Language :: Python
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Development Status :: 3 - Alpha
+Environment :: Console :: Framebuffer
+Environment :: Web Environment
+Intended Audience :: Developers
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Multimedia :: Graphics"""
+
+setup(
+    name='PyHeat',
+    version='0.2',
+    description='OpenGL Heatmap Library',
+    classifiers=classifiers.split('\n'),
+    author='Andrew McCollum',
+    author_email='amccollum@gmail.com',
+    license='MIT',
+    url='http://github.com/amccollum/pyheat',
+    packages=['pyheat'],
+    requires=[
+        'PyOpenGL',
+        'PIL',
+        ],
+    )
