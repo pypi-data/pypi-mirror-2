8aaf30533d0ddc7b89636d74a95ff3fd37345282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:05 +0000

    Add package raptus.contenttemplates/1.0b1/raptus.contenttemplates-1.0b1.tar.gz

---
+++ packages/raptus.contenttemplates/raptus.contenttemplates-1.0b1.tar.gz/raptus.contenttemplates-1.0b1/raptus/contenttemplates/__init__.py
@@ -0,0 +1,4 @@
+from zope.i18nmessageid import MessageFactory
+raptusContenttemplatesMessageFactory = MessageFactory('raptus.contenttemplates')
+from Products.PlacelessTranslationService.utility import PTSTranslationDomain
+raptuscontenttemplatesdomain = PTSTranslationDomain('raptus.contenttemplates')
