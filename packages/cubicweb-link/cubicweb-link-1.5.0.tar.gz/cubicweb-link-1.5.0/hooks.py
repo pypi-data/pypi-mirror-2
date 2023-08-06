f8ffd417fbb943466e17baaa529071d829c289f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:48 +0000

    Add package cubicweb-link/1.5.0/cubicweb-link-1.5.0.tar.gz

---
+++ packages/cubicweb-link/cubicweb-link-1.5.0.tar.gz/cubicweb-link-1.5.0/hooks.py
@@ -0,0 +1,7 @@
+from cubicweb.selectors import is_instance
+from cubicweb.sobjects.notification import ContentAddedView
+
+class LinkAddedView(ContentAddedView):
+    """get notified from new links"""
+    __select__ = is_instance('Link')
+    content_attr = 'description'
