d8ada6da0eb58562a854e0032df6eac3fc8335ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:45 +0000

    Add package ll-xist/3.15.3/ll-xist-3.15.3.zip

---
+++ packages/ll-xist/ll-xist-3.15.3.zip/ll-xist-3.15.3/src/ll/scripts/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+## Copyright 1999-2010 by LivingLogic AG, Bayreuth/Germany
+## Copyright 1999-2010 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See ll/__init__.py for the license
