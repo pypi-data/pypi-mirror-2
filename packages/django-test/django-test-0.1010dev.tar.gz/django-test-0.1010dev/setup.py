f7ff23466d8e4cd6b03cac5bcc11ff53b56b92d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:47 +0000

    Add package django-test/0.1010dev/django-test-0.1010dev.tar.gz

---
+++ packages/django-test/django-test-0.1010dev.tar.gz/django-test-0.1010dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+  name='django-test',
+  version='0.1010dev',
+  py_modules=['django_test'],
+  license='GPL',
+  long_description="Awesome replacement for django test runner",
+)
