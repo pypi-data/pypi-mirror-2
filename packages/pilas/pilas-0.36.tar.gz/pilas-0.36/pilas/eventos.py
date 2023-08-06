cdac7ede1606654f1b186181e102efc693752871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:06 +0000

    Add package pilas/0.36/pilas-0.36.tar.gz

---
+++ packages/pilas/pilas-0.36.tar.gz/pilas-0.36/pilas/eventos.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+# Pilas engine - A video game framework.
+#
+# Copyright 2010 - Hugo Ruscitti
+# License: LGPLv3 (see http://www.gnu.org/licenses/lgpl.html)
+#
+# Website - http://www.pilas-engine.com.ar
+
+import dispatch
+
+mueve_mouse = dispatch.Signal(providing_args=['x', 'y', 'dx', 'dy'])
+click_de_mouse = dispatch.Signal(providing_args=['button', 'x', 'y'])
+termina_click = dispatch.Signal(providing_args=['button', 'x', 'y'])
+mueve_rueda = dispatch.Signal(providing_args=['delta'])
+pulsa_tecla = dispatch.Signal(providing_args=['code'])
+pulsa_tecla_escape = dispatch.Signal(providing_args=[])
+actualizar = dispatch.Signal(providing_args=[])
+
+# Se emite cuando el mundo ingresa o sale del modo depuracion (pulsando F12)
+inicia_modo_depuracion = dispatch.Signal(providing_args=[]) 
+sale_modo_depuracion = dispatch.Signal(providing_args=[])
+actualiza_modo_depuracion = dispatch.Signal(providing_args=[])
