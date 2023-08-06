137e6f1bed9433c4cee7463e57a42725cc63490c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:35 +0000

    Add package Plone/3.3.6/Plone-3.3.6.tar.gz

---
+++ packages/Plone/Plone-3.3.6.tar.gz/Plone-3.3.6/Products/CMFPlone/interfaces/InterfaceTool.py
@@ -0,0 +1,8 @@
+""" InterfaceTool interface.
+
+$Id: InterfaceTool.py 51307 2011-07-19 09:39:45Z evilbungle $
+"""
+
+# BBB: module will be removed in Plone 3.0
+#      zope2 interfaces created on runtime:
+#      - IInterfaceTool
---
+++ packages/Plone/Plone-3.3.6.tar.gz/Plone-3.3.6/Products/CMFPlone/interfaces/PropertiesTool.py
@@ -0,0 +1,9 @@
+""" Properties tool interface.
+
+$Id: PropertiesTool.py 51307 2011-07-19 09:39:45Z evilbungle $
+"""
+
+# BBB: module will be removed in Plone 4.0
+#      zope2 interfaces created on runtime:
+#      - IPortalProperties
+#      - ISimpleItemWithProperties
