13a20be7092d5eb73b174cad1145ba716b151e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:24 +0000

    Add package collective.vaporisation/1.2.0/collective.vaporisation-1.2.0.tar.gz

---
+++ packages/collective.vaporisation/collective.vaporisation-1.2.0.tar.gz/collective.vaporisation-1.2.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of collective.vaporisation
+"""
+import os
+from setuptools import setup, find_packages
+
+version = '1.2.0'
+
+tests_require=['zope.testing']
+
+setup(name='collective.vaporisation',
+      version=version,
+      description="Plone portlet for vaporisation tagcloud",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        "Development Status :: 4 - Beta",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='plone plonegov vaporisation tag-cloud portlet',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='https://svn.plone.org/svn/collective/collective.vaporisation',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'collective.vaporisation.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*- 
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
