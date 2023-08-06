838fea7b5cce36164839de136647e635ec03a8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package tvbutler/0.1a5/tvbutler-0.1a5.zip

---
+++ packages/tvbutler/tvbutler-0.1a5.zip/tvbutler-0.1a5/HISTORY.rst
@@ -0,0 +1,25 @@
+0.1a5 - Unreleased
+==================
+
+* log to stdout, too to get instant feedback when calling tvbutler from console
+* robustness fix (tvbutler would barf on some entries and stop processing the remaining entries)
+
+0.1a4 - 2010-12-08
+==================
+
+* added global regular expression filter (defaults to skip archives of
+  seasons)
+
+  `UPGRADE NOTE`: add ``global_exclude_regex=(all.month|month.of|season[\s\d]*complete)``
+  to the ``[main]`` section your existing config to activate this feature.
+
+0.1a3 - 2010-12-05
+==================
+
+* added logging
+
+
+0.1a2 - 2010-12-05
+==================
+
+Initial release (0.1a was nuked due to packaging foobar)
---
+++ packages/tvbutler/tvbutler-0.1a5.zip/tvbutler-0.1a5/tvbutler/settings.py
@@ -0,0 +1,27 @@
+import logging
+from os import mkdir, path
+import ConfigParser
+from shutil import copy
+
+CONFIG_PATH = path.expanduser('~/.tvbutler')
+DB_URL = 'sqlite:///%s' % path.join(CONFIG_PATH, 'database.db')
+log = logging.getLogger('tvbutler')
+
+settings_path = path.join(CONFIG_PATH, 'config')
+if not path.exists(CONFIG_PATH):
+    mkdir(CONFIG_PATH, 0700)
+    default_settings_path = path.join(path.dirname(path.abspath(__file__)), 'config-sample.ini')
+    copy(default_settings_path, settings_path)
+    print "No feeds specified. Add them to %s and try again." % settings_path
+    exit()
+
+settings = ConfigParser.ConfigParser()
+settings.read(settings_path)
+
+logfile = logging.FileHandler(path.join(CONFIG_PATH, 'log'))
+logfile.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+log.addHandler(logfile)
+console = logging.StreamHandler()
+console.setFormatter(logfile.setFormatter(logging.Formatter('%(message)s')))
+log.addHandler(console)
+log.setLevel(logging.INFO)
