e6753a609624ba83784f5992613741eff04c5e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:15 +0000

    Add package rpclib/2.2.0-beta/rpclib-2.2.0-beta.tar.gz

---
+++ packages/rpclib/rpclib-2.2.0-beta.tar.gz/rpclib-2.2.0-beta/src/rpclib/application.py
@@ -0,0 +1,102 @@
+
+#
+# rpclib - Copyright (C) Rpclib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+import logging
+logger = logging.getLogger(__name__)
+
+from rpclib.model.fault import Fault
+from rpclib._base import EventManager
+
+class Application(object):
+    transport = None
+
+    def __init__(self, services, tns, interface, in_protocol, out_protocol,
+                                                                    name=None):
+        '''Constructor.
+
+        @param An iterable of ServiceBase subclasses that define the exposed
+               services.
+        @param The targetNamespace attribute of the exposed service.
+        @param The name attribute of the exposed service.
+        '''
+
+        self.services = services
+        self.tns = tns
+        self.name = name
+        if self.name is None:
+            self.name = self.__class__.__name__.split('.')[-1]
+
+        self.interface = interface
+        self.interface.set_app(self)
+
+        self.in_protocol = in_protocol
+        self.in_protocol.set_app(self)
+
+        self.out_protocol = out_protocol
+        self.out_protocol.set_app(self)
+
+        self.__public_methods = {}
+        self.__classes = {}
+
+        self.event_manager = EventManager(self)
+        self.error_handler = None
+
+    def process_request(self, ctx, req_obj):
+        """Takes a MethodContext instance and the native request object.
+        Returns the response to the request as a native python object.
+
+        Not meant to be overridden.
+        """
+
+        try:
+            # implementation hook
+            ctx.service_class.event_manager.fire_event('method_call', ctx)
+
+            # call the method
+            ctx.out_object = ctx.service_class.call_wrapper(ctx, ctx.descriptor.function, req_obj)
+
+            # fire events
+            self.event_manager.fire_event('method_return_object', ctx)
+            ctx.service_class.event_manager.fire_event(
+                                                    'method_return_object', ctx)
+
+        except Fault, e:
+            logger.exception(e)
+
+            ctx.out_error = e
+
+            # fire events
+            self.event_manager.fire_event('method_exception_object', ctx)
+            if ctx.service_class != None:
+                ctx.service_class.event_manager.fire_event(
+                                                    'method_return_object', ctx)
+
+        except Exception, e:
+            logger.exception(e)
+
+            ctx.out_error = Fault('Server', str(e))
+
+            # fire events
+            self.event_manager.fire_event('method_exception_object', ctx)
+            if ctx.service_class != None:
+                ctx.service_class.event_manager.fire_event(
+                                                    'method_return_object', ctx)
+
+    def _has_callbacks(self):
+        return self.interface._has_callbacks()
