8bd03ac44d8f896b4eca27538c0d211f24780d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:29 +0000

    Add package pygeoyandex/0.0.3/pygeoyandex-0.0.3.tar.gz

---
+++ packages/pygeoyandex/pygeoyandex-0.0.3.tar.gz/pygeoyandex-0.0.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#coding: utf-8
+from setuptools import setup
+
+setup(
+    name='pygeoyandex',
+    version='0.0.3',
+    author='Zakhar Zibarov',
+    author_email='zakhar.zibarov@gmail.com',
+    py_modules=['pygeoyandex'],
+    url='http://bitbucket.org/zakhar/pygeoyandex',
+    description = 'Yandex geocoding and reverse geocoding',
+
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Natural Language :: Russian',
+    ),
+)
