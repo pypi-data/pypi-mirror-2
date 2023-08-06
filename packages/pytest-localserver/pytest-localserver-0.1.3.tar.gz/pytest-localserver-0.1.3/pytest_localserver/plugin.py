8c15f4525ac5bc7a46b7b9eb92e96b0e267bfc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:48 +0000

    Add package pytest-localserver/0.1.3/pytest-localserver-0.1.3.tar.gz

---
+++ packages/pytest-localserver/pytest-localserver-0.1.3.tar.gz/pytest-localserver-0.1.3/pytest_localserver/plugin.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2011 Sebastian Rahlf <basti at redtoad dot de>
+#
+# This program is release under the MIT license. You can find the full text of
+# the license in the LICENSE file.
+
+from pytest_localserver import http, https, smtp
+
+def pytest_funcarg__httpserver(request):
+    """The returned ``httpserver`` funcarg provides these
+    helper methods to...
+    """
+    server = http.Server()
+    server.start()
+    request.addfinalizer(server.stop)
+    return server
+
+def pytest_funcarg__httpsserver(request):
+    """The returned ``httpsserver`` funcarg provides these
+    helper methods to...
+    """
+    server = https.Server()
+    server.start()
+    request.addfinalizer(server.stop)
+    return server
+
+def pytest_funcarg__smtpserver(request):
+    """The returned ``smtpserver`` funcarg provides these
+    helper methods to...
+    """
+    server = smtp.Server()
+    server.start()
+    request.addfinalizer(server.stop)
+    return server
