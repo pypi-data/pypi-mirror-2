e76dc100b76df0efb0d5bb8d19c93613992e84d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:03 +0000

    Add package rpclib/2.3.1-beta/rpclib-2.3.1-beta.tar.gz

---
+++ packages/rpclib/rpclib-2.3.1-beta.tar.gz/rpclib-2.3.1-beta/src/rpclib/model/_base.py
@@ -0,0 +1,220 @@
+
+#
+# rpclib - Copyright (C) Rpclib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+import rpclib.const.xml_ns
+
+"""This module contains the ModelBase class and other building blocks for
+defining models.
+"""
+
+def nillable_dict(func):
+    """Decorator that retuns empty dictionary if input is None"""
+
+    def wrapper(cls, element):
+        if element is None:
+            return {}
+        else:
+            return func(cls, element)
+    return wrapper
+
+def nillable_string(func):
+    """Decorator that retuns None if input is None."""
+
+    def wrapper(cls, string):
+        if string is None:
+            return None
+        else:
+            return func(cls, string)
+    return wrapper
+
+class ModelBase(object):
+    """The base class for type markers. It defines the model interface for the
+    interface generators to use and also manages class customizations that are
+    mainly used for defining constraints on input values.
+    """
+
+    __namespace__ = None
+    __type_name__ = None
+
+    # These are not the xml schema defaults. The xml schema defaults are
+    # considered in ComplexModel's add_to_schema method. the defaults here
+    # are to reflect what people seem to want most.
+    class Attributes(object):
+        nillable = True
+        min_occurs = 0
+        max_occurs = 1
+
+    class Annotations(object):
+        doc = ""
+
+    class Empty(object):
+        pass
+
+    @staticmethod
+    def is_default(cls):
+        return True
+
+    @classmethod
+    def get_namespace_prefix(cls, interface):
+        """Returns the namespace prefix for the given interface. The
+        get_namespace_prefix of the interface class generates a prefix if none
+        is defined.
+        """
+
+        ns = cls.get_namespace()
+
+        retval = interface.get_namespace_prefix(ns)
+
+        return retval
+
+    @classmethod
+    def get_namespace(cls):
+        """Returns the namespace of the class. Defaults to the python module
+        name."""
+
+        return cls.__namespace__
+
+    @staticmethod
+    def resolve_namespace(cls, default_ns):
+        """This call finalizes the namespace assignment. The default namespace
+        is not available until the application calls populate_interface method
+        of the interface generator.
+        """
+
+        if cls.__namespace__ is rpclib.const.xml_ns.DEFAULT_NS:
+            cls.__namespace__ = default_ns
+
+        if (cls.__namespace__ in rpclib.const.xml_ns.const_prefmap and
+                                                       not cls.is_default(cls)):
+            cls.__namespace__ = default_ns
+
+        if cls.__namespace__ is None:
+            cls.__namespace__ = cls.__module__
+
+    @classmethod
+    def get_type_name(cls):
+        """Returns the class name unless the __type_name__ attribute is defined.
+        """
+
+        retval = cls.__type_name__
+        if retval is None:
+            retval = cls.__name__
+
+        return retval
+
+    @classmethod
+    def get_type_name_ns(cls, app):
+        """Returns the type name with a namespace prefix, separated by a column.
+        """
+
+        if cls.get_namespace() != None:
+            return "%s:%s" % (cls.get_namespace_prefix(app), cls.get_type_name())
+
+    @classmethod
+    @nillable_string
+    def to_string(cls, value):
+        """Returns str(value). This should be overridden if this is not
+        enough."""
+
+        return str(value)
+
+    @classmethod
+    @nillable_string
+    def to_dict(cls, value):
+        """Returns a dict with type name as key and str(value) as value. This
+        should be overridden if this is not enough."""
+
+        return {cls.get_type_name(): cls.to_string(value)}
+
+    @classmethod
+    def customize(cls, **kwargs):
+        """Returns a duplicate of cls only with values in function arguments
+        overwriting the ones in cls.Attributes."""
+
+        cls_name, cls_bases, cls_dict = cls._s_customize(cls, **kwargs)
+
+        return type(cls_name, cls_bases, cls_dict)
+
+    @staticmethod
+    def _s_customize(cls, ** kwargs):
+        """This function duplicates and customizes the class it belongs to. The
+        original class remains unchanged.
+
+        Not meant to be overridden.
+        """
+
+        cls_dict = {}
+
+        for k in cls.__dict__:
+            if not (k in ("__dict__", "__weakref__")):
+                cls_dict[k] = cls.__dict__[k]
+
+        class Attributes(cls.Attributes):
+            pass
+        cls_dict['Attributes'] = Attributes
+
+        class Annotations(cls.Annotations):
+            pass
+        cls_dict['Annotations'] = Annotations
+
+        if not ('_is_clone_of' in cls_dict):
+            cls_dict['_is_clone_of'] = cls
+
+        for k, v in kwargs.items():
+            if k != "doc":
+                setattr(Attributes, k, v)
+            else:
+                setattr(Annotations, k, v)
+
+        return (cls.__name__, cls.__bases__, cls_dict)
+
+class Null(ModelBase):
+    @classmethod
+    def to_string(cls, value):
+        return ""
+
+    @classmethod
+    def from_string(cls, value):
+        return None
+
+class SimpleModel(ModelBase):
+    __namespace__ = "http://www.w3.org/2001/XMLSchema"
+    __base_type__ = None
+
+    class Attributes(ModelBase.Attributes):
+        values = set()
+
+    def __new__(cls, ** kwargs):
+        """Overriden so that any attempt to instantiate a primitive will return
+        a customized class instead of an instance.
+
+        See rpclib.model.base.ModelBase for more information.
+        """
+
+        retval = cls.customize( ** kwargs)
+
+        if not retval.is_default(retval):
+            retval.__base_type__ = cls
+            retval.__type_name__ = kwargs.get("type_name", ModelBase.Empty)
+
+        return retval
+
+    @staticmethod
+    def is_default(cls):
+        return (cls.Attributes.values == SimpleModel.Attributes.values)
---
+++ packages/rpclib/rpclib-2.3.1-beta.tar.gz/rpclib-2.3.1-beta/src/rpclib/protocol/xml/_base.py
@@ -0,0 +1,211 @@
+
+#
+# rpclib - Copyright (C) Rpclib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+import logging
+logger = logging.getLogger(__name__)
+
+from lxml import etree
+
+from rpclib.const import xml_ns as ns
+
+from rpclib.util.cdict import cdict
+
+from rpclib.error import NotFoundError
+from rpclib.model import ModelBase
+
+from rpclib.model.complex import Array
+from rpclib.model.complex import Iterable
+from rpclib.model.complex import ComplexModelBase
+from rpclib.model.enum import EnumBase
+from rpclib.model.fault import Fault
+from rpclib.model.primitive import AnyXml
+from rpclib.model.primitive import AnyDict
+from rpclib.model.primitive import String
+from rpclib.model.primitive import Duration
+
+from rpclib.protocol import ProtocolBase
+
+from rpclib.protocol.xml.model import base_to_parent_element
+from rpclib.protocol.xml.model.enum import enum_to_parent_element
+from rpclib.protocol.xml.model.fault import fault_to_parent_element
+from rpclib.protocol.xml.model.complex import complex_to_parent_element
+from rpclib.protocol.xml.model.primitive import xml_to_parent_element
+from rpclib.protocol.xml.model.primitive import dict_to_parent_element
+from rpclib.protocol.xml.model.primitive import string_to_parent_element
+from rpclib.protocol.xml.model.primitive import duration_to_parent_element
+
+from rpclib.protocol.xml.model import base_from_element
+from rpclib.protocol.xml.model.complex import array_from_element
+from rpclib.protocol.xml.model.complex import iterable_from_element
+from rpclib.protocol.xml.model.complex import complex_from_element
+from rpclib.protocol.xml.model.enum import enum_from_element
+from rpclib.protocol.xml.model.fault import fault_from_element
+from rpclib.protocol.xml.model.primitive import dict_from_element
+from rpclib.protocol.xml.model.primitive import xml_from_element
+from rpclib.protocol.xml.model.primitive import string_from_element
+
+_serialization_handlers = cdict({
+    ModelBase: base_to_parent_element,
+    ComplexModelBase: complex_to_parent_element,
+    Fault: fault_to_parent_element,
+    String: string_to_parent_element,
+    AnyXml: xml_to_parent_element,
+    AnyDict: dict_to_parent_element,
+    EnumBase: enum_to_parent_element,
+    Duration: duration_to_parent_element,
+})
+
+_deserialization_handlers = cdict({
+    ModelBase: base_from_element,
+    ComplexModelBase: complex_from_element,
+    Fault: fault_from_element,
+    String: string_from_element,
+    AnyXml: xml_from_element,
+    AnyDict: dict_from_element,
+    Array: array_from_element,
+    Iterable: iterable_from_element,
+    EnumBase: enum_from_element,
+})
+
+class XmlObject(ProtocolBase):
+    def from_element(self, cls, element):
+        handler = _deserialization_handlers[cls]
+        return handler(self, cls, element)
+
+    def to_parent_element(self, cls, value, tns, parent_elt, * args, ** kwargs):
+        handler = _serialization_handlers[cls]
+        handler(self, cls, value, tns, parent_elt, * args, ** kwargs)
+
+    def create_in_document(self, ctx, charset=None):
+        ctx.in_document = etree.fromstring(ctx.in_string, charset)
+
+        body_doc = ctx.in_document
+
+        try:
+            self.validate(body_doc)
+            if (not (body_doc is None) and
+                (ctx.method_request_string is None)):
+                ctx.method_request_string = body_doc.tag
+                logger.debug("\033[92mMethod request_string: %r\033[0m" %
+                                                    ctx.method_request_string)
+
+        finally:
+            # for performance reasons, we don't want the following to run
+            # in production even though we won't see the results.
+            # that's why one needs to explicitly set the logging level of
+            # the 'rpclib.protocol.xml._base' to DEBUG to see the xml data.
+            if logger.level == logging.DEBUG:
+                try:
+                    logger.debug(etree.tostring(body_doc, pretty_print=True))
+                except etree.XMLSyntaxError, e:
+                    logger.debug(body_doc)
+                    raise Fault('Client.Xml', 'Error at line: %d, col: %d' %
+                                                                    e.position)
+
+        if ctx.method_request_string is None:
+            raise Exception("Could not extract method request string from "
+                            "the request!")
+        try:
+            if ctx.service_class is None: # i.e. if it's a server
+                self.set_method_descriptor(ctx)
+
+        except Exception, e:
+            logger.exception(e)
+            raise NotFoundError('Client', 'Method not found: %r' %
+                                                    ctx.method_request_string)
+
+        ctx.in_header_doc = None # XmlObject does not know between header and
+            # payload. That's SOAP's job to do.
+        ctx.in_body_doc = body_doc
+
+    def create_out_string(self, ctx, charset=None):
+        """Sets an iterable of string fragments to ctx.out_string"""
+        if charset is None:
+            charset = 'utf8'
+
+        ctx.out_string = [etree.tostring(ctx.out_document, xml_declaration=True,
+                                                            encoding=charset)]
+
+    def deserialize(self, ctx, way='out'):
+        """Takes a MethodContext instance and a string containing ONE root xml
+        tag.
+
+        Returns the corresponding native python object
+
+        Not meant to be overridden.
+        """
+
+        assert way in ('in', 'out')
+
+        if way == 'in':
+            body_class = ctx.descriptor.in_message
+
+        elif self.in_wrapper is self.OUT_WRAPPER:
+            body_class = ctx.descriptor.out_message
+
+        # decode method arguments
+        if ctx.in_body_doc is not None and len(ctx.in_body_doc) > 0:
+            ctx.in_object = self.from_element(body_class, ctx.in_body_doc)
+        else:
+            ctx.in_object = [None] * len(body_class._type_info)
+
+        self.event_manager.fire_event('deserialize', ctx)
+
+    def serialize(self, ctx, way='out'):
+        """Uses ctx.out_object, ctx.out_header or ctx.out_error to set
+        ctx.out_body_doc, ctx.out_header_doc and ctx.out_document as an
+        lxml.etree._Element instance.
+
+        Not meant to be overridden.
+        """
+
+        assert way in ('in', 'out')
+
+        # instantiate the result message
+        if way == 'in':
+            result_message_class = ctx.descriptor.in_message
+        else:
+            result_message_class = ctx.descriptor.out_message
+
+        result_message = result_message_class()
+
+        # assign raw result to its wrapper, result_message
+        out_type_info = result_message_class._type_info
+
+        if len(out_type_info) == 1:
+            attr_name = result_message_class._type_info.keys()[0]
+            setattr(result_message, attr_name, ctx.out_object)
+
+        else:
+            for i in range(len(out_type_info)):
+                attr_name = result_message_class._type_info.keys()[i]
+                setattr(result_message, attr_name, ctx.out_object[i])
+
+        # transform the results into an element
+        tmp_elt = etree.Element('{%s}punk' % ns.soap_env)
+        self.to_parent_element(result_message_class,
+                    result_message, self.app.interface.get_tns(), tmp_elt)
+        ctx.out_document = tmp_elt[0]
+
+        if logger.level == logging.DEBUG:
+            logger.debug('\033[91m' + "Response" + '\033[0m')
+            logger.debug(etree.tostring(ctx.out_document,
+                         xml_declaration=True, pretty_print=True))
+
+        self.event_manager.fire_event('serialize', ctx)
