c4d62553f5617442a139e2b9845b0b44ae8136fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:26 +0000

    Add package braintree/2.11.0/braintree-2.11.0.tar.gz

---
+++ packages/braintree/braintree-2.11.0.tar.gz/braintree-2.11.0/braintree/braintree_gateway.py
@@ -0,0 +1,19 @@
+from braintree.address_gateway import AddressGateway
+from braintree.credit_card_gateway import CreditCardGateway
+from braintree.customer_gateway import CustomerGateway
+from braintree.settlement_batch_summary_gateway import SettlementBatchSummaryGateway
+from braintree.subscription_gateway import SubscriptionGateway
+from braintree.transaction_gateway import TransactionGateway
+from braintree.transparent_redirect_gateway import TransparentRedirectGateway
+
+class BraintreeGateway(object):
+    def __init__(self, config):
+        self.config = config
+        self.address = AddressGateway(self)
+        self.credit_card = CreditCardGateway(self)
+        self.customer = CustomerGateway(self)
+        self.settlement_batch_summary = SettlementBatchSummaryGateway(self)
+        self.subscription = SubscriptionGateway(self)
+        self.transaction = TransactionGateway(self)
+        self.transparent_redirect = TransparentRedirectGateway(self)
+
