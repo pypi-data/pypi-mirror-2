07ea5ec9136561c7d32620a6ce9525c693fc25d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:16 +0000

    Add package Editra/0.6.0/Editra-0.6.0.tar.gz

---
+++ packages/Editra/Editra-0.6.0.tar.gz/Editra-0.6.0/src/info.py
@@ -0,0 +1,22 @@
+###############################################################################
+# Name: info.py                                                               #
+# Purpose: Global project information                                         #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2008 Cody Precord <staff@editra.org>                         #
+# License: wxWindows License                                                  #
+###############################################################################
+
+"""Editra Project information module"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id: info.py 66451 2010-12-26 16:11:56Z CJP $"
+__revision__ = "$Revision: 66451 $"
+
+#-----------------------------------------------------------------------------#
+
+AUTHOR = 'Cody Precord'
+VERSION = '0.6.0'
+PROG_NAME = 'Editra'
+HOME_PAGE = "http://editra.org"
+I18N_PAGE = "https://translations.launchpad.net/editra/trunk/+pots/editra"
+CONTACT_MAIL = "staff@editra.org"
