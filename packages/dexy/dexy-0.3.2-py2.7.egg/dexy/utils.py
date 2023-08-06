5cf9676edecdcb9ddba67daaba004e3e10adc87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:30 +0000

    Add package dexy/0.3.2/dexy-0.3.2-py2.7.egg

---
+++ packages/dexy/dexy-0.3.2-py2.7.egg/dexy/utils.py
@@ -0,0 +1,37 @@
+from ansi2html import Ansi2HTMLConverter
+from pynliner import Pynliner
+from BeautifulSoup import BeautifulSoup
+
+def ansi_output_to_html(ansi_text, log=None):
+    converter = Ansi2HTMLConverter()
+    html = converter.convert(ansi_text)
+
+    p = Pynliner()
+    if log:
+        p.log = log
+    p.from_string(html)
+    html_with_css_inline = p.run()
+
+    # Ansi2HTMLConverter returns a complete HTML document, we just want body
+    doc = BeautifulSoup(html_with_css_inline)
+    return doc.body.renderContents()
+
+def print_string_diff(str1, str2):
+    msg = ""
+    for i, c1 in enumerate(str1):
+        if len(str2) > i:
+            c2 = str2[i]
+            if c1 == c2:
+                flag = ""
+            else:
+                flag = " <---"
+            if ord(c1) > ord('a') and ord(c2) > ord('a'):
+                msg = msg + "\n%5d: %s\t%s\t\t%s\t%s %s" % (i, c1, c2,
+                                              ord(c1), ord(c2), flag)
+            else:
+                msg = msg + "\n%5d:  \t \t\t%s\t%s %s" % (i, ord(c1),
+                                              ord(c2), flag)
+        else:
+            flag = "<---"
+            msg = msg + "\n%5d:  \t \t\t%s %s" % (i, ord(c1), flag)
+    return msg
---
+++ packages/dexy/dexy-0.3.2-py2.7.egg/handlers/pynliner_handler.py
@@ -0,0 +1,22 @@
+from ordereddict import OrderedDict
+from dexy.handler import DexyHandler
+from pynliner import Pynliner
+
+class PynlinerHandler(DexyHandler):
+    """
+    Move CSS inline, for posting to web without a stylesheet or for emailing.
+    For now hard-coded to look for a pastie.css file in project root.
+    """
+    ALIASES = ['pynliner', 'inlinecss']
+
+    def process_dict(self, input_dict):
+        #matches = [k for k in self.artifact.input_artifacts_dict.keys() if k.endswith(".css|dexy")]
+        #k = matches[0]
+        css = open("pastie.css", "r").read()
+
+        output_dict = OrderedDict()
+        for k, v in input_dict.items():
+            p = Pynliner()
+            p.from_string(v).with_cssString(css)
+            output_dict[k] = p.run()
+        return output_dict
