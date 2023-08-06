8bb2ec43dc8412cda0de3a1862f48439db069339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:32 +0000

    Add package Mopidy/0.3.0/Mopidy-0.3.0.tar.gz

---
+++ packages/Mopidy/Mopidy-0.3.0.tar.gz/Mopidy-0.3.0/mopidy/__main__.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+# Add ../ to the path so we can run Mopidy from a Git checkout without
+# installing it on the system.
+sys.path.insert(0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+from mopidy.core import CoreProcess
+
+def main():
+    # Explictly call run() instead of start(), since we don't need to start
+    # another process.
+    CoreProcess().run()
+
+if __name__ == '__main__':
+    main()
