a49df8906d90327dcde3f30a9e325c9b0c11a8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:00 +0000

    Add package garlicsim_lib/0.6.2/garlicsim_lib-0.6.2.tar.gz

---
+++ packages/garlicsim_lib/garlicsim_lib-0.6.2.tar.gz/garlicsim_lib-0.6.2/garlicsim_lib/simpacks/prisoner/wx/widgets/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+Widgets for `prisoner`.
+'''
+
+from .state_viewer import StateViewer
---
+++ packages/garlicsim_lib/garlicsim_lib-0.6.2.tar.gz/garlicsim_lib-0.6.2/garlicsim_lib/simpacks/queue/wx/settings.py
@@ -0,0 +1,5 @@
+from . import widgets
+
+SEEK_BAR_GRAPHS = []
+BIG_WORKSPACE_WIDGETS = [widgets.StateViewer]
+SMALL_WORKSPACE_WIDGETS = []
