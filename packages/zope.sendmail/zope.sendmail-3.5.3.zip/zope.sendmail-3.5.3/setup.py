08b3d099383ecaa3084f0b1b87bbac26d60cbf12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:00 +0000

    Add package zope.sendmail/3.5.3/zope.sendmail-3.5.3.zip

---
+++ packages/zope.sendmail/zope.sendmail-3.5.3.zip/zope.sendmail-3.5.3/setup.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+
+setup(name='zope.sendmail',
+      version = '3.5.3',
+      url='http://pypi.python.org/pypi/zope.sendmail',
+      license='ZPL 2.1',
+      description='Zope sendmail',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description='\n\n'.join([
+          open('README.txt').read(),
+          open('CHANGES.txt').read(),
+          ]),
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['setuptools',
+                        'ZODB3',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.schema',
+                        'zope.security',
+                       ],
+      include_package_data = True,
+      zip_safe = False,
+      )
