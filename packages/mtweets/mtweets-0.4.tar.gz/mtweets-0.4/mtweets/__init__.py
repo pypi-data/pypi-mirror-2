8c950e6cc31c682ac4b37c15ef7f40bebf5620da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:10 +0000

    Add package mtweets/0.4/mtweets-0.4.tar.gz

---
+++ packages/mtweets/mtweets-0.4.tar.gz/mtweets-0.4/mtweets/__init__.py
@@ -0,0 +1,15 @@
+"""mtweets - Easy Twitter utilities in Python
+
+mtweets is an up-to-date library for Python that wraps the Twitter API.
+Other Python Twitter libraries seem to have fallen a bit behind, and
+Twitter's API has evolved a bit. Here's hoping this helps.
+
+Please review the online twitter docs for concepts used by twitter:
+
+    http://developer.twitter.com/doc
+
+like as Tweet entities, and how use each parameter
+"""
+
+from api import API
+from streaming import Stream
