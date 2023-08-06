891270ad415f21b2ab64389e08a9df373dab1512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:01 +0000

    Add package django-typogrify/1.2.1/django-typogrify-1.2.1.tar.gz

---
+++ packages/django-typogrify/django-typogrify-1.2.1.tar.gz/django-typogrify-1.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-typogrify',
+    version='1.2.1',
+    description='Make type not look like crap (for django apps)',
+    author='Chris Drackett',
+    author_email='chris@shelfworthy.com',
+    url='http://github.com/chrisdrackett/django-typogrify',
+    packages = [
+        "typogrify",
+        "typogrify.templatetags",
+    ],
+    install_requires = [
+        'textile',
+        'smartypants>=1.6'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        # 'Intended Audience :: Designers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Utilities',
+        'Topic :: Text Processing'
+    ],
+)
