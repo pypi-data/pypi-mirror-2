8d8d07c7b7f172d4c3c91ea0982bce5862a94938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:48 +0000

    Add package bpmappers/0.4/bpmappers-0.4.tar.gz

---
+++ packages/bpmappers/bpmappers-0.4.tar.gz/bpmappers-0.4/bpmappers/__init__.py
@@ -0,0 +1,10 @@
+from bpmappers.fields import *
+from bpmappers.mappers import *
+
+__all__ = [
+  'Field', 'NonKeyField', 'StubField', 'RawField', 'ChoiceField',
+  'DelegateField', 'ListDelegateField',
+  'NonKeyDelegateField', 'NonKeyListDelegateField', 'Mapper'
+]
+
+VERSION = (0, 4, 0, None)
