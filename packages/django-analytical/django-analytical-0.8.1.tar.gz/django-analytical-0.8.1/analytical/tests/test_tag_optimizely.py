ebdfb9eb7ca9062b1f9e5862793a795fea3d451c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:29 +0000

    Add package django-analytical/0.8.1/django-analytical-0.8.1.tar.gz

---
+++ packages/django-analytical/django-analytical-0.8.1.tar.gz/django-analytical-0.8.1/analytical/tests/test_tag_optimizely.py
@@ -0,0 +1,48 @@
+"""
+Tests for the Optimizely template tags and filters.
+"""
+
+from django.http import HttpRequest
+from django.template import Context
+
+from analytical.templatetags.optimizely import OptimizelyNode
+from analytical.tests.utils import TagTestCase
+from analytical.utils import AnalyticalException
+
+
+class OptimizelyTagTestCase(TagTestCase):
+    """
+    Tests for the ``optimizely`` template tag.
+    """
+
+    def setUp(self):
+        super(OptimizelyTagTestCase, self).setUp()
+        self.settings_manager.set(OPTIMIZELY_ACCOUNT_NUMBER='1234567')
+
+    def test_tag(self):
+        self.assertEqual(
+                '<script src="//cdn.optimizely.com/js/1234567.js"></script>',
+                self.render_tag('optimizely', 'optimizely'))
+
+    def test_node(self):
+        self.assertEqual(
+                '<script src="//cdn.optimizely.com/js/1234567.js"></script>',
+                OptimizelyNode().render(Context()))
+
+    def test_no_account_number(self):
+        self.settings_manager.delete('OPTIMIZELY_ACCOUNT_NUMBER')
+        self.assertRaises(AnalyticalException, OptimizelyNode)
+
+    def test_wrong_account_number(self):
+        self.settings_manager.set(OPTIMIZELY_ACCOUNT_NUMBER='123abc')
+        self.assertRaises(AnalyticalException, OptimizelyNode)
+
+    def test_render_internal_ip(self):
+        self.settings_manager.set(ANALYTICAL_INTERNAL_IPS=['1.1.1.1'])
+        req = HttpRequest()
+        req.META['REMOTE_ADDR'] = '1.1.1.1'
+        context = Context({'request': req})
+        r = OptimizelyNode().render(context)
+        self.assertTrue(r.startswith(
+                '<!-- Optimizely disabled on internal IP address'), r)
+        self.assertTrue(r.endswith('-->'), r)
