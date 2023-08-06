05f882ac9365dda51c2c8163433fd94f04129b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:06 +0000

    Add package django-easy-maps/0.7/django-easy-maps-0.7.tar.gz

---
+++ packages/django-easy-maps/django-easy-maps-0.7.tar.gz/django-easy-maps-0.7/easy_maps/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils.encoding import smart_str
+from geopy import geocoders
+
+
+
+class Address(models.Model):
+    address = models.CharField(max_length=255, db_index=True)
+    computed_address = models.CharField(max_length=255, null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    geocode_error = models.BooleanField(default=False)
+
+    def fill_geocode_data(self):
+        if not self.address:
+            self.geocode_error = True
+            return
+        try:
+            g = geocoders.Google(resource='maps')
+            address = smart_str(self.address)
+            self.computed_address, (self.latitude, self.longitude,) = g.geocode(address)
+            self.geocode_error = False
+        except (UnboundLocalError, ValueError,):
+            self.geocode_error = True
+
+    def save(self, *args, **kwargs):
+        # fill geocode data if it is unknown
+        if (self.longitude is None) or (self.latitude is None):
+            self.fill_geocode_data()
+        super(Address, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.address
