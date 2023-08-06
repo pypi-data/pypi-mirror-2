901cc12364429f457ce6a1566eb388a0457af858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:27 +0000

    Add package pycap/0.2/PyCap-0.2.tar.gz

---
+++ packages/pycap/PyCap-0.2.tar.gz/PyCap-0.2/redcap/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2011, Scott Burns
+All rights reserved.
+"""
+__version__ = '0.2'
+
+from project import Project
+from query import Query, QueryGroup
+from request import RCRequest, RCAPIError
