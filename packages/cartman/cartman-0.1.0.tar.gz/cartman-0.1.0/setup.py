00d8518f6ca14ddca7e800c5c0e70058eaf940f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:03 +0000

    Add package cartman/0.1.0/cartman-0.1.0.tar.gz

---
+++ packages/cartman/cartman-0.1.0.tar.gz/cartman-0.1.0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os.path
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+from cartman import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+setup(
+    name="cartman",
+    version=__version__,
+    description="trac command-line tools",
+    long_description=README + "\n\n" + CHANGES,
+    author="Bertrand Janin",
+    author_email="tamentis@neopulsar.org",
+    url="http://github.com/tamentis/tracman/",
+    scripts=["cm"],
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Home Automation",
+        "Framework :: Trac",
+        "Topic :: Software Development :: Bug Tracking",
+        "Topic :: System :: Monitoring",
+    ],
+    install_requires=[
+        "requests>=0.6.0",
+    ],
+    setup_requires=[
+        "nose>=1.0",
+        "coverage>=3.5",
+    ],
+)
