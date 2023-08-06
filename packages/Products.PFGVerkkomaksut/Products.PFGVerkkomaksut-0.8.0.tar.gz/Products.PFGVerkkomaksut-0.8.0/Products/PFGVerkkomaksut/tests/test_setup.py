f2a44fcb9504c03f0be5edb16448c85f2fb574e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:59 +0000

    Add package Products.PFGVerkkomaksut/0.8.0/Products.PFGVerkkomaksut-0.8.0.tar.gz

---
+++ packages/Products.PFGVerkkomaksut/Products.PFGVerkkomaksut-0.8.0.tar.gz/Products.PFGVerkkomaksut-0.8.0/Products/PFGVerkkomaksut/tests/test_setup.py
@@ -0,0 +1,51 @@
+import unittest
+from Products.CMFCore.utils import getToolByName
+from Products.PFGVerkkomaksut.tests.base import PFGVerkkomaksutTestCase
+
+class TestSetup(PFGVerkkomaksutTestCase):
+
+    def afterSetUp(self):
+        self.catalog = getToolByName(self.portal, 'portal_catalog')
+        self.types = getToolByName(self.portal, 'portal_types')
+        self.wftool = getToolByName(self.portal, 'portal_workflow')
+        self.content_types = [
+            'PFGVerkkomaksut',
+        ]
+        self.installer = getToolByName(self.portal, 'portal_quickinstaller')
+        self.skins      = getToolByName(self.portal, 'portal_skins')
+        self.properties = getToolByName(self.portal, 'portal_properties')
+        self.site_properties = getattr(self.properties, 'site_properties')
+        self.navtree_properties = getattr(self.properties, 'navtree_properties')
+
+    def test_is_pfg_installed(self):
+        self.failUnless(self.installer.isProductInstalled('PloneFormGen'))
+
+    def test_is_pfg_verkkomaksut_installed(self):
+        self.failUnless(self.installer.isProductInstalled('PFGVerkkomaksut'))
+
+    def testSkinLayersInstalled(self):
+        self.failUnless('PFGVerkkomaksut' in self.skins.objectIds())
+
+    ## Content Types
+    def test_contents_installed(self):
+        for type in self.content_types:
+            self.failUnless(type in self.types.objectIds())
+
+    def test_not_searchable(self):
+        self.failUnless('PFGVerkkomaksut' in list(self.site_properties.getProperty('types_not_searched')))
+
+    def test_not_in_navtree(self):
+        self.failUnless('PFGVerkkomaksut' in list(self.navtree_properties.getProperty('metaTypesNotToList')))
+
+#    # Workflow
+#    def test_workflow_mapped(self):
+#        for portal_type, chain in self.wftool.listChainOverrides():
+#            if portal_type in ('PFGVerkkomaksut'):
+#                self.assertEquals((), chain)
+#            if portal_type in ('File', 'Image'):
+#                self.assertEquals((), chain)
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestSetup))
+    return suite
---
+++ packages/Products.PFGVerkkomaksut/Products.PFGVerkkomaksut-0.8.0.tar.gz/Products.PFGVerkkomaksut-0.8.0/README.txt
@@ -0,0 +1,12 @@
+Introduction
+============
+
+Products.PFGVerkkomaksut provides verkkomaksut payment adapter to Products.PloneFormGen package.
+
+How to Use
+----------
+* Install the package from portal_quickinstaller.
+* Content type called "Verkkomaksut Payment Adapter" will be shown to be added within form folder of PloneFormGen.
+    * Remember, you need to add fixed point field or selectin field with required box checked, so that payment adapter knows which field to use as price charge field.
+
+
