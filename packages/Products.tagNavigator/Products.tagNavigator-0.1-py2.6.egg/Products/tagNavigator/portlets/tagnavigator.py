2b2ebcaf6d9a80f90317d0ecd61a16e836ccdf35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:58 +0000

    Add package Products.tagNavigator/0.1/Products.tagNavigator-0.1-py2.6.egg

---
+++ packages/Products.tagNavigator/Products.tagNavigator-0.1-py2.6.egg/Products/tagNavigator/portlets/tagnavigator.py
@@ -0,0 +1,110 @@
+from zope.interface import implements
+
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+
+from zope import schema
+from zope.formlib import form
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+from Products.tagNavigator.categoryutils import CategoryUtils
+
+from Products.tagNavigator import tagNavigatorMessageFactory as _
+
+class ITagNavigatorPortlet(IPortletDataProvider):
+    """A portlet
+
+    It inherits from IPortletDataProvider because for this portlet, the
+    data that is being rendered and the portlet assignment itself are the
+    same.
+    """
+    
+    description = schema.TextLine(
+        title=_(u"Description"),
+        description=_(u"Text to show on top of the tag selector."),
+        default=u'',
+        required=False
+    )
+    
+class Assignment(base.Assignment):
+    """Portlet assignment.
+
+    This is what is actually managed through the portlets UI and associated
+    with columns.
+    """
+
+    implements(ITagNavigatorPortlet)
+
+    def __init__(self, description=u''):
+	self.description = description
+
+    @property
+    def title(self):
+        """This property is used to give the title of the portlet in the
+        "manage portlets" screen.
+        """
+        return "Tag Navigator Portlet"
+
+
+class Renderer(base.Renderer, CategoryUtils):
+    """Portlet renderer.
+
+    This is registered in configure.zcml. The referenced page template is
+    rendered, and the implicit variable 'view' will refer to an instance
+    of this class. Other methods can be added and referenced in the template.
+    """
+
+    render = ViewPageTemplateFile('tagnavigator.pt')
+    
+    @property
+    def available(self):
+        if len(self.getSiteCategoriesAsList()) == 0:
+            return False
+        else:
+            return True
+    
+    @property
+    def Description(self):
+	return self.data.description
+    
+    def generateQueryString(self, categories, adding=None, subtracting=None):
+        '''Generates a URL querystring from a list of categories'''
+        query = self.context.absolute_url + "?"
+        finalCategories = list()
+        
+        if categories is not None:
+            finalCategories = categories[:]
+        
+        if adding is not None:
+            finalCategories.append(adding)
+            
+        if subtracting is not None:
+            if subtracting in finalCategories:
+                finalCategories.remove(subtracting)
+        
+        for cat in finalCategories:
+            query = query + "Subject%3Alist=" + cat + "&"
+        
+        return query[:-1]
+
+
+class AddForm(base.AddForm):
+    """Portlet add form.
+
+    This is registered in configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display. The create() method actually
+    constructs the assignment that is being added.
+    """
+    form_fields = form.Fields(ITagNavigatorPortlet)
+
+    def create(self, data):
+        return Assignment(**data)
+
+
+class EditForm(base.EditForm):
+    """Portlet edit form.
+
+    This is registered with configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display.
+    """
+    form_fields = form.Fields(ITagNavigatorPortlet)
