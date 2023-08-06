0ddaed5fc93e88d5bd5a43e2aba3179b207db010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:34 +0000

    Add package tw.iphonecheck/0.1/tw.iphonecheck-0.1.tar.gz

---
+++ packages/tw.iphonecheck/tw.iphonecheck-0.1.tar.gz/tw.iphonecheck-0.1/tw/iphonecheck/release.py
@@ -0,0 +1,11 @@
+# Metadata for the widget EGG. Should leave it for the (unimplemented) widget
+# tracker to track it
+
+__DISTRIBUTION__ = 'tw.iphonecheck'
+__DESCRIPTION__ = 'iphone like checkbox'
+__URL__ = ''
+__VERSION__ = '0.1'
+__AUTHOR__ = 'Nicolas Laurance'
+__EMAIL__ = 'nicolas[dot]laurance[at]gmail[dot]com'
+__COPYRIGHT__ = "Copyright 2011 Nicolas Laurance"
+__LICENSE__ = 'MIT'
