599881b8d4901e4dfd89331ff756fef74792e4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:48 +0000

    Add package cciaa.portlet.calendar/1.2.0/cciaa.portlet.calendar-1.2.0.tar.gz

---
+++ packages/cciaa.portlet.calendar/cciaa.portlet.calendar-1.2.0.tar.gz/cciaa.portlet.calendar-1.2.0/cciaa/portlet/calendar/browser/searchevents.py
@@ -0,0 +1,19 @@
+from Products.CMFCore.utils import getToolByName
+
+from Products.Five.browser import BrowserView
+
+
+class SearchEventsView(BrowserView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def date(self, request):
+        date=request['end']['query'][0]
+        date=date.strftime('%d-%m-%Y')
+        return date
+
+    def singolEvent(self, url):
+        self.request.RESPONSE.redirect(url)
+        return
