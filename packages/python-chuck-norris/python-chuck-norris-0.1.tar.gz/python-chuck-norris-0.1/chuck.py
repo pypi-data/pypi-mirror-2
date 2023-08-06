bb49170ab4f31f3bd316c613681822ae94824331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package python-chuck-norris/0.1/python-chuck-norris-0.1.tar.gz

---
+++ packages/python-chuck-norris/python-chuck-norris-0.1.tar.gz/python-chuck-norris-0.1/chuck.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1'
+
+def assert_true(value, msg=None):
+    return True
+
+def assert_false(value, msg=None):
+    return True
+
+def assert_raises(excClass, callableObj, *args, **kwargs):
+    return True
+
+def assert_equals(first, second, msg=None):
+    return True
+
+def assert_not_equals(first, second, msg=None):
+    return True
+
