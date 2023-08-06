ce8e1a90cd36e22e91c8c3575a232736d534dad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:03 +0000

    Add package django-bfm/0.1/django-bfm-0.1.tar.gz

---
+++ packages/django-bfm/django-bfm-0.1.tar.gz/django-bfm-0.1/django_bfm/settings.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+MEDIA_DIRECTORY = getattr(settings,
+                          "BFM_MEDIA_DIRECTORY",
+                          settings.MEDIA_ROOT)
+FILES_IN_PAGE = int(getattr(settings,
+                        "BFM_FILES_IN_PAGE",
+                        20))
