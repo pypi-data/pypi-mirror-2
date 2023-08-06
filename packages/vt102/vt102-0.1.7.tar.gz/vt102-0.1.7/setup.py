0190c65c539165fedf54d61de5f365441393e2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:09 +0000

    Add package vt102/0.1.7/vt102-0.1.7.tar.gz

---
+++ packages/vt102/vt102-0.1.7.tar.gz/vt102-0.1.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import sys
+
+setup(name="vt102",
+      version="0.1.7",
+      author="Sam Gibson",
+      author_email="sam@ifdown.net",
+      url="https://github.com/samfoo/vt102",
+      description="Simple vt102 emulator, useful for screen scraping.",
+      packages=["vt102"],
+      provides=["vt102"],
+      keywords="vt102 terminal emulator screen scraper",
+      license="Lesser General Public License v3.0",
+)
