865b5700d341e5dd8106dcd3232c171b41dca382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:10 +0000

    Add package Products.Maps/2.1.0/Products.Maps-2.1.0.zip

---
+++ packages/Products.Maps/Products.Maps-2.1.0.zip/Products.Maps-2.1.0/Products/Maps/__init__.py
@@ -0,0 +1,42 @@
+from zope.i18nmessageid import MessageFactory
+
+from Products.CMFCore.utils import ContentInit
+from Products.CMFCore.DirectoryView import registerDirectory
+
+from Products.Archetypes.public import listTypes
+from Products.Archetypes.public import process_types
+
+from Products.GenericSetup import EXTENSION
+from Products.GenericSetup import profile_registry
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+from Products.Maps import config
+
+MapsMessageFactory = MessageFactory('maps')
+
+registerDirectory(config.SKINS_DIR, config.GLOBALS)
+
+def initialize(context):
+
+    from Products.Maps import content
+    content # pyflakes
+
+    content_types, constructors, ftis = process_types(
+        listTypes(config.PROJECTNAME), config.PROJECTNAME)
+
+    ContentInit(
+        config.PROJECTNAME + ' Content',
+        content_types = content_types,
+        permission = 'Maps: Add GeoLocation',
+        extra_constructors = constructors,
+        fti = ftis,
+    ).initialize(context)
+
+    # Register the extension profile
+    profile_registry.registerProfile('default',
+                                     'Maps',
+                                     'maps',
+                                     'profiles/default',
+                                     'Maps',
+                                     EXTENSION,
+                                     IPloneSiteRoot)
