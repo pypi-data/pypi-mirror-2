488e0cd5c2847d82538a09986e8c2527c841a0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:12 +0000

    Add package eg.theme/1.02/eg.theme-1.08-py2.4.egg

---
+++ packages/eg.theme/eg.theme-1.08-py2.4.egg/eg/theme/browser/egosearch.py
@@ -0,0 +1,158 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+
+from zope import interface, schema
+from z3c.form import form, field, button, browser, widget
+from plone.z3cform.layout import wrap_form
+import datetime
+from z3c.form import interfaces
+from z3c.form.browser import checkbox, select
+from z3c.form.testing import TestRequest
+
+from Products.CMFCore.utils import getToolByName
+
+
+class IEgoSearch(interface.Interface):
+
+    SearchableText = schema.TextLine(title=u"Full Text", description=u"Fulltext \
+        search including title, description and text body.",
+        required = False)
+    author = schema.TextLine(title=u"Author", description=u"Please specify the\
+        user id.",
+        required = False)
+
+#    date_von = schema.Int(title=u"Startdatum", required = False,
+#                            description=u"Please enter a start intervall.Value must be 4 digit - e.g. '1600' or '1850'.")
+#    date_bis = schema.Int(title=u"Enddatum", required = False,
+#                            description=u"Please enter an end intervall. Value must be 4 digit - e.g. '1600' or '1850'.")
+    topic = schema.List (title = u"Topic", description = u"Please limit your \
+        search by selecting one or multiple items from the list.",
+        value_type=schema.Choice(vocabulary="eg.theme.vocabularies.topics"),
+#        default = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'],
+        required = False)
+    area = schema.List(title = u"Area ", description = u"Please limit your \
+        search by selecting one or multiple items from the list.",
+        value_type=schema.Choice(vocabulary="eg.theme.vocabularies.area"),
+#        default = ['0', '1', '2', '3', '4', '5', '6'],
+        required = False)
+    thread = schema.Choice(title=u"Thread", description=u"Please specify a thread.",
+        vocabulary = "eg.theme.vocabularies.thread",
+        default='10',
+        required = False)
+    period = schema.List(title=u"Period(s)", description=u"Please specify one or more period(s).",
+        value_type=schema.Choice(vocabulary = "eg.theme.vocabularies.timeframe"),
+        required = False)
+    period_all = schema.Bool(default=False, description=u"Select if you want to match all.",
+                required = False)
+#    video = schema.Bool (default=True, title = u'Video')
+#    audio = schema.Bool (default=True, title = u'Audio')
+#    images = schema.Bool (default=True, title = u'Images')
+    item_type = schema.List(title=u"Type", description=u"Please select one or more\
+        type(s) to refine your search.",
+        value_type=schema.Choice(vocabulary="eg.theme.vocabularies.media"),
+#        default=['n', 'a', 'i', 'v', 'o'],
+        required = False)
+
+
+class EgoSearchForm(form.Form):
+
+    fields = field.Fields(IEgoSearch)
+    ignoreContext = True
+    label = u"Ego Advanced Search"
+    
+#    def GetMemberFullName(self):
+#        member_fullname = []
+#        membership = getToolByName(self.context, 'portal_membership')
+#        for member in membership.listMembers():
+#            member_fullname.append(member.getProperty('fullname'))
+#        return member_fullname
+
+    def MemberList(context):
+        mlist = []
+        membership = getToolByName(context, 'portal_membership')
+        for m in membership.listMemberIds():
+            mlist.append(membership.getMemberInfo(m)['fullname'])
+        return mlist
+
+
+    def MyFieldWidget(field, req):
+        return widget.FieldWidget(field, select.SelectWidget(req))
+
+#    fields['period'].widgetFactory = MyFieldWidget
+#    fields['period2'].widgetFactory = MyFieldWidget
+
+    def MyMultiSelectWidget(field, req):
+        return widget.FieldWidget(field, checkbox.CheckBoxWidget(req))
+    
+    fields['area'].widgetFactory = MyMultiSelectWidget
+    fields['topic'].widgetFactory = MyMultiSelectWidget
+#    fields['thread'].widgetFactory = MyMultiSelectWidget
+    fields['item_type'].widgetFactory = MyMultiSelectWidget
+
+#    def MyMultiWidget(field, req):
+#        return widget.MultiWidget(field, multi.MultiWidget(req))
+#
+#    fields['date'].widgetFactory = MyMultiWidget
+
+    @property
+    def portal(self):
+        return getToolByName(self.context, 'portal_url').getPortalObject()
+
+    @button.buttonAndHandler(u"Search")
+    def handleSearch(self, action):
+        data, errors = self.extractData()
+#        am = MemberList()
+        if errors:
+            return
+        base_url = "%s/search" % self.portal.absolute_url()
+        qstring = "?portal_type=Document"
+        if data['SearchableText'] is not None:
+            qstring += "&SearchableText=%s" % data['SearchableText']
+        else:
+            qstring += "&SearchableText=%s" % ''
+        if data['author'] is not None:
+#            if data['author'] in am:
+#                qstring+="&Test=%s" % data['author']
+            qstring+="&Creator=%s" % data['author']
+        else:
+            qstring += "&Creator=%s" % ''
+#        if data['date_von'] is not None:
+#            qstring += "&timefrom:int=%s&timefrom_usage=range:min" % data['date_von']
+#        else:
+#            qstring = qstring
+#        if data['date_bis'] is not None:
+#            qstring += "&timeuntil:int=%s&timeuntil_usage=range:max" % data['date_bis']
+#        else:
+#            qstring = qstring
+        qstring += "&topic=%s" % '+OR+'.join(data['topic'])
+        qstring += "&area=%s" % '+OR+'.join(data['area'])
+        if data['thread'] == '10':
+            qstring += "&Subject=%s" % ''
+        else:
+            qstring += "&Subject=%s" % data['thread']
+        qstring += "&mediacontent=%s" % '+OR+'.join(data ['item_type'])
+        if data['period_all'] is not True:
+            qstring += "&timeframe=%s" % '+OR+'.join(data ['period'])
+        else:
+            qstring += "&timeframe=%s" % '+AND+'.join(data ['period'])
+        qstring += "&sort_on=effective&sort_order=descending"
+        qstring += "&sort_limit=5"
+        self.request.response.redirect(base_url + qstring)
+
+EgoSearchView = wrap_form(EgoSearchForm)
+
+#class EgoSearchForm(form.Form):
+#
+#    fields = field.Fields(IEgoSearch)
+#    ignoreContext = True
+#    label = u"Ego Advanced Search"
+#
+#
+#    @button.buttonAndHandler(u"Search")
+#    def handleSearch(self, action):
+#        data, errors = self.extractData()
+#        self.status = data
+#
+#EgoSearchView = wrap_form(EgoSearchForm)
+
---
+++ packages/eg.theme/eg.theme-1.08-py2.4.egg/eg/theme/extender.py
@@ -0,0 +1,404 @@
+from Products.Archetypes.public import *
+from Products.Archetypes import atapi
+from Products.ATContentTypes.interface import IATFolder
+from Products.ATContentTypes.interface import IATDocument
+from archetypes.schemaextender.field import ExtensionField
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from zope.component import adapts
+from zope.interface import implements
+
+class ShortTitleField(ExtensionField, StringField): pass
+class ArticleTypeField(ExtensionField, StringField): pass
+class ArticleLanguage(ExtensionField, StringField): pass
+class OriginalArticleLanguage(ExtensionField, StringField): pass
+class AvailableLanguages(ExtensionField, LinesField): pass
+class Editor(ExtensionField, StringField): pass
+class Publisher(ExtensionField, StringField): pass
+class Translator(ExtensionField, StringField): pass
+class TimeFrom(ExtensionField, IntegerField): pass
+class TimeUntil(ExtensionField, IntegerField): pass
+class TimeFrame(ExtensionField, LinesField): pass
+class Area(ExtensionField, LinesField): pass
+class Topic(ExtensionField, LinesField): pass
+class Licence(ExtensionField, StringField): pass
+class UniqueCode(ExtensionField, StringField): pass
+class URN(ExtensionField, StringField): pass
+class DDC(ExtensionField, LinesField): pass
+class PublicationDate(ExtensionField, DateTimeField): pass
+class PreviewImage(ExtensionField, ImageField): pass
+class MediaContentTypeField(ExtensionField, StringField): pass
+class IsTranslation(ExtensionField, BooleanField): pass
+
+
+class DocumentExtender(object):
+    adapts(IATDocument)
+    implements(ISchemaExtender)
+    # ISO 639 2-letter codes
+    # http://www.w3.org/WAI/ER/IG/ert/iso639.htm
+    languages=[("DE","German"),
+				("EN","English"),
+				("FR","French"),
+				("0","--------"),
+    			("AB","Abkhazian"),
+				("AA","Afar"),
+				("AF","Afrikaans"),
+				("SQ","Albanian"),
+				("AM","Amharic"),
+				("AR","Arabic"),
+				("HY","Armenian"),
+				("AS","Assamese"),
+				("AY","Aymara"),
+				("AZ","Azerbaijani"),
+				("BA","Bashkir"),
+				("EU","Basque"),
+				("BN","Bengali"),
+				("DZ","Bhutani"),
+				("BH","Bihari"),
+				("BI","Bislama"),
+				("BR","Breton"),
+				("BG","Bulgarian"),
+				("MY","Burmese"),
+				("BE","Byelorussian"),
+				("KM","Cambodian"),
+				("CA","Catalan"),
+				("ZH","Chinese"),
+				("CO","Corsican"),
+				("HR","Croatian"),
+				("CS","Czech"),
+				("DA","Danish"),
+				("NL","Dutch"),
+				("EO","Esperanto"),
+				("ET","Estonian"),
+				("FO","Faeroese"),
+				("FJ","Fiji"),
+				("FI","Finnish"),
+				("FR","French"),
+				("FY","Frisian"),
+				("GD","Gaelic"),
+				("GL","Galician"),
+				("KA","Georgian"),
+				("EL","Greek"),
+				("KL","Greenlandic"),
+				("GN","Guarani"),
+				("GU","Gujarati"),
+				("HA","Hausa"),
+				("IW","Hebrew"),
+				("HI","Hindi"),
+				("HU","Hungarian"),
+				("IS","Icelandic"),
+				("IN","Indonesian"),
+				("IA","Interlingua"),
+				("IE","Interlingue"),
+				("IK","Inupiak"),
+				("GA","Irish"),
+				("IT","Italian"),
+				("JA","Japanese"),
+				("JW","Javanese"),
+				("KN","Kannada"),
+				("KS","Kashmiri"),
+				("KK","Kazakh"),
+				("RW","Kinyarwanda"),
+				("KY","Kirghiz"),
+				("RN","Kirundi"),
+				("KO","Korean"),
+				("KU","Kurdish"),
+				("LO","Laothian"),
+				("LA","Latin"),
+				("LV","Latvian"),
+				("LN","Lingala"),
+				("LT","Lithuanian"),
+				("MK","Macedonian"),
+				("MG","Malagasy"),
+				("MS","Malay"),
+				("ML","Malayalam"),
+				("MT","Maltese"),
+				("MI","Maori"),
+				("MR","Marathi"),
+				("MO","Moldavian"),
+				("MN","Mongolian"),
+				("NA","Nauru"),
+				("NE","Nepali"),
+				("NO","Norwegian"),
+				("OC","Occitan"),
+				("OR","Oriya"),
+				("OM","Oromo"),
+				("PS","Pashto"),
+				("FA","Persian"),
+				("PL","Polish"),
+				("PT","Portuguese"),
+				("PA","Punjabi"),
+				("QU","Quechua"),
+				("RM","Rhaeto-Romance"),
+				("RO","Romanian"),
+				("RU","Russian"),
+				("SM","Samoan"),
+				("SG","Sangro"),
+				("SA","Sanskrit"),
+				("SR","Serbian"),
+				("SH","Serbo-Croatian"),
+				("ST","Sesotho"),
+				("TN","Setswana"),
+				("SN","Shona"),
+				("SD","Sindhi"),
+				("SI","Singhalese"),
+				("SS","Siswati"),
+				("SK","Slovak"),
+				("SL","Slovenian"),
+				("SO","Somali"),
+				("ES","Spanish"),
+				("SU","Sudanese"),
+				("SW","Swahili"),
+				("SV","Swedish"),
+				("TL","Tagalog"),
+				("TG","Tajik"),
+				("TA","Tamil"),
+				("TT","Tatar"),
+				("TE","Tegulu"),
+				("TH","Thai"),
+				("BO","Tibetan"),
+				("TI","Tigrinya"),
+				("TO","Tonga"),
+				("TS","Tsonga"),
+				("TR","Turkish"),
+				("TK","Turkmen"),
+				("TW","Twi"),
+				("UK","Ukrainian"),
+				("UR","Urdu"),
+				("UZ","Uzbek"),
+				("VI","Vietnamese"),
+				("VO","Volapuk"),
+				("CY","Welsh"),
+				("WO","Wolof"),
+				("XH","Xhosa"),
+				("JI","Yiddish"),
+				("YO","Yoruba"),
+				("ZU","Zulu"),]
+
+    fields = [
+        ShortTitleField(
+            "shorttitle",
+                widget = StringWidget(
+                label=u"Short Title",
+                il8n_domain='plone',
+            ),
+            required=False,
+			searchable=True
+        ),
+        ArticleTypeField(
+            "articletype",
+            widget = SelectionWidget(
+                label=u"Article Type",
+                il8n_domain='plone',
+            ),
+            vocabulary=[("g","General"), ("mb", "Media Description"), ("t", "Thread"), ("be", "Basic Element"), ("ub","Servey"), ("ve", "In Depth Element")],
+			required=False,
+			searchable=True
+        ),
+        MediaContentTypeField(
+            "mediacontent",
+            widget = SelectionWidget(
+                label=u"Media Content Type",
+                description=u"If this is a Media Description please select the primary type of Content",
+                il8n_domain='plone',
+            ),
+            vocabulary=[("n","No Media Description"),("a","Audio"), ("i", "Image"), ("v", "Video"), ("o", "Other")],
+			required=False,
+			searchable=True
+        ),
+        PreviewImage(
+            "previewimage",
+            widget = ImageWidget(
+                label=u"Preview Image",
+                description=u"This image is used as preview for search result pages",
+            ),
+			required=False,
+			searchable=True
+        ),
+        ArticleLanguage(
+			'articlelanguage',
+			widget=atapi.SelectionWidget(
+				label=u'Language',
+				description=u"Which language is this article written in?",
+			),
+			vocabulary=languages,                                           
+			required=False,
+			searchable=True
+		),
+		IsTranslation(
+			'istranslation',
+			widget=atapi.BooleanWidget(
+				label=u'This article is a translation from another language',
+			),                                      
+			required=False,
+			searchable=True
+		),
+		OriginalArticleLanguage(
+			'originallanguage',
+			widget=atapi.SelectionWidget(
+				label=u'Language of Original',
+				description=u"Which language was this document translated from? If this is the original please select the same as above.",
+			),
+			vocabulary=languages, 
+			required=False,
+			searchable=True
+		),
+		AvailableLanguages(
+			'availablelanguage',
+			widget=atapi.PicklistWidget(
+				label=u'Available Language',
+				description=u"A list of all languages in which translations of this article are available",
+				il8n_domain='pagetest',
+			),
+			vocabulary=languages, 
+			required=False,
+			searchable=True
+		),
+		Editor(
+			'copyeditor',
+			# Redaktion
+			widget=atapi.StringWidget(
+				label=u'Copy Editor',
+				il8n_domain='plone',
+			),
+			required=False,
+			searchable=True
+		),
+		Publisher(
+			'publisher',
+			# herausgeber
+			widget=atapi.StringWidget(
+				label=u'Editor',
+				il8n_domain='plone',
+			),
+			required=False,
+			searchable=True
+		),
+		Translator(
+			name='translator',
+			widget=atapi.StringWidget(
+				label=u'Translator',
+				label_msgid='pagetest_label_translator',
+				il8n_domain='pagetest',
+			),
+			required=False,
+			searchable=True
+		),
+		TimeFrom(
+			name='timefrom',
+			widget=atapi.IntegerWidget(
+				label=u'Time interval from (year)',
+				description=u"This determines the time interval that is used for advanced search queries",
+				size=4,
+				maxlength=4,
+			),
+			required=False,
+			searchable=True
+		),
+		TimeUntil(
+			name='timeuntil',
+			widget=atapi.IntegerWidget(
+				label=u'until (year)',
+				size=4,
+				maxlength=4,
+			),
+			required=False,
+			searchable=True
+		),
+		TimeFrame(
+			name='timeframe',
+			widget=atapi.PicklistWidget(
+				label=u'Periods',
+			),
+#                        vocabulary=("1", "2"),
+                        vocabulary_factory = "eg.theme.vocabularies.timeframe",
+			required=False,
+			searchable=True
+		),
+		Area(
+			name='area',
+			widget=atapi.PicklistWidget(
+				label=u'Area',
+			),
+			vocabulary_factory = "eg.theme.vocabularies.area",
+			required=False,
+			searchable=True
+		),
+		Topic(
+			name='topic',
+			widget=atapi.PicklistWidget(
+				label=u'Topic',
+			),
+                        vocabulary_factory = "eg.theme.vocabularies.topics",
+#			vocabulary=[("0","Education, Sciences"),
+#		                ("1", "Arts"),
+#		                ("2", "Social Matters, Society"),
+#		                ("3", "Politics"),
+#		                ("4", "Law, Constitution"),
+#		                ("5", "Religion"),
+#		                ("11", "Military"),
+#		                ("6", "Migration, Travel"),
+#		                ("7", "Media, Communication"),
+#		                ("8", "Agents, Intermediaries"),
+#		                ("9", "Theory, Methodology"),
+#						("10", "Economy, Technology")],
+			required=False,
+			searchable=True
+		),
+		Licence(
+			name='licence',
+			widget=atapi.SelectionWidget(
+				label=u'Licence',
+			),
+			vocabulary=[("cr", "All rights reserved"), ("by-nc-nd", "CC by-nc-nd - Attribution, Noncommercial, No Derivative Works")],
+			required=False,
+			searchable=True
+		),
+		UniqueCode(
+			name='unique',
+			widget=atapi.StringWidget(
+				label=u'Short URL without language code',
+				description=u'e.g. If the URL is http://www.ieg-ego.eu/boeschf-2010-de please enter boeschf-2010 here',
+				il8n_domain='plone',
+			),
+			required=False,
+			searchable=True
+		),
+		URN(
+			name='urn',
+			widget=atapi.StringWidget(
+				label=u'URN',
+				description=u'URN without resolver. e.g. urn:nbn:de:0159-20100921115',
+				il8n_domain='plone',
+			),
+			required=False,
+			searchable=True
+		),
+		DDC(
+			name='DDC',
+			widget=atapi.LinesWidget(
+				label=u'DDC',
+				description=u'Enter the relevant DDC codes, one per line.',
+				il8n_domain='plone',
+				cols=10,
+			),
+			required=False,
+			searchable=True
+		),
+		PublicationDate(
+			name='publicationsdate',
+			widget=atapi.CalendarWidget(
+				label=u'Publication date',
+				description=u'Select the date that should appear as publication date in the page header',
+				il8n_domain='plone',
+				show_hm=False,
+			),
+			required=False,
+			searchable=True
+		),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
