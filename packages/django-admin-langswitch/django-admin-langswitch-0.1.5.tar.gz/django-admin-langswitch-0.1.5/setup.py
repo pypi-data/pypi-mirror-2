11d7913f10c3410ef568e62c29e8b6d807298ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:28 +0000

    Add package django-admin-langswitch/0.1.5/django-admin-langswitch-0.1.5.tar.gz

---
+++ packages/django-admin-langswitch/django-admin-langswitch-0.1.5.tar.gz/django-admin-langswitch-0.1.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='django-admin-langswitch',
+    version='0.1.5',
+    author='Piotr Kilczuk',
+    author_email='p.kilczuk@neumea.pl',
+    url='http://github.com/centralniak',
+    description = 'Adds easy language switch in admin',
+    packages=find_packages(),
+    provides=['admin_langswitch'],
+    include_package_data=True,
+    #package_data={'cmsplugin': ['templates/*',]},
+    classifiers=[
+        'Framework :: Django',
+        #'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
