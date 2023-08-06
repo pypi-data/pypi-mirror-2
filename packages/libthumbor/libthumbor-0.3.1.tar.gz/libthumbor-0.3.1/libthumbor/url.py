02689425ff3e6e61dec5553bfdd171ab5972a0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:08 +0000

    Add package libthumbor/0.3.1/libthumbor-0.3.1.tar.gz

---
+++ packages/libthumbor/libthumbor-0.3.1.tar.gz/libthumbor-0.3.1/libthumbor/url.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# libthumbor - python extension to thumbor
+# http://github.com/heynemann/libthumbor
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
+
+'''URL composer to create options-based URLs for thumbor encryption.'''
+
+import hashlib
+
+AVAILABLE_HALIGN = ['left', 'center', 'right']
+AVAILABLE_VALIGN = ['top', 'middle', 'bottom']
+
+def calculate_width_and_height(url_parts, options):
+    '''Appends width and height information to url'''
+    width = options.get('width', 0)
+    has_width = width
+    height = options.get('height', 0)
+    has_height = height
+
+    flip = options.get('flip', False)
+    flop = options.get('flop', False)
+
+    if flip:
+        width = width * -1
+    if flop:
+        height = height * -1
+
+    if not has_width and not has_height:
+        if flip:
+            width = "-0"
+        if flop:
+            height = "-0"
+
+    if width or height:
+        url_parts.append('%sx%s' % (width, height))
+
+def url_for(**options):
+    '''Returns the url for the specified options'''
+
+    if 'image_url' not in options:
+        raise RuntimeError('The image_url argument is mandatory.')
+
+    url_parts = []
+
+    if options.get('meta', False):
+        url_parts.append('meta')
+
+    crop = options.get('crop', None)
+    if crop:
+        crop_left = crop[0][0]
+        crop_top = crop[0][1]
+        crop_right = crop[1][0]
+        crop_bottom = crop[1][1]
+
+        if crop_left > 0 or crop_top > 0 or crop_bottom > 0 or crop_right > 0:
+            url_parts.append("%sx%s:%sx%s" % (
+                crop_left,
+                crop_top,
+                crop_right,
+                crop_bottom
+            ))
+
+    calculate_width_and_height(url_parts, options)
+
+    halign = options.get('halign', 'center')
+    valign = options.get('valign', 'middle')
+
+    if not halign in AVAILABLE_HALIGN:
+        raise ValueError('Only "left", "center" and "right" are' + \
+                         ' valid values for horizontal alignment.')
+    if not valign in AVAILABLE_VALIGN:
+        raise ValueError('Only "top", "middle" and "bottom" are' + \
+                         ' valid values for vertical alignment.')
+
+    if halign != 'center':
+        url_parts.append(halign)
+    if valign != 'middle':
+        url_parts.append(valign)
+
+    if options.get('smart', False):
+        url_parts.append('smart')
+
+    image_hash = hashlib.md5(options['image_url']).hexdigest()
+    url_parts.append(image_hash)
+
+    return "/".join(url_parts)
