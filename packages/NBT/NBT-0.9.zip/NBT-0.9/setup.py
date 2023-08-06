2b78e91e3c2a5e7def0ed559188221acf0c52b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:30 +0000

    Add package NBT/0.9/NBT-0.9.zip

---
+++ packages/NBT/NBT-0.9.zip/NBT-0.9/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name='NBT',
+      version='0.9',
+      description='Named Binary Tag Reader/Writer',
+      author='Thomas Woolford',
+      author_email='woolford.thomas@gmail.com',
+      url='http://github.com/twoolie/NBT',
+      license = open("LICENSE.txt").read(),
+      long_description = open("README.txt").read(),
+      packages=['nbt'],
+     )
