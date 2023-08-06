c030a95535dfa65f51ac8c1862ee70ec81d0be93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:14 +0000

    Add package django-wsgiserver/0.6.9/django-wsgiserver-0.6.9.tar.gz

---
+++ packages/django-wsgiserver/django-wsgiserver-0.6.9.tar.gz/django-wsgiserver-0.6.9/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+ 
+# modified version no cherrypy reliance
+setup(
+    name="django-wsgiserver",
+    version="0.6.9",
+    license="BSD",
+    description="""django-wsgiserver installs a web server for Django using CherryPy's WSGI server.""",
+    author="""Peter Baumgartner Chris Lee-Messer the authors of CherryPy""",
+    author_email="chris@lee-messer.net, pete@lincolnloop.com",
+    maintainer="Chris Lee-Messer",
+    # from README.rst
+    long_description=open('README.rst').read(),
+    url="http://bitbucket.org/cleemesser/django-cherrypy-wsgiserver/downloads",
+    download_url="http://bitbucket.org/cleemesser/django-cherrypy-wsgiserver/downloads",
+    packages=[
+        "django_wsgiserver",
+        "django_wsgiserver.management",
+        "django_wsgiserver.management.commands",
+        "django_wsgiserver.wsgiserver",
+    ],
+    # package_data
+    # data_files=[],# where to put test certs
+    classifiers=['Framework :: Django',
+                 'Development Status :: 5 - Production/Stable',
+                 'Environment :: Console',
+                 'License :: OSI Approved :: BSD License',
+                 'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+                 'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+                 ],
+    )
+
