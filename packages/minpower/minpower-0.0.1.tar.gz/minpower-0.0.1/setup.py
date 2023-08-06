e215efed979720dc7c5da96664ca0bcbed18e142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:50 +0000

    Add package minpower/0.0.1/minpower-0.0.1.tar.gz

---
+++ packages/minpower/minpower-0.0.1.tar.gz/minpower-0.0.1/setup.py
@@ -0,0 +1,42 @@
+# minpower's setup script
+# build using
+# python setup.py sdist bdist_wininst
+
+from distutils.core import setup
+setup(
+    name = "minpower",
+    packages = ["minpower"], #code location
+    version = "0.0.1",
+    description = "power systems optimization made beautiful",
+    author = "Adam Greenhall",
+    author_email = "minpower@adamgreenhall.com",
+    url = "http://minpowertoolkit.com/",
+    download_url = "http://minpowertoolkit.com/minpower-0.0.1.tgz",
+    keywords = ["power systems","optimization",""],
+    classifiers = [    
+        "Development Status :: 3 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Science/Research",      
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",    
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        ],
+    long_description = """\
+power systems tools made beautiful
+-----------------------------------------
+
+* Solves ED, OPF, and UC problems.
+* Problems can be defined in simple spreadsheets. 
+* Visualizations for the answers. 
+* Many solvers supported.
+
+* Full documentation and tutorials: minpowertoolkit.com
+* Actively developed at github.com/adamgreenhall/minpower
+
+"""
+)
