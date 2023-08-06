a5e5dadf56d1cfc73bdc1e4dc7e5efe39ec87563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:20 +0000

    Add package lilydjwg.Path/0.2/lilydjwg.Path-0.2.tar.gz

---
+++ packages/lilydjwg.Path/lilydjwg.Path-0.2.tar.gz/lilydjwg.Path-0.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# @Author: Vayn a.k.a. VT <vayn@vayn.de>
+# @Name: setup.py
+# @Date: 2011年05月05日 星期四 05时58分14秒
+
+from setuptools import setup
+
+setup(
+    name='lilydjwg.Path',
+    version='0.2',
+    author='lilydjwg',
+    author_email='lilydjwg@gmail.com',
+    url='http://github.com/lilydjwg/winterpy',
+    license='GPL',
+    description='An object that handles path more easily',
+    zip_safe=False,
+    py_modules=['path'],
+)
