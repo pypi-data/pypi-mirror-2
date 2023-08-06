28771f4e407e44c664edda6f16f39f2c2f2e3989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:18 +0000

    Add package timed/0.4/timed-0.4.tar.gz

---
+++ packages/timed/timed-0.4.tar.gz/timed-0.4/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+  name='timed',
+  version='0.4',
+  description="command-line time tracking",
+  long_description=open('README').read(),
+  url='http://adeel.github.com/timed',
+  author='Adeel Ahmad Khan',
+  author_email='adeel@adeel.ru',
+  packages=['timed'],
+  scripts=['bin/timed'],
+  install_requires=['termcolor'],
+  license='BSD',
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Operating System :: POSIX',
+    'Topic :: Utilities',
+  ]
+)
