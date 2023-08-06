b6c711f7ef67222bd3d829a9603c9994e9040142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:41 +0000

    Add package Products.Faq/1.1.2/Products.Faq-1.1.2.zip

---
+++ packages/Products.Faq/Products.Faq-1.1.2.zip/Products.Faq-1.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.2'
+
+setup(name='Products.Faq',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Four Digits',
+      author_email='info@fourdigits.nl',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
