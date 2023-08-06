ef86d75b9cffbc4b45635600f4b531e611fbb9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:30 +0000

    Add package django-netcash/0.1.11/django-netcash-0.1.11.tar.gz

---
+++ packages/django-netcash/django-netcash-0.1.11.tar.gz/django-netcash-0.1.11/netcash/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from netcash.models import NetcashGateway, NetcashOrder
+
+class NetcashGatewayAdmin(admin.ModelAdmin):
+    list_display = ['name', 'data_url', 'accept_url', 'reject_url', 'netcash_ip']
+
+class NetcashOrderAdmin(admin.ModelAdmin):
+
+    def short_debug_info(self, obj):
+        if obj.debug_info:
+            if len(obj.debug_info) > 15:
+                return obj.debug_info[:14]+' ...'
+        return obj.debug_info
+    short_debug_info.short_description = 'debug info'
+
+    list_display = ['Reference', 'created_at', 'updated_at', 'Amount',
+                    'Reason', 'RETC', 'request_ip', 'TransactionAccepted', 'trusted', 'short_debug_info']
+    list_filter = ['TransactionAccepted', 'trusted']
+    search_fields = ['Reference']
+    date_hierarchy = 'created_at'
+
+admin.site.register(NetcashGateway, NetcashGatewayAdmin)
+admin.site.register(NetcashOrder, NetcashOrderAdmin)
