9f354e817a51060ed5ffd608215e3f5619e0adcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package django-mptt/0.4.2/django-mptt-0.4.2.zip

---
+++ packages/django-mptt/django-mptt-0.4.2.zip/django-mptt-0.4.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+# Dynamically calculate the version based on mptt.VERSION
+version_tuple = __import__('mptt').VERSION
+version = ".".join([str(v) for v in version_tuple])
+
+setup(
+    name = 'django-mptt',
+    description = '''Utilities for implementing Modified Preorder Tree Traversal
+        with your Django Models and working with trees of Model instances.''',
+    version = version,
+    author = 'Jonathan Buchanan',
+    author_email = 'craig.ds@gmail.com',
+    url = 'http://github.com/django-mptt/django-mptt',
+    test_suite="mptt.tests.test_runner.run_tests",
+    packages=find_packages(exclude=['mptt.tests']),
+    package_data={'mptt': ['templates/admin/*']},
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+)
