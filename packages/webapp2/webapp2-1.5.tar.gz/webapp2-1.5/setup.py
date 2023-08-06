9ba1b0ea8c9dc7cc11c22c9cb37a038ae6b9d1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:30 +0000

    Add package webapp2/1.5/webapp2-1.5.tar.gz

---
+++ packages/webapp2/webapp2-1.5.tar.gz/webapp2-1.5/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+webapp2
+~~~~~~~
+
+This is an attempt to improve Google App Engine's
+`webapp <http://code.google.com/appengine/docs/python/tools/webapp/>`_
+framework keeping maximum compatibility and same performance.
+
+Features overview: http://code.google.com/p/webapp-improved/
+"""
+from setuptools import setup
+
+setup(
+    name = 'webapp2',
+    version = '1.5',
+    license = 'Apache Software License',
+    url = 'http://www.tipfy.org/',
+    description = "Taking Google App Engine's webapp to the next level!",
+    long_description = __doc__,
+    author = 'Rodrigo Moraes',
+    author_email = 'rodrigo.moraes@gmail.com',
+    zip_safe = False,
+    platforms = 'any',
+    packages = [
+        'webapp2',
+        'webapp2_extras',
+    ],
+    include_package_data=True,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
