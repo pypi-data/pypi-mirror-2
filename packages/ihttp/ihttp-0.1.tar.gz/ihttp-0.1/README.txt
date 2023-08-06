0fdbc2baa0c430a116827a5f266604c5f8fb954e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:09 +0000

    Add package ihttp/0.1/ihttp-0.1.tar.gz

---
+++ packages/ihttp/ihttp-0.1.tar.gz/ihttp-0.1/README.txt
@@ -0,0 +1,11 @@
+iHttp
+=====
+
+Easy to use http request module.
+
+Installation
+------------
+
+	pip install -e git+git://github.com/alancastro/ihttp.git#egg=ihttp
+
+
---
+++ packages/ihttp/ihttp-0.1.tar.gz/ihttp-0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='ihttp',
+    packages=['ihttp'],
+    version='0.1',
+    description='Easy python http fetch module',
+    author='Alan Castro',
+    author_email='alanclic@gmail.com',
+    url='http://alancastro.org',
+    keywords = ["http", "fetch", "get", "post"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: Unix",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP"
+        ],
+    long_description = open('README.txt').read()
+)
