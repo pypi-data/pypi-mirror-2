c3447615e92c0b57fd6f38c53bf9aa6da59a16e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:37 +0000

    Add package dolmen.security.policies/0.1/dolmen.security.policies-0.1.tar.gz

---
+++ packages/dolmen.security.policies/dolmen.security.policies-0.1.tar.gz/dolmen.security.policies-0.1/src/dolmen/security/policies/tests.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import doctest
+import unittest
+import pprint
+import dolmen.security.policies
+from zope.component.testlayer import ZCMLFileLayer
+
+layer = ZCMLFileLayer(dolmen.security.policies)
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    files = ('README.txt',)
+    for filename in files:
+        docfile = doctest.DocFileSuite(
+            filename,
+            globs={'pprint': pprint.pprint},
+            optionflags=(doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS))
+        docfile.layer = layer
+        suite.addTest(docfile)
+    return suite
