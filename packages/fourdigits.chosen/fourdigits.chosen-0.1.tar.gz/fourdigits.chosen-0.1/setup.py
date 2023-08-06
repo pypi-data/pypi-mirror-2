bdb229d91bbe929c43181e16e6bfd7a7ddabd693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:55 +0000

    Add package fourdigits.chosen/0.1/fourdigits.chosen-0.1.tar.gz

---
+++ packages/fourdigits.chosen/fourdigits.chosen-0.1.tar.gz/fourdigits.chosen-0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='fourdigits.chosen',
+      version=version,
+      description="Integrates the Chosen javascript library in Plone",
+      long_description=open("README.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        ],
+      keywords='',
+      author='Roel Bruggink',
+      author_email='roel@jaroel.nl',
+      url='https://github.com/jaroel/fourdigits.chosen/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['fourdigits'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'five.grok',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
