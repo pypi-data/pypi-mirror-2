27cf5c94f3eb7a952d03077739979690b181c688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:57 +0000

    Add package naf/0.2.3/naf-0.2.3.tar.gz

---
+++ packages/naf/naf-0.2.3.tar.gz/naf-0.2.3/naf/__main__.py
@@ -0,0 +1,40 @@
+import sys
+import subprocess
+
+if sys.platform == 'linux2':
+    import pynotify as notify
+    import gtk
+else:
+    import Growl as notify
+
+def display_notification(command, return_code):
+    if return_code == 0:
+        description = u'finished successfully'
+    else:
+        description = u'finished with return code %s' % return_code
+
+    if hasattr(notify, 'GrowlNotifier'):
+        g = notify.GrowlNotifier(u'naf', notifications=['command_finished',])
+        g.register()
+        g.notify('command_finished', command, description)
+    elif hasattr(notify, 'Notification'):
+        if return_code == 0:
+            icon = gtk.STOCK_DIALOG_INFO
+        else:
+            icon = gtk.STOCK_DIALOG_ERROR
+        notification = notify.Notification(command, description, icon)
+        notification.show()
+
+def main():
+    if len(sys.argv) < 2:
+        print 'Usage: naf <command_to_run>'
+        sys.exit(1)
+    command_string = u' '.join(sys.argv[1:])
+    return_code = subprocess.call(command_string, shell=True)
+
+    display_notification(command_string, return_code)
+
+    sys.exit(return_code)
+
+if __name__ == '__main__':
+    main()
