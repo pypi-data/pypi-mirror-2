5b74b1db287868b949ca2e1373d0b3075e75076d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:55 +0000

    Add package zope.app.applicationcontrol/3.5.8/zope.app.applicationcontrol-3.5.8.zip

---
+++ packages/zope.app.applicationcontrol/zope.app.applicationcontrol-3.5.8.zip/zope.app.applicationcontrol-3.5.8/src/zope/app/applicationcontrol/interfaces.py
@@ -0,0 +1,26 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Application Control Interface
+
+$Id: interfaces.py 107936 2010-01-09 14:58:15Z faassen $
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.interface import Interface
+# BBB
+from zope.applicationcontrol.interfaces import (
+    IApplicationControl,
+    IRuntimeInfo,
+    IZopeVersion,
+    IServerControl)
