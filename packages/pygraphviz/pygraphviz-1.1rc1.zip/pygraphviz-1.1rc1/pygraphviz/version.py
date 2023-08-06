2d52d27230195c8e44e79047ce4c7006f85fba4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:17 +0000

    Add package pygraphviz/1.1rc1/pygraphviz-1.1rc1.zip

---
+++ packages/pygraphviz/pygraphviz-1.1rc1.zip/pygraphviz-1.1rc1/pygraphviz/version.py
@@ -0,0 +1,11 @@
+"""
+Version information for PyGraphviz, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+__version__ = '1.1.dev1952'
+__revision__ = '1952'
+__date__ = 'Sat Feb 12 08:34:07 2011'
+
