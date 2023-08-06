45642dd5e3d786bd3068692d7334a4c2cba474e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:51 +0000

    Add package repoman/0.1/repoman-0.1.tar.gz

---
+++ packages/repoman/repoman-0.1.tar.gz/repoman-0.1/repoman/lib/storage/generic.py
@@ -0,0 +1,22 @@
+
+
+class StorageInterface(object):
+
+    storage_type = 'generic'
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+
+
+class StroageObject(object):
+    def __init__(self):
+        pass
+
