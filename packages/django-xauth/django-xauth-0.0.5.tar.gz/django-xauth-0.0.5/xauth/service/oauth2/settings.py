f580712cc707545f2977d6cc8894720a7949af67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:58 +0000

    Add package django-xauth/0.0.5/django-xauth-0.0.5.tar.gz

---
+++ packages/django-xauth/django-xauth-0.0.5.tar.gz/django-xauth-0.0.5/xauth/service/oauth2/settings.py
@@ -0,0 +1,6 @@
+PROVIDERS = {
+    'facebook': {
+        'authenticate_url': 'https://www.facebook.com/dialog/oauth',
+        'access_token_url': 'https://graph.facebook.com/oauth/access_token',
+    },
+}
