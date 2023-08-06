6e4519774b28393c81723ee573ce6d76198dc30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:57 +0000

    Add package django-photos/0.2/django-photos-0.2.tar.gz

---
+++ packages/django-photos/django-photos-0.2.tar.gz/django-photos-0.2/src/photos/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+from taggit.managers import TaggableManager
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
+from os.path import join, abspath, dirname, basename
+import datetime
+from hashlib import sha224
+import random
+from django.db.models.signals import pre_delete, pre_save
+from shutil import rmtree
+
+def get_upload_to(instance, filename):
+    dirs = datetime.datetime.now().strftime("photos/%y/%j/%H/%M/%S")
+    praefix = instance.hash
+    return join(dirs, praefix, filename)
+
+def get_hash():
+    return sha224(str(random.random())).hexdigest()
+
+class Photo(models.Model):
+    photo = models.ImageField(_('Photo'), upload_to=get_upload_to, height_field='height', width_field='width', max_length=250)
+    height = models.PositiveIntegerField(_('Height'), editable=False)
+    width = models.PositiveIntegerField(_('Width'), editable=False)
+    display = models.BooleanField(_('Display'), default=True, help_text=_('Enable to display this photo.'))
+    name = models.CharField(_('Name'), max_length=250, help_text=_('If no name is given, it is created from filename.'))
+    description = models.TextField(_('Description'), blank=True, null=True)
+    user = models.ForeignKey(User, editable=False, blank=True, null=True)
+    tags = TaggableManager()
+    timestamp_created = models.DateTimeField(_('Timestamp created'), auto_now_add=True, editable=False)
+    timestamp_edited = models.DateTimeField(_('Timestamp edited'), auto_now=True, editable=False)
+    hash = models.CharField(_('Hash'), editable=False, default=get_hash, max_length=64)
+    
+    def get_albums(self):
+        return self.album_set.filter(display=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta(object):
+        verbose_name = _('Photo')
+        verbose_name_plural = _('Photos')
+        ordering = ('-timestamp_created', )
+        
+    @models.permalink
+    def get_absolute_url(self):
+        return ('photo-detail', [self.id])
+        
+class Album(models.Model):
+    display = models.BooleanField(_('Display'), default=True, help_text=_('Disable to not display this gallery.'))
+    name = models.CharField(_('Name'), max_length=250)   
+    description = models.TextField(_('Description'), blank=True, null=True)
+    photos = models.ManyToManyField(Photo, verbose_name=_('Photos'))
+    tags = TaggableManager()
+    user = models.ForeignKey(User, editable=False, blank=True, null=True)
+    timestamp_created = models.DateTimeField(_('Timestamp created'), auto_now_add=True, editable=False)
+    timestamp_edited = models.DateTimeField(_('Timestamp edited'), auto_now=True, editable=False)
+    
+    def get_photos(self):
+        return self.photos.filter(display=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta(object):
+        verbose_name = _('Album')
+        verbose_name_plural = _('Albums')
+        ordering = ('-timestamp_created', )
+        
+    @models.permalink
+    def get_absolute_url(self):
+        return ('album-detail', [self.id])
+
+def pre_save_photo(*args, **kwargs):
+    instance = kwargs['instance']
+    path = instance.photo.path
+    if not instance.name:
+        name = basename(path).rsplit(".", 1)[0]
+        instance.name = name
+    
+pre_save.connect(pre_save_photo, sender=Photo)
+
+def pre_delete_photo(*args, **kwargs):
+    instance = kwargs['instance']
+    try:
+        path = instance.photo.path
+    except ValueError, e:
+        pass # silently
+    else:
+        rmtree(path=abspath(dirname(path)), ignore_errors=True)
+
+pre_delete.connect(pre_delete_photo, sender=Photo)
+    
---
+++ packages/django-photos/django-photos-0.2.tar.gz/django-photos-0.2/src/photos/views.py
@@ -0,0 +1,49 @@
+from photos.models import Photo, Album
+from django.contrib.auth.models import User
+from django.views.generic.list_detail import object_list, object_detail
+from django.shortcuts import get_object_or_404
+
+def album_list(request, user_id=0, username=None, *args, **kwargs):
+    qs = Album.objects.filter(display=True)
+    user = None
+    
+    if user_id > 0:
+        user = get_object_or_404(User, id=user_id)
+        qs = qs.filter(user=user)
+    elif username is not None:
+        user = get_object_or_404(User, username=username)
+        qs = qs.filter(user=user)
+        
+    if user:
+        extra_context = kwargs.get('extra_context', {})
+        extra_context.update({'user': user})
+        kwargs.update({'extra_context': extra_context})
+        
+    return object_list(request, queryset=qs, *args, **kwargs)
+
+def photo_list(request, user_id=0, username=None, *args, **kwargs):
+    qs = Photo.objects.filter(display=True)
+    user = None
+    
+    if user_id > 0:
+        user = get_object_or_404(User, id=user_id)
+        qs = qs.filter(user=user)
+    elif username is not None:
+        user = get_object_or_404(User, username=username)
+        qs = qs.filter(user=user)
+        
+    if user:
+        extra_context = kwargs.get('extra_context', {})
+        extra_context.update({'user': user})
+        kwargs.update({'extra_context': extra_context})
+    
+    return object_list(request, queryset=qs, *args, **kwargs)
+
+def photo_detail(request, object_id, album_id, *args, **kwargs):
+    album = get_object_or_404(Album, id=album_id)
+    
+    extra_context = kwargs.get('extra_context', {})
+    extra_context.update({'album': album})
+    kwargs.update({'extra_context': extra_context})
+        
+    return object_detail(request, object_id=object_id, *args, **kwargs)
