c6b8248b9e989308c4ced04cd07d894e7522b834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package cyrax/1.1/cyrax-1.1.tar.gz

---
+++ packages/cyrax/cyrax-1.1.tar.gz/cyrax-1.1/cyraxlib/templatefunctions.py
@@ -0,0 +1,18 @@
+from jinja2 import contextfunction
+from cyraxlib import utils
+
+@contextfunction
+def url_for(context, path):
+    basepath = utils.get_base_path(context['site'].url)
+    return utils.relpath(
+        utils.safe_url_join(basepath, context['entry'].get_relative_url()),
+        utils.safe_url_join(basepath, path))
+
+
+@contextfunction
+def absolute_url_for(context, path):
+    return utils.safe_url_join(context['site'].url, path)
+
+
+functions = [url_for, absolute_url_for]
+
---
+++ packages/cyrax/cyrax-1.1.tar.gz/cyrax-1.1/cyraxlib/utils.py
@@ -0,0 +1,60 @@
+import urlparse
+import os.path as op
+import posixpath
+from itertools import izip, takewhile
+
+
+def new_base(obj, base):
+    name = base.__name__ + obj.__class__.__name__
+    return type(name, (base, ), dict(obj.__class__.__dict__))
+
+
+def safe_url_join(base, path):
+    """
+    >>> safe_url_join('blog/x', 'foo/bar')
+    'blog/x/foo/bar'
+    >>> safe_url_join('/blog/x/', 'foo/bar')
+    '/blog/x/foo/bar'
+    >>> safe_url_join('http://blog/x', 'foo/bar')
+    'http://blog/x/foo/bar'
+    """
+    scheme, netloc, basepath, params, query, fragment = urlparse.urlparse(base)
+    newpath = posixpath.join(basepath, path)
+    parts = (scheme, netloc, newpath, params, query, fragment)
+    return urlparse.urlunparse(parts)
+
+
+def get_base_path(url):
+    """
+    >>> get_base_path('http://google.com')
+    '/'
+    >>> get_base_path('http://piranha.org.ua/blog')
+    '/blog'
+    """
+    basepath = urlparse.urlparse(url)[2]
+    return basepath or '/'
+
+
+def url2path(url):
+    return op.sep.join(url.split('/'))
+
+
+def path2url(path):
+    return '/'.join(path.split(op.sep))
+
+
+def removecommon(p1, p2):
+    common = takewhile(lambda x: x[0] == x[1], izip(p1, p2))
+    l = len(list(common))
+    return p1[l:], p2[l:]
+
+
+def relpath(cur, dest):
+    p1, p2 = removecommon(cur.split(op.sep), dest.split(op.sep))
+    # empty element because of / in the end of name
+    if p1 and p1[-1] == '':
+        p1 = p1[:-1]
+    p = ['../'] * len(p1) + p2
+    if not p:
+        return ''
+    return op.join(*p)
