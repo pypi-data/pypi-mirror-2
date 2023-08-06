fa765437cf06ea29a9c8377f83a6d5afa2d933ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:41 +0000

    Add package zopyx.tinymceplugins.tinyautosave/1.0.1/zopyx.tinymceplugins.tinyautosave-1.0.1.tar.gz

---
+++ packages/zopyx.tinymceplugins.tinyautosave/zopyx.tinymceplugins.tinyautosave-1.0.1.tar.gz/zopyx.tinymceplugins.tinyautosave-1.0.1/README.txt
@@ -0,0 +1,12 @@
+zopyx.tinymceplugins.tinyautosave
+=================================
+
+Auto-save support for TinyMCE - based on the 
+``tinymce.plugins.AutoSave`` plugin for TinyMCE.
+
+Authors
+-------
+
+* Andreas Jung, Simon Pamies
+* Funded by Mentz DV, Munich
+
---
+++ packages/zopyx.tinymceplugins.tinyautosave/zopyx.tinymceplugins.tinyautosave-1.0.1.tar.gz/zopyx.tinymceplugins.tinyautosave-1.0.1/zopyx/tinymceplugins/tinyautosave/skins/autosave/tinyautosave/langs/de.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('de.tinyautosave',{
+restore_content: "Wiederherstellung von automatisch gespeicherten Inhalten",
+no_content: "Es gibt keine wiederherstellbaren Inhalte",
+warning_message: "Existierende Inhalte werden durch die Wiederherstellung gelöscht.\n\nSind Sie sicher, dass Sie mit der Wiederherstellung fortschreiten möchten?"
+});
