1def2e501df6062e238b79c8e8a47788df6b0cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:42 +0000

    Add package pydss/0.1a/pydss-0.1a.tar.gz

---
+++ packages/pydss/pydss-0.1a.tar.gz/pydss-0.1a/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name="pydss",
+      description="Python Distribution System Simuator",
+      author="Richard Lincoln",
+      author_email="r.w.lincoln@gmail.com",
+      version="0.1a",
+      license="Apache License 2.0",
+      packages=['pydss',
+                'pydss.common',
+                'pydss.control',
+                'pydss.conversion',
+                'pydss.delivery',
+                'pydss.executive',
+                'pydss.general',
+                'pydss.meter',
+                'pydss.parser',
+                'pydss.plot',
+                'pydss.shared'],
+      zip_safe=True)
