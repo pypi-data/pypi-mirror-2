88912018e06681fc846d6cf6fd25620e080dbc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:24 +0000

    Add package jarn.mkrelease/3.1/jarn.mkrelease-3.1.zip

---
+++ packages/jarn.mkrelease/jarn.mkrelease-3.1.zip/jarn.mkrelease-3.1/jarn/mkrelease/python.py
@@ -0,0 +1,24 @@
+import sys
+
+from exit import err_exit
+
+
+class Python(object):
+    """Python interpreter abstraction."""
+
+    @property
+    def python(self):
+        return sys.executable
+
+    def __str__(self):
+        return self.python
+
+    def get_env(self):
+        return {'PYTHONPATH': ':'.join(sys.path)}
+
+    def is_valid_python(self):
+        return sys.version_info[:2] >= (2, 6)
+
+    def check_valid_python(self):
+        if not self.is_valid_python():
+            err_exit('Python >= 2.6 required')
