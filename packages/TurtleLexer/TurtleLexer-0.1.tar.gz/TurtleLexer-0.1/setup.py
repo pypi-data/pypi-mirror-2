73452a771fb719eb0f53425d0ecaad8564e7be8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:16 +0000

    Add package TurtleLexer/0.1/TurtleLexer-0.1.tar.gz

---
+++ packages/TurtleLexer/TurtleLexer-0.1.tar.gz/TurtleLexer-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+  name = "TurtleLexer",
+  version = "0.1",
+  
+  py_modules = ["turtlelexer"],
+  
+  author = "Kier Davis",
+  author_email = "kierdavis@gmail.com",
+  description = "A Turtle lexer for Pygments",
+  url = "https://github.com/kierdavis/TurtleLexer",
+  keywords = "pygments lexer turtle rdf",
+  classifiers = ["Programming Language :: Python", "Operating System :: OS Independent", "Topic :: Software Development :: Libraries :: Python Modules"],
+  
+  install_requires = ["Pygments"],
+  entry_points = {"pygments.lexers": "turtlelexer = turtlelexer:TurtleLexer"},
+)
