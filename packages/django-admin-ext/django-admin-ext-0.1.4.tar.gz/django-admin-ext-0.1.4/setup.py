fa0e406fd0fa5c576ecabf8cde42203bc0026d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:49 +0000

    Add package django-admin-ext/0.1.4/django-admin-ext-0.1.4.tar.gz

---
+++ packages/django-admin-ext/django-admin-ext-0.1.4.tar.gz/django-admin-ext-0.1.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+from djadmin_ext import VERSION
+
+REQUIREMENTS = [
+    'django',
+]
+
+TEST_REQUIREMENTS = [
+    'selenium>=2.0'
+] + REQUIREMENTS
+
+setup(
+    name="django-admin-ext",
+    version=VERSION,
+    author="Aaron Madison and Matthew J Morrison",
+    description="Extensions to Django's admin site to add an ajax view.",
+    long_description=open('README.txt', 'r').read(),
+    url="https://github.com/imtapps/django-admin-ext",
+    packages=("djadmin_ext",),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    tests_require=TEST_REQUIREMENTS,
+    test_suite='runtests.runtests',
+    zip_safe=False,
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+)
