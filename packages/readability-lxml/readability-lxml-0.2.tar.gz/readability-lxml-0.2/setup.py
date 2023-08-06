69e212015fef3d56c3e841a04fdda1dfb4679b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:10 +0000

    Add package readability-lxml/0.2/readability-lxml-0.2.tar.gz

---
+++ packages/readability-lxml/readability-lxml-0.2.tar.gz/readability-lxml-0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="readability-lxml",
+    version="0.2",
+    author="Yuri Baburov",
+    author_email="burchik@gmail.com",
+    description="python port of arc90's readability bookmarklet",
+    long_description=open("README").read(),
+    license="Apache License 2.0",
+    url="http://github.com/buriy/python-readability",
+    packages=find_packages(),
+    install_requires=[
+        "chardet"
+        ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+)
