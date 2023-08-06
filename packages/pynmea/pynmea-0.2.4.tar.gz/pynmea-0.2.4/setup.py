8b91cacdcbbb8e972a62d8f73fa0ceb501a09b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:15 +0000

    Add package pynmea/0.2.4/pynmea-0.2.4.tar.gz

---
+++ packages/pynmea/pynmea-0.2.4.tar.gz/pynmea-0.2.4/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="pynmea",
+      version="0.2.4",
+      description="Python NMEA Library",
+      author="Becky Lewis",
+      author_email="pynmea@scaryclam.co.uk",
+      url="https://code.google.com/p/pynmea/",
+      license="MIT",
+      packages=['pynmea'])
