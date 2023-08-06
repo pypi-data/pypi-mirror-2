a98c1ab048d091c0ac4e1846de0ad00555d84a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:22 +0000

    Add package collective.validator.xhtmlStrict/1.2.0/collective.validator.xhtmlStrict-1.2.0.tar.gz

---
+++ packages/collective.validator.xhtmlStrict/collective.validator.xhtmlStrict-1.2.0.tar.gz/collective.validator.xhtmlStrict-1.2.0/collective/validator/xhtmlStrict/config.py
@@ -0,0 +1,7 @@
+from Products.CMFCore.permissions import setDefaultRoles
+
+PROJECTNAME = 'collective.validator.xhtmlStrict'
+TITLE = 'Validator XHTML STRICT'
+DESCRIPTION = 'Validator for xhtml strict type'
+
+GLOBALS = globals()
