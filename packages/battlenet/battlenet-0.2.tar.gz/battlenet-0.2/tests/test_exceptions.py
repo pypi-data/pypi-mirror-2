06e293ad1304e40f0481965a583676685a3ca579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:02 +0000

    Add package battlenet/0.2/battlenet-0.2.tar.gz

---
+++ packages/battlenet/battlenet-0.2.tar.gz/battlenet-0.2/tests/test_exceptions.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+import battlenet
+
+PUBLIC_KEY = os.environ.get('BNET_PUBLIC_KEY')
+PRIVATE_KEY = os.environ.get('BNET_PRIVATE_KEY')
+
+class EventletTest(unittest.TestCase):
+    def setUp(self):
+        self.connection = battlenet.Connection(public_key=PUBLIC_KEY, private_key=PRIVATE_KEY)
+
+    def test_character_not_found(self):
+        self.assertRaises(battlenet.APIError,
+            lambda: self.connection.get_character(battlenet.UNITED_STATES, 'Fake Realm', 'Fake Character'))
+
+    def test_realm_not_found(self):
+        self.assertRaises(battlenet.RealmNotFound, lambda: self.connection.get_realm(battlenet.EUROPE, 'Fake Realm'))
+
+    def tearDown(self):
+        del self.connection
+
+if __name__ == '__main__':
+    unittest.main()
