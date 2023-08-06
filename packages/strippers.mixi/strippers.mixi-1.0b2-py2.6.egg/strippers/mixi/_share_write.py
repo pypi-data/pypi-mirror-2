e5c252c595a8f480840cf3b4fae9c8d2acba3b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:59 +0000

    Add package strippers.mixi/1.0b2/strippers.mixi-1.0b2-py2.6.egg

---
+++ packages/strippers.mixi/strippers.mixi-1.0b2-py2.6.egg/strippers/mixi/_share_write.py
@@ -0,0 +1,23 @@
+# vim:fileencoding=utf-8
+import json
+
+SHARE_URI = 'http://api.mixi-platform.com/2/share'
+
+def _share(self, key, title, url, image=None, pc_url=None, smartphone_url=None, mobile_url=None, description=None, comment=None, visibility='self'):
+    params = {
+            'key'            : key,
+            'title'          : title,
+            'description'    : description,
+            'primary_url'    : url,
+            'comment'        : comment,
+            'image'          : image,
+            'pc_url'         : pc_url,
+            'smartphone_url' : smartphone_url,
+            'mobile_url'     : mobile_url,
+            'privacy'        : { 'visibility' : visibility },
+            }
+    data = json.dumps(params)
+    self.post(SHARE_URI, data, self.CONTENT_TYPE_JSON)
+    
+
+API_METHODS = { 'share' : _share, }
