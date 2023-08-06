5763cd4bdeeb7372747535c22f9e7beb2497c033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:38 +0000

    Add package django-facebook/2.0/django-facebook-2.0.zip

---
+++ packages/django-facebook/django-facebook-2.0.zip/django-facebook-2.0/django_facebook/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'Thierry Schellenbach'
+__copyright__ = 'Copyright 2010, Thierry Schellenbach'
+__credits__ = ['Thierry Schellenbach, mellowmorning.com']
+
+
+__license__ = 'BSD'
+__version__ = '2.0'
+__maintainer__ = 'Thierry Schellenbach'
+__email__ = 'thierryschellenbach@gmail.com'
+__status__ = 'Production'
