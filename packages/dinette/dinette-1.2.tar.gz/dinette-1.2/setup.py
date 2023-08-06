fef9db4c6e9e20668110f1ac7e8f21fe23461ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:55 +0000

    Add package dinette/1.2/dinette-1.2.tar.gz

---
+++ packages/dinette/dinette-1.2.tar.gz/dinette-1.2/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+overview = file('docs/overview.txt')
+data = overview.read()
+overview.close()
+
+setup(
+    name='dinette',
+    description='Dinette is a forum application in the spirit of PunBB.',
+    keywords='django, forum',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    version="1.2",
+    author="Agiliq Solutions",
+    author_email="hello@agiliq.com",
+    long_description= data,
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Internet :: WWW/HTTP :: WSGI',
+                   'Topic :: Software Development :: Libraries :: Application Frameworks',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+    url="http://www.agiliq.com/",
+    license="GPL",
+    platforms=["all"],
+)
