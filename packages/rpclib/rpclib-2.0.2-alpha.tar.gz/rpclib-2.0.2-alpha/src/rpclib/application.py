8a2d6024a52bedb31a6a10a37504fc69bca100f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:37 +0000

    Add package rpclib/2.0.2-alpha/rpclib-2.0.2-alpha.tar.gz

---
+++ packages/rpclib/rpclib-2.0.2-alpha.tar.gz/rpclib-2.0.2-alpha/src/rpclib/application.py
@@ -0,0 +1,118 @@
+
+#
+# rpclib - Copyright (C) Rpclib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+import logging
+logger = logging.getLogger(__name__)
+
+import traceback
+
+from rpclib.model.exception import Fault
+from rpclib._base import EventManager
+
+class Application(object):
+    transport = None
+
+    def __init__(self, services, interface_class, in_protocol_class,
+                                      out_protocol_class=None, *args, **kwargs):
+        '''Constructor.
+
+        @param An iterable of ServiceBase subclasses that define the exposed
+               services.
+        @param The targetNamespace attribute of the exposed service.
+        @param The name attribute of the exposed service.
+        '''
+
+        if out_protocol_class is None:
+            out_protocol_class = in_protocol_class
+
+        # interface should be initialized before protocols.
+        self.interface = interface_class(self, services, *args, **kwargs)
+
+        self.in_protocol = in_protocol_class(self)
+        self.out_protocol = out_protocol_class(self)
+        self.services = services
+
+        self.__public_methods = {}
+        self.__classes = {}
+
+        self.event_manager = EventManager(self)
+
+    def process_request(self, ctx, req_obj):
+        """Takes a MethodContext instance and the native request object.
+        Returns the response to the request as a native python object.
+
+        Not meant to be overridden.
+        """
+
+        try:
+            # implementation hook
+            ctx.service_class.event_manager.fire_event('method_call',ctx)
+
+            # retrieve the method
+            func = getattr(ctx.service_class, ctx.descriptor.name)
+
+            # call the method
+            ctx.out_object = ctx.service_class.call_wrapper(ctx, func, req_obj)
+
+            # fire events
+            self.event_manager.fire_event('method_return_object', ctx)
+            ctx.service_class.event_manager.fire_event(
+                                                    'method_return_object', ctx)
+
+        except Fault, e:
+            stacktrace=traceback.format_exc()
+            logger.error(stacktrace)
+
+            ctx.out_error = e
+
+            # fire events
+            self.event_manager.fire_event('method_exception_object', ctx)
+            if ctx.service_class != None:
+                ctx.service_class.event_manager.fire_event(
+                                                    'method_return_object', ctx)
+
+        except Exception, e:
+            stacktrace=traceback.format_exc()
+            logger.error(stacktrace)
+
+            ctx.out_error = Fault('Server', str(e))
+
+            # fire events
+            self.event_manager.fire_event('method_exception_object', ctx)
+            if ctx.service_class != None:
+                ctx.service_class.event_manager.fire_event(
+                                                    'method_return_object', ctx)
+
+    def get_service_class(self, method_name):
+        """This call maps method names to the services that will handle them.
+
+        Override this function to alter the method mappings. Just try not to get
+        too crazy with regular expressions :)
+        """
+
+        return self.interface.call_routes[method_name]
+
+    def _has_callbacks(self):
+        retval = False
+
+        for s in self.services:
+            if self.get_service(s)._has_callbacks():
+                return True
+
+        return retval
---
+++ packages/rpclib/rpclib-2.0.2-alpha.tar.gz/rpclib-2.0.2-alpha/src/rpclib/server/wsgi.py
@@ -0,0 +1,183 @@
+
+#
+# rpclib - Copyright (C) Rpclib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+"""An rpc server that uses http as transport, and wsgi as bridge api"""
+
+# FIXME: this is maybe still too soap-centric.
+
+import logging
+logger = logging.getLogger(__name__)
+
+import traceback
+
+from rpclib._base import MethodContext
+from rpclib.model.exception import Fault
+from rpclib.protocol.soap.mime import apply_mtom
+from rpclib.util import reconstruct_url
+from rpclib.server import ServerBase
+
+HTTP_500 = '500 Internal server error'
+HTTP_200 = '200 OK'
+HTTP_404 = '404 Method Not Found'
+HTTP_405 = '405 Method Not Allowed'
+
+class ValidationError(Fault):
+    pass
+
+class WsgiMethodContext(MethodContext):
+    def __init__(self, app, req_env, content_type):
+        self.http_req_env = req_env
+        self.http_resp_headers = {
+            'Content-Type': content_type,
+            'Content-Length': '0',
+        }
+        self.wsdl_error = None
+
+        MethodContext.__init__(self, app)
+
+class WsgiApplication(ServerBase):
+    transport = 'http://schemas.xmlsoap.org/soap/http'
+
+    def __init__(self, app):
+        ServerBase.__init__(self, app)
+
+        self._allowed_http_verbs = app.in_protocol.allowed_http_verbs
+
+    def __call__(self, req_env, start_response, wsgi_url=None):
+        '''This method conforms to the WSGI spec for callable wsgi applications
+        (PEP 333). It looks in environ['wsgi.input'] for a fully formed rpc
+        message envelope, will deserialize the request parameters and call the
+        method on the object returned by the get_handler() method.
+
+        @param the http environment
+        @param a callable that begins the response message
+        @param the optional url
+
+        @returns the string representation of the rpc message
+        '''
+
+        url = wsgi_url
+        if url is None:
+            url = reconstruct_url(req_env).split('.wsdl')[0]
+
+        if self.__is_wsdl_request(req_env):
+            return self.__handle_wsdl_request(req_env, start_response, url)
+
+        elif not (req_env['REQUEST_METHOD'].upper() in self._allowed_http_verbs):
+            start_response(HTTP_405, [
+                ('Content-type', ''),
+                ('Allow', ', '.join(self._allowed_http_verbs)),
+            ])
+            return ['']
+
+        else:
+            return self.__handle_rpc(req_env, start_response)
+
+    def __is_wsdl_request(self, req_env):
+        # Get the wsdl for the service. Assume path_info matches pattern:
+        # /stuff/stuff/stuff/serviceName.wsdl or
+        # /stuff/stuff/stuff/serviceName/?wsdl
+
+        return (
+            req_env['REQUEST_METHOD'].lower() == 'get'
+            and (
+                   req_env['QUERY_STRING'].endswith('wsdl')
+                or req_env['PATH_INFO'].endswith('wsdl')
+            )
+        )
+
+    def __handle_wsdl_request(self, req_env, start_response, url):
+        ctx = WsgiMethodContext(self.app, req_env, 'text/xml')
+
+        try:
+            wsdl = self.app.interface.get_interface_document()
+            if wsdl is None:
+                self.app.interface.build_interface_document(url)
+                wsdl = self.app.interface.get_interface_document()
+
+            assert wsdl != None
+            
+            self.event_manager.fire_event('wsdl',ctx) # implementation hook
+
+            ctx.http_resp_headers['Content-Length'] = str(len(wsdl))
+            start_response(HTTP_200, ctx.http_resp_headers.items())
+
+            return [wsdl]
+
+        except Exception, e:
+            logger.error(traceback.format_exc())
+            ctx.wsdl_error = e
+            # implementation hook
+            self.event_manager.fire_event('wsdl_exception', ctx)
+
+            start_response(HTTP_500, ctx.http_resp_headers.items())
+
+            return [""]
+
+    def __handle_rpc(self, req_env, start_response):
+        ctx = WsgiMethodContext(self.app, req_env,
+                                                self.app.out_protocol.mime_type)
+
+        # implementation hook
+        self.event_manager.fire_event('wsgi_call', ctx)
+
+        ctx.in_string, in_string_charset = \
+                        self.app.in_protocol.reconstruct_wsgi_request(req_env)
+
+        self.get_in_object(ctx, in_string_charset)
+
+        return_code = HTTP_200
+        if ctx.in_error:
+            out_object = ctx.in_error
+            return_code = HTTP_500
+
+        else:
+            if ctx.service_class == None:
+                start_response(HTTP_404, ctx.http_resp_headers.items())
+                return ['']
+
+            self.get_out_object(ctx)
+            if not (ctx.out_error is None):
+                return_code = HTTP_500
+
+        self.get_out_string(ctx)
+
+        # implementation hook
+        self.event_manager.fire_event('wsgi_return', ctx)
+
+        if ctx.descriptor and ctx.descriptor.mtom:
+            # when there are more than one return type, the result is 
+            # encapsulated inside a list. when there's just one, the result
+            # is returned unencapsulated. the apply_mtom always expects the
+            # objects to be inside an iterable, hence the following test.
+            out_type_info = ctx.descriptor.out_message._type_info
+            if len(out_type_info) == 1:
+                out_object = [out_object]
+
+            ctx.http_resp_headers, ctx.out_string = apply_mtom(
+                    ctx.http_resp_headers, ctx.out_string,
+                    ctx.descriptor.out_message._type_info.values(),
+                    out_object
+                )
+
+        # initiate the response
+        del ctx.http_resp_headers['Content-Length']
+        start_response(return_code, ctx.http_resp_headers.items())
+
+        return ctx.out_string
