8465f1b5677f215097495db85ac6170959205fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:55 +0000

    Add package grokcore.component/2.4/grokcore.component-2.4.tar.gz

---
+++ packages/grokcore.component/grokcore.component-2.4.tar.gz/grokcore.component-2.4/src/grokcore/component/util.py
@@ -0,0 +1,31 @@
+##############################################################################
+#
+# Copyright (c) 2006-2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Grok utility functions.
+"""
+from grokcore.component import directive
+
+def _sort_key(component):
+    # If components have a grok.order directive, sort by that.
+    explicit_order, implicit_order = directive.order.bind().get(component)
+    return (explicit_order,
+            component.__module__,
+            implicit_order,
+            component.__class__.__name__)
+
+
+def sort_components(components):
+    """Sort a list of components using the information provided by
+    `grok.order`.
+    """
+    return sorted(components, key=_sort_key)
