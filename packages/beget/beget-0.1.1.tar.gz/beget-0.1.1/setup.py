6b0394357edc286cc4a2bc5780921655ddfdbceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:04 +0000

    Add package beget/0.1.1/beget-0.1.1.tar.gz

---
+++ packages/beget/beget-0.1.1.tar.gz/beget-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+
+setup(
+    name="beget",
+    version="0.1.1",
+    url='http://github.com/rob-b/Beget',
+    license='BSD',
+    description="Django project generator",
+    long_description=README,
+    author='Rob Berry',
+    author_email='',
+    packages=['beget'],
+    scripts=['bin/beget'],
+    package_data={'beget': ['project_template/setup.py',
+                            'project_template/project/*py']},
+    tests_require=['fudge'],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+)
