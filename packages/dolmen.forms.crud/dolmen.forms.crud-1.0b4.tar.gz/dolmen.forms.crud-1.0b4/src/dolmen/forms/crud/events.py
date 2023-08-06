164c37ad8e5016d437a14b3da29eef4e55dda50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:19 +0000

    Add package dolmen.forms.crud/1.0b4/dolmen.forms.crud-1.0b4.tar.gz

---
+++ packages/dolmen.forms.crud/dolmen.forms.crud-1.0b4.tar.gz/dolmen.forms.crud-1.0b4/src/dolmen/forms/crud/events.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import grokcore.component as grok
+from dolmen.content import schema, IContent
+from dolmen.forms.base import Fields, IFieldUpdate
+from zope.component import getAdapters
+from zope.lifecycleevent import IObjectModifiedEvent, IObjectCreatedEvent
+
+
+@grok.subscribe(IContent, IObjectCreatedEvent)
+def notify_fields_creation(ob, event):
+    """This handler propagates the ObjectCreatedEvent to a more atomic
+    level, by calling an adapter on each field of the schema. This permits
+    to actually interact at the field level, after it gets a value for the
+    first time.
+    """
+    schemas = schema.bind(default=None).get(ob)
+    if schemas:
+        fields = Fields(*schemas)
+
+        for field_repr in fields:
+            field = field_repr._field
+            if field.get(ob) != field.missing_value:
+                handlers = getAdapters((ob, field), IFieldUpdate)
+                for handler in handlers:
+                    # Iteration through the generator
+                    pass
+
+
+@grok.subscribe(IContent, IObjectModifiedEvent)
+def notify_fields_update(ob, event):
+    """This handler propagates the ObjectModifiedEvent to a more atomic
+    level, by calling an adapter on each modified field. This permits to
+    actually interact at the field level, after it gets modified.
+    """
+    for desc in event.descriptions:
+        for name in desc.attributes:
+            field = desc.interface[name]
+            handlers = getAdapters((ob, field), IFieldUpdate)
+            for handler in handlers:
+                # Iteration through the generator
+                pass
