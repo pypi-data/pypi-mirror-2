00e2f211e28cfa37602228ed1eae96e66b1aee7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:52 +0000

    Add package ibidem-util/1.1/ibidem-util-1.1.zip

---
+++ packages/ibidem-util/ibidem-util-1.1.zip/ibidem-util-1.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+def read(filename):
+    f = open(filename)
+    contents = f.read()
+    f.close()
+    return contents
+
+classifiers = """
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+setup(
+    name = "ibidem-util",
+    version = "1.1",
+    packages = ["ibidem", "ibidem.util"],
+    namespace_packages = ["ibidem"],
+    zip_safe = True,
+    include_package_data = True,
+
+    # Metadata
+    author = "Morten Lied Johansen",
+    author_email = "mortenjo@ifi.uio.no",
+    description = "Ibidem utilities",
+    long_description = read("README.rst"),
+    license = "LGPL",
+    keywords = "ibidem util",
+    classifiers = filter(None, classifiers.split("\n")),
+    url = "http://ibidem.homeip.net/Misc/wiki/Utils"
+)
