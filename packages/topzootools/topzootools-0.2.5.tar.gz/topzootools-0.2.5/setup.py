7958c0b2b54cc3a4e8a4bf8019d6045774534b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:59 +0000

    Add package topzootools/0.2.5/topzootools-0.2.5.tar.gz

---
+++ packages/topzootools/topzootools-0.2.5.tar.gz/topzootools-0.2.5/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup (
+     name = "topzootools",
+     version = "0.2.5",
+     description = "Processing and conversion tools for Internet Topology Zoo",
+
+     long_description="Processing and conversion tools for Internet Topology Zoo",
+               
+     # simple to run 
+     entry_points = {
+         'console_scripts': [
+             'yed2zoo = TopZooTools.yed2zoo:main',    
+             'zooconvert = TopZooTools.convert:main',         
+             'zooplot = TopZooTools.geoplot:main',
+             'zoostats= TopZooTools.zoostats:main',
+         ],
+     },
+
+     author='Simon Knight',
+     author_email="simon.knight@adelaide.edu.au",
+     url="http://www.topology-zoo.org",
+     packages=['TopZooTools'],  
+     
+     package_data = {'': ['converter.py', 'geoplot.py', 'yed2zoo.py']},
+     
+     install_requires=['mako', 'networkx', 'matplotlib', 'numpy',],
+     classifiers = [
+         "Programming Language :: Python",
+         "Development Status :: 3 - Alpha",
+         "Intended Audience :: Science/Research",
+         "Intended Audience :: System Administrators",
+         "Intended Audience :: Telecommunications Industry",
+         "License :: Other/Proprietary License",
+         "Operating System :: MacOS :: MacOS X",
+         "Operating System :: POSIX :: Linux",
+         "Topic :: System :: Networking",
+         "Topic :: System :: Software Distribution",
+         "Topic :: Scientific/Engineering :: Mathematics",
+         ],     
+ 
+)
