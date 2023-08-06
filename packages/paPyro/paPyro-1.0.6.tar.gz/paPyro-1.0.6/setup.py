c7c6621f161300711c2d3c5d82446475ad2f9549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:30 +0000

    Add package paPyro/1.0.6/paPyro-1.0.6.tar.gz

---
+++ packages/paPyro/paPyro-1.0.6.tar.gz/paPyro-1.0.6/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='paPyro',
+      version='1.0.6',
+      author='León Domingo',
+      author_email='leon.domingo@ender.es',
+      description=('A PDF report generator written in Python'),
+      #license=???,
+      keywords='pdf reports ender',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+      ],
+      url='http://www.ender.es',      
+      packages=['papyro',
+                'papyro.ttfonts'],
+      package_data={
+        'papyro.ttfonts': ['*.ttf'],
+      },
+      install_requires=[
+        'Neptuno2',
+        'ReportLab>=2.5',
+        'SQLAlchemy==0.6.8',
+        'lxml',
+      ],
+     )
