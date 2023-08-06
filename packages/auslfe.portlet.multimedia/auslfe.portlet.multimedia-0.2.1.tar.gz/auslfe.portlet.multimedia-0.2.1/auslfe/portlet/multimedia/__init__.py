c81f283614c8a45c0f3e41b61d596e7c53c2dfc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:37 +0000

    Add package auslfe.portlet.multimedia/0.2.1/auslfe.portlet.multimedia-0.2.1.tar.gz

---
+++ packages/auslfe.portlet.multimedia/auslfe.portlet.multimedia-0.2.1.tar.gz/auslfe.portlet.multimedia-0.2.1/auslfe/portlet/multimedia/__init__.py
@@ -0,0 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+AuslfePortletMultimediaMessageFactory = MessageFactory('auslfe.portlet.multimedia')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
