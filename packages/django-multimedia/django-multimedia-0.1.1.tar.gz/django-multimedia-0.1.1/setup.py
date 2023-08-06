7c541ca6c80d14513fedac625083aac79012a873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:45 +0000

    Add package django-multimedia/0.1.1/django-multimedia-0.1.1.tar.gz

---
+++ packages/django-multimedia/django-multimedia-0.1.1.tar.gz/django-multimedia-0.1.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = __import__('multimedia').__version__
+
+install_requires = [
+    'setuptools',
+    'django-celery',
+    'django-filer',
+    'paramiko',
+    'pycrypto==2.0.1',
+]
+
+setup(
+    name = "django-multimedia",
+    version = version,
+    url = 'http://github.com/salvaorenick/django-multimedia',
+    license = 'BSD',
+    platforms=['OS Independent'],
+    description = "",
+    author = "Andrew Schoen",
+    author_email = 'andrew.schoen@gmail.com',
+    packages=find_packages(),
+    install_requires = install_requires,
+    include_package_data=True,
+    zip_safe=False,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Video :: Conversion',
+    ],
+    package_dir={
+        'multimedia': 'multimedia',
+    },
+)
