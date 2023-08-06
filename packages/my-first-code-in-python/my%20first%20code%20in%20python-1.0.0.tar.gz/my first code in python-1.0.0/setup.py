046e49015bba21cc053421b0d13c71b74ca929ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:34 +0000

    Add package my-first-code-in-python/1.0.0/my%20first%20code%20in%20python-1.0.0.tar.gz

---
+++ packages/my-first-code-in-python/my%20first%20code%20in%20python-1.0.0.tar.gz/my first code in python-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		='my first code in python',
+	version		='1.0.0',
+	py_modules	='my_first_code001',
+	author		='joe',
+	author_email	='avenuecool.dude@gmail.com',
+	url		='Under construction',
+	description	='prints a list which may or may not be nested',
+)
