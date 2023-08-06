f54dc5f646f48b3b0fbbb9f8069a52b7ad318b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:52 +0000

    Add package pfg.drafts/1.1/pfg.drafts-1.1.zip

---
+++ packages/pfg.drafts/pfg.drafts-1.1.zip/pfg.drafts-1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+version = '1.1'
+
+setup(name='pfg.drafts',
+      version=version,
+      description="Allows users to save drafts of PloneFormGen forms in progress",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='ploneformgen form draft save',
+      author='David Glick, Groundwire',
+      author_email='davidglick@groundwire.org',
+      url='http://svn.plone.org/svn/collective/pfg.drafts/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pfg'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.PloneFormGen',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+        'test': ['selenium>=2.0a5'],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
