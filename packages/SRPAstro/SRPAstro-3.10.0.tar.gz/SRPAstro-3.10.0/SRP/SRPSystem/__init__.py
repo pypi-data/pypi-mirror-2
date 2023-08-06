bbac3c35e572b52be1f9c1d90c452fdea6df84df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:22 +0000

    Add package SRPAstro/3.10.0/SRPAstro-3.10.0.tar.gz

---
+++ packages/SRPAstro/SRPAstro-3.10.0.tar.gz/SRPAstro-3.10.0/SRP/SRPSystem/__init__.py
@@ -0,0 +1,19 @@
+""" Init file for SRPSystem
+
+Context : SRP
+Module  : System
+Version : 1.0.0
+Author  : Stefano Covino
+Date    : 24/08/2011
+E-mail  : stefano.covino@brera.inaf.it
+URL     : http://www.me.oa-brera.inaf.it/utenti/covino
+
+Usage   : to be imported
+
+History : (24/08/2011) First named version.
+"""
+
+
+
+__all__ = ['ListDirTimeSorted', 'Pipe', 'Touch', 'Which']
+
