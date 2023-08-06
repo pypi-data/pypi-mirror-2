a3028c0ade8fdc8a8cd86a557ff3ceac9fc43ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:35 +0000

    Add package simple-gae-memoize/0.0.1/simple-gae-memoize-0.0.1.tar.gz

---
+++ packages/simple-gae-memoize/simple-gae-memoize-0.0.1.tar.gz/simple-gae-memoize-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="simple-gae-memoize",
+    version="0.0.1",
+    author="Cesar Canassa",
+    author_email="cesar.canassa@gmail.com",
+    description="A decorator to memoize App Engine functions using the memcache",
+    license="BSD",
+    keywords="gae memcache memoize",
+    url="http://packages.python.org/simple_gae_memoize",
+    packages=['memoize'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
