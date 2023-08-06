f9b7562862c8e0a600a76970896963a92d0579b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:39 +0000

    Add package pyxdeco/0.1/pyxdeco-0.1.tar.gz

---
+++ packages/pyxdeco/pyxdeco-0.1.tar.gz/pyxdeco-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='pyxdeco',
+      version=version,
+      description="Python eXtraordinary Decorators",
+      long_description=open('README.rst').read(),
+      klassifiers=[
+          "Programming Language :: Python",
+          "License :: OSI Approved :: MIT License",
+          "Intended Audience :: Developers"
+          "Topic :: Software Development :: Libraries :: Python Modules"
+      ],
+      keywords='',
+      author='Ian McCracken',
+      author_email='ian.mccracken@gmail.com',
+      url='http://github.com/iancmcc/pyxdeco',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[],
+      entry_points="""
+      """,
+      )
