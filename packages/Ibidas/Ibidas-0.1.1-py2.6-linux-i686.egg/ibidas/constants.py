2b0651edc4f52f7efda4d7d38e86543f1474efe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:30 +0000

    Add package Ibidas/0.1.1/Ibidas-0.1.1-py2.6-linux-i686.egg

---
+++ packages/Ibidas/Ibidas-0.1.1-py2.6-linux-i686.egg/ibidas/constants.py
@@ -0,0 +1,19 @@
+
+UNDEFINED = -1
+
+
+RS_TYPES_KNOWN = 1
+RS_SLICES_KNOWN = 2
+RS_ALL_KNOWN = RS_TYPES_KNOWN | RS_SLICES_KNOWN
+RS_INFERRED = 8
+RS_CHECK = 16
+
+
+BCCOPY=0
+BCENSURE=1
+BCEXIST=2
+BCNEW=3
+
+
+NOVAL="NOVALUE" #alternative None for functions which also accept NOne as valid input
+                #but still want some params to be optional
---
+++ packages/Ibidas/Ibidas-0.1.1-py2.6-linux-i686.egg/ibidas/passes/__init__.py
@@ -0,0 +1,10 @@
+from manager import Pass, PassManager
+from ensure_info import EnsureInfo
+from create_graph import CreateGraph
+from annotate_replinks import AnnotateRepLinks
+from prepeephole import PrePeepHole
+from create_expression_objects import CreateExpressions
+from wrapper_planner import WrapperPlanner
+from serialize_exec import SerializeExec
+from cytoscape_vis import DebugVisualizer
+
