39bcc4b1d9fb569b1b5cc6250fddcb7b9cdb0af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:21 +0000

    Add package django-notes/0.1.0/django-notes-0.1.0.tar.gz

---
+++ packages/django-notes/django-notes-0.1.0.tar.gz/django-notes-0.1.0/notes/admin.py
@@ -0,0 +1,8 @@
+from notes.models import Note, Topic
+from django.contrib import admin
+from django.contrib.contenttypes import generic
+
+class NoteInline(generic.GenericTabularInline):
+    model = Note
+
+admin.site.register(Topic)
