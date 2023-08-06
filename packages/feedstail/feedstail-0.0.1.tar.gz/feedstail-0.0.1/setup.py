95283b9aa52913c360c2a94f60c0c25fec8695cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:34 +0000

    Add package feedstail/0.0.1/feedstail-0.0.1.tar.gz

---
+++ packages/feedstail/feedstail-0.0.1.tar.gz/feedstail-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# Setup file for feedstail
+from setuptools import setup
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup( name             = "feedstail"
+     , description      = "A tail-f-like utility for feeds"
+     , long_description = long_description
+     , license          = "GNU General Public License v3"
+     , url              = "https://gitorious.org/feedstail/feedstail"
+
+     , author           = "Romain Gauthier"
+     , author_email     = "romain.gauthier@masteri2l.org"
+
+     , version          = "0.0.1"
+     , scripts          = ['bin/feedstail']
+     , packages         = ['feedstail']
+     , data_files       = [('', ['README.rst', 'LICENSE.txt'])]
+     , install_requires = ['argparse', 'FeedParser']
+
+     , classifiers      =
+         [ "Development Status :: 3 - Alpha"
+         , "License :: OSI Approved :: GNU General Public License (GPL)"
+         , "Operating System :: OS Independent"
+         , "Programming Language :: Python :: 2.6"
+         , "Topic :: Utilities"
+         ]
+      )
+
