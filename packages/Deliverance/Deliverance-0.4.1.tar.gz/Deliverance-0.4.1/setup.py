3556440dddf9faccc4282dca6ff9d4579ab61365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:54 +0000

    Add package Deliverance/0.4.1/Deliverance-0.4.1.tar.gz

---
+++ packages/Deliverance/Deliverance-0.4.1.tar.gz/Deliverance-0.4.1/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+version = '0.4.1'
+
+setup(name='Deliverance',
+      version=version,
+      description="Deliverance transforms HTML to theme pages",
+      long_description="""\
+Deliverance does transformations of HTML to 'theme' pages, similar in
+function to XSLT but using a simpler XML-based language to express the
+transformation.
+""",
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+      ],
+      keywords='wsgi theming html',
+      author='Ian Bicking, Ethan Jucovy',
+      author_email='deliverance-devel@lists.coactivate.org',
+      url='https://github.com/deliverance/Deliverance/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector', 
+      tests_require=['nose', "WebTest"],
+      install_requires=[
+        "lxml>=2.1alpha1",
+        "WebOb",
+        "WSGIProxy",
+        "Tempita",
+        "Pygments",
+        "WebError",
+        "DevAuth",
+        "Paste",
+        "PasteScript",
+        "WSGIFilter",
+        "chardet",
+        "simplejson",
+        ],
+      entry_points="""
+      [console_scripts]
+      deliverance-proxy = deliverance.proxycommand:main
+
+      [paste.paster_create_template]
+      deliverance = deliverance.paster_templates:DeliveranceTemplate
+      deliverance_plone = deliverance.paster_templates:PloneTemplate
+
+      [paste.filter_app_factory]
+      main = deliverance.middleware:make_deliverance_middleware
+
+      [paste.filter_factory]
+      garbagecollect = deliverance.garbagecollect:filter_factory
+      """,
+      )
