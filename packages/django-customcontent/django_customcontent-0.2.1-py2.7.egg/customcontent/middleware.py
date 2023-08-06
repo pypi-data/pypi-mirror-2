bd606dc5e92fa53ff9ef7237670b8f14dd73d5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:04 +0000

    Add package django-customcontent/0.2.1/django_customcontent-0.2.1-py2.7.egg

---
+++ packages/django-customcontent/django_customcontent-0.2.1-py2.7.egg/customcontent/middleware.py
@@ -0,0 +1,19 @@
+import re
+from customcontent.models import CustomContent
+from customcontent.templatetags.customcontent_tags import cc_item
+
+class CustomContentMiddleware(object):
+
+    def process_response(self, request, response):
+        """
+            TODO: refactor some to improve load time - probably need to
+            change the cc_item as that's doing a hit to the db for each
+            call of it when we already have our item essentially.
+        """
+        item = CustomContent.find(request)
+        head_data = cc_item(item, 'extra_head', 'js', 'css')
+        response.content = re.sub(r'(</head>)', r'%s\1' % head_data, response.content, 0, re.UNICODE)
+        # atm do not insert at positions, just append before body close
+        body_data = cc_item(item, 'customitem_set')
+        response.content = re.sub(r'(</body>)', r'%s\1' % body_data, response.content)
+        return response
