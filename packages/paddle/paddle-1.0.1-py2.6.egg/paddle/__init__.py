6021974902ac39257f06587ec90776b2bbbe7aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:41 +0000

    Add package paddle/1.0.1/paddle-1.0.1-py2.6.egg

---
+++ packages/paddle/paddle-1.0.1-py2.6.egg/paddle/__init__.py
@@ -0,0 +1,10 @@
+'''
+PADDLE: Proximal Algorithms for Dual Dictionary LEarning
+'''
+
+import common, dual, tight
+
+# major number for main changes
+# minor number for new features
+# release number for bug fixes and minor updates
+__version__ = '1.0.1'
