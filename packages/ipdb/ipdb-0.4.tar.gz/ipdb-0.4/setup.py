f067fe228d7c5b05a5ab9aab6add227899a5173f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:23 +0000

    Add package ipdb/0.4/ipdb-0.4.tar.gz

---
+++ packages/ipdb/ipdb-0.4.tar.gz/ipdb-0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.4'
+
+long_description = (file('README.rst').read() +
+    '\n\n' + file('HISTORY.txt').read())
+
+
+setup(name='ipdb',
+      version=version,
+      description="IPython-enabled pdb",
+      long_description=long_description,
+      classifiers=[],
+      keywords='pdb ipython',
+      author='Godefroid Chapelle',
+      author_email='gotcha@bubblenet.be',
+      url='https://github.com/gotcha/ipdb',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'ipython',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
