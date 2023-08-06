579a58f18132ff55a4936977adbf3ce9c067b149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:14 +0000

    Add package xodb/0.2.8/xodb-0.2.8.tar.gz

---
+++ packages/xodb/xodb-0.2.8.tar.gz/xodb-0.2.8/xodb/exc.py
@@ -0,0 +1,20 @@
+
+
+class XODBError(Exception):
+    pass
+
+
+class InvalidTermError(XODBError):
+    pass
+
+
+class AttributeRequired(XODBError):
+    pass
+
+
+class AttributeTypeRequired(XODBError):
+    pass
+
+
+class ValidationError(XODBError):
+    pass
