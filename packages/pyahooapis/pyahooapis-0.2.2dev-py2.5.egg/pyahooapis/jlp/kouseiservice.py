2ee2d6e1a67a4a233a8931ab3146fd3e2fc33241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:45 +0000

    Add package pyahooapis/0.2.2dev/pyahooapis-0.2.2dev-py2.5.egg

---
+++ packages/pyahooapis/pyahooapis-0.2.2dev-py2.5.egg/pyahooapis/jlp/kouseiservice.py
@@ -0,0 +1,94 @@
+#coding: utf-8
+
+from pyahooapis.service import Service, BaseObject
+
+FILTER_GROUP_MISS = 1
+FILTER_GROUP_SIMPLIFY = 2
+FILTER_GROUP_TO_BETTER = 4
+
+FILTER_GROUPS = {
+    FILTER_GROUP_MISS : 1,
+    FILTER_GROUP_SIMPLIFY : 2,
+    FILTER_GROUP_TO_BETTER : 3,
+    }
+
+NO_FILTER_FALSE_CONVERSION = 1
+NO_FILTER_MISUSE = 2
+NO_FILTER_USE_ATTENTION = 4
+NO_FILTER_UNPLEASANT = 8
+NO_FILTER_DEPENDENCE = 16
+NO_FILTER_FOREIGN_PLACE = 32
+NO_FILTER_NAME = 64
+NO_FILTER_NOT_RA = 128
+NO_FILTER_KANJI_OUTSIDE_THE_TABLE = 256
+NO_FILTER_PHRASEOLOGY = 512
+NO_FILTER_PARAPHRASE = 1024
+NO_FILTER_DOUBLE_NEGATION = 2048
+NO_FILTER_NOT_PARTICLE = 4096
+NO_FILTER_PLENASM = 8192
+NO_FILTER_ABBREVIATION = 16384
+
+NO_FILTERS = {
+    NO_FILTER_FALSE_CONVERSION : 1,
+    NO_FILTER_MISUSE : 2,
+    NO_FILTER_USE_ATTENTION : 3,
+    NO_FILTER_UNPLEASANT : 4,
+    NO_FILTER_DEPENDENCE : 5,
+    NO_FILTER_FOREIGN_PLACE : 6,
+    NO_FILTER_NAME : 7,
+    NO_FILTER_NOT_RA : 8,
+    NO_FILTER_KANJI_OUTSIDE_THE_TABLE : 9,
+    NO_FILTER_PHRASEOLOGY : 10,
+    NO_FILTER_PARAPHRASE : 11,
+    NO_FILTER_DOUBLE_NEGATION : 12,
+    NO_FILTER_NOT_PARTICLE : 13,
+    NO_FILTER_PLENASM : 14,
+    NO_FILTER_ABBREVIATION : 15,
+    }
+
+class KouseiService(Service):
+    def __init__(self, appid):
+        Service.__init__(self, appid, "http://jlp.yahooapis.jp/KouseiService/V1/kousei")
+        
+    def get_results(self,
+                   sentence,
+                   filter_group=None,
+                   no_filter=None,
+                   json=False):
+        
+        params = {}
+        
+        params['sentence'] = sentence
+        if filter_group is not None:
+            params['filter_group'] = self._binary2param(',', filter_group, FILTER_GROUPS)
+        if no_filter is not None:
+            params['no_filter'] = self._binary2param(',', no_filter, NO_FILTERS)
+        
+        dom = self._get_dom(params)
+        results = [
+                      Result(self._get_text(r, 'StartPos'),
+                             self._get_text(r, 'Length'),
+                             self._get_text(r, 'Surface'),
+                             self._get_text(r, 'ShitekiWord'),
+                             self._get_text(r, 'ShitekiInfo'))
+                      for r in dom.getElementsByTagName('Result')]
+        
+        return self.py2json(results) if json else results  
+        
+        
+class Result(BaseObject):
+    def __init__(self,
+                 start_pos,
+                 length,
+                 surface,
+                 shiteki_word,
+                 shiteki_info):
+        
+        self.start_pos = int(start_pos)
+        self.length = int(length)
+        self.surface = surface
+        self.shiteki_word = shiteki_word
+        self.shiteki_info = shiteki_info
+        
+    def __str__(self):
+        return self.surface.encode('utf8')
---
+++ packages/pyahooapis/pyahooapis-0.2.2dev-py2.5.egg/pyahooapis/service.py
@@ -0,0 +1,60 @@
+#coding: utf-8
+
+import re
+import urllib
+import urllib2
+from xml.dom import minidom
+
+import utils
+
+json = utils.import_json()
+
+
+newline_sub = re.compile('\n').sub
+property_match = re.compile('[a-zA-Z].*').match
+
+class Service(object):
+    
+    def __init__(self, appid, url):
+        self.appid = appid
+        self.url = url
+
+    def _get_text(self, node, tagName):
+        try:
+            return node.getElementsByTagName(tagName)[0].firstChild.nodeValue
+        except:
+            return None
+    
+    def _response(self, params):
+        return urllib2.urlopen(self.url, data='appid=%s&%s' % (self.appid, urllib.urlencode(params))).read()
+        
+    def _setParam(self, params, param, name, split):
+        if param is not None:
+            params[name] = split.join(map(str, param))
+    
+    def _get_dom(self, params):
+        return minidom.parseString(self._remove_newline(self._response(params)))
+        
+    def _remove_newline(self, xml):
+        return newline_sub('', xml)
+    
+    def _binary2list(self, binary, dct):
+        return [dct[key] for key in iter(dct) if binary & key]
+    
+    def _binary2param(self, split, binary, dct):
+        return split.join(map(str, self._binary2list(binary, dct)))
+        
+    def py2json(self, obj):
+        def _py2json(_obj):
+            if isinstance(_obj, list):
+                return [_py2json(_o) for _o in _obj]
+            elif isinstance(_obj, BaseObject):
+                propertys = (p for p in dir(_obj) if property_match(p))
+                return dict((p, _py2json(getattr(_obj, p))) for p in propertys)
+            else:
+                return _obj
+        return json.dumps(_py2json(obj), indent=False)
+                
+class BaseObject(object):
+    pass
+
