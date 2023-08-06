3c79557f55cbe11f6e466c7f8597be5967830cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:35 +0000

    Add package django-activitysync/0.1.2/django-activitysync-0.1.2.tar.gz

---
+++ packages/django-activitysync/django-activitysync-0.1.2.tar.gz/django-activitysync-0.1.2/setup.py
@@ -0,0 +1,30 @@
+"""Setup file for easy installation"""
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+version = __import__('activitysync').__version__
+
+setup(
+    name = 'django-activitysync',
+    version = version,
+    author = 'Dan Carroll',
+    author_email = 'dan@dancarroll.org',
+    description = 'Fast, easy, and extensible social activity aggregation for Django projects',
+    license = 'BSD',
+    keywords = 'django, social, activity, application, twitter, reddit, google reader',
+    url = 'https://bitbucket.org/dancarroll/django-activitysync',
+    download_url = 'https://bitbucket.org/dancarroll/django-activitysync/downloads',
+    packages = find_packages(),
+    long_description = open(join(dirname(__file__), 'README.rst')).read(),
+    classifiers = ['Framework :: Django',
+                   'Development Status :: 4 - Beta',
+                   'Topic :: Internet',
+                   'Intended Audience :: Developers',
+                   'Environment :: Web Environment',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent'],
+)
+
