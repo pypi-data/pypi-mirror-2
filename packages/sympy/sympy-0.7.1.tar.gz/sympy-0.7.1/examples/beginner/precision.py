c89e15a91ca34b05bb0bdcc5fef43cdfe76c6cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:35 +0000

    Add package sympy/0.7.1/sympy-0.7.1.tar.gz

---
+++ packages/sympy/sympy-0.7.1.tar.gz/sympy-0.7.1/examples/beginner/precision.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""Precision Example
+
+Demonstrates SymPy's arbitrary precision abilities
+"""
+
+import sympy
+from sympy import pprint
+
+def main():
+    e = sympy.Rational(2)**50/sympy.Rational(10)**50
+    pprint(e)
+
+if __name__ == "__main__":
+    main()
