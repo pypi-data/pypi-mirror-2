91068ab307fb4abb119cf413b2018d38df3cf56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:39 +0000

    Add package js.jquery_datalink/1.0.0/js.jquery_datalink-1.0.0.tar.gz

---
+++ packages/js.jquery_datalink/js.jquery_datalink-1.0.0.tar.gz/js.jquery_datalink-1.0.0/js/jquery_datalink/__init__.py
@@ -0,0 +1,9 @@
+from fanstatic import Library, Resource
+
+library = Library('jquery_datalink', 'resources')
+
+# Define the resources in the library like this.
+# For options and examples, see the fanstatic documentation.
+# resource1 = Resource(library, 'style.css')
+
+jquery_datalink = Resource(library, 'jquery.datalink.js')
