89d6fc87a242903b12c33ff7a48632ec07d76479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:31 +0000

    Add package vizing/0.0.2/vizing-0.0.2.tar.gz

---
+++ packages/vizing/vizing-0.0.2.tar.gz/vizing-0.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "vizing",
+    packages = ["vizing"],
+    version = "0.0.2",
+    description = "List-colouring of graphs",
+    author = "Matthew Henderson",
+    author_email = "matthew.james.henderson@gmail.com",
+    url = "http://packages.python.org/vizing/",
+    download_url = "http://pypi.python.org/pypi/vizing/0.0.1",
+    keywords = [""],
+    classifiers = [
+        "Programming Language :: Python",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+List-colouring of graphs
+------------------------
+"""
+)
