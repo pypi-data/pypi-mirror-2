4eef674aa7620e31c8b954681bae87a184d98618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:24 +0000

    Add package rpclib/2.0.4-alpha/rpclib-2.0.4-alpha.tar.gz

---
+++ packages/rpclib/rpclib-2.0.4-alpha.tar.gz/rpclib-2.0.4-alpha/src/rpclib/model/binary.py
@@ -0,0 +1,115 @@
+
+#
+# rpclib - Copyright (C) Rpclib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+import base64
+
+from cStringIO import StringIO
+from lxml import etree
+from rpclib.model import nillable_element
+from rpclib.model import nillable_value
+from rpclib.model import nillable_string
+from rpclib.model import ModelBase
+
+class Attachment(ModelBase):
+    __type_name__ = 'base64Binary'
+    __namespace__ = "http://www.w3.org/2001/XMLSchema"
+
+    def __init__(self, data=None, file_name=None):
+        self.data = data
+        self.file_name = file_name
+
+    def save_to_file(self):
+        '''This method writes the data to the specified file.  This method
+        assumes that the file_name is the full path to the file to be written.
+        This method also assumes that self.data is the base64 decoded data,
+        and will do no additional transformations on it, simply write it to
+        disk.
+        '''
+
+        if not self.data:
+            raise Exception("No data to write")
+
+        if not self.file_name:
+            raise Exception("No file_name specified")
+
+        f = open(self.file_name, 'wb')
+        f.write(self.data)
+        f.close()
+
+    def load_from_file(self):
+        '''This method loads the data from the specified file, and does
+        no encoding/decoding of the data
+        '''
+        if not self.file_name:
+            raise Exception("No file_name specified")
+        f = open(self.file_name, 'rb')
+        self.data = f.read()
+        f.close()
+
+    @classmethod
+    @nillable_value
+    def to_parent_element(cls, value, tns, parent_elt, name='retval'):
+        '''This class method takes the data from the attachment and
+        base64 encodes it as the text of an Element. An attachment can
+        specify a file_name and if no data is given, it will read the data
+        from the file
+        '''
+
+        element = etree.SubElement(parent_elt, '{%s}%s' % (tns,name))
+        element.text = base64.encodestring(cls.to_string(value))
+        
+    @classmethod
+    @nillable_element
+    def from_xml(cls, element):
+        '''This method returns an Attachment object that contains
+        the base64 decoded string of the text of the given element
+        '''
+        data = base64.decodestring(element.text)
+        a = Attachment(data=data)
+        return a
+
+    @classmethod
+    @nillable_string
+    def from_string(cls, value):
+        return Attachment(data=value)
+
+    @classmethod
+    @nillable_string
+    def to_string(cls, value):
+        if not (value.data is None):
+            # the data has already been loaded, just encode
+            # and return the element
+            return value.data
+
+        elif not (value.file_name is None):
+            # the data hasn't been loaded, but a file has been
+            # specified
+            data_string = StringIO()
+
+            file_name = value.file_name
+            file = open(file_name, 'rb')
+            base64.encode(file, data_string)
+            file.close()
+
+            # go back to the begining of the data
+            data_string.seek(0)
+            return data_string.read()
+
+        else:
+            raise Exception("Neither data nor a file_name has been specified")
---
+++ packages/rpclib/rpclib-2.0.4-alpha.tar.gz/rpclib-2.0.4-alpha/src/rpclib/test/test_sqla.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+#
+# rpclib - Copyright (C) Rpclib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+import unittest
+
+from rpclib.model.table import TableSerializer
+from rpclib.model.complex import ComplexModel
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+
+from sqlalchemy.orm import mapper
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
+
+class TestSqlAlchemy(unittest.TestCase):
+    def set_up(self):
+        self.metadata = MetaData()
+        self.DeclarativeBase = declarative_base(metadata=self.metadata)
+        self.engine = create_engine('sqlite:///:memory:', echo=True)
+        self.Session = sessionmaker(bind=self.engine)
+
+    setUp=set_up
+
+    def test_declarative(self):
+        from sqlalchemy import Integer
+        from sqlalchemy import String
+
+        class DbObject(TableSerializer,self.DeclarativeBase):
+            __tablename__ = 'db_object'
+
+            id = Column(Integer, primary_key=True)
+            s = Column(String)
+
+        self.metadata.create_all(self.engine)
+
+    def test_mapper(self):
+        import sqlalchemy
+
+        class User(self.DeclarativeBase):
+            __tablename__ = 'user'
+
+            id = Column(sqlalchemy.Integer, primary_key=True)
+            name = Column(sqlalchemy.String(50))
+            addresses = relationship("Address", backref="user")
+
+        class Address(self.DeclarativeBase):
+            __tablename__ = 'address'
+
+            id = Column(sqlalchemy.Integer, primary_key=True)
+            email = Column(sqlalchemy.String(50))
+            user_id = Column(sqlalchemy.Integer, ForeignKey('user.id'))
+
+        self.metadata.create_all(self.engine)
+
+        import rpclib.model.primitive
+
+        class AddressDetail(ComplexModel):
+            id = rpclib.model.primitive.Integer
+            user_name = rpclib.model.primitive.String
+            address = rpclib.model.primitive.String
+
+            @classmethod
+            def mapper(cls, meta):
+                user_t = meta.tables['user']
+                address_t = meta.tables['address']
+
+                cls._main_t = user_t.join(address_t)
+
+                cls._properties = {
+                    'id' : address_t.c.id,
+                    'user_name' : user_t.c.name,
+                    'address' : address_t.c.email,
+                }
+
+                cls._mapper = mapper(cls, cls._main_t,
+                    include_properties=cls._properties.values(),
+                    properties=cls._properties,
+                    primary_key=[address_t.c.id]
+                )
+
+        AddressDetail.mapper(self.metadata)
+
+    #def test_serialize(self):
+    #    raise Exception("Test Something!")
+
+    #def test_deserialize(self):
+    #    raise Exception("Test Something!")
+
+    def test_rpc(self):
+        import sqlalchemy
+        from sqlalchemy import sql
+
+        class KeyValuePair(TableSerializer, self.DeclarativeBase):
+            __tablename__ = 'key_value_store'
+            __namespace__ = 'punk'
+
+            id = Column(sqlalchemy.Integer, primary_key=True)
+            key = Column(sqlalchemy.String(100), nullable=False)
+            value = Column(sqlalchemy.String, nullable=False)
+
+        self.metadata.create_all(self.engine)
+
+        import hashlib
+
+        session = self.Session()
+
+        for i in xrange(1,10):
+            key = str(i)
+            m = hashlib.md5()
+            m.update(key)
+            value = m.hexdigest()
+
+            session.add(KeyValuePair(key=key, value=value))
+
+        session.commit()
+
+        from rpclib.service import ServiceBase
+        from rpclib.decorator import rpc
+        from rpclib.model.complex import Array
+        from rpclib.model.primitive import String
+
+        class Service(ServiceBase):
+            @rpc(String(max_occurs='unbounded'),
+                    _returns=Array(KeyValuePair),
+                    _in_variable_names={
+                        'keys': 'key'
+                    }
+                )
+            def get_values(ctx, keys):
+                session = self.Session()
+
+                return session.query(KeyValuePair).filter(sql.and_(
+                    KeyValuePair.key.in_(keys)
+                )).order_by(KeyValuePair.key)
+
+        from rpclib.application import Application
+        from rpclib.interface.wsdl import Wsdl11
+        from rpclib.protocol.http import HttpRpc
+        from rpclib.protocol.soap import Soap11
+
+        application = Application([Service],
+            interface_class=Wsdl11,
+            in_protocol_class=HttpRpc,
+            out_protocol_class=Soap11,
+            name='Service', tns='tns'
+        )
+
+        from rpclib.server.wsgi import WsgiMethodContext
+
+        ctx = WsgiMethodContext(application,{
+            'QUERY_STRING': 'key=1&key=2&key=3',
+            'PATH_INFO': '/get_values',
+        }, 'some-content-type')
+
+        from rpclib.server import ServerBase
+
+        server = ServerBase(application)
+        server.get_in_object(ctx)
+        server.get_out_object(ctx)
+        server.get_out_string(ctx)
+
+        i = 0
+        for e in ctx.out_document[0][0][0]:
+            i+=1
+            key = str(i)
+            m = hashlib.md5()
+            m.update(key)
+            value = m.hexdigest()
+
+            _key = e.find('{%s}key' % KeyValuePair.get_namespace())
+            _value = e.find('{%s}value' % KeyValuePair.get_namespace())
+
+            print _key, _key.text
+            print _value, _value.text
+
+            self.assertEquals(_key.text, key)
+            self.assertEquals(_value.text, value)
+
+if __name__ == '__main__':
+    unittest.main()
