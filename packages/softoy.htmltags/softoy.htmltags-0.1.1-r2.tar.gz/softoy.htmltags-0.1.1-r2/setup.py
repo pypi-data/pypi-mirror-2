4e498ea35538aaad0967fc836fba0308421a3d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:07 +0000

    Add package softoy.htmltags/0.1.1-r2/softoy.htmltags-0.1.1-r2.tar.gz

---
+++ packages/softoy.htmltags/softoy.htmltags-0.1.1-r2.tar.gz/softoy.htmltags-0.1.1-r2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.1-r2'
+
+setup(name='softoy.htmltags',
+      version=version,
+      description="",
+      long_description="""\
+        """,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Euh Ton-jae',
+      author_email='',
+      url='',
+      license='BSD',
+      package_dir = {'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['softoy',],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'softoy.tree',
+        'webhelpers',
+      ],
+      entry_points="""
+      """,
+      )
