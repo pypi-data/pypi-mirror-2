be57611af55e46732d0c12915701937a59b7fd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:19 +0000

    Add package collective.easyslider/0.6.2rc1/collective.easyslider-0.6.2rc1.zip

---
+++ packages/collective.easyslider/collective.easyslider-0.6.2rc1.zip/collective.easyslider-0.6.2rc1/collective/easyslider/browser/base.py
@@ -0,0 +1,59 @@
+
+
+class AbstractSliderView(object):
+    """
+    must have settings attribute specified
+    """
+    
+    def css(self):
+        return """
+#slider-container.slider-%(uid)s{
+    width: %(width)ipx;
+    height: %(height)ipx;
+    margin: %(centered)s;
+}
+#slider.slider-%(uid)s, #slider.slider-%(uid)s li.slide{ 
+    width:%(width)ipx;
+    height:%(height)ipx;
+}
+.slider-%(uid)s #nextBtn{ 
+    left:%(width)ipx;
+    top:-%(next_top)ipx
+}
+.slider-%(uid)s #prevBtn{
+	top:-%(prev_top)ipx;
+}	
+    """ % {
+                'width' : self.settings.width,
+                'height' : self.settings.height,
+                'next_top' : ((self.settings.height/2) + 75) + 50,
+                'prev_top' : ((self.settings.height/2) + 50),
+                'centered' : self.settings.centered and 'auto' or '0',
+                'uid' : self.context.UID()
+            }
+        
+    def js(self):
+        return """
+jQuery(document).ready(function(){	
+	jQuery("#slider.slider-%(uid)s").easySlider({
+	    speed : %(speed)i,
+	    vertical: %(vertical)s,
+	    auto : %(auto)s,
+	    pause : %(pause)i,
+	    continuous : %(continuous)s,
+	    navigation_type: '%(navigation_type)s',
+	    effect: '%(effect)s',
+	    fadeNavigation: %(fade_navigation)s
+	});
+});
+        """ % {
+            'speed' : self.settings.speed,
+            'vertical' : str(self.settings.vertical).lower(),
+            'auto' : str(self.settings.auto).lower(),
+            'pause' : self.settings.pause,
+            'continuous' : str(self.settings.continuous).lower(),
+            'navigation_type' : self.settings.navigation_type,
+            'effect' : self.settings.effect,
+            'fade_navigation' : str(self.settings.fade_navigation).lower(),
+            'uid' : self.context.UID()
+        }
