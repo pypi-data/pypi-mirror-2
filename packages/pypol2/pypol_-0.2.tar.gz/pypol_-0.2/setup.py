3f88c8c880a6b6e3bf97d817bf6b9b06654cef1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:48 +0000

    Add package pypol2/0.2/pypol_-0.2.tar.gz

---
+++ packages/pypol2/pypol_-0.2.tar.gz/pypol_-0.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(name='pypol_',
+      version='0.2',
+      description='Python polynomial library',
+      author='Michele Lacchia',
+      author_email='michelelacchia@gmail.com',
+      license='GNU GPL v3',
+      url='http://pypol.altervista.org/',
+      download_url='http://github.com/rubik/pypol/downloads/',
+      packages=['pypol'],
+      package_dir = {'pypol': 'src',
+                     'pypol.tests': 'src/tests'},
+      package_data = {'pypol':
+                                ['tests/*.py',
+                                'doc/*.rst',
+                                'doc/conf.py',
+                                'doc/Makefile',
+                                'doc/make.bat',
+                                'doc/_static/*.*',
+                                ]
+                    },
+      classifiers=['Topic :: Scientific/Engineering :: Mathematics',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.6',
+                   ],
+      )
