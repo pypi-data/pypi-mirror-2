0d493c484df1c1ba936bc7622d698582d920b136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:36 +0000

    Add package whistler/1.1/whistler-1.1.tar.gz

---
+++ packages/whistler/whistler-1.1.tar.gz/whistler-1.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+
+WHISTLER_VERSION="1.1"
+
+
+def get_file_contents(filename):
+    fd = file(path.join(path.dirname(__file__), filename), "r")
+    content = fd.read()
+    fd.close()
+    return content
+
+setup(
+    name = "whistler",
+    version = WHISTLER_VERSION,
+    description = "An extensible MUC bot for XMPP.",
+    long_description=get_file_contents("README.rst"),
+    author='Andres J. Diaz',
+    author_email='ajdiaz@connectical.com',
+    url='http://github.com/ajdiaz/whistler',
+    packages=find_packages(),
+    install_requires=[ "xmpppy >= 0.5.0rc1" ],
+    entry_points={
+        'console_scripts': [
+            'whistler = whistler.scripts:main'
+        ]
+    },
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Chat',
+    ],
+)
