d9b3a18ac3bdf6fdb5c200b7528f258eddc9147d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:13 +0000

    Add package instayc/0.1.0/instayc-0.1.0.tar.gz

---
+++ packages/instayc/instayc-0.1.0.tar.gz/instayc-0.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+import os, sys
+
+version = '0.1.0'
+
+install_requires = [
+    "instapaperlib>=0.4.0",
+    "feedparser>=5.0.1",
+]
+
+setup(
+    name="instayc",
+    version=version,
+    description="Python tool for auto adding hacker news articles to\
+    instapaper.",
+    long_description=(open("./README.rst").read()),
+    author="Jarod Luebbert",
+    author_email="jarodluebbert@gmail.com",
+    url="http://github.com/jarodl/instayc",
+    packages=["instayc"],
+    scripts=["bin/instayc"],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=install_requires,
+    license="MIT",
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    )
+)
