72e1c35130df5b0532bf764b2bedec10c8e3a6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:29 +0000

    Add package collective.cultureRelations/0.3/collective.cultureRelations-0.3.tar.gz

---
+++ packages/collective.cultureRelations/collective.cultureRelations-0.3.tar.gz/collective.cultureRelations-0.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.3'
+
+setup(name='collective.cultureRelations',
+      version=version,
+      description="A setup for collective.viewlet.references that configures it for cultural institutions according to the INTK Online Strategy",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone setup references culture',
+      author='David Jonas',
+      author_email='david@itsnotthatkind.org',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'collective.viewlet.references',
+          'collective.contentleadimage',
+          'collective.flowplayer',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
