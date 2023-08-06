0ea14e7e13c858d7a0b7edfe24b26e50f8a24731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:09 +0000

    Add package throw/0.1-2-g9032/throw-0.1-2-g9032.tar.gz

---
+++ packages/throw/throw-0.1-2-g9032.tar.gz/throw-0.1-2-g9032/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+import version
+
+setup(
+    name = "throw",
+    version = version.get_git_version(),
+    packages = find_packages(),
+    scripts = ['throw'],
+
+    install_requires = ['argparse'],
+
+    # metadata for upload to PyPI
+    author = "Rich Wareham",
+    author_email = "rjw57@cam.ac.uk",
+    description = "Simply share files from the command line",
+    license = "Apache 2.0",
+    keywords = "file sharing throw email",
+    url = "http://github.com/rjw57/throw",
+)
