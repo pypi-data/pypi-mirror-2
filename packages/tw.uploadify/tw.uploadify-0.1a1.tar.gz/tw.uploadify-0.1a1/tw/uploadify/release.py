634ba378ddd6ed27520de1b8ae18f03c79dfb4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:06 +0000

    Add package tw.uploadify/0.1a1/tw.uploadify-0.1a1.tar.gz

---
+++ packages/tw.uploadify/tw.uploadify-0.1a1.tar.gz/tw.uploadify-0.1a1/tw/uploadify/release.py
@@ -0,0 +1,11 @@
+# Metadata for the widget EGG. Should leave it for the (unimplemented) widget
+# tracker to track it
+
+__DISTRIBUTION__ = 'tw.uploadify'
+__DESCRIPTION__ = 'upload files with progress informations'
+__URL__ = ''
+__VERSION__ = '0.1a1'
+__AUTHOR__ = 'Nicolas Laurance'
+__EMAIL__ = 'nicolas[dot]laurance[at]gmail[dot]com'
+__COPYRIGHT__ = "Copyright 2011 Nicolas Laurance"
+__LICENSE__ = 'MIT'
