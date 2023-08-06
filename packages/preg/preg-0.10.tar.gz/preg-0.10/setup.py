c63bcd8e22adbad56e8e57c9f551b8d94e2276d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:51 +0000

    Add package preg/0.10/preg-0.10.tar.gz

---
+++ packages/preg/preg-0.10.tar.gz/preg-0.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='preg',
+  version='0.10',
+  author = "Andrew Magee",
+  author_email = "amagee@gmail.com",
+  packages=find_packages(),
+  license='GPL',
+  long_description="Python-customisable grep-like command-line searcher.",
+)
