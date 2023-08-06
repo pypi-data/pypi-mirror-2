40364d5f4d694e103afd629a7f210f97b37b4c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:13 +0000

    Add package pendrell/0.3.6/pendrell-0.3.6-py2.6.egg

---
+++ packages/pendrell/pendrell-0.3.6-py2.6.egg/pendrell/_version.py
@@ -0,0 +1,25 @@
+try:
+  from twisted.python.versions import Version
+
+except ImportError:
+  class Version(object):
+    def __init__(self, package, major, minor, nano, pre=None):
+      self.package = package
+      self.major = major
+      self.minor = minor
+      self.nano = nano
+      self.pre = pre
+
+    def short(self):
+      fmt = "{0.major}.{0.minor}.{0.nano}"
+      if self.pre:
+        fmt += "pre{0.pre}"
+      return fmt.format(self)
+
+
+copyright = """Copyright (c) 2008-2010 Oliver V. Gould.  All rights reserved."""
+version = Version("pendrell", 0, 3, 6)
+
+# Don't export t.p.v.Version
+del Version
+
