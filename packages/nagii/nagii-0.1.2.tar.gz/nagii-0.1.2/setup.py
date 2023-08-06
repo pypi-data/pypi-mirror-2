7bf38737e93acc45497cdca792506d9586cf727d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:47 +0000

    Add package nagii/0.1.2/nagii-0.1.2.tar.gz

---
+++ packages/nagii/nagii-0.1.2.tar.gz/nagii-0.1.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# -*- mode: python; sh-basic-offset: 4; indent-tabs-mode: nil; coding: utf-8 -*-
+# vim: tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8
+
+import setuptools
+
+
+VERSION = '0.1.2'
+
+setuptools.setup(
+    name = 'nagii',
+    version = VERSION,
+    author = 'Jorge Gallegos',
+    author_email = 'kad@blegh.net',
+    description = 'A straightforward Nagios modelling library',
+    install_requires = [
+        'mako',
+        'markdown2',
+    ],
+    packages = setuptools.find_packages('src'),
+    package_dir = { '': 'src' },
+    zip_safe = True,
+    keywords = 'nagios',
+    url = 'https://github.com/thekad/nagii',
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.4',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Monitoring',
+    ],
+)
