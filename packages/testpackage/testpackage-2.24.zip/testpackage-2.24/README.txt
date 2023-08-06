adcd7738f7141990b566a9af8591297475ad170d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:53 +0000

    Add package testpackage/2.24/testpackage-2.24.zip

---
+++ packages/testpackage/testpackage-2.24.zip/testpackage-2.24/README.txt
@@ -0,0 +1,5 @@
+============
+Test Package
+============
+
+This is a test package. It has ÜTF-8 chäråctêrs in its README – and an mdash.
---
+++ packages/testpackage/testpackage-2.24.zip/testpackage-2.24/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = '2.24'
+
+setup(name='testpackage',
+      version=version,
+      description='Test package.',
+      long_description=open('README.txt').read(),
+      classifiers=[
+          'Programming Language :: Python',
+      ],
+      keywords='test package tønsberg',
+      author='Jarn AS',
+      author_email='info@jarn.com',
+      url='http://www.jarn.com/',
+      license='private',
+      packages = ['testpackage'],
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+)
