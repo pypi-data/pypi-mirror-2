46f54d82b943899d5478e0c574e62a8fe465490d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:06 +0000

    Add package collective.rtvideo.vimeo/0.1.0/collective.rtvideo.vimeo-0.1.0.tar.gz

---
+++ packages/collective.rtvideo.vimeo/collective.rtvideo.vimeo-0.1.0.tar.gz/collective.rtvideo.vimeo-0.1.0/README.txt
@@ -0,0 +1,14 @@
+Introduction
+============
+
+This is an add-on adapter for `RedTurtle Video`__ product for Plone. For additional documentation see
+the main product's page.
+
+Add this to your Plone installation for beeing able to use `Vimeo`__ video link as valid
+URLs for "Video link" content type.
+
+__ http://plone.org/products/redturtle.video
+__ http://www.vimeo.com/
+
+
+
---
+++ packages/collective.rtvideo.vimeo/collective.rtvideo.vimeo-0.1.0.tar.gz/collective.rtvideo.vimeo-0.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='collective.rtvideo.vimeo',
+      version=version,
+      description="The Vimeo Plone support for RedTurtle Video",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Multimedia :: Video",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        ],
+      keywords='plone plonegov video embed vimeo',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://plone.org/products/redturtle.video',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.rtvideo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'redturtle.video>=0.6.0',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
