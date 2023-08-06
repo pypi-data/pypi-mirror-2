c504fecd3ae6401b3adb5968b9d9e3732ddbda7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:29 +0000

    Add package hiredis/0.0.1/hiredis-0.0.1.tar.gz

---
+++ packages/hiredis/hiredis-0.0.1.tar.gz/hiredis-0.0.1/src/reader.h
@@ -0,0 +1,14 @@
+#ifndef __READER_H
+#define __READER_H
+
+#include "hiredis.h"
+
+typedef struct {
+    PyObject_HEAD
+    void *reader;
+} hiredis_ReaderObject;
+
+extern PyTypeObject hiredis_ReaderType;
+extern redisReplyObjectFunctions hiredis_ObjectFunctions;
+
+#endif
