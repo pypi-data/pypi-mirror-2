6a5820f3f821c2bd820c4f0907b1b6aaebc8230b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:28 +0000

    Add package redgreen/0.0.7/redgreen-0.0.7-py2.6.egg

---
+++ packages/redgreen/redgreen-0.0.7-py2.6.egg/redgreen/file_change_iterator.py
@@ -0,0 +1,16 @@
+import time
+from .file_status import FileStatus
+
+class FileChangeIterator(object):
+    def __init__(self, filename):
+        super(FileChangeIterator, self).__init__()
+        self.filename = filename
+    def __iter__(self):
+        last_status = None
+        while True:
+            new_status = FileStatus(self.filename)
+            if new_status != last_status:
+                yield self.filename
+            else:
+                yield None
+            last_status = new_status
