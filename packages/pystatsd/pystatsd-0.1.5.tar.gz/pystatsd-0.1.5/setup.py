e4c6c90b8690fedeb891e086ec5ad35c4ddc551c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:30 +0000

    Add package pystatsd/0.1.5/pystatsd-0.1.5.tar.gz

---
+++ packages/pystatsd/pystatsd-0.1.5.tar.gz/pystatsd-0.1.5/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pystatsd",
+    version = "0.1.5",
+    author = "Steve Ivy",
+    author_email = "steveivy@gmail.com",
+    description = ("pystatsd is a client for Etsy's statsd server, a front end/proxy for the Graphite stats collection and graphing server."),
+    url='https://github.com/sivy/py-statsd',
+    license = "BSD",
+    packages=['pystatsd'],
+    install_requires=['argparse >= 1.2'],
+    long_description=read('README'),
+    requires=['argparse'],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+    ],
+    scripts=['bin/pystatd-server']
+)
