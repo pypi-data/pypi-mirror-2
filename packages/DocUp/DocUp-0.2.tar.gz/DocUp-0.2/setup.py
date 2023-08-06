ab8ae4acbc6d900953de68de67d1aa1578b92b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:01 +0000

    Add package DocUp/0.2/DocUp-0.2.tar.gz

---
+++ packages/DocUp/DocUp-0.2.tar.gz/DocUp-0.2/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+setup(name='DocUp',
+      description='Documentation Uploader',
+      author='Tony Bashi',
+      install_requires=[
+       "markdown>=2.0.3",
+        ],
+      author_email='bashia@uvic.ca',
+      version='0.2',
+      py_modules=['DocUp'],
+      )
+
