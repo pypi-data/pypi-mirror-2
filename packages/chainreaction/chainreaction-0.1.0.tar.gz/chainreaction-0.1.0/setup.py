ea37e8f08cadbba47817b9154c85550aaed8d649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:01 +0000

    Add package chainreaction/0.1.0/chainreaction-0.1.0.tar.gz

---
+++ packages/chainreaction/chainreaction-0.1.0.tar.gz/chainreaction-0.1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+from sys import version
+if version < '2.7.0':
+    raise Exception("This module doesn't support any version less than 2.7")
+
+import sys
+sys.path.append("./test")
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    "Programming Language :: Python",
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.0',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+
+setup(
+    author='Keita Oouchi',
+    author_email='keita.oouchi@gmail.com',
+    url='https://github.com/ghostreet/chainreaction',
+    name='chainreaction',
+    version='0.1.0',
+    package_dir={"":"src"},
+    packages=['chainreaction'],
+    test_suite="test_reactor.suite",
+    license='BSD License',
+    classifiers=classifiers,
+    description='Wrapper module which provides iterable object with infix style method chaining.',
+    long_description=open('README.rst').read(),
+)
