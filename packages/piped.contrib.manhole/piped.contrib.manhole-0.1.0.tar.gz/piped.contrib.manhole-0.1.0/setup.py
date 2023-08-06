3c2dc0c95de813b44c263bc43ec7e8af911260d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:05 +0000

    Add package piped.contrib.manhole/0.1.0/piped.contrib.manhole-0.1.0.tar.gz

---
+++ packages/piped.contrib.manhole/piped.contrib.manhole-0.1.0.tar.gz/piped.contrib.manhole-0.1.0/setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2010-2011, Found IT A/S and Piped Project Contributors.
+# See LICENSE for details.
+import os
+
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name = 'piped.contrib.manhole',
+    license = 'MIT',
+
+    author = 'Found IT A/S',
+    author_email = 'developers@piped.io',
+    url = 'http://piped.io',
+
+    packages = find_packages(where=here),
+    namespace_packages = ['piped', 'piped.contrib'],
+
+    version = '0.1.0',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Framework :: Twisted',
+        'Operating System :: OS Independent',
+    ],
+    description = 'Twisted Manhole support for Piped.',
+
+    install_requires = ['piped', 'pyasn1', 'pycrypto', 'setuptools']
+)
