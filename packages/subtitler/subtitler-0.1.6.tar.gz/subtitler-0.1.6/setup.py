e22274f68018d31a806a8370ae4060bb5abaaee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:58 +0000

    Add package subtitler/0.1.6/subtitler-0.1.6.tar.gz

---
+++ packages/subtitler/subtitler-0.1.6.tar.gz/subtitler-0.1.6/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "subtitler",
+    version = "0.1.6",
+    author = "Adam Strauch",
+    author_email = "cx@initd.cz",
+    description = ("Tool/library for searching subtitles by hash or typed query."),
+    license = "BSD",
+    keywords = "subtitle subtitles",
+    url = "https://github.com/creckx/Subtitler",
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    long_description="Subtitler is small tool for downloading subtitles from opensubtitles.org. Contain simple library for XML-RPC API.",#read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+        
+        ],
+    entry_points="""
+    [console_scripts]
+    subtitler = subtitler.subtitler:main
+    """
+)
