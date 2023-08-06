bd4fd34d4cfa64c89b6b4adb3f590f7926619969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:01 +0000

    Add package atreal.massloader/3.0.0/atreal.massloader-3.0.0.zip

---
+++ packages/atreal.massloader/atreal.massloader-3.0.0.zip/atreal.massloader-3.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '3.0.0'
+
+setup(name='atreal.massloader',
+      version=version,
+      description="Add an action to import zip files.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='atReal',
+      author_email='contact@atreal.net',
+      url='https://svn.plone.org/svn/collective/atreal.massloader/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['atreal'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'pylzma',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
