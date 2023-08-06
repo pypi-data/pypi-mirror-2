5d387b0ce80fd9d7f83bade7a38101d2bf94d572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:35 +0000

    Add package qtalchemy/0.6.10/qtalchemy-0.6.10-py2.6.egg

---
+++ packages/qtalchemy/qtalchemy-0.6.10-py2.6.egg/qtalchemy/dialogs/objectforms.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+from qtalchemy import MapperMixin, user_message, LayoutLayout, LayoutWidget, Message
+from PyQt4 import QtCore, QtGui
+from sqlalchemy.orm import mapper, create_session, relation, object_session, Query
+import traceback
+import sys
+
+def MessageButtonsToQt(flags):
+    result = QtGui.QMessageBox.NoButton
+    for x in "Ok Cancel Yes No".split(' '):
+        if flags & getattr(Message, x):
+            result |= getattr(QtGui.QMessageBox, x)
+    return result
+
+def QtToMessageButtons(flags):
+    result = 0
+    for x in "Ok Cancel Yes No".split(' '):
+        if flags & getattr(QtGui.QMessageBox, x):
+            result |= getattr(Message, x)
+    return result
+
+
+class SessionMessageBox(object):
+    """
+    This class is a callable to hook to a session message handler to display 
+    messages for this session.
+    """
+    def __init__(self, parent=None):
+        self.parent = parent
+
+    def __call__(self, session, instance, flags, msg):
+        # TODO:  pass icon
+        # TODO:  pass exception info
+        # TODO:  pass additional buttons
+        # TODO:  allow data entry on instance (??) -- augment the message box with a bound data form
+        msgBox = QtGui.QMessageBox(self.parent)
+        msgBox.setWindowTitle(QtGui.QApplication.applicationName())
+        #if icon is not None:
+        #    msgBox.setIcon( icon )
+        msgBox.setText( msg )
+        
+        msgBox.setStandardButtons(MessageButtonsToQt(flags))
+        return QtToMessageButtons(msgBox.exec_())
+
+def exception_message_box(e,text,parent=None,icon=None):
+    """
+    Extract exception information and display a message box with exception details in 
+    a details extension of the message.
+    """
+    msg = QtGui.QMessageBox(parent)
+    msg.setWindowTitle(QtGui.QApplication.applicationName())
+    if icon is not None:
+        msg.setIcon( icon )
+    msg.setText( text )
+    msg.setInformativeText( user_message(e) )
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    msg.setDetailedText( ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback)) )
+    msg.setStandardButtons( QtGui.QMessageBox.Ok )
+    return msg.exec_()
+
+def messaged_commit(session, parent):
+    """
+    This helper function displays a message to the user if the commit on the session fails.
+    
+    The message text is given by the user_message function.
+    """
+    try:
+        session.commit()
+    except Exception, e:
+        exception_message_box(e,"There was an error saving the data.",parent=parent,icon=QtGui.QMessageBox.Warning)
+
+class MapperDialog(QtGui.QDialog, MapperMixin):
+    def __init__(self,parent):
+        QtGui.QDialog.__init__(self,parent)
+
+    def accept(self):
+        self.submit()
+        QtGui.QDialog.accept(self)
+
+class BoundDialog(MapperDialog):
+    def __init__(self,parent,session=None,flush=True):
+        MapperDialog.__init__(self,parent)
+        self.flush_on_close = flush
+        self.session = session
+
+    def init_simple_session(self,row,ssrc,row_id,table_type,key_col):
+        # standard bit of init code to get a data session going
+        if row is not None:
+            self.main_row = row
+            self.session=object_session(row)
+        elif ssrc is not None:
+            self.session=ssrc()
+            if row_id is None:
+                self.main_row = table_type()
+                self.session.add(self.main_row)
+            else:
+                self.main_row = self.session.query(table_type).filter(key_col==row_id).one()
+        else:
+            assert False, "One of 'row' or 'ssrc' must be valid."
+
+    def accept(self):
+        MapperDialog.submit(self)
+        try:
+            if self.flush_on_close:
+                self.flush()
+                self.session.close()
+            QtGui.QDialog.accept(self)
+        except Exception, e:
+            self.session.rollback()
+            exception_message_box(e,"There was an error saving data.",icon=QtGui.QMessageBox.Warning,parent=self)
+
+    def reject(self):
+        MapperDialog.reject(self)
+        if self.flush_on_close and self.session is not None:
+            self.session.close()
+
+    def flush(self):
+        assert self.session is not None, "If flushing changes, we must have a session"
+        self.session.commit()
+
+def BoundDialogObject(parent,row):
+    b = BoundDialog(parent)
+    b.init_simple_session(row,None,None,None,None)
+    b.setWindowTitle(row.DialogTitle)
+
+    main = QtGui.QVBoxLayout()
+    b.setLayout(main)
+    grid = LayoutLayout(main,QtGui.QFormLayout())
+
+    m = b.mapClass(type(row))
+    for col in row.DialogVisualElements:
+        m.addBoundField(grid,col)
+    m.connect_instance(row)
+
+    buttonbox = LayoutWidget(main,QtGui.QDialogButtonBox())
+    buttonbox.addButton(QtGui.QDialogButtonBox.Ok)
+    buttonbox.addButton(QtGui.QDialogButtonBox.Cancel)
+    buttonbox.accepted.connect(b.accept)
+    buttonbox.rejected.connect(b.reject)
+
+    b.show()
+    b.exec_()
---
+++ packages/qtalchemy/qtalchemy-0.6.10-py2.6.egg/qtalchemy/widgets/table.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#       Copyright (C) 2010, Joel B. Mohler <joel@kiwistrawberry.us>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#                  http://www.gnu.org/licenses/
+##############################################################################
+
+from PyQt4 import QtCore, QtGui
+from qtalchemy import fromQType,  writeTableColumnGeo, readTableColumnGeo
+
+class TableView(QtGui.QTableView):
+    """
+    The TableView provides a QTableView override with support for the following elements:
+
+    - row deletion with ctrl+delete for editable models
+    - tab skipping read-only columns for editable models
+    - clipboard copy of the selected blocks (actually an enclosing rectangle of the selection)
+    - load & save column geometry from user settings
+
+    :param parent:  Qt parent widget for this table
+    :param extensionId:  settings location for saving column geometry
+    
+    Note:  You are encouraged to use the extensionId parameter 
+    in setModel rather than this one.  The setModel function determines 
+    the available columns so that is a better point to specify the 
+    related concern of saving column geometry.
+    """
+    def __init__(self, parent=None, extensionId=None):
+        QtGui.QTableView.__init__(self,parent)
+        self.use_edit_tab_semantics = False
+        self.setProperty("ExtensionId", extensionId)
+
+        self.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
+        self.setEditTriggers(QtGui.QAbstractItemView.AllEditTriggers)
+
+        # hook up header events to save section widths
+        header = self.horizontalHeader()
+        header.sectionResized.connect(self.saveSections)
+        header.sectionAutoResize.connect(self.saveSections2)
+
+    def setModel(self, model, extensionId=None):
+        """
+        :param model:  Qt model to connect to this table
+        :param extensionId:  settings location for saving column geometry
+        """
+        if extensionId is not None:
+            # clobber the parameter to __init__ if specified non-None here
+            self.setProperty("ExtensionId", extensionId)
+
+        QtGui.QTableView.setModel(self, model)
+        # hook up model resets to preserve selection across reloads
+        self.model().modelAboutToBeReset.connect(self.preReset)
+        self.model().modelReset.connect(self.postReset)
+        if self.property("ExtensionId") is not None:
+            readTableColumnGeo(self, self.property("ExtensionId"))
+
+    def keyPressEvent(self, event):
+        """
+        * Ctrl+Delete should delete the row.
+        * Copy key should copy selection 
+        """
+        if event.modifiers() & QtCore.Qt.ControlModifier and event.key() == QtCore.Qt.Key_Delete:
+            model = self.model()
+            index = self.currentIndex()
+            
+            # We'll just call removeRows.  If the model doesn't support it, it returns False
+            model.removeRows(index.row(),1,index.parent())
+            
+            #print "deleting"
+        elif event.matches(QtGui.QKeySequence.Copy):
+            # We copy out the smallest rectangular array encompassing the selected indexes.
+            model = self.model()
+            selected = self.selectedIndexes()
+            if len(selected):
+                parent = selected[0].parent()
+                max_row = min_row = selected[0].row()
+                max_column = min_column = selected[0].column()
+                for i in selected:
+                    if i.row() > max_row: max_row = i.row()
+                    if i.row() < min_row: min_row = i.row()
+                    if i.column() > max_column: max_column = i.column()
+                    if i.column() < min_column: min_column = i.column()
+                rows = ['\t'.join([model.headerData(x,QtCore.Qt.Horizontal,QtCore.Qt.DisplayRole) for x in range(min_column,max_column+1)])]
+                for y in range(min_row,max_row+1):
+                    rows.append('\t'.join([str(fromQType(model.data(model.createIndex(y,x,parent),QtCore.Qt.DisplayRole))) for x in range(min_column,max_column+1)]))
+                cb = QtGui.QApplication.instance().clipboard()
+                m = QtCore.QMimeData()
+                m.setData("text/plain",'\n'.join(rows))
+                cb.setMimeData(m)
+        else:
+            QtGui.QTableView.keyPressEvent(self,event)
+
+    def nextIndex(self,index):
+        model = self.model()
+        if index.column()+1 < model.columnCount(index.parent()):
+            return model.createIndex(index.row(),index.column()+1,index.parent())
+        elif index.row()+1 < model.rowCount(index.parent()):
+            return model.createIndex(index.row()+1,0,index.parent())
+        else:
+            return model.createIndex(0,0,index.parent())
+
+    def prevIndex(self,index):
+        model = self.model()
+        if index.column() > 0:
+            return model.createIndex(index.row(),index.column()-1,index.parent())
+        elif index.row() > 0:
+            return model.createIndex(index.row()-1,model.columnCount(index.parent())-1,index.parent())
+        else:
+            return model.createIndex(index.rowCount(index.parent())-1,model.columnCount(index.parent())-1,index.parent())
+
+    def edit(self, index, trigger, event):
+        # At this point this method is overridden strictly as a place to set the flag to initiate the 
+        # semantics of moveCursor to skip read-only cells in tabbing.
+        inherited = QtGui.QTableView.edit(self, index, trigger, event)
+        if inherited:
+            self.use_edit_tab_semantics = True
+        return inherited
+
+    def moveCursor(self, cursorAction, modifiers):
+        if self.use_edit_tab_semantics == True:
+            # Skip read-only cells.
+            if cursorAction == QtGui.QAbstractItemView.MoveNext:
+                index = self.currentIndex()
+                model = self.model()
+                while True:
+                    candidate = self.nextIndex(index)
+                    if (model.flags(candidate) & QtCore.Qt.ItemIsEditable) == QtCore.Qt.ItemIsEditable:
+                        return candidate
+                    index = candidate
+            elif cursorAction == QtGui.QAbstractItemView.MovePrevious:
+                index = self.currentIndex()
+                model = self.model()
+                while True:
+                    candidate = self.prevIndex(index)
+                    if (model.flags(candidate) & QtCore.Qt.ItemIsEditable) == QtCore.Qt.ItemIsEditable:
+                        return candidate
+                    index = candidate
+        return QtGui.QTableView.moveCursor(self,cursorAction,modifiers)
+
+    def saveSections(self, logicalIndex, oldSize, newSize):
+        if self.property("ExtensionId") is not None:
+            writeTableColumnGeo(self, self.property("ExtensionId"))
+
+    def saveSections2(self, logicalIndex, mode):
+        if self.property("ExtensionId") is not None:
+            writeTableColumnGeo(self, self.property("ExtensionId"))
+
+    def preReset(self):
+        index = self.selectionModel().currentIndex()
+        if index is not None and index.isValid():
+            oc = self.model().objectConverter
+            self.preserved_id = oc(index.internalPointer())
+
+    def postReset(self):
+        if hasattr(self, "preserved_id"):
+            oc = self.model().objectConverter
+            for i in range(self.model().rowCount(QtCore.QModelIndex())):
+                if oc(self.model().index(i, 0, QtCore.QModelIndex()).internalPointer()) == self.preserved_id:
+                    self.setCurrentIndex(self.model().index(i, 0, QtCore.QModelIndex()))
+                    break
