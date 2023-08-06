c7b658349d307fdc508dea182b179778401f9fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:35 +0000

    Add package pysrt/0.3.1/pysrt-0.3.1.tar.gz

---
+++ packages/pysrt/pysrt-0.3.1.tar.gz/pysrt-0.3.1/pysrt/__init__.py
@@ -0,0 +1,5 @@
+VERSION = (0, 3, 1)
+from pysrt.srttime import SubRipTime
+from pysrt.srtitem import SubRipItem
+from pysrt.srtfile import SubRipFile
+from pysrt.srtexc import InvalidItem, InvalidTimeString
