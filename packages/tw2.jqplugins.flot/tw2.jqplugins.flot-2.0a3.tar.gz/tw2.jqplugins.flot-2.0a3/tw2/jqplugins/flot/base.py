f8f3f3fa1238f6d1d6badb49f384a46eae071b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:45 +0000

    Add package tw2.jqplugins.flot/2.0a3/tw2.jqplugins.flot-2.0a3.tar.gz

---
+++ packages/tw2.jqplugins.flot/tw2.jqplugins.flot-2.0a3.tar.gz/tw2.jqplugins.flot-2.0a3/tw2/jqplugins/flot/base.py
@@ -0,0 +1,11 @@
+
+import tw2.core as twc
+import tw2.jquery.base as twjq_c
+import defaults
+
+flot_js = twc.JSLink(
+    modname=__name__,
+    filename='static/jqplugins/flot/0.7/jquery.flot.min.js'
+)
+
+__all__ = ['flot_js']
