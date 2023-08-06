515c8e2d903fda47c8f2047ca71022a3b202e724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:20 +0000

    Add package queued_search/1.0.3/queued_search-1.0.3.tar.gz

---
+++ packages/queued_search/queued_search-1.0.3.tar.gz/queued_search-1.0.3/queued_search/__init__.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+
+__author__ = 'Daniel Lindsley'
+__version__ = (1, 0, 3)
+
+
+def get_queue_name():
+    """
+    Standized way to fetch the queue name.
+    
+    Can be overridden by specifying ``SEARCH_QUEUE_NAME`` in your settings.
+    
+    Given that the queue name is used in disparate places, this is primarily
+    for sanity.
+    """
+    return getattr(settings, 'SEARCH_QUEUE_NAME', 'haystack_search_queue')
