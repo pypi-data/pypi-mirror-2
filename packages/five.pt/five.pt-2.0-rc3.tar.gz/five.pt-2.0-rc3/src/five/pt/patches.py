795eb297d2ba9c93b83e007a5cc3dac0cbbd9b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:12 +0000

    Add package five.pt/2.0-rc3/five.pt-2.0-rc3.tar.gz

---
+++ packages/five.pt/five.pt-2.0-rc3.tar.gz/five.pt-2.0-rc3/src/five/pt/patches.py
@@ -0,0 +1,176 @@
+"""Monkey-patching page template classes.
+
+Since many templates are instantiated at module-import, we patch using
+a duck-typing strategy.
+
+We replace the ``__get__``-method of the ViewPageTemplateFile class
+(both the Five variant and the base class). This allows us to return a
+Chameleon template instance, transparent to the calling class.
+"""
+
+from zope.app.pagetemplate.viewpagetemplatefile import (
+    ViewPageTemplateFile as ZopeViewPageTemplateFile)
+
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from Products.PageTemplates.PageTemplateFile import PageTemplate
+from Products.PageTemplates.ZopePageTemplate import ZopePageTemplate
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile as \
+     FiveViewPageTemplateFile
+
+from five.pt.pagetemplate import ViewPageTemplateFile
+from five.pt.pagetemplate import BaseTemplate
+from five.pt.pagetemplate import BaseTemplateFile
+from five.pt.pagetemplate import EXTRA_CONTEXT_KEY
+
+from Acquisition import aq_base
+from Acquisition import aq_parent
+from Acquisition.interfaces import IAcquirer
+from Acquisition import ImplicitAcquisitionWrapper
+
+from ComputedAttribute import ComputedAttribute
+from AccessControl.SecurityInfo import ClassSecurityInfo
+from App.class_init import InitializeClass
+
+# declare Chameleon's repeatdict public
+from chameleon.tal import RepeatDict
+
+RepeatDict.security = ClassSecurityInfo()
+RepeatDict.security.declareObjectPublic()
+RepeatDict.__allow_access_to_unprotected_subobjects__ = True
+
+InitializeClass(RepeatDict)
+
+try:
+    from Products.Five.browser.pagetemplatefile import BoundPageTemplate
+except ImportError:
+    from zope.app.pagetemplate.viewpagetemplatefile import BoundPageTemplate
+    import Acquisition
+
+    class BoundPageTemplate(BoundPageTemplate, Acquisition.Implicit):
+        """Implementing Acquisition.interfaces.IAcquirer and
+        IAcquisitionWrapper.
+        """
+
+        __parent__ = property(lambda self: self.im_self)
+
+        def __call__(self, im_self=None, *args, **kw):
+            if self.im_self is None:
+                im_self = im_self
+            else:
+                im_self = aq_base(self.im_self)
+                if IAcquirer.providedBy(im_self):
+                    im_self = im_self.__of__(im_self.context)
+            return self.im_func(im_self, *args, **kw)
+
+    class BaseTemplateFile(BaseTemplateFile, Acquisition.Implicit):
+        """Implement Acquisition.interfaces.IAcquirer and
+        IAcquisitionWrapper.
+        """
+
+_marker = object()
+
+
+def get_bound_template(self, instance, type):
+    if instance is None:
+        return self
+
+    template = getattr(self, '_v_template', _marker)
+    if template is _marker:
+        self._v_template = template = ViewPageTemplateFile(self.filename)
+
+    return BoundPageTemplate(template, instance)
+
+
+def _get_five_pt_template(self):
+    template = getattr(self, '_v_template', _marker)
+    if template is _marker or self._text != template.body:
+        self._v_template = template = BaseTemplate(self._text, keep_source=True)
+
+    return template
+
+def _get_five_pt_template_wrapped(self):
+    template = _get_five_pt_template(self)
+
+    if IAcquirer.providedBy(template):
+        template = template.__of__(aq_parent(self))
+    else:
+        template = ImplicitAcquisitionWrapper(template, aq_parent(self))
+
+    return template
+
+def call_template(self, *args, **kw):
+    # avoid accidental exposure of the extra context parameter
+    kw.pop(EXTRA_CONTEXT_KEY, None)
+    template = self._get_five_pt_template()
+    return template(self, *args, **kw)
+
+def pt_render(self, source=False, extra_context=None):
+    if source:
+        return self._text
+    if extra_context is None:
+        extra_context = {}
+    template = self._get_five_pt_template()
+    return template(self, **{EXTRA_CONTEXT_KEY:extra_context})
+
+def _get_five_pt_template_file_wrapped(self, *args, **kw):
+    template = getattr(self, '_v_template', _marker)
+    if template is _marker:
+        self._v_template = template = BaseTemplateFile(self.filename)
+
+    if IAcquirer.providedBy(template):
+        template = template.__of__(aq_parent(self))
+    else:
+        template = ImplicitAcquisitionWrapper(template, aq_parent(self))
+
+    return template
+
+
+def get_macros(self):
+    template = self._get_five_pt_template()
+    return template.macros
+
+FiveViewPageTemplateFile.__get__ = get_bound_template
+ZopeViewPageTemplateFile.__get__ = get_bound_template
+PageTemplate._get_five_pt_template = _get_five_pt_template
+PageTemplate.__call__ = call_template
+PageTemplate.macros = ComputedAttribute(get_macros, 1)
+PageTemplateFile._get_five_pt_template = _get_five_pt_template_file_wrapped
+PageTemplateFile.__call__ = call_template
+PageTemplateFile.macros = property(get_macros)
+ZopePageTemplate._get_five_pt_template = _get_five_pt_template_wrapped
+ZopePageTemplate._bindAndExec = call_template
+ZopePageTemplate.pt_render = pt_render
+ZopePageTemplate.macros = ComputedAttribute(get_macros, 1)
+
+try:
+    from five.grok.components import ZopeTwoPageTemplate
+
+    class GrokViewAwarePageTemplateFile(ViewPageTemplateFile):
+
+        def pt_getContext(self, instance, request, **kw):
+            return {}
+
+        def _pt_get_context(self, instance, request, kwargs={}):
+            namespace = super(
+                GrokViewAwarePageTemplateFile, self)._pt_get_context(
+                instance, request, kwargs)
+            if hasattr(self, 'pt_grokContext'):
+                namespace.update(self.pt_grokContext)
+            return namespace
+
+        def pt_render(self, namespace):
+            self.pt_grokContext = namespace
+            # namespace contains self.pt_getContext() result + \
+            # five.grok.components.ZopeTwoPageTemplate.getNamespace(view)
+            # result we have currently context, request, static, and
+            # view in the dict
+            view = namespace["view"]
+            return self.__call__(_ob=view)
+            # z3c.pt.pagetemplate.ViewPageTemplate.__call__ will call
+            # self._pt_get_context(ob, None, None)
+
+    def setFromFilename(self, filename, _prefix=None):
+        self._template = GrokViewAwarePageTemplateFile(filename, _prefix)
+    ZopeTwoPageTemplate.setFromFilename = setFromFilename
+except ImportError:
+    pass
---
+++ packages/five.pt/five.pt-2.0-rc3.tar.gz/five.pt-2.0-rc3/src/five/pt/tests/test_basetemplatefile.py
@@ -0,0 +1,57 @@
+from Testing.ZopeTestCase import ZopeTestCase
+from five.pt.pagetemplate import BaseTemplateFile
+
+
+class TestPageTemplateFile(ZopeTestCase):
+    def afterSetUp(self):
+        from Products.Five import zcml
+        import Products.Five
+        import z3c.pt
+        import five.pt
+        zcml.load_config("configure.zcml", Products.Five)
+        zcml.load_config("configure.zcml", five.pt)
+        zcml.load_config("configure.zcml", z3c.pt)
+
+    def test_locals_base(self):
+        template = BaseTemplateFile('locals_base.pt')
+        result = template()
+        self.failUnless('here==context:True' in result)
+        self.failUnless('container==None:True' in result)
+        self.failUnless("nothing:" in result)
+
+    def test_nocall(self):
+        template = BaseTemplateFile("nocall.pt")
+
+        def dont_call():
+            raise RuntimeError()
+        result = template(callable=dont_call)
+        self.failUnless(repr(dont_call) in result)
+
+    def test_exists(self):
+        template = BaseTemplateFile("exists.pt")
+
+        def dont_call():
+            raise RuntimeError()
+        result = template(callable=dont_call)
+        self.failUnless('ok' in result)
+
+    def test_simple(self):
+        template = BaseTemplateFile("simple.pt")
+        result = template()
+        self.failUnless('Hello world!' in result)
+
+    def test_secure(self):
+        soup = '<foo></bar>'
+        template = BaseTemplateFile("secure.pt")
+        from zExceptions import Unauthorized
+        try:
+            result = template(soup=soup)
+        except Unauthorized:
+            pass
+        else:
+            self.fail("Expected unauthorized.")
+
+        from AccessControl.SecurityInfo import allow_module
+        allow_module("cgi")
+        result = template(soup=soup)
+        self.failUnless('&lt;foo&gt;&lt;/bar&gt;' in result)
