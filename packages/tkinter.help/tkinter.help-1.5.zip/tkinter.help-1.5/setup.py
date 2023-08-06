28e5f3aae6f386c40b43a922b29b965ed6334d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:53 +0000

    Add package tkinter.help/1.5/tkinter.help-1.5.zip

---
+++ packages/tkinter.help/tkinter.help-1.5.zip/tkinter.help-1.5/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+	name = "tkinter.help",
+	version = "1.5",
+	license = "GNU/GPL GNU Public License",
+	description = "This programm is for newbees!",
+	long_description = "ENGLISH: tkhelp helps newbees! The program shows you the many widgets! \n GERMAN: tkhelp ist ein Programm, dass anderen Neueinsteigern helfen soll, die vielen Widgets zu sehen, und sie dann sogar einsetzen zu können. Aber das kommt noch!",
+	author = "Logander4 Tutorials",
+	author_email = "logander4@icqmail.com",
+	url = "http://log4-tutorials.de",
+	download_url = "http:/log4-tutorials.de/software",
+	py_modules = ["tkhelp", "canvas1", "canvas2"] ,
+	classifiers= [
+	"Development Status :: 5 - Production/Stable",
+	"Intended Audience :: End Users/Desktop",
+	"License :: OSI Approved :: Python Software Foundation License",
+	"License :: Freeware",
+	"Natural Language :: German",
+	"Operating System :: MacOS :: MacOS X",
+	"Operating System :: Microsoft :: Windows",
+	"Operating System :: POSIX",
+	"Programming Language :: Python :: 3.1",
+	"Topic :: Desktop Environment",
+	"Topic :: Software Development :: Documentation"
+	]
+)
