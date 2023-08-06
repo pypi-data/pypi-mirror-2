0092a7c794997b236b3cd5cbc3c59330bc4036b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:08 +0000

    Add package ooredis/1.3/ooredis-1.3.tar.gz

---
+++ packages/ooredis/ooredis-1.3.tar.gz/ooredis-1.3/ooredis/__init__.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+__all__ = [
+    'connect', 'get_client',
+    'type_case',
+    'Dict', 'List', 'Set', 'SortedSet', 'SingleValue', 'Counter',
+    '__version__',
+]
+
+from client import connect, get_client
+import type_case
+
+from mix.dict import Dict
+from mix.list import List
+from mix.set import Set
+from mix.sorted_set import SortedSet
+from mix.single_value import SingleValue, Counter
+
+__version__ = 1.3
