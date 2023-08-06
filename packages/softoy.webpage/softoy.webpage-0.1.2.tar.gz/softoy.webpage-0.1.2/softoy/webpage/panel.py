3f96d22d0377d764a7e30cda3c77c0719605e8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:51 +0000

    Add package softoy.webpage/0.1.2/softoy.webpage-0.1.2.tar.gz

---
+++ packages/softoy.webpage/softoy.webpage-0.1.2.tar.gz/softoy.webpage-0.1.2/softoy/webpage/panel.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from softoy.htmltags.htmltags import Tag as T
+
+def centering(length, parent_length):
+    offset = (parent_length - length) / 2
+    if offset >= 0:
+        return offset
+    else:
+        return 0
+
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Panel(T):
+    def __init__(self, x, y, **kw):
+        super(Panel, self).__init__('div', **kw)
+        self.x = x
+        self.y = y
+        self.width = kw.get('w', 100)  #?
+        self.height = kw.get('h', 100)  #?
+        self.w = kw.get('w', 100)
+        self.h = kw.get('h', 100)
+        self.style['left'] = self.px(self.x)
+        self.style['top'] = self.px(self.y)
+        self.style['width'] = self.px(self.w)
+        self.style['height'] = self.px(self.h)
+        self.attrs['class'] = 'abs'
+
+    def px(self, n):
+        if n is not None and type(n) == int:
+            return '%spx' % n
+
+    def reside_center(self, tag, width, height):
+        x = centering(width, self.w)
+
