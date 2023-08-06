f47df2ff02d54c11bf51cbb38450fd15c8698830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:15 +0000

    Add package ore.contentmirror/0.7.1/ore.contentmirror-0.7.1.tar.gz

---
+++ packages/ore.contentmirror/ore.contentmirror-0.7.1.tar.gz/ore.contentmirror-0.7.1/ore/contentmirror/ptests/base.py
@@ -0,0 +1,60 @@
+import traceback
+from StringIO import StringIO
+
+from Testing.ZopeTestCase import Sandboxed
+from Products.PloneTestCase import PloneTestCase
+
+from ore.contentmirror.ptests.layer import MirrorLayer
+from ore.contentmirror.tests.base import reset_db
+from ore.contentmirror.operation import get_buffer
+
+PloneTestCase.setupPloneSite()
+
+
+class MirrorTestCase(Sandboxed, PloneTestCase.PloneTestCase):
+
+    layer = MirrorLayer
+
+    events = ()
+
+    _cleanup_funcs = ()
+
+    def setUp(self):
+        super(MirrorTestCase, self).setUp()
+        reset_db()
+
+    def tearDown(self):
+        super(MirrorTestCase, self).tearDown()
+
+        for cleanup, args, kw in self._cleanup_funcs:
+            cleanup(*args, **kw)
+
+    def addCleanup(self, func, *args, **kw):
+        if not self._cleanup_funcs:
+            self._cleanup_funcs = []
+        self._cleanup_funcs.append((func, args, kw))
+
+    def flush(self):
+        get_buffer().flush()
+
+    def capture_events(self):
+        events = []
+
+        def capture(evt):
+            stack_trace = StringIO()
+            traceback.print_stack(limit=30, file=stack_trace)
+            events.append((evt, stack_trace))
+
+        from zope import event
+        event.subscribers.append(capture)
+
+        self.addCleanup(event.subscribers.remove, capture)
+        return events
+
+    def pprint_events(self, events_capture, traces=False):
+        print "Events Capture and Traces"
+        for event, trace in events_capture:
+            print event
+            if traces:
+                print trace.getvalue()
+            print
