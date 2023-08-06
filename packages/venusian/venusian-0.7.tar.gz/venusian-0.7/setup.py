e4d1173cfcbb75605bcb7958fbb6dcd1d3815d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:16 +0000

    Add package venusian/0.7/venusian-0.7.tar.gz

---
+++ packages/venusian/venusian-0.7.tar.gz/venusian-0.7/setup.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2010 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+__version__ = '0.7'
+
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except:
+    README = ''
+    CHANGES = ''
+
+requires = []
+
+setup(name='venusian',
+      version=__version__,
+      description='A library for deferring decorator actions',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        ],
+      keywords='web wsgi zope',
+      author="Chris McDonough, Agendaless Consulting",
+      author_email="pylons-devel@googlegroups.com",
+      url="http://pylonsproject.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require = requires,
+      setup_requires = 'nose',
+      install_requires = requires,
+      # Normal unittest can't support running the venusian tests under
+      # py 2.4 or 2.5; when it scans the 'classdecorators' fixture, it
+      # barfs.  We use nose.collector as a workaround.
+      test_suite="nose.collector", 
+      entry_points = """\
+      """
+      )
+
