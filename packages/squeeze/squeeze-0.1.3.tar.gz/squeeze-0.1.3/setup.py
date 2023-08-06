5c9716517ef8fc2c70c5926b21960700b188b1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:55 +0000

    Add package squeeze/0.1.3/squeeze-0.1.3.tar.gz

---
+++ packages/squeeze/squeeze-0.1.3.tar.gz/squeeze-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="squeeze",
+    description="HTML/JS/CSS Compressor packaged for Python",
+    long_description=open('README').read(),
+    author='Yesudeep Mangalapilly',
+    author_email='yesudeep@gmail.com',
+    version="0.1.3",
+    url="http://pypi.python.org/pypi/squeeze",
+    license='Public Domain',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            "squeeze = squeeze:main"
+        ]
+    },
+    package_data={
+        '': ["*.jar"]
+    },
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ]
+)
