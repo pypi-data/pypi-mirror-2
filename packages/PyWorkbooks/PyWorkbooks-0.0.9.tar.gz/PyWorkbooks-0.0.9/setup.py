7a13c57454aac4ab6ad6d03516823b2a9f51f586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:34 +0000

    Add package PyWorkbooks/0.0.9/PyWorkbooks-0.0.9.tar.gz

---
+++ packages/PyWorkbooks/PyWorkbooks-0.0.9.tar.gz/PyWorkbooks-0.0.9/setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# setup.py
+import sys
+
+if sys.platform == 'win32':
+   if len(sys.argv) < 2:
+      sys.argv.append('install') # pretty sure they want to install... 
+   
+from distutils.core import setup
+import os
+
+version = '0.0.9'
+
+if sys.platform != 'win32':
+   setup(name = 'PyWorkbooks',
+         version = version,
+         author = 'Garrett Berg',
+         url = 'https://sourceforge.net/projects/pyworkbooks/',
+         packages = ['PyWorkbooks'],
+         description = "Seamless spreadsheet integration with Excel and Gnumeric."
+                       "Treat open workbooks as if they are numpy arrays!",
+         classifiers = [
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2'
+            ],
+
+         data_files = [('/usr/share/doc/python-PyWorkbooks', ['PyWorkbooks Documentation.pdf']),
+                        #(os.path.expanduser('~') + '/.gnumeric/1.10.8/plugins/' , ['plugins'])
+                      ]
+         )
+
+else:
+   setup(name = 'PyWorkbooks',
+         version = version,
+         author = 'Garrett Berg',
+         url = 'https://sourceforge.net/projects/pyworkbooks/',
+         packages = ['PyWorkbooks'],
+         classifiers = [
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2'
+            ],
+         )
