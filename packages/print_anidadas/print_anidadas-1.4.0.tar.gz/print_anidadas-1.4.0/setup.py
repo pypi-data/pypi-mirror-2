088265d4d582d97b6df17627ef205990c1f25259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:03 +0000

    Add package print_anidadas/1.4.0/print_anidadas-1.4.0.tar.gz

---
+++ packages/print_anidadas/print_anidadas-1.4.0.tar.gz/print_anidadas-1.4.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name         = 'print_anidadas',
+    version      = '1.4.0',
+    py_modules   = ['print_anidadas'],
+    author       = 'Rotura',
+    author_email = 'rknxtra@gmail.com',
+    url          = 'http://lavidaeslagomas.blogspot.com',
+    description  = '''Un sencillo listador de listas anidadas. Tiene la capacidad de tabular
+    en cada nivel de anidamiento para mostrar mejor los elementos y distinguir en que nivel se
+    encuentran. Ademas es capaz de recibir como parametro la salida donde se quiere imprimir
+    el resultado, pudiendo utilizarse para volcar listas en archivos de datos en un formato
+    legible y limpio.'''
+    )
