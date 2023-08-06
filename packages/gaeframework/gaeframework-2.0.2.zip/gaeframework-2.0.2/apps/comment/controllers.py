a953f01b08ef33322ecd78822dc763d52c9ddfdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:38 +0000

    Add package gaeframework/2.0.2/gaeframework-2.0.2.zip

---
+++ packages/gaeframework/gaeframework-2.0.2.zip/gaeframework-2.0.2/apps/comment/controllers.py
@@ -0,0 +1,38 @@
+'''
+Comments - attach comments to any object.
+'''
+from comment.forms import UserCommentForm
+from user import login_required
+from gae import db
+import re
+
+@login_required()
+def create_comment(app):
+    """
+    Create a comment.
+
+    HTTP POST is required. If ``POST['submit'] == "preview"`` or if there are
+    errors a preview template, ``comment/preview.html``, will be rendered.
+    """
+    data = app.request.POST.copy()
+    if data:
+        # get object model name
+        object_key = db.Key(data['obj'])
+        object_type = object_key.kind()
+        # get application name
+        path = re.findall("[A-Z][^A-Z]*", object_type)[0].lower()
+        try:
+            # load module with object, attached to comment
+            __import__("%s.models" % (path))
+        except ImportError:
+            raise Exception("Not found object '%s' in application '%s'" % (object_type, path))
+        # filled form
+        form = UserCommentForm(data=data, initial={"obj": object_key})
+        # preview the comment
+        if "preview" in data or not form.is_valid():
+            return app.render('comments/preview', {
+                              "form":     form,
+                              "comment":  form.data.get("text", ""),
+                              })
+        form.save()
+    return app.redirect("go back")
---
+++ packages/gaeframework/gaeframework-2.0.2.zip/gaeframework-2.0.2/apps/user/controllers.py
@@ -0,0 +1,65 @@
+'''
+User account management.
+'''
+from user.forms import UserLoginForm, UserRegistrationForm
+from user.models import User
+from user import login_required
+
+
+def users_list(app):
+    return app.render('user/users_list', {'users': User.all().order("-created")})
+
+
+def login(app):
+    if app.request.POST:
+        # filled form
+        form = UserLoginForm(data=app.request.POST)
+        if form.is_valid():
+            nick = form.cleaned_data.get("nick")
+            password = form.cleaned_data.get("password")
+            user = User.get_by_key_name(nick)
+            # correct user
+            if user and user.password == password:
+                app.session["user"] = user
+                # when the user login, that we rotate the session ID (security)
+                app.session.regenerate_id()
+                return app.redirect("go back")
+            else:
+                form._errors["nick"] = form.error_class(["User with given nick name and password not found"])
+    else:
+        # empty form
+        form = UserLoginForm()
+    return app.render('user/login', {'form': form})
+
+@login_required()
+def logout(app):
+    del app.session["user"]
+    return app.redirect("go back")
+
+def registration(app):
+    if app.request.POST:
+        # filled form
+        form = UserRegistrationForm(data=app.request.POST)
+        if form.is_valid():
+            # check user nick name duplicates
+            nick = form.cleaned_data.get("nick")
+            if User.get_by_key_name(nick):
+                form._errors["nick"] = form.error_class(["User with given nick name already registered"])
+            else:
+                user = form.save()
+                app.session["user"] = user
+                # when the user login, that we rotate the session ID (security)
+                app.session.regenerate_id()
+                return app.redirect("go back")
+    else:
+        # empty form
+        form = UserRegistrationForm()
+    return app.render('user/registration', {'form': form})
+
+def activate(app, account_id):
+    '''Activate already registered user account'''
+    pass
+
+def deactivate(app, account_id):
+    '''Deactivate user account'''
+    pass
