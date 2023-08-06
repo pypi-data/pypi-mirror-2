dc1a9bb18ea4cfe2f00f9ca68969a776f5cb23e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:05 +0000

    Add package maju_test/1.3.0/maju_test-1.3.0.tar.gz

---
+++ packages/maju_test/maju_test-1.3.0.tar.gz/maju_test-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'maju_test',
+    version = '1.3.0',
+    py_modules = ['maju_test'],
+    author = 'maju',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description= 'It\'s test program from Head First book',
+)
