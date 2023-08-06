bc46221b3962193c3abe781a41270630511cbc00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:34 +0000

    Add package django-mongokit/0.2.0/django-mongokit-0.2.0.tar.gz

---
+++ packages/django-mongokit/django-mongokit-0.2.0.tar.gz/django-mongokit-0.2.0/django_mongokit/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ('connection', 'get_database', 'get_version')
+
+from shortcut import get_database, get_version, connection
+
+                      
---
+++ packages/django-mongokit/django-mongokit-0.2.0.tar.gz/django-mongokit-0.2.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Copyright (c) 2010 Peter Bengtsson, peter@fry-it.com
+from distutils.core import setup
+
+setup(name='django-mongokit',
+      version=open('django_mongokit/version.txt').read(),
+      author="Peter Bengtsson",
+      author_email="peter@fry-it.com",
+      url="http://github.com/peterbe/django-mongokit",
+      description='Bridging Django to MongoDB with the MongoKit ODM',
+      long_description=open('README.md').read(),
+      package_dir={'djangomongokitlib':'django-mongokitlib'},
+      packages=['django_mongokit', 'django_mongokit.mongodb', 'django_mongokit.forms'],
+      package_data={'django_mongokit':['version.txt']},
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities']
+)
