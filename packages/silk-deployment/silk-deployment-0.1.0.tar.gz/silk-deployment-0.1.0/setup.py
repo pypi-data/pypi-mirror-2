de1cfa710baf513680873e7c14a60d8ed2683062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:13 +0000

    Add package silk-deployment/0.1.0/silk-deployment-0.1.0.tar.gz

---
+++ packages/silk-deployment/silk-deployment-0.1.0.tar.gz/silk-deployment-0.1.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='silk-deployment',
+    version='0.1.0',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+	packages=find_packages(),
+    package_dir={'silk': 'silk'},
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': [
+            'silk = silk.utils:cmd_dispatcher',
+            'freeze2yaml = silk.utils:freeze_2_yaml',
+            'yaml2freeze = silk.utils:yaml_2_freeze',
+        ],
+    },
+	install_requires = [
+        'gunicorn',#for running in production
+        'cherrypy',#for 'silk run' (local devserver)
+        'Fabric >= 0.9.2',
+        'setproctitle',
+        'PyYAML',
+	],
+    url='http://bits.btubbs.com/silk-deployment',
+    license='LICENSE.txt',
+    description='A Fabric-based tool for deploying WSGI apps on an Ubuntu/Nginx/Supervisord/Gunicorn stack.',
+    long_description=open('README.rst').read(),
+)
+
