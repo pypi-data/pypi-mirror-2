e83f96eab9097ed7b7dbe10c6b6e76854b076da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:02 +0000

    Add package TreeSoup/0.1/TreeSoup-0.1.tar.gz

---
+++ packages/TreeSoup/TreeSoup-0.1.tar.gz/TreeSoup-0.1/setup.py
@@ -0,0 +1,25 @@
+
+from distutils.core import setup
+
+setup(
+    name='TreeSoup',
+    version='0.1',
+    description='BeautifulSoup-like wrapper around ElementTree.',
+    url='http://github.com/mikeboers/TreeSoup',
+    py_modules=['treesoup'],
+    
+    author='Mike Boers',
+    author_email='treesoup@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: XML',
+    ],
+)
