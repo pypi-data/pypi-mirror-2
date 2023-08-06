67d6ded264584b9a172df40fa73e9c43b3731504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:28 +0000

    Add package pybb/0.1.10/pybb-0.1.10.tar.gz

---
+++ packages/pybb/pybb-0.1.10.tar.gz/pybb-0.1.10/setup.py
@@ -0,0 +1,33 @@
+# Copyright: 2011, Grigoriy Petukhov
+# Author: Grigoriy Petukhov (http://lorien.name)
+# License: BSD
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pybb',
+    version = '0.1.10',
+    description = 'Django forum application',
+    long_description = open('README').read(),
+    author = 'Grigoriy Petukhov',
+    author_email = 'lorien@lorien.name',
+    url = 'http://pybb.org',
+
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ['django-common', 'markdown', 'pytils', 'south'],
+
+    license = "BSD",
+    keywords = "django application forum board",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
