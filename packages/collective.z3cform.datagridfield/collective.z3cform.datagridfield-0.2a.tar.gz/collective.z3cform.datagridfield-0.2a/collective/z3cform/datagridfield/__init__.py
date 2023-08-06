34cc9f0886866a44bf466a0589f6afc79d46c084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:30 +0000

    Add package collective.z3cform.datagridfield/0.2a/collective.z3cform.datagridfield-0.2a.tar.gz

---
+++ packages/collective.z3cform.datagridfield/collective.z3cform.datagridfield-0.2a.tar.gz/collective.z3cform.datagridfield-0.2a/collective/z3cform/datagridfield/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('collective.z3cform.datagridfield')
+
+from datagridfield import DataGridFieldFactory
+from datagridfield import DataGridField
