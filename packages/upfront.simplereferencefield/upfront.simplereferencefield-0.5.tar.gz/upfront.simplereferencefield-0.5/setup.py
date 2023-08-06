70b5d03ee53b9314f2f44336893a707cea93364a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:27 +0000

    Add package upfront.simplereferencefield/0.5/upfront.simplereferencefield-0.5.tar.gz

---
+++ packages/upfront.simplereferencefield/upfront.simplereferencefield-0.5.tar.gz/upfront.simplereferencefield-0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.5'
+
+setup(name='upfront.simplereferencefield',
+      version=version,
+      description="Store references on an object and not in the reference catalog",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        ],
+      keywords='',
+      author='Roche Compaan',
+      author_email='roche@upfrontsystems.co.za',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['upfront'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
