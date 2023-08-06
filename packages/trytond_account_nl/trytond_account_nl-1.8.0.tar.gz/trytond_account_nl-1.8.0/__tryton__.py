217be570fdb179716bcca0886b76db42272f10f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:15 +0000

    Add package trytond_account_nl/1.8.0/trytond_account_nl-1.8.0.tar.gz

---
+++ packages/trytond_account_nl/trytond_account_nl-1.8.0.tar.gz/trytond_account_nl-1.8.0/__tryton__.py
@@ -0,0 +1,26 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name': 'Account Netherlands',
+    'name_nl_NL': 'Boekhouding Nederland',
+    'version': '1.8.0',
+    'author': 'NFG',
+    'email': 'info@nfg.nl',
+    'website': 'http://www.tryton.org/',
+    'description': '''Define an account chart template for The Netherlands.
+Useful to create a Dutch account chart with the wizard in
+"Financial Management>Configuration>General Account>Create Chart of Accounts from Template".
+''',
+    'description_nl_NL': '''Definieert een nederlands grootboekschema, en
+    belasting schema.
+''',
+    'depends': [
+        'account',
+    ],
+    'xml': [
+        'account_nl.xml',
+    ],
+    'translation': [
+        'nl_NL.csv',
+    ],
+}
