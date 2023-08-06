7c1350bcc4b502eceba7e69c0ac6264921114207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:27 +0000

    Add package zetalibrary/0.3.7/zetalibrary-0.3.7.tar.gz

---
+++ packages/zetalibrary/zetalibrary-0.3.7.tar.gz/zetalibrary-0.3.7/zetalibrary/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os.path
+
+VERSION_INFO = (0, 3, 7)
+
+__project__ = PROJECT = __name__
+__version__ = VERSION = '.'.join(str(i) for i in VERSION_INFO)
+__author__ = AUTHOR = "Kirill Klenov <horneds@gmail.com>"
+__license__ = LICENSE = "GNU LGPL"
+
+ZETALIBDIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'zetalib')
+
+
+class ZetaError( Exception ):
+    """ Zeta error.
+    """
+    def __init__( self, message, linker=None ):
+        if linker and len(linker.tree):
+            message = "%s: %s" % (linker.tree[-1]['current'], message)
+        super( ZetaError, self ).__init__( message )
