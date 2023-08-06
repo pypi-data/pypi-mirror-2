bd746b26ec8f48f4105a8d1b4776b6da6a95246f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:38 +0000

    Add package date-to-words/1.0.0/date%20to%20words-1.0.0.zip

---
+++ packages/date-to-words/date%20to%20words-1.0.0.zip/date to words-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'date to words',
+    version='1.0.0',
+    py_modules=['date2words'],
+    author='abhilash',
+    author_email=' ',
+    url=' ',
+    description='A simple program to convert date in numeric form to words',
+    )
