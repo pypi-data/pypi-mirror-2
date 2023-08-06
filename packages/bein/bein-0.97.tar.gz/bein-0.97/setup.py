92ddf8c9a00434ef2c453777cbd9f9fe412b7d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:13 +0000

    Add package bein/0.97/bein-0.97.tar.gz

---
+++ packages/bein/bein-0.97.tar.gz/bein-0.97/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='bein',
+      version='0.97',
+      url='http://madhadron.com/bein',
+      description='Miniature LIMS and workflow manager for bioinformatics',
+      author='Fred Ross',
+      author_email='madhadron@gmail.com',
+      packages=['bein'],
+      scripts=['beinclient'],
+      classifiers=['Topic :: System :: Shells', 'Topic :: Scientific/Engineering :: Bio-Informatics']
+      )
