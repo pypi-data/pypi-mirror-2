cee869ae32c303e4edbe3dc6b840363596c8a32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:23 +0000

    Add package pytest_gae/0.2/pytest_gae-0.2.tar.gz

---
+++ packages/pytest_gae/pytest_gae-0.2.tar.gz/pytest_gae-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+fhandler = open('README.rst', 'r')
+long_desc = fhandler.read()
+fhandler.close()
+
+setup(name='pytest_gae',
+      version='0.2',
+      description="pytest plugin for apps written with Google's AppEngine",
+      long_description=long_desc,
+      author='Petras Zdanavicius (petraszd)',
+      author_email='petraszd@gmail.com',
+      url='http://bitbucket.org/petraszd/pytest_gae/',
+      py_modules=['pytest_gae'],
+      install_requires=['pytest'],
+      entry_points={'pytest11': ['pytest_gae = pytest_gae']},
+      license='MIT License',
+      keywords='py.test pytest google app engine',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2',
+                   'Topic :: Software Development :: Testing'])
