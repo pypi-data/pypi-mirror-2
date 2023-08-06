94ca3a531ab3b9df4d3e18904bff3eb242088600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:46 +0000

    Add package vmw.vco/0.2/vmw.vco-0.2.tar.gz

---
+++ packages/vmw.vco/vmw.vco-0.2.tar.gz/vmw.vco-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name = "vmw.vco",
+    version = "0.2",
+    package_dir = {'': 'src'},
+    packages = find_packages('src'),
+    namespace_packages = ['vmw',],
+
+    install_requires = ['vmw.ZSI',
+                        'setuptools',
+                        'Twisted',
+                        ],
+
+    description="Python bindings for the VMware Orchestrator",
+    author="Yann Hodique",
+    author_email="yhodique@vmware.com",
+    url="http://sigma.github.com/vmw.vco",
+
+)
