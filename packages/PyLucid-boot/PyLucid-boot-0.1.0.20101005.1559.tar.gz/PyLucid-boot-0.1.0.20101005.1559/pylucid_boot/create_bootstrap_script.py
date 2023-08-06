21cfd0a9711106004f3dd8668cade181a81cd8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:49 +0000

    Add package PyLucid-boot/0.1.0.20101005.1559/PyLucid-boot-0.1.0.20101005.1559.tar.gz

---
+++ packages/PyLucid-boot/PyLucid-boot-0.1.0.20101005.1559.tar.gz/PyLucid-boot-0.1.0.20101005.1559/pylucid_boot/create_bootstrap_script.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+"""
+    Create/update the virtualenv bootstrap script BOOTSTRAP_SCRIPT from
+    the BOOTSTRAP_SOURCE file.
+"""
+
+import os
+import sys
+
+try:
+    import virtualenv
+except ImportError, err:
+    print "Import error:", err
+    print
+    print "Please install virtualenv!"
+    print "e.g.: easy_install virtualenv"
+    print "More info:"
+    print "http://pypi.python.org/pypi/virtualenv"
+    print
+    sys.exit()
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+BOOTSTRAP_SCRIPT = os.path.join(ROOT, "pylucid-boot.py")
+BOOTSTRAP_SOURCE = os.path.join(ROOT, "source-pylucid-boot.py")
+
+
+def create_bootstrap_script():
+    info = "source bootstrap script: %r" % BOOTSTRAP_SOURCE
+    print "read", info
+    content = "# %s\n" % info
+    f = file(BOOTSTRAP_SOURCE, "r")
+    content += f.read()
+    f.close()
+
+    print "Create/Update %r" % BOOTSTRAP_SCRIPT
+
+    output = virtualenv.create_bootstrap_script(content)
+
+    # Add info lines
+    output_lines = output.splitlines()
+    output_lines.insert(2, "## Generate with %r" % __file__)
+    output_lines.insert(2, "## using: %r" % virtualenv.__file__)
+    output = "\n".join(output_lines)
+    #print output
+
+    f = file(BOOTSTRAP_SCRIPT, 'w')
+    f.write(output)
+    f.close()
+
+
+if __name__ == "__main__":
+    create_bootstrap_script()
+    print " -- END -- "
