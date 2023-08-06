e6eab12fadae143120dd7bc20948c2bf2db8368c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:54 +0000

    Add package pilas/0.40/pilas-0.40.tar.gz

---
+++ packages/pilas/pilas-0.40.tar.gz/pilas-0.40/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+        name='pilas',
+        version='0.40',
+        description='A simple to use video game framework.',
+        author='Hugo Ruscitti',
+        author_email='hugoruscitti@gmail.com',
+        install_requires=[
+            'setuptools',
+            'Box2D',
+            ],
+        packages=['pilas', 'pilas.actores', 'pilas.dispatch', 'pilas.ejemplos', 'pilas.motores', 'pilas.interfaz'],
+        url='http://www.pilas-engine.com.ar',
+        include_package_data = True,
+        package_data = {
+            'images': ['pilas/data/*', 'pilas/ejemplos/data/*', 'pilas/data/fondos/*',
+                       'pilas/data/juegobase/*', 'pilas/data/juegobase/data/*'],
+            },
+
+        scripts=['bin/pilas'],
+
+        classifiers = [
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+            'Natural Language :: Spanish',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Games/Entertainment',
+            'Topic :: Education',
+            'Topic :: Software Development :: Libraries',
+          ],
+    )
+
