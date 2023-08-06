1dd2b0466f9e08cc48ba55c133792c363989ed04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:41 +0000

    Add package kajiki/0.2.2/Kajiki-0.2.2.tar.gz

---
+++ packages/kajiki/Kajiki-0.2.2.tar.gz/Kajiki-0.2.2/kajiki/__init__.py
@@ -0,0 +1,5 @@
+from .util import expose, flattener
+from .template import Template
+from .loader import MockLoader, FileLoader, PackageLoader
+from .text import TextTemplate
+from .xml_template import XMLTemplate
---
+++ packages/kajiki/Kajiki-0.2.2.tar.gz/Kajiki-0.2.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.2.2'
+
+setup(name='Kajiki',
+      version=version,
+      description="Really fast well-formed xml templates",
+      long_description="""Are you tired of the slow performance of Genshi? But
+      you still long for the assurance that your output is well-formed that you
+      miss from all those other templating engines? Do you wish you had Jinja's
+      blocks with Genshi's syntax? Then look  no further, Kajiki is for you!
+      Kajiki quickly compiles Genshi-like syntax to *real python bytecode*
+      that renders with blazing-fast speed! Don't delay! Pick up your
+      copy of Kajiki today!""",  
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Text Processing :: Markup :: XML'
+        ], 
+      keywords='template xml',
+      author='Rick Copeland',
+      author_email='rick446@usa.net',
+      url='http://bitbucket.org/rick446/fastpt',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
