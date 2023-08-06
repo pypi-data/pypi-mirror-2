9d3bf00b938e71981d9e7fd0cf97b7b181946762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:11 +0000

    Add package django-xslt/0.1.1/django-xslt-0.1.1.tar.gz

---
+++ packages/django-xslt/django-xslt-0.1.1.tar.gz/django-xslt-0.1.1/src/django/contrib/xslt/testhelp.py
@@ -0,0 +1,33 @@
+from lxml import etree
+from StringIO import StringIO
+
+# This is used as a parsed-XML cache
+# We put all the content we parse here keyed by content
+XMLCACHE={}
+
+def assertXpath(xml, xpr, assertion_message="", namespaces=None, html=False):
+    """Assert the Xpath 'xpr' evals against the 'xml' document.
+
+    assertion_message can be specified as an alternative error message from a failure.
+    namespaces can be specified as a list of namespace key:url pairs to be passed to XSLT
+    html is boolean to specify whether to parse the document as HTML or not.
+    """
+
+    namespaces = {} if not namespaces else namespaces
+    try:
+        doc = XMLCACHE.get(xml)
+        if not doc:
+            parser = etree.HTMLParser() if html else etree.XMLParser()
+            doc = etree.parse(StringIO(xml), parser)
+            XMLCACHE[xml] = doc
+    except Exception, e:
+        raise e
+    else:
+        ret = doc.xpath(xpr, namespaces=namespaces)
+        if not ret:
+            assertion_message = assertion_message \
+                if assertion_message \
+                else "{%s} did not evaluate with the specified document" % xpr
+            raise AssertionError(assertion_message)
+
+# End        
