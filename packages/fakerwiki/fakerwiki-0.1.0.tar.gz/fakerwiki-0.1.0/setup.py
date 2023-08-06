61281937cd9a7d5e2fb2b3b41c542a33a5d30f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package fakerwiki/0.1.0/fakerwiki-0.1.0.tar.gz

---
+++ packages/fakerwiki/fakerwiki-0.1.0.tar.gz/fakerwiki-0.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fakerwiki',
+      version='0.1.0',
+      description='FakerWiki is a library for local testing of Python ScraperWiki scripts.',
+      author='Christopher Groskopf',
+      author_email='staringmonkey@gmail.com',
+      url='http://github.com/bouvard/fakerwiki',
+      license='GPLv3',
+      packages=['scraperwiki'],
+     )
+
