e82d93feaeb36f9ccbac65fe46c0237cc1e0d93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package collective.geo.geographer/0.1.1/collective.geo.geographer-0.1.1.tar.gz

---
+++ packages/collective.geo.geographer/collective.geo.geographer-0.1.1.tar.gz/collective.geo.geographer-0.1.1/collective/geo/geographer/tests/test_geocoder.py
@@ -0,0 +1,58 @@
+import unittest
+import simplejson
+from geopy.geocoders.google import GQueryError
+
+from zope.interface import alsoProvides
+
+from Products.Five.testbrowser import Browser
+from Products.PloneTestCase.setup import portal_owner
+from Products.PloneTestCase.setup import default_password
+
+from collective.geo.geographer.tests.base import FunctionalTestCase
+from collective.geo.geographer.tests.base import test_params
+from collective.geo.geographer.interfaces import IGeoreferenceable
+from collective.geo.geographer.interfaces import IGeoCoder
+
+
+class TestGeocoder(FunctionalTestCase):
+
+    def afterSetUp(self):
+        self.oid = self.folder.invokeFactory('Document', 'doc')
+        self.obj = self.folder[self.oid]
+        self.geo = IGeoCoder(self.obj)
+        alsoProvides(self.obj, IGeoreferenceable)
+
+    def test_geocoder_base(self):
+        for item in test_params:
+            locations = self.geo.retrieve(item['address'])
+            self.assertEquals([loc for loc in locations], item['output'])
+
+    def test_geocoder_error(self):
+        self.assertRaises(GQueryError,
+                          self.geo.retrieve,
+                          "not existent place aklhj asaas")
+
+    def test_geocoder_view(self):
+        browser = Browser()
+        browser.addHeader('Authorization',
+                          'Basic %s:%s' % (portal_owner, default_password))
+
+        for item in test_params:
+            obj_url = "%s/@@geocoderview?address=%s" % \
+                            (self.portal.absolute_url(), item['address'])
+            browser.open(obj_url)
+            view_contents = simplejson.loads(browser.contents)
+
+            i = 0
+            for place, (lat, lon) in view_contents:
+                test_place, (test_lat, test_lon) = item['output'][i]
+                self.assertEquals(test_place, place)
+                self.assertEquals(test_lat, lat)
+                self.assertEquals(test_lon, lon)
+                i += 1
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestGeocoder))
+    return suite
---
+++ packages/collective.geo.geographer/collective.geo.geographer-0.1.1.tar.gz/collective.geo.geographer-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.1'
+
+setup(name='collective.geo.geographer',
+      version=version,
+      description="Geographic annotation for Plone",
+      long_description=open("collective/geo/geographer/README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='gis geography geospatial plone',
+      author='Giorgio Borelli',
+      author_email='giorgio@giorgioborelli.it',
+      url="http://plone.org/products/collective.geo",
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.geo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'simplejson',
+          'geopy',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
