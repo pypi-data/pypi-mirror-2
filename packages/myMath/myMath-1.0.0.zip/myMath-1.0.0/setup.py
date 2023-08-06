1c85aa09b2dcd0ebb38829cfbac7e62c3b2c2b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:54 +0000

    Add package myMath/1.0.0/myMath-1.0.0.zip

---
+++ packages/myMath/myMath-1.0.0.zip/myMath-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'myMath',
+	version = '1.0.0',
+	py_modules = ['myMath'],
+	author = 'anybody',
+	author_email = 'Manfred.Paul@gmx.de',
+	url = '-',
+	description = 'Mathematic functions'
+	)
