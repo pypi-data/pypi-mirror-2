88880958eed9e270039d10fb679d5ebbd77c029f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:49 +0000

    Add package haufe.sharepoint/0.1.6/haufe.sharepoint-0.1.6.tar.gz

---
+++ packages/haufe.sharepoint/haufe.sharepoint-0.1.6.tar.gz/haufe.sharepoint-0.1.6/haufe/sharepoint/tests/test_connector.py
@@ -0,0 +1,14 @@
+import unittest
+
+from haufe.sharepoint.connection import Connector
+
+class ConnectorTests(unittest.TestCase):
+
+    def testConnector(self):
+        connector = Connector()
+    
+def test_suite():$
+    suite = unittest.TestSuite()$
+    suite.addTest(unittest.makeSuite(ConnectorTests))$
+    return suite$
+         C
