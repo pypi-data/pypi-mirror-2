c1505045d057b7ea6eb849a4cdbc754374371f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:42 +0000

    Add package django-sentry/1.4.1/django-sentry-1.4.1.tar.gz

---
+++ packages/django-sentry/django-sentry-1.4.1.tar.gz/django-sentry-1.4.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-sentry',
+    version='1.4.1',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://github.com/dcramer/django-sentry',
+    description = 'Exception Logging to a Database in Django',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'django-paging>=0.2.2',
+        'django-indexer==0.2.1',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
