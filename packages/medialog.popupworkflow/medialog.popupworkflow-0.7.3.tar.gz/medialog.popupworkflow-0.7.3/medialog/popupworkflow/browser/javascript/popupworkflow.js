12c0d6d19f627627015fa24a4dae6411ae21d920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:23 +0000

    Add package medialog.popupworkflow/0.7.3/medialog.popupworkflow-0.7.3.tar.gz

---
+++ packages/medialog.popupworkflow/medialog.popupworkflow-0.7.3.tar.gz/medialog.popupworkflow-0.7.3/medialog/popupworkflow/browser/javascript/popupworkflow.js
@@ -0,0 +1,23 @@
+
+jq(document).ready(function(){
+    jq(document).ready(function(){
+        // add rel tag for all links with class 'prettyPhoto'
+        jq("a.prettyPhoto").attr({"rel": "prettyPhoto"});
+        
+        // add iframe attributes for links for popupworkflow'
+        jq(".state-popuppublished").attr("href", function() { 
+            return this.href + "/placeit?iframe=true&width=75%&height=75%";
+        }).attr({"rel": "prettyPhoto"});
+        
+        // add iframe attributes for all links with class 'prettyPhotoIframe'
+        jq("a.prettyPhotoIframe").attr("href", function() { 
+            return this.href + "?iframe=true&width=75%&height=75%";
+        }).attr({"rel": "prettyPhoto"});
+
+
+        // enable prettyPhoto
+        jq("a[rel^='prettyPhoto']").prettyPhoto({theme:'light_rounded'});
+    });
+});
+
+
