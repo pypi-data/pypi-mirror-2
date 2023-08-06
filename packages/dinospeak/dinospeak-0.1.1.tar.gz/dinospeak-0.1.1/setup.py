3e473ada76602a79bb05dc9dcf70910678fe7268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package dinospeak/0.1.1/dinospeak-0.1.1.tar.gz

---
+++ packages/dinospeak/dinospeak-0.1.1.tar.gz/dinospeak-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from distutils.core import setup, Extension
+
+readme_file = open(u'README')
+long_description = readme_file.read()
+
+setup(
+    name = "dinospeak",
+    description = ("Dinosaur language translation library"),
+    long_description = long_description,
+    version = "0.1.1",
+    author = 'Dimitris Glezos',
+    author_email = 'glezos@indifex.com',
+    install_requires = ["smaz"],
+    zip_safe = False,
+    package_data = {
+        '': ['README', 'LICENSE']
+    },
+    packages = find_packages(),
+)
