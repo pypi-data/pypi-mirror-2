6103d881b41983dacf3112acbce717e7abb9a856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:05 +0000

    Add package django-imagekit/0.3.4/django-imagekit-0.3.4.tar.gz

---
+++ packages/django-imagekit/django-imagekit-0.3.4.tar.gz/django-imagekit-0.3.4/setup.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='django-imagekit',
+    version='0.3.4',
+    description='Automated image processing for Django models.',
+    author='Justin Driscoll',
+    author_email='justin@driscolldev.com',
+    url='http://github.com/jdriscoll/django-imagekit/',
+    packages=[
+        'imagekit',
+        'imagekit.management',
+        'imagekit.management.commands'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Utilities'
+    ]
+)
+
+
