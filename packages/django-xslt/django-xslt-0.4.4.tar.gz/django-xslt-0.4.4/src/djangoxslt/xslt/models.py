f9143559f066e8c5f0b024f2441407bce8587f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:09 +0000

    Add package django-xslt/0.4.4/django-xslt-0.4.4.tar.gz

---
+++ packages/django-xslt/django-xslt-0.4.4.tar.gz/django-xslt-0.4.4/src/djangoxslt/xslt/models.py
@@ -0,0 +1,25 @@
+"""Models for XSLT.
+
+These are purely for testing the XSLT code.
+"""
+
+from django.db import models
+from managers import RenderingManager
+
+# A test manager
+class XSLTTestManager(RenderingManager):
+    pass
+
+# A test model
+class XSLTTestModel(models.Model):
+    name = models.CharField(max_length=50)
+    about = models.TextField()
+    count = models.IntegerField()
+
+    # Setup the manager to be a rendering manager
+    objects = XSLTTestManager()
+
+    def __xml__(self):
+        return "<name>%s</name>" % self.name
+
+# End
---
+++ packages/django-xslt/django-xslt-0.4.4.tar.gz/django-xslt-0.4.4/src/djangoxslt/xslt/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns(
+    'djangoxslt.xslt.views',
+    url(r'^testtransform/(?P<page>[A-Z-a-z0-9_-]+)/$', 'page', {"namespace": "testtransform_"}),
+    )
