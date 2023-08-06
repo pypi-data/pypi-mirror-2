49d131957b171e2ac67d8a8c1527f4fd004a8943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:04 +0000

    Add package PyZen/0.2/PyZen-0.2.tar.gz

---
+++ packages/PyZen/PyZen-0.2.tar.gz/PyZen-0.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'PyZen',
+    version = '0.2',
+    packages = find_packages(),
+    author = 'Noah Kantrowitz',
+    author_email = 'noah@coderanger.net',
+    description = 'Continuous testing for paranoid developers.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    license = 'BSD',
+    keywords = 'test unittest continuous django',
+    url = 'http://github.com/coderanger/pyzen',
+    classifiers = [
+        #'Development Status :: 1 - Planning',
+        #'Development Status :: 2 - Pre-Alpha',
+        #'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    zip_safe = False,
+    entry_points = {
+        'distutils.commands': [
+            'zen = pyzen.distutils:zen',
+        ],
+        'console_scripts': [
+            'pyzen = pyzen.simple:main',
+        ],
+    },
+)
