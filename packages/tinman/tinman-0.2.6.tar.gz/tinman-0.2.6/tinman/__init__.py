5e28a10a1e69dab0f55840acb3a8e042578e279a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:47 +0000

    Add package tinman/0.2.6/tinman-0.2.6.tar.gz

---
+++ packages/tinman/tinman-0.2.6.tar.gz/tinman-0.2.6/tinman/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Core Tinman imports
+"""
+__author__ = 'Gavin M. Roy'
+__email__ = '<gmr@myyearbook.com>'
+__since__ = '2011-03-14'
+__version__ = "0.2.6"
+
+__all__ = ['tinman.application',
+           'tinman.cache',
+           'tinman.cli',
+           'tinman.clients',
+           'tinman.utils',
+           'tinman.whitelist']
+
+def main(*args):
+
+    # Import our CLI parser
+    from tinman import cli
+
+    # Run the main routine
+    process = cli.TinmanProcess()
+    process.run()
+
+if __name__ == "__main__":
+
+    # Run our CLI process
+    main()
