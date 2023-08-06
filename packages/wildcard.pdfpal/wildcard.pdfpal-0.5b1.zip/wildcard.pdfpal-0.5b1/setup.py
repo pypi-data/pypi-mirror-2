b4933bf135038f3a44613b6d2f32b9c3524a2483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:41 +0000

    Add package wildcard.pdfpal/0.5b1/wildcard.pdfpal-0.5b1.zip

---
+++ packages/wildcard.pdfpal/wildcard.pdfpal-0.5b1.zip/wildcard.pdfpal-0.5b1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.5b1'
+
+setup(name='wildcard.pdfpal',
+      version=version,
+      description="PDF Thumbnail generation, OCR indexing and extra views integrated with plone.app.async",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='pdf plone thumbnail ocr async',
+      author='Nathan Van Gheem',
+      author_email='nathan.vangheem@wildcardcorp.com',
+      url='http://svn.plone.org/svn/plone/plone.example',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['wildcard'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'archetypes.schemaextender'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """
+      )
