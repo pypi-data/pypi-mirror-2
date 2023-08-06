3aee41b4df9849ed1ff857150f3484e3dd4b5e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:10 +0000

    Add package perc/1.0.3/perc-1.0.3.tar.gz

---
+++ packages/perc/perc-1.0.3.tar.gz/perc-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'perc',
+        version = '1.0.3',
+        py_modules = ['perc'],
+        author = 'AlienDev',
+        author_email = 'yb@aliendev.com',
+        url = 'http://www.aliendev.com',
+        description = 'This module calculates percentages (based on two numbers) and gives the option to include a decimal point declaration while defaulting to two',
+    )
