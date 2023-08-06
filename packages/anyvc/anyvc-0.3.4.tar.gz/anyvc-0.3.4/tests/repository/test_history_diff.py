cd09069b541ae6845db3dfb0e48aa49c5abc7d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:18 +0000

    Add package anyvc/0.3.4/anyvc-0.3.4.tar.gz

---
+++ packages/anyvc/anyvc-0.3.4.tar.gz/anyvc-0.3.4/tests/repository/test_history_diff.py
@@ -0,0 +1,11 @@
+
+
+def test_diff_create_simple(repo):
+
+    with repo.transaction(author='test', message='test') as root:
+        with root.join('test.txt').open('w') as f:
+            f.write('test')
+
+    head = repo.get_default_head()
+    diff = head.get_parent_diff()
+    assert '+test' in diff
