d3f82c1bbcce0e4de822a691aed912ee62332380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:03 +0000

    Add package ghsync/0.2.2/ghsync-0.2.2.tar.gz

---
+++ packages/ghsync/ghsync-0.2.2.tar.gz/ghsync-0.2.2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from distutils.core import setup
+
+
+
+if sys.argv[-1] == "publish":
+    os.system('python setup.py sdist upload')
+    sys.exit()
+    
+required = ['github2']
+
+
+setup(
+    name='ghsync',
+    version='0.2.2',
+    description='GitHub Syncer. Clones or Pulls all GitHub repos.',
+    long_description=open('README.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/ghsync',
+    packages= [
+        'ghsync',
+    ],
+    install_requires=required,
+    license='ISC',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+    entry_points={
+        'console_scripts': [
+            'ghsync = ghsync.core:run',
+        ],
+    }  
+)
