471524f8eab7d912616efb6ab55f839033bb2b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:23 +0000

    Add package js.jquery_elastic/1.6.5/js.jquery_elastic-1.6.5.tar.gz

---
+++ packages/js.jquery_elastic/js.jquery_elastic-1.6.5.tar.gz/js.jquery_elastic-1.6.5/js/jquery_elastic/__init__.py
@@ -0,0 +1,9 @@
+from fanstatic import Library, Resource
+from js.jquery import jquery
+
+library = Library('jquery_elastic', 'resources')
+
+elastic = Resource(library,
+                   'jquery.elastic.source.js',
+                   minified='jquery.elastic.js',
+                   depends=[jquery])
