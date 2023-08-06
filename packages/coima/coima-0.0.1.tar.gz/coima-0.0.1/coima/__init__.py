35f7f94bb2789b348cdc7c436ec4689742351c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package coima/0.0.1/coima-0.0.1.tar.gz

---
+++ packages/coima/coima-0.0.1.tar.gz/coima-0.0.1/coima/__init__.py
@@ -0,0 +1,40 @@
+import re
+
+class Template(object):
+
+    def __init__(self, to_substitute, f):
+        self.to_substitute = to_substitute
+        self.f = f
+        self.cache = []
+
+
+    def file_obj(self):
+        try:
+            obj = open(self.f).readlines()
+            return obj
+        except IOError, error:
+            print error
+        except OSError, error:
+            print error
+
+
+    def line_replacement(self):
+        for line in self.file_obj():
+            items = [i for i in line.split()]
+            for position, item in enumerate(items):
+                for i, part in enumerate(re.split(r'\{\{(.*?)\}\}', item)):
+                    if i % 2:
+                        replace = self.to_substitute.get(part)
+                        if replace:
+                            items[position] = replace 
+            self.cache.append(items)
+            
+
+    def render(self):
+        self.line_replacement()
+        for i in self.cache:
+            if len(i) > 0:
+                print ' '.join(i)
+            else:
+                print ''
+
