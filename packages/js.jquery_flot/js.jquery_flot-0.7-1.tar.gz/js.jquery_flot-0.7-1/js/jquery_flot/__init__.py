1b4ad3e0a2832d1b40db5cc785c05f8ad4a5c790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:43 +0000

    Add package js.jquery_flot/0.7-1/js.jquery_flot-0.7-1.tar.gz

---
+++ packages/js.jquery_flot/js.jquery_flot-0.7-1.tar.gz/js.jquery_flot-0.7-1/js/jquery_flot/__init__.py
@@ -0,0 +1,15 @@
+import fanstatic
+import js.jquery
+
+library = fanstatic.Library('flot', 'resources')
+
+def less_than_ie8_renderer(url):
+    return ('<!--[if lte IE 8]><script language="javascript" '
+        'type="text/javascript" src="%s"></script><![endif]-->') % url
+
+excanvas = fanstatic.Resource(library, 'excanvas.js',
+    minified='excanvas.min.js', renderer=less_than_ie8_renderer)
+
+
+flot = fanstatic.Resource(library, 'jquery.flot.js',
+    minified='jquery.flot.min.js', depends=[js.jquery.jquery, excanvas])
