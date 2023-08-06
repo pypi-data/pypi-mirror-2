cc5b034a318c7b15e3b65feab6a2e8d0365906fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:24 +0000

    Add package fred/1.2/fred-1.2.tar.gz

---
+++ packages/fred/fred-1.2.tar.gz/fred-1.2/fred/fred_api_key.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""
+FRED API documentation: http://api.stlouisfed.org/docs/fred/
+
+File for storing your FRED API key.
+"""
+
+API_KEY = '43fd8b0a00c5e3a195bd0b794f921e82'
