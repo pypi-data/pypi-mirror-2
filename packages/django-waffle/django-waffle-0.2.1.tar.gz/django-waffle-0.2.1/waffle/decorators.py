f62619b4e0ad0605434090723fcb65230e195755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:22 +0000

    Add package django-waffle/0.2.1/django-waffle-0.2.1.tar.gz

---
+++ packages/django-waffle/django-waffle-0.2.1.tar.gz/django-waffle-0.2.1/waffle/decorators.py
@@ -0,0 +1,22 @@
+from functools import wraps
+
+from django.http import Http404
+from django.utils.decorators import available_attrs
+
+from waffle import is_active
+
+
+def waffle(flag_name):
+    def decorator(view):
+        @wraps(view, assigned=available_attrs(view))
+        def _wrapped_view(request, *args, **kwargs):
+            if flag_name.startswith('!'):
+                active = is_active(request, flag_name[1:])
+            else:
+                active = is_active(request, flag_name)
+
+            if not active:
+                raise Http404
+            return view(request, *args, **kwargs)
+        return _wrapped_view
+    return decorator
