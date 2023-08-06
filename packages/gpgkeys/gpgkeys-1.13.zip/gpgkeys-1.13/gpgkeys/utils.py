4ff84cd55460210dbf1e846ba584edf98d3e4b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:50 +0000

    Add package gpgkeys/1.13/gpgkeys-1.13.zip

---
+++ packages/gpgkeys/gpgkeys-1.13.zip/gpgkeys-1.13/gpgkeys/utils.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def decode(text):
+    """Decode from the charset of the current locale."""
+    if sys.version_info[0] >= 3:
+        return text.decode(sys.getfilesystemencoding(), 'surrogateescape')
+    else:
+        return text.decode(sys.getfilesystemencoding(), 'replace')
+
+
+def encode(text):
+    """Encode to the charset of the current locale."""
+    if sys.version_info[0] >= 3:
+        return text.encode(sys.getfilesystemencoding(), 'surrogateescape')
+    else:
+        return text.encode(sys.getfilesystemencoding(), 'replace')
+
+
+def b(text):
+    """Python 2.5 doesn't know about byte literals."""
+    if sys.version_info[0] >= 3:
+        return text.encode('utf-8')
+    else:
+        return text
+
