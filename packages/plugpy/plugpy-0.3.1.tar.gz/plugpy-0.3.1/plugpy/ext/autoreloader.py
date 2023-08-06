5d353fbcbede06c6e11b0f93a345f2fef7adc617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:10 +0000

    Add package plugpy/0.3.1/plugpy-0.3.1.tar.gz

---
+++ packages/plugpy/plugpy-0.3.1.tar.gz/plugpy-0.3.1/plugpy/ext/autoreloader.py
@@ -0,0 +1,61 @@
+from plugpy import Plugin, log
+import os
+import sys
+import thread
+
+environ_key = 'RUN_AUTO_RELOAD_PLUGIN'
+
+def use_autoreload():
+    return os.environ.get(environ_key) == "true"
+
+def restart_with_reloader():
+    while True:
+        args = [sys.executable] + sys.argv
+        if sys.platform == "win32":
+            args = ['"%s"' % arg for arg in args]
+        new_environ = os.environ.copy()
+        new_environ[environ_key] = 'true'
+        exit_code = os.spawnve(os.P_WAIT, sys.executable, args, new_environ)
+        if exit_code != 3:
+            return exit_code
+
+
+class AutoReloadPlugin(object):
+    
+    require_reload = False
+    
+    def reloader_run(self, main_loop, reloader_loop):
+        if use_autoreload():
+            
+            thread.start_new_thread(reloader_loop, ())
+            try:
+                main_loop()
+            except KeyboardInterrupt:
+                if self.require_reload:
+                    sys.exit(3)
+        else:
+            try:
+                sys.exit(restart_with_reloader())
+            except KeyboardInterrupt:
+                pass
+    
+    def on_start(self):
+        log("auto reloader start")
+        self.reloader_run(self.main_loop, self.reloader_loop)
+
+    def on_reload(self):
+        log("reload")
+        self.require_reload = True
+        thread.interrupt_main()
+
+    def on_stop(self):
+        log("stop")
+        thread.interrupt_main()
+    
+    def main_loop(self):
+        pass
+
+    def reloader_loop(self):
+        pass
+
+
---
+++ packages/plugpy/plugpy-0.3.1.tar.gz/plugpy-0.3.1/plugpy/ext/filewatcher.py
@@ -0,0 +1,147 @@
+from plugpy import Plugin, log, abspath
+import os
+import os.path
+import fnmatch 
+import thread
+import time
+
+default_ignore = ['*/.hg', '*/.hg/*', '*/.svn/*', '*/.svn', '*/CVS/*', '*.pyc']
+default_ext = ['.py']
+default_interval = '3'
+default_files = ["./"]
+
+
+class FileWatchPlugin(object):
+    
+    _ignore = ['*/.svn/*', '*/.svn', '*/CVS/*', '*.pyc']
+    running = False
+
+    def __init__(self, **config):
+        self.files = {}
+        
+        observe_ext = config.get('observe_ext', default_ext)
+        if not type(observe_ext) is list:
+            observe_ext = [observe_ext]
+        self._observe_ext = observe_ext
+        log('observe_ext %s' % self._observe_ext)
+
+        self._interval = int(config.get('interval', default_interval))
+        log('interval %s' % self._interval)
+        
+        self._ignore = default_ignore
+        self._ignore.extend(config.get('ignore', []))
+        log('ignore %s' % self._ignore)
+        
+        observe_file = config.get('observe_files', default_files)
+        if not type(observe_file) is list:
+            observe_file = [observe_file]
+
+        observe_file = map(abspath, observe_file)
+        log('observe_files %s' % observe_file)
+        self.observe(observe_file)
+        
+    def on_start(self, use_thread=False):
+        if use_thread:
+            thread.start_new_thread(self.run, ())
+        else:
+            self.run()
+
+    def run(self):
+        if self.running:
+            return
+
+        self.running = True
+        while self.running:
+            self.check()
+            time.sleep(self._interval)
+
+    def observe(self, files):
+        for file in files:
+            file = file.strip()
+            if os.path.isdir(file):
+                if not self.ignore_filter(file):
+                    self.save_mtime(file)
+                
+                self.dir_observe(file)
+            else:
+                path = os.path.abspath(file)
+                if not self.ignore_filter(path):
+                    self.save_mtime(path)
+        log('find files %s' % self.files.keys())
+
+    def dir_observe(self, dir):
+
+        created = []
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                path = os.path.join(root, file)
+                if not self.ignore_filter(path):
+                    newfile = self.save_mtime(path)
+                    if newfile:
+                        created.append(newfile)
+            for dir in dirs:
+                path = os.path.join(root, dir)
+                if not self.ignore_filter(path):
+                    newfile = self.save_mtime(path)
+                    if newfile:
+                        created.append(newfile)
+        return created
+                
+    def save_mtime(self, path, force=False):
+        if os.path.exists(path):
+            match = False
+            if not os.path.isdir(path):
+                for ext in self._observe_ext:
+                    if path.endswith(ext.strip()):
+                        match = True
+                        break
+                if not match:
+                    return 
+            if force or not path in self.files:
+                self.files[path] = os.stat(path).st_mtime
+                return path
+    
+    def check(self):
+        created = []
+        modified = []
+        deleted = []
+        for path, mtime in self.files.items():
+            c, m, d = self.is_modify(path, mtime)
+            created.extend(c)
+            modified.extend(m)
+            deleted.extend(d)
+
+        if created or modified or deleted:
+            log('modify files created %s modified %s deleted %s' % (created,
+                modified, deleted))
+            self.on_modified(created=created, modified=modified,
+                    deleted=deleted)
+    
+    def on_modified(self, created, modified, deleted):
+        pass
+    
+    def is_modify(self, path, mtime):
+
+        created = []
+        modified = []
+        deleted = []
+        if not os.path.exists(path):
+            del self.files[path]
+            deleted.append(path)
+        else:
+            if os.stat(path).st_mtime > mtime:
+                self.save_mtime(path, True)
+                if os.path.isdir(path):
+                    modified.extend(self.dir_observe(path))
+                else:
+                    modified.append(path)
+
+        return created, modified, deleted
+    
+    def ignore_filter(self, path):
+        return reduce(lambda x,y:x or y,
+                [fnmatch.fnmatch(path, i) for i in self._ignore])
+    
+    def on_stop(self):
+        self.running = False
+
