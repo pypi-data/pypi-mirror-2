9c632a33b08e330bd9ca36be8972e90254d25f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:56 +0000

    Add package py3o.template/0.2/py3o.template-0.2.tar.gz

---
+++ packages/py3o.template/py3o.template-0.2.tar.gz/py3o.template-0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.2'
+
+setup(name='py3o.template',
+      version=version,
+      description="An easy solution to design reports using OpenOffice",
+      long_description=open("README.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='OpenOffice templating PDF',
+      author='Florent Aide',
+      author_email='florent.aide@gmail.com',
+      url='',
+      license='BSD License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      namespace_packages=['py3o'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          'lxml',
+          'genshi',
+          ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      test_suite = 'nose.collector',
+      )
+2
