ddf0b4c153bdfd050e2cbaffcd521ff952c583b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:37 +0000

    Add package gaeframework/2.0.6/gaeframework-2.0.6.zip

---
+++ packages/gaeframework/gaeframework-2.0.6.zip/gaeframework-2.0.6/project1/gae/exceptions.py
@@ -0,0 +1,10 @@
+'''
+All available exceptions in GAE framework engine.
+'''
+class PageNotFound(Exception):
+    '''404 error'''
+    pass
+
+class AccessDenied(Exception):
+    '''500 error'''
+    pass
