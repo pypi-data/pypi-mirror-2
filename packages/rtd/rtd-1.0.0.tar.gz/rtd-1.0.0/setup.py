ea975411c651eaf2e92f6e43a974abe3e3f092df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:03 +0000

    Add package rtd/1.0.0/rtd-1.0.0.tar.gz

---
+++ packages/rtd/rtd-1.0.0.tar.gz/rtd-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import codecs
+
+setup(
+    name='rtd',
+    version='1.0.0',
+    author='Eric Holscher',
+    author_email='eric@ericholscher.com',
+    url='http://github.com/ericholscher/rtd',
+    scripts=['bin/rtd'],
+    license='BSD',
+    description='Find documentation on Read the Docs!',
+    long_description=codecs.open("README.rst", "r", "utf-8").read()
+)
+
