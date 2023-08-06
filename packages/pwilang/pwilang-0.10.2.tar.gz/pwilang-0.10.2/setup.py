b64f04ca514aeb488e6db757bc8135c9dd6c5250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:27 +0000

    Add package pwilang/0.10.2/pwilang-0.10.2.tar.gz

---
+++ packages/pwilang/pwilang-0.10.2.tar.gz/pwilang-0.10.2/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pwilang",
+    version = "0.10.2",
+    author = "Christophe Eymard",
+    author_email = "christophe@ravelsoft.com",
+    description = ("A text processor with its own syntax a little nicer than html that uses jinja for complex text rendering."),
+    license = "GPLv3",
+    keywords = "text processor html jinja",
+    url = "http://code.google.com/p/pwilang/",
+    packages=['pwilang'],
+    scripts=["pwilang/pwilang"],
+    long_description=read('README'),
+    requires=['jinja2', 'ply', 'beautifulsoup'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+    ],
+)
+
