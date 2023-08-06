c85fdcbb5c417fd58079a773e6a7c68dbf207e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:16 +0000

    Add package marrow.util/1.1.0/marrow.util-1.1.0-py2.7.egg

---
+++ packages/marrow.util/marrow.util-1.1.0-py2.7.egg/marrow/util/release.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+"""Release information about Marrow."""
+
+
+name = "marrow.util"
+version = "1.1.0"
+release = "1.1"
+
+summary = "Commonly shared Python utility subclasses and functions."
+description = """"""
+author = "Alice Bevan-McGregor"
+email = "alice+marrow@gothcandy.com"
+url = "http://github.com/pulp/marrow.util"
+download_url = "http://cheeseshop.python.org/pypi/marrow.util/"
+copyright = "2009-2011, Alice Bevan-McGregor"
+license = "MIT"
