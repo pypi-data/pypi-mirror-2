423b00de0aacb5041e7d819d4fd14c3dac670bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:02 +0000

    Add package sword2/0.1.14.901571025c23/sword2-0.1.14.901571025c23.tar.gz

---
+++ packages/sword2/sword2-0.1.14.901571025c23.tar.gz/sword2-0.1.14.901571025c23/sword2/exceptions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Provides various Exception classes to match HTTP error code responses.
+"""
+
+class HTTPResponseError(Exception):
+    """Generic exception for http codes greater than 399 and less than 599 """
+    def __init__(self, response=None):
+        self.response = response
+        
+class ServerError(HTTPResponseError):
+    """ for http error codes 500 and up """
+    pass
+
+class NotAuthorised(HTTPResponseError):
+    pass
+
+class Forbidden(HTTPResponseError):
+    pass
+
+class RequestTimeOut(HTTPResponseError):
+    pass
+
+class NotFound(HTTPResponseError):
+    pass
+
+class PackagingFormatNotAvailable(HTTPResponseError):
+    pass
+    
