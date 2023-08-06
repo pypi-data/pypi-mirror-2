156b43859bc5c37bca8d3967a911e86dd7c672fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:46 +0000

    Add package django-url-methods/0.1.0/django-url-methods-0.1.0.tar.gz

---
+++ packages/django-url-methods/django-url-methods-0.1.0.tar.gz/django-url-methods-0.1.0/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.  
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="django-url-methods",
+    version="0.1.0",
+    description=("Django-url-methods provide various methods to work with urls"),
+    license="LGPL",
+    keywords="django url",
+
+    author="Alexander Ivanov",
+    author_email="alexander.ivanov@redsolution.ru",
+
+    maintainer='Alexander Ivanov',
+    maintainer_email='alexander.ivanov@redsolution.ru',
+
+    url="http://packages.python.org/django-url-methods",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Freely Distributable',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Natural Language :: Russian',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+    packages=find_packages(exclude=['example', 'example.*']),
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+    long_description=open('README').read(),
+)
