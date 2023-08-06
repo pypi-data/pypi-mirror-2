116de7fe427faadfb1cd201256a3e3d2cef6c2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:53 +0000

    Add package bahai.theme/1.0.2/bahai.theme-1.0.2.tar.gz

---
+++ packages/bahai.theme/bahai.theme-1.0.2.tar.gz/bahai.theme-1.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.2'
+
+setup(name='bahai.theme',
+      version=version,
+      description="Theme for Bahai.dk site",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['bahai'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+           'plonetheme.classic',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
