eeeb21d53f13ddad18e8055a941a142c72cf3162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:31 +0000

    Add package django-payfast/0.1.8/django-payfast-0.1.8.tar.gz

---
+++ packages/django-payfast/django-payfast-0.1.8.tar.gz/django-payfast-0.1.8/payfast/conf.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+
+MERCHANT_ID = settings.PAYFAST_MERCHANT_ID
+MERCHANT_KEY = settings.PAYFAST_MERCHANT_KEY
+
+TEST_MODE = getattr(settings, 'PAYFAST_TEST_MODE', False)
+
+LIVE_SERVER = 'https://www.payfast.co.za'
+SANDBOX_SERVER = 'https://sandbox.payfast.co.za'
+SERVER = SANDBOX_SERVER if TEST_MODE else LIVE_SERVER
+
+PROCESS_URL = SERVER + '/eng/process'
+
+REQUIRE_AMOUNT_MATCH = getattr(settings, 'PAYFAST_REQUIRE_AMOUNT_MATCH', True)
+USE_POSTBACK = getattr(settings, 'PAYFAST_USE_POSTBACK', True)
+
+IP_HEADER = getattr(settings, 'PAYFAST_IP_HEADER', 'REMOTE_ADDR') # request.META key with client ip address
+IP_ADDRESSES = getattr(settings, 'PAYFAST_IP_ADDRESSES', ['196.33.227.224', '196.33.227.225'])
+
