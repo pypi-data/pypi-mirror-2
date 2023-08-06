d25a1ec43479c2d6838fef48a640df52aece4472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:57 +0000

    Add package bp.logging/0.2-dev/bp.logging-0.2-dev.zip

---
+++ packages/bp.logging/bp.logging-0.2-dev.zip/bp.logging-0.2-dev/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version  = '0.2-dev'
+requires = [
+    'setuptools',
+    # -*- Extra requirements: -*-
+]
+
+setup(
+    name                    = 'bp.logging',
+    version                 = version,
+    description             = "Reduce logging related repetition in your code",
+    long_description        = open("README.txt").read() + "\n\n" + open(os.path.join("docs", "HISTORY.txt")).read() + "\n\n" + open(os.path.join("docs", "LICENSE.txt")).read(),
+    classifiers             = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+    ],
+    keywords                = 'logging',
+    author                  = 'Andreas Kaiser',
+    author_email            = 'disko@binary-punks.com',
+    url                     = 'https://github.com/disko/bp.logging',
+    license                 = 'BSD',
+    packages                = find_packages(exclude=['ez_setup']),
+    namespace_packages      = ['bp'],
+    include_package_data    = True,
+    zip_safe                = False,
+    install_requires        = requires,
+    entry_points            = """
+    # -*- Entry points: -*-
+    """,
+)
