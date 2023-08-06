7e3de489fdc8cbe06732e2daeb0e414166549672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:01 +0000

    Add package pidsim/1.0rc5/pidsim-1.0rc5.tar.gz

---
+++ packages/pidsim/pidsim-1.0rc5.tar.gz/pidsim-1.0rc5/pidsim/error.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+    pidsim.error
+    ~~~~~~~~~~~~
+
+    Error handling module.
+    
+    :copyright: 2009-2010 by Rafael Goncalves Martins
+    :license: GPL-2, see LICENSE for more details.
+"""
+
+__all__ = ['ControlSystemsError']
+
+
+class ControlSystemsError(Exception):
+    """ControlSystemsError class
+    
+    A simple error handling class, extends the Exception class, from
+    the Python standard library.
+    
+    """
+    
+    pass
