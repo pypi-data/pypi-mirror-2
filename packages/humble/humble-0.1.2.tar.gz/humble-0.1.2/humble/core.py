f12b5d4c9b6dd0bdf27b8dfe7c552d2bbfd8e586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:09 +0000

    Add package humble/0.1.2/humble-0.1.2.tar.gz

---
+++ packages/humble/humble-0.1.2.tar.gz/humble-0.1.2/humble/core.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+"""
+humble.cli
+~~~~~~~~~~
+
+This module contains the main API interface for Humble.
+
+"""
+
+
+import json
+
+from github2.client import Github
+import requests
+
+
+github = Github()
+
+
+__title__ = 'humble'
+__version__ = '0.1.2'
+__build__ = 0x000102
+__author__ = 'Kenneth Reitz'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2011 Kenneth Reitz'
+__docformat__ = 'restructuredtext'
+
+
+GITHUB_API = 'http://github.com/api/v1/json/'
+
+
+
+def get_info_for(username):
+    try:
+        return github.users.show(username)
+    except RuntimeError:
+        return None
+
+
+def get_repos_for(username):
+    
+    r = requests.get('{0}{1}'.format(GITHUB_API, username))
+    repos = json.loads(r.content).get('user', {}).get('repositories', None)
+    
+    if repos:
+        def _sort(repo):
+            return repo.get('watchers', '0')
+    
+        return sorted(repos, key=_sort, reverse=True)
+    else:
+        return []
+    
+    
+# print get_repos_for('kennethreitz')
+# print dir(get_info_for('kennethreitz'))
+
+# print get_info_for('kennethreitz').public_repo_count
---
+++ packages/humble/humble-0.1.2.tar.gz/humble-0.1.2/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from distutils.core import setup
+
+
+def publish():
+    """Publish to PyPi"""
+    os.system("python setup.py sdist upload")
+
+if sys.argv[-1] == "publish":
+    publish()
+    sys.exit()
+
+required = ['clint>=0.2.1', 'github2', 'requests']
+
+if sys.version_info[0:1] < (2, 6):
+    required.append('simplejson')
+
+setup(
+    name='humble',
+    version='0.1.2',
+    description='Shows stats on a given GitHub user.',
+    long_description=open('README.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/humble',
+    packages= ['humble'],
+    install_requires=required,
+    license='ISC',
+    classifiers=(
+#       'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
+    entry_points = {
+        'console_scripts': [
+            'humble = humble.cli:start'
+        ]
+    }
+)
