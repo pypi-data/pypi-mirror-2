50d8d46a0d496bd8765f9904da43f524affcbae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:39 +0000

    Add package z3c.schema/0.7.1/z3c.schema-0.7.1.tar.gz

---
+++ packages/z3c.schema/z3c.schema-0.7.1.tar.gz/z3c.schema-0.7.1/setup.py
@@ -0,0 +1,74 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 121238 2011-04-03 19:53:19Z janjaapdriessen $
+"""
+import os
+import xml.sax.saxutils
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    text = open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    return text + '\n\n'
+
+setup(
+    name = 'z3c.schema',
+    version='0.7.1',
+    author = 'Zope Community',
+      author_email = "zope-dev@zope.org",
+      description = "Additional schema fields for Zope 3",
+    long_description=(
+        read('README.txt') +
+        '.. contents::\n\n'+
+        read('CHANGES.txt') +
+        read('src', 'z3c', 'schema', 'baseurl', 'README.txt') +
+        read('src', 'z3c', 'schema', 'dateselect', 'README.txt') +
+        read('src', 'z3c', 'schema', 'email', 'README.txt') +
+        read('src', 'z3c', 'schema', 'hostname', 'README.txt') +
+        read('src', 'z3c', 'schema', 'ip', 'README.txt') +
+        read('src', 'z3c', 'schema', 'optchoice', 'README.txt') +
+        read('src', 'z3c', 'schema', 'payments', 'README.txt') +
+        read('src', 'z3c', 'schema', 'regex', 'README.txt')
+        ),
+    license = 'ZPL 2.1',
+    keywords = 'zope zope3 z3c schema',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.schema',
+    packages = find_packages('src'),
+    include_package_data=True,
+    package_dir = {'':'src'},
+    namespace_packages=['z3c',],
+    extras_require = dict(
+      test=[
+          'zope.testing',
+           ]
+    ),
+    install_requires = [
+      'setuptools',
+      'zope.i18nmessageid',
+      'zope.interface',
+      'zope.schema >= 3.6',
+      ],
+    zip_safe=False,
+)
---
+++ packages/z3c.schema/z3c.schema-0.7.1.tar.gz/z3c.schema-0.7.1/src/z3c/schema/hostname/__init__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: __init__.py 71298 2006-11-24 03:47:49Z rogerineichen $
+"""
+
+from z3c.schema.hostname.interfaces import *
+from z3c.schema.hostname.field import isValidHostName
+from z3c.schema.hostname.field import HostName
