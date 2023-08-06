555ba4300e6a1ce063e3e8b796c5652a582c03b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:36 +0000

    Add package pypdflib/0.1.a1/pypdflib-0.1.a1.tar.gz

---
+++ packages/pypdflib/pypdflib-0.1.a1.tar.gz/pypdflib-0.1.a1/setup.py
@@ -0,0 +1,31 @@
+import sys
+import os
+from setuptools import setup, find_packages
+
+name='pypdflib'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description=(
+        read('README.md')
+        + '\n' 
+        )
+
+setup(
+    name=name,
+    version='0.1.a1',
+    url='http://github.com/santhoshtr/pypdflib',
+    license='LGPL 3.0',
+    description='PangoCairo based Python PDF Library',
+    author='Santhosh Thottingal',
+    author_email='santhosh.thottingal@gmail.com',
+    long_description=long_description,
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    setup_requires = ['setuptools-git'],
+    namespace_packages=['pypdflib'],
+    include_package_data = True,
+    install_requires=['setuptools'],
+    zip_safe = False,
+    )
---
+++ packages/pypdflib/pypdflib-0.1.a1.tar.gz/pypdflib-0.1.a1/src/pypdflib/utils/__init__.py
@@ -0,0 +1,2 @@
+from hyphenation import *
+from normalizer import normalize
