1c5b0a14dab092ae7bca38763b913569db64b3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:00 +0000

    Add package beeswithmachineguns/0.1.0/beeswithmachineguns-0.1.0.tar.gz

---
+++ packages/beeswithmachineguns/beeswithmachineguns-0.1.0.tar.gz/beeswithmachineguns-0.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='beeswithmachineguns',
+      version='0.1.0',
+      description='A utility for arming (creating) many bees (micro EC2 instances) to attack (load test) targets (web applications).',
+      author='Christopher Groskopf',
+      author_email='cgroskopf@tribune.com',
+      url='http://github.com/newsapps/beeswithmachineguns',
+      license='MIT',
+      packages=['beeswithmachineguns'],
+      scripts=['bees'],
+      install_requires=['boto', 'paramiko'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Testing :: Traffic Generation',
+          'Topic :: Utilities',
+          ],
+     )
