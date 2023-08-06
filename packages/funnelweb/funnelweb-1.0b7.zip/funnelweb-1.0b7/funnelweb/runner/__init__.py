b3cc33ef0442677171338796e8248841f8a1aa1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:11 +0000

    Add package funnelweb/1.0b7/funnelweb-1.0b7.zip

---
+++ packages/funnelweb/funnelweb-1.0b7.zip/funnelweb-1.0b7/funnelweb/runner/__init__.py
@@ -0,0 +1,118 @@
+from collective.transmogrifier.tests import registerConfig
+from collective.transmogrifier.transmogrifier import Transmogrifier
+from pkg_resources import resource_string, resource_filename
+from collective.transmogrifier.transmogrifier import configuration_registry
+import funnelweb
+from optparse import OptionParser, OptionGroup
+
+import sys
+import ConfigParser
+
+import logging
+try:
+    from Zope2.App import zcml
+except:
+    from Products.Five import zcml
+
+logging.basicConfig(level=logging.INFO)
+                    
+
+class Context:
+    pass
+
+
+class NoErrorParser(OptionParser):
+    def error(self):
+        pass
+ 
+def runner(args={}):
+    parser = OptionParser()
+    
+    parser.add_option("-p", "--pipeline", dest="pipeline",
+                  help="Transmogrifier pipeline.cfg to use", metavar="FILE")
+    
+    pargs = [arg for arg in sys.argv[1:] if arg in ['--pipeline','-p']]
+    (options, cargs) = parser.parse_args(pargs)
+    if options.pipeline is None:
+        config = resource_filename(__name__,'pipeline.cfg')
+    else:
+        config = options.get('pipeline')
+    cparser = ConfigParser.RawConfigParser()
+    cparser.read(config)
+    pipeline = [p.strip() for p in cparser.get('transmogrifier','pipeline').split()]
+    for section in pipeline:
+        if section == 'transmogrifier':
+            continue
+        if cparser.has_option(section,'@doc'):
+            doc = cparser.get(section,'@doc')
+        else:
+            doc = ''
+        group = OptionGroup(parser, section, doc)
+        for key,value in cparser.items(section):
+            if key.startswith('@'):
+                if key == '@doc':
+                    continue
+                metavar,_,help = value.partition(': ')
+                if metavar.upper() == metavar:
+                    action = "store"
+                else:
+                    action = "store_true"
+                    help = value
+                group.add_option("--%s:%s"%(section,key[1:]), action=action,
+                                             help=help,
+                                             metavar=metavar)
+        parser.add_option_group(group)
+    pargs = [arg for arg in sys.argv[1:] if not arg.startswith('--template') ]
+    (options, cargs) = parser.parse_args(pargs)
+
+    
+    cargs = {}
+    for k,_,v in [a.partition('=') for a in sys.argv[1:]]:
+        k = k.lstrip('--')
+        if ':' in k:
+            part,_,key = k.partition(':')
+            if key.lower()=='debug':
+                logger = logging.getLogger(part)
+                logger.setLevel(logging.DEBUG)
+            else:
+                section = cargs.setdefault(part, {})
+                if key in section:
+                    section[key] = '%s\n%s' % (section[key],v)
+                else:
+                    section[key] = v
+        else:
+            cargs[k] = v
+    for k,v in cargs.items():
+        args.setdefault(k, {}).update(v)
+
+    config = resource_filename(__name__,'pipeline.cfg')
+    if args.get('pipeline') == '':
+        f = open(config)
+        print f.read()
+        f.close()
+        return
+    else:
+        config = args.get('pipeline', config)
+
+    zcml.load_config('configure.zcml', funnelweb)
+
+
+    context = Context()
+    configuration_registry.registerConfiguration(
+        u'transmogrify.config.funnelweb',
+        u"",
+        u'', config)
+
+    transmogrifier = Transmogrifier(context)
+    overrides = {}
+    if type(args) == type(''):
+      for arg in args:
+        section,keyvalue = arg.split(':',1)
+        key,value = keyvalue.split('=',1)
+        overrides.setdefault('section',{})[key] = value
+    else:
+        overrides = args
+        
+    transmogrifier(u'transmogrify.config.funnelweb', **overrides)
+
+
---
+++ packages/funnelweb/funnelweb-1.0b7.zip/funnelweb-1.0b7/setup.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of funnelweb
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0b7'
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    'Contributors\n' 
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' + 
+    read('CHANGES.txt')
+    + '\n' +
+   'Download\n'
+    '********\n'
+    )
+entry_point = 'funnelweb.recipe:Recipe'
+entry_points = {"zc.buildout": ["default = %s" % entry_point],
+                'console_scripts': ['funnelweb = funnelweb.runner:runner']}
+
+tests_require=['zope.testing', 'zc.buildout']
+
+setup(name='funnelweb',
+      version=version,
+      description="Crawl and parse static sites and import to Plone",
+      long_description=long_description,
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Zope Public License',
+        ],
+      keywords='buildout crawler spider plone',
+      author='Dylan Jay',
+      author_email='software@pretaweb.com',
+      url='http://pypi.python.org/pypi/funnelweb',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['funnelweb'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout',
+                        'zc.recipe.egg',
+                        'collective.transmogrifier',
+                        'transmogrify.webcrawler>=1.0b6',
+                        'transmogrify.siteanalyser>=1.0b8',
+                        'transmogrify.htmlcontentextractor>=1.0b4',
+                        'transmogrify.pathsorter>=1.0b3',
+                        'transmogrify.ploneremote>=1.0b3',
+                        'Products.CMFCore',
+                'zope.app.pagetemplate',
+                'zope.app.component',
+          'z3c.autoinclude'
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'funnelweb.recipe.tests.test_docs.test_suite',
+      entry_points=entry_points,
+      )
