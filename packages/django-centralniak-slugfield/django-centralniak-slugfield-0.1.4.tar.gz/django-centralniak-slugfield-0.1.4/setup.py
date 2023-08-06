4dbfbd11e040b4fa8b85d487e3db0e40c7ef8133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:41 +0000

    Add package django-centralniak-slugfield/0.1.4/django-centralniak-slugfield-0.1.4.tar.gz

---
+++ packages/django-centralniak-slugfield/django-centralniak-slugfield-0.1.4.tar.gz/django-centralniak-slugfield-0.1.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='django-centralniak-slugfield',
+    version='0.1.4',
+    author='Piotr Kilczuk',
+    author_email='p.kilczuk@neumea.pl',
+    url='http://github.com/centralniak',
+    description = 'Provides more handy support for slugs in models',
+    packages=find_packages(),
+    provides=['django_centralniak_slugfield'],
+    classifiers=[
+        'Framework :: Django',
+        #'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
