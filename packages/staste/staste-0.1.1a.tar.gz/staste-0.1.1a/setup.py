0174a7e6512117011925caf576760ff16d37fd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:38 +0000

    Add package staste/0.1.1a/staste-0.1.1a.tar.gz

---
+++ packages/staste/staste-0.1.1a.tar.gz/staste-0.1.1a/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='staste',
+      version='0.1.1a',
+      description="Slightly complicated event tracker for your Django website.",
+      author='Valentin Golev',
+      author_email='v.golev@gmail.com',
+      url='http://staste.unfoldthat.com/',
+      packages=['staste', 'staste.charts'],
+     )
