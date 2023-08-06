fc5ef5a65bb11fece227128dcb77a2f49042d8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:05 +0000

    Add package jmbo-post/0.0.3/jmbo-post-0.0.3.tar.gz

---
+++ packages/jmbo-post/jmbo-post-0.0.3.tar.gz/jmbo-post-0.0.3/post/models.py
@@ -0,0 +1,16 @@
+from django.core.urlresolvers import reverse
+from jmbo.models import ModelBase
+from ckeditor.fields import RichTextField
+
+class Post(ModelBase):
+    content = RichTextField(
+        blank=True,
+        null=True,
+    )
+
+    class Meta():
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+    
+    def get_absolute_url(self):
+        return reverse('post_object_detail', kwargs={'slug': self.slug})
---
+++ packages/jmbo-post/jmbo-post-0.0.3.tar.gz/jmbo-post-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-post',
+    version='0.0.3',
+    description='Jmbo post app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-post',
+    packages = find_packages(),
+    install_requires = [
+        'django-ckeditor',
+        'jmbo',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
