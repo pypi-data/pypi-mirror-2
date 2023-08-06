0f1508a16241be30fe3a1007dd756b4dfda9fbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:10 +0000

    Add package M5/0.3.2/M5-0.3.2.tar.gz

---
+++ packages/M5/M5-0.3.2.tar.gz/M5-0.3.2/templates/m5.env.production.js
@@ -0,0 +1,13 @@
+/*
+       @@@@@@@@@@@@@>      @@@@@@@@@@>
+       @>    @@@>  @>      @>         
+       @>     @>   @>      @@@@@@@@@@>
+       @>          @>               @>
+       @>          @>               @>
+       @>          @>      @@@@@@@@@>       
+
+  Use this file to configure production-environment specific settings. The logical include "@require m5.env"
+  will include m5.env.<environment>.js automatically.
+  
+*/
+M5.settings.app_name = '%%NAME%%'
