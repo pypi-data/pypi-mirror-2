7546d6dd963664cfdec5be44390020cc153c58c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:18 +0000

    Add package plonetheme.laboral/0.5/plonetheme.laboral-0.5-py2.6.egg

---
+++ packages/plonetheme.laboral/plonetheme.laboral-0.5-py2.6.egg/plonetheme/laboral/browser/interfaces.py
@@ -0,0 +1,14 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+from zope.viewlet.interfaces import IViewletManager
+
+class IThemeSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer.
+    """
+
+class IHeadManager(IViewletManager):
+    """Viewlet manager on top of the site to contain the logo and searchbox
+    """
+
+class IFooterManager(IViewletManager):
+    """Viewlet manager on bottom of the site to contain the login menu
+    """
