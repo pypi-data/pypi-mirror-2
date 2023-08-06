ca2b1422d4a294bdd7a9932557fd5e3e0a297888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:08 +0000

    Add package poly2tri/0.3.2/poly2tri-0.3.2.tar.gz

---
+++ packages/poly2tri/poly2tri-0.3.2.tar.gz/poly2tri-0.3.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from distutils.extension import Extension
+
+import sys
+if 'setuptools.extension' in sys.modules:
+    m = sys.modules['setuptools.extension']
+    m.Extension.__dict__ = m._Extension.__dict__
+
+
+CYTHON_SOURCES =  """src/cdt.pxd
+src/cdt.pxi
+src/p2t.cpp
+src/p2t.pxd
+src/p2t.pyx
+src/shapes.pxd
+src/shapes.pxi""".split("\n")
+
+CPP_SOURCES = """poly2tri/common/shapes.cc
+poly2tri/sweep/advancing_front.cc
+poly2tri/sweep/cdt.cc
+poly2tri/sweep/sweep.cc
+poly2tri/sweep/sweep_context.cc""".split("\n")
+
+mod_math = Extension(
+    "p2t",
+    CYTHON_SOURCES + CPP_SOURCES,
+    language = "c++"
+)
+
+setup(
+    name = "poly2tri",
+    version = "0.3.2",
+    author = "Mason Green",
+    description = "A 2D constrained Delaunay triangulation library",
+    url = "http://code.google.com/p/poly2tri/",
+
+    ext_modules = [mod_math],
+    setup_requires = ["cython==0.14.1", "setuptools_cython==0.2.1"],
+    install_requires = ["cython==0.14.1"],
+)
