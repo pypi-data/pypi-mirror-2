4a93e6ae36f6675fc9bfb2e1496e43a4f32b3607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:45 +0000

    Add package pystatsd/0.1.1/pystatsd-0.1.1.tar.gz

---
+++ packages/pystatsd/pystatsd-0.1.1.tar.gz/pystatsd-0.1.1/README.txt
@@ -0,0 +1,26 @@
+Introduction
+------------
+
+pystatsd is a client and server implementation of Etsy's brilliant statsd server, a front end/proxy for the Graphite stats collection and graphing server.
+
+* Graphite
+    - http://graphite.wikidot.com
+* Statsd 
+    - code: https://github.com/etsy/statsd
+    - blog post: http://codeascraft.etsy.com/2011/02/15/measure-anything-measure-everything/
+
+Usage
+-------------
+
+See statsd_test for sample usage:
+
+    from pystatsd import Client, Server
+
+    sc = Client('example.org',8125)
+
+    sc.timing('python_test.time',500)
+    sc.increment('python_test.inc_int')
+    sc.decrement('python_test.decr_int')
+
+    srvr = Server(debug=True)
+    srvr.serve()
---
+++ packages/pystatsd/pystatsd-0.1.1.tar.gz/pystatsd-0.1.1/pystatsd/statsd.py
@@ -0,0 +1,86 @@
+# statsd.py
+
+# Steve Ivy <steveivy@gmail.com>
+# http://monkinetic.com
+
+import socket
+import random
+
+# Sends statistics to the stats daemon over UDP
+class Client(object):
+    
+    def __init__(self, host='localhost', port=8125):
+        """
+        Create a new Statsd client.
+        * host: the host where statsd is listening, defaults to localhost
+        * port: the port where statsd is listening, defaults to 8125
+
+        >>> from pystatsd import statsd
+        >>> stats_client = statsd.Statsd(host, port)
+        """
+        self.host = host
+        self.port = port
+    
+    def timing(self, stat, time, sample_rate=1):
+        """
+        Log timing information for a single stat
+        >>> from pystatsd import statsd
+        >>> statsd_client.timing('some.time','500|ms')
+        """
+        stats = {stat: "%d|ms" % time}
+        self.send(stats, sample_rate)
+
+    def increment(self, stats, sample_rate=1):
+        """
+        Increments one or more stats counters
+        >>> statsd_client.increment('some.int')
+        >>> statsd_client.increment('some.int',0.5)
+        """
+        self.update_stats(stats, 1, sample_rate)
+
+    def decrement(self, stats, sample_rate=1):
+        """
+        Decrements one or more stats counters
+        >>> statsd_client.decrement('some.int')
+        """
+        self.update_stats(stats, -1, sample_rate)
+    
+    def update_stats(self, stats, delta=1, sampleRate=1):
+        """
+        Updates one or more stats counters by arbitrary amounts
+        >>> statsd_client.update_stats('some.int',10)
+        """
+        if (type(stats) is not list):
+            stats = [stats]
+        data = {}
+        for stat in stats:
+            data[stat] = "%s|c" % delta
+
+        self.send(data, sampleRate)
+    
+    def send(self, data, sample_rate=1):
+        """
+        Squirt the metrics over UDP
+        """
+        addr=(self.host, self.port)
+        
+        sampled_data = {}
+        
+        if(sample_rate < 1):
+            if random.random() <= sample_rate:
+                for stat, value in data.iteritems():
+                    value = data[stat]
+                    sampled_data[stat] = "%s|@%s" %(value, sample_rate)
+        else:
+            sampled_data=data
+        
+        udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            for stat, value in sampled_data.iteritems():
+                send_data = "%s:%s" % (stat, value)
+                udp_sock.sendto(send_data, addr)
+        except:
+            import sys
+            from pprint import pprint
+            print "Unexpected error:", pprint(sys.exc_info())
+            pass # we don't care
