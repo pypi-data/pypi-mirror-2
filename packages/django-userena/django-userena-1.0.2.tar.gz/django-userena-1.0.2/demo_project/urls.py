4b5b2cc490a29d48eb88483070e908998a18be68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:01 +0000

    Add package django-userena/1.0.2/django-userena-1.0.2.tar.gz

---
+++ packages/django-userena/django-userena-1.0.2.tar.gz/django-userena-1.0.2/demo_project/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/', include('userena.urls')),
+    (r'^messages/', include('userena.contrib.umessages.urls')),
+    url(r'^$',
+        direct_to_template,
+        {'template': 'static/promo.html'},
+        name='promo'),
+    (r'^i18n/', include('django.conf.urls.i18n')),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$',
+         'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True, }),
+)
