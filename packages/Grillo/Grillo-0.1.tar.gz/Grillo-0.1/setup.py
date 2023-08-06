feb83734abde56704c2d66a564c21aa1e4090d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:34 +0000

    Add package Grillo/0.1/Grillo-0.1.tar.gz

---
+++ packages/Grillo/Grillo-0.1.tar.gz/Grillo-0.1/setup.py
@@ -0,0 +1,37 @@
+
+from setuptools import setup, find_packages
+
+from grillo import __version__
+
+
+setup(
+
+    name="Grillo",
+    version=__version__,
+    author="Stephen McDonald",
+    author_email="stephen.mc@gmail.com",
+    description="A terminal based chat server and client.",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    url="http://github.com/stephenmcd/grillo",
+    zip_safe=False,
+    py_modules=["grillo"],
+
+    entry_points="""
+        [console_scripts]
+        grillo=grillo:main
+    """,
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Chat",
+        "Topic :: Terminals :: Telnet",
+    ]
+
+)
