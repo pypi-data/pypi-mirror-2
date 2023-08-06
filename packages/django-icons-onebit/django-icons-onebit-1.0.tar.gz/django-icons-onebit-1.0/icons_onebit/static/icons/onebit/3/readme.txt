0903c4aff4a7ec52291d7ee673dd2fd4e4fa1833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:51 +0000

    Add package django-icons-onebit/1.0/django-icons-onebit-1.0.tar.gz

---
+++ packages/django-icons-onebit/django-icons-onebit-1.0.tar.gz/django-icons-onebit-1.0/icons_onebit/static/icons/onebit/3/readme.txt
@@ -0,0 +1,9 @@
+Onebit icons #3
+
+Ammount of icons:
+25
+
+File Types:
+.png
+
+More icons at http://www.icojoy.com
---
+++ packages/django-icons-onebit/django-icons-onebit-1.0.tar.gz/django-icons-onebit-1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup
+PACK_NAME = 'onebit'
+
+setup(
+    name = 'django-icons-' + PACK_NAME,
+    version = '1.0',
+    author = 'Mikhail Korobov',
+    author_email = 'kmike84@gmail.com',
+    url = 'http://bitbucket.org/kmike/django-icons/',
+    description = PACK_NAME + ' icons pack for django.contrib.staticfiles',
+    long_description = open('README.rst').read(),
+    license = 'MIT license',
+
+    packages=['icons_'+PACK_NAME],
+    include_package_data = True,
+    install_requires=['distribute'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
