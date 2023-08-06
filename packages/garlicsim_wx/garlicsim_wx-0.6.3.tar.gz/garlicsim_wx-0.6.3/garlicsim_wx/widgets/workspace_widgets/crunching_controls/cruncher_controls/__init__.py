e0dc4bcf73e6e44b8448c0ae6cb4c6d4fcd24546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:03 +0000

    Add package garlicsim_wx/0.6.3/garlicsim_wx-0.6.3.tar.gz

---
+++ packages/garlicsim_wx/garlicsim_wx-0.6.3.tar.gz/garlicsim_wx-0.6.3/garlicsim_wx/widgets/workspace_widgets/crunching_controls/cruncher_controls/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+Defines the `CruncherControls` class.
+
+See its documentation for more details.
+'''
+
+from .cruncher_controls import CruncherControls
---
+++ packages/garlicsim_wx/garlicsim_wx-0.6.3.tar.gz/garlicsim_wx-0.6.3/garlicsim_wx/widgets/workspace_widgets/playback_controls/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+Defines the `PlaybackControls` class.
+
+See its documentation for more info.
+'''
+
+from .playback_controls import PlaybackControls
+
+__all__ = ["PlaybackControls"]
