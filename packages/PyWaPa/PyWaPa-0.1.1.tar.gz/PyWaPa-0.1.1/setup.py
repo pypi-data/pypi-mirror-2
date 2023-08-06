79ea0ae81a5bba261192c799bad57a1182aaf280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:42 +0000

    Add package PyWaPa/0.1.1/PyWaPa-0.1.1.tar.gz

---
+++ packages/PyWaPa/PyWaPa-0.1.1.tar.gz/PyWaPa-0.1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='PyWaPa',
+    version = '0.1.1',
+    description = 'Python WhAtever Parser is a python markup converter from xml, json, yaml and ini to python dictionary. It allows also conversion between markup languages.',
+    author = 'FELD Boris',
+    author_email = 'lothiraldan@gmail.com',
+    url = 'http://bitbucket.org/lothiraldan/pywapa',
+    packages = ['pywapa', 'pywapa.parsers'],
+    requires = ['pyyaml'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: XML',
+    ],
+)
