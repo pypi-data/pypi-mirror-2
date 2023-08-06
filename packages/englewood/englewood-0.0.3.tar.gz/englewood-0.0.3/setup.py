baea9988d44363a8d092d8973957b450cdbdab3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:32 +0000

    Add package englewood/0.0.3/englewood-0.0.3.tar.gz

---
+++ packages/englewood/englewood-0.0.3.tar.gz/englewood-0.0.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+def readme(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='englewood',
+    version='0.0.3',
+    description='Tools for deriving different types of maps from shapefiles.',
+    long_description=readme('README.rst'),
+    author='Christopher Groskopf',
+    author_email='staringmonkey@gmail.com',
+    url='http://blog.apps.chicagotribune.com/',
+    license='MIT',
+    packages=[
+        'englewood', 
+    ],
+    install_requires = [
+        'GDAL==1.6.0',
+        ]
+)
