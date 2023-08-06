094166bc63f95b3e6d267366c7b29615a3a00630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:59 +0000

    Add package grokcore.layout/1.4/grokcore.layout-1.4.tar.gz

---
+++ packages/grokcore.layout/grokcore.layout-1.4.tar.gz/grokcore.layout-1.4/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+
+readme_filename = os.path.join('src', 'grokcore', 'layout', 'README.txt')
+long_description = open(readme_filename).read() + '\n\n' + \
+                   open('CHANGES.txt').read()
+
+test_requires = [
+    'zope.annotation',
+    'zope.container',
+    'zope.schema',
+    'zope.security',
+    'zope.session',
+    'zope.site',
+    'zope.testing',
+    'zope.traversing',
+    ]
+
+setup(name='grokcore.layout',
+      version='1.4',
+      description="A layout component package for zope3 and Grok.",
+      long_description = long_description,
+      classifiers=[
+          "Framework :: Zope3",
+          "Programming Language :: Python",
+          "Programming Language :: Zope",
+          "Intended Audience :: Developers",
+          "Development Status :: 5 - Production/Stable",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
+      keywords='grok layout zope3 pagelet theming',
+      author='Souheil Chelfouh',
+      author_email='trollfot@gmail.com',
+      url='http://pypi.python.org/pypi/grokcore.layout',
+      license='GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages=['grokcore'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={'test': test_requires},
+      install_requires=[
+          'grokcore.component',
+          'grokcore.formlib',
+          'grokcore.security',
+          'grokcore.view >= 2.3',
+          'martian',
+          'setuptools',
+          'zope.authentication',
+          'zope.component >= 3.9.1',
+          'zope.errorview',
+          'zope.interface',
+          'zope.publisher',
+          ],
+      )
---
+++ packages/grokcore.layout/grokcore.layout-1.4.tar.gz/grokcore.layout-1.4/src/grokcore/layout/interfaces.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from grokcore.view import interfaces
+from zope.interface import Interface, Attribute
+
+
+class ILayout(Interface):
+    """Layout view.
+    """
+
+
+class IPage(interfaces.IGrokView):
+    """A view using a layout to render itself.
+    """
+
+    def content():
+        """Give you back the result of your Page to be included inside
+        the layout.
+        """
+
+
+class IBaseClasses(Interface):
+    Page = Attribute("Base class for a layout page.")
+
+    Layout = Attribute("Base class for layout.")
+
+    FormPage = Attribute("Base class for form page.")
+
+    AddFormPage = Attribute("Base class for add form page.")
+
+    EditFormPage = Attribute("Base class for edit form page.")
+
+    DisplayFormPage = Attribute("Base class for diplay form layout.")
+
+    UnauthorizedPage = Attribute("Base class for unauthorized page.")
+
+    NotFoundPage = Attribute("Base class for not found page.")
+
+    ExceptionPage = Attribute("Base class for exception page.")
+
+
+class IGrokcoreLayoutAPI(IBaseClasses):
+    """Grokcore layout API.
+    """
