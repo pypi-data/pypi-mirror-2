1fd73d596ce9be0bb7343e35dd2d897b31ca90da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:10 +0000

    Add package js.jquery_markitup/1.1.10-1/js.jquery_markitup-1.1.10-1.tar.gz

---
+++ packages/js.jquery_markitup/js.jquery_markitup-1.1.10-1.tar.gz/js.jquery_markitup-1.1.10-1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '1.1.10-1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='js.jquery_markitup',
+    version=version,
+    description="Fanstatic packaging of markitup",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Fanstatic Developers',
+    author_email='fanstatic@googlegroups.com',
+    url='https://github.com/do3cc/js.jquery_markitup',
+    packages=find_packages(),namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=[],
+    install_requires=[
+        'fanstatic',
+        'js.jquery',
+        'setuptools',
+        ],
+    entry_points={
+        'fanstatic.libraries': [
+            'markitup = js.jquery_markitup:library',
+            ],
+        },
+    )
