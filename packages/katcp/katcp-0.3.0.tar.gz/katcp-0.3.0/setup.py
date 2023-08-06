5fa1022fc5c714438a1a2a59191c1c1950b75745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:55 +0000

    Add package katcp/0.3.0/katcp-0.3.0.tar.gz

---
+++ packages/katcp/katcp-0.3.0.tar.gz/katcp-0.3.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from katcp import version
+
+setup (
+    name = "katcp",
+    version = version.VERSION_STR,
+    description = "Karoo Array Telescope Communication Protocol library",
+    author = "Simon Cross",
+    author_email = "simon.cross@ska.ac.za",
+    packages = find_packages(),
+    scripts = [
+        "scripts/katcp-exampleserver.py",
+        "scripts/katcp-exampleclient.py",
+    ],
+    url='http://pypi.python.org/pypi/katcp',
+    download_url='http://pypi.python.org/pypi/katcp',
+    license="BSD",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+    platforms = [ "OS Independent" ],
+    keywords="kat kat7 ska",
+    zip_safe = False,
+    # Bitten Test Suite
+    test_suite = "katcp.test.suite",
+)
