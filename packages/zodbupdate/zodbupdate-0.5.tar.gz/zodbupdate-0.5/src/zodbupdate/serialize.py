c523d91bad92c895e4c90b055a14bef5c98e1b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:12 +0000

    Add package zodbupdate/0.5/zodbupdate-0.5.tar.gz

---
+++ packages/zodbupdate/zodbupdate-0.5.tar.gz/zodbupdate-0.5/src/zodbupdate/serialize.py
@@ -0,0 +1,284 @@
+##############################################################################
+#
+# Copyright (c) 2009-2010 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import cPickle
+import cStringIO
+import logging
+import types
+import sys
+
+from ZODB.broken import find_global, Broken, rebuild
+from zodbupdate import utils
+
+logger = logging.getLogger('zodbupdate')
+known_broken_modules = {}
+
+
+def is_broken(symb):
+    """Return true if the given symbol is broken.
+    """
+    return isinstance(symb, types.TypeType) and Broken in symb.__mro__
+
+
+def create_broken_module_for(symb):
+    """If your pickle refer a broken class (not an instance of it, a
+       reference to the class symbol itself) you have no choice than
+       having this module available in the same symbol and with the
+       same name, otherwise repickling doesn't work (as both pickle
+       and cPikle __import__ the module, and verify the class symbol
+       is the same than the one provided).
+    """
+    parts = symb.__module__.split('.')
+    previous = None
+    for fullname, name in reversed(
+        [('.'.join(parts[0:p+1]), parts[p]) for p in range(1, len(parts))]):
+        if fullname not in sys.modules:
+            if fullname not in known_broken_modules:
+                module = types.ModuleType(fullname)
+                module.__name__ = name
+                module.__file__ = '<broken module to pickle class reference>'
+                module.__path__ = []
+                known_broken_modules[fullname] = module
+            else:
+                if previous:
+                    module = known_broken_modules[fullname]
+                    setattr(module, *previous)
+                break
+            if previous:
+                setattr(module, *previous)
+            previous = (name, module)
+        else:
+            if previous:
+                setattr(sys.modules[fullname], *previous)
+                break
+    if symb.__module__ in known_broken_modules:
+        setattr(known_broken_modules[symb.__module__], symb.__name__, symb)
+    elif symb.__module__ in sys.modules:
+        setattr(sys.modules[symb.__module__], symb.__name__, symb)
+
+
+class BrokenModuleFinder(object):
+    """This broken module finder works with create_broken_module_for.
+    """
+
+    def load_module(self, fullname):
+        module = known_broken_modules[fullname]
+        if fullname not in sys.modules:
+            sys.modules[fullname] = module
+        module.__loader__ = self
+        return module
+
+    def find_module(self, fullname, path=None):
+        if fullname in known_broken_modules:
+            return self
+        return None
+
+
+sys.meta_path.append(BrokenModuleFinder())
+
+
+class NullIterator(object):
+    """An empty iterator that doesn't gives any result.
+    """
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        raise StopIteration
+
+
+class IterableClass(type):
+
+    def __iter__(cls):
+        """Define a empty iterator to fix unpickling of missing
+        Interfaces that have been used to do alsoProvides on a another
+        pickled object.
+        """
+        return NullIterator()
+
+
+class ZODBBroken(Broken):
+    """Extend ZODB Broken to work with broken objects that doesn't
+    have any __Broken_newargs__ sets (which happens if their __new__
+    method is not called).
+    """
+    __metaclass__ = IterableClass
+
+    def __reduce__(self):
+        """We pickle broken objects in hope of being able to fix them later.
+        """
+        return (rebuild,
+                ((self.__class__.__module__, self.__class__.__name__)
+                 + getattr(self, '__Broken_newargs__', ())),
+                self.__Broken_state__)
+
+
+class ZODBReference(object):
+    """Class to remenber reference we don't want to touch.
+    """
+
+    def __init__(self, ref):
+        self.ref = ref
+
+
+class ObjectRenamer(object):
+    """This load and save a ZODB record, modifying all references to
+    renamed class according the given renaming rules:
+
+    - in global symbols contained in the record,
+
+    - in persistent reference information,
+
+    - in class information (first pickle of the record).
+    """
+
+    def __init__(self, changes, pickler_name='C'):
+        self.__added = dict()
+        self.__changes = dict()
+        for old, new in changes.iteritems():
+            self.__changes[tuple(old.split(' '))] = tuple(new.split(' '))
+        self.__changed = False
+        self.__pickler_name = pickler_name
+
+    def __update_symb(self, symb_info):
+        """This method look in a klass or symbol have been renamed or
+        not. If the symbol have not been renamed explicitly, it's
+        loaded and its location is checked to see if it have moved as
+        well.
+        """
+        if symb_info in self.__changes:
+            self.__changed = True
+            return self.__changes[symb_info]
+        else:
+            symb = find_global(*symb_info, Broken=ZODBBroken)
+            if is_broken(symb):
+                logger.warning(
+                    u'Warning: Missing factory for %s' % u' '.join(symb_info))
+                create_broken_module_for(symb)
+            elif hasattr(symb, '__name__') and hasattr(symb, '__module__'):
+                new_symb_info = (symb.__module__, symb.__name__)
+                if new_symb_info != symb_info:
+                    logger.info(
+                        u'New implicit rule detected %s to %s' %
+                        (u' '.join(symb_info), u' '.join(new_symb_info)))
+                    self.__changes[symb_info] = new_symb_info
+                    self.__added[symb_info] = new_symb_info
+                    self.__changed = True
+                    return new_symb_info
+        return symb_info
+
+    def __find_global(self, *klass_info):
+        """Find a class with the given name, looking for a renaming
+        rule first.
+
+        Using ZODB find_global let us manage missing classes.
+        """
+        return find_global(*self.__update_symb(klass_info), Broken=ZODBBroken)
+
+    def __persistent_load(self, reference):
+        """Load a persistent reference. The reference might changed
+        according a renaming rules. We give back a special object to
+        represent that reference, and not the real object designated
+        by the reference.
+        """
+        if isinstance(reference, tuple):
+            oid, klass_info = reference
+            if isinstance(klass_info, tuple):
+                klass_info = self.__update_symb(klass_info)
+            return ZODBReference((oid, klass_info))
+        if isinstance(reference, list):
+            mode, information = reference
+            if mode == 'm':
+                database_name, oid, klass_info = information
+                if isinstance(klass_info, tuple):
+                    klass_info = self.__update_symb(klass_info)
+                return ZODBReference(['m', (database_name, oid, klass_info)])
+        return ZODBReference(reference)
+
+    def __unpickler(self, input_file):
+        """Create an unpickler with our custom global symbol loader
+        and reference resolver.
+        """
+        return utils.UNPICKLERS[self.__pickler_name](
+            input_file, self.__persistent_load, self.__find_global)
+
+    def __persistent_id(self, obj):
+        """Save the given object as a reference only if it was a
+        reference before. We re-use the same information.
+        """
+        if not isinstance(obj, ZODBReference):
+            return None
+        return obj.ref
+
+    def __pickler(self, output_file):
+        """Create a pickler able to save to the given file, objects we
+        loaded while paying attention to any reference we loaded.
+        """
+        pickler = cPickle.Pickler(output_file, 1)
+        pickler.persistent_id = self.__persistent_id
+        return pickler
+
+    def __update_class_meta(self, class_meta):
+        """Update class information, which can contain information
+        about a renamed class.
+        """
+        if isinstance(class_meta, tuple):
+            symb, args = class_meta
+            if is_broken(symb):
+                symb_info = (symb.__module__, symb.__name__)
+                logger.warning(
+                    u'Warning: Missing factory for %s' % u' '.join(symb_info))
+                return (symb_info, args)
+            elif isinstance(symb, tuple):
+                return self.__update_symb(symb), args
+        return class_meta
+
+    def rename(self, input_file):
+        """Take a ZODB record (as a file object) as input. We load it,
+        replace any reference to renamed class we know of. If any
+        modification are done, we save the record again and return it,
+        return None otherwise.
+        """
+        self.__changed = False
+
+        unpickler = self.__unpickler(input_file)
+        class_meta = unpickler.load()
+        data = unpickler.load()
+
+        class_meta = self.__update_class_meta(class_meta)
+
+        if not (self.__changed or
+                (hasattr(unpickler, 'need_repickle') and
+                 unpickler.need_repickle())):
+            return None
+
+        output_file = cStringIO.StringIO()
+        pickler = self.__pickler(output_file)
+        try:
+            pickler.dump(class_meta)
+            pickler.dump(data)
+        except cPickle.PicklingError, error:
+            logger.error('Error: cannot pickling modified record: %s' % error)
+            # Could not pickle that record, skip it.
+            return None
+
+        output_file.truncate()
+        return output_file
+
+    def get_found_implicit_rules(self):
+        result = {}
+        for old, new in self.__added.items():
+            result[' '.join(old)] = ' '.join(new)
+        return result
---
+++ packages/zodbupdate/zodbupdate-0.5.tar.gz/zodbupdate-0.5/src/zodbupdate/update.py
@@ -0,0 +1,119 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from ZODB.FileStorage import FileStorage
+from struct import pack, unpack
+import ZODB.POSException
+import ZODB.broken
+import ZODB.utils
+import cStringIO
+import logging
+import transaction
+import zodbupdate.serialize
+
+logger = logging.getLogger('zodbupdate')
+
+TRANSACTION_COUNT = 100000
+
+
+class Updater(object):
+    """Update class references for all current objects in a storage."""
+
+    def __init__(self, storage, dry=False, renames=None,
+                 start_at='0x00', debug=False, pickler_name='C'):
+        self.dry = dry
+        self.storage = storage
+        self.processor = zodbupdate.serialize.ObjectRenamer(
+            renames or {}, pickler_name)
+        self.start_at = start_at
+        self.debug = debug
+
+    def __new_transaction(self):
+        t = transaction.Transaction()
+        self.storage.tpc_begin(t)
+        t.note('Updated factory references using `zodbupdate`.')
+        return t
+
+    def __commit_transaction(self, t, changed):
+        if self.dry or not changed:
+            logger.info('Dry run selected or no changes, aborting transaction.')
+            self.storage.tpc_abort(t)
+        else:
+            logger.info('Committing changes.')
+            self.storage.tpc_vote(t)
+            self.storage.tpc_finish(t)
+
+    def __call__(self):
+        try:
+            count = 0
+            t = self.__new_transaction()
+
+            for oid, serial, current in self.records:
+                logger.debug('Processing OID %s' % ZODB.utils.oid_repr(oid))
+
+                new = self.processor.rename(current)
+                if new is None:
+                    continue
+
+                logger.debug('Updated OID %s' % ZODB.utils.oid_repr(oid))
+                self.storage.store(oid, serial, new.getvalue(), '', t)
+                count += 1
+
+                if count > TRANSACTION_COUNT:
+                    count = 0
+                    self.__commit_transaction(t, True)
+                    t = self.__new_transaction()
+
+            self.__commit_transaction(t, count != 0)
+        except (Exception,), e:
+            if not self.debug:
+                raise e
+            import sys, pdb
+            (type, value, traceback) = sys.exc_info()
+            pdb.post_mortem(traceback)
+            del traceback
+            raise e
+
+    @property
+    def records(self):
+        next = ZODB.utils.repr_to_oid(self.start_at)
+        if not isinstance(self.storage, FileStorage):
+            # Only FileStorage as _index (this is not an API defined attribute)
+            while True:
+                oid, tid, data, next = self.storage.record_iternext(next)
+                yield oid, tid, cStringIO.StringIO(data)
+                if next is None:
+                    break
+        else:
+            index = self.storage._index
+
+            while True:
+                oid = index.minKey(next)
+                try:
+                    data, tid = self.storage.load(oid, "")
+                except ZODB.POSException.POSKeyError, e:
+                    logger.error(
+                        u'Warning: Jumping record %s, '
+                        u'referencing missing key in database: %s' %
+                        (ZODB.utils.oid_repr(oid), str(e)))
+                else:
+                    yield  oid, tid, cStringIO.StringIO(data)
+
+                oid_as_long, = unpack(">Q", oid)
+                next = pack(">Q", oid_as_long + 1)
+                try:
+                    next = index.minKey(next)
+                except ValueError:
+                    # No more records
+                    break
