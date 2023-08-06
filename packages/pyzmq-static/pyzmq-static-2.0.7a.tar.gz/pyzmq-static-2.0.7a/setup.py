a0bae72c38ad87985f9ba6d368c1c189c5a163c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:33 +0000

    Add package pyzmq-static/2.0.7a/pyzmq-static-2.0.7a.tar.gz

---
+++ packages/pyzmq-static/pyzmq-static-2.0.7a.tar.gz/pyzmq-static-2.0.7a/setup.py
@@ -0,0 +1,39 @@
+import sys
+from distutils.core import setup, Extension
+from glob import glob
+
+sources = glob('src/*')
+libraries = []
+include_dirs = ['include']
+
+if hasattr(sys, 'getwindowsversion'):
+    libraries.extend([ 'rpcrt4', 'ws2_32' ])
+    include_dirs.append('include_nt')
+else:
+    sources.extend(glob('src_uuid/*.c'))
+    include_dirs.append('include_linux')
+
+ext = Extension('zmq._zmq', sources, libraries=libraries,
+                include_dirs=include_dirs)
+
+setup(name='pyzmq-static',
+      version='2.0.7a',
+      description='Statically linked Python bindings for 0MQ',
+      long_description=open('README.txt').read(),
+      author='Brandon Craig Rhodes',
+      author_email='brandon@rhodesmill.org',
+      url='http://bitbucket.org/brandon/pyzmq-static',
+      packages = ['zmq', 'zmq.tests', 'zmq.eventloop'],
+      ext_modules=[ ext ],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        #'Operating System :: MacOS :: MacOS X',  # must test!
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Topic :: System :: Networking'
+        ]
+      )
