0aacb78e937f477d9415d42cf4d7e50f3fc07dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:37 +0000

    Add package jmbo/0.1.7/jmbo-0.1.7.tar.gz

---
+++ packages/jmbo/jmbo-0.1.7.tar.gz/jmbo-0.1.7/CHANGELOG.rst
@@ -0,0 +1,36 @@
+Changelog
+=========
+
+0.1.7 (2011-06-15)
+------------------
+#. Jmbo rename.
+
+0.1.6
+-----
+#. Added state admin bulk actions.
+
+0.1.5
+-----
+#. Use photologue 2.6.praekelt
+
+0.1.4
+-----
+#. Generate slug optimization.
+
+0.1.3
+-----
+#. Refactored ModelBase.comment_count to resolve comments for leaf class or modelbase content types.
+
+0.1.2
+-----
+#. Generic form issues corrected.
+
+0.1.1
+-----
+#. Use django-photologue 2.5.praekelt
+
+0.1.0
+-----
+#. Improved generate_slug utils method.
+#. Removed ModelBaseAdminForm.
+
---
+++ packages/jmbo/jmbo-0.1.7.tar.gz/jmbo-0.1.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo',
+    version='0.1.7',
+    description='Jmbo base app.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://www.jmbo.org',
+    packages = find_packages(),
+    dependency_links = [
+        'http://dist.plone.org/thirdparty/',
+        'http://github.com/praekelt/django-photologue/tarball/2.6.praekelt#egg=django-photologue-2.6.praekelt',
+    ],
+    install_requires = [
+        'PIL',
+        'django-category',
+        'django-photologue==2.6.praekelt',
+        'django-publisher',
+        'django-secretballot',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
