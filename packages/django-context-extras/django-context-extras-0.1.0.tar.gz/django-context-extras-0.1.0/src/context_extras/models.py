2f0300cb1fbd893660e33d302113a24ec724baab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package django-context-extras/0.1.0/django-context-extras-0.1.0.tar.gz

---
+++ packages/django-context-extras/django-context-extras-0.1.0.tar.gz/django-context-extras-0.1.0/src/context_extras/models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+#  This file is part of django-context-extras.
+#
+#  django-context-extras provides some extra context processors for Django
+#  projects.
+#
+#  Development Web Site:
+#    - http://www.codetrax.org/projects/django-context-extras
+#
+#  Sponsored by:
+#    - http://www.g-loaded.eu/
+#    - http://www.codetrax.org/
+#
+#  Copyright (c) 2010 George Notaras <gnot@g-loaded.eu>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
