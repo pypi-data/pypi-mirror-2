81b4367121ca472ca3848233844e28d5d1bdf51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:37 +0000

    Add package collective.fastview/0.2.2/collective.fastview-0.2.2.zip

---
+++ packages/collective.fastview/collective.fastview-0.2.2.zip/collective.fastview-0.2.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2.2'
+
+setup(name='collective.fastview',
+      version=version,
+      description="View and viewlet helper modules for Plone theme and five.grok developers",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='five.grok viewlet view helper grok',
+      author='mFabrik Research Oy',
+      author_email='research@mfabrik.com',
+      url='http://webandmobile.mfabik.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+      )
