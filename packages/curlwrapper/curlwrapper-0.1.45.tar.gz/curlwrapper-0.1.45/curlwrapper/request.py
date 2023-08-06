08dab34135b5faf4bc4d98c5c8f01592f6329924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:13 +0000

    Add package curlwrapper/0.1.45/curlwrapper-0.1.45.tar.gz

---
+++ packages/curlwrapper/curlwrapper-0.1.45.tar.gz/curlwrapper-0.1.45/curlwrapper/request.py
@@ -0,0 +1,29 @@
+import urllib
+
+class Request:
+    """This is the Request object that contains the tricky stuff 
+    for a http request"""
+    def __init__(self, url='', 
+            post=None, 
+            referer='', 
+            proxy=None, 
+            auto_encode_post=True):
+        #unimplemented
+        self.cookies = ''
+        self.auto_encode_post = auto_encode_post
+        #implemented
+        self.url = url
+        if referer == '':
+            self.referer = self.url
+        else:
+            self.referer = referer
+        if isinstance(post, dict) and self.auto_encode_post:
+            self.post = urllib.urlencode(post)
+        else:
+            self.post = post
+        self.proxy = proxy
+        self.tries = 0
+        self.additional_headers = None
+    
+        return None    
+	
