97482f04e5d49caccc824055b2e80b6ac67d5d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:34 +0000

    Add package strippers.mixi/1.0a3/strippers.mixi-1.0a3-py2.6.egg

---
+++ packages/strippers.mixi/strippers.mixi-1.0a3-py2.6.egg/strippers/mixi/_profile_read.py
@@ -0,0 +1,22 @@
+# vim:fileencoding=utf-8
+from urllib import urlencode
+import json
+
+PEOPLE_URI = 'http://api.mixi-platform.com/2/people/%s/%s'
+
+def _get_friends(self, user_id='@me', group_id='@friends', sort_by=None, sort_order='ascending', count=20, start_index=0):
+    params = {
+            'count': count,
+            'startIndex': start_index,
+            'sortOrder': sort_order,
+            }
+    if sort_by:
+        params['sortBy'] = sort_by
+    uri = PEOPLE_URI % (str(user_id), str(group_id))
+    res = self.send_api_request(uri, params)
+    return json.loads(res)
+
+
+API_METHODS = {
+        'get_friends' : _get_friends,
+        }
