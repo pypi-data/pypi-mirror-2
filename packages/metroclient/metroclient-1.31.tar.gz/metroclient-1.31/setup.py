00a8caf17bb90da2ecbc19d5454c7435ea017ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:58 +0000

    Add package metroclient/1.31/metroclient-1.31.tar.gz

---
+++ packages/metroclient/metroclient-1.31.tar.gz/metroclient-1.31/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='metroclient',
+    version = '1.31',
+    author = 'AppHosted Team',
+    author_email = 'support@apphosted.com',
+    packages = ['metroclient'],
+    description = 'AppHosted application deployment utility',
+    url = 'https://apphosted.com',
+    license = 'License :: OSI Approved :: GNU General Public License (GPL)',
+    long_description = """
+    Metro is the application deployment utility for managing and deploying web applications
+    hosted on http://apphosted.com""",
+    download_url = 'https://metro.apphosted.com/client/get/',
+    scripts = ['metro'],
+    package_data = {
+        'metroclient': ['*.txt'],
+    },
+    install_requires = ['httplib2','paramiko'],
+    platforms = [
+        "All",
+        ],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities",
+        ]
+    )
+
