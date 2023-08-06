97bae5c77ae3cea136839bf33f636a054150d403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:33 +0000

    Add package veliberator/0.3.1/veliberator-0.3.1.tar.gz

---
+++ packages/veliberator/veliberator-0.3.1.tar.gz/veliberator-0.3.1/setup.py
@@ -0,0 +1,43 @@
+"""Setup script for veliberator"""
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+import veliberator
+
+setup(
+    name='veliberator',
+    version=veliberator.__version__,
+    license=veliberator.__license__,
+
+    author=veliberator.__author__,
+    author_email=veliberator.__email__,
+    url=veliberator.__url__,
+
+    description="Veliberator provides a Python API for getting informations" \
+    " about the stations of the Velib' network.",
+    long_description=open(os.path.join('README.rst')).read(),
+    keywords='velib, api, service',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+
+    scripts=['./veliberator/scripts/veliberator'],
+    test_suite='veliberator.tests.global_test_suite',
+    packages=find_packages(exclude=['tests']),
+
+    zip_safe=False,
+    platforms='any',
+    include_package_data=True,
+
+    install_requires=['SQLAlchemy>=0.7.2',
+                      'Elixir>=0.7.1',
+                      'simplejson>=2.1.6'],
+    )
