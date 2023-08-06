c87dc6b86faa3378281494ef3d17d22ef8d27b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:50 +0000

    Add package latimes-appengine-template/0.003/latimes-appengine-template-0.003.tar.gz

---
+++ packages/latimes-appengine-template/latimes-appengine-template-0.003.tar.gz/latimes-appengine-template-0.003/setup.py
@@ -0,0 +1,27 @@
+"""
+Tricks lifted from Django's own setup.py and django_debug_toolbar.
+"""
+from setuptools import setup, find_packages
+
+setup(
+      name='latimes-appengine-template',
+      version='0.003',
+      description='A basic template to start a new Django application hosted by Google App Engine.',
+      author='Ben Welsh',
+      author_email='ben.welsh@gmail.com',
+      url='https://github.com/datadesk/latimes-appengine-template',
+      download_url='git@github.com:datadesk/latimes-appengine-template.git',
+#      packages=find_packages(),
+#      package_data={
+#        'appengine_template': [
+#             '*.yaml',
+#             'django.zip',
+#            ],
+#        },
+      entry_points = {
+        'console_scripts': [
+            'startappengineproject = appengine_template.command:execute_from_command_line',
+        ],
+    },
+)
+
