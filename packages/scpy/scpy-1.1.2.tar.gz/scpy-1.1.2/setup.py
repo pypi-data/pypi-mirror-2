b41092176a14668c6e01634fe500dbf66a8d1ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:36 +0000

    Add package scpy/1.1.2/scpy-1.1.2.tar.gz

---
+++ packages/scpy/scpy-1.1.2.tar.gz/scpy-1.1.2/setup.py
@@ -0,0 +1,34 @@
+'''
+Created on Dec 12, 2010
+
+@author: falmarri
+'''
+from setuptools import setup
+setup(
+    name = "scpy",
+    version = "1.1.2",
+    scripts = ['scpy'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['pyinotify', 'pexpect'],
+
+
+    # metadata for upload to PyPI
+    author = "Falmarri",
+    author_email = "psychicsurgeon@gmail.com",
+    description = "File/Directory syncing over scp",
+    long_description=open('README','r').read(),
+    license = "GPL",
+    keywords = "scp sync autosync",
+    url = "http://weaknweary.blogspot.com/2010/10/python-enhancement-to-scp.html",   # project home page, if any
+
+    classifiers=['Topic :: System :: Networking',
+                 'Intended Audience :: Developers',
+                 'Operating System :: POSIX :: Linux',
+                 'Topic :: Utilities',
+                 ]
+
+
+    # could also include long_description, download_url, classifiers, etc.
+)
