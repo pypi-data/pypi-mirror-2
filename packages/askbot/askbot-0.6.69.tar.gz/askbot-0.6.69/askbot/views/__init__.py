686b2c0eb6c403b7f6cc16b3cd98b005e09ac844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:35 +0000

    Add package askbot/0.6.69/askbot-0.6.69.tar.gz

---
+++ packages/askbot/askbot-0.6.69.tar.gz/askbot-0.6.69/askbot/views/__init__.py
@@ -0,0 +1,9 @@
+"""
+:synopsis: django view functions for the askbot project
+"""
+from askbot.views import readers
+from askbot.views import writers
+from askbot.views import commands
+from askbot.views import users
+from askbot.views import meta
+from askbot.views import avatar_views
