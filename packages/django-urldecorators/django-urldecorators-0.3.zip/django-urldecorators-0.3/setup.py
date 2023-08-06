1d3d6c6c07a836a94f5f9db467c85b203a5c180b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:19 +0000

    Add package django-urldecorators/0.3/django-urldecorators-0.3.zip

---
+++ packages/django-urldecorators/django-urldecorators-0.3.zip/django-urldecorators-0.3/setup.py
@@ -0,0 +1,37 @@
+
+import codecs
+from setuptools import setup, find_packages
+
+
+VERSION = (0, 3)
+VERSION_STR = ".".join(map(str, VERSION))
+
+
+url='http://github.com/mila/django-urldecorators/tree/master'
+
+try:
+    long_description = codecs.open('README', "r", "utf-8").read()
+except IOError:
+    long_description = "See %s" % url
+
+
+setup(
+    name='django-urldecorators',
+    version=VERSION_STR,
+    description='Django-urldecorators is a reusable Django application which allows apply '
+                'view decorators and middleware components depending on requested url.',
+    author='Miloslav Pojman',
+    author_email='miloslav.pojman@gmail.com',
+    url=url,
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    long_description=long_description,
+)
