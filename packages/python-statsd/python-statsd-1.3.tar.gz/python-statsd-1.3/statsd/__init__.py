57d6064944bbd1f3d5579fa120cfaf39042d4d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:54 +0000

    Add package python-statsd/1.3/python-statsd-1.3.tar.gz

---
+++ packages/python-statsd/python-statsd-1.3.tar.gz/python-statsd-1.3/statsd/__init__.py
@@ -0,0 +1,7 @@
+from statsd.connection import Connection
+from statsd.client import Client
+from statsd.timer import Timer
+from statsd.counter import Counter, increment, decrement
+
+__all__ = ['Client', 'Connection', 'Timer', 'Counter', 'increment', 'decrement']
+
