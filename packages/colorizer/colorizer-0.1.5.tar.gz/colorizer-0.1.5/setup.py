6dc41405175dd4e7634c581ab1709aecfb8744d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:59 +0000

    Add package colorizer/0.1.5/colorizer-0.1.5.tar.gz

---
+++ packages/colorizer/colorizer-0.1.5.tar.gz/colorizer-0.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'colorizer',
+    version = '0.1.5',
+    description = 'Console colorizer, which acts like grep but paint each match in it\'s own color.',
+    author = 'Alexander Artemenko',
+    author_email = 'svetlyak.40wt@gmail.com',
+    url = 'http://github.com/svetlyak40wt/colorizer/',
+    license = 'New BSD License',
+    install_requires = ['termcolor'],
+    classifiers = [
+        'Environment :: Console',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+    ],
+    packages = find_packages(),
+    entry_points = """
+    [console_scripts]
+    colorize = colorizer:main
+    """,
+)
