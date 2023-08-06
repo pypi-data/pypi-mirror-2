82d4c1d044c4d977b7ce65b4387c6bdbbe4f8634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:38 +0000

    Add package Products.FacultyStaffDirectory/3.0b1/Products.FacultyStaffDirectory-3.0b1.zip

---
+++ packages/Products.FacultyStaffDirectory/Products.FacultyStaffDirectory-3.0b1.zip/Products.FacultyStaffDirectory-3.0b1/Products/FacultyStaffDirectory/setuphandlers.py
@@ -0,0 +1,22 @@
+from Products.CMFCore.utils import getToolByName
+from plone.app.workflow.remap import remap_workflow
+from Products.membrane.config import TOOLNAME as MEMBRANE_TOOL 
+
+def upgrade_2_to_3(context):
+
+    if context.readDataFile('upgrade_2_to_3.txt') is None:
+        return
+
+    portal = context.getSite()
+    logger = context.getLogger('FacultyStaffDirectory')
+    try:
+        remap_workflow(portal,
+                       ('FSDFacultyStaffDirectory',),
+                       ('fsd_directory_workflow',),
+                       {})
+    except Exception, message:
+        logger.error(message)
+        raise
+
+    mbtool = getToolByName(portal, MEMBRANE_TOOL)
+    mbtool.clearFindAndRebuild()
