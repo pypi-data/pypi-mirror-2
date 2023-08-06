0741da9029c3da379e82498b2d872dbaaac52ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:26 +0000

    Add package Ibidas/0.1.4/Ibidas-0.1.4.tar.gz

---
+++ packages/Ibidas/Ibidas-0.1.4.tar.gz/Ibidas-0.1.4/ibidas/constants.py
@@ -0,0 +1,13 @@
+
+UNDEFINED = -1
+
+
+BCCOPY=0
+BCENSURE=1
+BCEXIST=2
+BCNEW=3
+BCINSERT=4
+
+
+NOVAL="NOVALUE" #alternative None for functions which also accept NOne as valid input
+                #but still want some params to be optional
