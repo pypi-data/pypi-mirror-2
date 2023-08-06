1a62d5918b00128bd866f42e00ad1d97624a9436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:51 +0000

    Add package GrailTact/1.1/GrailTact-1.1.tar.gz

---
+++ packages/GrailTact/GrailTact-1.1.tar.gz/GrailTact-1.1/GrailTact/GrailTact/config.py
@@ -0,0 +1,14 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'GrailTact.GrailTact'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'Contactworkplaceandworktitle': 'GrailTact.GrailTact: Add Contact workplace and work title',
+    'Contactphonenumber': 'GrailTact.GrailTact: Add Contact phone number',
+    'Contactemailaddress': 'GrailTact.GrailTact: Add Contact email address',
+    'Contactnetaddress': 'GrailTact.GrailTact: Add Contact net address',
+    'Contact': 'GrailTact.GrailTact: Add Contact',
+    'Contactlandingpage': 'GrailTact.GrailTact: Add Contact landing page',
+}
