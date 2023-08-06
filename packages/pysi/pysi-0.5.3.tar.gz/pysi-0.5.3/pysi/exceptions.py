9a2a28b6ce595e811cd9ed3426d87a20dc49ef99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:01 +0000

    Add package pysi/0.5.3/pysi-0.5.3.tar.gz

---
+++ packages/pysi/pysi-0.5.3.tar.gz/pysi-0.5.3/pysi/exceptions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import wsgi
+
+
+class NotFound(Exception):
+    pass
+
+class BasicAuth(Exception):
+    def __init__(self):
+        self.res = wsgi.Response('Authorization Error', 
+            code=401, content_type='text/plain',
+            headers={'WWW-Authenticate' : 'Basic realm="Enter password"'},
+        )
