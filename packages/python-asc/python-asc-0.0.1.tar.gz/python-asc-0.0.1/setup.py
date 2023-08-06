db2e8439eace279d0454e5ee4d72b2031d31c20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:05 +0000

    Add package python-asc/0.0.1/python-asc-0.0.1.tar.gz

---
+++ packages/python-asc/python-asc-0.0.1.tar.gz/python-asc-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-asc',
+    version='0.0.1',
+    description='Simple Python library parsing ASC EPG feeds.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='https://github.com/praekelt/python-asc',
+    packages = find_packages(),
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
