f262a86ac13119ff66c62fe9ce3b6c799e81cc70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:30 +0000

    Add package collective.formcriteria/2.0b1/collective.formcriteria-2.0b1.tar.gz

---
+++ packages/collective.formcriteria/collective.formcriteria-2.0b1.tar.gz/collective.formcriteria-2.0b1/src/collective/formcriteria/criteria/path.py
@@ -0,0 +1,17 @@
+from Products.ATContentTypes.criteria import path
+
+from collective.formcriteria.criteria import common
+
+
+class FormPathCriterion(
+    common.FormCriterion, path.ATPathCriterion):
+    __doc__ = path.ATPathCriterion.__doc__
+
+    schema = path.ATPathCriterion.schema.copy(
+        ) + common.FormCriterion.schema.copy()
+    schema['value'].widget.hide_form_label = True
+    schema['formFields'].vocabulary = common.makeVocabularyForFields(
+        schema['value'], schema['recurse'])
+
+common.registerCriterion(
+    FormPathCriterion, orig=path.ATPathCriterion)
