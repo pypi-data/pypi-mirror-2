93f96d1ee243e1c22a6a6c8039dc60a31cb3e701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:08 +0000

    Add package django-lastfmauth/0.1a2/django-lastfmauth-0.1a2.tar.gz

---
+++ packages/django-lastfmauth/django-lastfmauth-0.1a2.tar.gz/django-lastfmauth-0.1a2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="django-lastfmauth",
+    version=__import__("lastfmauth").__version__,
+    url="http://github.com/matagus/django-lastfm-auth",
+    license="MIT",
+    description="Django app to let your website visitors register/login using LastFM authentication webservice.",
+
+    author="Matias Agustin Mendez",
+    author_email="me@matagus.com.ar",
+
+    packages=find_packages(),
+    package_dir={"lastfmauth": "lastfmauth"},
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Internet :: WWW/HTTP"
+    ],
+    zip_safe=False,
+)
