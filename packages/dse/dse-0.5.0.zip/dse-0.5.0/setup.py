604c51ae893d05bd956d002f8b0473937410a156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:57 +0000

    Add package dse/0.5.0/dse-0.5.0.zip

---
+++ packages/dse/dse-0.5.0.zip/dse-0.5.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='dse',
+      version='0.5.0',
+      description='DSE - Delayed SQL Executor',
+      author='Thomas Weholt',
+      author_email='thomas@weholt.org',
+      long_description=open('README.txt').read(),
+      py_modules = ['dse'],
+      install_requires = ('django'),
+      #data_files=[('docs', ['dse.html']),],
+      url = "https://bitbucket.org/weholt/dse",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Framework :: Django',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Database',
+          ],
+      )
