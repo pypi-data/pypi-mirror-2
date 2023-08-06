80cd8c79e2ae8c747c1aecab4af7cf9de6dd32a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:27 +0000

    Add package django_qrcode_filter/0.0.10alpha/django_qrcode_filter-0.0.10alpha.tar.gz

---
+++ packages/django_qrcode_filter/django_qrcode_filter-0.0.10alpha.tar.gz/django_qrcode_filter-0.0.10alpha/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages, setup
+
+setup(
+	name='django_qrcode_filter',
+	version='0.0.10alpha',
+	maintainer = "Zenobius Jiricek",
+	maintainer_email = "airtonix@gmail.com",
+	url="projects.airtonix.net/project/django-qrcode-filter",
+	description = "A simplet django application that provides a block and fitler templatetag to create inline DataURI based qrcode images.",
+	license='LICENSE.md',
+	packages=find_packages(exclude='tests'),
+	include_package_data = True
+)
+
