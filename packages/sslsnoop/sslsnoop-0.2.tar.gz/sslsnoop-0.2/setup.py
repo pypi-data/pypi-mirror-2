6af7a95562c3ce4a76ae808109862d2a6c320e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:45 +0000

    Add package sslsnoop/0.2/sslsnoop-0.2.tar.gz

---
+++ packages/sslsnoop/sslsnoop-0.2.tar.gz/sslsnoop-0.2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from glob import glob
+
+setup(name="sslsnoop",
+    version="0.2",
+    description="Dumps the live traffic of an ssl-encrypted stream.",
+    long_description=open('README').read(),
+
+    url="http://packages.python.org/sslsnoop/",
+    download_url="http://github.com/trolldbois/sslsnoop/tree/master",
+    license='GPL',
+    classifiers=[
+        "Topic :: System :: Networking",
+        "Topic :: Security",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    keywords=['memory','analysis','forensics','struct','ptrace','openssh','openssl','decrypt'],
+    author="Loic Jaquemet",
+    author_email="loic.jaquemet+python@gmail.com",
+    packages = ['sslsnoop'],
+    scripts = ['scripts/sslsnoop-openssh', 'scripts/sslsnoop-openssl'],
+    extras_require = {
+        'haystack':  ["python-haystack"],
+        'ptrace':  ["python-ptrace"],
+    },
+)
