6991e7d27d1676bc10853b84fe5be2b9dc7d3599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:18 +0000

    Add package tkinter.help/2.0/tkinter.help-2.0.zip

---
+++ packages/tkinter.help/tkinter.help-2.0.zip/tkinter.help-2.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+	name = "tkinter.help",
+	version = "2.0",
+	license = "MIT License",
+	description = "Small Preview of Tkinter Widgets",
+	long_description = "ENGLISH: tkhelp helps newbees! The program shows you the many widgets! \n GERMAN: tkhelp ist ein Programm, dass anderen Neueinsteigern helfen soll, die vielen Widgets zu sehen, und sie dann sogar einsetzen zu können. Aber das kommt noch!",
+	author = "Daemon Tutorials",
+	author_email = "logander4@icqmail.com",
+	url = "http://daemon-tuts.de",
+	py_modules = ["tkhelp"] ,
+	classifiers = [
+	"Development Status :: 5 - Production/Stable",
+	"Intended Audience :: End Users/Desktop",
+	"License :: OSI Approved :: MIT License",
+	"License :: Freeware",
+	"Natural Language :: German",
+	"Operating System :: MacOS :: MacOS X",
+	"Operating System :: Microsoft :: Windows",
+	"Operating System :: POSIX",
+	"Programming Language :: Python :: 3.1",
+	"Topic :: Desktop Environment",
+	"Topic :: Software Development :: Documentation"
+	]
+)
