8baed032e23adb1dad4c36549c6006bcef4d4c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:06 +0000

    Add package tgext.asyncjob/0.2.1/tgext.asyncjob-0.2.1.tar.gz

---
+++ packages/tgext.asyncjob/tgext.asyncjob-0.2.1.tar.gz/tgext.asyncjob-0.2.1/tgext/asyncjob/queue.py
@@ -0,0 +1,26 @@
+import Queue, tg, uuid
+from progress import asyncjobs_progress_status
+
+class AsyncJobQueue(object):
+    def __init__(self):
+        super(AsyncJobQueue, self).__init__()
+        self.queue = Queue.Queue()
+
+    def get(self):
+        return self.queue.get()
+
+    def done(self, entryid):
+        asyncjobs_progress_status.remove(entryid)
+        self.queue.task_done()
+
+    def perform(self, what, args, params):
+        uid = str(uuid.uuid1())
+        asyncjobs_progress_status.track(uid)
+        self.queue.put({'callable':what,
+                        'args':args,
+                        'params':params,
+                        'uid':uid})
+        return uid
+
+def asyncjob_perform(what, *args, **params):
+    return tg.app_globals.asyncjob_queue.perform(what, args, params)
