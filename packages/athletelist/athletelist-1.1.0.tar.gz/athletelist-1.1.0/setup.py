6562c953726f9d226163a92b607c9e80f10e03fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:30 +0000

    Add package athletelist/1.1.0/athletelist-1.1.0.tar.gz

---
+++ packages/athletelist/athletelist-1.1.0.tar.gz/athletelist-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+            name            = 'athletelist',
+            version         = '1.1.0',
+            py_modules      = ['athletelist'],
+            author          = 'nparsons',
+            author_email    = 'parsons.nicholas1@gmail.com',
+            url             = 'http://www.cs.albany.edu/~napt',
+            description     = 'An AthleteList to keep track of runner times'
+    )
