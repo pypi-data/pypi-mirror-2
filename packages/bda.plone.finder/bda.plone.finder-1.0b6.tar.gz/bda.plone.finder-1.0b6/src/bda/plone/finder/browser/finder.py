62047eadf1751d9c4b5398b45a9f909c3fe9f69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:21 +0000

    Add package bda.plone.finder/1.0b6/bda.plone.finder-1.0b6.tar.gz

---
+++ packages/bda.plone.finder/bda.plone.finder-1.0b6.tar.gz/bda.plone.finder-1.0b6/src/bda/plone/finder/browser/finder.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from zope.interface import implements
+from zope.component import getAdapters
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import ViewletBase
+from bda.plone.finder.interfaces import (
+    IFinder,
+    IAction,
+)
+from bda.plone.finder.browser.utils import (
+    anon,
+    get_provider,
+    ExecutionInfo,
+)
+
+class FinderViewlet(ViewletBase, ExecutionInfo):
+    
+    render = ViewPageTemplateFile('templates/viewlet.pt')
+    
+    def update(self):
+        self.show = not anon()
+    
+    @property
+    def base_url(self):
+        return self.request['ACTUAL_URL']
+
+class Finder(BrowserView, ExecutionInfo):
+    
+    implements(IFinder)
+    
+    __call__ = ViewPageTemplateFile('templates/finder.pt')
+    
+    @property
+    def show(self):
+        return not anon()
+    
+    @property
+    def actions(self):
+        flavor = self.flavor
+        groups = dict()
+        actions = list(getAdapters((self.context, self.request), IAction))
+        for id, action in actions:
+            if flavor != action.flavor:
+                continue
+            if not groups.get(action.group):
+                groups[action.group] = list()
+            groups[action.group].append({
+                'id': id,
+                'title': action.title,
+                'order': action.order,
+                'dropdown': action.dropdown,
+            })
+        ret = list()
+        for key in sorted(groups.keys()):
+            ret.append(sorted(groups[key],
+                        cmp=lambda x, y: x['order'] > y['order'] and 1 or -1))
+        return ret
+    
+    @property
+    def columns(self):
+        ret = list()
+        uid = self.uid
+        provider = get_provider(self.context, self.flavor, uid)
+        if provider is not None:
+            ret = provider.rendered_columns(uid)
+        while len(ret) < 4:
+            ret.append('<div class="finder_column">&nbsp;</div>')
+        return ret
---
+++ packages/bda.plone.finder/bda.plone.finder-1.0b6.tar.gz/bda.plone.finder-1.0b6/src/bda/plone/finder/browser/utils.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+from AccessControl import getSecurityManager
+from zope.component import (
+    getAdapters,
+    getUtility,
+)
+from bda.plone.finder.interfaces import (
+    IUidProvider,
+    IColumnProvider,
+)
+
+def anon():
+    user = getSecurityManager().getUser()
+    return user.has_role('Anonymous')
+
+def has_permission(permission, context):
+    mtool = context.portal_membership
+    if not mtool.checkPermission(permission, context):
+        return False
+    return True
+
+def col_id(id):
+    return 'finder_column_%s' % id
+
+def item_id(id):
+    return 'finder_nav_item_%s' % id
+
+def nav_item(uid,
+             icon,
+             title,
+             folderish=False,
+             selected=False,
+             state=None,
+             cut=False):
+    if len(title) > 29:
+        title = title.decode('utf-8')
+        title = '%s...%s' % (title[:12], title[-12:])
+    return {
+        'uid': uid,
+        'icon': icon,
+        'title': title,
+        'is_folderish': folderish,
+        'selected': selected,
+        'state': state,
+        'cut': cut,
+    }
+
+def get_provider(context, flavor, uid):
+    ret = list()
+    for name, provider in getAdapters((context, ), IColumnProvider):
+        if flavor != provider.flavor:
+            continue
+        if provider.provides(uid):
+            return provider
+    return None
+
+class ExecutionInfo(object):
+    
+    @property
+    def flavor(self):
+        return self.request.get('flavor', 'default')
+    
+    @property
+    def uid(self):
+        if self.request.get('uid'):
+            return self.request['uid']
+        uid_provider = getUtility(IUidProvider, name=self.flavor)
+        return uid_provider.uid(self.context, self.request)
+    
+    @property
+    def pobj(self):
+        return self.context.portal_url.getPortalObject()
+
+class ControlPanelItems(object):
+    
+    def __init__(self, context):
+        self.context = context
+    
+    def item_by_id(self, id, groups=['Plone', 'Products']):
+        for group in groups:
+            for item in self.items_by_group(group):
+                if item['id'] == id:
+                    return item
+    
+    def items_by_group(self, group):
+        """Group 'Plone' or 'Products'
+        """
+        return self.context.portal_controlpanel.enumConfiglets(group=group)
