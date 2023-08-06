f3cf710ea4bf38a49f7d3c4ceb34e7894fc3ad55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:11 +0000

    Add package mygengo/1.2.2/mygengo-1.2.2.tar.gz

---
+++ packages/mygengo/mygengo-1.2.2.tar.gz/mygengo-1.2.2/mygengo/test_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+"""
+	This file mainly exists for storing your public/private
+	keypair in a location that's not tracked through Git. To
+	run the pyGengo test suite, fill in the values below and copy
+	this file over to pygengo_test_keys.py.
+
+	e.g: cp pygengo_test_keys_example.py pygengo_test_keys.py
+"""
+
+public_key = 'XMg$PDzx-oZhKK^Uvr3H$cX-Z~5zBI[i~5Tx5E1U7u9n)29k$@_er^i2}wF_wmP2'
+private_key = 'b|Um7um^CKyWBz@G@w-Bf=sV}GVY{wHB)^v8@uKyyQuR1=kK6i3baDSGqSDp]}cL'
