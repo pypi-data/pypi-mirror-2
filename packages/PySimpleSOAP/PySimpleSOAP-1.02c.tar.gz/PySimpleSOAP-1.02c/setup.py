f6024237fe66f91110e1c85245683de524d6cbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:44 +0000

    Add package PySimpleSOAP/1.02c/PySimpleSOAP-1.02c.tar.gz

---
+++ packages/PySimpleSOAP/PySimpleSOAP-1.02c.tar.gz/PySimpleSOAP-1.02c/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PySimpleSOAP',
+      version='1.02c',
+      description='Python Simple SOAP Library',
+      author='Mariano Reingart',
+      author_email='reingart@gmail.com',
+      url='http://code.google.com/p/pysimplesoap',
+      packages=['pysimplesoap',],
+     )
+
