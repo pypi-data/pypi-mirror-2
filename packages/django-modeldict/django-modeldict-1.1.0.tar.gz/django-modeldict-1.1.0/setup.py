8b4586cabb4bcb01e8bdd5606c06af775c893826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:41 +0000

    Add package django-modeldict/1.1.0/django-modeldict-1.1.0.tar.gz

---
+++ packages/django-modeldict/django-modeldict-1.1.0.tar.gz/django-modeldict-1.1.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+
+setup(
+    name='django-modeldict',
+    version='1.1.0',
+    author='DISQUS',
+    author_email='opensource@disqus.com',
+    url='http://github.com/disqus/django-modeldict/',
+    description = 'Stores a model as a dictionary',
+    packages=find_packages(),
+    zip_safe=False,
+    tests_require=[
+        'django',
+    ],
+    test_suite = 'runtests.runtests',
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
