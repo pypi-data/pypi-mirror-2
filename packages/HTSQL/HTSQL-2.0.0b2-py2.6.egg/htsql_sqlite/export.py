eb0b1195a5e8759fece1323c9c7cb22b33fd991f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:19 +0000

    Add package HTSQL/2.0.0b2/HTSQL-2.0.0b2-py2.6.egg

---
+++ packages/HTSQL/HTSQL-2.0.0b2-py2.6.egg/htsql_sqlite/export.py
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2006-2010, Prometheus Research, LLC
+# Authors: Clark C. Evans <cce@clarkevans.com>,
+#          Kirill Simonov <xi@resolvent.net>
+#
+
+
+"""
+:mod:`htsql_sqlite.export`
+==========================
+
+This module exports the `engine.sqlite` addon.
+"""
+
+
+from htsql.addon import Addon
+import htsql_sqlite.connect
+import htsql_sqlite.split_sql
+import htsql_sqlite.introspect
+import htsql_sqlite.tr.serializer
+
+
+class ENGINE_SQLITE(Addon):
+    """
+    Declares the `engine.sqlite` addon.
+    """
+
+
