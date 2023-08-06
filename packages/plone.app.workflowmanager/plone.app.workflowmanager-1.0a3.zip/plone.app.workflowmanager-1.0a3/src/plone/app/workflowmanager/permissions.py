040aae6eae5a2a2a2331f4a0104ec1f4f19c8799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:19 +0000

    Add package plone.app.workflowmanager/1.0a3/plone.app.workflowmanager-1.0a3.zip

---
+++ packages/plone.app.workflowmanager/plone.app.workflowmanager-1.0a3.zip/plone.app.workflowmanager-1.0a3/src/plone/app/workflowmanager/permissions.py
@@ -0,0 +1,39 @@
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory(u"plone")
+
+managed_permissions = [
+    {'perm': 'Add portal content',
+     'name': _(u'Add'),
+     'description': _(u'Add content in this container.')
+    },
+
+    {'perm': 'Modify portal content',
+     'name': _(u'Edit'),
+     'description': _(u'Allows changing the content of an item.')
+    },
+
+    {'perm': 'View',
+     'name': _(u'View'),
+     'description': _(u'Not only means view HTML, but also FTP, WebDAV and other forms of access.')
+    },
+
+    {'perm': 'Review portal content',
+     'name': _(u'Review'),
+     'description': _(u'Allowed to review the content.')
+    },
+
+    {'perm': 'List folder contents',
+     'name': _(u'List'),
+     'description': _(u"List the contents of a folder. This doesn't check if you have access to view the actual object listed.")
+    },
+
+    {'perm': 'Access contents information',
+     'name': _(u'Access'),
+     'description': _(u'Allow access to content without necessarily viewing the object. For example, in a list of results.')
+    }]
+
+allowed_guard_permissions = {
+    'rr': _(u'Request review'),
+    'rpc': _(u'Review portal content'),
+    'mpc': _(u'Modify portal content')
+}
---
+++ packages/plone.app.workflowmanager/plone.app.workflowmanager-1.0a3.zip/plone.app.workflowmanager-1.0a3/src/plone/app/workflowmanager/tests/test_actions.py
@@ -0,0 +1,91 @@
+import unittest2 as unittest
+
+from plone.app.testing import TEST_USER_NAME, \
+    login
+
+from plone.app.workflowmanager.testing import INTEGRATION_MANAGER_TESTING, BaseTest
+from plone.app.workflowmanager.browser.actions import AddActionView, DeleteActionView
+from plone.app.workflowmanager.actionmanager import ActionManager
+
+
+class TestActions(BaseTest):
+
+    layer = INTEGRATION_MANAGER_TESTING
+
+    def test_adding_action(self):
+        portal = self.layer['portal']
+        login(portal, TEST_USER_NAME)
+        req = self.getRequest({
+            'form.actions.add' : 'Add',
+            'selected-transition' : 'retract',
+            'selected-workflow' : 'workflow-1'}, True)
+        view = AddActionView(portal, req)
+        res = view()
+        am = ActionManager()
+        rule = am.get_rule(view.selected_transition)
+        self.assertTrue(rule is not None)
+
+    def test_adding_action_fails(self):
+        portal = self.layer['portal']
+        login(portal, TEST_USER_NAME)
+        req = self.getRequest({
+            'selected-transition': 'retract',
+            'selected-workflow': 'workflow-1'}, True)
+        view = AddActionView(portal, req)
+        try:
+            res = view()
+        except AttributeError, ex:
+            self.assertTrue(str(ex) == "'TestRequest' object has no attribute 'RESPONSE'")
+        am = ActionManager()
+        rule = am.get_rule(view.selected_transition)
+        self.assertTrue(rule is None)
+
+    def test_accessing_adding_action(self):
+        portal = self.layer['portal']
+        login(portal, TEST_USER_NAME)
+
+        req = self.getRequest({
+            'selected-transition': 'publish',
+            'selected-workflow': 'workflow-1'}, True)
+        view = DeleteActionView(portal, req)
+        try:
+            res = view()
+        except AttributeError, ex:
+            self.assertTrue(str(ex) == "'TestRequest' object has no attribute 'RESPONSE'")
+        am = ActionManager()
+        rule = am.get_rule(view.selected_transition)
+        self.assertTrue(len(rule.actions) == 1)
+
+    def test_removing_action(self):
+        portal = self.layer['portal']
+        login(portal, TEST_USER_NAME)
+
+        req = self.getRequest({
+            'form.actions.delete': 'Delete',
+            'selected-transition': 'publish',
+            'selected-workflow': 'workflow-1',
+            'action_index': '0'}, True)
+        view = DeleteActionView(portal, req)
+        res = view()
+        am = ActionManager()
+        rule = am.get_rule(view.selected_transition)
+        self.assertEquals(len(rule.actions), 0)
+
+    def test_cancel_removing_action(self):
+        portal = self.layer['portal']
+        login(portal, TEST_USER_NAME)
+
+        req = self.getRequest({
+            'form.actions.cancel': 'Cancel',
+            'selected-transition': 'publish',
+            'selected-workflow': 'workflow-1'}, True)
+        view = DeleteActionView(portal, req)
+        res = view()
+        am = ActionManager()
+        rule = am.get_rule(view.selected_transition)
+        self.assertEquals(len(rule.actions), 1)
+
+        
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
