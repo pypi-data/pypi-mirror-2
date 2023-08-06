15fd9f063dc95b33eb68473f2de52d7a7de63645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:44 +0000

    Add package PyCIM/15.13.2/PyCIM-15.13.2.tar.gz

---
+++ packages/PyCIM/PyCIM-15.13.2.tar.gz/PyCIM-15.13.2/PyCIM/PrettyPrintXML.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2010-2011 Richard Lincoln
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from xml.etree.cElementTree import parse, tostring
+
+def xmlpp(source):
+    root = parse(source).getroot()
+    indent(root)
+    return tostring(root)
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
---
+++ packages/PyCIM/PyCIM-15.13.2.tar.gz/PyCIM-15.13.2/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+# Read the long description from the README.
+thisdir = os.path.abspath(os.path.dirname(__file__))
+f = open(os.path.join(thisdir, "README"))
+kwds = {"long_description": f.read()}
+f.close()
+
+setup(name="PyCIM",
+      version="15.13.2",
+      author="Richard Lincoln",
+      author_email="r.w.lincoln@gmail.com",
+      description="Python implementation of the Common Information Model.",
+      license="MIT",
+      url="http://www.pycim.com/",
+      include_package_data=False,
+      packages=find_packages(),
+      test_suite="PyCIM.Test",
+      zip_safe=True,
+      **kwds)
+
+# python setup.py sdist bdist_egg bdist_wininst bdist_msi upload
